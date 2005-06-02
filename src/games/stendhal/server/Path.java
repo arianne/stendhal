2a116cf2f43e5099e2cafbf4eb479b6a6c7caa33
Benjamin Lerman <quisar@users.sourceforge.net>
2005-06-02 07:15:39 +0000

    We add a visibility parameter to path finding...

--- src/games/stendhal/server/Path.java
+++ src/games/stendhal/server/Path.java
@@ -21,23 +21,23 @@ import games.stendhal.server.entity.*;
 
 import java.util.*;
 
-public class Path 
+public class Path
   {
   private static RPServerManager rpman;
   private static RPWorld world;
-  
+
   public static class Node
     {
     public int x;
     public int y;
-    
+
     public Node(int x, int y)
       {
       this.x=x;
       this.y=y;
       }
     }
-    
+
   public static void initialize(RPServerManager rpman, RPWorld world)
     {
     Path.rpman=rpman;
@@ -48,7 +48,7 @@ public class Path
     {
     int rndx=x-entity.getx();
     int rndy=y-entity.gety();
-    
+
     if(Math.abs(rndx)>Math.abs(rndy))
       {
       if(Math.signum(rndx)<0)
@@ -78,53 +78,63 @@ public class Path
 
     world.modify(entity);
     }
-  
+
   static class NavigableStendhalNode implements Navigable
     {
     private Entity entity;
     private Entity dest;
     private StendhalRPZone zone;
-    
+    private int radius;
+    private Pathfinder.Node center;
+
     public NavigableStendhalNode(Entity entity, Entity dest, StendhalRPZone zone)
       {
       this.entity=entity;
       this.dest=dest;
       this.zone=zone;
+      center = null;
       }
-    
+
+    public void setVisibility(int x, int y, int radius)
+      {
+      center = new Pathfinder.Node(x,y);
+      this.radius = radius;
+      }
+
     public boolean isValid(Pathfinder.Node node)
       {
       if(dest!=null && dest.distance(node.x,node.y)==0)
         {
         return true;
         }
-      else
+      if(getDistance(node, center) > radius)
         {
-        return !zone.collides(entity, node.getX(),node.getY());
-        }      
+        return false;
+        }
+      return !zone.collides(entity, node.getX(),node.getY());
       }
-      
+
     public double getCost(Pathfinder.Node n1, Pathfinder.Node n2)
       {
       return Math.abs(n1.getX()-n2.getX())+Math.abs(n1.getY()-n2.getY());
       }
-      
+
     public double getDistance(Pathfinder.Node n1, Pathfinder.Node n2)
       {
       return getCost(n1,n2);
       }
-   
+
     public int createNodeID(Pathfinder.Node node)
       {
       return node.getY()*zone.getWidth()+node.getX();
       }
-    
+
     public int maxNumberOfNodes()
       {
       return (zone.getWidth()*zone.getHeight())/10;
       }
     }
-    
+
   public static List<Node> searchPath(Entity entity, int x, int y, int destx, int desty)
     {
     Logger.trace("Path::searchPath",">");
@@ -139,12 +149,12 @@ public class Path
       {
       path.doStep();
       }
-    
+
     if(path.getStatus()==Pathfinder.IN_PROGRESS)
       {
       return new LinkedList<Node>();
       }
-     
+
     Logger.trace("Path::searchPath","D","Optimal route to ("+x+","+y+") OL:"+path.getOpen().size()+" CL:"+path.getClosed().size());
     List<Node> list=new LinkedList<Node>();
     Pathfinder.Node node=path.getBestNode();
@@ -152,10 +162,10 @@ public class Path
       {
       Logger.trace("Path::searchPath","D",node.toString());
       list.add(0,new Node(node.getX(),node.getY()));
-      node=node.getParent();      
+      node=node.getParent();
       }
 
-    
+
     Logger.trace("Path::searchPath","<");
     return list;
     }
@@ -169,17 +179,18 @@ public class Path
     path.setEndpoints((int)entity.getx(),(int)entity.gety(),(int)dest.getx(),(int)dest.gety());
 
     path.init();
+    navMap.setVisibility(entity.getx(), entity.gety(), 20);
     // HACK: Time limited the A* search.
     while(path.getStatus()==Pathfinder.IN_PROGRESS && path.getClosed().size()<navMap.maxNumberOfNodes())
       {
       path.doStep();
       }
-     
+
     if(path.getStatus()==Pathfinder.IN_PROGRESS)
       {
       return new LinkedList<Node>();
       }
-     
+
     Logger.trace("Path::searchPath","D","Optimal route to ("+dest.getx()+","+dest.gety()+") OL:"+path.getOpen().size()+" CL:"+path.getClosed().size());
     List<Node> list=new LinkedList<Node>();
     Pathfinder.Node node=path.getBestNode();
@@ -187,38 +198,38 @@ public class Path
       {
       Logger.trace("Path::searchPath","D",node.toString());
       list.add(0,new Node(node.getX(),node.getY()));
-      node=node.getParent();      
+      node=node.getParent();
       }
 
-    
+
     Logger.trace("Path::searchPath","<");
     return list;
     }
-    
+
   public static boolean followPath(RPEntity entity, double speed)
     {
     List<Node> path=entity.getPath();
-    
+
     if(path.size()==0)
       {
       return true;
       }
-    
+
     int pos=entity.getPathPosition();
-    
+
     Node actual=path.get(pos);
-    
+
     if(entity.distance(actual.x, actual.y)==0)
       {
       Logger.trace("Path::followPath","D","Completed waypoint("+pos+")("+actual.x+","+actual.y+") on Path");
       pos++;
-      if(pos<path.size())      
+      if(pos<path.size())
         {
         entity.setPathPosition(pos);
         actual=path.get(pos);
         Logger.trace("Path::followPath","D","Moving to waypoint("+pos+")("+actual.x+","+actual.y+") on Path from ("+entity.getx()+","+entity.gety()+")");
-        moveto(entity,actual.x, actual.y,speed);     
-        return false;    
+        moveto(entity,actual.x, actual.y,speed);
+        return false;
         }
       else
         {
@@ -230,15 +241,15 @@ public class Path
           {
           entity.stop();
           }
-          
+
         return true;
         }
       }
     else
       {
       Logger.trace("Path::followPath","D","Moving to waypoint("+pos+")("+actual.x+","+actual.y+") on Path from ("+entity.getx()+","+entity.gety()+")");
-      moveto(entity,actual.x, actual.y,speed);     
-      return false;    
-      }    
+      moveto(entity,actual.x, actual.y,speed);
+      return false;
+      }
     }
   }
