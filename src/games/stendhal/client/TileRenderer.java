adcb98b7e4bcf8105a14110a68df7f4f847f027f
Anders Asplund <damnter@users.sourceforge.net>
2005-07-11 02:13:19 +0000

    *** empty log message ***

--- src/games/stendhal/client/TileRenderer.java
+++ src/games/stendhal/client/TileRenderer.java
@@ -107,7 +107,18 @@ public class TileRenderer
   
   private void createAnimateTiles()
     {
-    // Double daisy 
+    // Outside_0 = 0 - 479
+    // Outside_1 = 480 - 959
+    // Dungeon_0 = 960 - 1439
+    // Dungeon_1 = 1440 - 1919
+    // Interior_0 = 1920 - 2399
+    // Navigation = 2400 - 2400
+    // Objects = 2401 - 2500
+    // Collision = 2501 - 2502
+    // Outside_3 = 2503 - 2982
+
+
+    // Double daisy
     addAnimatedTile(124,new int[]{124,154,184,214});
     addAnimatedTile(154,new int[]{154,184,214,124});
     addAnimatedTile(184,new int[]{184,214,124,154});
@@ -118,8 +129,68 @@ public class TileRenderer
     addAnimatedTile(155,new int[]{155,185,215,125});
     addAnimatedTile(185,new int[]{185,215,125,155});
     addAnimatedTile(215,new int[]{215,125,155,185});
+
+    // Waterfall start
+    addAnimatedTile(2623,new int[]{2623,2653,2683,2713});
+    addAnimatedTile(2653,new int[]{2713,2623,2653,2683});
+    addAnimatedTile(2683,new int[]{2683,2713,2623,2653});
+    addAnimatedTile(2713,new int[]{2653,2683,2713,2623});
     
-    // Waterfall    
+    // Waterfall middle
+    addAnimatedTile(2624,new int[]{2624,2654,2684,2714});
+    addAnimatedTile(2654,new int[]{2714,2624,2654,2684});
+    addAnimatedTile(2684,new int[]{2684,2714,2624,2654});
+    addAnimatedTile(2714,new int[]{2654,2684,2714,2624});
+
+    // Waterfall end
+    addAnimatedTile(2625,new int[]{2625,2655,2685,2715});
+    addAnimatedTile(2655,new int[]{2715,2625,2655,2685});
+    addAnimatedTile(2685,new int[]{2685,2715,2625,2655});
+    addAnimatedTile(2715,new int[]{2655,2685,2715,2625});
+
+    // Waterfall end left
+    addAnimatedTile(2626,new int[]{2626,2656,2686,2716});
+    addAnimatedTile(2656,new int[]{2716,2626,2656,2686});
+    addAnimatedTile(2686,new int[]{2686,2716,2626,2656});
+    addAnimatedTile(2716,new int[]{2656,2686,2716,2626});
+
+    // Waterfall end right
+    addAnimatedTile(2627,new int[]{2627,2657,2687,2717});
+    addAnimatedTile(2657,new int[]{2717,2627,2657,2687});
+    addAnimatedTile(2687,new int[]{2687,2717,2627,2657});
+    addAnimatedTile(2717,new int[]{2657,2687,2717,2627});
+
+
+    // Waterfall golden start
+    addAnimatedTile(2628,new int[]{2628,2658,2688,2718});
+    addAnimatedTile(2658,new int[]{2718,2628,2658,2688});
+    addAnimatedTile(2688,new int[]{2688,2718,2628,2658});
+    addAnimatedTile(2718,new int[]{2658,2688,2718,2628});
+
+    // Waterfall golden middle
+    addAnimatedTile(2629,new int[]{2629,2659,2689,2719});
+    addAnimatedTile(2659,new int[]{2719,2629,2659,2689});
+    addAnimatedTile(2689,new int[]{2689,2719,2629,2659});
+    addAnimatedTile(2719,new int[]{2659,2689,2719,2629});
+
+    // Waterfall golden end
+    addAnimatedTile(2630,new int[]{2630,2660,2690,2720});
+    addAnimatedTile(2660,new int[]{2720,2630,2660,2690});
+    addAnimatedTile(2690,new int[]{2690,2720,2630,2660});
+    addAnimatedTile(2720,new int[]{2660,2690,2720,2630});
+
+    // Waterfall golden end left
+    addAnimatedTile(2631,new int[]{2631,2661,2691,2721});
+    addAnimatedTile(2661,new int[]{2721,2631,2661,2691});
+    addAnimatedTile(2691,new int[]{2691,2721,2631,2661});
+    addAnimatedTile(2721,new int[]{2661,2691,2721,2631});
+
+    // Waterfall golden end right
+    addAnimatedTile(2632,new int[]{2632,2662,2692,2722});
+    addAnimatedTile(2662,new int[]{2722,2632,2662,2692});
+    addAnimatedTile(2692,new int[]{2692,2722,2632,2662});
+    addAnimatedTile(2722,new int[]{2662,2692,2722,2632});
+
     }
   
   /** Render the data to screen. We assume that Gamescreen will clip.
