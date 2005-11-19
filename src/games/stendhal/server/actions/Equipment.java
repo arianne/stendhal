b7a010a427a62d2f7ccc0fd735bffa9d9ad47a2e
Matthias Totz <mtotz@users.sourceforge.net>
2005-11-19 18:30:28 +0000

    cleaned up Equipment actions

--- src/games/stendhal/server/actions/Equipment.java
+++ src/games/stendhal/server/actions/Equipment.java
@@ -38,8 +38,10 @@ import org.apache.log4j.Logger;
  *  The source can be:
  *    baseitem   - object id of the item which should be moved
  *    
+ *  (optional, only when the source is inside a slot)    
  *    baseobject - (only when the item is in a slot) object id of the object containing the slot where the item is in
  *    baseslot   - (only when the item is in a slot) slot name where the item is in
+ *  (/optional)    
  *    
  *    
  *  The target can be either an 'equip':
@@ -130,282 +132,6 @@ public class Equipment extends ActionListener
     
     Log4J.finishMethod(logger,"equip");
   }
-//  private void onEquip(RPWorld world, StendhalRPRuleProcessor rules, Player player, RPAction action)
-//    {
-//    Log4J.startMethod(logger,"equip");    
-//
-//    if(action.has("baseitem") && action.has("targetobject") && action.has("targetslot"))
-//      {
-//       // The base item
-//      Entity base=null;
-//      // The container that contains base item, a player, a chest, etc...
-//      Entity baseObject=null;
-//      // The slot of the container where base item is stored
-//      RPSlot baseSlot=null;
-//      
-//      if(action.has("baseobject") && action.has("baseslot"))
-//        {
-//        // Taking the item from another object or player
-//        int id=action.getInt("baseobject");
-//
-//        StendhalRPZone zone=(StendhalRPZone)world.getRPZone(player.getID());
-//        RPObject.ID objectid=new RPObject.ID(id, zone.getID());
-//        if(!zone.has(objectid))
-//          {
-//          logger.debug("Rejected because zone doesn't have base object");
-//          logger.debug(action);
-//          return;
-//          }
-//
-//
-//        RPObject object=zone.get(objectid);
-//        if(!(object instanceof Player || 
-//             object instanceof Chest  ||
-//             object instanceof Corpse))
-//          {
-//          logger.debug("Rejected because base object isn't a passive entity or a player");
-//          logger.debug(object);
-//          logger.debug(action);
-//          return;
-//          }
-//        
-//        baseObject=(Entity)object;
-//
-//        if(baseObject instanceof Player && !player.getID().equals(baseObject.getID()))
-//          {
-//          // Only allowed to equip our own player.
-//          logger.debug("Rejected because base object is a player that is not yours");
-//          logger.debug(baseObject);
-//          logger.debug(action);
-//          return;
-//          }
-//
-//        if(!baseObject.hasSlot(action.get("baseslot")))
-//          {
-//          logger.debug("Rejected because base object don't have slot base slot");
-//          logger.debug(action);
-//          return;
-//          }        
-//
-//        baseSlot=baseObject.getSlot(action.get("baseslot"));
-//        
-//        int baseItem=action.getInt("baseitem");
-//        for(RPObject item: baseSlot)
-//          {
-//          if(item.getID().getObjectID()==baseItem)
-//            {
-//            base=(Entity)item;            
-//            break;
-//            }
-//          }        
-//        
-//        if(base==null)
-//          {
-//          logger.debug("Rejected because base item isn't stored inside base slot");
-//          logger.debug(baseObject);
-//          logger.debug(action);
-//          return;
-//          }
-//        }
-//      else
-//        {
-//        // Taking item from floor
-//        int id=action.getInt("baseitem");
-//
-//        StendhalRPZone zone=(StendhalRPZone)world.getRPZone(player.getID());
-//        RPObject.ID objectid=new RPObject.ID(id, zone.getID());
-//        if(!zone.has(objectid))
-//          {
-//          logger.debug("Rejected because zone doesn't have base item");
-//          logger.debug(action);
-//          return;
-//          }
-//
-//
-//        RPObject object=zone.get(objectid);
-//        if(!(object instanceof PassiveEntity))
-//          {
-//          logger.debug("Rejected because base item isn't a passive entity");
-//          logger.debug(object);
-//          logger.debug(action);
-//          return;
-//          }
-//        
-//        base=(Entity)object;
-//        }
-//        
-//
-//      Entity target=null;
-//      // The container that contains target item, a player, a chest, etc...
-//      Entity targetObject=null;
-//      // The slot of the container where target item is stored
-//      RPSlot targetSlot=null;
-//      
-//      // Taking the item from another object or player
-//      int id=action.getInt("targetobject");
-//
-//      StendhalRPZone zone=(StendhalRPZone)world.getRPZone(player.getID());
-//      RPObject.ID objectid=new RPObject.ID(id, zone.getID());
-//      if(!zone.has(objectid))
-//        {
-//        logger.debug("Rejected because zone doesn't have target object");
-//        logger.debug(action);
-//        return;
-//        }
-//
-//
-//      RPObject object=zone.get(objectid);
-//      if(!(object instanceof Player || 
-//           object instanceof Chest  ||
-//           object instanceof Corpse))
-//        {
-//        logger.debug("Rejected because target object isn't a passive entity or a player");
-//        logger.debug(object);
-//        logger.debug(action);
-//        return;
-//        }
-//      
-//      targetObject=(Entity)object;
-//
-//      if(targetObject instanceof Player && !player.getID().equals(targetObject.getID()))
-//        {
-//        // Only allowed to equip our own player.
-//        logger.debug("Rejected because target object is a player that is not yours");
-//        logger.debug(targetObject);
-//        logger.debug(action);
-//        return;
-//        }
-//
-//      if(!targetObject.hasSlot(action.get("targetslot")))
-//        {
-//        logger.debug("Rejected because target object don't have slot target slot");
-//        logger.debug(action);
-//        return;
-//        }        
-//
-//      targetSlot=targetObject.getSlot(action.get("targetslot"));
-//      
-//      if(action.has("targetitem"))
-//        {
-//        int targetItem=action.getInt("targetitem");
-//        for(RPObject item: targetSlot)
-//          {
-//          if(item.getID().getObjectID()==targetItem)
-//            {
-//            target=(Entity)item;            
-//            break;
-//            }
-//          }        
-//        
-//        if(target==null)
-//          {
-//          logger.debug("Rejected because target item isn't stored inside target slot");
-//          logger.debug(targetObject);
-//          logger.debug(action);
-//          return;
-//          }
-//        }
-//      else
-//        {
-//        logger.debug("No targetitem attribute");        
-//        }
-//        
-//      if(target!=null && base.equals(target))
-//        {
-//        //Stupid case, put an item in itself.
-//        logger.debug("Putting an item in itself");        
-//        return;
-//        }
-//        
-//
-//      /* Now just do it :) Remove from base and add to target slot. */
-//      if(player.nextto(targetObject,0.25) && 
-//         ( baseObject!=null && player.nextto(baseObject,0.25)  // If base object is stored in a slot
-//         || 
-//           player.nextto(base,0.25) // If base object is on floor
-//        ))
-//        {
-//        
-//        if(target!=null)
-//          {
-//          // If we place a money item over another money item we add them
-//          if(base instanceof Money && target instanceof Money)
-//            {
-//            if(baseObject==null)
-//              {
-//              // Floor case
-//              world.remove(base.getID());
-//              }
-//            else
-//              {
-//              // From another slot
-//              baseSlot.remove(base.getID());
-//              world.modify(baseObject);
-//              }
-//              
-//            logger.debug("Adding money("+target+") + money("+base+")");
-//            int result=((Money)target).add((Money)base);
-//            logger.debug("Added money: "+result);
-//            logger.debug(target);            
-//            
-//            logger.debug("PLAYER: "+targetObject);
-//            
-//            world.modify(targetObject);
-//            return;
-//            }
-//          else
-//            {
-//            logger.debug("Can't add items ("+base+") and ("+target+")");
-//            }
-//          }
-//        else
-//          {
-//          logger.debug("There is no target item");
-//          }
-//          
-//        if(!targetSlot.isFull())
-//          {
-//          if(baseObject==null)
-//            {
-//            // Floor case
-//            world.remove(base.getID());
-//            
-//            // Gives a valid id inside the slot
-//            targetSlot.assignValidID(base);
-//            targetSlot.add(base);
-//              
-//            world.modify(targetObject);
-//            }
-//          else
-//            {
-//            // From another slot
-//            baseSlot.remove(base.getID());
-//            
-//            targetSlot.assignValidID(base);
-//            targetSlot.add(base);
-//            
-//            world.modify(baseObject);
-//            world.modify(targetObject);
-//            }
-//          }
-//        else
-//          {
-//          logger.debug("targetSlot is full");
-//          logger.debug(targetObject);
-//          }
-//        }
-//      else
-//        {
-//        logger.debug("baseObject or base or Target is not near the player");
-//        logger.debug(player);
-//        logger.debug(targetObject);
-//        logger.debug(baseObject);
-//        logger.debug(base);
-//        }
-//      }
-//
-//    Log4J.finishMethod(logger,"equip");
-//    }
 
   private void onDrop(RPWorld world, StendhalRPRuleProcessor rules, Player player, RPAction action)
     {
@@ -528,6 +254,15 @@ public class Equipment extends ActionListener
         baseItemId = new RPObject.ID(baseItemId.getObjectID(),"");
  
         parent = getEntityFromId(player,world,action.getInt(BASE_OBJECT));
+        
+        // is the container a player and not the current one?
+        if (parent instanceof Player && !parent.getID().equals(player.getID()))
+        {
+          // trying to remove an item from another player
+          return;
+        }
+        
+        
         slot = action.get(BASE_SLOT);
 
         base = (Entity) parent.getSlot(slot).get(baseItemId);
@@ -632,6 +367,13 @@ public class Equipment extends ActionListener
         parent = getEntityFromId(player,world,action.getInt(TARGET_OBJECT));
         slot = action.get(TARGET_SLOT);
 
+        // is the container a player and not the current one?
+        if (parent instanceof Player && !parent.getID().equals(player.getID()))
+        {
+          // trying to drop an item into another players inventory
+          return;
+        }
+        
         // check slot
         if (!parent.hasSlot(slot))
         {
