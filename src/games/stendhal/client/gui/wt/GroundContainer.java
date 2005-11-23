97996c60ff6079cdfa50212ac2f6f2e2b0fde13d
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2005-11-23 18:09:14 +0000

    Changed Item by PassiveEntity... as we also want to drag corpses.

--- src/games/stendhal/client/gui/wt/GroundContainer.java
+++ src/games/stendhal/client/gui/wt/GroundContainer.java
@@ -20,7 +20,7 @@ import games.stendhal.client.GameObjects;
 import games.stendhal.client.GameScreen;
 import games.stendhal.client.StendhalClient;
 import games.stendhal.client.entity.Entity;
-import games.stendhal.client.entity.Item;
+import games.stendhal.client.entity.PassiveEntity;
 import games.stendhal.client.gui.wt.core.Draggable;
 import games.stendhal.client.gui.wt.core.Panel;
 
