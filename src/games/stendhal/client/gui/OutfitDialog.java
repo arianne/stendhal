169e4321eb0840fda199f582a299a97738ab98db
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2005-12-20 11:07:54 +0000

    fixed [ 1385616 ] Stendhal: stuck when setting outfit

--- src/games/stendhal/client/gui/OutfitDialog.java
+++ src/games/stendhal/client/gui/OutfitDialog.java
@@ -66,7 +66,7 @@ public class OutfitDialog extends javax.swing.JDialog {
      * @param total_clothes an integer with the total of sprites with clothes
      */
     public OutfitDialog(java.awt.Frame parent, String title, int total_hairs, int total_heads, int total_bodies, int total_clothes) {
-        super(parent, true);
+        super(parent, false);
         initComponents();
         setTitle(title);
         
