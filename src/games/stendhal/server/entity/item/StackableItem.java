7d4f2537b2895db2d90307a7f91332f09d63f49b
Matthias Totz <mtotz@users.sourceforge.net>
2005-12-23 20:49:15 +0000

    removed unused import

--- src/games/stendhal/server/entity/item/StackableItem.java
+++ src/games/stendhal/server/entity/item/StackableItem.java
@@ -15,7 +15,6 @@ package games.stendhal.server.entity.item;
 import marauroa.common.game.*;
 import java.util.Map;
 import java.util.List;
-import java.util.LinkedList;
 
 public class StackableItem extends Item implements Stackable
   {
