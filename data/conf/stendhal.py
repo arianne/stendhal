3ce59bfb30cd889e010de6015f01d305a84b8bef
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2006-02-15 12:40:20 +0000

    Finer tune of stendhal.py

--- data/conf/stendhal.py
+++ data/conf/stendhal.py
@@ -50,18 +50,25 @@ class Configuration(StendhalPythonConfig):
         print "Adding sign to zone"
 
         self.addShop("weapons",{"knife":15,
-                   "small_axe":15,
-                   "club":10,
-                   "dagger":25,
-                   "wooden_shield":25,
-                   "dress":25,
-                   "leather_helmet":25,
-                   "leather_legs":30})
+                    "small_axe":15,
+                    "club":10,
+                    "dagger":25,
+                    "wooden_shield":25,
+                    "dress":25,
+                    "leather_helmet":25,
+                    "leather_legs":30})
 
         self.addShop("healing",{"antidote":50,
-			"minor_potion":100,
-			"potion":250,
-			"greater_potion":500})
+		    "minor_potion":100,
+                    "potion":250,
+                    "greater_potion":500})
+
+        self.addShop("drinks",{"beer":10,
+		    "wine":15})
+
+        self.addShop("food",{"cheese":20,
+		    "meat":40,
+                    "ham":60})
 
         
         def pythonillaMethod(npc):
@@ -84,6 +91,49 @@ class Configuration(StendhalPythonConfig):
             Behaviours.addGoodbye(npc,"Good luck in your travels!")
             
         addNPC(zone, rules,"Pythonilla",pythonillaMethod)
+
+        def diogenesMethod(npc):
+            global conf
+
+            # Set the NPC path
+            npc.initializePath([(20,41),(26,42),(26,44),(31,44),(31,42),(35,42),(35,48),(35,28),(22,28)])
+
+            # Create an outfit for this player
+            npc.put("outfit",0)
+
+            # Adds all the behaviour chat
+            Behaviours.addGreeting(npc,"Greetings, do you have a coin to spare?")
+            Behaviours.addJob(npc,"Hehehe! Job! hehehe! Muahahaha!.")
+            Behaviours.addHelp(npc,"I can't help you, but you can help Stendhal: tell your friends about Stendhal and help us to create maps.")
+            Behaviours.addReply(npc,"chat","Most people will #help you if you ask them, and you may also ask them about there #job. Some people also have a few #quests you could do for them.")
+            Behaviours.addQuest(npc,["I have been told that on the deepest place of the dungeon under this city someone also buy sheeps, but *it* pays better!.",
+                                     "Ah, quests... just like the old days when I was young! I remember one quest that was about... Oh look, a bird!hmm, what?! Oh, Oops! I forgot it! :("])
+            Behaviours.addGoodbye(npc,"Hey! My coin! Argh! ... ")
+            
+        addNPC(zone, rules,"Diogenes",diogenesMethod)
+
+        def margaretMethod(npc):
+            global conf
+
+            # Set the NPC path
+            npc.initializePath([(17,12),(17,13),(16,8),(13,8),(13,6),(13,10),(23,10),(23,13),(23,10),(17,10)])
+
+            # Create an outfit for this player
+            npc.put("outfit",0)
+
+            # Adds all the behaviour chat
+            Behaviours.addGreeting(npc,"Greetings, how may I help you?")
+            Behaviours.addJob(npc,"I am the bar maid for this fair tavern. We sell fine #drinks and #food.")
+            Behaviours.addHelp(npc,"At the tavern you can get #drinks and take a break to meet new people!.")
+
+            shop=conf.getShop("drinks")
+            shop.putAll(conf.getShop("food"))
+            
+            Behaviours.addSeller(npc,Behaviours.SellerBehaviour(shop))
+            Behaviours.addGoodbye(npc,"Goodbye, come back again.")
+            
+        zone=world.getRPZone("0_semos_tavern")
+        addNPC(zone, rules,"Margaret",margaretMethod)
 
         # On this point:
         # - Game is starting.
