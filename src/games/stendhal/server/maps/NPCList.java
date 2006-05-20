1bf56ed54930ab6df9928a25c64625953e47efa0
Daniel Herding <wikipedian@users.sourceforge.net>
2006-05-20 14:01:00 +0000

    docu

--- src/games/stendhal/server/maps/NPCList.java
+++ src/games/stendhal/server/maps/NPCList.java
@@ -5,10 +5,14 @@ import java.util.HashMap;
 import games.stendhal.server.entity.npc.*;
 
 /**
- * This Singleton contains all NPCs in the Stendhal world.
+ * This Singleton should contain all NPCs in the Stendhal world that are
+ * unique.
  */
 public class NPCList {
 
+	/**
+	 * The Singleton instance.
+	 */
 	private static NPCList instance;
 	
 	private Map<String, SpeakerNPC> contents;
@@ -29,6 +33,11 @@ public class NPCList {
 		contents = new HashMap<String, SpeakerNPC>();
 	}
 
+	/**
+	 * Returns the NPC with the given name. 
+	 * @param name The NPC's name
+	 * @return The NPC, or null if there is no NPC with this name
+	 */
 	public SpeakerNPC get(String name) {
 		return contents.get(name);
 	}
@@ -42,12 +51,23 @@ public class NPCList {
 		return contents.containsKey(name);
 	}
 
+	/**
+	 * Adds an NPC to the NPCList. Does nothing if an NPC with the same name
+	 * already exists. This makes sure that each NPC can be uniquely identified
+	 * by his/her name.
+	 * @param npc The NPC that should be added
+	 */
 	public void add(SpeakerNPC npc) {
 		if (!contents.containsKey(npc.getName())) {
 			contents.put(npc.getName(), npc);
 		}
 	}
 
+	/**
+	 * Removes an NPC from the NPCList. Does nothing if no NPC with the given
+	 * name exists.
+	 * @param name The name of the NPC that should be removed
+	 */
 	public SpeakerNPC remove(String name) {
 		return contents.remove(name);
 	}
