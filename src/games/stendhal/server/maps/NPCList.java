36cc9e6a18d86159a1fed5c75a6d4892a16e5633
Daniel Herding <wikipedian@users.sourceforge.net>
2006-05-20 10:32:52 +0000

    docu

--- src/games/stendhal/server/maps/NPCList.java
+++ src/games/stendhal/server/maps/NPCList.java
@@ -4,50 +4,52 @@ import java.util.Map;
 import java.util.HashMap;
 import games.stendhal.server.entity.npc.*;
 
+/**
+ * This Singleton contains all NPCs in the Stendhal world.
+ */
+public class NPCList {
 
-public class NPCList 
-  {
-  static private NPCList instance;
-  
-  static public NPCList get()
-    {
-    if(instance==null)
-      {
-      instance=new NPCList();     
-      }
-    
-    return instance;
-    }
-  
-  private Map<String, SpeakerNPC> contents;
-  
-  private NPCList()
-    {
-    contents=new HashMap<String, SpeakerNPC>();
-    }
-  
-  public SpeakerNPC get(String name)
-    {
-    return contents.get(name);
-    }
-  
-  public boolean has(String name)
-    {
-    return contents.containsKey(name);
-    }
+	private static NPCList instance;
+	
+	private Map<String, SpeakerNPC> contents;
 
-  public SpeakerNPC add(SpeakerNPC npc)
-    {
-    if(!contents.containsKey(npc.getName()))
-      {
-      contents.put(npc.getName(), npc);            
-      }    
-    return npc;
-    }
-  
-  public SpeakerNPC remove(String name)
-    {
-    return contents.remove(name);
-    }
 
-  }
+	/**
+	 * Returns the Singleton instance.
+	 * @return The instance
+	 */
+	static public NPCList get() {
+		if (instance == null) {
+			instance = new NPCList();
+		}
+		return instance;
+	}
+
+	private NPCList() {
+		contents = new HashMap<String, SpeakerNPC>();
+	}
+
+	public SpeakerNPC get(String name) {
+		return contents.get(name);
+	}
+
+	/**
+	 * Checks whether an NPC with the given name exists.
+	 * @param name The NPC's name
+	 * @return true iff an NPC with the given name exists 
+	 */
+	public boolean has(String name) {
+		return contents.containsKey(name);
+	}
+
+	public SpeakerNPC add(SpeakerNPC npc) {
+		if (!contents.containsKey(npc.getName())) {
+			contents.put(npc.getName(), npc);
+		}
+		return npc;
+	}
+
+	public SpeakerNPC remove(String name) {
+		return contents.remove(name);
+	}
+}
