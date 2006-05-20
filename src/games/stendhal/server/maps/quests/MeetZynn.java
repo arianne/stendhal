8b05e4870a7bf7982c9c6e27c1ed402466e095b6
Daniel Herding <wikipedian@users.sourceforge.net>
2006-05-20 13:51:18 +0000

    Java Code Conventions

--- src/games/stendhal/server/maps/quests/MeetZynn.java
+++ src/games/stendhal/server/maps/quests/MeetZynn.java
@@ -21,7 +21,6 @@ import games.stendhal.server.entity.npc.SpeakerNPC;
  * - As much as wanted.
  */
 public class MeetZynn implements IQuest {
-	private StendhalRPWorld world;
 
 	private NPCList npcs;
 
@@ -34,90 +33,73 @@ public class MeetZynn implements IQuest {
 		 * for players higher than level 15.
 		 */
 
-		Behaviours.addReply(
-				npc,
-				"history",
-				"There are at present two powers at Faiumoni: Deniran empire and Blordrough's dark legion. Deniran still controls the central and north part of Faiumoni, and has under its control the mines of Gold and Mithril. On the other side the Blordrough's legion has conquered the south of the island and has under its control several mines of steel and an important gold mine.");
+		Behaviours.addReply(npc,
+							"history",
+							"There are at present two powers at Faiumoni: Deniran empire and Blordrough's dark legion. Deniran still controls the central and north part of Faiumoni, and has under its control the mines of Gold and Mithril. On the other side the Blordrough's legion has conquered the south of the island and has under its control several mines of steel and an important gold mine.");
 
-		Behaviours.addReply(
-				npc,
-				"news",
-				"Deniran empire is seeking help from adventurers to take part in their army on the task of recovering the total control of Faiumoni, unfortunatelly they have found an awesome resistence. The empire army has been sent to defend the south part of the island that is suffering constants attacks.");
+		Behaviours.addReply(npc,
+							"news",
+							"Deniran empire is seeking help from adventurers to take part in their army on the task of recovering the total control of Faiumoni, unfortunatelly they have found an awesome resistence. The empire army has been sent to defend the south part of the island that is suffering constants attacks.");
 
-		Behaviours.addReply(
-				npc,
-				"geography",
-				"I can tell you about the different #places in Faiumoni. I can teach you how to #use maps or where to #get them. I can recommend you a #SPS psychic");
+		Behaviours.addReply(npc,
+							"geography",
+							"I can tell you about the different #places in Faiumoni. I can teach you how to #use maps or where to #get them. I can recommend you a #SPS psychic");
 
-		Behaviours.addReply(
-				npc,
-				"places",
-				"I can talk to you about the island of #Faiumoni or about its most famous locations: #Semos city, #Ados city, #Or'ril castle, #Narwol city or #Deniran city.");
+		Behaviours.addReply(npc,
+							"places",
+							"I can talk to you about the island of #Faiumoni or about its most famous locations: #Semos city, #Ados city, #Or'ril castle, #Narwol city or #Deniran city.");
 
-		Behaviours.addReply(
-				npc,
-				"Faiumoni",
-				"It is the big island we are in, with mountains on its north part, a big desert on the middle and a long river that crosses it on its half.");
+		Behaviours.addReply(npc,
+							"Faiumoni",
+							"It is the big island we are in, with mountains on its north part, a big desert on the middle and a long river that crosses it on its half.");
 
-		Behaviours.addReply(
-				npc,
-				"Semos",
-				"It is precisely our small city on the North of Faiumoni. It is populated by 40-50 persons.");
+		Behaviours.addReply(npc,
+							"Semos",
+							"It is precisely our small city on the North of Faiumoni. It is populated by 40-50 persons.");
 
-		Behaviours.addReply(
-				npc,
-				"Ados",
-				"It is a coastal city with a very important port, where merchants arrive to sell their stuff. Ados is the end of an important commercial route that has its heart at Deniran, the capital of Faiumoni.");
+		Behaviours.addReply(npc,
+							"Ados",
+							"It is a coastal city with a very important port, where merchants arrive to sell their stuff. Ados is the end of an important commercial route that has its heart at Deniran, the capital of Faiumoni.");
 
-		Behaviours.addReply(
-				npc,
-				"Or'ril",
-				"It is one of a set of castles that were built around the imperial path to defend the route from Ados to Deniran. Now Or'ril is abandoned because the empire army has been sent to defend the south part of the empire that is suffering constants attacks. It used to be populated by a unit of Deniran's swordmen, around 60 of them, plus a set of helpers. It is not known if there is anyone any longer living in the castle.");
+		Behaviours.addReply(npc,
+							"Or'ril",
+							"It is one of a set of castles that were built around the imperial path to defend the route from Ados to Deniran. Now Or'ril is abandoned because the empire army has been sent to defend the south part of the empire that is suffering constants attacks. It used to be populated by a unit of Deniran's swordmen, around 60 of them, plus a set of helpers. It is not known if there is anyone any longer living in the castle.");
 
-		Behaviours.addReply(
-				npc,
-				"Narwol",
-				"It is an ancient elven city, that legend says was built to defend Teruykeh, the elven capital, against an ancient evil. Narwol was built long years before the first human set his feet on Faiumoni. Elves dislike common treats with the rest of races, so their cities are generally hidden inside forests or deep valleys.");
+		Behaviours.addReply(npc,
+							"Narwol",
+							"It is an ancient elven city, that legend says was built to defend Teruykeh, the elven capital, against an ancient evil. Narwol was built long years before the first human set his feet on Faiumoni. Elves dislike common treats with the rest of races, so their cities are generally hidden inside forests or deep valleys.");
 
-		Behaviours.addReply(
-				npc,
-				"Deniran",
-				"It is the jewel of the crown. Deniran is the center of Faiumoni and supports the army that tries to defeat enemies that want to conquer Faiumoni. Deniran has a very important commercial route with other empires that is a channel through the imperial path up to Ados on the north and with Sikhw on the south. Unfortunatelly Sikhw was conquered some time ago, and no one can reach it anymore.");
+		Behaviours.addReply(npc,
+							"Deniran",
+							"It is the jewel of the crown. Deniran is the center of Faiumoni and supports the army that tries to defeat enemies that want to conquer Faiumoni. Deniran has a very important commercial route with other empires that is a channel through the imperial path up to Ados on the north and with Sikhw on the south. Unfortunatelly Sikhw was conquered some time ago, and no one can reach it anymore.");
 
-		Behaviours.addReply(
-				npc,
-				"use",
-				"You have to become familiarized with map's #levels, map's zone #naming and map's #positioning. Once you've managed that, you can go to meet any other adventurer when you know his position.");
+		Behaviours.addReply(npc,
+							"use",
+							"You have to become familiarized with map's #levels, map's zone #naming and map's #positioning. Once you've managed that, you can go to meet any other adventurer when you know his position.");
 
-		Behaviours.addReply(
-				npc,
-				"levels",
-				"Maps are classified by levels to indicate their height respect to the surface. Thus, surface is indicated by level 0, subsurface by level -1, subsubsurface by level -2. These level indexes appear as prefixes in the name of every map. Thus the map of the surface of Semos city is 0_semos_city and the first level of the dungeon under Semos city is -1_semos_dungeon. Interiors of buildings have levels as suffixes and INT as prefix.");
+		Behaviours.addReply(npc,
+							"levels",
+							"Maps are classified by levels to indicate their height respect to the surface. Thus, surface is indicated by level 0, subsurface by level -1, subsubsurface by level -2. These level indexes appear as prefixes in the name of every map. Thus the map of the surface of Semos city is 0_semos_city and the first level of the dungeon under Semos city is -1_semos_dungeon. Interiors of buildings have levels as suffixes and INT as prefix.");
 
-		Behaviours.addReply(
-				npc,
-				"naming",
-				"Every set of zones of the map has a central zone wich is the reference point for other surrounding areas. These areas are named consequently after this central area with an added suffix that indicates the path taken from the central area to the area of interest. Thus,  0_semos_forest_n2_w is the zone in the neighborhood of the forest of semos that is two areas north and then one area west from the forest's central area.");
+		Behaviours.addReply(npc,
+							"naming",
+							"Every set of zones of the map has a central zone wich is the reference point for other surrounding areas. These areas are named consequently after this central area with an added suffix that indicates the path taken from the central area to the area of interest. Thus,  0_semos_forest_n2_w is the zone in the neighborhood of the forest of semos that is two areas north and then one area west from the forest's central area.");
 
-		Behaviours.addReply(
-				npc,
-				"positioning",
-				"Position (X,Y) is relative to every zone of the map: X stands for horizontal position and increases to the right and Y stands for vertical position and increases to the bottom: this means that (0,0) is the top left corner.");
+		Behaviours.addReply(npc,
+							"positioning",
+							"Position (X,Y) is relative to every zone of the map: X stands for horizontal position and increases to the right and Y stands for vertical position and increases to the bottom: this means that (0,0) is the top left corner.");
 
-		Behaviours.addReply(
-				npc,
-				"get",
-				"You can get an scaled-down aerial image of the known surface of stendhal at #http://arianne.sourceforge.net/wiki_images/atlas_world.png But BEWARE, while maps can be very useful they might also spoil a little the surprise of discovering for the first time");
+		Behaviours.addReply(npc,
+							"get",
+							"You can get an scaled-down aerial image of the known surface of stendhal at #http://arianne.sourceforge.net/wiki_images/atlas_world.png But BEWARE, while maps can be very useful they might also spoil a little the surprise of discovering for the first time");
 
-		Behaviours.addReply(
-				npc,
-				"SPS",
-				"#SPS stands for #S tendhal's #P ositioning #S ystem. You can ask #Io about how to know your own or other adventurers' position anytime.");
+		Behaviours.addReply(npc,
+							"SPS",
+							"#SPS stands for #S tendhal's #P ositioning #S ystem. You can ask #Io about how to know your own or other adventurers' position anytime.");
 
-		Behaviours.addReply(
-				npc,
-				"Io",
-				"Her full name is Io Flotto. She spends most of her time in the Temple... floating... Yes, I know it sounds weird but I have to admit all my sextants have never worked half as well as her \"intuitive skill\".");
+		Behaviours.addReply(npc,
+							"Io",
+							"Her full name is Io Flotto. She spends most of her time in the Temple... floating... Yes, I know it sounds weird but I have to admit all my sextants have never worked half as well as her \"intuitive skill\".");
 
 		/**
 		 * I still have to think of a way to reward a good amount of XP to the
@@ -125,25 +107,26 @@ public class MeetZynn implements IQuest {
 		 * list of all the things the player has asked and reward him when the
 		 * list is complete?
 		 */
-		npc.add(1, "bye", null, 0, null,
-
-		new SpeakerNPC.ChatAction() {
-			public void fire(Player player, String text, SpeakerNPC engine) {
-				int level = player.getLevel();
-				if (level < 15) {
-					engine.say("Bye, if you're going to stay here remember to keep silence.");
-				}
-
-				else {
-					engine.say("Bye, you should consider getting a library card...");
-				}
-			}
-		});
+		npc.add(1,
+				"bye",
+				null,
+				0,
+				null,
+				new SpeakerNPC.ChatAction() {
+					public void fire(Player player, String text, SpeakerNPC engine) {
+						int level = player.getLevel();
+						if (level < 15) {
+							engine.say("Bye, if you're going to stay here remember to keep silence.");
+						}
+						else {
+							engine.say("Bye, you should consider getting a library card...");
+						}
+					}
+				});
 	}
 
 	public MeetZynn(StendhalRPWorld w, StendhalRPRuleProcessor rules) {
 		this.npcs = NPCList.get();
-		this.world = w;
 
 		step_1();
 	}
