57c53b945f24eacc67405d69ab2916fdf1d19b0a
Daniel Herding <wikipedian@users.sourceforge.net>
2006-05-22 00:30:56 +0000

    Java Code Conventions

--- src/games/stendhal/server/maps/Jail.java
+++ src/games/stendhal/server/maps/Jail.java
@@ -1,15 +1,8 @@
 package games.stendhal.server.maps;
 
-import games.stendhal.common.Rand;
 import games.stendhal.server.StendhalRPWorld;
 import games.stendhal.server.StendhalRPZone;
-import games.stendhal.server.entity.Chest;
-import games.stendhal.server.entity.Player;
-import games.stendhal.server.entity.Portal;
-import games.stendhal.server.entity.OneWayPortal;
-import games.stendhal.server.entity.Sign;
 import games.stendhal.server.entity.npc.Behaviours;
-import games.stendhal.server.entity.npc.NPC;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.pathfinder.Path;
 
@@ -17,43 +10,38 @@ import marauroa.common.game.IRPZone;
 
 import java.util.List;
 import java.util.LinkedList;
-import java.util.Map;
-import java.util.HashMap;
 
+public class Jail implements IContent {
+	public Jail(StendhalRPWorld world) {
+		StendhalRPZone zone = (StendhalRPZone) world.getRPZone(new IRPZone.ID(
+				"int_admin_jail"));
+		NPCList npcs = NPCList.get();
 
-public class Jail implements IContent
-  {
-  public Jail(StendhalRPWorld world)
-    {
-    StendhalRPZone zone=(StendhalRPZone)world.getRPZone(new IRPZone.ID("int_admin_jail"));
-    NPCList npcs=NPCList.get();
-    
-    SpeakerNPC npc = new SpeakerNPC("Sten Tanquilos")
-      {
-      protected void createPath()
-        {
-        List<Path.Node> nodes=new LinkedList<Path.Node>();
-        nodes.add(new Path.Node(4,14));
-        nodes.add(new Path.Node(27,14));
-        nodes.add(new Path.Node(27,17));
-        nodes.add(new Path.Node(4,17));
-        setPath(nodes,true);
-        }
+		SpeakerNPC npc = new SpeakerNPC("Sten Tanquilos") {
+			protected void createPath() {
+				List<Path.Node> nodes = new LinkedList<Path.Node>();
+				nodes.add(new Path.Node(4, 14));
+				nodes.add(new Path.Node(27, 14));
+				nodes.add(new Path.Node(27, 17));
+				nodes.add(new Path.Node(4, 17));
+				setPath(nodes, true);
+			}
 
-      protected void createDialog()
-        {
-        Behaviours.addGreeting(this);
-        Behaviours.addJob(this,"I am the jail keeper. You have been confined here because of your bad behaviour.");
-        Behaviours.addHelp(this,"Wait for an admin to come here and decide about you. There is meanwhile no exit from here.");
-        Behaviours.addGoodbye(this);
-        }
-      };
-    npcs.add(npc);
+			protected void createDialog() {
+				Behaviours.addGreeting(this);
+				Behaviours.addJob(this,
+						"I am the jail keeper. You have been confined here because of your bad behaviour.");
+				Behaviours.addHelp(this,
+						"Wait for an admin to come here and decide about you. There is meanwhile no exit from here.");
+				Behaviours.addGoodbye(this);
+			}
+		};
+		npcs.add(npc);
 
-    zone.assignRPObjectID(npc);
-    npc.put("class","youngsoldiernpc");
-    npc.set(4,14);
-    npc.initHP(100);
-    zone.addNPC(npc);    
-    }
-  }
+		zone.assignRPObjectID(npc);
+		npc.put("class", "youngsoldiernpc");
+		npc.set(4, 14);
+		npc.initHP(100);
+		zone.addNPC(npc);
+	}
+}
--- src/games/stendhal/server/maps/ShopList.java
+++ src/games/stendhal/server/maps/ShopList.java
@@ -4,105 +4,99 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
+/**
+ * Singleton class that contains inventory and prices of NPC stores.
+ *
+ */
+public class ShopList {
+	static {
+		ShopList shops = get();
 
-public class ShopList 
-  {
-  static
-    {
-    ShopList shops=get();
-    
-    shops.add("food&drinks","beer",10);
-    shops.add("food&drinks","wine",15);
-    shops.add("food&drinks","flask",5);
-    shops.add("food&drinks","cheese",20);
-    shops.add("food&drinks","apple",10);
-    shops.add("food&drinks","carrot",10);
-    shops.add("food&drinks","meat",40);
-    shops.add("food&drinks","ham",80);
+		shops.add("food&drinks", "beer", 10);
+		shops.add("food&drinks", "wine", 15);
+		shops.add("food&drinks", "flask", 5);
+		shops.add("food&drinks", "cheese", 20);
+		shops.add("food&drinks", "apple", 10);
+		shops.add("food&drinks", "carrot", 10);
+		shops.add("food&drinks", "meat", 40);
+		shops.add("food&drinks", "ham", 80);
 
-    shops.add("healing","antidote",50);
-    shops.add("healing","minor_potion",100);
-    shops.add("healing","potion",250);
-    shops.add("healing","greater_potion",500);
+		shops.add("healing", "antidote", 50);
+		shops.add("healing", "minor_potion", 100);
+		shops.add("healing", "potion", 250);
+		shops.add("healing", "greater_potion", 500);
 
-    shops.add("scrolls","home_scroll",20);
-    shops.add("scrolls","empty_scroll",1200);    
-    
-    shops.add("sellstuff","knife",15);
-    shops.add("sellstuff","small_axe",15);
-    shops.add("sellstuff","club",10);
-    shops.add("sellstuff","dagger",25);
-    shops.add("sellstuff","wooden_shield",25);
-    shops.add("sellstuff","dress",25);
-    shops.add("sellstuff","leather_helmet",25);
-    shops.add("sellstuff","leather_legs",35);
+		shops.add("scrolls", "home_scroll", 20);
+		shops.add("scrolls", "empty_scroll", 1200);
 
-    shops.add("sellrangedstuff","wooden_bow",300);
-    shops.add("sellrangedstuff","wooden_arrow",2);
+		shops.add("sellstuff", "knife", 15);
+		shops.add("sellstuff", "small_axe", 15);
+		shops.add("sellstuff", "club", 10);
+		shops.add("sellstuff", "dagger", 25);
+		shops.add("sellstuff", "wooden_shield", 25);
+		shops.add("sellstuff", "dress", 25);
+		shops.add("sellstuff", "leather_helmet", 25);
+		shops.add("sellstuff", "leather_legs", 35);
 
-    shops.add("buystuff","short_sword",15);
-    shops.add("buystuff","sword",60);
-    shops.add("buystuff","studded_shield",20);
-    shops.add("buystuff","studded_armor",22);
-    shops.add("buystuff","studded_helmet",17);
-    shops.add("buystuff","studded_legs",20);
-    shops.add("buystuff","chain_armor",29);
-    shops.add("buystuff","chain_helmet",25);
-    shops.add("buystuff","chain_legs",27);
-    }
-  
-  static private ShopList instance;
-  
-  static public ShopList get()
-    {
-    if(instance==null)
-      {
-      instance=new ShopList();     
-      }
-    
-    return instance;
-    }
-  
-  private Map<String, Map<String, Integer>> contents;
-  
-  private ShopList()
-    {
-    contents=new HashMap<String, Map<String, Integer>>();
-    }
-  
-  public Map<String, Integer> get(String name)
-    {
-    return contents.get(name);
-    }
-  
-  public String toString(String name, String header)
-    {
-    Map<String, Integer> items=contents.get(name);
-    
-    StringBuffer sb=new StringBuffer(header+"|\n");    
-    
-    for(String item: items.keySet())
-      {
-      sb.append(item+"\t"+items.get(item)+"|\n");      
-      }
-    
-    return sb.toString();
-    }
-  
-  public void add(String name,String item, int price)
-    {
-    Map<String, Integer> shop;
-    
-    if(!contents.containsKey(name))
-      {
-      shop=new LinkedHashMap<String, Integer>();
-      contents.put(name, shop);            
-      }
-    else
-      {
-      shop=contents.get(name);
-      }
+		shops.add("sellrangedstuff", "wooden_bow", 300);
+		shops.add("sellrangedstuff", "wooden_arrow", 2);
 
-    shop.put(item, price);
-    }
-  }
+		shops.add("buystuff", "short_sword", 15);
+		shops.add("buystuff", "sword", 60);
+		shops.add("buystuff", "studded_shield", 20);
+		shops.add("buystuff", "studded_armor", 22);
+		shops.add("buystuff", "studded_helmet", 17);
+		shops.add("buystuff", "studded_legs", 20);
+		shops.add("buystuff", "chain_armor", 29);
+		shops.add("buystuff", "chain_helmet", 25);
+		shops.add("buystuff", "chain_legs", 27);
+	}
+
+	static private ShopList instance;
+
+	/**
+	 * Returns the Singleton instance.
+	 * @return The instance
+	 */
+	static public ShopList get() {
+		if (instance == null) {
+			instance = new ShopList();
+		}
+		return instance;
+	}
+
+	private Map<String, Map<String, Integer>> contents;
+
+	private ShopList() {
+		contents = new HashMap<String, Map<String, Integer>>();
+	}
+
+	public Map<String, Integer> get(String name) {
+		return contents.get(name);
+	}
+
+	public String toString(String name, String header) {
+		Map<String, Integer> items = contents.get(name);
+
+		StringBuffer sb = new StringBuffer(header + "|\n");
+
+		for (String item : items.keySet()) {
+			sb.append(item + "\t" + items.get(item) + "|\n");
+		}
+
+		return sb.toString();
+	}
+
+	public void add(String name, String item, int price) {
+		Map<String, Integer> shop;
+
+		if (!contents.containsKey(name)) {
+			shop = new LinkedHashMap<String, Integer>();
+			contents.put(name, shop);
+		} else {
+			shop = contents.get(name);
+		}
+
+		shop.put(item, price);
+	}
+}
