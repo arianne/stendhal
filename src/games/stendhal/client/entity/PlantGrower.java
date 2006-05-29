bf0c7e3f47780dea39a23bae365e211a0d4f59a7
Daniel Herding <wikipedian@users.sourceforge.net>
2006-05-29 00:50:16 +0000

    forgot constructor

--- src/games/stendhal/client/entity/PlantGrower.java
+++ src/games/stendhal/client/entity/PlantGrower.java
@@ -1,10 +1,20 @@
 package games.stendhal.client.entity;
 
+import games.stendhal.client.GameObjects;
+
 import java.awt.Rectangle;
 import java.awt.geom.Rectangle2D;
 
+import marauroa.common.game.AttributeNotFoundException;
+import marauroa.common.game.RPObject;
+
 public class PlantGrower extends Entity {
 
+	public PlantGrower(GameObjects gameObjects, RPObject object)
+			throws AttributeNotFoundException {
+		super(gameObjects, object);
+	}
+	
 	@Override
 	public Rectangle2D getArea() {
 		return new Rectangle.Double(x, y, 1, 1);
