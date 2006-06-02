6effc9ce2a42354dce764521d1bb43d2c0488835
Daniel Herding <wikipedian@users.sourceforge.net>
2006-06-02 21:06:15 +0000

    removed useless imports

--- src/games/stendhal/client/entity/SheepFood.java
+++ src/games/stendhal/client/entity/SheepFood.java
@@ -15,9 +15,7 @@ package games.stendhal.client.entity;
 import games.stendhal.client.GameObjects;
 import games.stendhal.client.Sprite;
 import games.stendhal.client.SpriteStore;
-import games.stendhal.client.StendhalClient;
 
-import java.awt.Color;
 import java.awt.Rectangle;
 import java.awt.geom.Rectangle2D;
 
