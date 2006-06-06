1e19d9cd7c033a0d6283f4220d455fd3832555dd
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-06-06 05:58:13 +0000

    Check if player is naked

--- src/games/stendhal/server/maps/quests/MeetKetteh.java
+++ src/games/stendhal/server/maps/quests/MeetKetteh.java
@@ -34,28 +34,36 @@ public class MeetKetteh implements IQuest {
 
 		npc.add(ConversationStates.IDLE,
 				Behaviours.GREETING_MESSAGES,
-				new SpeakerNPC.ChatCondition() {
-					public boolean fire(Player player, SpeakerNPC engine) {
-						// TODO: check whether the player is naked
-						return true; // player.equals(0);
-					}
-				},
+				null,
 				ConversationStates.ATTENDING,
 				null,
 				new SpeakerNPC.ChatAction() {
 					public void fire(Player player, String text,
 							SpeakerNPC engine) {
-						// A little trick to make NPC remember if it has met
-						// player before anc react accordingly
-						// NPC_name quest doesn't exist anywhere else neither is
-						// used for any other purpose
-						if (!player.isQuestCompleted("Ketteh")) {
-							engine.say("Who are you? Aiiieeeee!!! You're naked! Right-click on you and choose SET OUTFIT! Shhh! Don't even think on clicking on the white bar at the bottom and write to reply to me! And if you happen to talk to anyone in the city you'd better begin the conversation saying HI. And don't be gross and just leave: say BYE to end the conversation. And use #CTRL #+ #arrow to turn around and face me when I'm talking to you! Wait! I am sure you are a friend of that onlooker Nomyr who's always peeking at the windows! Now use the #arrows and go out!");
-							player.setQuest("Ketteh", "done");
-						} else {
+
+                        int outfit = player.getInt("outfit");
+                        int clothes_index = (outfit / 100) % 100;
+
+                        // Check whether player is naked
+                        if (clothes_index == 0) {
+                            engine.say("Who are you? Aiiieeeee!!! You're naked! Right-click on you and choose SET OUTFIT! Shhh! Don't even think on clicking on the white bar at the bottom and write to reply to me! And if you happen to talk to anyone in the city you'd better begin the conversation saying HI. And don't be gross and just leave: say BYE to end the conversation. And use #CTRL #+ #arrow to turn around and face me when I'm talking to you! Wait! I am sure you are a friend of that onlooker Nomyr who's always peeking at the windows! Now use the #arrows and go out!");
+                            player.setQuest("Ketteh", "seen_naked");
+
+                        // OK, player is NOT naked this time, but was last time.
+                        } else if (player.hasQuest("Ketteh") && player.getQuest("Ketteh").equals("seen_naked")) {
 							engine.say("Hi again, " + player.getName()
 									+ ". How can I #shout at you this time?");
-						}
+                            player.setQuest("Ketteh", "seen"); // don't be unforgiving
+
+                        // We have met the player before and it was NOT naked last time nor is it know
+                        } else if (player.hasQuest("Ketteh")){
+                            engine.say("Hi again, " + player.getName() + ".");
+                        } else {
+
+                            // We haver never seen the player before.
+                            engine.say("Hi " + player.getName() + ", nice to meet you.");
+                            player.setQuest("Ketteh", "seen");
+                        }
 					}
 				});
 
@@ -66,6 +74,21 @@ public class MeetKetteh implements IQuest {
 				"Ok, don't move. I'm calling the law enforcers!",
 				null);
 
+        npc.add(ConversationStates.ATTENDING,
+                "shout",
+                null,
+                ConversationStates.ATTENDING,
+                null,
+                new SpeakerNPC.ChatAction() {
+                    public void fire(Player player, String text,
+                            SpeakerNPC engine) {
+                        if (player.hasQuest("Ketteh") && player.getQuest("Ketteh").equals("seen_naked")) {
+                            engine.say("Oh, good, you wear clothes this time. The last time we met, you were naked!");
+                        } else {
+                            engine.say("Sometimes naked people pass by. I'll get really mad if this happens.");
+                        }
+                    }
+                });
 	}
 
 	public MeetKetteh(StendhalRPWorld w, StendhalRPRuleProcessor rules) {
