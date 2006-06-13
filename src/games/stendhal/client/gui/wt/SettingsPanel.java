11917fdeefb969e8476ce76b3d5375b3799c6626
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2006-06-13 17:56:16 +0000

    Fixed buddy closed window problem.

--- src/games/stendhal/client/gui/wt/SettingsPanel.java
+++ src/games/stendhal/client/gui/wt/SettingsPanel.java
@@ -1,259 +1,284 @@
 /* $Id$ */
-/***************************************************************************
- *                      (C) Copyright 2005 - Marauroa                      *
- ***************************************************************************
- ***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-/*
- * SettingsPanel.java
- *
- * Created on 26. Oktober 2005, 20:12
- */
+/***************************************************************************
+ *                      (C) Copyright 2005 - Marauroa                      *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/*
+ * SettingsPanel.java
+ *
+ * Created on 26. Oktober 2005, 20:12
+ */
 
-package games.stendhal.client.gui.wt;
+package games.stendhal.client.gui.wt;
 
-import games.stendhal.client.GameObjects;
-import games.stendhal.client.entity.Player;
-import games.stendhal.client.gui.wt.core.*;
-import games.stendhal.common.CollisionDetection;
+import games.stendhal.client.GameObjects;
+import games.stendhal.client.entity.Player;
+import games.stendhal.client.gui.wt.core.*;
+import games.stendhal.common.CollisionDetection;
 
-import java.awt.Graphics;
-import java.awt.GraphicsConfiguration;
-import java.awt.Point;
-import java.util.HashMap;
-import java.util.Map;
+import java.awt.Graphics;
+import java.awt.GraphicsConfiguration;
+import java.awt.Point;
+import java.util.HashMap;
+import java.util.Map;
 
-import marauroa.common.game.RPObject;
+import marauroa.common.game.RPObject;
 
-/**
- * The panel where you can adjust your settings
- * 
- * @author mtotz
- */
-public class SettingsPanel extends WtPanel implements WtClickListener,
-		WtCloseListener {
-	/** width of this panel */
-	private static final int WIDTH = 200;
+/**
+ * The panel where you can adjust your settings
+ * 
+ * @author mtotz
+ */
+public class SettingsPanel extends WtPanel implements WtClickListener,
+		WtCloseListener {
+	/** width of this panel */
+	private static final int WIDTH = 200;
 
-	/** buffered collision detection layer for minimap */
-	private CollisionDetection cd;
+	/** buffered collision detection layer for minimap */
+	private CollisionDetection cd;
 
-	/** buffered GraphicsConfiguration for minimap */
-	private GraphicsConfiguration gc;
+	/** buffered GraphicsConfiguration for minimap */
+	private GraphicsConfiguration gc;
 
-	/** buffered zone name for minimap */
-	private String zone;
+	/** buffered zone name for minimap */
+	private String zone;
 
-	/** buffered gameObjects for character panel */
-	private GameObjects gameObjects;
+	/** buffered gameObjects for character panel */
+	private GameObjects gameObjects;
 
-	/** the Character panel */
-	private Character character;
+	/** the Character panel */
+	private Character character;
 
-	private Buddies buddies;
+	private Buddies buddies;
 
-	/** the minimap panel */
-	private Minimap minimap;
+	/** the minimap panel */
+	private Minimap minimap;
 
-	/** the inventory */
-	private EntityContainer inventory;
+	/** the inventory */
+	private EntityContainer inventory;
 
-	/** the frame */
-	private WtPanel frame;
+	/** the frame */
+	private WtPanel frame;
 
-	/** the player */
-	private Player player;
+	/** the player */
+	private Player player;
 
-	/** map of the buttons (for faster access) ) */
-	private Map<String, WtButton> buttonMap;
+	/** map of the buttons (for faster access) ) */
+	private Map<String, WtButton> buttonMap;
 
-	/** is the minimap enabled (shown) */
-	private boolean minimapEnabled;
+	/** is the minimap enabled (shown) */
+	private boolean minimapEnabled;
 
-	/** is the character panel enabled (shown) */
-	private boolean characterEnabled;
+	/** is the character panel enabled (shown) */
+	private boolean characterEnabled;
 
-	/** is the inventory panel enabled (shown) */
-	private boolean inventoryEnabled;
+  /** is the inventory panel enabled (shown) */
+  private boolean inventoryEnabled;
 
-	/** Creates a new instance of OptionsPanel */
-	public SettingsPanel(WtPanel frame, GameObjects gameObjects) {
-		super("settings", (frame.getWidth() - WIDTH) / 2, 0, WIDTH, 200);
-		setTitletext("Settings");
-		setFrame(true);
-		setTitleBar(true);
-		setMinimizeable(true);
-		setMinimized(true);
-		setCloseable(false);
-		minimapEnabled = true;
-		characterEnabled = true;
-		inventoryEnabled = true;
+  /** is the buddies panel enabled (shown) */
+  private boolean buddiesEnabled;
 
-		character = new Character(gameObjects);
-		character.registerCloseListener(this);
-		frame.addChild(character);
+	/** Creates a new instance of OptionsPanel */
+	public SettingsPanel(WtPanel frame, GameObjects gameObjects) {
+		super("settings", (frame.getWidth() - WIDTH) / 2, 0, WIDTH, 200);
+		setTitletext("Settings");
+		setFrame(true);
+		setTitleBar(true);
+		setMinimizeable(true);
+		setMinimized(true);
+		setCloseable(false);
+		minimapEnabled = true;
+		characterEnabled = true;
+		inventoryEnabled = true;
 
-		buddies = new Buddies(gameObjects);
-		buddies.registerCloseListener(this);
-		frame.addChild(buddies);
+		character = new Character(gameObjects);
+		character.registerCloseListener(this);
+		frame.addChild(character);
 
-		inventory = new EntityContainer(gameObjects, "bag", 3, 4);
-		inventory.registerCloseListener(this);
-		frame.addChild(inventory);
+		buddies = new Buddies(gameObjects);
+		buddies.registerCloseListener(this);
+		frame.addChild(buddies);
 
-		buttonMap = new HashMap<String, WtButton>();
-		buttonMap.put("minimap", new WtButton("minimap", 150, 30,
-				"Enable Minimap"));
-		buttonMap.put("character", new WtButton("character", 150, 30,
-				"Enable Character"));
-		buttonMap.put("bag", new WtButton("bag", 150, 30, "Enable Inventory"));
+		inventory = new EntityContainer(gameObjects, "bag", 3, 4);
+		inventory.registerCloseListener(this);
+		frame.addChild(inventory);
 
-		int y = 10;
-		for (WtButton button : buttonMap.values()) {
-			button.moveTo(10, y);
-			y += 40;
-			button.setPressed(true);
-			button.registerClickListener(this);
-			addChild(button);
-		}
+		buttonMap = new HashMap<String, WtButton>();
+		buttonMap.put("minimap", new WtButton("minimap", 150, 30,
+				"Enable Minimap"));
+		buttonMap.put("character", new WtButton("character", 150, 30,
+				"Enable Character"));
+    buttonMap.put("bag", new WtButton("bag", 150, 30, "Enable Inventory"));
+    buttonMap.put("buddy", new WtButton("buddy", 150, 30, "Enable Buddies"));
 
-		this.gameObjects = gameObjects;
-		this.frame = frame;
-	}
+		int y = 10;
+		for (WtButton button : buttonMap.values()) {
+			button.moveTo(10, y);
+			y += 40;
+			button.setPressed(true);
+			button.registerClickListener(this);
+			addChild(button);
+		}
 
-	/** we're using the window manager */
-	protected boolean useWindowManager() {
-		return true;
-	}
+		this.gameObjects = gameObjects;
+		this.frame = frame;
+	}
 
-	/** updates the minimap */
-	public void updateMinimap(CollisionDetection cd, GraphicsConfiguration gc,
-			String zone) {
-		this.cd = cd;
-		this.gc = gc;
-		this.zone = zone;
+	/** we're using the window manager */
+	protected boolean useWindowManager() {
+		return true;
+	}
 
-		// close the old minimap if there is one
-		if (minimap != null) {
-			boolean enabled = minimapEnabled;
-			minimap.close(); // onClose() Listener disables the minimap
-			minimap = null;
+	/** updates the minimap */
+	public void updateMinimap(CollisionDetection cd, GraphicsConfiguration gc,
+			String zone) {
+		this.cd = cd;
+		this.gc = gc;
+		this.zone = zone;
 
-			minimapEnabled = enabled; // restore enabled state
-		}
-		if (minimapEnabled) {
-			// add a new one
-			minimap = new Minimap(cd, gc, zone);
-			minimap.registerCloseListener(this);
-			frame.addChild(minimap);
-		}
-	}
+		// close the old minimap if there is one
+		if (minimap != null) {
+			boolean enabled = minimapEnabled;
+			minimap.close(); // onClose() Listener disables the minimap
+			minimap = null;
 
-	/** updates the minimap */
-	public void setPlayer(RPObject playerObject) {
-		if (playerObject == null) {
-			return;
-		}
+			minimapEnabled = enabled; // restore enabled state
+		}
+		if (minimapEnabled) {
+			// add a new one
+			minimap = new Minimap(cd, gc, zone);
+			minimap.registerCloseListener(this);
+			frame.addChild(minimap);
+		}
+	}
 
-		Player newPlayer = (Player) gameObjects.get(playerObject.getID());
-		// check if the player object has changed. Note: this is an exact object
-		// reference check
-		if (newPlayer == player) {
-			return;
-		}
+	/** updates the minimap */
+	public void setPlayer(RPObject playerObject) {
+		if (playerObject == null) {
+			return;
+		}
 
-		this.player = newPlayer;
+		Player newPlayer = (Player) gameObjects.get(playerObject.getID());
+		// check if the player object has changed. Note: this is an exact object
+		// reference check
+		if (newPlayer == player) {
+			return;
+		}
 
-		if (character != null) {
-			character.setPlayer(player);
-		}
-		if (inventory != null) {
-			inventory.setSlot(player, "bag");
-		}
-	}
+		this.player = newPlayer;
 
-	/** draw the panel */
-	public Graphics draw(Graphics g) {
-		if (minimap != null && player != null) {
-			minimap.setPlayerPos(player.getx(), player.gety());
-		}
+		if (character != null) {
+			character.setPlayer(player);
+		}
+		if (inventory != null) {
+			inventory.setSlot(player, "bag");
+		}
+	}
 
-		return super.draw(g);
-	}
+	/** draw the panel */
+	public Graphics draw(Graphics g) {
+		if (minimap != null && player != null) {
+			minimap.setPlayerPos(player.getx(), player.gety());
+		}
 
-	/** a button was clicked */
-	public void onClick(String name, Point point) {
-		// check minimap
-		if (name.equals("minimap")) {
-			// minimap disabled?
-			if (minimapEnabled) {
-				minimap.close();
-				minimap = null;
-				minimapEnabled = false;
-				// be sure to update the button
-				buttonMap.get(name).setPressed(false);
-			} else if (!minimapEnabled) {
-				// minimap enabled
-				minimap = new Minimap(cd, gc, zone);
-				minimap.registerCloseListener(this);
-				frame.addChild(minimap);
-				minimapEnabled = true;
-				buttonMap.get(name).setPressed(true);
-			}
-		}
+		return super.draw(g);
+	}
 
-		// check character panel
-		if (name.equals("character")) {
-			// character disabled?
-			if (characterEnabled) {
-				character.close();
-				character = null;
-				characterEnabled = false;
-				// be sure to update the button
-				buttonMap.get(name).setPressed(false);
-			} else if (!characterEnabled) {
-				// character enabled
-				character = new Character(gameObjects);
-				character.registerCloseListener(this);
-				character.setPlayer(player);
-				frame.addChild(character);
-				characterEnabled = true;
-				buttonMap.get(name).setPressed(true);
-			}
-		}
+	/** a button was clicked */
+	public void onClick(String name, Point point) {
+		// check minimap
+		if (name.equals("minimap")) {
+			// minimap disabled?
+			if (minimapEnabled) {
+				minimap.close();
+				minimap = null;
+				minimapEnabled = false;
+				// be sure to update the button
+				buttonMap.get(name).setPressed(false);
+			} else if (!minimapEnabled) {
+				// minimap enabled
+				minimap = new Minimap(cd, gc, zone);
+				minimap.registerCloseListener(this);
+				frame.addChild(minimap);
+				minimapEnabled = true;
+				buttonMap.get(name).setPressed(true);
+			}
+		}
 
-		// check inventory panel
-		if (name.equals("bag")) {
-			// inventory disabled?
-			if (inventoryEnabled) {
-				inventory.close();
-				inventory = null;
-				inventoryEnabled = false;
-				// be sure to update the button
-				buttonMap.get("bag").setPressed(false);
-			} else if (!inventoryEnabled) {
-				// character enabled
-				inventory = new EntityContainer(gameObjects, "bag", 3, 4);
-				inventory.registerCloseListener(this);
-				frame.addChild(inventory);
-				inventory.setSlot(player, "bag");
-				inventoryEnabled = true;
-				// be sure to update the button
-				buttonMap.get("bag").setPressed(true);
-			}
-		}
-	}
+		// check character panel
+		if (name.equals("character")) {
+			// character disabled?
+			if (characterEnabled) {
+				character.close();
+				character = null;
+				characterEnabled = false;
+				// be sure to update the button
+				buttonMap.get(name).setPressed(false);
+			} else if (!characterEnabled) {
+				// character enabled
+				character = new Character(gameObjects);
+				character.registerCloseListener(this);
+				character.setPlayer(player);
+				frame.addChild(character);
+				characterEnabled = true;
+				buttonMap.get(name).setPressed(true);
+			}
+		}
 
-	/** a window is closed */
-	public void onClose(String name) {
-		// pseudo-release the button
-		onClick(name, null);
-	}
+		// check inventory panel
+		if (name.equals("bag")) {
+			// inventory disabled?
+			if (inventoryEnabled) {
+				inventory.close();
+				inventory = null;
+				inventoryEnabled = false;
+				// be sure to update the button
+				buttonMap.get("bag").setPressed(false);
+			} else if (!inventoryEnabled) {
+				// character enabled
+				inventory = new EntityContainer(gameObjects, "bag", 3, 4);
+				inventory.registerCloseListener(this);
+				frame.addChild(inventory);
+				inventory.setSlot(player, "bag");
+				inventoryEnabled = true;
+				// be sure to update the button
+				buttonMap.get("bag").setPressed(true);
+			}
+		}
 
-}
+    // check buddy panel
+    if (name.equals("buddy")) {
+      // buddy disabled?
+      if (buddiesEnabled) {
+        buddies.close();
+        buddies = null;
+        buddiesEnabled = false;
+        // be sure to update the button
+        buttonMap.get("buddy").setPressed(false);
+      } else if (!buddiesEnabled) {
+        // character enabled
+        buddies = new Buddies(gameObjects);
+        buddies.registerCloseListener(this);
+        frame.addChild(buddies);
+        buddiesEnabled = true;
+        // be sure to update the button
+        buttonMap.get("buddy").setPressed(true);
+      }
+    }
+	}
+
+	/** a window is closed */
+	public void onClose(String name) {
+		// pseudo-release the button
+		onClick(name, null);
+	}
+
+}
+
