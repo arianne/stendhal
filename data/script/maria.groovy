4e0ec7abbd8445e58357e6592060199c8faee2c2
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-06-15 21:20:03 +0000

    initial checkin of an admin summonable NPC which sells food&drinks at stand-up-parties. Use /script maria.groovy and she will appear on your right side.

---
+++ data/script/maria.groovy
@@ -0,0 +1,38 @@
+/* $Id$ */
+
+import games.stendhal.server.entity.*
+import games.stendhal.server.entity.item.*
+import games.stendhal.server.scripting.*
+import games.stendhal.server.entity.npc.*;
+import games.stendhal.server.pathfinder.Path
+import games.stendhal.common.Direction;
+
+/**
+ * Creates a portable NPC which sell foods&drinks at meetings.
+ *
+ * As admin use /script portable_margaret.groovy to sommon her right next to you.
+ * Please put her back in int_admin_playground after use.
+ */
+
+	// Create NPC
+	npc=new ScriptingNPC("Maria")
+	npc.setClass("tavernbarmaidnpc")
+
+	// Place NPC
+	myZone = "int_admin_playground";
+	x = 11;
+	y = 4;
+	if (player != null) {
+		myZone = game.getZone(player);
+		x = player.getx() + 1;
+		y = player.gety();
+	}
+	game.setZone(myZone);
+	npc.set(x, y);
+	game.add(npc)
+
+	// Create Dialog
+	npc.behave("greet", "Hi, how can i help you?")
+	npc.behave("job","I am the bar maid for the fair tavern. We sell fine beers and food.")
+	npc.behave("help", "You can get an #offer of drinks and take a break to meet new people!")
+	npc.behave("sell", ShopList.get().get("food&drinks"))
