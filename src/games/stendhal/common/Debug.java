aa855086bbfa79f16c44102fb9090b38508f0b23
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2006-06-17 16:57:41 +0000

    "Fixed?" double avatars bug

--- src/games/stendhal/common/Debug.java
+++ src/games/stendhal/common/Debug.java
@@ -18,7 +18,7 @@ package games.stendhal.common;
  * TODO: Reemplaze with a XML configuration file?
  */
 public class Debug {
-	public static final String VERSION = "0.50";
+	public static final String VERSION = "0.51";
 
 	/** if this is enable tileset are loaded on demand. */
 	public static final boolean VERY_FAST_CLIENT_START = true;
