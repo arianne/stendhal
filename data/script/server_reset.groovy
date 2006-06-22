f4fc003b4fcd2a814ecc0a4b5ace7a9485783805
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-06-22 16:47:57 +0000

    realy kill the server

--- data/script/server_reset.groovy
+++ data/script/server_reset.groovy
@@ -1,12 +1,12 @@
 /**
- * Exists the server the hard way. Do not use it unless the server has
- * already crashed. You should warn connected players to logout if that
- * is still possible.
+ * Kills the server the hard way without doing a normal shutdown.
+ * Do not use it unless the server has already crashed. You should
+ * warn connected players to logout if that is still possible.
  * 
  * If the server is started in a loop, it will come up again:
  * while sleep 60; do java -jar marauroa -c marauroa.ini -l; done
  */
 
 if (player != null) {
-	System.exit(1);
+	Runtime.getRuntime().halt(1);
 }
