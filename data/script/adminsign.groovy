621b36ab5e2258bc86e26c9c579a02ef4e4eea9d
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-07-14 07:37:29 +0000

    new method signToString and syntax-fixes (/me wants compile-as-you-type-support for groovy).

--- data/script/adminsign.groovy
+++ data/script/adminsign.groovy
@@ -24,7 +24,7 @@ class AdminSignManager {
 			if (myZone == "-") {
 				game.setZone(game.getZone(player));
 			} else {
-				game.setZone(myZone);
+				game.setZone(myZone); // TODO seems not to work
 			}
 			int x = 0;
 			if (args[1] == "-") {
@@ -68,24 +68,37 @@ class AdminSignManager {
 			storage.put("signcounter", signcounter);
 		} else {
 			// syntax error, print help text
-			game.privateText(player, "Syntax: adminsign.groovy <zone> <x> <y> <text> The first 3 parameters can be \"-\".\r\nadminsign.groovy list\r\nadminsign.groovy del <n>");
+			game.privateText(player, "This script creates, lists or removes signs. Syntax: \r\nadminsign.groovy <zone> <x> <y> <text> The first 3 parameters can be \"-\".\r\nadminsign.groovy list\r\nadminsign.groovy del <n>");
 		}
 	}
 	
-	public void delete(String[] args) {
+	public void delete(Player player, String[] args) {
 		int i = Integer.parseInt(args[1]);
 		Sign sign = storage.get(i);
 		if (sign != null) {
 			storage.remove(i);
 			game.remove(sign);
-			// TODO put above display-code in a own method and invoke it here
-			game.privateText(player, "Sign removed");
+			StringBuilder sb = new StringBuilder();
+			sb.append("Removed sign ");
+			signToString(sb, sign);
+			game.privateText(player, sb.toString());
 		} else {
 			game.privateText(player, "Sign " + i + " does not exist");
 		}
 	}
 	
-	public void list(String[] args) {
+	private void signToString(StringBuilder sb, Sign sign) {
+		String zone = sign.getWorld().getRPZone(sign.getID()).getID().getID();
+		sb.append(zone);
+		sb.append(" ");
+		sb.append(sign.getx());
+		sb.append(" ");
+		sb.append(sign.gety());
+		sb.append(" ");
+		sb.append("\"" + sign.get("text") + "\"");
+	}
+	
+	public void list(Player player, String[] args) {
 		StringBuilder sb = new StringBuilder();
 		sb.append("Listing signs:");
 		int signcounter = storage.get("signcounter");
@@ -99,14 +112,7 @@ class AdminSignManager {
 				sb.append("\r\n");
 				sb.append(i);
 				sb.append(". ");
-				String zone = sign.getWorld().getRPZone(sign.getID()).getID().getID();
-				sb.append(zone);
-				sb.append(" ");
-				sb.append(sign.getx());
-				sb.append(" ");
-				sb.append(sign.gety());
-				sb.append(" ");
-				sb.append("\"" + sign.get("text") + "\"");
+				signToString(sb, sign);
 			}
 			i++;
 		}
@@ -118,9 +124,9 @@ class AdminSignManager {
 		// read zone and x,y. Use player's data as default on "-".
 		String temp = args[0];
 		if (temp.equals("list")) {
-			list(args);
-		} else if (temp.equals("del")) {
-			del(args);
+			list(player, args);
+		} else if (temp.equals("del") || temp.equals("delete") || temp.equals("remove")) {
+			delete(player, args);
 		} else {
 			add(player, args);
 		}
