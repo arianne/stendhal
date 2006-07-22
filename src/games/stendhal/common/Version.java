b920e6f9b020caf2c54264f6820183573a748cc5
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-07-22 20:37:50 +0000

    added compare()

--- src/games/stendhal/common/Version.java
+++ src/games/stendhal/common/Version.java
@@ -18,7 +18,7 @@ public class Version {
 	 * @param parts number of parts to extract
 	 * @return parts of the version-string
 	 */
-	public static String extractVersion(String version, int parts) {
+	public static String cut(String version, int parts) {
 		int pos = 0;
 		for (int i = 0; i < parts; i++) {
 			int temp = version.indexOf(".", pos + 1);
@@ -30,14 +30,57 @@ public class Version {
 		}
 		return version.substring(0, pos);
 	}
+
+	/**
+	 * compares to versions
+	 *
+	 * @param v1 1st version string
+	 * @param v2 2nd version string
+	 * @return see compare
+	 */
+	public static int compare(String v1, String v2) {
+		while (!v1.equals("") || !v2.equals("")) {
+			String c1;
+			int p1 = v1.indexOf(".");
+			if (p1 > -1) {
+				c1 = v1.substring(0, p1);
+				v1 = v1.substring(p1 + 1);
+			} else {
+				c1 = v1;
+				v1 = "";
+			}
+			if (c1.equals("")) {
+				c1 = "0";
+			}
 	
-	public static boolean checkVersionCompatibility(String v1, String v2) {
-		String ev1 = extractVersion(v1, 2);
-		String ev2 = extractVersion(v2, 2);
+			String c2;
+			int p2 = v2.indexOf(".");
+			if (p2 > -1) {
+				c2 = v2.substring(0, p2);
+				v2 = v2.substring(p2 + 1);
+			} else {
+				c2 = v2;
+				v2 = "";
+			}
+			if (c2.equals("")) {
+				c2 = "0";
+			}
+			
+			int res = c1.compareTo(c2);
+			if (res != 0) {
+				return res;
+			}
+		}
+		return 0;
+	}
+
+	public static boolean checkCompatibility(String v1, String v2) {
+		String ev1 = cut(v1, 2);
+		String ev2 = cut(v2, 2);
 		boolean res = ev1.equals(ev2);
 		return res;
 	}
-	
+
 	private Version() {
 		// hide constructor; this is a static class
 	}
--- src/games/stendhal/test/common/TestVersion.java
+++ src/games/stendhal/test/common/TestVersion.java
@@ -5,19 +5,27 @@ import games.stendhal.common.Version;
 public class TestVersion extends TestCase {
 
 	public void testExtractVersion() {
-		assertEquals("extratVersion 0: ", "",       Version.extractVersion("0.52.1", 0));
-		assertEquals("extratVersion 1: ", "0",      Version.extractVersion("0.52.1", 1));
-		assertEquals("extratVersion 2: ", "0.52",   Version.extractVersion("0.52.1", 2));
-		assertEquals("extratVersion 3: ", "0.52.1", Version.extractVersion("0.52.1", 3));
-		assertEquals("extratVersion 4: ", "0.52.1", Version.extractVersion("0.52.1", 4));
+		assertEquals("extratVersion 0: ", "",       Version.cut("0.52.1", 0));
+		assertEquals("extratVersion 1: ", "0",      Version.cut("0.52.1", 1));
+		assertEquals("extratVersion 2: ", "0.52",   Version.cut("0.52.1", 2));
+		assertEquals("extratVersion 3: ", "0.52.1", Version.cut("0.52.1", 3));
+		assertEquals("extratVersion 4: ", "0.52.1", Version.cut("0.52.1", 4));
 	}
 	
 	public void testCheckVersionCompatibility() {
-		assertTrue("VersionCompatible 0.52 ~ 0.52", Version.checkVersionCompatibility("0.52", "0.52"));
-		assertTrue("VersionCompatible 0.52 ~ 0.52.0", Version.checkVersionCompatibility("0.52", "0.52.0"));
-		assertTrue("VersionCompatible 0.52 ~ 0.52.1", Version.checkVersionCompatibility("0.52", "0.52.1"));
-		assertTrue("VersionCompatible ! 0.53 ~ 0.52", !Version.checkVersionCompatibility("0.53", "0.52"));
-		assertTrue("VersionCompatible ! 0.52 ~ 0.53", !Version.checkVersionCompatibility("0.52", "0.53"));
+		assertTrue("VersionCompatible 0.52 ~ 0.52", Version.checkCompatibility("0.52", "0.52"));
+		assertTrue("VersionCompatible 0.52 ~ 0.52.0", Version.checkCompatibility("0.52", "0.52.0"));
+		assertTrue("VersionCompatible 0.52 ~ 0.52.1", Version.checkCompatibility("0.52", "0.52.1"));
+		assertTrue("VersionCompatible ! 0.53 ~ 0.52", !Version.checkCompatibility("0.53", "0.52"));
+		assertTrue("VersionCompatible ! 0.52 ~ 0.53", !Version.checkCompatibility("0.52", "0.53"));
+	}
+	
+	public void testCompare() {
+		assertTrue("VersionCompare 0.52 ~ 0.52",   Version.compare("0.52", "0.52")   == 0);
+		assertTrue("VersionCompare 0.52 ~ 0.52.0", Version.compare("0.52", "0.52.0") == 0);
+		assertTrue("VersionCompare 0.52 ~ 0.52.1", Version.compare("0.52", "0.52.1") < 0);
+		assertTrue("VersionCompare ! 0.53 ~ 0.52", Version.compare("0.53", "0.52")   > 0);
+		assertTrue("VersionCompare ! 0.52 ~ 0.53", Version.compare("0.52", "0.53")   < 0);
 	}
 
 	public static void main(String[] args) {
