703a5aa0d48f92b3faf1c99c2ee0d6966f8bb6d1
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-07-23 20:03:53 +0000

    initial checkin

---
+++ data/script/plague.groovy
@@ -0,0 +1,23 @@
+// extract position of admin
+myZone = game.getZone(player);
+x = player.getx();
+y = player.gety();
+game.setZone(myZone);
+
+// create monsters
+String creatureClass = "rat";
+if (args.length >= 2) {
+	creatureClass = args[1];
+}
+creature = game.getCreature(creatureClass);
+if (args.length < 1) {
+	game.add(creature, x, y);
+} else {
+	int k = Integer.parseInt(args[0]);
+	for (dx in -k..k) {
+		for (dy in -k..k) {
+			game.add(creature, x + dx, y + dy);
+		}
+	}
+}
+
