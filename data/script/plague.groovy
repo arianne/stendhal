5dc7cded5fa61b0d419200c97d056b13aa976b0e
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-07-23 20:35:54 +0000

    do not run on startup

--- data/script/plague.groovy
+++ data/script/plague.groovy
@@ -1,23 +1,27 @@
-// extract position of admin
-myZone = game.getZone(player);
-x = player.getx();
-y = player.gety();
-game.setZone(myZone);
+if (player != null) {
 
-// create monsters
-String creatureClass = "rat";
-if (args.length >= 2) {
-	creatureClass = args[1];
-}
-creature = game.getCreature(creatureClass);
-if (args.length < 1) {
-	game.add(creature, x, y);
-} else {
-	int k = Integer.parseInt(args[0]);
-	for (dx in -k..k) {
-		for (dy in -k..k) {
-			game.add(creature, x + dx, y + dy);
+	// extract position of admin
+	myZone = game.getZone(player);
+	x = player.getx();
+	y = player.gety();
+	game.setZone(myZone);
+
+	// select creature
+	String creatureClass = "rat";
+	if (args.length >= 2) {
+		creatureClass = args[1];
+	}
+	creature = game.getCreature(creatureClass);
+
+	// spawn the specified amout of them
+	if (args.length < 1) {
+		game.add(creature, x, y);
+	} else {
+		int k = Integer.parseInt(args[0]);
+		for (dx in -k..k) {
+			for (dy in -k..k) {
+				game.add(creature, x + dx, y + dy);
+			}
 		}
 	}
 }
-
