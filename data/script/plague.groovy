8944f86b76af9a9e291ef49891a954b9ce2f19e8
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-07-30 11:18:06 +0000

    check the name of creature

--- data/script/plague.groovy
+++ data/script/plague.groovy
@@ -12,16 +12,22 @@ if (player != null) {
 		creatureClass = args[1];
 	}
 	creature = game.getCreature(creatureClass);
-
-	// spawn the specified amout of them
-	if (args.length < 1) {
-		game.add(creature, x, y);
+	if (creature == null) {
+		player.sendPrivateText("No such creature");
 	} else {
-		int k = Integer.parseInt(args[0]);
-		if (k < 5) {
-			for (dx in -k..k) {
-				for (dy in -k..k) {
-					game.add(creature, x + dx, y + dy);
+
+		// spawn the specified amout of them
+		if (args.length < 1) {
+			game.add(creature, x, y);
+		} else {
+			int k = Integer.parseInt(args[0]);
+			if (k < 5) {
+				for (dx in -k..k) {
+					for (dy in -k..k) {
+						if ((dx != 0) || (dy != 0)) {
+							game.add(creature, x + dx, y + dy);
+						}
+					}
 				}
 			}
 		}
