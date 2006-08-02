adc954e7a62990810e0f644f7c7a98ed5adf57cf
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-08-02 21:15:49 +0000

    read irc-login (NickServ) from config-file

--- src/games/stendhal/bot/postman/PostmanIRC.java
+++ src/games/stendhal/bot/postman/PostmanIRC.java
@@ -1,7 +1,10 @@
 package games.stendhal.bot.postman;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Properties;
 
+import org.apache.log4j.Logger;
 import org.jibble.pircbot.IrcException;
 import org.jibble.pircbot.NickAlreadyInUseException;
 import org.jibble.pircbot.PircBot;
@@ -12,6 +15,16 @@ import org.jibble.pircbot.PircBot;
  * @author hendrik
  */
 public class PostmanIRC extends PircBot {
+	private static Logger logger = Logger.getLogger(PostmanIRC.class);
+	private Properties prop = new Properties();
+	
+	public PostmanIRC() {
+        try {
+            this.prop.loadFromXML(new FileInputStream(System.getProperty("user.home") + "/.stendhal-postman-conf.xml"));
+        } catch (Exception e) {
+            logger.error(e, e);
+        }
+	}
 
 	/**
 	 * Postman IRC bot.
@@ -21,13 +34,15 @@ public class PostmanIRC extends PircBot {
 	 * @throws NickAlreadyInUseException 
 	 */
 	public void connect() throws NickAlreadyInUseException, IOException, IrcException {
-		setName("postman-bot");
-		setLogin("postman");
+		setName(prop.getProperty("name"));
+		setLogin(prop.getProperty("login"));
 		setVersion("0.1");
 	    setVerbose(true);
 	    connect("irc.freenode.net");
 	    joinChannel("#arianne");
-	    // sendMessage("NickServ", "identify ");
+	    joinChannel("#arianne-support");
+	    String pass = prop.getProperty("pass");
+	    sendMessage("NickServ", "identify " + pass);
 	}
 
 	public static void main(String[] main) throws NickAlreadyInUseException, IOException, IrcException {
