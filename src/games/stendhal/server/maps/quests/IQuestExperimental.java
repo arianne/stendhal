c08bb6af0ca3d7e28331c909b9896404a8260323
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-08-03 20:52:55 +0000

    removed parameters which are now in quest.xml and added JavaDoc

--- src/games/stendhal/server/maps/quests/IQuestExperimental.java
+++ src/games/stendhal/server/maps/quests/IQuestExperimental.java
@@ -10,23 +10,63 @@ import java.util.List;
  */
 public interface IQuestExperimental extends IQuest{
 
+	/**
+	 * Initialize the quest on server startup.
+	 * This method may create or enhance SpeakerNPCs.
+	 */
 	public void init();
-	
-	public String getName();
-	
-	public String getTitle();
-	
-	public String getDescription();
-	
-	public String getGMDescription();
-	
+
+	/**
+	 * Adjusts the quest states of an old release. This method is
+	 * called after a player logs in the first time after an server
+	 * update.
+	 *
+	 * @param player Player
+	 */
+	public void convertOnUpdate(Player player);
+
+	/**
+	 * Was the quest started?
+	 *
+	 * @param player Player
+	 * @return true, if it was started, false otherwise
+	 */
 	public boolean isStarted(Player player);
-	
+
+	/**
+	 * Was the quest completed?<!--sic--> Note: A quest can be completed
+	 * without its status beeing "Done" (e. g. rejected, failed). 
+	 *
+	 * @param player Player
+	 * @return true, if it was completed, false otherwise
+	 */
 	public boolean isCompleted(Player player);
-	
+
+	/**
+	 * May the quest be repeated?
+	 *
+	 * @param player Player
+	 * @return true, if it can be repeated, false otherwise
+	 */
 	public boolean isRepeatable(Player player);
-	
+
+	/**
+	 * Gets a list of history item-names.
+	 * The texts will be looked up in quest.xml
+	 *
+	 * @param player Player
+	 * @return list of history item-names
+	 */
 	public List<String> getHistory(Player player);
-	
-	public String getHint(Player player);
+
+	/**
+	 * Gets a list of possible hint-names. The hint system will ensure
+	 * that the same hint is not displayed twice. This class creates
+	 * a list of useful hints (without hints about already completed parts). 
+	 * The texts will be looked up in quest.xml
+	 *
+	 * @param player Player
+	 * @return list of history item-names
+	 */
+	public List<String> getHint(Player player);
 }
