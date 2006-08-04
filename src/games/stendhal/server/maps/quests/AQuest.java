eac7fbb692b4d9c98898ca1153afddd8eff763e4
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-08-04 14:58:35 +0000

    implemented some methods

--- src/games/stendhal/server/maps/quests/AQuest.java
+++ src/games/stendhal/server/maps/quests/AQuest.java
@@ -5,19 +5,33 @@ import games.stendhal.server.StendhalRPWorld;
 import games.stendhal.server.entity.Player;
 import games.stendhal.server.entity.npc.NPCList;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Abstract class for quests. This is a default implementation of IQuest. 
- *
+ * Abstract class for quests. This is a default implementation of IQuest.
+ * 
  * @author hendrik
  */
 public class AQuest implements IQuest {
+
+	/** The internal name of this quest. (e.g used to read quest.xml) */
 	protected String name = null;
+
+	/** The slot-name in !quests */
+	protected String slotName = "XXX"; // TODO init it in the quest-classes
+
+	/** NPCList * */
 	protected NPCList npcs = null;
+
+	/** StendhalRPRuleProcessor * */
 	protected StendhalRPRuleProcessor rules = null;
+
+	/** StendhalRPWorld * */
 	protected StendhalRPWorld world = null;
 
+	
+	private static final List<String> emptyList = new ArrayList<String>();
 
 	public void init(String name) {
 		this.name = name;
@@ -34,18 +48,19 @@ public class AQuest implements IQuest {
 	}
 
 	public List<String> getHint(Player player) {
-		// TODO Auto-generated method stub
-		return null;
+		return emptyList;
 	}
 
 	public List<String> getHistory(Player player) {
-		// TODO Auto-generated method stub
-		return null;
+		// TODO this method should be abstact after all quests are converted
+		return emptyList;
 	}
 
 	public boolean isCompleted(Player player) {
-		// TODO Auto-generated method stub
-		return false;
+		return player.hasQuest(slotName)
+				&& (player.isQuestCompleted(slotName)
+						|| player.getQuest(slotName).equals("rejected") || player
+						.getQuest(slotName).equals("failed"));
 	}
 
 	public boolean isRepeatable(Player player) {
@@ -54,8 +69,7 @@ public class AQuest implements IQuest {
 	}
 
 	public boolean isStarted(Player player) {
-		// TODO Auto-generated method stub
-		return false;
+		return player.hasQuest(slotName);
 	}
 
 }
