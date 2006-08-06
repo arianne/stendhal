0f849b7184c38ecea7d338cca079f9fa28c61063
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-08-06 22:23:48 +0000

    added documentation

--- src/games/stendhal/server/maps/quests/PlinksToy.java
+++ src/games/stendhal/server/maps/quests/PlinksToy.java
@@ -7,15 +7,35 @@ import games.stendhal.server.entity.PlantGrower;
 import marauroa.common.game.IRPZone;
 
 /**
- * @author hendrik
+ * QUEST: Plink's Toy
+ *
+ * PARTICIPANTS:
+ * - Plink
+ * - some wolfs
+ *
+ * STEPS:
+ * - Plink tells you that he got scared by some wolfs and
+	 ran away dropping his teddy.
+ * - Find the teddy in the Park Of Wolfs
+ * - Bring it back to Plink
+ *
+ * REWARD:
+ * - 20 XP
+ *
+ * REPETITIONS:
+ * - None.
  */
 public class PlinksToy extends AQuest {
-	
+
 	private static final String QUEST_SLOT = "plinks_toy";
 
 	@Override
 	public void init(String name) {
 		super.init(name, QUEST_SLOT);
+	}
+
+	private void step_1() {
+		// TODO: plink asks for his teddy
 	}
 	
 	private void step_2() {
@@ -26,18 +46,21 @@ public class PlinksToy extends AQuest {
 		plantGrower.sety(84);
 		plantGrower.setDescription("Plink lost his teddy here.");
 		zone.add(plantGrower);
-		
+
 		rules.getPlantGrowers().add(plantGrower);
+	}
+	
+	private void step_3() {
+		// TODO: take the teddy back to Plink 
 	}
 
 	@Override
 	public void addToWorld(StendhalRPWorld world, StendhalRPRuleProcessor rules) {
 		super.addToWorld(world, rules);
 
+		step_1();
 		step_2();
+		step_3();
 	}
-
-
-	
 
 }
