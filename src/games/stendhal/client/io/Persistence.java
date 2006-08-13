8bffc8c72423b60f864070ec764033de5cabcf1c
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-08-13 21:59:47 +0000

    initial checkin

---
+++ src/games/stendhal/client/io/Persistence.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package games.stendhal.client.io;
+
+import games.stendhal.common.Debug;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Allows transparent access to files. Subclasses implement Persistence
+ * for normal and webstart environment.
+ *
+ * @author hendrik
+ */
+public abstract class Persistence {
+	private static Persistence instance = null;
+
+	/**
+	 * Returns the Persistence manager for this environmen
+	 *
+	 * @return Persistence
+	 */
+	public static Persistence get() {
+		if (instance == null) {
+			if (Debug.WEB_START_SANDBOX) {
+				// TODO: reflection
+			} else {
+				// TODO: instance = new FileSystemPersistence()
+			}
+		}
+		return instance;
+	}
+	
+	/**
+	 * Gets an input stream to this "virtual" file
+	 *
+	 * @param filename filename (without path)
+	 * @return InputStream
+	 * @throws IOException on io error
+	 */
+	public abstract InputStream getInputStream(String filename) throws IOException;
+
+	/**
+	 * Gets an output stream to this "virtual" file
+	 *
+	 * @param filename filename (without path)
+	 * @return InputStream
+	 * @throws IOException on io error
+	 */
+	public abstract OutputStream getOutputStream(String filename) throws IOException;
+
+}
