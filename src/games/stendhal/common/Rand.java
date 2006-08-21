095b5dff48d720efd8171335627db52daeedcaff
Daniel Herding <wikipedian@users.sourceforge.net>
2006-08-21 17:18:34 +0000

    docu

--- src/games/stendhal/common/Rand.java
+++ src/games/stendhal/common/Rand.java
@@ -14,6 +14,9 @@ package games.stendhal.common;
 
 import java.util.Random;
 
+/**
+ * Helper functions to generate random numbers.
+ */
 public class Rand {
 	private static Random rand;
 
@@ -21,27 +24,55 @@ public class Rand {
 		rand = new Random();
 	}
 
+	/**
+	 * Simulates flipping a coin.
+	 * @return Either 1 or 2, equally distributed.
+	 */
 	public static int throwCoin() {
 		return rand.nextInt(2) + 1;
 	}
 	
+	/**
+	 * Simulates rolling a dice with 6 sides.
+	 * @return A random number between 1 and 6, equally distributed.
+	 */
 	public static int roll1D6() {
 		return rand.nextInt(6) + 1;
 	}
 
+	/**
+	 * Simulates rolling a dice with 20 sides.
+	 * @return A random number between 1 and 20, equally distributed.
+	 */
 	public static int roll1D20() {
 		return rand.nextInt(20) + 1;
 	}
 
+	/**
+	 * Simulates rolling a dice with 100 sides.
+	 * @return A random number between 1 and 100, equally distributed.
+	 */
 	public static int roll1D100() {
 		return rand.nextInt(100) + 1;
 	}
 
-	public static int rand(int max) {
-		return rand.nextInt(max);
+	/**
+	 * Simulates rolling a dice with the given number of sides.
+	 * @param numberOfSides the number of sides that the simulated dice has
+	 * @return A random number between 1 and <i>numberOfSides</i>, equally
+	 *         distributed.
+	 */
+	public static int rand(int numberOfSides) {
+		return rand.nextInt(numberOfSides);
 	}
 
-	public static int rand(int n, int sd) {
-		return (int) (rand.nextGaussian() * sd + n);
+	/**
+	 * Generates a normally distributed random number and rounds it. 
+	 * @param mean The mean value
+	 * @param sd The standard deviation
+	 * @return An integer near <i>mean</i>
+	 */
+	public static int rand(int mean, int sd) {
+		return (int) (rand.nextGaussian() * sd + mean);
 	}
 }
