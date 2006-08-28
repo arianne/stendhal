141f7096d21e9634bcbbd6de96da23a8ef3b4662
Daniel Herding <wikipedian@users.sourceforge.net>
2006-08-28 00:43:37 +0000

    don't highlight text except for stuff that players can reply fixed minor typos etc.

--- src/games/stendhal/server/maps/quests/MeetKetteh.java
+++ src/games/stendhal/server/maps/quests/MeetKetteh.java
@@ -39,22 +39,21 @@ public class MeetKetteh extends AbstractQuest{
                         int outfit = player.getInt("outfit");
                         int clothes_index = (outfit / 100) % 100;
 
-                        // Check whether player is naked
                         if (clothes_index == 0) {
-                            engine.say("Who are you? Aiiieeeee!!! You're naked! Right-click on you and choose SET OUTFIT! Shhh! Don't even think on clicking on the white bar at the bottom and write to reply to me! And if you happen to talk to anyone in the city you'd better begin the conversation saying HI. And don't be gross and just leave: say BYE to end the conversation. And use #CTRL #+ #arrow to turn around and face me when I'm talking to you! Wait! I am sure you are a friend of that onlooker Nomyr who's always peeking at the windows! Now use the #arrows and go out!");
+                            // The player is naked
+                            engine.say("Who are you? Aiiieeeee!!! You're naked! Right-click on yourself and choose SET OUTFIT! Shhh! Don't even think on clicking on the white bar at the bottom and writing to reply to me! And if you happen to talk to anyone in the city you'd better begin the conversation saying HI. And don't be gross and just leave: say BYE to end the conversation. And use CTRL + arrow to turn around and face me when I'm talking to you! Wait! I am sure you are a friend of that onlooker Nomyr who's always peeking at the windows! Now use the arrow keys and go out!");
                             player.setQuest("Ketteh", "seen_naked");
 
-                        // OK, player is NOT naked this time, but was last time.
                         } else if (player.hasQuest("Ketteh") && player.getQuest("Ketteh").equals("seen_naked")) {
+                            // OK, player is NOT naked this time, but was last time.
 							engine.say("Hi again, " + player.getName()
-									+ ". How can I #shout at you this time?");
+						 			+ ". How can I #shout at you this time?");
                             player.setQuest("Ketteh", "seen"); // don't be unforgiving
 
-                        // We have met the player before and it was NOT naked last time nor is it know
                         } else if (player.hasQuest("Ketteh")){
+                            // We have met the player before and he was NOT naked last time nor is he now
                             engine.say("Hi again, " + player.getName() + ".");
                         } else {
-
                             // We haver never seen the player before.
                             engine.say("Hi " + player.getName() + ", nice to meet you.");
                             player.setQuest("Ketteh", "seen");
@@ -81,7 +80,7 @@ public class MeetKetteh extends AbstractQuest{
                         if (player.hasQuest("Ketteh") && player.getQuest("Ketteh").equals("seen_naked")) {
                             engine.say("Oh, good, you wear clothes this time. The last time we met, you were naked!");
                         } else {
-                            engine.say("Sometimes naked people pass by. I'll get really mad if this happens.");
+                            engine.say("Sometimes naked people pass by. I get really mad when that happens.");
                         }
                     }
                 });
