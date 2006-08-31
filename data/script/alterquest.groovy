b86fb29c3b3db43e039bee712da3e67fcbd6042c
Daniel Herding <wikipedian@users.sourceforge.net>
2006-08-31 14:57:09 +0000

    fixed bugs

--- data/script/alterquest.groovy
+++ data/script/alterquest.groovy
@@ -1,18 +1,15 @@
+import games.stendhal.server.entity.Player;
+
 if (player != null) {
-	List players = rules.getPlayers();
-	boolean found = false;
-	for (aPlayer in players) {
-		if (aPlayer.getName().equals(args[0])) {
-			aPlayer.sendPrivateText("Admin " + player.getName() + " changed your state of the quest '" 
-					+ args[1] + "' from '" + oldQuestState + "' to " + args[3]);
-			aPlayer.setQuest(args[1], args[2]);
-		}
-	}
-	
-	if (found) {
+	Player target = rules.getPlayer(args[0]);
+	if (target != null) {
+		String oldQuestState = target.getQuest(args[1]);
+		target.setQuest(args[1], args[2]);
+		target.sendPrivateText("Admin " + player.getName() + " changed your state of the quest '" 
+				+ args[1] + "' from '" + oldQuestState + "' to '" + args[2] + "'");
 		player.sendPrivateText("Changed the state of quest '" 
-				+ args[1] + "' from '" + oldQuestState + "' to " + args[3]);
+				+ args[1] + "' from '" + oldQuestState + "' to '" + args[2] + "'");
 	} else {
-		player.sendPrivateText(args[0] " is not logged in");
+		player.sendPrivateText(args[0] + " is not logged in");
 	}
 }
