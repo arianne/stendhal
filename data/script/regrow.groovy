c0090177824c15aacd64952a1e3feb524296df77
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-09-04 05:27:54 +0000

    removed regrow.groovy

--- data/script/regrow.groovy
+++
@@ -1,9 +0,0 @@
-// Sets all plant growers to full growth.
-
-import games.stendhal.server.StendhalRPRuleProcessor;
-import games.stendhal.server.entity.PlantGrower;
-
-for (PlantGrower grower in StendhalRPRuleProcessor.get().getPlantGrowers()) {
-	grower.setToFullGrowth();
-	grower.notifyWorldAboutChanges();
-}
