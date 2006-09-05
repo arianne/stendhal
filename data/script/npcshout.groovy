1d4210670e5426eda48fcd7d65a856cf03e7b903
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-09-05 21:43:35 +0000

    deprecated warning

--- data/script/npcshout.groovy
+++ data/script/npcshout.groovy
@@ -1,27 +1,3 @@
-import games.stendhal.server.*
-import games.stendhal.server.entity.*
-
 if (player != null) {
-	if (args.length < 2) {
-		player.sendPrivateText("Usage: /script npcshout.groovy npc text");
-	} else {
-		StringBuilder sb = new StringBuilder();
-		sb.append(args[0]);
-		sb.append(" shouts: ");
-		boolean first = true;
-		for (String word in args) {
-			if (first) {
-				first = false;
-			} else {
-				sb.append(word);
-				sb.append(" ");
-			}
-		}
-		String text = sb.toString();
-	
-		List players = rules.getPlayers();
-		for (player2 in players) {
-			player2.sendPrivateText(text);
-		}
-	}
+	player.sendPrivateText("This script was renamed to /script NPCShout.class");
 }
--- data/script/plague.groovy
+++ data/script/plague.groovy
@@ -1,35 +1,3 @@
 if (player != null) {
-
-	// extract position of admin
-	myZone = game.getZone(player);
-	x = player.getX();
-	y = player.getY();
-	game.setZone(myZone);
-
-	// select creature
-	String creatureClass = "rat";
-	if (args.length >= 2) {
-		creatureClass = args[1];
-	}
-	creature = game.getCreature(creatureClass);
-	if (creature == null) {
-		player.sendPrivateText("No such creature");
-	} else {
-
-		// spawn the specified amout of them
-		if (args.length < 1) {
-			game.add(creature, x, y);
-		} else {
-			int k = Integer.parseInt(args[0]);
-			if (k < 3) {
-				for (dx in -k..k) {
-					for (dy in -k..k) {
-						if ((dx != 0) || (dy != 0)) {
-							game.add(creature, x + dx, y + dy + 1);
-						}
-					}
-				}
-			}
-		}
-	}
+	player.sendPrivateText("This script was renamed to /script Plague.class");
 }
