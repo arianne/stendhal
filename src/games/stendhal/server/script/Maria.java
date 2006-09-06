8a700627c333adc476c46ee0a2bc055d610356ed
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-09-06 22:29:39 +0000

    ported maria.groovy to Maria.java

---
+++ src/games/stendhal/server/script/Maria.java
@@ -0,0 +1,105 @@
+/* $Id$ */
+package games.stendhal.server.script;
+
+import games.stendhal.server.entity.Player;
+import games.stendhal.server.entity.item.Item;
+import games.stendhal.server.entity.npc.ShopList;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.scripting.ScriptImpl;
+import games.stendhal.server.scripting.ScriptingNPC;
+import games.stendhal.server.scripting.ScriptingSandbox;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Creates a portable NPC which sell foods&drinks at meetings.
+ * 
+ * As admin use /script maria.groovy to sommon her right next to you. Please put
+ * her back in int_admin_playground after use.
+ */
+public class Maria extends ScriptImpl {
+	private static Logger logger = Logger.getLogger(Maria.class);
+
+	private class MariaCouponAction extends SpeakerNPC.ChatAction {
+		private ScriptingSandbox sandbox = null;
+
+		public MariaCouponAction(ScriptingSandbox sandbox) {
+			this.sandbox = sandbox;
+		}
+
+		public void fire(Player player, String text, SpeakerNPC engine) {
+			if (player.getQuest("MariaCoupon") == null) {
+				Item item = sandbox.getItem("coupon");
+				item.setDescription("Coupon for one free beer at Semos' tavern.");
+				player.equip(item, true);
+				engine.say("Give this coupon to Margaret in Semos' Tavern to get a free beer.");
+				player.setQuest("MariaCoupon", "coupon");
+			} else {
+				engine.say("Sorry, only one free beer per person.");
+			}
+		}
+	}
+
+	class MargaretCouponAction extends SpeakerNPC.ChatAction {
+		private ScriptingSandbox sandbox = null;
+
+		public MargaretCouponAction(ScriptingSandbox sandbox) {
+			this.sandbox = sandbox;
+		}
+
+		public void fire(Player player, String text, SpeakerNPC engine) {
+			if (player.drop("coupon")) {
+				Item beer = sandbox.getItem("beer");
+				player.equip(beer, true);
+				engine.say("Here is your free beer.");
+				player.setQuest("MariaCoupon", "done");
+			} else {
+				engine.say("Sorry, you don't have a coupon. You can get one from Maria.");
+			}
+		}
+	}
+
+	@Override
+	public void load(Player admin, List<String> args, ScriptingSandbox sandbox) {
+
+		// Create NPC
+		ScriptingNPC npc = new ScriptingNPC("Maria");
+		npc.setClass("tavernbarmaidnpc");
+
+		// Place NPC in int_admin_playground on server start
+		String myZone = "int_admin_playground";
+		sandbox.setZone(myZone);
+		int x = 11;
+		int y = 4;
+
+		// if this script is executed by an admin, Maria will be placed next to
+		// him.
+		if (admin != null) {
+			sandbox.setZone(sandbox.getZone(admin));
+			x = admin.getX() + 1;
+			y = admin.getY();
+		}
+
+		// Set zone and position
+		sandbox.setZone(myZone);
+		npc.set(x, y);
+		sandbox.add(npc);
+
+		// Create Dialog
+		npc.behave("greet", "Hi, how can i help you?");
+		npc.behave("job", "I am one of the bar maids at Semos' #tavern and doing outside services. We sell fine beers and food.");
+		npc.behave("tavern", /* "I have a #coupon for a free beer in Semos' tavern. "+ */ "It is on the left side of the temple.");
+		npc.behave("help", "You can get an #offer of drinks and take a break to meet new people!");
+		try {
+			npc.behave("sell", ShopList.get().get("food&drinks"));
+		} catch (NoSuchMethodException e) {
+			logger.error(e, e);
+		}
+
+		// TODO Modify Margaret
+		// game.getNPC("Margaret");
+	}
+
+}
