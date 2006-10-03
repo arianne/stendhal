ed62f2b9fe997cba9f0b2e10a1c6b3f7c141fc19
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-10-03 16:55:06 +0000

    changed code to compile on Java 1.2

--- src/games/stendhal/client/Starter.java
+++ src/games/stendhal/client/Starter.java
@@ -24,12 +24,28 @@ public class Starter {
 			if (Float.parseFloat(version) < 1.5f) {
 				JOptionPane.showMessageDialog(null, "You need at least Java 1.5.0 (also known as 5.0) but you only have " + version + ". You can download it at http://java.sun.com");
 			}
+		} catch (RuntimeException e) {
+			// ignore
+		}
 
+		try {
 			// invoke real client with reflection in order to prevent
 			// a class-load-time dependency.
+
+			// get class
 			Class clazz = Class.forName("games.stendhal.client.stendhal");
-			Method method = clazz.getMethod("main", String[].class);
-			method.invoke(null, (Object) args);
+
+			// get param types of main method
+			Class[] paramTypes = new Class[1];
+			paramTypes[0] = args.getClass();
+
+			// get param values of main method
+			Object[] params = new Object[1];
+			params[0] = args;
+
+			// get method and invoke it
+			Method method = clazz.getMethod("main", paramTypes);
+			method.invoke(null, params);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
