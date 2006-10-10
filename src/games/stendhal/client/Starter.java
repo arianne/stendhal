8f5913452d1fbd4badc6b103dc230a8fdb0c314a
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-10-10 17:18:08 +0000

    added (inactive) code to dynamicly add .jar-files to the classpath

--- src/games/stendhal/client/Starter.java
+++ src/games/stendhal/client/Starter.java
@@ -16,7 +16,17 @@
 //*****************************************************************************
 package games.stendhal.client;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Properties;
+import java.util.StringTokenizer;
 
 import javax.swing.JOptionPane;
 
@@ -50,6 +60,7 @@ public class Starter {
 
 			// get class
 			Class clazz = Class.forName("games.stendhal.client.stendhal");
+//			Class clazz = getMainClass("games.stendhal.client.stendhal");
 
 			// get param types of main method
 			Class[] paramTypes = new Class[1];
@@ -67,4 +78,49 @@ public class Starter {
 		}
 	}
 
-}
+	private static Class getMainClass(String className) throws Exception {
+		Class clazz = null;
+		try {
+			// discover folder for .jar-files
+			String jarFolder = System.getProperty("user.home") + "/stendhal/jar/";
+
+			// load jar.properties
+			String propFile = jarFolder + "jar.properties";
+			Properties prop = new Properties();
+			InputStream is = new FileInputStream(propFile);
+			prop.load(is);
+			is.close();
+
+			// get list of .jar-files
+			String jarNameString = prop.getProperty("load", "jar.properties does not contain \"load=\" line");
+			List jarNameList = new LinkedList();
+			StringTokenizer st = new StringTokenizer(jarNameString, ",");
+			while (st.hasMoreTokens()) {
+				jarNameList.add(st.nextToken());
+			}
+			// convert .jar-filename-list to URL-array
+		    URL[] jarFiles = new URL[jarNameList.size()];
+		    Iterator itr = jarNameList.iterator();
+		    int i = 0;
+		    while (itr.hasNext()) {
+		    	jarFiles[i] = new File(jarFolder + itr.next()).toURI().toURL();
+		    	i++;
+		    }
+		    // Create new class loader which the list of .jar-files as classpath 
+		    ClassLoader loader = new URLClassLoader(jarFiles);
+
+		    // load class through new loader
+		    clazz = loader.loadClass(className);
+            		
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Something nasty happend while trying to build classpath.\r\nPlease open a bug report at http://sf.net/projects/arianne with this error message:\r\n" + e);
+		}
+
+		if (clazz == null) {
+			// fallback to normal classloading
+			clazz = Class.forName(className);
+		}
+		return clazz;
+	}
+ }
+
