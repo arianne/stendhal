d78706fd7b6220b00441dd3d28ef21357774fecb
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-10-10 21:43:01 +0000

    added boot-method that will invoke the main method of the specified class in the extended classpath (last part is deactived for now)

--- src/games/stendhal/client/update/Bootstrap.java
+++ src/games/stendhal/client/update/Bootstrap.java
@@ -1,11 +1,9 @@
-/**
- * 
- */
 package games.stendhal.client.update;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Iterator;
@@ -17,16 +15,18 @@ import java.util.StringTokenizer;
 import javax.swing.JOptionPane;
 
 /**
- * @author hendrik
+ * Starts a program after doing some classpath magic.
  *
+ * @author hendrik
  */
 public class Bootstrap {
+
 	private static Class getMainClass(String className) throws Exception {
 		Class clazz = null;
 		try {
 			// discover folder for .jar-files
 			String pathSep = System.getProperty("file.separator");
-			String jarFolder = System.getProperty("user.home") + pathSep+ "stendhal" + pathSep + "jar" + pathSep;
+			String jarFolder = System.getProperty("user.home") + pathSep + "stendhal" + pathSep + "jar" + pathSep;
 
 			// load jar.properties
 			String propFile = jarFolder + "jar.properties";
@@ -65,5 +65,37 @@ public class Bootstrap {
 			clazz = Class.forName(className);
 		}
 		return clazz;
+	}
+
+	/**
+	 * Starts the main-method of specified class after
+	 * dynamically building the classpath
+	 *
+	 * @param className name of class with "main"-method
+	 * @param args command line arguments
+	 */
+	public void boot(String className, String[] args) {
+		try {
+
+			// build classpath
+			// switch comment-markers on the next to lines, to use new code
+			// Class clazz = getMainClass(className);
+			Class clazz = Class.forName(className);
+
+			// get param types of main method
+			Class[] paramTypes = new Class[1];
+			paramTypes[0] = args.getClass();
+
+			// get param values of main method
+			Object[] params = new Object[1];
+			params[0] = args;
+
+			// get method and invoke it
+			Method method = clazz.getMethod("main", paramTypes);
+			method.invoke(null, params);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
