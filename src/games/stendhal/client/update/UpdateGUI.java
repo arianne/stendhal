1b19bcd0da8f2d34c6aa081e5a143a9cab8af88e
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-10-11 19:15:44 +0000

    added messageBox method

--- src/games/stendhal/client/update/UpdateGUI.java
+++ src/games/stendhal/client/update/UpdateGUI.java
@@ -8,6 +8,7 @@ import javax.swing.JOptionPane;
  * @author hendrik
  */
 public class UpdateGUI {
+	private static final String DIALOG_TITLE = "Stendhal Update";
 
 	/**
 	 * Asks the user to accept an update.
@@ -17,11 +18,21 @@ public class UpdateGUI {
 	public static boolean askForUpdate() {
 		int resCode = JOptionPane.showConfirmDialog(null, 
 				"There is a new version. Should Stendhal be updated?",
-				"Stendhal Update",
+				DIALOG_TITLE,
 				JOptionPane.YES_NO_OPTION,
 				JOptionPane.QUESTION_MESSAGE);
 		
 		return (resCode == JOptionPane.YES_OPTION);
 	}
-	
+
+
+	/**
+	 * Displays a message box
+	 *
+	 * @param message message to display
+	 */
+	public static void messageBox(String message) {
+		JOptionPane.showMessageDialog(null, message, 
+						DIALOG_TITLE, JOptionPane.INFORMATION_MESSAGE);
+	}
 }
