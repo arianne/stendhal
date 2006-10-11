7d2eff26b9dac6b9664086d95bed1fb54798ca74
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-10-11 06:38:59 +0000

    some debug and test code

--- src/games/stendhal/client/update/UpdateManager.java
+++ src/games/stendhal/client/update/UpdateManager.java
@@ -1,6 +1,10 @@
 package games.stendhal.client.update;
 
+import games.stendhal.common.Version;
+
 import java.util.Properties;
+
+import marauroa.common.Log4J;
 
 import org.apache.log4j.Logger;
 
@@ -11,7 +15,7 @@ import org.apache.log4j.Logger;
  */
 public class UpdateManager {
 	// TODO: fix URL after testing is completed
-	private static final String SERVER_FOLDER = "http://localhost/stendhal/";
+	private static final String SERVER_FOLDER = "http://localhost/stendhal/updates/";
 	private static Logger logger = Logger.getLogger(UpdateManager.class);
 	private Properties fileList = null;
 
@@ -19,9 +23,26 @@ public class UpdateManager {
 	 * Connects to the server and loads a Property object which contains
 	 * information about the files available for update.
 	 */
-	public void init() {
-		HttpClient httpClient = new HttpClient(SERVER_FOLDER + "file-list.properties");
+	private void init() {
+		HttpClient httpClient = new HttpClient(SERVER_FOLDER + "update.properties");
 		fileList = httpClient.fetchProperties();
 	}
 
+	public void process() {
+		init();
+		if (fileList == null) {
+			return;
+		}
+		
+		fileList.list(System.out);
+		logger.info(Version.VERSION);
+		logger.info(fileList.getProperty("version." + Version.VERSION, "unkown"));
+	}
+
+	// debug code
+	public static void main(String args[]) {
+		Log4J.init("data/conf/log4j.properties");
+		UpdateManager um = new UpdateManager();
+		um.process();
+	}
 }
