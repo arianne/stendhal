57ba45254a1d1a2f2ad51648d13e96b5d026fcf0
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-10-20 06:02:59 +0000

    teleport player out of this room on login

--- src/games/stendhal/server/maps/quests/ReverseArrow.java
+++ src/games/stendhal/server/maps/quests/ReverseArrow.java
@@ -2,8 +2,12 @@ package games.stendhal.server.maps.quests;
 
 import games.stendhal.common.Grammar;
 import games.stendhal.common.MathHelper;
+import games.stendhal.server.StendhalRPWorld;
 import games.stendhal.server.entity.Player;
 import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.portal.OnePlayerRoomDoor;
+import games.stendhal.server.entity.portal.Portal;
+import marauroa.common.game.IRPZone;
 
 /**
  * A quest where the player has to invert an arrow build out of stones
@@ -16,20 +20,29 @@ public class ReverseArrow extends AbstractQuest {
 	private static final String QUEST_SLOT = "reverse_arrow";
 	private static final int MAX_MOVES = 3;
 	private SpeakerNPC npc = null;
+	private Portal exit = null;
+	private OnePlayerRoomDoor door = null;
+	
 
 	@Override
 	public void init(String name) {
 		super.init(name, QUEST_SLOT);
 	}
 	
-	public void setNPC(SpeakerNPC npc) {
-		this.npc = npc;
-	}
-
 	private void step_1() {
 		// TODO: create NPC
 		// TODO: create zone with tokens
 		// TODO: create door
+	}
+
+	@Override
+	public void convertOnUpdate(Player player) {
+		super.convertOnUpdate(player);
+		IRPZone playerZone = StendhalRPWorld.get().getRPZone(player.getID());
+		IRPZone onePlayerRoomZone = StendhalRPWorld.get().getRPZone(door.getDestinationZone());
+		if (playerZone.equals(onePlayerRoomZone)) {
+			exit.onUsed(player);
+		}
 	}
 
 	public void onTokenMoved(Player player) {
@@ -40,7 +53,7 @@ public class ReverseArrow extends AbstractQuest {
 			// TODO: check token position
 			// TODO: teleport player out
 			if (true) {
-				player.setQuest(QUEST_SLOT, "true");
+				player.setQuest(QUEST_SLOT, "done");
 			} else {
 				player.setQuest(QUEST_SLOT, "failed");
 			}
