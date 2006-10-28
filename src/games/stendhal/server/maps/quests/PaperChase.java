bff73e35e8cd73a1d5d68989f6dace5466db5ad9
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-10-28 12:16:35 +0000

    initial checkin

---
+++ src/games/stendhal/server/maps/quests/PaperChase.java
@@ -0,0 +1,90 @@
+package games.stendhal.server.maps.quests;
+
+import games.stendhal.server.entity.Player;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.NPCList;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+
+/**
+ * A kind of paper chase
+ *
+ * @author hendrik
+ */
+public class PaperChase extends AbstractQuest {
+	private static final String QUEST_SLOT = "paper_chase";
+	private NPCList npcs = NPCList.get();
+	
+	private String[] points = new String[] {
+		"Carmen",
+		"Monogenes",
+		"Hayunn Naratha",
+		//TODO: load groovy before quest are inited (or convert kanmararn.groovy to java) "Henry", // TODO: ignore if groovy is inactive
+		"Margaret",
+		"Balduin",
+		//TODO: load groovy before quest are inited (or convert deathmatch_entry.groovy to java) "Deathmatch Recruiter", // TODO: ignore if groovy is inactive
+		"Katinka",
+		"Haizen",
+		"Bario",
+		"Ceryl",
+		"Nishiya",
+		"Marcus",
+		"Jynath",
+		"Loretta",
+		"Fidorea"
+	};
+	
+	private class PaperChasePoint extends SpeakerNPC.ChatAction {
+		int idx;
+		PaperChasePoint(int idx) {
+			this.idx = idx;
+		}
+
+		@Override
+		public void fire(Player player, String text, SpeakerNPC engine) {
+			String state = points[idx];
+			String next = points[idx + 1];
+			// TODO: analyse quest state
+			// TODO: 
+		}
+		
+	}
+
+	@Override
+	public void init(String name) {
+		super.init(name, QUEST_SLOT);
+	}
+
+	/**
+	 * Adds the task to the specified NPC.
+	 * Note that the start and end of this quest have to be coded specially.
+	 *
+	 * @param idx index of way point
+	 */
+	private void addTaskToNPC(int idx) {
+		String state = points[idx];
+		SpeakerNPC npc = npcs.get(state);
+		npc.add(ConversationStates.ATTENDING, 
+					"paper",
+					null,
+					ConversationStates.ATTENDING,
+					null,
+					new PaperChasePoint(idx));
+	}
+
+	@Override
+	public void addToWorld() {
+		super.addToWorld();
+
+		// TODO: add Fidorea to world introducing the quest
+		// TODO: detect using of scrolls
+
+		// add normal way points (without first and last) 
+		for (int i = 0; i < points.length - 1; i++) {
+			addTaskToNPC(i);
+		}
+
+		// TODO: Fidorea doing the post processing of this quest (calc points based on time and level)
+		// TODO: store and read result (with server restart in mind)
+		// TODO: create sign as Hall of Fame
+}
+}
