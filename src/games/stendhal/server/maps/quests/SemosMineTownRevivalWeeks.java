57810dcd1d1ede4121737d8bb2ad51a5caa5aec9
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-10-29 12:41:13 +0000

    befriend Susi

--- src/games/stendhal/server/maps/quests/SemosMineTownRevivalWeeks.java
+++ src/games/stendhal/server/maps/quests/SemosMineTownRevivalWeeks.java
@@ -7,6 +7,7 @@ import games.stendhal.server.entity.Player;
 import games.stendhal.server.entity.Sign;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.StandardInteraction;
 import games.stendhal.server.pathfinder.Path;
 
 import java.util.Arrays;
@@ -57,10 +58,11 @@ public class SemosMineTownRevivalWeeks extends AbstractQuest {
 			@Override
 			protected void createDialog() {
 				addGreeting("I like the #Semos #Mine #Town #Revival #Weeks. Its like a huge party.");
-				addHelp("Just talk to Fidorea to get a nice costume.");
 				addJob("I am just a litte girl having lots of fun here during the #Semos #Mine #Town #Revival #Weeks-");
 				addGoodbye("Have fun!");
 				add(ConversationStates.ATTENDING, "debuggera", null, ConversationStates.ATTENDING, "She is my crazy twin sister.", null);
+				
+				// Revival Weeks
 				add(ConversationStates.ATTENDING, Arrays.asList("Semos", "Mine", "Town", "Revival", "Weeks"),
 					ConversationStates.ATTENDING, "During the Revival Weeks we #celebrate the old and now mostly dead Semos Mine Town. Lots of people from Ados came for a visit.", null);
 				add(ConversationStates.ATTENDING, Arrays.asList("celebrate", "celebration", "party"),
@@ -72,13 +74,31 @@ public class SemosMineTownRevivalWeeks extends AbstractQuest {
 					},
 					ConversationStates.ATTENDING, "You can get a costume from Fidorea over there or you can try to solve a difficult puzzle in one of the houses.", null);
 				add(ConversationStates.ATTENDING, Arrays.asList("celebrate", "celebration", "party"),
-								new SpeakerNPC.ChatCondition() {
-									@Override
-									public boolean fire(Player player, SpeakerNPC engine) {
-										return player.has("outfit_org");
-									}
-								},
-								ConversationStates.ATTENDING, "I see, you already got a costume from Fidorea. But have you tried your luck in a difficult puzzle in one of the houses?", null);
+					new SpeakerNPC.ChatCondition() {
+						@Override
+						public boolean fire(Player player, SpeakerNPC engine) {
+							return player.has("outfit_org");
+						}
+					},
+					ConversationStates.ATTENDING, "I see, you already got a costume from Fidorea. But have you tried your luck in a difficult puzzle in one of the houses?", null);
+
+			    // friends
+			    add(ConversationStates.ATTENDING, Arrays.asList("friend", "friends"), new StandardInteraction.QuestInStateCondition("susi", "friends"), ConversationStates.ATTENDING, "We are friends.", null);
+			    add(ConversationStates.ATTENDING, Arrays.asList("friend", "friends"), new StandardInteraction.QuestNotInStateCondition("susi", "friends"), ConversationStates.INFORMATION_1, "Please repeat:\r\n                        \"A circle is round,\"", null);
+			    add(ConversationStates.INFORMATION_1, Arrays.asList("A circle is round,", "A circle is round"), null, ConversationStates.INFORMATION_2, "\"it has no end.\"", null);
+			    add(ConversationStates.INFORMATION_2, Arrays.asList("it has no end.", "it has no end"), null, ConversationStates.INFORMATION_3, "\"That's how long,\"", null);
+			    add(ConversationStates.INFORMATION_3, Arrays.asList("That's how long,", "That's how long", "Thats how long,", "Thats how long"), null, ConversationStates.INFORMATION_4, "\"I will be your friend.\"", null);
+			    add(ConversationStates.INFORMATION_4, Arrays.asList("I will be your friend.", "I will be your friend"), null, ConversationStates.ATTENDING, "Cool. We are friends now.", new StandardInteraction.SetQuestAction("susi", "friends"));
+
+			    // help
+				add(ConversationStates.ATTENDING, SpeakerNPC.HELP_MESSAGES, 
+					new StandardInteraction.QuestInStateCondition("susi", "friends"), 
+					ConversationStates.ATTENDING, 
+					"I have made a lot of friends during the #Semos #Mine #Town #Revival #Weeks.", null);
+				add(ConversationStates.ATTENDING, SpeakerNPC.HELP_MESSAGES, 
+								new StandardInteraction.QuestNotInStateCondition("susi", "friends"), 
+								ConversationStates.ATTENDING, 
+								"I need a #friend.", null);
 			}
 		};
 		npcs.add(npc);
