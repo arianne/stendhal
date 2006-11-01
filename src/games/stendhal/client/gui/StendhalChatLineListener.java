fe8112149f99fcd6dadcefcaacce551659240980
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-11-01 18:42:56 +0000

    documented ghostmode

--- src/games/stendhal/client/gui/StendhalChatLineListener.java
+++ src/games/stendhal/client/gui/StendhalChatLineListener.java
@@ -423,6 +423,7 @@ public class StendhalChatLineListener implements ActionListener, KeyListener {
 						"- /script <scriptname> \t\tLoad (or reload) a 'groovy' script on the server",
 						"- /teleport <player> <zone> <x> <y> \tTeleport the specified <player> to the given location",
 						"- /teleportto <player> \t\tTeleport yourself near the specified player",
+						"- /ghostmode \t\t\t Makes yourself invisible and intangible",
 						"- /alter <player> <attrib> <mode> <value> \tAlter stat <attrib> of <player> by the given amount; <mode> can be ADD, SUB, or SET. See /gmhelp_alter for details",
 						"- /summon <creature|item> <x> <y> \tSummon the specified item or creature at co-ordinates <x>, <y> in the current zone",
 						"- /summonat <player> <slot> <item> <amount> Summon the specified item into the specified slot of <player>; <amount> defaults to 1 if not specified",
