d9d40e592890634a194d7cc5cedc127c9415a7e9
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-11-13 22:58:24 +0000

    readded missing greating for Dagobert

--- src/games/stendhal/server/maps/semos/SemosCityInside.java
+++ src/games/stendhal/server/maps/semos/SemosCityInside.java
@@ -135,6 +135,7 @@ public class SemosCityInside {
 
 			@Override
 			protected void createDialog() {
+				addGreeting("Welcome to the bank of Semos! Do you need #help on your personal chest?");
 				addHelp("Follow the corridor to the right, and you will find the magic chests. You can store your belongings in any of them, and nobody else will be able to touch them!");
 				addJob("I'm the Customer Advisor here at Semos Bank.");
 				addGoodbye("It was a pleasure to serve you.");
