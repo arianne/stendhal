aab4cd81a9f05c7d9f797ff175038e6364a38bec
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-11-25 14:05:58 +0000

    added sleep* methods

--- src/games/stendhal/client/scripting/ClientScriptInterface.java
+++ src/games/stendhal/client/scripting/ClientScriptInterface.java
@@ -1,5 +1,7 @@
 package games.stendhal.client.scripting;
 
+import org.apache.log4j.Logger;
+
 
 /**
  * Interface used by client side scripts to interact with the game
@@ -7,6 +9,7 @@ package games.stendhal.client.scripting;
  * @author hendrik
  */
 public class ClientScriptInterface {
+	private static Logger logger = Logger.getLogger(ClientScriptInterface.class);
 
 	/**
 	 * handles a string command in the same way the chat line does
@@ -15,5 +18,27 @@ public class ClientScriptInterface {
 	 */
 	public void invoke(String input) {
 		ChatLineParser.get().parseAndHandle(input);
+		sleepMillis(300);
+	}
+	
+	/**
+	 * waits the specified number of milliseconds.
+	 *
+	 * @param millis amount of milliseconds to wait
+	 */
+	public void sleepMillis(long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (InterruptedException e) {
+			logger.error(e, e);
+		}
+	}
+
+	public void sleepSeconds(long seconds) {
+		sleepMillis(seconds * 1000);
+	}
+
+	public void sleepTurns(long turns) {
+		sleepMillis(turns * 300);
 	}
 }
