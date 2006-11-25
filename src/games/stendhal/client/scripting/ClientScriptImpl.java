6af85c23d2d11303307c64f9804ef07437d73b98
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-11-25 19:38:20 +0000

    initial checkin

---
+++ src/games/stendhal/client/scripting/ClientScriptImpl.java
@@ -0,0 +1,9 @@
+package games.stendhal.client.scripting;
+
+/**
+ * default implemantation for ClientScript
+ */
+public abstract class ClientScriptImpl implements ClientScript {
+
+	// no methods yet
+}
