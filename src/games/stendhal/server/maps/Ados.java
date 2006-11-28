7290cc453241743f5fb3d5e4f100edd57b7d7ad9
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-11-28 21:28:53 +0000

    added new classes AdosCityOutside und AdosCityInside

--- src/games/stendhal/server/maps/Ados.java
+++ src/games/stendhal/server/maps/Ados.java
@@ -1,5 +1,7 @@
 package games.stendhal.server.maps;
 
+import games.stendhal.server.maps.ados.AdosCityInside;
+import games.stendhal.server.maps.ados.AdosCityOutside;
 import games.stendhal.server.maps.ados.AdosMountainsNorthWest;
 import games.stendhal.server.maps.ados.AdosOutsideNorthWest;
 import games.stendhal.server.maps.ados.AdosRock;
@@ -9,6 +11,8 @@ import games.stendhal.server.maps.ados.Deathmatch;
 public class Ados implements IContent {
 
 	public Ados() {
+		new AdosCityOutside().build();
+		new AdosCityInside().build();
 		new AdosMountainsNorthWest().build();
 		new AdosRock().build();
 		new AdosOutsideNorthWest().build();
