e781a6d1cbed4a241e98b6840767d6021132c6d4
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-12-17 23:54:15 +0000

    removed the NPC creation because it is not in AdosCityOutside and MakeupArtistBehaviour

--- src/games/stendhal/server/maps/quests/CostumeParty.java
+++ src/games/stendhal/server/maps/quests/CostumeParty.java
@@ -1,22 +1,11 @@
 package games.stendhal.server.maps.quests;
 
-import games.stendhal.common.Direction;
-import games.stendhal.common.Rand;
 import games.stendhal.server.StendhalRPRuleProcessor;
-import games.stendhal.server.StendhalRPWorld;
-import games.stendhal.server.StendhalRPZone;
 import games.stendhal.server.entity.Player;
-import games.stendhal.server.entity.npc.ConversationStates;
-import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.events.TurnListener;
 import games.stendhal.server.events.TurnNotifier;
-import games.stendhal.server.pathfinder.Path;
 
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-
-import marauroa.common.game.IRPZone;
 
 /**
  * Puts the player into a funny constume
@@ -53,54 +42,8 @@ public class CostumeParty extends AbstractQuest {
 		super.init(name, QUEST_SLOT);
 	}
 	
-	private void createNPC() {
-		StendhalRPZone zone = (StendhalRPZone) StendhalRPWorld.get().getRPZone(new IRPZone.ID("0_semos_mountain_n2"));
-		SpeakerNPC npc = new SpeakerNPC("Fidorea") {
-			@Override
-			protected void createPath() {
-				// npc does not move
-				List<Path.Node> nodes = new LinkedList<Path.Node>();
-				setPath(nodes, false);
-			}
-
-			@Override
-			protected void createDialog() {
-				addGreeting(null, new SpeakerNPC.ChatAction() {
-					@Override
-					public void fire(Player player, String text,
-							SpeakerNPC engine) {
-						int outfit = player.getInt("outfit");
-						if (!player.has("outfit_org")) {
-							player.put("outfit_org", outfit);
-						}
-						// hair, head, outfit, body
-						int randomHead = Rand.rand(5);
-						int head = 80 + randomHead;
-						outfit = 00 * 1000000 + head * 10000 + (outfit % 10000);
-						player.put("outfit", outfit);
-						engine.say("I hope, you like your costume.");
-						player.setQuest(QUEST_SLOT, Long.toString(System.currentTimeMillis() + 30 * 60 * 1000));
-					}
-				});
-				addHelp("If you don't like your costume, you can remove it by clicking on yourself and choosing Set Outfit.");
-				addJob("I am a makeup artist living in Ados. But I come here once a year for the Semos Mine Town Revival Weeks.");
-				addQuest("Just have fun.");
-				add(ConversationStates.ATTENDING, Arrays.asList("offer"), ConversationStates.ATTENDING, "I will give you a costume free of charge.", null);
-				addGoodbye("Come back to me, if you want another costume.");
-			}
-		};
-		npcs.add(npc);
-		zone.assignRPObjectID(npc);
-		npc.put("class", "woman_008_npc");
-		npc.set(80, 108);
-		npc.setDirection(Direction.DOWN);
-		npc.initHP(100);
-		zone.addNPC(npc);
-	}
-
 	public void addToWorld() {
 		super.addToWorld();
-		createNPC();
 		TurnNotifier.get().notifyInTurns(65, new CostumeStripTimer(), null);
 	}
 }
