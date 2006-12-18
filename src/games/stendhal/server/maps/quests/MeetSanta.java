138e7d3aa72a81f102d5737e24fbd9e2e2e9ccca
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-12-18 23:12:23 +0000

    added the easy stuff

--- src/games/stendhal/server/maps/quests/MeetSanta.java
+++ src/games/stendhal/server/maps/quests/MeetSanta.java
@@ -1,9 +1,14 @@
 package games.stendhal.server.maps.quests;
 
-import games.stendhal.server.entity.npc.NPC;
-import games.stendhal.server.entity.npc.SellerBehaviour;
+import games.stendhal.server.StendhalRPWorld;
+import games.stendhal.server.StendhalRPZone;
+import games.stendhal.server.entity.Player;
+import games.stendhal.server.entity.item.Item;
+import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.StandardInteraction;
 import games.stendhal.server.events.TurnListener;
+import games.stendhal.server.events.TurnNotifier;
 import games.stendhal.server.pathfinder.Path;
 
 import java.util.LinkedList;
@@ -20,14 +25,15 @@ import java.util.List;
  * - Get reward
  *
  * REWARD:
- * - Any random good reward: food, money, potions, items, etc...
- * 
+ * - a present which can be opend to obtain a random good reward:
+ *   food, money, potions, items, etc...
+ *
  * REPETITIONS:
  * - None
  */
-
 public class MeetSanta extends AbstractQuest implements TurnListener {
 	private static final String QUEST_SLOT = "meet_santa_06";
+	private SpeakerNPC santa = null;
 
 	@Override
 	public void init(String name) {
@@ -35,19 +41,34 @@ public class MeetSanta extends AbstractQuest implements TurnListener {
 	}
 	
 	private SpeakerNPC createSanta() {
-		SpeakerNPC npc = npcs.get("Santa");
-		if(npc!=null) {
-			return npc;
-		}
-		
-		SpeakerNPC santa = new SpeakerNPC("Santa") {
+		santa = new SpeakerNPC("Santa") {
 			@Override
 			protected void createPath() {
+				// npc does not move
+				List<Path.Node> nodes = new LinkedList<Path.Node>();
+				setPath(nodes, false);
 			}
 
 			@Override
 			protected void createDialog() {
-				addGreeting();
+				add(ConversationStates.IDLE, SpeakerNPC.GREETING_MESSAGES,
+					new StandardInteraction.QuestCompletedCondition(QUEST_SLOT),
+					ConversationStates.ATTENDING, "Hi again.", null);
+
+				add(ConversationStates.IDLE, SpeakerNPC.GREETING_MESSAGES,
+					new StandardInteraction.QuestNotCompletedCondition(QUEST_SLOT),
+					ConversationStates.ATTENDING, null,
+					new SpeakerNPC.ChatAction() {
+
+						@Override
+						public void fire(Player player, String text, SpeakerNPC engine) {
+							Item item = StendhalRPWorld.get().getRuleManager().getEntityManager().getItem("present");
+							engine.say("Merry Christmas! I have a present for you.");
+							player.equip(item, true);
+							player.setQuest(QUEST_SLOT, "done");
+						}
+				});
+
 				addJob("I am Santa Claus! Where have you been in these years?");
 				addGoodbye();
 			}
@@ -56,15 +77,31 @@ public class MeetSanta extends AbstractQuest implements TurnListener {
 		santa.put("class", "santaclausnpc");
 		santa.set(17, 12);
 		santa.initHP(100);
-		
+
+		// start in int_admin_playground
+		StendhalRPZone zone = (StendhalRPZone) StendhalRPWorld.get().getRPZone("int_admin_playground");
+		zone.assignRPObjectID(santa);
+		zone.addNPC(santa);
+
 		return santa;
 	}
 
 	public void onTurnReached(int currentTurn, String message) {
-		/* Say bye */
-		/* Teleport to another random place */ 
-		/* Schedule so we are notified again in X turns */
+		// Say bye
+		santa.say("Bye.");
+
+		// Teleport to another random place
+
+
+		// Schedule so we are notified again in 5 minutes
+		TurnNotifier.get().notifyInTurns(5*60*3, this, null);
 	}
 
+	@Override
+	public void addToWorld() {
+		super.addToWorld();
+		createSanta();
+		TurnNotifier.get().notifyInTurns(6, this, null);
+	}
 
 }
