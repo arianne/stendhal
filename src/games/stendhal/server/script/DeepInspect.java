8695cd908bfd2822d37bf3e4978473f9f46ec213
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-12-20 20:26:05 +0000

    initial checkin

---
+++ src/games/stendhal/server/script/DeepInspect.java
@@ -0,0 +1,46 @@
+package games.stendhal.server.script;
+
+import games.stendhal.server.StendhalRPRuleProcessor;
+import games.stendhal.server.entity.Player;
+import games.stendhal.server.scripting.ScriptImpl;
+
+import java.util.List;
+
+import marauroa.common.game.RPObject;
+import marauroa.common.game.RPSlot;
+
+/**
+ * Deep inspects a player and all his/her items
+ *
+ * @author hendrik
+ */
+public class DeepInspect extends ScriptImpl {
+
+	@Override
+	public void execute(Player admin, List<String> args) {
+		super.execute(admin, args);
+		if (args.size() == 0) {
+			admin.sendPrivateText("Need player name as parameter.");
+			return;
+		}
+		Player player = StendhalRPRuleProcessor.get().getPlayer(args.get(0));
+		StringBuffer sb = new StringBuffer();
+		sb.append("Inspecting " + player.getName() + "\n");
+
+		// inspect slots
+		for (RPSlot slot : player.slots()) {
+			// don't return buddy-list for privacy reasons
+			if (slot.getName().equals("!buddy")) {
+				continue;
+			}
+			sb.append("\nSlot " + slot.getName() + ": \n");
+
+			// list objects
+			for (RPObject object : slot) {
+				sb.append("   " + object + "\n");
+			}
+		}
+		
+		player.sendPrivateText(sb.toString());
+	}
+}
