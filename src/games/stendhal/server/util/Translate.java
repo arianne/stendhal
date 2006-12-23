060eea2f0babca85291d04a5db63fc0e322d3eba
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-12-23 18:31:15 +0000

    added velocity support

--- src/games/stendhal/server/util/Translate.java
+++ src/games/stendhal/server/util/Translate.java
@@ -2,10 +2,13 @@ package games.stendhal.server.util;
 
 import games.stendhal.common.Grammar;
 
-import java.io.IOException;
+import java.io.StringWriter;
+import java.io.Writer;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
 
 /**
  * A simple translation framework
@@ -15,7 +18,7 @@ import org.apache.log4j.Logger;
 public class Translate {
 	private static Logger logger = Logger.getLogger(Translate.class);
 	private static Properties dictionary = new Properties();
-	private static Grammar grammar = null;
+	private static Grammar grammar = new Grammar();
 
 	/**
 	 * Loads the specified dictionary
@@ -23,11 +26,15 @@ public class Translate {
 	 * @param language the 2 letter language code
 	 */
 	public static void initLanguage(String language) {
+		Grammar tempGrammar = null;
 		try {
 			dictionary.load(Translate.class.getResourceAsStream("../properties/" + language + ".txt"));
-			grammar = (Grammar) Class.forName(dictionary.getProperty("_grammar.class")).newInstance();
+			tempGrammar = (Grammar) Class.forName(dictionary.getProperty("_grammar.class")).newInstance();
 		} catch (Exception e) {
 			logger.error(e, e);
+		}
+		if (tempGrammar != null) {
+			grammar = tempGrammar;
 		}
 	}
 
@@ -39,11 +46,31 @@ public class Translate {
 	 * @return translated text
 	 */
 	public static String _(String text, String... args) {
+
+		// use translated text, if one was specified
 		String translated = text;
 		if (dictionary.get(text) != null) {
 			translated = dictionary.getProperty(text);
 		}
-		// TODO: parse arguments
-		return translated;
+
+		// extract arguments
+		VelocityContext context = new VelocityContext();
+		context.put("grammar", grammar);
+		int i = 0;
+		for (String arg : args) {
+			i++;
+			context.put(Integer.toString(i), arg);
+		}
+
+		// parse arguments and invoke scripts
+		Writer writer = new StringWriter();
+		try {
+			Velocity.evaluate(context, writer, "errors", translated);
+		} catch (Exception e) {
+			logger.error(e, e);
+		}
+		
+		// return result
+		return writer.toString();
 	}
 }
