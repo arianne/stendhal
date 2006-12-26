0c999a8e9f3feeefefa7b9af15bf6c2a7d715709
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-12-26 22:31:31 +0000

    remove links

--- src/games/stendhal/server/maps/ados/AdosCityInside.java
+++ src/games/stendhal/server/maps/ados/AdosCityInside.java
@@ -69,6 +69,13 @@ public class AdosCityInside {
 			content = content.replaceAll("<!--[^>]*-->", "");
 			// remove templates (note reg exp is incorret)
 			content = content.replaceAll("\\{\\{[^\\}]*\\}\\}", "");
+			// remove complex links
+			content = content.replaceAll("\\[\\[[^\\]]*\\|", "");
+			// remove simple links
+			content = content.replaceAll("\\[\\[", "");
+			content = content.replaceAll("\\]\\]", "");
+			
+			// ignore leading empty lines and spaces
 			content = content.trim();
 
 			// extract the first paragraph (ignoring very short once but oposing a max len)
