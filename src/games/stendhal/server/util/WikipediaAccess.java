08a3bcfe31df79fc6a49390d3220c9a8d6d4ba35
Hendrik Brummermann <nhnb@users.sourceforge.net>
2006-12-27 12:37:51 +0000

    implemented method parse

--- src/games/stendhal/server/util/WikipediaAccess.java
+++ src/games/stendhal/server/util/WikipediaAccess.java
@@ -1,5 +1,10 @@
 package games.stendhal.server.util;
 
+import games.stendhal.client.update.HttpClient;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
@@ -16,6 +21,7 @@ public class WikipediaAccess extends DefaultHandler {
 	private boolean isContent = false;
 	/** was the parsing completed */
 	private boolean finished = false;
+	private String error = null;
 
 	@Override
 	public void startElement(String namespaceURI, String lName, String qName, Attributes attrs) {
@@ -29,34 +35,55 @@ public class WikipediaAccess extends DefaultHandler {
 		}
 	}
 
+	/**
+	 * returns the unparsed text
+	 *
+	 * @return content
+	 */
 	public String getText() {
 		return text.toString();
 	}
-	
+
+	/**
+	 * Gets the last error message
+	 *
+	 * @return error message or <code>null</code> in case no error occured
+	 */
+	public String getError() {
+		return error;
+	}
+
+	/**
+	 * Returns the first paragraph of the specified article without wiki code
+	 *
+	 * @return content
+	 */
 	public String getProcessedText() {
 		String content = getText();
-		// remove image links
-		content = content.replaceAll("\\[\\[[iI]mage:[^\\]]*\\]\\]", "");
-		// remove comments (note reg exp is incorret)
-		content = content.replaceAll("<!--[^>]*-->", "");
-		// remove templates (note reg exp is incorret)
-		content = content.replaceAll("\\{\\{[^\\}]*\\}\\}", "");
-		// remove complex links
-		content = content.replaceAll("\\[\\[[^\\]]*\\|", "");
-		// remove simple links
-		content = content.replaceAll("\\[\\[", "");
-		content = content.replaceAll("\\]\\]", "");
-		
-		// ignore leading empty lines and spaces
-		content = content.trim();
-
-		// extract the first paragraph (ignoring very short once but oposing a max len)
-		int size = content.length();
-		int endOfFirstParagraph = content.indexOf("\n", 50);
-		if (endOfFirstParagraph < 0) {
-			endOfFirstParagraph = size;
+		if (content != null) {
+			// remove image links
+			content = content.replaceAll("\\[\\[[iI]mage:[^\\]]*\\]\\]", "");
+			// remove comments (note reg exp is incorret)
+			content = content.replaceAll("<!--[^>]*-->", "");
+			// remove templates (note reg exp is incorret)
+			content = content.replaceAll("\\{\\{[^\\}]*\\}\\}", "");
+			// remove complex links
+			content = content.replaceAll("\\[\\[[^\\]]*\\|", "");
+			// remove simple links
+			content = content.replaceAll("\\[\\[", "");
+			content = content.replaceAll("\\]\\]", "");
+			
+			// ignore leading empty lines and spaces
+			content = content.trim();
+	
+			// extract the first paragraph (ignoring very short once but oposing a max len)
+			int size = content.length();
+			int endOfFirstParagraph = content.indexOf("\n", 50);
+			if (endOfFirstParagraph < 0) {
+				endOfFirstParagraph = size;
+			}
+			content = content.substring(0, Math.min(endOfFirstParagraph, 1024));
 		}
-		content = content.substring(0, Math.min(endOfFirstParagraph, 1024));
 		return content;
 	}
 
@@ -64,9 +91,25 @@ public class WikipediaAccess extends DefaultHandler {
 	 * starts the parsing of the specified article
 	 *
 	 * @param title
+	 * @throws Exception in case of an unexpected error
 	 */
-	public void startParsing(String title) {
-		finished = true;
+	public void parse(String title) throws Exception {
+		try {
+			// look it up using the Wikipedia API
+			HttpClient httpClient = new HttpClient("http://en.wikipedia.org/w/query.php?format=xml&titles=" + title + "&what=content");
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+	
+			// Parse the input
+			SAXParser saxParser = factory.newSAXParser();
+			saxParser.parse(httpClient.getInputStream(), this);
+	
+			// finished
+			finished = true;
+		} catch (Exception e) {
+			finished = true;
+			error = e.toString();
+			throw e;
+		}
 	}
 
 	/**
