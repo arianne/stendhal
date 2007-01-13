0fb4664232364cf9c7a0de153d6116ebb34d5028
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-01-13 18:19:40 +0000

    fixed compiler error and code format

--- src/games/stendhal/server/maps/Orril.java
+++ src/games/stendhal/server/maps/Orril.java
@@ -9,6 +9,7 @@ import games.stendhal.server.entity.NPCOwnedChest;
 import games.stendhal.server.entity.creature.Creature;
 import games.stendhal.server.entity.creature.ItemGuardCreature;
 import games.stendhal.server.entity.npc.BuyerBehaviour;
+import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.NPCList;
 import games.stendhal.server.entity.npc.ShopList;
 import games.stendhal.server.entity.npc.SpeakerNPC;
@@ -18,6 +19,7 @@ import games.stendhal.server.pathfinder.Path;
 import games.stendhal.server.rule.defaultruleset.DefaultEntityManager;
 import games.stendhal.server.rule.defaultruleset.DefaultItem;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -271,37 +273,35 @@ public class Orril implements IContent {
 	}
 
 	private void buildDwarfSmithArea(StendhalRPZone zone) {
-	SpeakerNPC hogart=npcs.add("Hogart",new SpeakerNPC()
-      		{
-      		protected void createPath()
-        		{
-        		List<Path.Node> nodes=new LinkedList<Path.Node>();
-        		nodes.add(new Path.Node(12,6));
-        		nodes.add(new Path.Node(12,10));
-			nodes.add(new Path.Node(20,10));
-			nodes.add(new Path.Node(20,8));
-			nodes.add(new Path.Node(20,11));
-			nodes.add(new Path.Node(12,11));
-			nodes.add(new Path.Node(12,6));
-        		setPath(nodes,true);
-        		}
+		SpeakerNPC hogart = new SpeakerNPC("Hogart") {
+			protected void createPath() {
+				List<Path.Node> nodes=new LinkedList<Path.Node>();
+				nodes.add(new Path.Node(12,6));
+				nodes.add(new Path.Node(12,10));
+				nodes.add(new Path.Node(20,10));
+				nodes.add(new Path.Node(20,8));
+				nodes.add(new Path.Node(20,11));
+				nodes.add(new Path.Node(12,11));
+				nodes.add(new Path.Node(12,6));
+				setPath(nodes,true);
+			}
 
-      		protected void createDialog()
-        		{
-			//addGreeting();
-        		addJob("I am a master blacksmith. I used to forge weapons in secret for the dwarves in the mine, but they have forgotten me and my #stories.");
-			addHelp("I could tell you a #story...");	
-			add(ConversationStates.ATTENDING, Arrays.asList("story", "stories"), ConversationStates.ATTENDING, "I expect a scruff like you has never heard of Lady Tembells, huh? She was so beautiful. She died young and her distraught husband asked a powerful Lord to bring her back to life. The fool didn't get what he bargained for, she became a #vampire.", null);
-        		add(ConversationStates.ATTENDING, Arrays.asList("vampire"), ConversationStates.ATTENDING, "The husband had hired the help of a Vampire Lord! The Lady became his Vampire Bride and her maids became vampiresses. The Catacombs of North Semos are a deadly place now.", null);
-			addGoodbye("So long. I bet you won't sleep so well tonight.");
-        		} //remaining behaviour defined in maps.quests.VampireSword
-      		};
-    		hogart.setDescription("You see Hogart, a retired master dwarf smith."); 
-    		zone.assignRPObjectID(hogart);
-    		hogart.put("class","olddwarfnpc");
-    		hogart.set(12,6); 
-    		hogart.initHP(100);
-    		zone.addNPC(hogart);    
+			protected void createDialog() {
+				//addGreeting();
+				addJob("I am a master blacksmith. I used to forge weapons in secret for the dwarves in the mine, but they have forgotten me and my #stories.");
+				addHelp("I could tell you a #story...");	
+				add(ConversationStates.ATTENDING, Arrays.asList("story", "stories"), ConversationStates.ATTENDING, "I expect a scruff like you has never heard of Lady Tembells, huh? She was so beautiful. She died young and her distraught husband asked a powerful Lord to bring her back to life. The fool didn't get what he bargained for, she became a #vampire.", null);
+				add(ConversationStates.ATTENDING, Arrays.asList("vampire"), ConversationStates.ATTENDING, "The husband had hired the help of a Vampire Lord! The Lady became his Vampire Bride and her maids became vampiresses. The Catacombs of North Semos are a deadly place now.", null);
+				addGoodbye("So long. I bet you won't sleep so well tonight.");
+			} //remaining behaviour defined in maps.quests.VampireSword
+		};
+
+		hogart.setDescription("You see Hogart, a retired master dwarf smith."); 
+		zone.assignRPObjectID(hogart);
+		hogart.put("class","olddwarfnpc");
+		hogart.set(12,6); 
+		hogart.initHP(100);
+		zone.addNPC(hogart);    
 	} 
 
 	private void buildCampfireArea(StendhalRPZone zone) {
