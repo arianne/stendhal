5c0817f3865c5892bdfd05bc60a59964e47dc93c
Katie Russell <kymara@users.sourceforge.net>
2007-01-18 23:04:45 +0000

    added -1,-2,-3 semos catacombs

--- src/games/stendhal/server/StendhalRPWorld.java
+++ src/games/stendhal/server/StendhalRPWorld.java
@@ -257,9 +257,9 @@ public class StendhalRPWorld extends RPWorld {
 		addArea("-1_semos_mine_n2_w2");
 		addArea("-1_semos_mine_n2");
 		addArea("-1_semos_mine_nw");
-		addArea("-1_semos_graveyard");
 		addArea("-1_semos_dungeon");
 		addArea("-1_semos_jail");
+		addArea("-1_semos_catacombs");
 		addArea("-1_orril_mountain_w");
 		addArea("-1_orril_dungeon");
 		addArea("-1_orril_castle_w");
@@ -268,7 +268,8 @@ public class StendhalRPWorld extends RPWorld {
 		// Level -2
 		addArea("-2_ados_outside_nw");
 		addArea("-2_semos_dungeon");
-		addArea("-2_semos_jail");		
+		addArea("-2_semos_jail");
+		addArea("-2_semos_catacombs");
 		addArea("-2_kotoch_entrance");
 		addArea("-2_orril_dwarf_mine");
 		addArea("-2_orril_lich_palace");
@@ -277,6 +278,7 @@ public class StendhalRPWorld extends RPWorld {
 		// Level -3
 		addArea("-3_ados_outside_nw");
 		addArea("-3_semos_dungeon");
+		addArea("-3_semos_catacombs");
 		addArea("-3_orril_dwarf_blacksmith");
 		addArea("-3_orril_dungeon");
 
