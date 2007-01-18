26141e06eb438fe046be31065d5fcd9febc572a7
Katie Russell <kymara@users.sourceforge.net>
2007-01-18 23:04:02 +0000

    added -1,-2,-3 catacombs

--- src/games/stendhal/server/maps/semos/SemosPlainsNorth.java
+++ src/games/stendhal/server/maps/semos/SemosPlainsNorth.java
@@ -1,12 +1,18 @@
 package games.stendhal.server.maps.semos;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
+import games.stendhal.common.Direction;
 import games.stendhal.server.StendhalRPWorld;
 import games.stendhal.server.StendhalRPZone;
 import games.stendhal.server.entity.npc.NPCList;
 import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.entity.portal.Portal;
 import games.stendhal.server.pathfinder.Path;
 import marauroa.common.game.IRPZone;
 
@@ -17,7 +23,13 @@ public class SemosPlainsNorth {
 		StendhalRPWorld world = StendhalRPWorld.get();
 
 		buildSemosNorthPlainsArea((StendhalRPZone) world
-				.getRPZone(new IRPZone.ID("0_semos_plains_n")));
+					  .getRPZone(new IRPZone.ID("0_semos_plains_n"))); 
+		buildSemosCatacombs1Area((StendhalRPZone) world
+					 .getRPZone(new IRPZone.ID("-1_semos_catacombs")));
+		buildSemosCatacombs2Area((StendhalRPZone) world
+					 .getRPZone(new IRPZone.ID("-2_semos_catacombs")));
+		buildSemosCatacombs3Area((StendhalRPZone) world
+					 .getRPZone(new IRPZone.ID("-3_semos_catacombs")));
 	}
 
 	private void buildSemosNorthPlainsArea(StendhalRPZone zone) {
@@ -59,6 +71,13 @@ public class SemosPlainsNorth {
 		npc.set(36, 108);
 		npc.initHP(100);
 		zone.addNPC(npc);
+	}
+	private void buildSemosCatacombs1Area(StendhalRPZone zone) {
+	}
+	private void buildSemosCatacombs2Area(StendhalRPZone zone) {
+	}
+
+	private void buildSemosCatacombs3Area(StendhalRPZone zone) {
 	}
 
 }
