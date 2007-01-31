4238996772c8232c894c2fafa953c7bb15e961ec
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-01-31 19:12:23 +0000

    initial checkin

---
+++ src/games/stendhal/server/script/LogoutPlayer.java
@@ -0,0 +1,46 @@
+/* $Id$ */
+package games.stendhal.server.script;
+
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.scripting.ScriptImpl;
+
+import java.util.List;
+
+import marauroa.server.game.NoSuchClientIDException;
+import marauroa.server.game.PlayerEntryContainer;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Logout a player
+ *
+ * @author hendrik
+ */
+public class LogoutPlayer extends ScriptImpl {
+	private static Logger logger = Logger.getLogger(LogoutPlayer.class);
+
+	@Override
+	public void execute(Player admin, List<String> args) {
+
+		// help text
+		if (args.size() == 0) {
+			admin.sendPrivateText("/script KillPlayer.class <playername> logs a player out");
+			return;
+		}
+
+		// remove the player from Narauroa's player container
+		try {
+			PlayerEntryContainer playerContainer = PlayerEntryContainer.getContainer();
+			int clientid = playerContainer.getClientidPlayer(args.get(0));
+			if (clientid > -1) {
+				playerContainer.removeRuntimePlayer(clientid);
+			}
+		} catch (Exception e) {
+			logger.error(e, e);
+		}
+
+		// TODO: remove player from Stendhal
+	}
+
+}
+
