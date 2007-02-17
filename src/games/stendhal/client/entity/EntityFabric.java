e4b3c5351e96edc8c7825ad29407e889d7c087c0
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-02-17 03:44:59 +0000

    helperclass to get rid of dependencies

---
+++ src/games/stendhal/client/entity/EntityFabric.java
@@ -0,0 +1,33 @@
+package games.stendhal.client.entity;
+
+import games.stendhal.client.GameObjects;
+import games.stendhal.client.StendhalClient;
+import marauroa.common.game.RPObject;
+
+/***************************************************************************
+ *                      (C) Copyright 2003 - Marauroa                      *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+public class EntityFabric {
+
+public static Entity createPlayer(GameObjects gameObjects, RPObject object){
+	Player pl = new Player(gameObjects,object);
+	StendhalClient.get().addEventLine("new Player"+pl.getLevel());  
+	return pl;
+}
+	
+	
+	
+	
+	
+	
+	
+}
