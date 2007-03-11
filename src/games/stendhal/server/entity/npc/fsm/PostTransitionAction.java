63da5f6ca9541bd71338d3aae52d3eb6d0bfbc15
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-03-11 16:04:46 +0000

    added javadoc

--- src/games/stendhal/server/entity/npc/fsm/PostTransitionAction.java
+++ src/games/stendhal/server/entity/npc/fsm/PostTransitionAction.java
@@ -3,7 +3,19 @@ package games.stendhal.server.entity.npc.fsm;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.player.Player;
 
+/**
+ * This action is executed after a successful transition of the state maschine.
+ *
+ * @author hendrik
+ */
 public interface PostTransitionAction {
-	
+
+	/**
+	 * does some action after a transition
+	 *
+	 * @param player player who caused the transition
+	 * @param text   text he/she said
+	 * @param engine the NPC doing the transition
+	 */
 	public void fire(Player player, String text, SpeakerNPC engine);
 }
--- src/games/stendhal/server/entity/npc/fsm/PreTransitionCondition.java
+++ src/games/stendhal/server/entity/npc/fsm/PreTransitionCondition.java
@@ -3,7 +3,20 @@ package games.stendhal.server.entity.npc.fsm;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.player.Player;
 
+/**
+ * a condition to check before an transition is executed
+ *
+ * @author hendrik
+ */
 public interface PreTransitionCondition {
 
+	/**
+	 * can the transition be done?
+	 *
+	 * @param player player who caused the transition
+	 * @param text   text he/she said
+	 * @param engine the NPC doing the transition
+	 * @return true, if the transition is possible, false otherwise
+	 */
 	public boolean fire(Player player, String text, SpeakerNPC engine);
 }
