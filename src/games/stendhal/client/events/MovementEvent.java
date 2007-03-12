6bc66b6f3c5a5eef3a157cb71c463a459fd40fe2
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-03-12 15:45:32 +0000

    changed comments into javadoc

--- src/games/stendhal/client/events/MovementEvent.java
+++ src/games/stendhal/client/events/MovementEvent.java
@@ -3,15 +3,36 @@ package games.stendhal.client.events;
 import games.stendhal.common.Direction;
 
 public interface MovementEvent {
-	// When rpentity moves, it will be called with the data.
+
+	/**
+	 * When rpentity moves, it will be called with the data.
+	 * @param x
+	 * @param y 
+	 * @param direction
+	 * @param speed 
+	 */
 	public void onMove(int x, int y, Direction direction, double speed);
 
-	// When rpentity reachs the [x,y,1,1] area.
+
+	/**
+	 * When rpentity reachs the [x,y,1,1] area.
+	 * @param x
+	 * @param y
+	 */
 	public void onEnter(int x, int y);
 
-	// When rpentity leaves the [x,y,1,1] area.
+	/**
+	 * When rpentity leaves the [x,y,1,1] area.
+	 * @param x
+	 * @param y
+	 */
 	public void onLeave(int x, int y);
 
-	// When rpentity stops
+	
+	/**
+	 * Entity has been stopped at Point(x,y)
+	 * @param x
+	 * @param y
+	 */
 	public void onStop(int x, int y);
 }
