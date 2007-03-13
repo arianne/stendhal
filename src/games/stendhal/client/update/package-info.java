f8401cfe226be223d2d03d50f4912832d1260f00
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-03-13 21:46:58 +0000

    added package docu (pointed out disallowed dependencies)

---
+++ src/games/stendhal/client/update/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * This packages manages the startup and update of the client.
+ *
+ * <font color="red"><b>Important: The classes in this package
+ * may not depend have a compile/load time dependency on anything
+ * outside this package (except for the JDK of course). This means
+ * no Log4j, no Marauroa, no Stendhal.</b></font>
+ */
+package games.stendhal.client.update;
