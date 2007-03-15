dc960e4f7d440f2c1b374dd0222da964ba074155
Daniel Herding <wikipedian@users.sourceforge.net>
2007-03-15 23:25:38 +0000

    use the new Outfit datastructure

--- src/games/stendhal/server/actions/OutfitAction.java
+++ src/games/stendhal/server/actions/OutfitAction.java
@@ -13,6 +13,7 @@
 package games.stendhal.server.actions;
 
 import games.stendhal.server.StendhalRPRuleProcessor;
+import games.stendhal.server.entity.player.Outfit;
 import games.stendhal.server.entity.player.Player;
 import marauroa.common.Log4J;
 import marauroa.common.game.RPAction;
@@ -32,11 +33,8 @@ public class OutfitAction extends ActionListener {
 
 		if (action.has("value")) {
 			StendhalRPRuleProcessor.get().addGameEvent(player.getName(), "outfit", action.get("value"));
-			player.put("outfit", action.get("value"));
-			if (player.has("outfit_org")) {
-				player.remove("outfit_org");
-			}
-			player.notifyWorldAboutChanges();
+			Outfit outfit = new Outfit(action.getInt("value"));
+			player.setOutfit(outfit, false);
 		}
 
 		Log4J.finishMethod(logger, "outfit");
