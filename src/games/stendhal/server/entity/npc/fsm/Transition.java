9db5e20e9ce928c576fe27de5862786f51b4b461
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-03-15 06:06:35 +0000

    javadoc

--- src/games/stendhal/server/entity/npc/fsm/Transition.java
+++ src/games/stendhal/server/entity/npc/fsm/Transition.java
@@ -9,26 +9,40 @@ import games.stendhal.server.entity.player.Player;
  * the same one); while doing so, other actions can take place.
  */
 public class Transition {
-	// The state where this transition starts at
+	/** The state where this transition starts at */
 	private int state;
 
-	// The state where this transition leads to
+	/** The state where this transition leads to */
 	private int nextState;
 
-	// The string a player's text must either start with or equal to
-	// in order to trigger this transition
+	/** 
+	 * The string a player's text must either start with or equal to
+	 * in order to trigger this transition
+	 */
 	private String trigger;
 
-	// The condition that has to be fulfilled so that the transition can
-	// be triggered
+	/**
+	 * The condition that has to be fulfilled so that the transition can
+	 * be triggered
+	 */
 	private PreTransitionCondition condition;
 
-	// The text that the NPC will say when the transition is triggered
+	/** The text that the NPC will say when the transition is triggered */
 	private String reply;
 
-	// The action that will take place when the transition is triggered
+	/** The action that will take place when the transition is triggered */
 	private PostTransitionAction action;
 
+	/**
+	 * Creates a new transition
+	 *
+	 * @param currentState old state
+	 * @param trigger      input trigger
+	 * @param condition    additional precondition
+	 * @param nextState    state after the transition
+	 * @param reply        output
+	 * @param action       additional action after the condition
+	 */
 	public Transition(int currentState, String trigger, PreTransitionCondition condition,
 			int nextState, String reply, PostTransitionAction action) {
 		this.state = currentState;
@@ -42,6 +56,7 @@ public class Transition {
 	/**
 	 * Checks whether this is a "wildcard" transition (see class comment
 	 * of SpeakerNPC) which can be fired by the given text.
+	 *
 	 * @param text The text that the player has said
 	 * @return true iff this is a wildcard transition and the triggering
 	 *         text has been said
@@ -90,32 +105,53 @@ public class Transition {
 		}
 	}
 
-	
-
+	/**
+	 * @return Action to execute after transition or null if there is none
+	 */
     public PostTransitionAction getAction() {
 		return action;
 	}
 
+    /**
+     * @return condition to check before doing the transition or null if there is non
+     */
 	public PreTransitionCondition getCondition() {
 		return condition;
 	}
 
+	/**
+	 * @return state after transition
+	 */
 	public int getNextState() {
 		return nextState;
 	}
 
+	/**
+	 * @return output or null if there is none
+	 */
 	public String getReply() {
 		return reply;
 	}
 
+	/**
+	 * Sets the output for this transition
+	 *
+	 * @param reply output
+	 */
 	public void setReply(String reply) {
 		this.reply = reply;
 	}
 
+	/**
+	 * @return the source state
+	 */
 	public int getState() {
 		return state;
 	}
 
+	/**
+	 * @return input
+	 */
 	public String getTrigger() {
 		return trigger;
 	}
