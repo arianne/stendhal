79b3f8ebdca36f732089375c2325794eac6d547d
Nadine Schlonies <n-a-d-i-n-e@users.sourceforge.net>
2007-03-17 21:03:16 +0000

    added NPC for a new quest

--- src/games/stendhal/server/maps/athor/OL0_IslandWest.java
+++ src/games/stendhal/server/maps/athor/OL0_IslandWest.java
@@ -2,8 +2,6 @@ package games.stendhal.server.maps.athor;
 
 import games.stendhal.common.Direction;
 import games.stendhal.server.StendhalRPZone;
-import games.stendhal.server.entity.Chest;
-import games.stendhal.server.entity.NPCOwnedChest;
 import games.stendhal.server.entity.npc.NPCList;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.maps.ZoneConfigurator;
@@ -55,12 +53,44 @@ public class OL0_IslandWest implements ZoneConfigurator {
 			}
 		};
 		npcs.add(cyk);
-
+		
 		zone.assignRPObjectID(cyk);
 		cyk.put("class", "swimmer1npc");
 		cyk.set(172, 39);
 		cyk.setDirection(Direction.DOWN);
 		cyk.initHP(100);
 		zone.add(cyk);
+		
+		SpeakerNPC zara = new SpeakerNPC("Zara") {
+			@Override
+			protected void createPath() {
+				// doesn't move
+				List<Path.Node> nodes = new LinkedList<Path.Node>();
+				setPath(nodes, false);
+			}
+
+			@Override
+			public void say(String text) {
+				// Zara doesn't move around because she's "lying" on her towel.
+				say(text, false);
+			}
+			
+			@Override
+			protected void createDialog() {
+				addGreeting("Nice to meet you!");
+				addJob("I'm on holiday! Let's talk about anything else!");
+				// TODO
+				addHelp("Be careful! On this island is a desert where many adventurers found their death...");
+				addGoodbye("I hope to see you soon!");
+			}
+		};
+		npcs.add(zara);
+		
+		zone.assignRPObjectID(zara);
+		zara.put("class", "swimmer8npc");
+		zara.set(188, 32);
+		zara.setDirection(Direction.DOWN);
+		zara.initHP(100);
+		zone.add(zara);
 	}
 }
