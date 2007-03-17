74424795a4e3409b4d8bd6cc07abc11eee59d772
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-03-17 20:03:52 +0000

    moved teleporter behaviour out of MeetBunny and MeetSanta to new class TeleporterBehaviour

---
+++ src/games/stendhal/server/entity/npc/TeleporterBehaviour.java
@@ -0,0 +1,120 @@
+// $Id$
+package games.stendhal.server.entity.npc;
+
+import games.stendhal.common.Direction;
+import games.stendhal.common.Rand;
+import games.stendhal.server.StendhalRPRuleProcessor;
+import games.stendhal.server.StendhalRPWorld;
+import games.stendhal.server.StendhalRPZone;
+import games.stendhal.server.events.TurnListener;
+import games.stendhal.server.events.TurnNotifier;
+import games.stendhal.server.pathfinder.Path;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+/**
+ * teleports the SpeakerNPC to a random location on the outside world
+ * and causes it to walk a random bit.
+ *
+ * @author hendrik
+ */
+public class TeleporterBehaviour implements TurnListener {
+	private static Logger logger = Logger.getLogger(TeleporterBehaviour.class);
+	private StendhalRPZone zone = null;
+	private ArrayList<StendhalRPZone> zones = null;
+	private SpeakerNPC speakerNPC = null;
+
+	/**
+	 * Creates a new TeleporterBehaviour
+	 *
+	 * @param speakerNPC SpeakerNPC
+	 * @param repeatedText text to repeat
+	 */
+	public TeleporterBehaviour(final SpeakerNPC speakerNPC, final String repeatedText) {
+		this.speakerNPC  = speakerNPC;
+		listZones();
+		TurnNotifier.get().notifyInTurns(60, this, null);
+		// say something every minute so that can be noticed more easily
+		TurnNotifier.get().notifyInTurns(60, new TurnListener() {
+			public void onTurnReached(int currentTurn, String message) {
+				speakerNPC.say(repeatedText);
+				TurnNotifier.get().notifyInTurns(60 * 3, this, null);
+			}
+		}, null);
+	}
+
+	/**
+	 * Creates an ArrayList of "outside" zones for bunny.
+	 */
+	private void listZones() {
+		Iterator itr = StendhalRPWorld.get().iterator();
+		zones = new ArrayList<StendhalRPZone>();
+        while (itr.hasNext()) {
+        	StendhalRPZone aZone = (StendhalRPZone) itr.next();
+        	String zoneName = aZone.getID().getID();
+        	if (zoneName.startsWith("0") && !zoneName.equals("0_nalwor_city") 
+        		&& !zoneName.equals("0_orril_castle") 
+        		&& !zoneName.equals("0_ados_swamp")
+        		&& !zoneName.equals("0_ados_outside_w")
+        		&& !zoneName.equals("0_ados_wall_n")) {
+        		zones.add(aZone);
+        	}
+        }
+	}
+
+	public void onTurnReached(int currentTurn, String message) {
+		// Say bye
+		speakerNPC.say("Bye.");
+
+		// We make bunny to stop speaking to anyone.
+		speakerNPC.setCurrentState(ConversationStates.IDLE);
+
+		// remove bunny from old zone
+		zone.remove(speakerNPC);
+
+		// Teleport to another random place
+		boolean found = false;
+		int x = -1;
+		int y = -1;
+		while (!found) {
+			zone = zones.get(Rand.rand(zones.size()));
+			x = Rand.rand(zone.getWidth() - 4) + 2;
+			y = Rand.rand(zone.getHeight() - 5) + 2;
+			if (!zone.collides(x, y) && !zone.collides(x, y + 1)) {
+				zone.assignRPObjectID(speakerNPC);
+				speakerNPC.set(x, y);
+				speakerNPC.setDirection(Direction.RIGHT);
+
+				zone.add(speakerNPC);
+				StendhalRPRuleProcessor.get().addNPC(speakerNPC);
+				found = true;
+				logger.info("Placing bunny at " + zone.getID().getID() + " " + x + " " + y);
+			} else {
+				logger.warn("Cannot place bunny at " + zone.getID().getID() + " " + x + " " + y);
+			}
+		}
+
+		// try to build a path (but give up after 10 successless tries)
+		for (int i = 0; i < 10; i++) {
+			int tx = Rand.rand(zone.getWidth() - 4) + 2;
+			int ty = Rand.rand(zone.getHeight() - 5) + 2;
+			List<Path.Node> path = Path.searchPath(speakerNPC, tx, ty);
+			if ((path != null) && (path.size() > 1)) {
+				// create path back
+				for (int j = path.size() - 1; j > 0; j--) {
+					path.add(path.get(j));
+				}
+				logger.info(path);
+				speakerNPC.setPath(path, true);
+				break;
+			}
+		}
+
+		// Schedule so we are notified again in 5 minutes
+		TurnNotifier.get().notifyInTurns(5 * 60 * 3, this, null);
+	}
+}
--- src/games/stendhal/server/maps/quests/MeetBunny.java
+++ src/games/stendhal/server/maps/quests/MeetBunny.java
@@ -1,8 +1,5 @@
 package games.stendhal.server.maps.quests;
 
-import games.stendhal.common.Direction;
-import games.stendhal.common.Rand;
-import games.stendhal.server.StendhalRPRuleProcessor;
 import games.stendhal.server.StendhalRPWorld;
 import games.stendhal.server.StendhalRPZone;
 import games.stendhal.server.entity.item.Item;
@@ -10,17 +7,12 @@ import games.stendhal.server.entity.npc.ConversationPhrases;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.StandardInteraction;
+import games.stendhal.server.entity.npc.TeleporterBehaviour;
 import games.stendhal.server.entity.player.Player;
-import games.stendhal.server.events.TurnListener;
-import games.stendhal.server.events.TurnNotifier;
 import games.stendhal.server.pathfinder.Path;
 
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-
-import org.apache.log4j.Logger;
 
 /**
  * QUEST: Meet the Easter Bunny anywhere around the World.
@@ -40,19 +32,17 @@ import org.apache.log4j.Logger;
  * REPETITIONS:
  * - None
  */
-public class MeetBunny extends AbstractQuest implements TurnListener {
+public class MeetBunny extends AbstractQuest {
 	private static final String QUEST_SLOT = "meet_bunny_07";
-	private static Logger logger = Logger.getLogger(MeetBunny.class);
 	/** the Bunny NPC */
 	protected SpeakerNPC bunny = null;
 	private StendhalRPZone zone = null;
-	private ArrayList<StendhalRPZone> zones = null;
 
 	@Override
 	public void init(String name) {
 		super.init(name, QUEST_SLOT);
 	}
-	
+
 	private SpeakerNPC createbunny() {
 		bunny = new SpeakerNPC("Easter Bunny") {
 			@Override
@@ -99,92 +89,11 @@ public class MeetBunny extends AbstractQuest implements TurnListener {
 		return bunny;
 	}
 
-	/**
-	 * Creates an ArrayList of "outside" zones for bunny.
-	 */
-	private void listZones() {
-		Iterator itr = StendhalRPWorld.get().iterator();
-		zones = new ArrayList<StendhalRPZone>();
-        while (itr.hasNext()) {
-        	StendhalRPZone aZone = (StendhalRPZone) itr.next();
-        	String zoneName = aZone.getID().getID();
-        	if (zoneName.startsWith("0") && !zoneName.equals("0_nalwor_city") 
-        		&& !zoneName.equals("0_orril_castle") 
-        		&& !zoneName.equals("0_ados_swamp")
-        		&& !zoneName.equals("0_ados_outside_w")
-        		&& !zoneName.equals("0_ados_wall_n")) {
-        		zones.add(aZone);
-        	}
-        }
-	}
-
-	public void onTurnReached(int currentTurn, String message) {
-		// Say bye
-		bunny.say("Bye.");
-
-		// We make bunny to stop speaking to anyone.
-		bunny.setCurrentState(ConversationStates.IDLE);
-
-		// remove bunny from old zone
-		zone.remove(bunny);
-
-		// Teleport to another random place
-		boolean found = false;
-		int x = -1;
-		int y = -1;
-		while (!found) {
-			zone = zones.get(Rand.rand(zones.size()));
-			x = Rand.rand(zone.getWidth() - 4) + 2;
-			y = Rand.rand(zone.getHeight() - 5) + 2;
-			if (!zone.collides(x, y) && !zone.collides(x, y + 1)) {
-				zone.assignRPObjectID(bunny);
-				bunny.set(x, y);
-				bunny.setDirection(Direction.RIGHT);
-
-				zone.add(bunny);
-				StendhalRPRuleProcessor.get().addNPC(bunny);
-				found = true;
-				logger.info("Placing bunny at " + zone.getID().getID() + " " + x + " " + y);
-			} else {
-				logger.warn("Cannot place bunny at " + zone.getID().getID() + " " + x + " " + y);
-			}
-		}
-
-		// try to build a path (but give up after 10 successless tries)
-		for (int i = 0; i < 10; i++) {
-			int tx = Rand.rand(zone.getWidth() - 4) + 2;
-			int ty = Rand.rand(zone.getHeight() - 5) + 2;
-			List<Path.Node> path = Path.searchPath(bunny, tx, ty);
-			int size = path.size();
-			if ((path != null) && (size > 1)) {
-				// create path back
-				for (int j = size - 1; j > 0; j--) {
-					path.add(path.get(j));
-				}
-				logger.info(path);
-				bunny.setPath(path, true);
-				break;
-			}
-		}
-
-		// Schedule so we are notified again in 5 minutes
-		TurnNotifier.get().notifyInTurns(5 * 60 * 3, this, null);
-	}
-
 	@Override
 	public void addToWorld() {
 		super.addToWorld();
 		createbunny();
-		listZones();
-		TurnNotifier.get().notifyInTurns(60, this, null);
-
-		// say something every minute so that can be noticed more easily
-		TurnNotifier.get().notifyInTurns(60, new TurnListener() {
-			public void onTurnReached(int currentTurn, String message) {
-				bunny.say("*hop* *hop* *hop* Happy Easter!");
-				TurnNotifier.get().notifyInTurns(60 * 3, this, null);
-			}
-		}, null);
+		new TeleporterBehaviour(bunny, "*hop* *hop* *hop* Happy Easter!");
 	}
 
 }
