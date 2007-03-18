6b70426aed8bc48e50a2c21766fc43ffeb912864
Daniel Herding <wikipedian@users.sourceforge.net>
2007-03-18 02:45:39 +0000

    made transparent signs useful by making it possible to walk over them added a readable layer to the wooden arch in semos village

--- src/games/stendhal/server/entity/Sign.java
+++ src/games/stendhal/server/entity/Sign.java
@@ -18,16 +18,29 @@ import marauroa.common.game.AttributeNotFoundException;
 import marauroa.common.game.RPClass;
 import org.apache.log4j.Logger;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Sign extends Entity {
 	/** the logger instance. */
 	private static final Logger logger = Log4J.getLogger(Sign.class);
+	
+	/**
+	 * Classes of signs that players, NPCs etc. can walk over
+	 * and where you can put items on if they are not placed
+	 * on a collision tile.
+	 */ 
+	private static final List<String> NON_OBSTACLE_CLASSES = Arrays.asList(
+			"book_blue",
+			"book_red",
+			"transparent");
 
 	public static void generateRPClass() {
 		try {
 			RPClass sign = new RPClass("sign");
 			sign.isA("entity");
 			sign.add("text", RPClass.LONG_STRING);
-			sign.add("class", RPClass.LONG_STRING);
+			sign.add("class", RPClass.STRING);
 		} catch (RPClass.SyntaxException e) {
 			logger.error("cannot generate RPClass", e);
 		}
@@ -55,4 +68,10 @@ public class Sign extends Entity {
 	public void setClass(String clazz) {
 		put("class", clazz);
 	}
+	
+	@Override
+	public boolean isObstacle(Entity entity) {
+		return !(has("class") && NON_OBSTACLE_CLASSES.contains(get("class")));
+	}
+
 }
