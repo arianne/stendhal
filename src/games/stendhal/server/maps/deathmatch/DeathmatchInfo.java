5ac8851bfb84191d8e813370c25acd0f7e6724c3
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-03-21 23:30:14 +0000

    java doc

--- src/games/stendhal/server/maps/deathmatch/DeathmatchInfo.java
+++ src/games/stendhal/server/maps/deathmatch/DeathmatchInfo.java
@@ -3,11 +3,23 @@ package games.stendhal.server.maps.deathmatch;
 import games.stendhal.server.StendhalRPZone;
 import games.stendhal.server.util.Area;
 
+/**
+ * Stores information about the place of the deathmatch.
+ *
+ * @author hendrik
+ */
 public class DeathmatchInfo {
 	private final Area arena;
 	private final String zoneName;
 	private final StendhalRPZone zone;
 
+	/**
+	 * Creates a new DeathmatchInfo
+	 *
+	 * @param arena    combat area
+	 * @param zoneName name of zone
+	 * @param zone     zone
+	 */
 	public DeathmatchInfo(final Area arena, final String zoneName, final StendhalRPZone zone) {
 		super();
 		this.arena = arena;
@@ -15,14 +27,29 @@ public class DeathmatchInfo {
 		this.zone = zone;
 	}
 
+	/**
+	 * gets the arena
+	 *
+	 * @return combat area
+	 */
 	public Area getArena() {
 		return arena;
 	}
 
+	/**
+	 * gets the zone
+	 *
+	 * @return zone
+	 */
 	public StendhalRPZone getZone() {
 		return zone;
 	}
 
+	/**
+	 * get the zone name
+	 *
+	 * @return name of zone
+	 */
 	public String getZoneName() {
 		return zoneName;
 	}
