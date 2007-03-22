d482e7f61564336c61511f75efc9be60d219a637
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-03-22 00:05:07 +0000

    use DeathmatchState, javadoc and @Override

--- src/games/stendhal/server/maps/deathmatch/StartAction.java
+++ src/games/stendhal/server/maps/deathmatch/StartAction.java
@@ -4,22 +4,28 @@ import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.events.TurnNotifier;
 
-import java.util.Date;
-
+/**
+ * Action to start a new deathmatch session for the player
+ *
+ * @author hendrik
+ */
 public class StartAction extends SpeakerNPC.ChatAction {
 	private DeathmatchInfo deathmatchInfo;
-	
+
+	/**
+	 * creates a new StartAction for the specified deathmatch
+	 
+	 * @param deathmatchInfo deathmatch to start
+	 */
 	public StartAction(DeathmatchInfo deathmatchInfo) {
 		this.deathmatchInfo = deathmatchInfo;
 	}
 
+	@Override
 	public void fire(Player player, String text, SpeakerNPC engine) {
 		engine.say("Have fun!");
-		int level = player.getLevel() - 2;
-		if(level < 1) {
-			level = 1;
-		}
-		player.setQuest("deathmatch", "start;"+ level + ";" + (new Date()).getTime());
+		DeathmatchState deathmatchState = DeathmatchState.createStartState(player);
+		player.setQuest("deathmatch", deathmatchState.toQuestString());
 		DeathmatchEngine scriptingAction = new DeathmatchEngine(player, deathmatchInfo);
 		TurnNotifier.get().notifyInTurns(0, scriptingAction, null);
 	}
