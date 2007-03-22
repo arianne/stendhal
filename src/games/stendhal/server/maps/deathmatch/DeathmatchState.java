764e7df86e16a57677037aeb7835bb65717e839f
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-03-22 00:02:08 +0000

    inital checkin

---
+++ src/games/stendhal/server/maps/deathmatch/DeathmatchState.java
@@ -0,0 +1,50 @@
+package games.stendhal.server.maps.deathmatch;
+
+import java.util.Date;
+
+import games.stendhal.server.entity.player.Player;
+
+/**
+ * manages the deathmatch state (which is store in a quest slot)
+ *
+ * @author hendrik
+ */
+public class DeathmatchState {
+
+	private String state = null;
+	private int level = 0;
+	private long date = 0;
+
+	/**
+	 * creates a start state
+	 *
+	 * @param player Player
+	 * @return start state
+	 */
+	public static DeathmatchState createStartState(Player player) {
+		DeathmatchState deathmatchState = new DeathmatchState();
+		deathmatchState.state = "start";
+		deathmatchState.date = new Date().getTime();
+		int level = player.getLevel() - 2;
+		if(level < 1) {
+			level = 1;
+		}
+		deathmatchState.level = level;
+		return deathmatchState;
+	}
+
+	/**
+	 * return the state as string which can be stored in the quest slot
+	 *
+	 * @return quest string
+	 */
+	public String toQuestString() {
+		return state + ";" + level + ";" + date;
+	}
+
+	@Override
+	public String toString() {
+		// use toQuestString() because it is better than Object.toString()
+		return toQuestString();
+	}
+}
