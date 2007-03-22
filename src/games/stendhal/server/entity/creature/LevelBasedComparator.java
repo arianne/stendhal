e8a9fb847ff43779ca0c28b0663c2d3475c38ab8
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-03-22 12:45:31 +0000

    added javadoc, implemented Serializable and compare RPEntity instead of only Creatures

--- src/games/stendhal/server/entity/creature/LevelBasedComparator.java
+++ src/games/stendhal/server/entity/creature/LevelBasedComparator.java
@@ -1,11 +1,18 @@
 package games.stendhal.server.entity.creature;
 
+import games.stendhal.server.entity.RPEntity;
+
+import java.io.Serializable;
 import java.util.Comparator;
 
+/**
+ * Compares to RPEntities based on their level
+ *
+ * @author hendrik
+ */
+public class LevelBasedComparator implements Comparator<RPEntity>, Serializable {
 
-public class LevelBasedComparator implements Comparator<Creature> {
-
-	public int compare(Creature o1, Creature o2) {
+	public int compare(RPEntity o1, RPEntity o2) {
 		return o1.getLevel() - o2.getLevel();
 	}
 
