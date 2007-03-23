23f52553208cb4cc011bc6233622681fc2cb02ce
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-03-23 11:20:58 +0000

    initial checkin

---
+++ src/games/stendhal/client/scripting/command/SlashCommandFactory.java
@@ -0,0 +1,45 @@
+package games.stendhal.client.scripting.command;
+
+import java.util.HashMap;
+
+public class SlashCommandFactory {
+
+	/** Set of client supported commands */
+	private static HashMap<String, SlashCommand> commands;
+
+	public static void register() {
+		commands = new HashMap<String, SlashCommand>();
+
+//TODO:		commands.put("/", new RemessageCommand());
+		commands.put("away", new AwayCommand());
+//TODO:		commands.put("tell", new MessageCommand());
+		commands.put("answer", new AnswerCommand());
+//TODO:		commands.put("msg", new MessageCommand());
+		commands.put("support", new SupportCommand());
+		commands.put("supporta", new SupportAnswerCommand());
+		commands.put("supportanswer", new SupportAnswerCommand());
+		commands.put("where", new WhereCommand());
+		commands.put("who", new WhoCommand());
+		commands.put("drop", new DropCommand());
+		commands.put("add", new AddBuddyCommand());
+		commands.put("remove", new RemoveBuddyCommand());
+		commands.put("ignore", new IgnoreCommand());
+		commands.put("quit", new QuitCommand());
+		commands.put("help", new HelpCommand());
+		commands.put("sound", new SoundCommand());
+//TODO:		commands.put("record", new RecordCommand());
+
+		commands.put("tellall", new TellAllCommand());
+		commands.put("teleport", new TeleportCommand());
+		commands.put("teleportto", new TeleportToCommand());
+		commands.put("adminlevel", new AdminLevelCommand());
+		commands.put("alter", new AlterCommand());
+		commands.put("summon", new SummonCommand());
+		commands.put("summonat", new SummonAtCommand());
+		commands.put("inspect", new InspectCommand());
+		commands.put("jail", new JailCommand());
+		commands.put("invisible", new InvisibleCommand());
+		commands.put("gmhelp", new GMHelpCommand());
+		commands.put("gmhelp_alter", new GMHelpAlterCommand());
+	}
+}
