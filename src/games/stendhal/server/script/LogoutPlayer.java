d66d6dbe74d377988792f622f947537bbaf8f5bc
timothyb89 <timothyb89@users.sourceforge.net>
2007-03-28 04:15:47 +0000

    This is Seather's update to LogoutPlayer.java from http://sourceforge.net/tracker/index.php?func=detail&aid=1688835&group_id=1111&atid=301111

--- src/games/stendhal/server/script/LogoutPlayer.java
+++ src/games/stendhal/server/script/LogoutPlayer.java
@@ -1,12 +1,16 @@
-/* $Id$ */
+
 package games.stendhal.server.script;
 
+import games.stendhal.server.StendhalRPWorld;
+import games.stendhal.server.StendhalRPZone;
+import games.stendhal.server.StendhalRPRuleProcessor;
 import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.scripting.ScriptImpl;
 
 import java.util.List;
 
 import marauroa.server.game.PlayerEntryContainer;
+import marauroa.common.game.RPObject;
 
 import org.apache.log4j.Logger;
 
@@ -27,19 +31,40 @@ public class LogoutPlayer extends ScriptImpl {
 			admin.sendPrivateText("/script LogoutPlayer.class <playername> logs a player out");
 			return;
 		}
-
-		// remove the player from Narauroa's player container
+		
 		try {
+			//see processLogoutEvent in marauroa-1.34/src/marauroa/server/game/GameServerManager.java
+			
 			PlayerEntryContainer playerContainer = PlayerEntryContainer.getContainer();
+			
 			int clientid = playerContainer.getClientidPlayer(args.get(0));
-			if (clientid != -1) {
-				playerContainer.removeRuntimePlayer(clientid);
+			if (clientid == -1) {
+				admin.sendPrivateText(args.get(0) + " not found");
+				return;
 			}
+			
+			Player player = StendhalRPRuleProcessor.get().getPlayer(args.get(0));
+			StendhalRPZone zone = (StendhalRPZone) StendhalRPWorld.get().getRPZone(player.getID());
+			RPObject.ID id = playerContainer.getRPObjectID(clientid);
+			RPObject object = zone.get(id);
+			
+			//player.sendPrivateText("You have been logged out by an admin");
+			
+			// remove player from Stendhal
+			if(StendhalRPRuleProcessor.get().onExit(id)) {
+				// NOTE: Set the Object so that it is stored in Database 
+				playerContainer.setRPObject(clientid, object);
+			}
+
+			// remove the player from Narauroa's player container
+			playerContainer.removeRuntimePlayer(clientid);
+			
+			admin.sendPrivateText(args.get(0) + " has been logged out");
+			
 		} catch (Exception e) {
 			logger.error(e, e);
 		}
 
-		// TODO: remove player from Stendhal
 	}
 
 }
