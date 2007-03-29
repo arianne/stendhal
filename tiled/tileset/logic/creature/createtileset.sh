f7a1aa69beda658b6b1b964da65a108bc368c239
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2007-03-29 11:43:50 +0000

    Fixed the tilesets... I broke them :(

--- tiled/tileset/logic/creature/createtileset.sh
+++ tiled/tileset/logic/creature/createtileset.sh
@@ -3,7 +3,7 @@
 for img in `ls $1/*.png` 
 do
   convert $img -crop $2 $img.tmp.png
-  convert $img.tmp.png -trim +repage -resize "30x30>" -size 30x30 +swap -gravity center -bordercolor white -border 1x1 $img.2.tmp.png
+  convert $img.tmp.png -trim +repage -resize "30x30>" -size 30x30 xc:white +swap -gravity center -composite -bordercolor black -border 1x1 $img.2.tmp.png
 done
 
 convert `ls $1/*.2.tmp.png` +append $3
