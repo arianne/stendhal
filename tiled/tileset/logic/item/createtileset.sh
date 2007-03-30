68796badecf45f1c64cef9626ee7edd87c1eb6a3
Nadine Schlonies <n-a-d-i-n-e@users.sourceforge.net>
2007-03-30 19:36:16 +0000

    copied miguel's creature script for items

---
+++ tiled/tileset/logic/item/createtileset.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for img in `ls $1/*.png` 
+do
+  convert $img -crop $2 $img.tmp.png
+  convert $img.tmp.png -trim +repage -resize "30x30>" -size 30x30 xc:white +swap -gravity center -composite -bordercolor black -border 1x1 $img.2.tmp.png
+done
+
+convert `ls $1/*.2.tmp.png` +append $3
+
+rm $1/*.tmp.png
+
+#example call for herbs:
+#./createtileset.sh ../../../../data/sprites/items/herb/ 95 herbs.png
