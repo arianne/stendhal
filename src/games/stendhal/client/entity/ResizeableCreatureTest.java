5256df9cfb5a91005c57f72586a33ae9abcbaba6
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-04-01 15:39:17 +0000

    removed annotation of not thrown exception as it seems to work now

--- src/games/stendhal/client/entity/ResizeableCreatureTest.java
+++ src/games/stendhal/client/entity/ResizeableCreatureTest.java
@@ -7,14 +7,17 @@ import marauroa.common.game.RPObject;
 
 
 public class ResizeableCreatureTest {
-@Test //(expected=IllegalArgumentException.class)
+@Test 
 public final void onChangedAddedTest(){
 
 	RPObject rp = new RPObject();
 	rp.put("type", 1);
 	rp.put("class", 1);
 	rp.put("name", "hugo");
-	Creature cr = new ResizeableCreature(rp);
+	rp.put("width", 1);
+	rp.put("height", 1);
+	
+	new ResizeableCreature(rp);
 	
 
 }
