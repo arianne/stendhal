75049d4a86d4f7fcc3a40fb1b87a9c59f246c98e
cvs2git <ariannebot@users.sourceforge.net>
2007-04-01 08:45:47 +0000

    This commit was manufactured by cvs2svn to create branch 'VERSION_00_RELEASE_58'.
    Cherrypick from master 2007-04-01 08:45:46 UTC Hendrik Brummermann <nhnb@users.sourceforge.net> 'new script for admins to remove player items':
        src/games/stendhal/server/script/DropPlayerItems.java

---
+++ src/games/stendhal/server/script/DropPlayerItems.java
@@ -0,0 +1,43 @@
+package games.stendhal.server.script;
+
+import games.stendhal.common.Grammar;
+import games.stendhal.server.StendhalRPRuleProcessor;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.scripting.ScriptImpl;
+
+import java.util.List;
+
+/**
+ * drop the specified amount of items from the player.
+ *
+ * @author hendrik
+ */
+public class DropPlayerItems extends ScriptImpl {
+
+	@Override
+	public void execute(Player admin, List<String> args) {
+		super.execute(admin, args);
+		if (args.size() < 2) {
+			admin.sendPrivateText("<player> [<amount>] <item>");
+			return;
+		}
+		Player player = StendhalRPRuleProcessor.get().getPlayer(args.get(0));
+		String itemName = null;
+		int amount = 1;
+		if (args.size() == 3) {
+			amount = Integer.parseInt(args.get(1));
+			itemName = args.get(2);
+		} else {
+			itemName = args.get(1);
+		}
+		
+		boolean res = player.drop(itemName, amount);
+		String msg = "Admin " + admin.getName() + " removed "
+			+ amount + " " + Grammar.plnoun(amount, itemName)
+			+ " from player " + player.getName() + ": " + res;
+		player.sendPrivateText(msg);
+		admin.sendPrivateText(msg);
+		StendhalRPRuleProcessor.get().addGameEvent(admin.getName(), "admindrop", 
+				player.getName(), Integer.toString(amount), itemName);
+	}
+}
