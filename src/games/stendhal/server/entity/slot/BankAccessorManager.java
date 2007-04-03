cceeb4638d3eccee7dacceea9be084d0ec5d4853
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-04-03 21:42:25 +0000

    return an empty list for unknown banks

--- src/games/stendhal/server/entity/slot/BankAccessorManager.java
+++ src/games/stendhal/server/entity/slot/BankAccessorManager.java
@@ -39,26 +39,38 @@ public class BankAccessorManager {
 	 * @param entity Accessor
 	 */
 	public void add(Banks bank, Entity entity) {
-		List<Entity> bankAccess = accessors.get(bank);
-		if (bankAccess == null) {
-			bankAccess = new LinkedList<Entity>();
-			accessors.put(bank, bankAccess);
-		}
+		List<Entity> bankAccess = getListAddingUnkownBanks(bank);
 		if (!bankAccess.contains(entity)) {
 			bankAccess.add(entity);
 		}
 	}
 
 	/**
-	 * gets the list of accessors for think bank
+	 * gets the list of accessors for the specified bank. In case
+	 * the bank is unkown, an empty list is automatically created
 	 *
 	 * @param bank Banks
 	 * @return list of accessors
+	 */
+	private List<Entity> getListAddingUnkownBanks(Banks bank) {
+		List<Entity> bankAccess = accessors.get(bank);
+		if (bankAccess == null) {
+			bankAccess = new LinkedList<Entity>();
+			accessors.put(bank, bankAccess);
+		}
+		return bankAccess;
+	}
+	
+	/**
+	 * gets the list of accessors for think bank
+	 *
+	 * @param bank Banks
+	 * @return list of accessors or an empty list if this bank is unkown
 	 */
 	protected List<Entity> get(Banks bank) {
 
 		// If the visibilty of this method should be raised,
 		// please return only a copy of this list
-		return accessors.get(bank);
+		return getListAddingUnkownBanks(bank);
 	}
 }
