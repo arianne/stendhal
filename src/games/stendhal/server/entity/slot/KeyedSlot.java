14a1a45ddb8f1317174305673dc9e209d077571b
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-04-03 21:03:58 +0000

    initial checkin

---
+++ src/games/stendhal/server/entity/slot/KeyedSlot.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package games.stendhal.server.entity.slot;
+
+import games.stendhal.server.entity.Entity;
+
+/**
+ * abused slots of players which contain one RPObject used as hashmap
+ *
+ * @author hendrik
+ */
+public class KeyedSlot extends EntitySlot {
+
+	/**
+	 * creates a new keyed slot
+	 *
+	 * @param name name of slot
+	 */
+	public KeyedSlot(String name) {
+		super(name);
+	}
+
+	@Override
+	public boolean isItemSlot() {
+		return false;
+	}
+
+	@Override
+	public boolean isReachableBy(Entity entity) {
+		return false;
+	}
+
+}
---
+++ src/games/stendhal/server/entity/slot/PlayerSlot.java
@@ -0,0 +1,25 @@
+package games.stendhal.server.entity.slot;
+
+import games.stendhal.server.entity.Entity;
+
+/**
+ * Slots of players which contain items
+ *
+ * @author hendrik 
+ */
+public class PlayerSlot extends EntitySlot {
+
+	/**
+	 * Creates a new PlayerSlot
+	 *
+	 * @param name name of slot
+	 */
+	public PlayerSlot(String name) {
+		super(name);
+	}
+
+	@Override
+	public boolean isReachableBy(Entity entity) {
+		return super.hasAsParent(entity.getID());
+	}
+}
