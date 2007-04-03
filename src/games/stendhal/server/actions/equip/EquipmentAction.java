2725ef1a3f179e13a0cd7302afb573556fe01ed7
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-04-03 19:08:42 +0000

    activated "new" drop code and removed old code (note: "new" means older than one year)

--- src/games/stendhal/server/actions/equip/EquipmentAction.java
+++ src/games/stendhal/server/actions/equip/EquipmentAction.java
@@ -149,161 +149,39 @@ public class EquipmentAction implements ActionListener {
 	private void onDrop(Player player, RPAction action) {
 		Log4J.startMethod(logger, "drop");
 
-		final boolean USE_NEW_CODE = false;
-
-		// New Code Starts Here
-		// FIXME: This code does not work.
-
-		if (USE_NEW_CODE) {
-			// get source and check it
-			SourceObject source = new SourceObject(action, player);
-			if (!source.isValid() || !source.checkDistance(player, EquipActionConsts.MAXDISTANCE)
-			        || !source.checkClass(validContainerClassesList)) {
-				// source is not valid
-				return;
-			}
-
-			// get destination and check it
-			DestinationObject dest = new DestinationObject(action, player);
-			if (!dest.isValid() || !dest.checkDistance(player, 5.0) || !dest.checkClass(validContainerClassesList)) {
-				logger.warn("destination is invalid. action is: " + action);
-				// destination is not valid
-				return;
-			}
-
-			Entity entity = source.getEntity();
-			String itemName = "entity";
-			if (entity.has("name")) {
-				itemName = entity.get("name").replace("_", " ");
-			} else if (entity instanceof Item) {
-				itemName = "item";
-			}
-			
-			// FIXME: This will remove the item from the slot, but it does not
-			// reappear
-			// on the ground. See DestinationObject.addToWorld()#600
-			source.moveTo(dest, player);
-			if (entity.has("bound")) {
-				player.sendPrivateText("You put a valuable item on the ground. Please note that it will expire in " + (Item.DEGRADATION_TIMEOUT / 60) + " minutes, as all items do. But in this case there is no way to restore it.");
-			}
-			int amount = 1;
-			if (entity instanceof StackableItem) {
-				amount = ((StackableItem) entity).getQuantity();
-			}
-			StendhalRPRuleProcessor.get().addGameEvent(player.getName(), "drop", itemName, source.getSlot(), dest.getSlot(), Integer.toString(amount));
+		// get source and check it
+		SourceObject source = new SourceObject(action, player);
+		if (!source.isValid() || !source.checkDistance(player, EquipActionConsts.MAXDISTANCE)
+		        || !source.checkClass(validContainerClassesList)) {
+			// source is not valid
 			return;
 		}
-		// Old Code Starts Here
 
-		if (action.has("baseobject") && action.has("baseslot") && action.has("x") && action.has("y")
-		        && action.has("baseitem")) {
-			StendhalRPZone zone = (StendhalRPZone) StendhalRPWorld.get().getRPZone(player.getID());
-
-			int baseObject = action.getInt("baseobject");
-
-			RPObject.ID baseobjectid = new RPObject.ID(baseObject, zone.getID());
-			if (!zone.has(baseobjectid)) {
-				return;
-			}
-
-			RPObject base = zone.get(baseobjectid);
-			if (!((base instanceof Player) || (base instanceof Corpse) || (base instanceof Chest))) {
-				// Only allow to drop objects from players, corpses or chests
-				return;
-			}
-
-			if ((base instanceof Player) && !player.getID().equals(base.getID())) {
-				// Only allowed to drop item of our own player.
-				return;
-			}
-
-			Entity baseEntity = (Entity) base;
-
-			if (baseEntity.hasSlot(action.get("baseslot"))) {
-				RPSlot slot = baseEntity.getSlot(action.get("baseslot"));
-
-				if (slot.size() == 0) {
-					return;
-				}
-
-				RPObject object = null;
-				int item = action.getInt("baseitem");
-				// scan through the slot to find the requested item
-				for (RPObject rpobject : slot) {
-					if (rpobject.getID().getObjectID() == item) {
-						object = rpobject;
-						break;
-					}
-				}
-
-				// no item found...we take the first one
-				if (object == null) {
-					object = slot.iterator().next();
-				}
-
-				// It is always an entity
-				Entity entity = (Entity) object;
-
-				int x = action.getInt("x");
-				int y = action.getInt("y");
-
-				int quantity = 0;
-
-				if ((entity instanceof Stackable) && action.has(EquipActionConsts.QUANTITY)) {
-					int entityQuantity = ((Stackable) entity).getQuantity();
-
-					quantity = action.getInt(EquipActionConsts.QUANTITY);
-					if ((entityQuantity < 1) || (quantity < 1) || (quantity >= entityQuantity)) {
-						quantity = 0; // quantity == 0 performs a regular move
-						// of the entire item
-					}
-				}
-
-				if (player.nextTo(baseEntity) && (baseEntity.squaredDistance(x, y) < 8 * 8)
-				        && !zone.simpleCollides(entity, x, y)) {
-					if (quantity != 0) {
-						StackableItem newItem = ((StackableItem) entity).splitOff(quantity);
-
-						zone.assignRPObjectID(newItem);
-						newItem.set(x, y);
-						zone.add(newItem, player);
-					} else {
-						slot.remove(entity.getID());
-
-						entity.setX(x);
-						entity.setY(y);
-
-						zone.assignRPObjectID(entity);
-						zone.add(entity, player);
-
-						// HACK: Avoid a problem on database
-						if (entity.has("#db_id")) {
-							entity.remove("#db_id");
-						}
-					}
-					if (entity.has("bound")) {
-						player.sendPrivateText("You put a valuable item on the ground. Please note that it will expire in " + (Item.DEGRADATION_TIMEOUT / 60) + " minutes, as all items do. But in this case there is no way to restore it.");
-					}
-
-					String itemName = "entity";
-					if (entity.has("name")) {
-						itemName = entity.get("name").replace("_", " ");
-					} else if (entity instanceof Item) {
-						itemName = "item";
-					}
-					
-					int amount = 1;
-					if (entity instanceof StackableItem) {
-						amount = ((StackableItem) entity).getQuantity();
-					}
-					StendhalRPRuleProcessor.get().addGameEvent(player.getName(), "drop", itemName, slot.getName(), "ground", Integer.toString(amount));
-
-					
-					baseEntity.notifyWorldAboutChanges();
-				}
-			}
+		// get destination and check it
+		DestinationObject dest = new DestinationObject(action, player);
+		if (!dest.isValid() || !dest.checkDistance(player, 5.0) || !dest.checkClass(validContainerClassesList)) {
+			logger.warn("destination is invalid. action is: " + action);
+			// destination is not valid
+			return;
 		}
 
+		Entity entity = source.getEntity();
+		String itemName = "entity";
+		if (entity.has("name")) {
+			itemName = entity.get("name").replace("_", " ");
+		} else if (entity instanceof Item) {
+			itemName = "item";
+		}
+		
+		source.moveTo(dest, player);
+		if (entity.has("bound")) {
+			player.sendPrivateText("You put a valuable item on the ground. Please note that it will expire in " + (Item.DEGRADATION_TIMEOUT / 60) + " minutes, as all items do. But in this case there is no way to restore it.");
+		}
+		int amount = 1;
+		if (entity instanceof StackableItem) {
+			amount = ((StackableItem) entity).getQuantity();
+		}
+		StendhalRPRuleProcessor.get().addGameEvent(player.getName(), "drop", itemName, source.getSlot(), dest.getSlot(), Integer.toString(amount));
 		player.updateItemAtkDef();
 
 		Log4J.finishMethod(logger, "drop");
