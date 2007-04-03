007f87ad78ebf8c01d57f75960dbc79df612172f
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-04-03 22:05:25 +0000

    implemented isReachableBy

--- src/games/stendhal/server/entity/slot/BankSlot.java
+++ src/games/stendhal/server/entity/slot/BankSlot.java
@@ -2,12 +2,15 @@ package games.stendhal.server.entity.slot;
 
 import games.stendhal.server.entity.Entity;
 
+import java.util.List;
+
 /**
  * a bank slot
  *
  * @author hendrik
  */
 public class BankSlot extends PlayerSlot {
+	private Banks bank = null;
 
 	/**
 	 * creates a new keyed slot
@@ -16,16 +19,28 @@ public class BankSlot extends PlayerSlot {
 	 */
 	public BankSlot(Banks bank) {
 		super(bank.getSlotName());
+		this.bank = bank;
 	}
 
 	@Override
 	public boolean isReachableBy(Entity entity) {
-		// TODO: Check if we are next to a chest which acts as an interface
-		//       to this bank slot
+		// Check if we are next to a chest which acts as an interface
+		// to this bank slot
+		List<Entity> accessors = BankAccessorManager.get().get(bank);
 		boolean found = false;
+		for (Entity accessor : accessors) {
+			if (entity.nextTo(accessor)) {
+				found = true;
+				break;
+			}
+		}
+
 		if (!found) {
+			// sorry, we are not near a personal chest
 			return false;
 		}
+
+		// now check that it is the slot of the right player
 		return super.isReachableBy(entity);
 	}
 
