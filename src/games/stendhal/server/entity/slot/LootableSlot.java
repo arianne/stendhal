496553694004d5577a01992f65ba2069d8bded3e
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-04-04 20:15:55 +0000

    intial checkin of an LootableSlot (used by corpse)

---
+++ src/games/stendhal/server/entity/slot/LootableSlot.java
@@ -0,0 +1,28 @@
+package games.stendhal.server.entity.slot;
+
+import games.stendhal.server.entity.Entity;
+
+/**
+ * a lootable slot of some creature
+ *
+ * @author hendrik
+ */
+public class LootableSlot extends PlayerSlot {
+	private Entity owner = null;
+
+	/**
+	 * creates a new lootable slot
+	 *
+	 * @param owner owner of this Slot
+	 */
+	public LootableSlot(Entity owner) {
+		super("content");
+		this.owner = owner;
+	}
+
+	@Override
+	public boolean isReachableBy(Entity entity) {
+		return entity.nextTo(owner);
+	}
+	
+}
