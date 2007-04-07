5c6623f443041a605ea864afcb041a851a9151a8
timothyb89 <timothyb89@users.sourceforge.net>
2007-04-07 21:17:21 +0000

    Spell2DView based off of Item2DView

---
+++ src/games/stendhal/client/entity/Spell2DView.java
@@ -0,0 +1,58 @@
+/*
+ * @(#) games/stendhal/client/entity/Item2DView.java
+ *
+ * $Id$
+ */
+package games.stendhal.client.entity;
+
+//
+//
+
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+
+import games.stendhal.client.GameScreen;
+
+//
+//
+
+/**
+ * The 2D view of an item.
+ */
+public class Spell2DView extends Entity2DView {
+	/**
+	 * Create a 2D view of an item.
+	 *
+	 * @param	entity		The entity to render.
+	 */
+	public Spell2DView(final Spell spell) {
+		super(spell);
+	}
+
+
+	//
+	// Entity2DView
+	//
+
+	/**
+	 * Get the 2D area that is drawn in.
+	 *
+	 * @return	The 2D area this draws in.
+	 */
+	public Rectangle2D getDrawnArea() {
+		return new Rectangle.Double(getX(), getY(), 1.0, 1.0);
+        }
+
+	/**
+	 * Determines on top of which other entities this entity should be
+	 * drawn. Entities with a high Z index will be drawn on top of ones
+	 * with a lower Z index.
+	 * 
+	 * Also, players can only interact with the topmost entity.
+	 * 
+	 * @return	The drawing index.
+	 */
+	public int getZIndex() {
+		return 7000;
+	}
+}
