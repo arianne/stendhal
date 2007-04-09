f44d2102e668251bc09be49c23d7ea071278b5b3
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-04-09 09:59:16 +0000

    create fire

--- src/games/stendhal/server/script/Cataclysm.java
+++ src/games/stendhal/server/script/Cataclysm.java
@@ -4,6 +4,7 @@ package games.stendhal.server.script;
 import games.stendhal.common.Rand;
 import games.stendhal.server.StendhalRPWorld;
 import games.stendhal.server.StendhalRPZone;
+import games.stendhal.server.entity.Fire;
 import games.stendhal.server.entity.npc.ConversationPhrases;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.NPC;
@@ -26,6 +27,7 @@ public class Cataclysm extends ScriptImpl {
 	    
 	    reduceNPCsHP();
 	    addGreeting();
+	    setOnFire();
     }
 
 	private void reduceNPCsHP() {
@@ -48,6 +50,25 @@ public class Cataclysm extends ScriptImpl {
 	    	}
 	    }
     }
+
+	private void createFire(StendhalRPZone zone, int x, int y, int width, int height) {
+		Fire fire = new Fire(width, height);
+		zone.assignRPObjectID(fire);
+		fire.set(x, y);
+		zone.add(fire);
+	}
+	
+	private void setOnFire() {
+	    StendhalRPZone zone = (StendhalRPZone) StendhalRPWorld.get().getRPZone("0_semos_city");
+	    createFire(zone, 10, 10, 1, 1);
+	    createFire(zone, 15, 15, 2, 1);
+	    createFire(zone, 20, 20, 3, 1);
+	    createFire(zone, 25, 25, 1, 2);
+	    createFire(zone, 30, 30, 1, 3);
+	    createFire(zone, 35, 35, 2, 2);
+	    createFire(zone, 40, 40, 3, 3);
+	    createFire(zone, 45, 45, 4, 4);
+	}
 	
 	// TODO: remove HealerBehaviour from Carmen
 	// TODO: Change Greetings of the SpeakerNPCs in Semos
