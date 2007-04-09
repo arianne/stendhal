23953eadc128f458defbb08061bdbbe0212bde50
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-04-09 10:04:51 +0000

    made HearingAria a static utility class

--- src/games/stendhal/client/soundreview/HearingArea.java
+++ src/games/stendhal/client/soundreview/HearingArea.java
@@ -8,55 +8,43 @@ import java.awt.geom.Rectangle2D;
  * @author astrid
  *
  */
-public class HearingArea {
+public abstract class HearingArea {
 
 	public static final int HEARINGDIST = 20;
 
-	private int left;
+	private static int left;
 
-	private int right;
+	private static int right;
 
-	private int lower;
+	private static int lower;
 
-	private int upper;
+	private static int upper;
 
-	private static HearingArea instance = new HearingArea(0, 0);
-
-	public static HearingArea get() {
-		return instance;
-	}
-
-	private HearingArea(int x, int y) {
-		instance = this;
-		set(x, y);
-
-	}
-
-	public void set(int x, int y) {
+	public static void set(int x, int y) {
 		left = x - HEARINGDIST;
 		right = x + HEARINGDIST;
 		upper = y - HEARINGDIST;
 		lower = y + HEARINGDIST;
 	}
 
-	public boolean contains(int x, int y) {
+	public static  boolean contains(int x, int y) {
 		if (left < x ? x < right : false) {
 			return (upper < y ? y < lower : false);
 		}
 		return false;
 	}
 
-	public void moveTo(int x, int y) {
+	public static void moveTo(int x, int y) {
 		set(x, y);
 
 	}
 
-	public void set(double x, double y) {
+	public static void set(double x, double y) {
 		set((int) x, (int) y);
 
 	}
 
-	public Rectangle2D getAsRect() {
+	public static Rectangle2D getAsRect() {
 		return new Rectangle2D.Double(left, upper, 2 * HEARINGDIST, 2 * HEARINGDIST);
 	}
 
--- tests/games/stendhal/client/soundreview/HearingAreaTest.java
+++ tests/games/stendhal/client/soundreview/HearingAreaTest.java
@@ -11,53 +11,49 @@ import org.junit.Test;
 
 public class HearingAreaTest {
 
-	HearingArea area;
 
 	@Before
 	public void setUp() throws Exception {
-		area =  HearingArea.get();
-		area.set(0,0);
+		
+		HearingArea.set(0,0);
 	}
 
 	@Test
 	public void contains() {
-		assertTrue("both in ", area.contains(0, 0));
-		assertFalse("both out", area.contains(100, 100));
-		assertFalse("edge does not belong", area.contains(-HearingArea.HEARINGDIST, -HearingArea.HEARINGDIST));
-		assertFalse("edge does not belong", area.contains(HearingArea.HEARINGDIST, HearingArea.HEARINGDIST));
-		assertFalse("edge does not belong", area.contains(-HearingArea.HEARINGDIST, HearingArea.HEARINGDIST));
-		assertFalse("edge does not belong", area.contains(HearingArea.HEARINGDIST, -HearingArea.HEARINGDIST));
-		assertTrue("inner edge belongs", area.contains(-(HearingArea.HEARINGDIST - 1), -(HearingArea.HEARINGDIST - 1)));
-		assertTrue("inner edge belongs", area.contains((HearingArea.HEARINGDIST - 1), (HearingArea.HEARINGDIST - 1)));
-		assertTrue("inner edge belongs", area.contains(-(HearingArea.HEARINGDIST - 1), (HearingArea.HEARINGDIST - 1)));
-		assertTrue("inner edge belongs", area.contains((HearingArea.HEARINGDIST - 1), -(HearingArea.HEARINGDIST - 1)));
-		assertFalse("x in , y out", area.contains(0, 100));
-		assertFalse("x out y in", area.contains(100, 10));
+		assertTrue("both in ", HearingArea.contains(0, 0));
+		assertFalse("both out", HearingArea.contains(100, 100));
+		assertFalse("edge does not belong", HearingArea.contains(-HearingArea.HEARINGDIST, -HearingArea.HEARINGDIST));
+		assertFalse("edge does not belong", HearingArea.contains(HearingArea.HEARINGDIST, HearingArea.HEARINGDIST));
+		assertFalse("edge does not belong", HearingArea.contains(-HearingArea.HEARINGDIST, HearingArea.HEARINGDIST));
+		assertFalse("edge does not belong", HearingArea.contains(HearingArea.HEARINGDIST, -HearingArea.HEARINGDIST));
+		assertTrue("inner edge belongs", HearingArea.contains(-(HearingArea.HEARINGDIST - 1), -(HearingArea.HEARINGDIST - 1)));
+		assertTrue("inner edge belongs", HearingArea.contains((HearingArea.HEARINGDIST - 1), (HearingArea.HEARINGDIST - 1)));
+		assertTrue("inner edge belongs", HearingArea.contains(-(HearingArea.HEARINGDIST - 1), (HearingArea.HEARINGDIST - 1)));
+		assertTrue("inner edge belongs", HearingArea.contains((HearingArea.HEARINGDIST - 1), -(HearingArea.HEARINGDIST - 1)));
+		assertFalse("x in , y out", HearingArea.contains(0, 100));
+		assertFalse("x out y in", HearingArea.contains(100, 10));
 	}
 
 	@Test
 	public void move() {
-		assertTrue("both in ", area.contains(0, 0));
-		area.moveTo(20, 20);
-		assertFalse(" edge does not belong", area.contains(0, 0));
-		assertFalse(" edge does not belong", area.contains(40, 40));
-		assertFalse(" edge does not belong", area.contains(0, 40));
-		assertFalse(" edge does not belong", area.contains(40, 0));
-		assertFalse("both out", area.contains(100, 100));
-		assertTrue("both in", area.contains(20, 20));
+		assertTrue("both in ", HearingArea.contains(0, 0));
+		HearingArea.moveTo(20, 20);
+		assertFalse(" edge does not belong", HearingArea.contains(0, 0));
+		assertFalse(" edge does not belong", HearingArea.contains(40, 40));
+		assertFalse(" edge does not belong", HearingArea.contains(0, 40));
+		assertFalse(" edge does not belong", HearingArea.contains(40, 0));
+		assertFalse("both out", HearingArea.contains(100, 100));
+		assertTrue("both in", HearingArea.contains(20, 20));
 	}
 
-	@Test
-	public void get() {
-		assertTrue("instance is object", area == HearingArea.get());
-	}
+	
 
 	@Test
 	public void getAsArea() {
 		int centerX = 1;
 		int centerY = 5;
-		area.set(centerX, centerY);
-		Rectangle2D rect = area.getAsRect();
+		HearingArea.set(centerX, centerY);
+		Rectangle2D rect = HearingArea.getAsRect();
 		assertEquals(-HearingArea.HEARINGDIST + centerX, (int) rect.getMinX());
 		assertEquals(-HearingArea.HEARINGDIST + centerY, (int) rect.getMinY());
 		assertEquals(HearingArea.HEARINGDIST + centerX, (int) rect.getMaxX());
