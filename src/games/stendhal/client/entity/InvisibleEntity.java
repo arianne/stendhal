a6f9f998283005cd531e9a4940622ec1dcfaea65
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-04-10 21:02:43 +0000

    client side support for larger invisible entities

--- src/games/stendhal/client/entity/InvisibleEntity.java
+++ src/games/stendhal/client/entity/InvisibleEntity.java
@@ -12,13 +12,36 @@ package games.stendhal.client.entity;
 import java.awt.Rectangle;
 import java.awt.geom.Rectangle2D;
 
+import marauroa.common.game.RPObject;
+
 /**
  * An generic entity that is not drawn.
  */
 public class InvisibleEntity extends Entity {
+	int height = 1;
+	int width = 1;
+	
+	
+	
+	@Override
+	public void onChangedAdded(RPObject base, RPObject diff) {
+		super.onChangedAdded(base, diff);
+		if (diff.has("width")) {
+			width = diff.getInt("width");
+		} else if (base.has("width")) {
+			width = base.getInt("width");
+		}
+		if (diff.has("height")) {
+			height = diff.getInt("height");
+		} else if (base.has("height")) {
+			height = base.getInt("height");
+		}
+	}
+
+
 	@Override
 	public Rectangle2D getArea() {
-		return new Rectangle.Double(x, y, 1, 1);
+		return new Rectangle.Double(x, y, width, height);
 	}
 
 
