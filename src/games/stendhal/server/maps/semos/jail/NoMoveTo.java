547a08007883c9431d2b8dffe8a4680add89a79f
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-04-11 20:28:55 +0000

    disable moveto (using mouse and pathfinding) so that the player has to walk around all the obstacles

---
+++ src/games/stendhal/server/maps/semos/jail/NoMoveTo.java
@@ -0,0 +1,35 @@
+package games.stendhal.server.maps.semos.jail;
+
+import games.stendhal.server.Jail;
+import games.stendhal.server.StendhalRPZone;
+import games.stendhal.server.entity.npc.ConversationPhrases;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.NPCList;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.pathfinder.Path;
+import games.stendhal.server.maps.ZoneConfigurator;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Semos Jail - Level -3
+ * 
+ * @author hendrik
+ */
+public class NoMoveTo implements ZoneConfigurator {
+
+
+	/**
+	 * Configure a zone.
+	 *
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+	public void configureZone(StendhalRPZone zone, Map<String, String> attributes) {
+		zone.setTeleportable(false);
+		zone.setEnableMoveTo(false);
+	}
+}
