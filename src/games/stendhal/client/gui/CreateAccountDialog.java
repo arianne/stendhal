e3013c4141740785a51979f0fbd2bcfe64a3a12e
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-04-12 21:45:11 +0000

    fixed size of port window

--- src/games/stendhal/client/gui/CreateAccountDialog.java
+++ src/games/stendhal/client/gui/CreateAccountDialog.java
@@ -140,7 +140,7 @@ public class CreateAccountDialog extends JDialog {
 		contentPane.add(serverPortLabel, c);
 		c.gridx = 1;
 		c.gridy = 1;
-		c.insets = new Insets(4, 4, 4, 150);
+		c.insets = new Insets(4, 4, 4, 4);
 		c.fill = GridBagConstraints.BOTH;
 		contentPane.add(serverPortField, c);
 
