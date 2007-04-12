d582b912cec5740de40ca8109d9e6e6f166cbb73
Daniel Herding <wikipedian@users.sourceforge.net>
2007-04-12 10:29:52 +0000

    cleaned up code, renamed identifiers, fixed docu

--- src/games/stendhal/server/maps/quests/KanmararnSoldiers.java
+++ src/games/stendhal/server/maps/quests/KanmararnSoldiers.java
@@ -39,17 +39,22 @@ import marauroa.common.game.SlotIsFullException;
  *
  * STEPS:
  *  - optional: speak to Sergeant James to get the task to find the map
- *  - talk to Henry to get the task to find some prove that the other 3 soldiers are dead.
- *  - collect the item in each of the corpse of the three other soldiers
- *  - bring them back to Herny to get the map
+ *  - talk to Henry to get the task to find some proof that the other 3 soldiers are dead.
+ *  - collect the item in each of the corpses of the three other soldiers
+ *  - bring them back to Henry to get the map
  *  - bring the map to Sergeant James 
  *
  * REWARD:
- *  - IOU (for quest MCPeglegIOU.java)
- *  - steel boots
+ *  - from Henry:
+ *  	- you can keep the IOU paper (for quest MCPeglegIOU)
+ *      - 2500 XP
+ *  - from Sergeant James
+ *      - steel boots
  *
  * REPETITIONS:
  * - None.
+ * 
+ * @see McPeglegIOU
  */
 public class KanmararnSoldiers extends AbstractQuest {
 
@@ -124,42 +129,42 @@ public class KanmararnSoldiers extends AbstractQuest {
 	}
 
 	private class HenryQuestAction extends SpeakerNPC.ChatAction {
-		public void fire(Player player, String text, SpeakerNPC engine) {
-			if(!player.isQuestCompleted("soldier_henry") && !"map".equals(player.getQuest("soldier_henry"))) {
-				engine.say("Find my #group, Peter, Tom and Charles, prove it and I will reward you. Will you do it?");
+		public void fire(Player player, String text, SpeakerNPC npc) {
+			if(!player.isQuestCompleted(QUEST_SLOT) && !"map".equals(player.getQuest(QUEST_SLOT))) {
+				npc.say("Find my #group, Peter, Tom, and Charles, prove it and I will reward you. Will you do it?");
 			} else {
-				engine.say("I'm so sad that most of my friends are dead.");
-				engine.setCurrentState(1);
+				npc.say("I'm so sad that most of my friends are dead.");
+				npc.setCurrentState(ConversationStates.ATTENDING);
 			}
 		}
 	}
 
 	private class HenryQuestAcceptAction extends SpeakerNPC.ChatAction {
-		public void fire(Player player, String text, SpeakerNPC engine) {
-			player.setQuest("soldier_henry","start");
+		public void fire(Player player, String text, SpeakerNPC npc) {
+			player.setQuest(QUEST_SLOT, "start");
 		}
 	}
 
 	private class HenryQuestStartedCondition extends SpeakerNPC.ChatCondition {
-		public boolean fire(Player player, String text, SpeakerNPC engine) {
-			return (player.hasQuest("soldier_henry") && player.getQuest("soldier_henry").equals("start"));
+		public boolean fire(Player player, String text, SpeakerNPC npc) {
+			return (player.hasQuest(QUEST_SLOT) && player.getQuest(QUEST_SLOT).equals("start"));
 		}
 	}
 
 	private class HenryQuestNotCompletedCondition extends SpeakerNPC.ChatCondition {
-		public boolean fire(Player player, String text, SpeakerNPC engine) {
-			return (!player.hasQuest("soldier_henry") || player.getQuest("soldier_henry").equals("start"));
+		public boolean fire(Player player, String text, SpeakerNPC npc) {
+			return (!player.hasQuest(QUEST_SLOT) || player.getQuest(QUEST_SLOT).equals("start"));
 		}
 	}
 
 	private class HenryQuestCompletedCondition extends SpeakerNPC.ChatCondition {
-		public boolean fire(Player player, String text, SpeakerNPC engine) {
-			return (player.hasQuest("soldier_henry") && !player.getQuest("soldier_henry").equals("start"));
+		public boolean fire(Player player, String text, SpeakerNPC npc) {
+			return (player.hasQuest(QUEST_SLOT) && !player.getQuest(QUEST_SLOT).equals("start"));
 		}
 	}
 
 	private class HenryQuestCompleteAction extends SpeakerNPC.ChatAction {
-		public void fire(Player player, String text, SpeakerNPC engine) {
+		public void fire(Player player, String text, SpeakerNPC npc) {
 
 			List<Item> allLeatherLegs = player.getAllEquipped("leather_legs");
 			Item questLeatherLegs = null;
@@ -189,36 +194,37 @@ public class KanmararnSoldiers extends AbstractQuest {
 			}
 
 			if((questLeatherLegs != null) && (questNote != null) && (questScaleArmor != null)) {
-				engine.say("Oh my! Peter, Tom and Charles are all dead? *cries*. Anyway, here is your reward. And keep the IOU.");
+				npc.say("Oh my! Peter, Tom, and Charles are all dead? *cries*. Anyway, here is your reward. And keep the IOU.");
 				player.addXP(2500);
 				player.drop(questLeatherLegs);
 				player.drop(questScaleArmor);
-				Item map = StendhalRPWorld.get().getRuleManager().getEntityManager().getItem("map");
-				map.put("infostring", engine.get("name"));
+				Item map = StendhalRPWorld.get().getRuleManager().
+						getEntityManager().getItem("map");
+				map.put("infostring", npc.get("name"));
 				map.setDescription("You see a hand drawn map, but no matter how you look at it, nothing on it looks familiar.");
 				player.equip(map);
-				player.setQuest("soldier_henry","map");
-				engine.setCurrentState(1);
+				player.setQuest(QUEST_SLOT, "map");
+				npc.setCurrentState(ConversationStates.ATTENDING);
 			} else {
-				engine.say("You didn't prove that you have found them all!");
+				npc.say("You didn't prove that you have found them all!");
 			}
 		}
 	}
 
 	private class JamesQuestCompleteCondition extends SpeakerNPC.ChatCondition {
-		public boolean fire(Player player, String text, SpeakerNPC engine) {
-			return (player.hasQuest("soldier_henry") && player.getQuest("soldier_henry").equals("map"));
+		public boolean fire(Player player, String text, SpeakerNPC npc) {
+			return (player.hasQuest(QUEST_SLOT) && player.getQuest(QUEST_SLOT).equals("map"));
 		}
 	}
 
 	private class JamesQuestCompletedCondition extends SpeakerNPC.ChatCondition {
-		public boolean fire(Player player, String text, SpeakerNPC engine) {
-			return (player.isQuestCompleted("soldier_henry"));
+		public boolean fire(Player player, String text, SpeakerNPC npc) {
+			return (player.isQuestCompleted(QUEST_SLOT));
 		}
 	}
 
 	private class JamesQuestCompleteAction extends SpeakerNPC.ChatAction {
-		public void fire(Player player, String text, SpeakerNPC engine) {
+		public void fire(Player player, String text, SpeakerNPC npc) {
 		
 			List<Item> allMaps = player.getAllEquipped("map");
 			Item questMap = null;
@@ -229,20 +235,20 @@ public class KanmararnSoldiers extends AbstractQuest {
 				}
 			}
 			if (questMap != null) {
-				engine.say("The map! Wonderful! Thank you. And here is your reward.");
+				npc.say("The map! Wonderful! Thank you. And here is your reward.");
 				player.addXP(5000);
 				player.drop(questMap);
 				
-				Item item = StendhalRPWorld.get()
-				.getRuleManager().getEntityManager().getItem(
-						"steel_boots");
+				Item item = StendhalRPWorld.get().getRuleManager().
+						getEntityManager().getItem("steel_boots");
 				item.put("bound", player.getName());
-				item.put("infostring", engine.get("name"));
+				// Is this infostring really needed?
+				item.put("infostring", npc.get("name"));
 				player.equip(item);
-				player.setQuest("soldier_henry","done");
-				engine.setCurrentState(1);
+				player.setQuest(QUEST_SLOT, "done");
+				npc.setCurrentState(ConversationStates.ATTENDING);
 			} else {
-				engine.say("Well, where is the map?");
+				npc.say("Well, where is the map?");
 			}
 		}
 	}
@@ -252,13 +258,54 @@ public class KanmararnSoldiers extends AbstractQuest {
 	 */
 	private void step_1() {
 		SpeakerNPC henry = npcs.get("Henry");
-		henry.add(ConversationStates.ATTENDING, Arrays.asList("quest", "task"), null, ConversationStates.QUEST_OFFERED, null, new HenryQuestAction());
-		henry.add(ConversationStates.QUEST_OFFERED, ConversationPhrases.YES_MESSAGES,null, ConversationStates.ATTENDING, "Thank you! I'll be waiting for your return.", new HenryQuestAcceptAction());
-		henry.add(ConversationStates.QUEST_OFFERED, "group", null, ConversationStates.QUEST_OFFERED, "The General sent five of us to explore this area in search for #treasure.", null);
-		henry.add(ConversationStates.QUEST_OFFERED, "no", null, ConversationStates.ATTENDING, "Ok. I understand. I'm scared of the #dwarves myself.", null);
-		henry.add(ConversationStates.IDLE, ConversationPhrases.GREETING_MESSAGES, new HenryQuestStartedCondition(), ConversationStates.ATTENDING, null, new HenryQuestCompleteAction());
-		henry.add(ConversationStates.ATTENDING, Arrays.asList("map", "group", "help"), new HenryQuestCompletedCondition(), ConversationStates.ATTENDING, "I'm so sad that most of my friends are dead.", null);
-		henry.add(ConversationStates.ATTENDING, Arrays.asList("map"), new HenryQuestNotCompletedCondition(), ConversationStates.ATTENDING, "If you find my friends, i will give you the map", null);
+		henry.add(ConversationStates.ATTENDING,
+				Arrays.asList("quest", "task"),
+				null,
+				ConversationStates.QUEST_OFFERED,
+				null,
+				new HenryQuestAction());
+		
+		henry.add(ConversationStates.QUEST_OFFERED,
+				ConversationPhrases.YES_MESSAGES,
+				null,
+				ConversationStates.ATTENDING,
+				"Thank you! I'll be waiting for your return.",
+				new HenryQuestAcceptAction());
+		
+		henry.add(ConversationStates.QUEST_OFFERED,
+				"group",
+				null,
+				ConversationStates.QUEST_OFFERED,
+				"The General sent five of us to explore this area in search for #treasure.",
+				null);
+
+		henry.add(ConversationStates.QUEST_OFFERED,
+				ConversationPhrases.NO_MESSAGES,
+				null,
+				ConversationStates.ATTENDING,
+				"OK. I understand. I'm scared of the #dwarves myself.",
+				null);
+		
+		henry.add(ConversationStates.IDLE,
+				ConversationPhrases.GREETING_MESSAGES,
+				new HenryQuestStartedCondition(),
+				ConversationStates.ATTENDING,
+				null,
+				new HenryQuestCompleteAction());
+		
+		henry.add(ConversationStates.ATTENDING,
+				Arrays.asList("map", "group", "help"),
+				new HenryQuestCompletedCondition(),
+				ConversationStates.ATTENDING,
+				"I'm so sad that most of my friends are dead.",
+				null);
+		
+		henry.add(ConversationStates.ATTENDING,
+				Arrays.asList("map"),
+				new HenryQuestNotCompletedCondition(),
+				ConversationStates.ATTENDING,
+				"If you find my friends, i will give you the map",
+				null);
 	}
 
 	/**
@@ -278,7 +325,9 @@ public class KanmararnSoldiers extends AbstractQuest {
 		zone.add(tom);
 
 		// Add a refiller to automatically fill the corpse of unlucky Tom
-		CorpseRefiller tomRefiller = new CorpseRefiller(tom, "leather_legs", "You see torn leather legs that are heavily covered with blood.");
+		CorpseRefiller tomRefiller = new CorpseRefiller(tom,
+				"leather_legs",
+				"You see torn leather legs that are heavily covered with blood.");
 		tomRefiller.start();
 
 		// Now we create the corpse of the third NPC
@@ -291,7 +340,9 @@ public class KanmararnSoldiers extends AbstractQuest {
 		zone.assignRPObjectID(charles);
 		zone.add(charles);
 		// Add a refiller to automatically fill the corpse of unlucky Charles
-		CorpseRefiller charlesRefiller = new CorpseRefiller(charles, "note", "You read: \"IOU 250 gold. (signed) McPegleg\"");
+		CorpseRefiller charlesRefiller = new CorpseRefiller(charles,
+				"note",
+				"You read: \"IOU 250 gold. (signed) McPegleg\"");
 		charlesRefiller.start();
 
 		// Now we create the corpse of the fourth NPC
@@ -304,7 +355,9 @@ public class KanmararnSoldiers extends AbstractQuest {
 		zone.assignRPObjectID(peter);
 		zone.add(peter);
 		// Add a refiller to automatically fill the corpse of unlucky Peter
-		CorpseRefiller peterRefiller = new CorpseRefiller(peter, "scale_armor", "You see a slightly rusty scale armor. It is heavily deformed by several strong hammer blows.");
+		CorpseRefiller peterRefiller = new CorpseRefiller(peter,
+				"scale_armor",
+				"You see a slightly rusty scale armor. It is heavily deformed by several strong hammer blows.");
 		peterRefiller.start();
 	}
 
@@ -317,16 +370,34 @@ public class KanmararnSoldiers extends AbstractQuest {
 		// quest related stuff
 		james.addHelp("Think I need a little help myself. My #group got killed and #one of my men ran away. Too bad he had the #map.");
 		james.addQuest("Find my fugitive soldier and bring him to me ... or at least the #map he's carrying.");
-		james.add(ConversationStates.ATTENDING, Arrays.asList("group"), ConversationStates.ATTENDING, "We were five, three of us died. You probably passed their corpses.", null);
-		james.add(ConversationStates.ATTENDING, Arrays.asList("one", "henry"), ConversationStates.ATTENDING, "Yes, my youngest soldier. He ran away.", null);
-		james.add(ConversationStates.ATTENDING, Arrays.asList("map"), ConversationStates.ATTENDING, "The #treasure map that leads into the heart of the #dwarven #kingdom.", null);
-		james.add(ConversationStates.ATTENDING, Arrays.asList("treasure"), ConversationStates.ATTENDING, "A big treasure is rumored to be somewhere in this dungeon.", null);
-		james.add(ConversationStates.ATTENDING, Arrays.asList("dwarf", "dwarves", "dwarven"), ConversationStates.ATTENDING, "They are strong enemies! We're in their #kingdom.", null);
-		james.add(ConversationStates.ATTENDING, Arrays.asList("peter", "tom", "charles"), ConversationStates.ATTENDING, "He was a good soldier and fought bravely.", null);
-		james.add(ConversationStates.ATTENDING, Arrays.asList("kingdom", "Kanmararn"), ConversationStates.ATTENDING, "Kanmararn, the legendary kingdom of the #dwarves.", null);
+		james.addReply("group",
+				"We were five, three of us died. You probably passed their corpses.");
+		james.addReply(Arrays.asList("one", "henry"),
+				"Yes, my youngest soldier. He ran away.");
+		james.addReply("map",
+				"The #treasure map that leads into the heart of the #dwarven #kingdom.");
+		james.addReply("treasure",
+				"A big treasure is rumored to be somewhere in this dungeon.");
+		james.addReply(Arrays.asList("dwarf", "dwarves", "dwarven"),
+				"They are strong enemies! We're in their #kingdom.");
+		james.addReply(Arrays.asList("peter", "tom", "charles"),
+				"He was a good soldier and fought bravely.");
+		james.addReply(Arrays.asList("kingdom", "kanmararn"),
+				"Kanmararn, the legendary kingdom of the #dwarves.");
 
-		james.add(ConversationStates.ATTENDING, Arrays.asList("map", "henry"), new JamesQuestCompleteCondition(), 1, null, new JamesQuestCompleteAction());
-		james.add(ConversationStates.ATTENDING, Arrays.asList("map", "henry", "quest", "task", "help", "group", "one"), new JamesQuestCompletedCondition(), ConversationStates.ATTENDING, "Thanks again for bringing me the map!", null);
+		james.add(ConversationStates.ATTENDING,
+				Arrays.asList("map", "henry"),
+				new JamesQuestCompleteCondition(),
+				ConversationStates.ATTENDING,
+				null,
+				new JamesQuestCompleteAction());
+		
+		james.add(ConversationStates.ATTENDING,
+				Arrays.asList("map", "henry", "quest", "task", "help", "group", "one"),
+				new JamesQuestCompletedCondition(),
+				ConversationStates.ATTENDING,
+				"Thanks again for bringing me the map!",
+				null);
 	}
 
 	@Override
