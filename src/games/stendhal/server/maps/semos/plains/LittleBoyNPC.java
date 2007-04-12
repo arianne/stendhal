0231809cf40a4ecf39d9aa40274d563968e39e69
Daniel Herding <wikipedian@users.sourceforge.net>
2007-04-12 12:21:19 +0000

    moved Plink to XML

--- src/games/stendhal/server/maps/semos/plains/LittleBoyNPC.java
+++ src/games/stendhal/server/maps/semos/plains/LittleBoyNPC.java
@@ -1,67 +1,21 @@
 package games.stendhal.server.maps.semos.plains;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
-import games.stendhal.server.StendhalRPZone;
-import games.stendhal.server.entity.npc.NPCList;
 import games.stendhal.server.entity.npc.SpeakerNPC;
-import games.stendhal.server.maps.ZoneConfigurator;
-import games.stendhal.server.pathfinder.Path;
+import games.stendhal.server.entity.npc.SpeakerNPCFactory;
 
-public class LittleBoyNPC implements ZoneConfigurator {
+/**
+ * A little boy who lives at a farm.
+ * 
+ * @see games.stendhal.server.maps.quests.PlinksToy
+ */
+public class LittleBoyNPC extends SpeakerNPCFactory {
 
-	private NPCList npcs = NPCList.get();
-
-	/**
-	 * Configure a zone.
-	 *
-	 * @param	zone		The zone to be configured.
-	 * @param	attributes	Configuration attributes.
-	 */
-	public void configureZone(StendhalRPZone zone, Map<String, String> attributes) {
-		buildSemosNorthPlainsArea(zone);
-	}
-
-	private void buildSemosNorthPlainsArea(StendhalRPZone zone) {
-		SpeakerNPC npc = new SpeakerNPC("Plink") {
-
-			@Override
-			protected void createPath() {
-				List<Path.Node> nodes = new LinkedList<Path.Node>();
-				nodes.add(new Path.Node(36, 108));
-				nodes.add(new Path.Node(37, 108));
-				nodes.add(new Path.Node(37, 105));
-				nodes.add(new Path.Node(42, 105));
-				nodes.add(new Path.Node(42, 111));
-				nodes.add(new Path.Node(48, 111));
-				nodes.add(new Path.Node(47, 103));
-				nodes.add(new Path.Node(47, 100));
-				nodes.add(new Path.Node(53, 100));
-				nodes.add(new Path.Node(53, 90));
-				nodes.add(new Path.Node(49, 90));
-				nodes.add(new Path.Node(49, 98));
-				nodes.add(new Path.Node(46, 98));
-				nodes.add(new Path.Node(46, 99));
-				nodes.add(new Path.Node(36, 99));
-
-				setPath(nodes, true);
-			}
-
-			@Override
-			protected void createDialog() {
-				addGreeting();
-				addJob("I play all day.");
-				addHelp("Be careful out east, there are wolves about!");
-				addGoodbye();
-			}
-		};
-		npcs.add(npc);
-		zone.assignRPObjectID(npc);
-		npc.put("class", "plinknpc");
-		npc.set(36, 108);
-		npc.initHP(100);
-		zone.add(npc);
+	@Override
+	protected void createDialog(SpeakerNPC npc) {
+		// NOTE: These texts are only available after finishing the quest.
+		npc.addGreeting();
+		npc.addJob("I play all day.");
+		npc.addHelp("Be careful out east, there are wolves about!");
+		npc.addGoodbye();
 	}
 }
