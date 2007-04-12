0b4af42ae45bc499cab4bc3cf1ccaf1b11befc10
Daniel Herding <wikipedian@users.sourceforge.net>
2007-04-12 14:56:18 +0000

    moved 3 NPCs to XML

--- src/games/stendhal/server/maps/semos/blacksmith/BlacksmithNPC.java
+++ src/games/stendhal/server/maps/semos/blacksmith/BlacksmithNPC.java
@@ -1,106 +1,54 @@
 package games.stendhal.server.maps.semos.blacksmith;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
-import games.stendhal.server.StendhalRPZone;
-import games.stendhal.server.entity.npc.ConversationStates;
-import games.stendhal.server.entity.npc.NPCList;
 import games.stendhal.server.entity.npc.ProducerBehaviour;
 import games.stendhal.server.entity.npc.SellerBehaviour;
 import games.stendhal.server.entity.npc.ShopList;
 import games.stendhal.server.entity.npc.SpeakerNPC;
-import games.stendhal.server.maps.ZoneConfigurator;
-import games.stendhal.server.pathfinder.Path;
+import games.stendhal.server.entity.npc.SpeakerNPCFactory;
 
-public class BlacksmithNPC implements ZoneConfigurator {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
-	private NPCList npcs = NPCList.get();
+/**
+ * The blacksmith (original name: Xoderos). Brother of the goldsmith in Ados.
+ * He refuses to sell weapons, but he casts iron for the player, and he sells
+ * tools.
+ * 
+ * @author daniel
+ * 
+ * @see games.stendhal.server.maps.quests.HungryJoshua
+ */
+public class BlacksmithNPC extends SpeakerNPCFactory {
 
-	/**
-	 * Configure a zone.
-	 *
-	 * @param	zone		The zone to be configured.
-	 * @param	attributes	Configuration attributes.
-	 */
-	public void configureZone(StendhalRPZone zone, Map<String, String> attributes) {
-		buildSemosBlacksmithArea(zone, attributes);
-	}
+	@Override
+	protected void createDialog(SpeakerNPC npc) {
+		npc.addReply("wood",
+		        "I need some wood to keep my furnace lit. You can find any amount of it just lying around in the forest.");
 
-	private void buildSemosBlacksmithArea(StendhalRPZone zone, Map<String, String> attributes) {
-		SpeakerNPC xoderos = new SpeakerNPC("Xoderos") {
+		npc.addReply(Arrays.asList("ore", "iron", "iron_ore"),
+				"You can find iron ore up in the mountains west of Or'ril, near the dwarf mines. Be careful up there!");
 
-			@Override
-			protected void createPath() {
-				List<Path.Node> nodes = new LinkedList<Path.Node>();
-				nodes.add(new Path.Node(23, 11));
-				nodes.add(new Path.Node(29, 11));
-				nodes.add(new Path.Node(29, 4));
-				nodes.add(new Path.Node(17, 4));
-				nodes.add(new Path.Node(17, 8));
-				nodes.add(new Path.Node(28, 8));
-				nodes.add(new Path.Node(28, 11));
-				setPath(nodes, true);
-			}
+		npc.addReply("gold_pan",
+		        "With this tool you are able to prospect for gold. In the south there is a lake near a waterfall where I once found a #gold_nugget. Maybe you are lucky as well.");
 
-			@Override
-			protected void createDialog() {
-				add(
-				        ConversationStates.ATTENDING,
-				        "wood",
-				        null,
-				        ConversationStates.ATTENDING,
-				        "I need some wood to keep my furnace lit. You can find any amount of it just lying around in the forest.",
-				        null);
+		npc.addReply("gold_nugget",
+		        "My brother Joshua lives in Ados. He can cast gold nuggets to bars of pure gold.");
 
-				add(
-				        ConversationStates.ATTENDING,
-				        Arrays.asList("ore", "iron", "iron_ore"),
-				        null,
-				        ConversationStates.ATTENDING,
-				        "You can find iron ore up in the mountains west of Or'ril, near the dwarf mines. Be careful up there!",
-				        null);
+		npc.addHelp("If you bring me #wood and #iron_ore, I can #cast the iron for you. Then you could sell it to the dwarves, to make yourself a little money.");
+		npc.addJob("Greetings. Unfortunately, because of the war, I'm not currently allowed to sell weapons to anyone except the official armoury. However, I can still #cast iron for you, or I can make you an #offer on some good tools.");
+		npc.addGoodbye();
+		npc.addSeller(new SellerBehaviour(ShopList.get().get("selltools")));
 
-				add(
-				        ConversationStates.ATTENDING,
-				        "gold_pan",
-				        null,
-				        ConversationStates.ATTENDING,
-				        "With this tool you are able to prospect for gold. In the south there is a lake near a waterfall where I once found a #gold_nugget. Maybe you are lucky as well.",
-				        null);
+		// Xoderos casts iron if you bring him wood and iron ore.
+		Map<String, Integer> requiredResources = new HashMap<String, Integer>();
+		requiredResources.put("wood", 1);
+		requiredResources.put("iron_ore", 1);
 
-				add(ConversationStates.ATTENDING, "gold_nugget", null, ConversationStates.ATTENDING,
-				        "My brother Joshua lives in Ados. He can cast gold nuggets to bars of pure gold.", null);
+		ProducerBehaviour behaviour = new ProducerBehaviour("xoderos_cast_iron",
+				"cast", "iron", requiredResources, 5 * 60);
 
-				addHelp("If you bring me #wood and #iron_ore, I can #cast the iron for you. Then you could sell it to the dwarves, to make yourself a little money.");
-				addJob("Greetings. Unfortunately, because of the war, I'm not currently allowed to sell weapons to anyone except the official armoury. However, I can still #cast iron for you, or I can make you an #offer on some good tools.");
-				addGoodbye();
-				// Once Ados is ready, we can have an expert tool smith; then Xoderos
-				// won't sell tools anymore.
-				addSeller(new SellerBehaviour(ShopList.get().get("selltools")));
-
-				// Xoderos casts iron if you bring him wood and iron ore.
-				Map<String, Integer> requiredResources = new HashMap<String, Integer>();
-				requiredResources.put("wood", new Integer(1));
-				requiredResources.put("iron_ore", new Integer(1));
-
-				ProducerBehaviour behaviour = new ProducerBehaviour("xoderos_cast_iron", "cast", "iron",
-				        requiredResources, 5 * 60);
-
-				addProducer(
-				        behaviour,
-				        "Greetings. I am sorry to tell you that, because of the war, I am not allowed to sell you any weapons. However, I can #cast iron for you. I can also #offer you tools.");
-
-			}
-		};
-		npcs.add(xoderos);
-		zone.assignRPObjectID(xoderos);
-		xoderos.put("class", "blacksmithnpc");
-		xoderos.set(23, 11);
-		xoderos.initHP(100);
-		zone.add(xoderos);
+		npc.addProducer(behaviour,
+		        "Greetings. I am sorry to tell you that, because of the war, I am not allowed to sell you any weapons. However, I can #cast iron for you. I can also #offer you tools.");
 	}
 }
--- src/games/stendhal/server/maps/semos/plains/MillerNPC.java
+++ src/games/stendhal/server/maps/semos/plains/MillerNPC.java
@@ -1,67 +1,34 @@
 package games.stendhal.server.maps.semos.plains;
 
-import games.stendhal.common.Direction;
-import games.stendhal.server.StendhalRPZone;
-import games.stendhal.server.entity.npc.NPCList;
 import games.stendhal.server.entity.npc.ProducerBehaviour;
 import games.stendhal.server.entity.npc.SpeakerNPC;
-import games.stendhal.server.maps.ZoneConfigurator;
-import games.stendhal.server.pathfinder.Path;
+import games.stendhal.server.entity.npc.SpeakerNPCFactory;
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 
-public class MillerNPC implements ZoneConfigurator {
+/**
+ * The miller (original name: Jenny). She mills flour for players who bring
+ * grain. 
+ */
+public class MillerNPC extends SpeakerNPCFactory {
 
-	private NPCList npcs = NPCList.get();
+	@Override
+	protected void createDialog(SpeakerNPC npc) {
+		npc.addJob("I run this windmill, where I can #mill people's #grain into flour for them. I also supply the bakery in Semos.");
+		npc.addReply("grain",
+		        "There's a farm nearby; they usually let people harvest there. You'll need a scythe, of course.");
+		npc.addHelp("Do you know the bakery in Semos? I'm proud to say they use my flour. But the wolves ate my delivery boy again recently... they're probably running out.");
+		npc.addGoodbye();
 
-	/**
-	 * Configure a zone.
-	 *
-	 * @param	zone		The zone to be configured.
-	 * @param	attributes	Configuration attributes.
-	 */
-	public void configureZone(StendhalRPZone zone, Map<String, String> attributes) {
-		buildSemosNorthEastPlainsArea(zone);
-	}
+		// Jenny mills flour if you bring her grain.
+		Map<String, Integer> requiredResources = new HashMap<String, Integer>();
+		requiredResources.put("grain", 5);
 
-	private void buildSemosNorthEastPlainsArea(StendhalRPZone zone) {
-		SpeakerNPC npc = new SpeakerNPC("Jenny") {
+		ProducerBehaviour behaviour = new ProducerBehaviour("jenny_mill_flour",
+				"mill", "flour", requiredResources, 2 * 60);
 
-			@Override
-			protected void createPath() {
-				List<Path.Node> nodes = new LinkedList<Path.Node>();
-				setPath(nodes, false);
-			}
-
-			@Override
-			protected void createDialog() {
-				addJob("I run this windmill, where I can #mill people's #grain into flour for them. I also supply the bakery in Semos.");
-				addReply("grain",
-				        "There's a farm nearby; they usually let people harvest there. You'll need a scythe, of course.");
-				addHelp("Do you know the bakery in Semos? I'm proud to say they use my flour. But the wolves ate my delivery boy again recently... they're probably running out.");
-				addGoodbye();
-
-				// Jenny mills flour if you bring her grain.
-				Map<String, Integer> requiredResources = new HashMap<String, Integer>();
-				requiredResources.put("grain", new Integer(5));
-
-				ProducerBehaviour behaviour = new ProducerBehaviour("jenny_mill_flour", "mill", "flour",
-				        requiredResources, 2 * 60);
-
-				addProducer(behaviour,
-				        "Greetings! I am Jenny, the local miller. If you bring me some #grain, I can #mill it into flour for you.");
-
-			}
-		};
-		npcs.add(npc);
-		zone.assignRPObjectID(npc);
-		npc.put("class", "woman_003_npc");
-		npc.setDirection(Direction.DOWN);
-		npc.set(19, 38);
-		npc.initHP(100);
-		zone.add(npc);
+		npc.addProducer(behaviour,
+		        "Greetings! I am Jenny, the local miller. If you bring me some #grain, I can #mill it into flour for you.");
 	}
 }
