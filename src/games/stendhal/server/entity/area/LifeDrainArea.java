5692fba65f44c2261331832756ea939c433ae157
Chad F <chad3f@users.sourceforge.net>
2007-04-12 00:55:31 +0000

    Used new common super class. Need sleep.. didn't test beyond server start.

--- src/games/stendhal/server/entity/area/LifeDrainArea.java
+++ src/games/stendhal/server/entity/area/LifeDrainArea.java
@@ -32,7 +32,7 @@ import games.stendhal.server.events.TurnNotifier;
  * An area that drains an RPEntity of HP while over it.
  *
  */
-public class LifeDrainArea extends PassiveEntity implements MovementListener, TurnListener {
+public class LifeDrainArea extends OccupantArea {
 
 	/**
 	 * The logger instance.
@@ -45,32 +45,10 @@ public class LifeDrainArea extends PassiveEntity implements MovementListener, Tu
 	protected int damage;
 
 	/**
-	 * How often damage is given while stationary (in turns).
-	 */
-	protected int interval;
-
-	/**
-	 * The inflict damage only on players.
-	 */
-	protected boolean playersOnly;
-
-	/**
-	 * The chance of damage while walking (0.0 - 1.0).
-	 */
-	protected double probability;
-
-	/**
-	 * A list of entities [potentially] in range.
-	 */
-	protected List<RPEntity.ID> targets;
-
-	/**
 	 * Random number generator.
 	 */
 	protected Random rand;
 
-	private int width = 1;
-	private int height = 1;
 
 	/**
 	 * Create a damaging area.
@@ -80,44 +58,18 @@ public class LifeDrainArea extends PassiveEntity implements MovementListener, Tu
 	 * @param   height      Height of this area
 	 * @param	interval	How often damage is given while
 	 *				stationary (in turns).
-	 * @param	probability	The chance of damage while walking
-	 *				(0.0 - 1.0).
 	 */
-	public LifeDrainArea(String name, int width, int height, int damage, int interval, double probability) throws AttributeNotFoundException {
-		put("name", name);
-		put("type", "damaging_area");
+	public LifeDrainArea(String name, int width, int height, int damage, int interval) throws AttributeNotFoundException {
+		super("damaging_area", name, width, height, interval);
 
 		this.damage = damage;
-		this.interval = interval;
-		this.probability = probability;
-		this.height = height;
-		this.width = width;
 
-		put("width", width);
-		put("height", height);
-		
-		playersOnly = false;
 		rand = new Random();
-		targets = new LinkedList<RPEntity.ID>();
 	}
 
 	//
 	// LifeDrainArea
 	//
-
-	/**
-	 * Add an entity to the target list.
-	 *
-	 * @param	entity		The RPEntity to add.
-	 */
-	protected void addTarget(RPEntity entity) {
-		targets.add(entity.getID());
-		entity.onAttacked(this, true);
-
-		if (targets.size() == 1) {
-			TurnNotifier.get().notifyInTurns(interval, this, null);
-		}
-	}
 
 	/**
 	 * Calculate the entity's final defense value.
@@ -198,190 +150,52 @@ public class LifeDrainArea extends PassiveEntity implements MovementListener, Tu
 		return true;
 	}
 
+
+	//
+	// OccupantArea
+	//
+
 	/**
-	 * Apply any damage done while moving.
+	 * An entity has entered the area. This should not apply any actions
+	 * that <code>handleMovement()</code> does.
 	 *
-	 * @param	entity		The RPEntity to [possibly] damage.
+	 * @param	entity		The RPEntity that was added.
+	 *
+	 * @return	<code>false</code> if this entity should not be
+	 *		processed, <code>true</code> otherwise.
 	 */
-	protected void handleMovement(RPEntity entity) {
+	protected boolean handleAdded(RPEntity entity) {
+		if(!super.handleAdded(entity)) {
+			return false;
+		}
+
+		entity.onAttacked(this, true);
+		doDamage(entity);
+		return true;
 	}
 
 	/**
-	 * Remove an entity from the target list.
+	 * Apply actions done at regular intervals.
 	 *
-	 * @param	entity		The RPEntity to remove.
+	 * @param	entity		The RPEntity occupant.
+	 *
+	 * @return	<code>false</code> if this entity should be removed
+	 *		from further processing, <code>true</code> otherwise.
 	 */
-	protected void removeTarget(RPEntity entity) {
+	protected boolean handleInterval(RPEntity entity) {
+		doDamage(entity);
+		return true;
+	}
+
+
+	/**
+	 * An entity has left the area. This should not apply any actions
+	 * that <code>handleMovement()</code> does.
+	 *
+	 * @param	entity		The RPEntity that was added.
+	 */
+	protected void handleRemoved(RPEntity entity) {
 		entity.onAttacked(this, false);
-		targets.remove(entity.getID());
-
-		if (targets.isEmpty()) {
-			TurnNotifier.get().dontNotify(this, null);
-		}
-	}
-
-	/**
-	 * Set whether only players get damage.
-	 *
-	 * @param	playersOnly	Whether to only attack players.
-	 */
-	public void setPlayersOnly(boolean playersOnly) {
-		this.playersOnly = playersOnly;
-	}
-
-	//
-	// Entity
-	//
-
-	/**
-	 * Get the damage area.
-	 *
-	 *
-	 */
-	@Override
-	public void getArea(Rectangle2D rect, double x, double y) {
-		rect.setRect(x, y, width, height);
-	}
-
-	/**
-	 * Called when this object is added to a zone.
-	 *
-	 * @param	zone		The zone this was added to.
-	 */
-	@Override
-	public void onAdded(StendhalRPZone zone) {
-		super.onAdded(zone);
-		zone.addMovementListener(this);
-	}
-
-	/**
-	 * Called when this object is being removed from a zone.
-	 *
-	 * @param	zone		The zone this will be removed from.
-	 */
-	@Override
-	public void onRemoved(StendhalRPZone zone) {
-		zone.removeMovementListener(this);
-		super.onRemoved(zone);
-	}
-
-	/**
-	 * Handle object attribute change(s).
-	 */
-	@Override
-	public void update() throws AttributeNotFoundException {
-		StendhalRPZone zone;
-
-		super.update();
-
-		/*
-		 * Reregister incase coordinates changed (could be smarter)
-		 */
-		zone = getZone();
-		zone.removeMovementListener(this);
-		zone.addMovementListener(this);
-	}
-
-	//
-	// MovementListener
-	//
-
-	/**
-	 * Invoked when an entity enters the object area.
-	 *
-	 * @param	entity		The RPEntity who moved.
-	 * @param	zone		The new zone.
-	 * @param	newX		The new X coordinate.
-	 * @param	newY		The new Y coordinate.
-	 */
-	public void onEntered(RPEntity entity, StendhalRPZone zone, int newX, int newY) {
-		/*
-		 * Only effect players?
-		 */
-		if (playersOnly && !(entity instanceof Player)) {
-			return;
-		}
-
-		addTarget(entity);
-	}
-
-	/**
-	 * Invoked when an entity leaves the object area.
-	 *
-	 * @param	entity		The RPEntity who entered.
-	 * @param	zone		The old zone.
-	 * @param	oldX		The old X coordinate.
-	 * @param	oldY		The old Y coordinate.
-	 *
-	 */
-	public void onExited(RPEntity entity, StendhalRPZone zone, int oldX, int oldY) {
-		/*
-		 * Only effect players?
-		 */
-		if (playersOnly && !(entity instanceof Player)) {
-			return;
-		}
-
-		removeTarget(entity);
-	}
-
-	/**
-	 * Invoked when an entity moves while over the object area.
-	 *
-	 * @param	entity		The RPEntity who left.
-	 * @param	zone		The zone.
-	 * @param	oldX		The old X coordinate.
-	 * @param	oldY		The old Y coordinate.
-	 * @param	newX		The new X coordinate.
-	 * @param	newY		The new Y coordinate.
-	 */
-	public void onMoved(RPEntity entity, StendhalRPZone zone, int oldX, int oldY, int newX, int newY) {
-	}
-
-	//
-	// TurnListener
-	//
-
-	/**
-	 * This method is called when the turn number is reached.
-	 *
-	 * @param	currentTurn	Current turn number.
-	 * @param	message		The string that was used.
-	 */
-	public void onTurnReached(int currentTurn, String message) {
-		IRPZone zone;
-		Rectangle2D area;
-
-		zone = getZone();
-		area = getArea();
-
-		/*
-		 * Damage entities still in the area, remove those not here
-		 */
-		Iterator<RPEntity.ID> iter = targets.iterator();
-
-		while (iter.hasNext()) {
-			RPEntity.ID id = iter.next();
-
-			if (!zone.has(id)) {
-				iter.remove();
-			} else {
-				RPEntity entity = (RPEntity) zone.get(id);
-
-				if (area.intersects(entity.getArea())) {
-					if (!doDamage(entity)) {
-						entity.onAttacked(this, false);
-						iter.remove();
-					}
-				} else {
-					entity.onAttacked(this, false);
-					iter.remove();
-				}
-			}
-		}
-
-		if (!targets.isEmpty()) {
-			TurnNotifier.get().notifyInTurns(interval, this, null);
-		}
+		super.handleRemoved(entity);
 	}
 }
--- src/games/stendhal/server/entity/area/LifeDrainAreaFactory.java
+++ src/games/stendhal/server/entity/area/LifeDrainAreaFactory.java
@@ -16,7 +16,7 @@ import games.stendhal.server.StendhalRPWorld;
 /**
  * A base factory for <code>LifeDrainArea</code> objects.
  */
-public class LifeDrainAreaFactory implements ConfigurableFactory {
+public class LifeDrainAreaFactory extends OccupantAreaFactory {
 
 	//
 	// LifeDrainAreaFactory
@@ -36,7 +36,9 @@ public class LifeDrainAreaFactory implements ConfigurableFactory {
 		String s;
 
 		if ((s = ctx.getAttribute("maximum-damage")) == null) {
-			throw new IllegalArgumentException("Required attribute 'maximum-damage' missing");
+return 0;
+// For now, unused.. require later
+//			throw new IllegalArgumentException("Required attribute 'maximum-damage' missing");
 		}
 
 		try {
@@ -46,172 +48,14 @@ public class LifeDrainAreaFactory implements ConfigurableFactory {
 		}
 	}
 
-	/**
-	 * Extract the name of the damaging entity.
-	 *
-	 * @param	ctx		The configuration context.
-	 *
-	 * @return	The entity's name.
-	 *
-	 * @throws	IllegalArgumentException
-	 *				If the attribute is missing.
-	 */
-	protected String getName(ConfigurableFactoryContext ctx) throws IllegalArgumentException {
-		String s;
-
-		if ((s = ctx.getAttribute("name")) == null) {
-			throw new IllegalArgumentException("Required attribute 'name' missing");
-		}
-
-		return s;
-	}
-
-	/**
-	 * Extract the width from context
-	 *
-	 * @param	ctx		The configuration context.
-	 * @return	width (1 if unspecified)
-	 * @throws	IllegalArgumentException If the attribute is invalid.
-	 */
-	protected int getWidth(ConfigurableFactoryContext ctx) throws IllegalArgumentException {
-		String s;
-
-		if ((s = ctx.getAttribute("width")) == null) {
-			return 1;
-		}
-
-		try {
-			return Integer.parseInt(s);
-		} catch (NumberFormatException ex) {
-			throw new IllegalArgumentException("Invalid 'width' attribute: " + s);
-		}
-	}
-
-	/**
-	 * Extract the height from context
-	 *
-	 * @param	ctx		The configuration context.
-	 * @return	height (1 if unspecified)
-	 * @throws	IllegalArgumentException If the attribute is invalid.
-	 */
-	protected int getHeight(ConfigurableFactoryContext ctx) throws IllegalArgumentException {
-		String s;
-
-		if ((s = ctx.getAttribute("height")) == null) {
-			return 1;
-		}
-
-		try {
-			return Integer.parseInt(s);
-		} catch (NumberFormatException ex) {
-			throw new IllegalArgumentException("Invalid 'height' attribute: " + s);
-		}
-	}
-
-
-	/**
-	 * Extract the interval (in seconds) to inflict damage while
-	 * stationary.
-	 *
-	 * @param	ctx		The configuration context.
-	 *
-	 * @return	The stationary interval (in turns).
-	 *
-	 * @throws	IllegalArgumentException
-	 *				If the attribute is missing.
-	 */
-	protected int getInterval(ConfigurableFactoryContext ctx) throws IllegalArgumentException {
-		String s = ctx.getAttribute("interval");
-
-		if (s == null) {
-			throw new IllegalArgumentException("Required attribute 'interval' missing");
-		}
-
-		try {
-			return StendhalRPWorld.get().getTurnsInSeconds(Integer.parseInt(s));
-		} catch (NumberFormatException ex) {
-			throw new IllegalArgumentException("Invalid 'interval' attribute: " + s);
-		}
-	}
-
-	/**
-	 * Extract the flag to only damage players.
-	 *
-	 * @param	ctx		The configuration context.
-	 *
-	 * @return	The flag to only damage players.
-	 *
-	 * @throws	IllegalArgumentException
-	 *				If the attribute is invalid.
-	 */
-	protected boolean getPlayersOnly(ConfigurableFactoryContext ctx) throws IllegalArgumentException {
-		String s;
-
-		if ((s = ctx.getAttribute("players-only")) == null) {
-			return false;
-		}
-
-		if (s.equals("true")) {
-			return true;
-		}
-
-		if (s.equals("false")) {
-			return false;
-		}
-
-		throw new IllegalArgumentException("Invalid 'players-only' attribute: " + s);
-	}
-
-	/**
-	 * Extract the moving damage probability (as percent) from a context.
-	 *
-	 * @param	ctx		The configuration context.
-	 *
-	 * @return	The damage probability (0.0 - 1.0).
-	 *
-	 * @throws	IllegalArgumentException
-	 *				If the attribute is invalid.
-	 */
-	protected double getProbability(ConfigurableFactoryContext ctx) throws IllegalArgumentException {
-		String s;
-
-		if ((s = ctx.getAttribute("probability")) == null) {
-			return 0.0;
-		}
-
-		try {
-			return Integer.parseInt(s) / 100.0;
-		} catch (NumberFormatException ex) {
-			throw new IllegalArgumentException("Invalid 'probability' attribute: " + s);
-		}
-	}
 
 	//
-	// ConfigurableFactory
+	// OccupantAreaFactory
 	//
 
-	/**
-	 * Create a damaging area.
-	 *
-	 * @param	ctx		Configuration context.
-	 *
-	 * @return	A LifeDrainArea.
-	 *
-	 * @throws	IllegalArgumentException
-	 *				If there is a problem with the
-	 *				attributes. The exception message
-	 *				should be a value sutable for
-	 *				meaningful user interpretation.
-	 *
-	 * @see		LifeDrainArea
-	 */
-	public Object create(ConfigurableFactoryContext ctx) throws IllegalArgumentException {
+	protected OccupantArea createArea(ConfigurableFactoryContext ctx) throws IllegalArgumentException {
 		LifeDrainArea area;
 
-		area = new LifeDrainArea(getName(ctx), getWidth(ctx), getHeight(ctx), getDamage(ctx), getInterval(ctx), getProbability(ctx));
-
-		area.setPlayersOnly(getPlayersOnly(ctx));
-
-		return area;
+		return new LifeDrainArea(getName(ctx), getWidth(ctx), getHeight(ctx), getDamage(ctx), getInterval(ctx));
 	}
 }
