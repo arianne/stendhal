c4b94188d221ed104ef3e4079d3c1098b9f584fc
Daniel Herding <wikipedian@users.sourceforge.net>
2007-04-12 11:52:27 +0000

    minor refactorings

--- src/games/stendhal/server/maps/quests/Campfire.java
+++ src/games/stendhal/server/maps/quests/Campfire.java
@@ -90,6 +90,8 @@ public class Campfire extends AbstractQuest {
 			}
 			int turnsSinceLastBroughtWood = StendhalRPRuleProcessor.get().getTurn() - turnWhenLastBroughtWood;
 			if (turnsSinceLastBroughtWood < 0) {
+				// TODO: use time instead of turn number, that will make such
+				// things easier.
 				// The server was restarted since last doing the quest.
 				// Make sure the player can repeat the quest.
 				turnsSinceLastBroughtWood = 0;
@@ -99,7 +101,7 @@ public class Campfire extends AbstractQuest {
 		}
 	}
 	
-	private void step_1() {
+	private void prepareRequestingStep() {
 		SpeakerNPC npc = npcs.get("Sally");
 
 		npc.add(ConversationStates.IDLE,
@@ -109,18 +111,19 @@ public class Campfire extends AbstractQuest {
 			null,
 			new SpeakerNPC.ChatAction() {
 			@Override
-			public void fire(Player player, String text, SpeakerNPC engine) {
-				if (canStartQuestNow(engine, player)) {
-					engine.say("Hi! Could you do me a #favor?");
+			public void fire(Player player, String text, SpeakerNPC npc) {
+				if (canStartQuestNow(npc, player)) {
+					npc.say("Hi! Could you do me a #favor?");
 				} else if (player.getQuest(QUEST_SLOT).equals("start")) { 
 					if (player.isEquipped("wood", REQUIRED_WOOD)){
-						engine.say("Hi again! You've got wood, I see; do you have those 10 pieces of wood I asked about earlier?");
-						engine.setCurrentState(ConversationStates.QUEST_ITEM_BROUGHT);
+						npc.say("Hi again! You've got wood, I see; do you have those 10 pieces of wood I asked about earlier?");
+						npc.setCurrentState(ConversationStates.QUEST_ITEM_BROUGHT);
 					} else {
-						engine.say("You're back already? Don't forget that you promised to collect ten pieces of wood for me!");
+						npc.say("You're back already? Don't forget that you promised to collect ten pieces of wood for me!");
 					}
 				} else {
-					engine.say("Oh, I still have plenty of wood from the last time you helped me. Thank you for helping!");
+					// TODO: say how many minutes are left.
+					npc.say("Oh, I still have plenty of wood from the last time you helped me. Thank you for helping!");
 				}
 			}
 		});
@@ -133,15 +136,15 @@ public class Campfire extends AbstractQuest {
 				new SpeakerNPC.ChatAction() {
 					@Override
 					public void fire(Player player, String text,
-							SpeakerNPC engine) {
-						if (canStartQuestNow(engine, player)) {
-							engine.say("I need more wood to keep my campfire running, But I can't leave it unattended to go get some! Could you please get some from the forest for me? I need ten pieces.");
+							SpeakerNPC npc) {
+						if (canStartQuestNow(npc, player)) {
+							npc.say("I need more wood to keep my campfire running, But I can't leave it unattended to go get some! Could you please get some from the forest for me? I need ten pieces.");
 						} else if (player.getQuest(QUEST_SLOT).equals("start")){
-							engine.say("You already promised me to bring me some wood! Ten pieces, remember?");
+							npc.say("You already promised me to bring me some wood! Ten pieces, remember?");
 						}
 						else {
-							engine.say("I don't need any more wood at the moment, but thanks for asking.");
-							engine.setCurrentState(ConversationStates.ATTENDING);
+							npc.say("I don't need any more wood at the moment, but thanks for asking.");
+							npc.setCurrentState(ConversationStates.ATTENDING);
 						}
 					}
 				});
@@ -154,25 +157,21 @@ public class Campfire extends AbstractQuest {
 				"Okay. You can find wood in the forest north of here. Come back when you get ten pieces of wood!",
 				new SpeakerNPC.ChatAction() {
 					@Override
-					public void fire(Player player, String text, SpeakerNPC engine) {
+					public void fire(Player player, String text, SpeakerNPC npc) {
 						player.setQuest(QUEST_SLOT, "start");
 					}
 				});
 		
 		// player is not willing to help
 		npc.add(ConversationStates.QUEST_OFFERED,
-				"no",
+				ConversationPhrases.NO_MESSAGES,
 				null,
 				ConversationStates.ATTENDING,
 				"Oh dear, how am I going to cook all this meat? Perhaps I'll just have to feed it to the animals...",
 				null);
 	}
 
-	private void step_2() {
-		// Just find the wood somewhere. It isn't a quest
-	}
-
-	private void step_3() {
+	private void prepareBringingStep() {
 		SpeakerNPC npc = npcs.get("Sally");
 
 		npc.add(ConversationStates.QUEST_ITEM_BROUGHT,
@@ -182,7 +181,7 @@ public class Campfire extends AbstractQuest {
 				null,
 				new SpeakerNPC.ChatAction() {
 					@Override
-					public void fire(Player player, String text, SpeakerNPC engine) {
+					public void fire(Player player, String text, SpeakerNPC npc) {
 						if (player.drop("wood", REQUIRED_WOOD)) {
 							player.setQuest(QUEST_SLOT, Integer.toString(StendhalRPRuleProcessor.get().getTurn()));
 							player.addXP(50);
@@ -193,20 +192,20 @@ public class Campfire extends AbstractQuest {
 							} else {
 								rewardClass = "ham";
 							}
-							engine.say("Thank you! Here, take some " + rewardClass + "!");
+							npc.say("Thank you! Here, take some " + rewardClass + "!");
 							EntityManager manager = StendhalRPWorld.get().getRuleManager().getEntityManager();
 							StackableItem reward = (StackableItem) manager.getItem(rewardClass);
 							reward.setQuantity(REQUIRED_WOOD);
 							player.equip(reward, true);
 							player.notifyWorldAboutChanges();
 						} else {
-							engine.say("Hey! Where did you put the wood?");
+							npc.say("Hey! Where did you put the wood?");
 						}
 					}
 				});
 
 		npc.add(ConversationStates.QUEST_ITEM_BROUGHT,
-				"no",
+				ConversationPhrases.NO_MESSAGES,
 				null,
 				ConversationStates.ATTENDING,
 				"Oh... well, I hope you find some quickly; this fire's going to burn out soon!",
@@ -217,8 +216,7 @@ public class Campfire extends AbstractQuest {
 	public void addToWorld() {
 		super.addToWorld();
 
-		step_1();
-		step_2();
-		step_3();
+		prepareRequestingStep();
+		prepareBringingStep();
 	}
 }
