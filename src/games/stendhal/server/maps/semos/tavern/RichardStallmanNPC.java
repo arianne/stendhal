3483d80f338c06167da783eceaf62f586b9b9a63
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2007-04-13 14:34:26 +0000

    Added a RMS sprite to promote free software. Many players don't know Stendhal is free software.

---
+++ src/games/stendhal/server/maps/semos/tavern/RichardStallmanNPC.java
@@ -0,0 +1,56 @@
+package games.stendhal.server.maps.semos.tavern;
+
+import games.stendhal.common.Direction;
+import games.stendhal.server.StendhalRPZone;
+import games.stendhal.server.entity.npc.NPCList;
+import games.stendhal.server.entity.npc.ShopList;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.pathfinder.Path;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+
+public class RichardStallmanNPC {
+	private NPCList npcs = NPCList.get();
+
+	private ShopList shops = ShopList.get();
+
+	/**
+	 * Configure a zone.
+	 *
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+	public void configureZone(StendhalRPZone zone, Map<String, String> attributes) {
+		buildStallman(zone);
+	}
+
+	private void buildStallman(StendhalRPZone zone) {
+		SpeakerNPC stallman = new SpeakerNPC("Richard Stallman") {
+
+			@Override
+			protected void createPath() {
+				List<Path.Node> nodes = new LinkedList<Path.Node>();
+				setPath(nodes, true);
+			}
+
+			@Override
+			protected void createDialog() {
+				addGreeting("Welcome to Stendhal! True free software!.");
+				addJob("I am the free software evangelizer! I am the founder of GNU.");
+				addHelp("Help Stendhal to be even better. Donate your time, tell your friends to play, create maps.");
+				addGoodbye();
+			}
+		};
+		npcs.add(stallman);
+		zone.assignRPObjectID(stallman);
+		stallman.put("class", "richardstallmannpc");
+		stallman.set(26, 10);
+		stallman.setDirection(Direction.DOWN);
+		stallman.initHP(100);
+		zone.add(stallman);
+
+	}
+}
