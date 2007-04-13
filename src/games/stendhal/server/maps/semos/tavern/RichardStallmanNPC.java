b19c598f2a867e86fdbef0cff318f0a9a3e93462
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2007-04-13 16:30:15 +0000

    Per request, Stallman points on GNU and GPL to its pages respectively.

--- src/games/stendhal/server/maps/semos/tavern/RichardStallmanNPC.java
+++ src/games/stendhal/server/maps/semos/tavern/RichardStallmanNPC.java
@@ -42,12 +42,15 @@ public class RichardStallmanNPC implements ZoneConfigurator {
 			@Override
 			protected void createDialog() {
 				addGreeting("Welcome to Stendhal! True #free software!.");
-				addJob("I am the #free software evangelizer! I am the founder of GNU.");
+				addJob("I am the #free software evangelizer! I am the founder of #GNU.");
 				addHelp("Help #Stendhal to be even better. Donate your time, tell your friends to play, create maps.");
 				addReply("free",
 					"''Free software'' is a matter of liberty, not price. To understand the concept, you should think of ''free'' as in ''free speech,'' not as in ''free beer''.");
 				addReply("stendhal",
-					"Stendhal is #free software. You can run, copy, distribute, study, change and improve this software.");
+					"Stendhal is #free software under #GNU #GPL. You can run, copy, distribute, study, change and improve this software.");
+				addReply("gnu", "http://www.gnu.org/");
+				addReply("gpl", "http://www.gnu.org/licenses/gpl.html");
+				
 				
 				addGoodbye();
 			}
