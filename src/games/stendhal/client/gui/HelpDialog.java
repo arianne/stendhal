10d4691dbee5ae52df830ddd60a4012159732e8d
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-04-14 00:29:44 +0000

    Heavily based on "How to Use Trees" Java Tutorial (see http://java.sun.com/docs/books/tutorial/uiswing/components/tree.html)

--- src/games/stendhal/client/gui/HelpDialog.java
+++ src/games/stendhal/client/gui/HelpDialog.java
@@ -1,100 +1,238 @@
-/* $Id$ */
-/***************************************************************************
- *                      (C) Copyright 2003 - Marauroa                      *
- ***************************************************************************
- ***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
 package games.stendhal.client.gui;
 
+/**
+ * A 1.4 application that requires the following additional files:
+ *   TreeDemoHelp.html
+ *    arnold.html
+ *    bloch.html
+ *    chan.html
+ *    jls.html
+ *    swingtutorial.html
+ *    tutorial.html
+ *    tutorialcont.html
+ *    vm.html
+ */
+import games.stendhal.client.SpriteStore;
+
 import java.awt.Dimension;
-import java.awt.Frame;
+import java.awt.GridLayout;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
-import javax.swing.JDialog;
 import javax.swing.JEditorPane;
-import javax.swing.JLabel;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JWindow;
-import javax.swing.event.HyperlinkEvent;
-import javax.swing.event.HyperlinkListener;
-import javax.swing.text.html.HTMLDocument;
-import javax.swing.text.html.HTMLFrameHyperlinkEvent;
+import javax.swing.JSplitPane;
+import javax.swing.JTree;
+import javax.swing.UIManager;
+import javax.swing.event.TreeSelectionEvent;
+import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeSelectionModel;
 
-/**
- * Summary description for CreateAccountDialog
- *
- */
-public class HelpDialog extends JDialog {
+public class HelpDialog extends JPanel
+                      implements TreeSelectionListener {
+    private JEditorPane htmlPane;
+    private JTree tree;
+    private URL helpURL;
+    
+    //Optionally set the look and feel.
+    private static boolean useSystemLookAndFeel = true;
 
-	private static final long serialVersionUID = 4436228792112530975L;
+    public HelpDialog() {
+        super(new GridLayout(1,0));
 
-	// Variables declaration
-	private JLabel usernameLabel;
+        //Create the nodes.
+        DefaultMutableTreeNode top =
+            new DefaultMutableTreeNode("The Java Series");
+        createNodes(top);
 
-	private JLabel serverLabel;
+        //Create a tree that allows one selection at a time.
+        tree = new JTree(top);
+        tree.getSelectionModel().setSelectionMode
+                (TreeSelectionModel.SINGLE_TREE_SELECTION);
 
-	private JLabel serverPortLabel;
+        //Listen for when the selection changes.
+        tree.addTreeSelectionListener(this);
 
-	private JLabel passwordLabel;
- 
+        //Create the scroll pane and add the tree to it. 
+        JScrollPane treeView = new JScrollPane(tree);
 
-	private Frame owner;
+        //Create the HTML viewing pane.
+        htmlPane = new JEditorPane();
+        htmlPane.setEditable(false);
+        initHelp();
+        JScrollPane htmlView = new JScrollPane(htmlPane);
 
-	public HelpDialog(Frame owner) throws MalformedURLException, IOException {
-		super(owner);
-		this.owner = owner;
-		initializeComponent();
+        //Add the scroll panes to a split pane.
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        splitPane.setTopComponent(treeView);
+        splitPane.setBottomComponent(htmlView);
 
-		this.setVisible(true);
-	}
-	
-	class Hyperactive implements HyperlinkListener {
-		 
-        public void hyperlinkUpdate(HyperlinkEvent e) {
-            if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-                JEditorPane pane = (JEditorPane) e.getSource();
-                if (e instanceof HTMLFrameHyperlinkEvent) {
-                    HTMLFrameHyperlinkEvent  evt = (HTMLFrameHyperlinkEvent)e;
-                    HTMLDocument doc = (HTMLDocument)pane.getDocument();
-                    doc.processHTMLFrameHyperlinkEvent(evt);
-                } else {
-                    try {
-                        pane.setPage(e.getURL());
-                    } catch (Throwable t) {
-                        t.printStackTrace();
-                    }
-                }
-            }
+        Dimension minimumSize = new Dimension(200, 50);
+        htmlView.setMinimumSize(minimumSize);
+        treeView.setMinimumSize(minimumSize);
+        splitPane.setDividerLocation(200); //XXX: ignored in some releases
+                                           //of Swing. bug 4101306
+
+        splitPane.setPreferredSize(new Dimension(500, 300));
+
+        //Add the split pane to this panel.
+        add(splitPane);
+    }
+
+    /** Required by TreeSelectionListener interface. */
+    public void valueChanged(TreeSelectionEvent e) {
+        DefaultMutableTreeNode node = (DefaultMutableTreeNode)
+                           tree.getLastSelectedPathComponent();
+
+        if (node == null) return;
+
+        Object nodeInfo = node.getUserObject();
+        if (node.isLeaf()) {
+            BookInfo book = (BookInfo)nodeInfo;
+            displayURL(book.bookURL);
+        } else {
+            displayURL(helpURL); 
         }
     }
 
-	private void initializeComponent() throws MalformedURLException, IOException {
+    private class BookInfo {
+        public String bookName;
+        public URL bookURL;
 
-		JEditorPane editor = new JEditorPane(new URL("http://arianne.sourceforge.net/wiki/index.php?title=Stendhal_Manual/Gameplay"));
-		editor.setEditable(false);
-		editor.addHyperlinkListener(new Hyperactive());
+        public BookInfo(String book, String filename) {
+            bookName = book;
+            bookURL = SpriteStore.get().getResourceURL("data/docu/" + filename);
+            if (bookURL == null) {
+                System.err.println("Couldn't find file: "
+                                   + filename);
+            }
+        }
 
-		JScrollPane scroller = new JScrollPane(editor);
-		add(scroller);
+        public String toString() {
+            return bookName;
+        }
+    }
 
-		// CreateAccountDialog
-		this.setTitle("Stendhal Help");
-		this.setResizable(true);
-		this.setSize(new Dimension(350, 275));
-		this.setLocationRelativeTo(owner);
+    private void initHelp() {
+        String s = "TreeDemoHelp.html";
+        helpURL = HelpDialog.class.getResource(s);
+        if (helpURL == null) {
+            System.err.println("Couldn't open help file: " + s);
+        }
+        displayURL(helpURL);
+    }
 
-	}
+    private void displayURL(URL url) {
+        try {
+            if (url != null) {
+                htmlPane.setPage(url);
+            } else { //null url
+		htmlPane.setText("File Not Found");
+            }
+        } catch (IOException e) {
+            System.err.println("Attempted to read a bad URL: " + url);
+        }
+    }
 
-	public static void main(String[] args) throws MalformedURLException, IOException {
-		new HelpDialog(null);
-	}
-	
+    private void createNodes(DefaultMutableTreeNode top) {
+        DefaultMutableTreeNode category = null;
+        DefaultMutableTreeNode book = null;
+
+        category = new DefaultMutableTreeNode("Books for Java Programmers");
+        top.add(category);
+
+        //original Tutorial
+        book = new DefaultMutableTreeNode(new BookInfo
+            ("The Java Tutorial: A Short Course on the Basics",
+            "gameplay.html"));
+        category.add(book);
+
+        //Tutorial Continued
+        book = new DefaultMutableTreeNode(new BookInfo
+            ("The Java Tutorial Continued: The Rest of the JDK",
+            "tutorialcont.html"));
+        category.add(book);
+
+        //JFC Swing Tutorial
+        book = new DefaultMutableTreeNode(new BookInfo
+            ("The JFC Swing Tutorial: A Guide to Constructing GUIs",
+            "swingtutorial.html"));
+        category.add(book);
+
+        //Bloch
+        book = new DefaultMutableTreeNode(new BookInfo
+            ("Effective Java Programming Language Guide",
+	     "bloch.html"));
+        category.add(book);
+
+        //Arnold/Gosling
+        book = new DefaultMutableTreeNode(new BookInfo
+            ("The Java Programming Language", "arnold.html"));
+        category.add(book);
+
+        //Chan
+        book = new DefaultMutableTreeNode(new BookInfo
+            ("The Java Developers Almanac",
+             "chan.html"));
+        category.add(book);
+
+        category = new DefaultMutableTreeNode("Books for Java Implementers");
+        top.add(category);
+
+        //VM
+        book = new DefaultMutableTreeNode(new BookInfo
+            ("The Java Virtual Machine Specification",
+             "vm.html"));
+        category.add(book);
+
+        //Language Spec
+        book = new DefaultMutableTreeNode(new BookInfo
+            ("The Java Language Specification",
+             "jls.html"));
+        category.add(book);
+    }
+        
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        if (useSystemLookAndFeel) {
+            try {
+                UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception e) {
+                System.err.println("Couldn't use system look and feel.");
+            }
+        }
+
+        //Create and set up the window.
+        JFrame frame = new JFrame("TreeDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create and set up the content pane.
+        HelpDialog newContentPane = new HelpDialog();
+        newContentPane.setOpaque(true); //content panes must be opaque
+        frame.setContentPane(newContentPane);
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    
+ 
 }
