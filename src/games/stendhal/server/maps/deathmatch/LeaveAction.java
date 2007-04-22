854ca53fa948c1227e101bdb880b89ec6bd2ee97
Anders Asplund <damnter@users.sourceforge.net>
2007-04-22 03:44:49 +0000

    Fixed leave bug in deathmath arena

--- src/games/stendhal/server/maps/deathmatch/LeaveAction.java
+++ src/games/stendhal/server/maps/deathmatch/LeaveAction.java
@@ -16,11 +16,21 @@ public class LeaveAction extends SpeakerNPC.ChatAction {
 	@Override
 	public void fire(Player player, String text, SpeakerNPC engine) {
 		DeathmatchState deathmatchState = DeathmatchState.createFromQuestString(player.getQuest("deathmatch"));
-		if (deathmatchState.getLifecycleState() != DeathmatchLifecycle.DONE) {
+/*		if (deathmatchState.getLifecycleState() != DeathmatchLifecycle.DONE) {
 			StendhalRPZone zone = (StendhalRPZone) StendhalRPWorld.get().getRPZone("0_semos_plains_n");
 			player.teleport(zone, 100, 115, null, player);
 		} else {
 			engine.say("I don't think you claimed your #victory yet.");
+		}
+		return;
+*/
+		if (deathmatchState.getLifecycleState() == DeathmatchLifecycle.VICTORY) {
+			engine.say("I don't think you claimed your #victory yet.");
+		} else if (deathmatchState.getLifecycleState() == DeathmatchLifecycle.DONE) {
+			StendhalRPZone zone = (StendhalRPZone) StendhalRPWorld.get().getRPZone("0_semos_plains_n");
+			player.teleport(zone, 100, 115, null, player);
+		} else {
+				engine.say("What are you? A coward?");
 		}
 		return;
 	}
