5d211f829d0bf4ba9eff84889a0af0c6278afc01
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-04-22 15:07:45 +0000

    added attributes to MockPlayer

--- tests/games/stendhal/server/actions/equip/EquipmentActionTest.java
+++ tests/games/stendhal/server/actions/equip/EquipmentActionTest.java
@@ -1,12 +1,10 @@
 // $Id$
 package games.stendhal.server.actions.equip;
 
-import games.stendhal.client.entity.User;
 import games.stendhal.server.entity.player.Player;
 import marauroa.common.game.RPAction;
 import marauroa.common.game.RPObject;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 
@@ -21,11 +19,13 @@ public class EquipmentActionTest {
 
 		public MockPlayer() {
 			super(new RPObject());
+			setX(10);
+			setY(5);
+			setID(new ID(1, "0_semos_city"));
 		}
 	}
 
 	@Test
-	@Ignore // not working yet
 	public void testDropInvalidBaseItem() {
 		
 		Player player = new MockPlayer();
@@ -33,8 +33,8 @@ public class EquipmentActionTest {
 		drop.put("type", "drop");
 		drop.put("baseobject", player.getID().getObjectID());
 		drop.put("baseslot", "bag");
-		drop.put("x", (int)User.get().getX());
-		drop.put("y", (int)User.get().getY() + 1);
+		drop.put("x", player.getX());
+		drop.put("y", player.getY() + 1);
 		drop.put("quantity", "1");
 		drop.put("baseitem", -1);
 		
