1c0aa790d64bcb17db5d4bb83535d07975862f56
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-04-24 06:03:20 +0000

    ported to juni4

--- tests/games/stendhal/common/TestClasspathEntry.java
+++ tests/games/stendhal/common/TestClasspathEntry.java
@@ -2,41 +2,42 @@ package games.stendhal.common;
 
 import games.stendhal.client.update.ClasspathEntry;
 
-public class TestClasspathEntry extends TestCase {
+import org.junit.Assert;
+import org.junit.Test;
 
+public class TestClasspathEntry {
+
+	@Test
 	public void testParsingSimpleJar() {
 		String test = "stendhal-0.54.jar";
 		ClasspathEntry ce = new ClasspathEntry(test);
-		assertEquals(test + " filename", "stendhal-0.54.jar", ce.getFilename());
-		assertEquals(test + " type", "stendhal", ce.getType());
-		assertEquals(test + " version", "0.54", ce.getVersion());
+		Assert.assertEquals(test + " filename", "stendhal-0.54.jar", ce.getFilename());
+		Assert.assertEquals(test + " type", "stendhal", ce.getType());
+		Assert.assertEquals(test + " version", "0.54", ce.getVersion());
 	}
 
+	@Test
 	public void testParsingJarInFolder() {
 		String test = "/tmp/stendhal-0.54.jar";
 		ClasspathEntry ce = new ClasspathEntry(test);
-		assertEquals(test + " filename", "/tmp/stendhal-0.54.jar", ce.getFilename());
-		assertEquals(test + " type", "stendhal", ce.getType());
-		assertEquals(test + " version", "0.54", ce.getVersion());
+		Assert.assertEquals(test + " filename", "/tmp/stendhal-0.54.jar", ce.getFilename());
+		Assert.assertEquals(test + " type", "stendhal", ce.getType());
+		Assert.assertEquals(test + " version", "0.54", ce.getVersion());
 	}
 
+	@Test
 	public void testCompare() {
 		ClasspathEntry v054 = new ClasspathEntry("stendhal-0.54.jar");
 		ClasspathEntry v055 = new ClasspathEntry("stendhal-0.55.jar");
 		ClasspathEntry classes = new ClasspathEntry(".");
 
-		assertTrue("< (1)", v054.compareTo(v055) < 0);
-		assertTrue("> (1)", v055.compareTo(v054) > 0);
-		assertTrue("= (1)", v054.compareTo(v054) == 0);
-		assertTrue("= (2)", v055.compareTo(v055) == 0);
-		assertTrue("= (3)", classes.compareTo(classes) == 0);
-		assertTrue("< (2)", v054.compareTo(classes) < 0);
-		assertTrue("> (2)", classes.compareTo(v054) > 0);
+		Assert.assertTrue("< (1)", v054.compareTo(v055) < 0);
+		Assert.assertTrue("> (1)", v055.compareTo(v054) > 0);
+		Assert.assertTrue("= (1)", v054.compareTo(v054) == 0);
+		Assert.assertTrue("= (2)", v055.compareTo(v055) == 0);
+		Assert.assertTrue("= (3)", classes.compareTo(classes) == 0);
+		Assert.assertTrue("< (2)", v054.compareTo(classes) < 0);
+		Assert.assertTrue("> (2)", classes.compareTo(v054) > 0);
 	}
 
-	public static void main(String[] args) {
-		TestClasspathEntry testCase = new TestClasspathEntry();
-
-		testCase.runTestCase(TestClasspathEntry.class);
-	}
 }
--- tests/games/stendhal/common/TestVersion.java
+++ tests/games/stendhal/common/TestVersion.java
@@ -1,42 +1,46 @@
 package games.stendhal.common;
 
 import games.stendhal.client.update.Version;
+import junit.framework.TestCase;
+
+import org.junit.Assert;
+import org.junit.Test;
 
 public class TestVersion extends TestCase {
 
+	@Test
 	public void testExtractVersion() {
-		assertEquals("extratVersion 0: ", "", Version.cut("0.52.1", 0));
-		assertEquals("extratVersion 1: ", "0", Version.cut("0.52.1", 1));
-		assertEquals("extratVersion 2: ", "0.52", Version.cut("0.52.1", 2));
-		assertEquals("extratVersion 3: ", "0.52.1", Version.cut("0.52.1", 3));
-		assertEquals("extratVersion 4: ", "0.52.1", Version.cut("0.52.1", 4));
+		Assert.assertEquals("extratVersion 0: ", "", Version.cut("0.52.1", 0));
+		Assert.assertEquals("extratVersion 1: ", "0", Version.cut("0.52.1", 1));
+		Assert.assertEquals("extratVersion 2: ", "0.52", Version.cut("0.52.1", 2));
+		Assert.assertEquals("extratVersion 3: ", "0.52.1", Version.cut("0.52.1", 3));
+		Assert.assertEquals("extratVersion 4: ", "0.52.1", Version.cut("0.52.1", 4));
 	}
 
+	@Test
 	public void testCheckVersionCompatibility() {
-		assertTrue("VersionCompatible 0.52 ~ 0.52", Version.checkCompatibility("0.52", "0.52"));
-		assertTrue("VersionCompatible 0.52 ~ 0.52.0", Version.checkCompatibility("0.52", "0.52.0"));
-		assertTrue("VersionCompatible 0.52 ~ 0.52.1", Version.checkCompatibility("0.52", "0.52.1"));
-		assertTrue("VersionCompatible ! 0.53 ~ 0.52", !Version.checkCompatibility("0.53", "0.52"));
-		assertTrue("VersionCompatible ! 0.52 ~ 0.53", !Version.checkCompatibility("0.52", "0.53"));
+		Assert.assertTrue("VersionCompatible 0.52 ~ 0.52", Version.checkCompatibility("0.52", "0.52"));
+		Assert.assertTrue("VersionCompatible 0.52 ~ 0.52.0", Version.checkCompatibility("0.52", "0.52.0"));
+		Assert.assertTrue("VersionCompatible 0.52 ~ 0.52.1", Version.checkCompatibility("0.52", "0.52.1"));
+		Assert.assertTrue("VersionCompatible ! 0.53 ~ 0.52", !Version.checkCompatibility("0.53", "0.52"));
+		Assert.assertTrue("VersionCompatible ! 0.52 ~ 0.53", !Version.checkCompatibility("0.52", "0.53"));
 	}
 
+	@Test
 	public void testCompare() {
-		assertTrue("VersionCompare 0.52 = 0.52", Version.compare("0.52", "0.52") == 0);
-		assertTrue("VersionCompare 0.52 = 0.52.0", Version.compare("0.52", "0.52.0") == 0);
-		assertTrue("VersionCompare 0.52 < 0.52.1", Version.compare("0.52", "0.52.1") < 0);
-		assertTrue("VersionCompare 0.53 > 0.52", Version.compare("0.53", "0.52") > 0);
-		assertTrue("VersionCompare 0.52 < 0.53", Version.compare("0.52", "0.53") < 0);
+		Assert.assertTrue("VersionCompare 0.52 = 0.52", Version.compare("0.52", "0.52") == 0);
+		Assert.assertTrue("VersionCompare 0.52 = 0.52.0", Version.compare("0.52", "0.52.0") == 0);
+		Assert.assertTrue("VersionCompare 0.52 < 0.52.1", Version.compare("0.52", "0.52.1") < 0);
+		Assert.assertTrue("VersionCompare 0.53 > 0.52", Version.compare("0.53", "0.52") > 0);
+		Assert.assertTrue("VersionCompare 0.52 < 0.53", Version.compare("0.52", "0.53") < 0);
 	}
 
+	@Test
 	public void testCompareInt() {
-		assertTrue("VersionCompare 0.2 < 0.10", Version.compare("0.2", "0.10") < 0);
-		assertTrue("VersionCompare 0.10 > 0.2", Version.compare("0.10", "0.2") > 0);
-		assertTrue("VersionCompare 0.02 = 0.2", Version.compare("0.02", "0.2") == 0);
+		Assert.assertTrue("VersionCompare 0.2 < 0.10", Version.compare("0.2", "0.10") < 0);
+		Assert.assertTrue("VersionCompare 0.10 > 0.2", Version.compare("0.10", "0.2") > 0);
+		Assert.assertTrue("VersionCompare 0.02 = 0.2", Version.compare("0.02", "0.2") == 0);
 	}
 
-	public static void main(String[] args) {
-		TestVersion testCase = new TestVersion();
 
-		testCase.runTestCase(TestVersion.class);
-	}
 }
