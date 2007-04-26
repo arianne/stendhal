2284aaa411ee6cba6779b4ae720a303ae2ad05f0
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2007-04-26 14:41:52 +0000

    Added serialization test to layer definition.

--- tests/games/stendhal/server/tools/LayerDefinitionTest.java
+++ tests/games/stendhal/server/tools/LayerDefinitionTest.java
@@ -1,9 +1,18 @@
 package games.stendhal.server.tools;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import games.stendhal.tools.tiled.LayerDefinition;
 import games.stendhal.tools.tiled.StendhalMapStructure;
 import games.stendhal.tools.tiled.TileSetDefinition;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import marauroa.common.net.InputSerializer;
+import marauroa.common.net.OutputSerializer;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -60,6 +69,34 @@ public class LayerDefinitionTest {
 		
 		assertEquals(0,layer.getTileAt(57, 34));
 		assertNull(layer.getTilesetFor(layer.getTileAt(57, 34)));
+	}
+	
+	@Test
+	public void testSerialization() throws IOException, ClassNotFoundException {
+		ByteArrayOutputStream array=new ByteArrayOutputStream();
+		OutputSerializer out=new OutputSerializer(array);
+		
+		LayerDefinition layer=map.getLayer("layer1");
+		layer.writeObject(out);
+		
+		byte[] serialized=array.toByteArray();
+		
+		ByteArrayInputStream sarray=new ByteArrayInputStream(serialized);
+		InputSerializer in=new InputSerializer(sarray);
+		
+		LayerDefinition serializedLayer=(LayerDefinition) in.readObject(new LayerDefinition(0,0));
+		
+		assertEquals(layer.getName(), serializedLayer.getName());
+		assertEquals(layer.getWidth(), serializedLayer.getWidth());
+		assertEquals(layer.getHeight(), serializedLayer.getHeight());
+		assertEquals(layer.exposeRaw().length, serializedLayer.exposeRaw().length);
+		
+		byte[] a=layer.exposeRaw();
+		byte[] b=serializedLayer.exposeRaw();
+		
+		for(int i=0;i<a.length;i++) {
+			assertEquals(a[i],b[i]);
+		}
 	}
 	
 	
