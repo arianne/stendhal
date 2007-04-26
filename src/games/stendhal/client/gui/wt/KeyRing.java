1895f30b16c6300d70c3154a40824b9ed589d74b
Chad F <chad3f@users.sourceforge.net>
2007-04-26 02:24:33 +0000

    Added preliminary code that uses new feature listener. Not active yet.

--- src/games/stendhal/client/gui/wt/KeyRing.java
+++ src/games/stendhal/client/gui/wt/KeyRing.java
@@ -11,16 +11,22 @@ package games.stendhal.client.gui.wt;
 
 import games.stendhal.client.StendhalClient;
 import games.stendhal.client.entity.User;
+import games.stendhal.client.events.FeatureChangeListener;
 
 /**
  * A key ring.
  */
-public class KeyRing extends EntityContainer {
+public class KeyRing extends EntityContainer implements FeatureChangeListener {
 	/**
 	 * Create a key ring.
+	 *
+	 * @param	client		The stendhal client.
 	 */
 	public KeyRing(StendhalClient client) {
 		super(client, "keyring", 2, 3);
+
+		// Not yet
+		// client.addFeatureChangeListener(this);
 	}
 
 
@@ -42,6 +48,41 @@ public class KeyRing extends EntityContainer {
 					setMinimizeable(false);
 					setMinimized(true);
 				}
+			}
+		}
+	}
+
+
+	//
+	// FeatureChangeListener
+	//
+
+	/**
+	 * A feature was disabled.
+	 *
+	 * @param	name		The name of the feature.
+	 */
+	public void featureDisabled(String name) {
+		if(name.equals("keyring")) {
+			if(isMinimizeable()) {
+				setMinimizeable(false);
+				setMinimized(true);
+			}
+		}
+	}
+
+
+	/**
+	 * A feature was enabled.
+	 *
+	 * @param	name		The name of the feature.
+	 * @param	value		Optional feature specific data.
+	 */
+	public void featureEnabled(String name, String value) {
+		if(name.equals("keyring")) {
+			if(!isMinimizeable()) {
+				setMinimizeable(true);
+				setMinimized(false);
 			}
 		}
 	}
