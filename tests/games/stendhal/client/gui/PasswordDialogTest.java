e3fb39c27623e073b229149dd0eb8dce336ddf37
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-05-01 22:22:39 +0000

    removed unnecessary setup teardown

--- tests/games/stendhal/client/gui/PasswordDialogTest.java
+++ tests/games/stendhal/client/gui/PasswordDialogTest.java
@@ -10,26 +10,10 @@ import org.junit.Test;
 
 public class PasswordDialogTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
 	@Test
 	public void testCheckPass() {
 		PasswordDialog pwd = new PasswordDialog();
-		//assertTrue(pwd.checkPass(new char[0], new char[0]));
+		assertTrue(pwd.checkPass(new char[0], new char[0]));
 		char[] pw1 = {'b','l','a'};
 		
 		assertTrue(pwd.checkPass(pw1,pw1));
