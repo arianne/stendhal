d912721eeebbef5c8d6a2da2a973c8fb671d8bd7
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-05-02 15:23:58 +0000

    removed unused imports

--- tests/games/stendhal/client/gui/PasswordDialogTest.java
+++ tests/games/stendhal/client/gui/PasswordDialogTest.java
@@ -1,11 +1,9 @@
 package games.stendhal.client.gui;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class PasswordDialogTest {
