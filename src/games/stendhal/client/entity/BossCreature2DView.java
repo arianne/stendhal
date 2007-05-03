6c35f070622a4e6362495cc12fd59aafef7e5ee1
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2007-05-03 22:25:52 +0000

    Make it nicer. It has two orientations... left and right.

--- src/games/stendhal/client/entity/BossCreature2DView.java
+++ src/games/stendhal/client/entity/BossCreature2DView.java
@@ -5,29 +5,43 @@ import games.stendhal.client.Sprite;
 
 import java.awt.Rectangle;
 import java.awt.geom.Rectangle2D;
+import java.util.HashMap;
 import java.util.Map;
 
 import marauroa.common.game.RPObject;
 
 
 public class BossCreature2DView extends Creature2DView {
-	private AnimatedSprite animatedSprite;
-
 	public BossCreature2DView(BossCreature creature) {
 		super(creature,creature.getWidth(),creature.getHeight());
     }
 
 	@Override
 	protected void buildSprites(Map<String, AnimatedSprite> map, RPObject object) {
-		animatedSprite=new AnimatedSprite(new Sprite[] { getAnimationSprite(object)}, 0L, false);		
+		Sprite tiles=getAnimationSprite(object);
+		
+		map.put(ActiveEntity.STATE_RIGHT, getAnimatedWalk(tiles, 0, getWidth(), getHeight()));
+		map.put(ActiveEntity.STATE_LEFT, getAnimatedWalk(tiles, 1, getWidth(), getHeight()));
 	}
 
+	@Override
+	protected String getDefaultState() {
+		return ActiveEntity.STATE_RIGHT;
+	}
+	
 	@Override
 	public Rectangle2D getDrawnArea() {
 		return new Rectangle.Double(getX(), getY(), getWidth(), getHeight());
 	}
 
 	protected AnimatedSprite getSprite(final String state) {
-		return animatedSprite;
+		if(state.equals(ActiveEntity.STATE_UP)) {
+			return sprites.get(ActiveEntity.STATE_RIGHT);
+		}
+		if(state.equals(ActiveEntity.STATE_DOWN)) {
+			return sprites.get(ActiveEntity.STATE_LEFT);
+		}
+		
+		return sprites.get(state);
 	}
 }
