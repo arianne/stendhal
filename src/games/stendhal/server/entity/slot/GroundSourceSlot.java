03f2c7905ef30759d44a6379f64ebc266c58f995
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-05-06 09:18:10 +0000

    splitted source and target ground slots

--- src/games/stendhal/server/entity/slot/GroundSourceSlot.java
+++ src/games/stendhal/server/entity/slot/GroundSourceSlot.java
@@ -4,12 +4,12 @@ import games.stendhal.server.entity.Entity;
 import games.stendhal.server.entity.item.Item;
 
 /**
- * A pseudo slot representing the ground
+ * A pseudo slot representing the ground as source for an item 
  * (so that the ground has not to be treated special in equipment code).
  *
- * @author brummermann
+ * @author hendrik
  */
-public class GroundSlot implements Slot {
+public class GroundSourceSlot implements Slot {
 	private Item item;
 
 	/**
@@ -17,7 +17,7 @@ public class GroundSlot implements Slot {
 	 *
 	 * @param item item
 	 */
-	public GroundSlot(Item item) {
+	public GroundSourceSlot(Item item) {
 		this.item = item;
 	}
 
@@ -26,30 +26,13 @@ public class GroundSlot implements Slot {
 	}
 
 	public boolean isReachableForTakingThingsOutOfBy(Entity entity) {
-		// Is this a target slot?
-		if (isDestinationSlot()) {
-			return false;
-		}
-
 		// TODO: Check distance
 		return false;
 	}
 
 	public boolean isReachableForThrowingThingsIntoBy(Entity entity) {
-		// Is this a source slot?
-		if (isSourceSlot()) {
-			return false;
-		}
 
-		// TODO: Check distance
+		// this is a source slot
 		return false;
-	}
-
-	private boolean isSourceSlot() {
-		return item != null;
-	}
-
-	private boolean isDestinationSlot() {
-		return item == null;
 	}
 }
---
+++ src/games/stendhal/server/entity/slot/GroundTargetSlot.java
@@ -0,0 +1,46 @@
+package games.stendhal.server.entity.slot;
+
+import games.stendhal.server.StendhalRPZone;
+import games.stendhal.server.entity.Entity;
+
+/**
+ * A pseudo slot representing the ground as target for an item
+ * (so that the ground has not to be treated special in equipment code).
+ *
+ * @author hendrik
+ */
+public class GroundTargetSlot implements Slot {
+	private StendhalRPZone zone;
+	private int x;
+	private int y;
+
+	/**
+	 * generates a new GroundSlot
+	 * 
+	 * @param zone StendhalRPZone
+	 * @param x x
+	 * @param y y
+	 */
+	public GroundTargetSlot(StendhalRPZone zone, int x, int y) {
+	    super();
+	    this.zone = zone;
+	    this.x = x;
+	    this.y = y;
+    }
+
+	public boolean isItemSlot() {
+	    return true;
+    }
+
+	public boolean isReachableForTakingThingsOutOfBy(Entity entity) {
+
+		// this is only a target slot
+		return false;
+	}
+
+	public boolean isReachableForThrowingThingsIntoBy(Entity entity) {
+		// TODO: Check distance
+		return false;
+	}
+
+}
