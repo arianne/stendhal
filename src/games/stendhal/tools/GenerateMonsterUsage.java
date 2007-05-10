ea441f1dd1284713691c20e4c3de62a201882922
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2007-05-10 14:37:58 +0000

    Even better. Now shows creatures that doesn't exist at game.

--- src/games/stendhal/tools/GenerateMonsterUsage.java
+++ src/games/stendhal/tools/GenerateMonsterUsage.java
@@ -2,14 +2,17 @@ package games.stendhal.tools;
 
 import games.stendhal.server.StendhalRPWorld;
 import games.stendhal.server.StendhalRPZone;
+import games.stendhal.server.config.CreaturesXMLLoader;
 import games.stendhal.server.config.ZoneGroupsXMLLoader;
 import games.stendhal.server.entity.creature.Creature;
 import games.stendhal.server.entity.spawner.CreatureRespawnPoint;
+import games.stendhal.server.rule.defaultruleset.DefaultCreature;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import marauroa.common.game.IRPZone;
@@ -23,6 +26,14 @@ public class GenerateMonsterUsage {
 		loader.load();
 		
 		Map<String, Integer> count=new HashMap<String,Integer>();
+
+		CreaturesXMLLoader creatureLoader = CreaturesXMLLoader.get();
+		List<DefaultCreature> creatures = creatureLoader.load("data/conf/creatures.xml");
+		
+		for(DefaultCreature c: creatures) {
+			count.put(c.getCreatureName(), 0);
+		}
+				
 				
 		for(IRPZone zone: StendhalRPWorld.get()) {
 			for(CreatureRespawnPoint p: ((StendhalRPZone)zone).getRespawnPointList()) {
