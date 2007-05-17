e834a8be056b7e0ee0c3335a2866b6fed11d957a
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-05-17 20:51:19 +0000

    added configuration to disable this script

--- src/games/stendhal/server/script/NameChange.java
+++ src/games/stendhal/server/script/NameChange.java
@@ -1,10 +1,11 @@
 package games.stendhal.server.script;
 
-import java.util.List;
-
-import games.stendhal.server.StendhalRPRuleProcessor;
 import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.scripting.ScriptImpl;
+
+import java.util.List;
+
+import marauroa.common.Configuration;
 
 /**
  * Makes client display a fake player name by changing the title attribute. 
@@ -13,9 +14,24 @@ import games.stendhal.server.scripting.ScriptImpl;
  * @author timothyb89
  */
 public class NameChange extends ScriptImpl {
+	private static final String CONFIG_KEY = "stendhal.scripts.namechange.enabled";
 
 	@Override
 	public void execute(Player admin, List<String> args) {
+
+		// check configuration
+		try {
+	        if (!Configuration.getConfiguration().has(CONFIG_KEY) 
+	        	|| Boolean.parseBoolean(Configuration.getConfiguration().get(CONFIG_KEY)) == false) {
+	        	admin.sendPrivateText("This script is disabled in the server configuration file key " + CONFIG_KEY);
+	        	return;
+	        }
+        } catch (Exception e) {
+	        admin.sendPrivateText(e.toString());
+	        return;
+        }
+
+        // do title change
 		if (args.get(0).equals("remove")) {
 			admin.remove("title");
 			admin.sendPrivateText("Your original name has been restored. Please change zones for the changes to take effect.");
