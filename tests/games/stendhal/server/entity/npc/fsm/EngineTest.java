cd02168ed4c687fb552bfff6e218aa08b69a16c4
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-05-19 21:36:08 +0000

    removed unnecessary method and imports

--- tests/games/stendhal/server/entity/npc/fsm/EngineTest.java
+++ tests/games/stendhal/server/entity/npc/fsm/EngineTest.java
@@ -1,38 +1,17 @@
 package games.stendhal.server.entity.npc.fsm;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.SpeakerNPC.ChatAction;
 import games.stendhal.server.entity.npc.SpeakerNPC.ChatCondition;
 import games.stendhal.server.entity.player.Player;
-
 import marauroa.common.game.RPObject;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class EngineTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test (expected=IllegalArgumentException.class)
+	@Test(expected = IllegalArgumentException.class)
 	public void testEngine() {
 		new Engine(null);
 	}
@@ -40,62 +19,64 @@ public class EngineTest {
 	@Test
 	public void testGetFreeState() {
 		Engine en = new Engine(new SpeakerNPC("bob"));
-		assertEquals("creates an integer for a free state",1,en.getFreeState());
-		assertEquals("creates the next integer for a free state",2,en.getFreeState());
-		
+		assertEquals("creates an integer for a free state", 1, en.getFreeState());
+		assertEquals("creates the next integer for a free state", 2, en.getFreeState());
 	}
 
 	@Test
 	public void testAddSingleStringEmptyCondition() {
-		
+
 		Engine en = new Engine(new SpeakerNPC("bob"));
 		int state;
-		state=0;
+		state = 0;
 		String triggers = "boo";
-		
-			int nextState = state +1;
-			String reply = "huch";
-			ChatAction action = new ChatAction(){
 
-				@Override
-				public void fire(Player player, String text, SpeakerNPC npc) {
-					assertEquals("boo",text);
-					
-				}};
+		int nextState = state + 1;
+		String reply = "huch";
+		ChatAction action = new ChatAction() {
+
+			@Override
+			public void fire(Player player, String text, SpeakerNPC npc) {
+				assertEquals("boo", text);
+
+			}
+		};
 		en.add(state, triggers, null, nextState, reply, action);
 		Player pete = new Player(new RPObject());
 		en.step(pete, "boo");
 		assertEquals(nextState, en.getCurrentState());
-		
-	
+
 	}
+
 	@Test
 	public void testAddSingleStringValidCondition() {
 		SpeakerNPC bob = new SpeakerNPC("bob");
-	
+
 		Engine en = new Engine(bob);
 		int state;
-		state=0;
+		state = 0;
 		final String triggers = "boo";
-		
-				ChatCondition cc = new ChatCondition(){
 
-					@Override
-					public boolean fire(Player player, String text, SpeakerNPC npc) {
-						
-						assertEquals(triggers,text);
-						return true;
-					}}; 
-		
-			int nextState = state +1;
-			String reply = "huch";
-			ChatAction action = new ChatAction(){
+		ChatCondition cc = new ChatCondition() {
 
-				@Override
-				public void fire(Player player, String text, SpeakerNPC npc) {
-					
-					assertEquals(triggers,text);
-				}};
+			@Override
+			public boolean fire(Player player, String text, SpeakerNPC npc) {
+
+				assertEquals(triggers, text);
+				return true;
+			}
+		};
+
+		int nextState = state + 1;
+		String reply = "huch";
+		ChatAction action = new ChatAction() {
+
+			@Override
+			public void fire(Player player, String text, SpeakerNPC npc) {
+
+				assertEquals(triggers, text);
+			}
+		};
 		en.add(state, triggers, cc, nextState, reply, action);
 		Player pete = new Player(new RPObject());
 		en.step(pete, triggers);
@@ -103,5 +84,4 @@ public class EngineTest {
 		assertEquals(bob.get("text"), reply);
 	}
 
-	
 }
