8d27c15252de6be0be1278f2eb01552d8b1d5663
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-05-19 09:40:24 +0000

    fixed [ 1704525 ] Stendhal: Sound mute problem

--- src/games/stendhal/client/soundreview/SoundMaster.java
+++ src/games/stendhal/client/soundreview/SoundMaster.java
@@ -4,9 +4,13 @@ import games.stendhal.client.WorldObjects;
 import games.stendhal.client.WorldObjects.WorldListener;
 
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.Line;
 import javax.sound.sampled.LineEvent;
 import javax.sound.sampled.LineListener;
 import javax.sound.sampled.LineUnavailableException;
@@ -16,6 +20,7 @@ public class SoundMaster implements Runnable,WorldListener {
 private static SoundFileMap sfm=null;
 	private static Cliplistener cliplisten=null;
 	private static boolean isMute;
+	public static ConcurrentHashMap<Object, Line> playingClips= new ConcurrentHashMap<Object, Line>();
 
 	public void run() {
 }
@@ -68,6 +73,7 @@ private static SoundFileMap sfm=null;
 if (cl!=null){
 	
 			cl.addLineListener(cliplisten);
+			playingClips.putIfAbsent((Object)cl, cl);
 			if (shallLoop){
 				cl.loop(Clip.LOOP_CONTINUOUSLY);
 			}else{
@@ -76,6 +82,7 @@ if (cl!=null){
 
 			cl.start();
 			}
+			
 			return ac;
 			
 }
@@ -93,12 +100,14 @@ if (cl!=null){
 		// dont remove this please astriddemma 12.04.2007
 		public void update(LineEvent event) {
 			if (event.getType().equals(LineEvent.Type.START)) {
-	          
+				
             }
 			if (event.getType().equals(LineEvent.Type.CLOSE)) {
             }
 			if (event.getType().equals(LineEvent.Type.STOP)) {
 				event.getLine().close();
+				
+				playingClips.remove(event.getLine());
             }
 			
 			if (event.getType().equals(LineEvent.Type.OPEN)) {
@@ -127,7 +136,17 @@ if (cl!=null){
 		
 	}
 	public static void setMute(boolean on) {
+		
+		if (on){
+		Enumeration<Line> enu = playingClips.elements();
+		while (enu.hasMoreElements()){
+			Line lin = 	enu.nextElement();
+			lin.close();
+			
+		}
+		}
 		isMute=on;
+		
 		
 	}
 }
