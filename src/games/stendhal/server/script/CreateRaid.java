c7097351c256a69680af7536b02e16b9c3beab2c
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2007-05-21 21:29:49 +0000

    Refactored the script to create raids.

---
+++ src/games/stendhal/server/script/CreateRaid.java
@@ -0,0 +1,32 @@
+package games.stendhal.server.script;
+
+import java.util.List;
+import java.util.Map;
+
+import games.stendhal.server.StendhalRPZone;
+import games.stendhal.server.entity.creature.Creature;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.scripting.ScriptImpl;
+
+
+public abstract class CreateRaid extends ScriptImpl {
+	
+	abstract protected Map<String,Integer> createArmy();
+	
+	@Override
+	public void execute(Player admin, List<String> args) {
+		// extract position of admin
+		StendhalRPZone myZone = sandbox.getZone(admin);
+		int x = admin.getX();
+		int y = admin.getY();
+		sandbox.setZone(myZone);
+
+		for(Map.Entry<String, Integer> entry: createArmy().entrySet()) {
+			Creature creature = sandbox.getCreature(entry.getKey());
+			
+			for(int i=0;i<entry.getValue();i++) {
+				sandbox.add(creature,x+games.stendhal.common.Rand.rand(0, 30),y+games.stendhal.common.Rand.rand(0, 30));
+			}			
+		}
+	}
+}
--- src/games/stendhal/server/script/DrowAttackRaid.java
+++ src/games/stendhal/server/script/DrowAttackRaid.java
@@ -1,22 +1,15 @@
 package games.stendhal.server.script;
 
-import games.stendhal.server.StendhalRPZone;
-import games.stendhal.server.entity.creature.Creature;
-import games.stendhal.server.entity.player.Player;
-import games.stendhal.server.scripting.ScriptImpl;
-
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
- * @author hendrik
+ * @author miguel
  */
-public class DrowAttackRaid extends ScriptImpl {
-	private static Map<String,Integer> attackArmy;
+public class DrowAttackRaid extends CreateRaid {
 	
-	static {
-		attackArmy=new HashMap<String, Integer>();
+	protected Map<String,Integer> createArmy() {
+		Map<String,Integer> attackArmy=new HashMap<String, Integer>();
 		attackArmy.put("dark_elf",30);
 		attackArmy.put("dark_elf_archer",10);
 		attackArmy.put("dark_elf_elite_archer",5);
@@ -27,29 +20,7 @@ public class DrowAttackRaid extends ScriptImpl {
 		attackArmy.put("dark_elf_viceroy",1);
 		attackArmy.put("dark_elf_sacerdotist",3);
 		attackArmy.put("dark_elf_matronmother",1);
-	}
-
-	@Override
-	public void execute(Player admin, List<String> args) {
-
-		// help
-		if (args.size() == 0) {
-			admin.sendPrivateText("/script DrowAttackRaid <dificulty=[0-10]>");
-			return;
-		}
-
-		// extract position of admin
-		StendhalRPZone myZone = sandbox.getZone(admin);
-		int x = admin.getX();
-		int y = admin.getY();
-		sandbox.setZone(myZone);
-
-		for(Map.Entry<String, Integer> entry: attackArmy.entrySet()) {
-			Creature creature = sandbox.getCreature(entry.getKey());
-			
-			for(int i=0;i<entry.getValue();i++) {
-				sandbox.add(creature,x+games.stendhal.common.Rand.rand(0, 20),y+games.stendhal.common.Rand.rand(0, 20));
-			}			
-		}
+		
+		return attackArmy;
 	}
 }
