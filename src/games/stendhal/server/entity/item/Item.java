536a2a56122cd6880a7b55e64542a4ef6f174022
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2007-05-21 15:56:42 +0000

    Fixed item not showing RATE attack

--- src/games/stendhal/server/entity/item/Item.java
+++ src/games/stendhal/server/entity/item/Item.java
@@ -296,29 +296,18 @@ public class Item extends PassiveEntity implements TurnListener, EquipListener {
 		if (has("bound")) {
 			text = text + " It is a special quest reward for " + get("bound") + " and cannot be used by others.";
 		}
+
 		if (has("atk")) {
-			atk = get("atk");
+			stats += " ATK: " + get("atk");
 		}
 		if (has("def")) {
-			def = get("def");
+			stats += " DEF: " + get("def");
 		}
 		if (has("rate")) {
-			rate=get("rate");
+			stats += " RATE: " + get("rate");
 		}
 		if (has("amount")) {
-			amount = get("amount");
-		}
-		if (!atk.equals("0")) {
-			stats += " ATK: " + atk;
-		}
-		if (!def.equals("0")) {
-			stats += " DEF: " + def;
-		}
-		if (!rate.equals("0")) {
-			stats += " RATE: " + def;
-		}
-		if (!amount.equals("0")) {
-			stats += " HP: " + amount;
+			stats += " HP: " + get("amount");
 		}
 		if (stats.length() > 0) {
 			stats = " Stats are (" + stats.trim() + ").";
