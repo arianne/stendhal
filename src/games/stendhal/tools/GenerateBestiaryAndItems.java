b250fdeaa6fe281e191d4de53a97429ab1eacb60
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2007-05-24 08:51:08 +0000

    Test if I am working on the branch or not...

--- src/games/stendhal/tools/GenerateBestiaryAndItems.java
+++ src/games/stendhal/tools/GenerateBestiaryAndItems.java
@@ -11,6 +11,7 @@ import java.util.Comparator;
 import java.util.List;
 
 public class GenerateBestiaryAndItems {
+	/* TESTING Branching... */
 
 	public static void generateCreatures() throws Exception {
 		CreaturesXMLLoader creatureLoader = CreaturesXMLLoader.get();
