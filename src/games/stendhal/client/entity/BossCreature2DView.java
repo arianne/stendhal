e8adf55443bdcd0ef8b40234bd5856a63bb1f05d
Chad F <chad3f@users.sourceforge.net>
2007-05-26 00:08:03 +0000

    Adjusted for single frame images.

--- src/games/stendhal/client/entity/BossCreature2DView.java
+++ src/games/stendhal/client/entity/BossCreature2DView.java
@@ -1,6 +1,8 @@
 package games.stendhal.client.entity;
 
+import games.stendhal.client.GameScreen;
 import games.stendhal.client.Sprite;
+import games.stendhal.client.SpriteStore;
 
 import java.util.Map;
 
@@ -8,16 +10,34 @@ import java.util.Map;
 public class BossCreature2DView extends Creature2DView {
 	public BossCreature2DView(BossCreature creature) {
 		super(creature);
-    }
+	}
 
+
+	//
+	// Creature2DView
+	//
+
+	/*
+	 * Populate named state sprites.
+	 *
+	 * This only has a single frame for left and right direction.
+	 *
+	 * @param	map		The map to populate.
+	 */
 	@Override
 	protected void buildSprites(Map<Object, Sprite> map) {
 		Sprite tiles=getAnimationSprite();
-		
-		map.put(ActiveEntity.STATE_RIGHT, getAnimatedWalk(tiles, 0, getWidth(), getHeight()));
-		map.put(ActiveEntity.STATE_LEFT, getAnimatedWalk(tiles, 1, getWidth(), getHeight()));
+		SpriteStore store = SpriteStore.get();
 
-		map.put(ActiveEntity.STATE_UP, map.get(ActiveEntity.STATE_RIGHT));
-		map.put(ActiveEntity.STATE_DOWN, map.get(ActiveEntity.STATE_LEFT));
+		int pWidth = (int) (getWidth() * GameScreen.SIZE_UNIT_PIXELS);
+		int pHeight = (int) (getHeight() * GameScreen.SIZE_UNIT_PIXELS);
+
+		Sprite right = store.getTile(tiles, 0, 0, pWidth, pHeight);
+		Sprite left = store.getTile(tiles, 0, pHeight, pWidth, pHeight);
+
+		map.put(ActiveEntity.STATE_RIGHT, right);
+		map.put(ActiveEntity.STATE_LEFT, left);
+		map.put(ActiveEntity.STATE_UP, right);
+		map.put(ActiveEntity.STATE_DOWN, left);
 	}
 }
