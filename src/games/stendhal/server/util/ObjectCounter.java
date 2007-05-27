666f9ca8035c4dbfcba5687e79524792f0b6e653
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-05-27 14:10:36 +0000

    javadoc

--- src/games/stendhal/server/util/ObjectCounter.java
+++ src/games/stendhal/server/util/ObjectCounter.java
@@ -13,14 +13,27 @@ import java.util.Map;
 public class ObjectCounter<K> {
 	private Map<K, Integer> counter = new HashMap<K, Integer>();
 
+	/**
+	 * clears the counter
+	 */
 	public void clear() {
 		counter.clear();
 	}
 
+	/**
+	 * gets the counter map
+	 *
+	 * @return Map
+	 */
 	public Map<K, Integer> getMap() {
 		return counter;
 	}
-	
+
+	/**
+	 * adds one to the appropriate entry
+	 *
+	 * @param o object
+	 */
 	public void add(K o) {
 		Integer in = counter.get(o);
 		if (in == null) {
