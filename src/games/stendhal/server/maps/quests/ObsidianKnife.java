cf33e4373be048d20113b8cf1f1d7cb6f8545e7d
Katie Russell <kymara@users.sourceforge.net>
2007-06-02 15:58:48 +0000

    Obsidian Knife quest - NPC lives in kobold city. item stats may change after testing more

---
+++ src/games/stendhal/server/maps/quests/ObsidianKnife.java
@@ -0,0 +1,473 @@
+package games.stendhal.server.maps.quests;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import games.stendhal.common.Rand;
+import games.stendhal.server.StendhalRPWorld;
+import games.stendhal.server.entity.item.Item;
+import games.stendhal.server.entity.npc.ConversationPhrases;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.util.TimeUtil;
+
+/**
+ * QUEST: The Obsidian Knife
+ * 
+ * PARTICIPANTS:
+ * <ul>
+ *  <li>Alrak, a dwarf who abandoned the mountain dwarves to live in Kobold City</li>
+ *  <li>Ceryl, the librarian in Semos</li>
+ * </ul>
+ * 
+ * STEPS:
+ * <ul>
+ *  <li>Alrak is hungry and asks for 100 pieces of ham, cheese or meat</li>
+ *  <li>Then, Alrak is bored and asks for a book</li>
+ *  <li>Get the book from Ceryl, and remember the name of who it is for</li>
+ *  <li>Bring the book to Alrak - he reads it for 3 days</li>
+ *  <li>After 3 days Alrak has learned how to make a knife from obsidian</li>
+ *  <li>Get obsidian for the blade and a fish for the fish bone handle</li> 
+ *  <li>Alrak makes the knife for you</li>
+ * </ul>
+ * 
+ * REWARD:
+ * <ul>
+ *  <li>Obsidian Knife</li>
+ *  <li>3600 XP</li>
+ * </ul>
+ *
+ * REPETITIONS:
+ * <ul>
+ *  <li>None</li>
+ * </ul>
+ */
+public class ObsidianKnife extends AbstractQuest {
+
+	private static final int REQUIRED_FOOD = 100;
+	
+	private static final List<String> FOOD_LIST =  Arrays.asList("ham", "meat", "cheese" );
+	
+	private static final int REQUIRED_DAYS = 3;
+	
+	private static final int REQUIRED_MINUTES = 10;
+
+	private static final String QUEST_SLOT = "obsidian_knife";
+
+	private static final String FISH = "cod";
+	
+	private static final List<String> NAMES = Arrays.asList("Alrak", "alrak");
+
+	@Override
+	public void init(String name) {
+		super.init(name, QUEST_SLOT);
+	}
+
+	private void prepareQuestOfferingStep() {
+		SpeakerNPC npc = npcs.get("Alrak");
+
+		npc.add(ConversationStates.ATTENDING,
+				ConversationPhrases.QUEST_MESSAGES,
+				null,
+		        ConversationStates.QUEST_ITEM_QUESTION,
+		        null,
+		        new SpeakerNPC.ChatAction() {
+			        @Override
+			        public void fire(Player player, String text, SpeakerNPC npc) {
+				        if (!player.hasQuest(QUEST_SLOT) || /*player.hasQuest(QUEST_SLOT)&&*/player.getQuest(QUEST_SLOT).equals("rejected")) {
+					        npc.say("You know, it's hard to get food round here. I don't have any #supplies for next year.");
+				        } else if (player.isQuestCompleted(QUEST_SLOT)) {
+					        npc.say("I'm inspired to work again! I'm making things for Wrvil now. Thanks for getting me interested in forging again.");
+				        } else {
+					        npc.say("I'm sure I asked you to do something for me, already.");
+				        }
+			        }
+		        });
+		/* Player agrees to collect the food asked for. his quest slot gets set with it 
+		 *so that later when he returns and says the food name, Alrak can check if that 
+		 *was the food type he was asked to bring. */
+		npc.add(ConversationStates.QUEST_OFFERED,
+				ConversationPhrases.YES_MESSAGES,
+				null,
+				ConversationStates.ATTENDING,
+			        null,
+				new SpeakerNPC.ChatAction() {
+					@Override
+					public void fire(Player player, String text, SpeakerNPC npc) {
+						String food = player.getQuest(QUEST_SLOT);
+					        npc.say("Thank you! I hope it doesn't take too long to collect. Don't forget to say '" + food + "' when you have it.");
+						//player.setQuest(QUEST_SLOT, food);
+						player.addKarma(5.0);
+						// set food to null?
+					}
+				});
+		//Player says no. they might get asked to bring a different food next time but they've lost karma.
+		npc.add(ConversationStates.QUEST_OFFERED,
+                ConversationPhrases.NO_MESSAGES,
+                null,
+                ConversationStates.IDLE,
+                "I'm not sure how I'll survive next year now. Good bye, cruel soul.",
+                new SpeakerNPC.ChatAction() {
+					@Override
+					public void fire(Player player, String text, SpeakerNPC engine) {
+						player.addKarma(-5.0);
+						player.setQuest(QUEST_SLOT, "rejected");
+					}
+				});
+		// Player asks what supplies he needs, and a random choice of what he wants is made.
+		npc.add(ConversationStates.QUEST_ITEM_QUESTION,
+				"supplies",
+				null,
+				ConversationStates.QUEST_OFFERED,
+				 null,
+			        new SpeakerNPC.ChatAction() {
+				        @Override
+				        public void fire(Player player, String text, SpeakerNPC npc) {
+				        	String food = Rand.rand(FOOD_LIST);
+				        	player.setQuest(QUEST_SLOT, food);
+				    		npc.say("If you could get me " + REQUIRED_FOOD + " pieces of " + food + ", I'd be in your debt. Will you help me?");
+				        }
+			        });
+	}
+
+	private void bringFoodStep() {
+		SpeakerNPC npc = npcs.get("Alrak");
+		/** If player has quest and has brought the food, and says so, take it */
+		for (String item : FOOD_LIST) {	
+			npc.add(ConversationStates.ATTENDING,
+				item,
+				new SpeakerNPC.ChatCondition() {
+					@Override
+					public boolean fire(Player player, String text, SpeakerNPC npc) {
+						return player.hasQuest(QUEST_SLOT)
+								&& player.getQuest(QUEST_SLOT).equals(text) 
+										&& player.isEquipped(text,REQUIRED_FOOD);
+					}
+				},
+				ConversationStates.QUEST_ITEM_BROUGHT,
+				null,
+				new SpeakerNPC.ChatAction() {
+					@Override
+					    public void fire(Player player, String text, SpeakerNPC npc) {
+						if (player.drop(text, REQUIRED_FOOD)) {
+							player.addKarma(15.0);
+							player.addXP(100);
+							npc.say("Great! you brought the " + text + "!");
+							player.setQuest(QUEST_SLOT, "food_brought");
+							player.notifyWorldAboutChanges();
+							npc.setCurrentState(ConversationStates.ATTENDING);
+						}
+					}
+				}
+			);
+		}
+	}
+	
+
+private void requestBookStep() {
+	SpeakerNPC npc = npcs.get("Alrak");
+	
+	npc.add(ConversationStates.ATTENDING,
+			ConversationPhrases.QUEST_MESSAGES,
+			null,
+	        ConversationStates.QUEST_ITEM_BROUGHT,
+	        null,
+	        new SpeakerNPC.ChatAction() {
+		        @Override
+		        public void fire(Player player, String text, SpeakerNPC npc) {
+		        	if (player.hasQuest(QUEST_SLOT)&&player.getQuest(QUEST_SLOT).equals("food_brought")) {
+			        	npc.say("Now I'm less worried about food I've realised I'm bored. There's a #book I'd love to read.");
+			        } 
+		        }
+	        });
+	
+	npc.add(ConversationStates.QUEST_ITEM_BROUGHT,
+			"book",
+			null,
+			ConversationStates.QUEST_ITEM_BROUGHT,
+			"It's about gems and minerals. I doubt you'd be interested ... but do you think you could get it somehow?",
+			null
+			);
+	
+	npc.add(ConversationStates.QUEST_ITEM_BROUGHT,
+			ConversationPhrases.NO_MESSAGES,
+			null,
+			ConversationStates.IDLE,
+			"Shame, I would really like to learn more about precious stones. Ah well, good bye.",
+			null
+			);
+	
+	npc.add(ConversationStates.QUEST_ITEM_BROUGHT,
+			ConversationPhrases.YES_MESSAGES,
+			null,
+			ConversationStates.ATTENDING,
+			null,
+			new SpeakerNPC.ChatAction() {
+				@Override
+				    public void fire(Player player, String text, SpeakerNPC npc) {
+						player.addKarma(10.0);
+						npc.say("Thanks. Try asking at a library for a 'gem_book'.");
+						player.setQuest(QUEST_SLOT, "seeking_book");
+						player.notifyWorldAboutChanges();
+				}
+			}
+		);
+	
+}
+
+private void getBookStep() {
+	SpeakerNPC npc = npcs.get("Ceryl");
+	
+	npc.add(ConversationStates.ATTENDING,
+			"gem_book",
+			new SpeakerNPC.ChatCondition() {
+				@Override
+				public boolean fire(Player player, String text, SpeakerNPC npc) {
+					return player.hasQuest(QUEST_SLOT)
+							&& player.getQuest(QUEST_SLOT).equals("seeking_book");
+				}
+			},
+			ConversationStates.QUESTION_1,
+			"You're in luck! Ognir brought it back just last week. Now, who is it for?",
+			null);
+	
+for (String name : NAMES) {	
+	npc.add(ConversationStates.QUESTION_1,
+			name,
+			null,
+		    ConversationStates.QUESTION_1,
+		    null,
+		    new SpeakerNPC.ChatAction() {
+			@Override
+			    public void fire(Player player, String text, SpeakerNPC npc) {
+			    	npc.say("Ah, the mountain dwarf! Hope he enjoys the gem_book.");
+			    	Item item = StendhalRPWorld.get().getRuleManager().getEntityManager().getItem("book_blue");
+					item.put("bound", player.getName());
+					player.equip(item, true);	    	
+				}
+			}
+		);
+	}
+
+ // player says something which isn't the dwarf's name.
+ npc.add(ConversationStates.QUESTION_1, "",
+	new SpeakerNPC.ChatCondition() {
+	    @Override
+		public boolean fire(Player player, String text, SpeakerNPC npc) {
+		return !NAMES.contains(text);
+	    }
+	},
+	ConversationStates.QUESTION_1, "Hm, you better check who it's really for.", null);
+	
+	
+}
+
+private void bringBookStep() {
+	SpeakerNPC npc = npcs.get("Alrak");
+	npc.add(ConversationStates.IDLE,
+			ConversationPhrases.GREETING_MESSAGES,
+	        new SpeakerNPC.ChatCondition() {
+		        @Override
+		        public boolean fire(Player player, String text, SpeakerNPC npc) {
+			        return player.hasQuest(QUEST_SLOT)
+			                && player.getQuest(QUEST_SLOT).equals("seeking_book")
+			        	    && player.isEquipped("book_blue");
+		        }
+	        },
+	        ConversationStates.IDLE,
+	        null,
+	        new SpeakerNPC.ChatAction() {
+	        	@Override
+		        public void fire(Player player, String text, SpeakerNPC npc) {
+	        	 player.drop("book_blue");    
+		         npc.say("Great! I think I'll read this for a while. Bye!");
+		         player.addXP(500);
+	        	 player.setQuest(QUEST_SLOT, "reading;" + System.currentTimeMillis());
+	        	 npc.setCurrentState(ConversationStates.IDLE);
+		        }
+	         }
+	        );
+	npc.add(ConversationStates.IDLE,
+			ConversationPhrases.GREETING_MESSAGES,
+	        new SpeakerNPC.ChatCondition() {
+		        @Override
+		        public boolean fire(Player player, String text, SpeakerNPC npc) {
+			        return player.hasQuest(QUEST_SLOT)
+			                && player.getQuest(QUEST_SLOT).equals("seeking_book")
+			        	    && !player.isEquipped("book_blue");
+		        }
+	        },
+	        ConversationStates.ATTENDING,
+ "Hello again. I hope you haven't forgotten about the gem_book I wanted.", 
+			    null);
+	        
+
+	
+}
+
+private void offerKnifeStep() {
+	
+	SpeakerNPC npc = npcs.get("Alrak");
+
+		npc.add(ConversationStates.IDLE,
+				ConversationPhrases.GREETING_MESSAGES,
+		        new SpeakerNPC.ChatCondition() {
+			        @Override
+			        public boolean fire(Player player, String text, SpeakerNPC npc) {
+				        return player.hasQuest(QUEST_SLOT)
+				                && player.getQuest(QUEST_SLOT).startsWith("reading;");
+			        }
+		        },
+		        ConversationStates.IDLE,
+		        null,
+		        new SpeakerNPC.ChatAction() {
+			        @Override
+			        public void fire(Player player, String text, SpeakerNPC npc) {
+				        String[] tokens = player.getQuest(QUEST_SLOT).split(";");
+				        long delay = REQUIRED_DAYS * 60 *60 * 1000; // milliseconds in REQUIRED_DAYS days
+				        long timeRemaining = (Long.parseLong(tokens[1]) + delay)
+				                - System.currentTimeMillis();
+				        if (timeRemaining > 0L) {
+					        npc.say("I haven't finished reading that book. Maybe I'll be done in "
+			                        + TimeUtil.approxTimeUntil((int) (timeRemaining / 1000L))
+			                        + ".");
+					        return;
+				        }
+				        npc.say("I've finished reading! That was really interesting. I learned how to make a special #knife from #obsidian.");
+				        player.setQuest(QUEST_SLOT, "knife_offered");
+				        player.notifyWorldAboutChanges();
+				        npc.setCurrentState(ConversationStates.QUEST_2_OFFERED);
+			        }
+		        });
+
+
+		npc.add(ConversationStates.QUEST_2_OFFERED,
+				"obsidian",
+				null,
+				ConversationStates.QUEST_2_OFFERED,
+				 "That book says that the black gem, obsidian, can be used to make a very sharp cutting edge. Fascinating! If you bring me some I'll make a #knife for you.", 
+			    null);
+		
+		npc.add(ConversationStates.QUEST_2_OFFERED,
+				"knife",
+				null,
+				ConversationStates.QUEST_2_OFFERED,
+				 "I'll make an obsidian knife if you can get the gem which makes the blade. Bring a " + FISH + " bone to make the handle, too.", 
+			    null);
+		
+		npc.add(ConversationStates.IDLE,
+				ConversationPhrases.GREETING_MESSAGES,
+		        new SpeakerNPC.ChatCondition() {
+			        @Override
+			        public boolean fire(Player player, String text, SpeakerNPC npc) {
+				        return player.hasQuest(QUEST_SLOT)
+				                && player.getQuest(QUEST_SLOT).equals("knife_offered")
+				        	    && player.isEquipped("obsidian")&& player.isEquipped(FISH);
+			        }
+		        },
+		        ConversationStates.IDLE,
+		        null,
+		        new SpeakerNPC.ChatAction() {
+		        	@Override
+			        public void fire(Player player, String text, SpeakerNPC npc) {
+		        	 player.drop("obsidian"); 
+		        	 player.drop(FISH);
+			         npc.say("You found the gem for the blade and the fish bone to make the handle! I'll start work right away. Come back in "
+					        		+ REQUIRED_MINUTES + " minutes.");
+		        	 player.setQuest(QUEST_SLOT, "forging;" + System.currentTimeMillis());
+			        }
+		         }
+		       );
+		
+		npc.add(ConversationStates.IDLE,
+				ConversationPhrases.GREETING_MESSAGES,
+		        new SpeakerNPC.ChatCondition() {
+			        @Override
+			        public boolean fire(Player player, String text, SpeakerNPC npc) {
+				        return player.hasQuest(QUEST_SLOT)
+				                && player.getQuest(QUEST_SLOT).startsWith("forging;");
+			        }
+		        },
+		        ConversationStates.IDLE,
+		        null,
+		        new SpeakerNPC.ChatAction() {
+			        @Override
+			        public void fire(Player player, String text, SpeakerNPC npc) {
+				        String[] tokens = player.getQuest(QUEST_SLOT).split(";");
+				        long delay = REQUIRED_MINUTES * 60 * 1000; // minutes -> milliseconds
+				        long timeRemaining = (Long.parseLong(tokens[1]) + delay)
+				                - System.currentTimeMillis();
+				        if (timeRemaining > 0L) {
+					        npc.say("I haven't finished making the knife. Please check back in "
+			                        + TimeUtil.approxTimeUntil((int) (timeRemaining / 1000L))
+			                        + ".");
+					        return;
+				        }
+				        npc.say("The knife is ready! You know, that was enjoyable. I think I'll start making things again. Thanks!");
+				        player.addXP(3000);
+				        Item knife = StendhalRPWorld.get().getRuleManager()
+				                .getEntityManager().getItem("obsidian_knife");
+				        knife.put("bound", player.getName());
+				        player.equip(knife, true);
+				        player.setQuest(QUEST_SLOT, "done");
+				        player.notifyWorldAboutChanges();
+			        }
+		        });
+
+		// Here because of the random response from different actions bug, we define the greeting message for all cases not covered so far:
+
+	npc.add(ConversationStates.IDLE,
+			ConversationPhrases.GREETING_MESSAGES,
+	        new SpeakerNPC.ChatCondition() {
+		        @Override
+		        public boolean fire(Player player, String text, SpeakerNPC npc) {
+			        return !player.hasQuest(QUEST_SLOT)||player.isQuestCompleted(QUEST_SLOT)||player.hasQuest(QUEST_SLOT)&&player.getQuest(QUEST_SLOT).equals("food_brought") || player.hasQuest(QUEST_SLOT)&&player.getQuest(QUEST_SLOT).equals("start")||player.hasQuest(QUEST_SLOT)&&player.getQuest(QUEST_SLOT).equals("meat")||player.hasQuest(QUEST_SLOT)&&player.getQuest(QUEST_SLOT).equals("ham")||player.hasQuest(QUEST_SLOT)&&player.getQuest(QUEST_SLOT).equals("cheese");
+		        }
+	        },
+	        ConversationStates.ATTENDING,
+"How did you get down here? I usually only see #kobolds.", 
+			    null);
+
+	}
+
+	@Override
+	public void addToWorld() {
+		super.addToWorld();
+
+		prepareQuestOfferingStep();
+		bringFoodStep();
+		requestBookStep();	
+		getBookStep();
+		bringBookStep();
+		offerKnifeStep();
+	}
+
+	@Override
+	public List<String> getHistory(Player player) {
+		List<String> res = new ArrayList<String>();
+		if (!player.hasQuest(QUEST_SLOT)) {
+			return res;
+		}
+		res.add("FIRST_CHAT");
+		String questState = player.getQuest(QUEST_SLOT);
+		if (questState.equals("rejected")) {
+			res.add("QUEST_REJECTED");
+		}
+		if (player.isQuestInState(QUEST_SLOT, "start", "done")) {
+			res.add("QUEST_ACCEPTED");
+		}
+		if ((questState.equals("start") && player.isEquipped("goblet"))
+		        || questState.equals("done")) {
+			res.add("FOUND_ITEM");
+		}
+		if (player.getQuest(QUEST_SLOT).startsWith("forging;")) {
+			res.add("FORGING");
+		}
+		if (questState.equals("done")) {
+			res.add("DONE");
+		}
+		return res;
+	}
+}
---
+++ src/games/stendhal/server/maps/wofol/blacksmith/BlacksmithNPC.java
@@ -0,0 +1,70 @@
+package games.stendhal.server.maps.wofol.blacksmith;
+
+import games.stendhal.server.StendhalRPZone;
+import games.stendhal.server.entity.npc.NPCList;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.maps.ZoneConfigurator;
+import games.stendhal.server.pathfinder.Path;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Configure Wofol Blacksmith (-1_semos_mine_nw).
+ * 
+ * @author kymara
+ */
+public class BlacksmithNPC implements ZoneConfigurator {
+
+	private NPCList npcs;
+
+	public BlacksmithNPC() {
+		this.npcs = NPCList.get();
+	}
+
+	/**
+	 * Configure a zone.
+	 *
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+	public void configureZone(StendhalRPZone zone, Map<String, String> attributes) {
+		buildBlacksmith(zone);
+	}
+
+	private void buildBlacksmith(StendhalRPZone zone) {
+		SpeakerNPC dwarf = new SpeakerNPC("Alrak") {
+
+			@Override
+			protected void createPath() {
+				List<Path.Node> nodes = new LinkedList<Path.Node>();
+				nodes.add(new Path.Node(22, 7));
+				nodes.add(new Path.Node(22, 6));
+				nodes.add(new Path.Node(17, 6));
+				nodes.add(new Path.Node(17, 1));
+				nodes.add(new Path.Node(8, 1));
+				nodes.add(new Path.Node(8, 7));
+				setPath(nodes, true);
+			}
+
+			@Override
+			protected void createDialog() {
+				//addGreeting("How did you get down here? I usually only see #kobolds.");
+				addJob("I am a blacksmith. I was a mountain dwarf but I left that lot behind me. Good riddance, I say!");
+				addHelp("");
+				addReply("kobolds","You know, those odd furry creatures. Don't get much conversation out of any except #Wrvil.");
+				addReply("Wrvil","He runs a trading business not far from here. I used to make the odd item for him, but don't have any energy left.");
+				addGoodbye();
+			} //remaining behaviour defined in maps.quests.ObsidianKnife
+		};
+
+		dwarf.setDescription("You see Alrak, a reclusive dwarf smith.");
+		zone.assignRPObjectID(dwarf);
+		dwarf.put("class", "dwarfnpc");
+		dwarf.set(22, 7);
+		dwarf.initHP(100);
+		npcs.add(dwarf);
+		zone.add(dwarf);
+	}
+}
