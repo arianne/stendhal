3de2fc97034090a06c4641088dd2f5b7fbbc8a4c
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-06-28 18:26:34 +0000

    inserted VM arg 		System.setProperty("sun.java2d.pmoffscreen","false"); into main

--- src/games/stendhal/client/stendhal.java
+++ src/games/stendhal/client/stendhal.java
@@ -155,7 +155,7 @@ public class stendhal extends Thread {
 		//get size string
 		if (System.getProperty("stendhal.refactoringgui") != null) SCREEN_SIZE = "1000x480";
 		else SCREEN_SIZE="640x480";
-	    
+		System.setProperty("sun.java2d.pmoffscreen","false");
 		parseCommandlineArguments(args);
 		startLogSystem();
 		startSwingLookAndFeel();
