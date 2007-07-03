43c8269004d455ced7e96fa6bb57cc93f84b5d3b
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2007-07-03 15:54:32 +0000

    Ported to 2.0.

--- src/games/stendhal/bot/shouter/ShouterMain.java
+++ src/games/stendhal/bot/shouter/ShouterMain.java
@@ -22,13 +22,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import marauroa.client.ariannexpTimeoutException;
-import marauroa.client.net.DefaultPerceptionListener;
+import marauroa.client.TimeoutException;
+import marauroa.client.net.IPerceptionListener;
 import marauroa.client.net.PerceptionHandler;
 import marauroa.common.game.RPAction;
 import marauroa.common.game.RPObject;
-import marauroa.common.net.MessageS2CPerception;
-import marauroa.common.net.TransferContent;
+import marauroa.common.net.message.MessageS2CPerception;
+import marauroa.common.net.message.TransferContent;
 
 /**
  * Connects to the server and shouts a message.
@@ -47,13 +47,11 @@ public class ShouterMain {
 
 	private String port;
 
-	private boolean tcp;
-
 	protected long lastPerceptionTimestamp = 0;
 
 	protected Map<RPObject.ID, RPObject> world_objects;
 
-	protected marauroa.client.ariannexp clientManager;
+	protected marauroa.client.ClientFramework clientManager;
 
 	protected PerceptionHandler handler;
 
@@ -65,32 +63,62 @@ public class ShouterMain {
 	 * @param p password
 	 * @param c character name
 	 * @param P port
-	 * @param t TCP?
 	 * @throws SocketException on an network error
 	 */
-	public ShouterMain(String h, String u, String p, String c, String P, boolean t) throws SocketException {
+	public ShouterMain(String h, String u, String p, String c, String P) throws SocketException {
 		host = h;
 		username = u;
 		password = p;
 		character = c;
 		port = P;
-		tcp = t;
 
 		world_objects = new HashMap<RPObject.ID, RPObject>();
 
-		handler = new PerceptionHandler(new DefaultPerceptionListener() {
+		handler = new PerceptionHandler(new IPerceptionListener() {
+			public boolean onAdded(RPObject object) {
+	            return false;
+            }
 
-			@Override
-			public int onException(Exception e, marauroa.common.net.MessageS2CPerception perception) {
+			public boolean onClear() {
+	            return false;
+            }
+
+			public boolean onDeleted(RPObject object) {
+	            return false;
+            }
+
+			public void onException(Exception exception, MessageS2CPerception perception) {
 				System.out.println(perception);
 				System.err.println(perception);
-				e.printStackTrace();
-				return 0;
-			}
+				exception.printStackTrace();
+            }
 
+			public boolean onModifiedAdded(RPObject object, RPObject changes) {
+	            return false;
+            }
+
+			public boolean onModifiedDeleted(RPObject object, RPObject changes) {
+	            return false;
+            }
+
+			public boolean onMyRPObject(RPObject added, RPObject deleted) {
+	            return false;
+            }
+
+			public void onPerceptionBegin(byte type, int timestamp) {
+            }
+
+			public void onPerceptionEnd(byte type, int timestamp) {
+            }
+
+			public void onSynced() {
+            }
+
+			public void onUnsynced() {
+            }
 		});
 
-		clientManager = new marauroa.client.ariannexp("games/stendhal/log4j.properties") {
+		clientManager = new marauroa.client.ClientFramework("games/stendhal/log4j.properties") {
 
 			@Override
 			protected String getGameName() {
@@ -108,7 +136,7 @@ public class ShouterMain {
 				try {
 					handler.apply(message, world_objects);
 				} catch (Exception e) {
-					onError(3, "Exception while applying perception");
+					e.printStackTrace();
 				}
 			}
 
@@ -127,13 +155,6 @@ public class ShouterMain {
 			}
 
 			@Override
-			protected void onError(int code, String reason) {
-				System.out.println(reason);
-				System.err.println(reason);
-				Runtime.getRuntime().halt(1);
-			}
-
-			@Override
 			protected void onAvailableCharacters(String[] characters) {
 				try {
 					chooseCharacter(character);
@@ -146,12 +167,17 @@ public class ShouterMain {
 			protected void onTransfer(List<TransferContent> items) {
 				// do nothing
 			}
+
+			@Override
+            protected void onPreviousLogins(List<String> previousLogins) {
+				// do nothing
+            }
 		};
 	}
 
 	public void script() {
 		try {
-			clientManager.connect(host, Integer.parseInt(port), tcp);
+			clientManager.connect(host, Integer.parseInt(port));
 			clientManager.login(username, password);
 			readMessagesAndShoutThem();
 			clientManager.logout();
@@ -161,7 +187,7 @@ public class ShouterMain {
 		} catch (SocketException e) {
 			System.err.println("Socket Exception");
 			Runtime.getRuntime().halt(1);
-		} catch (ariannexpTimeoutException e) {
+		} catch (TimeoutException e) {
 			System.err.println("Cannot connect to Stendhal server. Server is down?");
 			Runtime.getRuntime().halt(1);
 		} catch (Exception e) {
@@ -206,7 +232,6 @@ public class ShouterMain {
 				String character = null;
 				String host = null;
 				String port = null;
-				boolean tcp = false;
 
 				while (i != args.length) {
 					if (args[i].equals("-u")) {
@@ -219,14 +244,12 @@ public class ShouterMain {
 						host = args[i + 1];
 					} else if (args[i].equals("-P")) {
 						port = args[i + 1];
-					} else if (args[i].equals("-t")) {
-						tcp = true;
 					}
 					i++;
 				}
 
 				if ((username != null) && (password != null) && (character != null) && (host != null) && (port != null)) {
-					ShouterMain shouter = new ShouterMain(host, username, password, character, port, tcp);
+					ShouterMain shouter = new ShouterMain(host, username, password, character, port);
 					shouter.script();
 					return;
 				}
@@ -242,8 +265,6 @@ public class ShouterMain {
 			System.out.println("* -u\tUsername to log into Marauroa server");
 			System.out.println("* -p\tPassword to log into Marauroa server");
 			System.out.println("* -c\tCharacter used to log into Marauroa server");
-			System.out.println("Optional parameters");
-			System.out.println("* -t\tuse tcp-connection to server");
 		} catch (Exception e) {
 			e.printStackTrace(System.err);
 			System.exit(1);
