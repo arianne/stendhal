5df316c52bfc9940b532d24b409c32f91ebdcdd1
Katie Russell <kymara@users.sourceforge.net>
2007-07-10 12:46:09 +0000

    NPC and quest files for club of thorns quest

---
+++ src/games/stendhal/server/maps/kotoch/OrcSamanNPC.java
@@ -0,0 +1,71 @@
+package games.stendhal.server.maps.kotoch;
+
+import games.stendhal.server.StendhalRPZone;
+import games.stendhal.server.entity.Sign;
+
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.NPCList;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.maps.ZoneConfigurator;
+import games.stendhal.server.pathfinder.FixedPath;
+import games.stendhal.server.pathfinder.Path;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author kymara
+ */
+public class OrcSamanNPC implements ZoneConfigurator {
+
+	private NPCList npcs = NPCList.get();
+
+
+	/**
+	 * Configure a zone.
+	 *
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+	public void configureZone(StendhalRPZone zone, Map<String, String> attributes) {
+		buildNPC(zone);
+	}
+
+	private void buildNPC(StendhalRPZone zone) {
+		SpeakerNPC npc = new SpeakerNPC("Orc Saman") {
+
+			@Override
+			protected void createPath() {
+				List<Path.Node> nodes = new LinkedList<Path.Node>();
+				nodes.add(new Path.Node(8, 112));
+				nodes.add(new Path.Node(16, 112));
+				nodes.add(new Path.Node(16, 114));
+				nodes.add(new Path.Node(22, 114));
+				nodes.add(new Path.Node(22, 118));				
+				nodes.add(new Path.Node(8, 118));
+				setPath(new FixedPath(nodes, true));
+			}
+
+			@Override
+			protected void createDialog() {
+				addGreeting("Oof.");
+				addJob("Me, Orc Saman.");
+				addHelp("Orc Saman need help! Make #task.");
+				add(ConversationStates.ATTENDING, "offer", null, ConversationStates.ATTENDING,
+				        "No trade.", null);
+ 				addGoodbye("see yoo.");
+			}
+		};
+		npc.setDescription("You see an Orc Saman.");
+		npcs.add(npc);
+		zone.assignRPObjectID(npc);
+		npc.put("class", "orcsamannpc");
+		npc.set(8,112);
+		npc.initHP(100);
+		zone.add(npc);
+
+
+	}
+}
