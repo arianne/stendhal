a3aed5eb0a9194243d3f69a726f961b313ea8dc4
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-08-11 14:00:12 +0000

    centralized accessors methods which are spread all over the code

--- src/games/stendhal/server/config/factory/ConfigurableFactoryContext.java
+++ src/games/stendhal/server/config/factory/ConfigurableFactoryContext.java
@@ -19,10 +19,6 @@ public class ConfigurableFactoryContext {
 		this.attributes = attributes;
 	}
 
-	//
-	// ConfigurableFactoryContext
-	//
-
 	/**
 	 * Get an attribute.
 	 *
@@ -30,8 +26,77 @@ public class ConfigurableFactoryContext {
 	 *
 	 * @return	The value of the attribute, or <code>null</code> if
 	 *		not set.
+	 * @deprecated use type safe methods
 	 */
+	@Deprecated
 	public String getAttribute(String name) {
 		return attributes.get(name);
+	}
+
+	/**
+	 * gets an attribute.
+	 *
+	 * @param  name          the attribute name.
+	 * @param  defaultValue  the default value it case it is not defined
+	 * @return the value of  the attribute
+	 * @throws IllegalArgumentException in case the value is not a valid integer
+	 */
+	protected int getInt(String name, int defaultValue) throws IllegalArgumentException {
+		String value = attributes.get(name);
+		if (value == null) {
+			return defaultValue;
+		}
+
+		try {
+			return Integer.parseInt(value);
+		} catch (NumberFormatException ex) {
+			throw new IllegalArgumentException("Invalid '" + name + "' attribute: " + value);
+		}
+	}
+
+	/**
+	 * gets an attribute.
+	 *
+	 * @param  name          the attribute name.
+	 * @return the value of  the attribute
+	 * @throws IllegalArgumentException in case the value is not a valid integer or is missing
+	 */
+	protected int getRequiredInt(String name) throws IllegalArgumentException {
+		String value = this.getRequiredString(name);
+		try {
+			return Integer.parseInt(value);
+		} catch (NumberFormatException ex) {
+			throw new IllegalArgumentException("Invalid '" + name + "' attribute: " + value);
+		}
+	}
+
+	/**
+	 * gets an attribute.
+	 *
+	 * @param  name          the attribute name.
+	 * @param  defaultValue  the default value it case it is not defined
+	 * @return the value of  the attribute
+	 */
+	protected String getString(String name, String defaultValue) {
+		String value = attributes.get(name);
+		if (value == null) {
+			return defaultValue;
+		}
+		return value;
+	}
+
+	/**
+	 * gets an attribute.
+	 *
+	 * @param  name          the attribute name.
+	 * @return the value of  the attribute
+	 * @throws IllegalArgumentException in case is missing
+	 */
+	protected String getRequiredString(String name) throws IllegalArgumentException {
+		String value = attributes.get(name);
+		if (value == null) {
+			throw new IllegalArgumentException("Missing required attribute " + name);
+		}
+		return value;
 	}
 }
