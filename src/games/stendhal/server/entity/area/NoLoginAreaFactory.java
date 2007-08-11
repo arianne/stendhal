c3033c04738b177410d308bbd8376c66b9f8f902
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-08-11 13:19:43 +0000

    new area which relocated players on login

---
+++ src/games/stendhal/server/entity/area/NoLoginAreaFactory.java
@@ -0,0 +1,86 @@
+package games.stendhal.server.entity.area;
+
+import games.stendhal.common.ConfigurableFactory;
+import games.stendhal.common.ConfigurableFactoryContext;
+
+/**
+ * A base factory for <code>NoLoginArea</code> objects.
+ */
+public class NoLoginAreaFactory implements ConfigurableFactory {
+	
+	/**
+	 * Extract the width from context
+	 *
+	 * @param	ctx		The configuration context.
+	 *
+	 * @return	The width, 1 if unspecified.
+	 *
+	 * @throws	IllegalArgumentException If the attribute is invalid.
+	 */
+	protected int getWidth(ConfigurableFactoryContext ctx) throws IllegalArgumentException {
+		String s;
+
+		if ((s = ctx.getAttribute("width")) == null) {
+			return 1;
+		}
+
+		try {
+			return Integer.parseInt(s);
+		} catch (NumberFormatException ex) {
+			throw new IllegalArgumentException("Invalid 'width' attribute: " + s);
+		}
+	}
+
+	/**
+	 * Extract the height from context
+	 *
+	 * @param	ctx		The configuration context.
+	 *
+	 * @return	The height, 1 if unspecified.
+	 *
+	 * @throws	IllegalArgumentException If the attribute is invalid.
+	 */
+	protected int getHeight(ConfigurableFactoryContext ctx) throws IllegalArgumentException {
+		String s;
+
+		if ((s = ctx.getAttribute("height")) == null) {
+			return 1;
+		}
+
+		try {
+			return Integer.parseInt(s);
+		} catch (NumberFormatException ex) {
+			throw new IllegalArgumentException("Invalid 'height' attribute: " + s);
+		}
+	}
+
+
+	protected int getNewX(ConfigurableFactoryContext ctx) throws IllegalArgumentException {
+		String s;
+		if ((s = ctx.getAttribute("new-x")) == null) {
+			throw new IllegalArgumentException("Required attribute 'new-x' missing");
+		}
+		try {
+			return Integer.parseInt(s);
+		} catch (NumberFormatException ex) {
+			throw new IllegalArgumentException("Invalid 'damage' attribute: " + s);
+		}
+	}
+
+	protected int getNewY(ConfigurableFactoryContext ctx) throws IllegalArgumentException {
+		String s;
+		if ((s = ctx.getAttribute("new-y")) == null) {
+			throw new IllegalArgumentException("Required attribute 'new-x' missing");
+		}
+		try {
+			return Integer.parseInt(s);
+		} catch (NumberFormatException ex) {
+			throw new IllegalArgumentException("Invalid 'damage' attribute: " + s);
+		}
+	}
+
+
+	public NoLoginArea create(ConfigurableFactoryContext ctx) throws IllegalArgumentException {
+		return new NoLoginArea(getWidth(ctx), getHeight(ctx), getNewX(ctx), getNewY(ctx));
+	}
+}
