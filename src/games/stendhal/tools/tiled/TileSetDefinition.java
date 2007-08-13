c33aa514d365074da593b629eb89d6252b71e488
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-08-13 21:24:35 +0000

    added hashcode as this already had implemented equals

--- src/games/stendhal/tools/tiled/TileSetDefinition.java
+++ src/games/stendhal/tools/tiled/TileSetDefinition.java
@@ -8,32 +8,38 @@ import marauroa.common.net.OutputSerializer;
 import marauroa.common.net.Serializable;
 
 /**
- * Stores a definition of a tileset.
- * Mainly its name, the source image used and the starting global id.
- * 
+ * Stores a definition of a tileset. Mainly its name, the source image used and
+ * the starting global id.
+ *
  * @author miguel
  *
  */
 public class TileSetDefinition implements Serializable {
 	/** The name of the tileset. Useless */
 	private String name;
+
 	/** The source image of this tileset */
 	private String source;
-	/** The id where this tileset begins to number tiles. */ 
-	private int gid;			
+
+	/** The id where this tileset begins to number tiles. */
+	private int gid;
 
 	/**
 	 * Constructor
-	 * @param name the *useless* name of the tileset.
-	 * @param firstGid the id where this tileset begins to number tiles.
+	 *
+	 * @param name
+	 *            the *useless* name of the tileset.
+	 * @param firstGid
+	 *            the id where this tileset begins to number tiles.
 	 */
 	public TileSetDefinition(String name, int firstGid) {
-		this.name=name;
-		this.gid=firstGid;
-    }
-	
+		this.name = name;
+		this.gid = firstGid;
+	}
+
 	/**
 	 * Returns the id where this tileset begins to number tiles
+	 *
 	 * @return the id where this tileset begins to number tiles
 	 */
 	public int getFirstGid() {
@@ -41,49 +47,68 @@ public class TileSetDefinition implements Serializable {
 	}
 
 	/**
-	 * Set the filename of the source image of the tileset. 
-	 * @param attributeValue the filename
+	 * Set the filename of the source image of the tileset.
+	 *
+	 * @param attributeValue
+	 *            the filename
 	 */
 	public void setSource(String attributeValue) {
-		this.source=attributeValue;
-    }
-	
+		this.source = attributeValue;
+	}
+
 	/**
 	 * Returns the filename of the source image of the tileset.
+	 *
 	 * @return the filename of the source image of the tileset.
 	 */
 	public String getSource() {
-		return source;		
+		return source;
 	}
-	
+
 	public byte[] encode() throws IOException {
 		ByteArrayOutputStream array = new ByteArrayOutputStream();
 		OutputSerializer out = new OutputSerializer(array);
-		
+
 		writeObject(out);
-		
+
 		return array.toByteArray();
-    }
+	}
 
 	public void readObject(InputSerializer in) throws IOException {
-		name=in.readString();
-		source=in.readString();
-		gid=in.readInt();
-    }
+		name = in.readString();
+		source = in.readString();
+		gid = in.readInt();
+	}
 
 	public void writeObject(OutputSerializer out) throws IOException {
 		out.write(name);
 		out.write(source);
-		out.write(gid);		
-    }
-	
+		out.write(gid);
+	}
+
 	@Override
 	public boolean equals(Object object) {
-		if(!(object instanceof TileSetDefinition)) {
+		if (object instanceof TileSetDefinition) {
+
+			TileSetDefinition set = (TileSetDefinition) object;
+			return set.name.equals(name) && set.source.equals(source)
+					&& set.gid == gid;
+		} else {
 			return false;
 		}
-		
-		TileSetDefinition set=(TileSetDefinition) object;
-		return set.name.equals(name) && set.source.equals(source) && set.gid==gid;
+
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 1;
+		if (name != null) {
+			hash = hash * name.hashCode();
+		}
+		if (source != null) {
+			hash = hash * source.hashCode();
+		}
+		hash = hash + gid;
+		return hash * super.hashCode();
 	}
 }
