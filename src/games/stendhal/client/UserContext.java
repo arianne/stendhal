4f4815f7dea173116a1ba0a1ae715e5f6cc367bd
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-08-14 22:02:20 +0000

    code beautify

--- src/games/stendhal/client/UserContext.java
+++ src/games/stendhal/client/UserContext.java
@@ -19,12 +19,12 @@ import marauroa.common.game.RPObject;
 import marauroa.common.game.RPSlot;
 
 /**
- * The player user context. This class holds/manages the data for the user
- * of this client. This is independent of any on-screen representation
- * Entity that, while related, serves an entirely different purpose.
+ * The player user context. This class holds/manages the data for the user of
+ * this client. This is independent of any on-screen representation Entity that,
+ * while related, serves an entirely different purpose.
  *
- * Currently this is just a helper class for StendhalClient. Maybe it will
- * be directly used by other code later.
+ * Currently this is just a helper class for StendhalClient. Maybe it will be
+ * directly used by other code later.
  *
  * THIS CLASS IS STILL UNDER DEVELOPMENT/TESTING.
  */
@@ -32,38 +32,37 @@ public class UserContext implements RPObjectChangeListener {
 	/**
 	 * The currently known buddies
 	 */
-	protected HashMap<String, Boolean>	buddies;
+	protected HashMap<String, Boolean> buddies;
 
 	/**
 	 * The currently enabled features.
 	 */
-	protected HashMap<String, String>	features;
+	protected HashMap<String, String> features;
 
 	/**
 	 * The buddy change listeners.
 	 */
-	protected BuddyChangeListener []	buddyListeners;
+	protected BuddyChangeListener[] buddyListeners;
 
 	/**
 	 * The feature change listeners.
 	 */
-	protected FeatureChangeListener []	featureListeners;
+	protected FeatureChangeListener[] featureListeners;
 
 	/**
 	 * The admin level.
 	 */
-	protected int				adminlevel;
+	protected int adminlevel;
 
 	/**
 	 * The player character's name.
 	 */
-	protected String			name;
+	protected String name;
 
 	/**
 	 * The owned sheep RPObject ID.
 	 */
-	protected int				sheepID;
-
+	protected int sheepID;
 
 	public UserContext() {
 		adminlevel = 0;
@@ -75,7 +74,6 @@ public class UserContext implements RPObjectChangeListener {
 		featureListeners = new FeatureChangeListener[0];
 	}
 
-
 	//
 	// UserContext
 	//
@@ -83,11 +81,11 @@ public class UserContext implements RPObjectChangeListener {
 	/**
 	 * Add a buddy change listener.
 	 *
-	 * @param	l		The listener.
+	 * @param l
+	 *            The listener.
 	 */
 	public void addBuddyChangeListener(BuddyChangeListener l) {
-		BuddyChangeListener []	newListeners;
-
+		BuddyChangeListener[] newListeners;
 
 		int len = buddyListeners.length;
 
@@ -98,15 +96,14 @@ public class UserContext implements RPObjectChangeListener {
 		buddyListeners = newListeners;
 	}
 
-
 	/**
 	 * Add a feature change listener.
 	 *
-	 * @param	l		The listener.
+	 * @param l
+	 *            The listener.
 	 */
 	public void addFeatureChangeListener(FeatureChangeListener l) {
-		FeatureChangeListener []	newListeners;
-
+		FeatureChangeListener[] newListeners;
 
 		int len = featureListeners.length;
 
@@ -117,185 +114,174 @@ public class UserContext implements RPObjectChangeListener {
 		featureListeners = newListeners;
 	}
 
-
 	/**
 	 * Fire admin level change event to all registered listeners.
 	 *
-	 * @param	adminLevel	The new admin level.
+	 * @param adminLevel
+	 *            The new admin level.
 	 */
 	protected void fireAdminLevelChanged(int adminLevel) {
 		// TODO: Impl
 	}
 
-
 	/**
 	 * Fire buddy added to all registered listeners.
 	 *
-	 * @param	buddyName	The name of the buddy.
+	 * @param buddyName
+	 *            The name of the buddy.
 	 */
 	protected void fireBuddyAdded(String buddyName) {
-		BuddyChangeListener [] listeners = buddyListeners;
+		BuddyChangeListener[] listeners = buddyListeners;
 
-//System.err.println("fireBuddyAdded() - buddyName = " + buddyName);
-		for(BuddyChangeListener l : listeners) {
+		// System.err.println("fireBuddyAdded() - buddyName = " + buddyName);
+		for (BuddyChangeListener l : listeners) {
 			l.buddyAdded(buddyName);
 		}
 	}
 
-
 	/**
 	 * Fire buddy offline to all registered listeners.
 	 *
-	 * @param	buddyName	The name of the buddy.
+	 * @param buddyName
+	 *            The name of the buddy.
 	 */
 	protected void fireBuddyOffline(String buddyName) {
-		BuddyChangeListener [] listeners = buddyListeners;
+		BuddyChangeListener[] listeners = buddyListeners;
 
-		for(BuddyChangeListener l : listeners) {
+		for (BuddyChangeListener l : listeners) {
 			l.buddyOffline(buddyName);
 		}
 	}
 
-
 	/**
 	 * Fire buddy online to all registered listeners.
 	 *
-	 * @param	buddyName	The name of the buddy.
+	 * @param buddyName
+	 *            The name of the buddy.
 	 */
 	protected void fireBuddyOnline(String buddyName) {
-		BuddyChangeListener [] listeners = buddyListeners;
+		BuddyChangeListener[] listeners = buddyListeners;
 
-		for(BuddyChangeListener l : listeners) {
+		for (BuddyChangeListener l : listeners) {
 			l.buddyOnline(buddyName);
 		}
 	}
 
-
 	/**
 	 * Fire buddy removed to all registered listeners.
 	 *
-	 * @param	buddyName	The name of the buddy.
+	 * @param buddyName
+	 *            The name of the buddy.
 	 */
 	protected void fireBuddyRemoved(String buddyName) {
-		BuddyChangeListener [] listeners = buddyListeners;
+		BuddyChangeListener[] listeners = buddyListeners;
 
-//System.err.println("fireBuddyRemoved() - buddyName = " + buddyName);
-		for(BuddyChangeListener l : listeners) {
+		// System.err.println("fireBuddyRemoved() - buddyName = " + buddyName);
+		for (BuddyChangeListener l : listeners) {
 			l.buddyRemoved(buddyName);
 		}
 	}
 
-
 	/**
 	 * Fire feature enabled to all registered listeners.
 	 *
-	 * @param	name		The name of the feature.
+	 * @param name
+	 *            The name of the feature.
 	 */
 	protected void fireFeatureDisabled(String name) {
-		FeatureChangeListener [] listeners = featureListeners;
+		FeatureChangeListener[] listeners = featureListeners;
 
-		for(FeatureChangeListener l : listeners) {
+		for (FeatureChangeListener l : listeners) {
 			l.featureDisabled(name);
 		}
 	}
 
-
 	/**
 	 * Fire feature enabled to all registered listeners.
 	 *
-	 * @param	name		The name of the feature.
-	 * @param	value		The optional feature value.
+	 * @param name
+	 *            The name of the feature.
+	 * @param value
+	 *            The optional feature value.
 	 */
 	protected void fireFeatureEnabled(String name, String value) {
-		FeatureChangeListener [] listeners = featureListeners;
+		FeatureChangeListener[] listeners = featureListeners;
 
-		for(FeatureChangeListener l : listeners) {
+		for (FeatureChangeListener l : listeners) {
 			l.featureEnabled(name, value);
 		}
 	}
 
-
 	/**
 	 * Fire name change event to all registered listeners.
 	 *
-	 * @param	name		The new player name.
+	 * @param name
+	 *            The new player name.
 	 */
 	protected void fireNameChanged(String name) {
 		// TODO: Impl
 	}
 
-
 	/**
 	 * Get the admin level.
 	 *
-	 * @return	The admin level.
+	 * @return The admin level.
 	 */
 	public int getAdminLevel() {
 		return adminlevel;
 	}
 
-
 	/**
 	 * Get the player character name.
 	 *
-	 * @return	The player character name.
+	 * @return The player character name.
 	 */
 	public String getName() {
 		return name;
 	}
 
-
 	/**
 	 * Get the player's owned sheep RPObject ID.
 	 *
-	 * @return	The RPObject ID of the sheep the player owns,
-	 *		or <code>0</code> if none.
+	 * @return The RPObject ID of the sheep the player owns, or <code>0</code>
+	 *         if none.
 	 */
 	public int getSheepID() {
 		return sheepID;
 	}
 
-
 	/**
 	 * Determine if the user is an admin.
 	 *
-	 * @return	<code>true</code> is the user is an admin.
+	 * @return <code>true</code> is the user is an admin.
 	 */
 	public boolean isAdmin() {
 		return (getAdminLevel() != 0);
 	}
 
-
 	/**
 	 * Remove a buddy change listener.
 	 *
-	 * @param	listener	The listener.
+	 * @param listener
+	 *            The listener.
 	 */
 	public void removeBuddyChangeListener(BuddyChangeListener listener) {
-		BuddyChangeListener []	newListeners;
-		int			idx;
-
+		BuddyChangeListener[] newListeners;
+		int idx;
 
 		idx = buddyListeners.length;
 
-		while(idx-- != 0) {
-			if(buddyListeners[idx] == listener) {
-				newListeners =
-					new BuddyChangeListener[
-						buddyListeners.length - 1];
+		while (idx-- != 0) {
+			if (buddyListeners[idx] == listener) {
+				newListeners = new BuddyChangeListener[buddyListeners.length - 1];
 
-				if(idx != 0) {
-					System.arraycopy(
-						buddyListeners, 0,
-						newListeners, 0,
-						idx);
+				if (idx != 0) {
+					System.arraycopy(buddyListeners, 0, newListeners, 0, idx);
 				}
 
-				if(++idx != buddyListeners.length) {
-					System.arraycopy(
-						buddyListeners, idx,
-						newListeners, idx - 1,
-						buddyListeners.length - idx);
+				if (++idx != buddyListeners.length) {
+					System.arraycopy(buddyListeners, idx, newListeners,
+							idx - 1, buddyListeners.length - idx);
 				}
 
 				buddyListeners = newListeners;
@@ -304,37 +290,29 @@ public class UserContext implements RPObjectChangeListener {
 		}
 	}
 
-
 	/**
 	 * Remove a feature change listener.
 	 *
-	 * @param	listener	The listener.
+	 * @param listener
+	 *            The listener.
 	 */
 	public void removeFeatureChangeListener(FeatureChangeListener listener) {
-		FeatureChangeListener []	newListeners;
-		int				idx;
-
+		FeatureChangeListener[] newListeners;
+		int idx;
 
 		idx = featureListeners.length;
 
-		while(idx-- != 0) {
-			if(featureListeners[idx] == listener) {
-				newListeners =
-					new FeatureChangeListener[
-						featureListeners.length - 1];
+		while (idx-- != 0) {
+			if (featureListeners[idx] == listener) {
+				newListeners = new FeatureChangeListener[featureListeners.length - 1];
 
-				if(idx != 0) {
-					System.arraycopy(
-						featureListeners, 0,
-						newListeners, 0,
-						idx);
+				if (idx != 0) {
+					System.arraycopy(featureListeners, 0, newListeners, 0, idx);
 				}
 
-				if(++idx != featureListeners.length) {
-					System.arraycopy(
-						featureListeners, idx,
-						newListeners, idx - 1,
-						featureListeners.length - idx);
+				if (++idx != featureListeners.length) {
+					System.arraycopy(featureListeners, idx, newListeners,
+							idx - 1, featureListeners.length - idx);
 				}
 
 				featureListeners = newListeners;
@@ -343,14 +321,14 @@ public class UserContext implements RPObjectChangeListener {
 		}
 	}
 
-
 	/**
 	 * A buddy list object added/changed attribute(s).
 	 *
-	 * @param	changes		The object changes.
+	 * @param changes
+	 *            The object changes.
 	 */
 	protected void processBuddiesAdded(final RPObject changes) {
-		for(String key : changes) {
+		for (String key : changes) {
 			/*
 			 * FIXME60 - Remove underscore prefix before DB reset.
 			 */
@@ -362,16 +340,15 @@ public class UserContext implements RPObjectChangeListener {
 
 			Boolean online = (changes.getInt(key) != 0);
 
-
-			if(!buddies.containsKey(buddyName)) {
+			if (!buddies.containsKey(buddyName)) {
 				buddies.put(buddyName, online);
 				fireBuddyAdded(buddyName);
 
-				if(online) {
+				if (online) {
 					fireBuddyOnline(buddyName);
 				}
-			} else if(buddies.get(buddyName) != online) {
-				if(online) {
+			} else if (buddies.get(buddyName) != online) {
+				if (online) {
 					fireBuddyOnline(buddyName);
 				} else {
 					fireBuddyOffline(buddyName);
@@ -380,14 +357,14 @@ public class UserContext implements RPObjectChangeListener {
 		}
 	}
 
-
 	/**
 	 * A buddy list object removed attribute(s).
 	 *
-	 * @param	changes		The object changes.
+	 * @param changes
+	 *            The object changes.
 	 */
 	protected void processBuddiesRemoved(final RPObject changes) {
-		for(String key : changes) {
+		for (String key : changes) {
 			/*
 			 * FIXME60 - Remove underscore prefix before DB reset.
 			 */
@@ -397,44 +374,43 @@ public class UserContext implements RPObjectChangeListener {
 
 			String buddyName = key.substring(1);
 
-			if(buddies.remove(buddyName) != null) {
+			if (buddies.remove(buddyName) != null) {
 				fireBuddyRemoved(buddyName);
 			}
 		}
 	}
 
-
 	/**
 	 * A feature object added/changed attribute(s).
 	 *
-	 * @param	changes		The object changes.
+	 * @param changes
+	 *            The object changes.
 	 */
 	protected void processFeatureAdded(final RPObject changes) {
-		for(String name : changes) {
-			if(!features.containsKey(name)) {
-				String value = changes.get(name);
+		for (String featureName : changes) {
+			if (!features.containsKey(featureName)) {
+				String value = changes.get(featureName);
 
-				features.put(name, value);
-				fireFeatureEnabled(name, value);
+				features.put(featureName, value);
+				fireFeatureEnabled(featureName, value);
 			}
 		}
 	}
-
 
 	/**
 	 * A feature object removed attribute(s).
 	 *
-	 * @param	changes		The object changes.
+	 * @param changes
+	 *            The object changes.
 	 */
 	protected void processFeatureRemoved(final RPObject changes) {
-		for(String name : changes) {
-			if(features.containsKey(name)) {
-				features.remove(name);
-				fireFeatureDisabled(name);
+		for (String featureName : changes) {
+			if (features.containsKey(featureName)) {
+				features.remove(featureName);
+				fireFeatureDisabled(featureName);
 			}
 		}
 	}
-
 
 	//
 	// RPObjectChangeListener
@@ -443,110 +419,120 @@ public class UserContext implements RPObjectChangeListener {
 	/**
 	 * An object was added.
 	 *
-	 * @param	object		The object.
+	 * @param object
+	 *            The object.
 	 */
 	public void onAdded(final RPObject object) {
-		if(object.has("adminlevel")) {
+		if (object.has("adminlevel")) {
 			adminlevel = object.getInt("adminlevel");
-//			fireAdminLevelChanged(adminlevel);
+			// fireAdminLevelChanged(adminlevel);
 		}
 
-
-		if(object.hasSlot("!buddy")) {
+		if (object.hasSlot("!buddy")) {
 			RPSlot slot = object.getSlot("!buddy");
 
-			if(slot.size() != 0) {
+			if (slot.size() != 0) {
 				processBuddiesAdded(slot.getFirst());
 			}
 		}
 	}
 
-
 	/**
 	 * The object added/changed attribute(s).
 	 *
-	 * @param	object		The base object.
-	 * @param	changes		The changes.
+	 * @param object
+	 *            The base object.
+	 * @param changes
+	 *            The changes.
 	 */
 	public void onChangedAdded(final RPObject object, final RPObject changes) {
-		if(changes.has("adminlevel")) {
+		if (changes.has("adminlevel")) {
 			adminlevel = changes.getInt("adminlevel");
 			fireAdminLevelChanged(adminlevel);
 		}
 
-		if(changes.has("name")) {
+		if (changes.has("name")) {
 			name = changes.get("name");
 			fireNameChanged(name);
 		}
 
-		if(changes.has("sheep")) {
+		if (changes.has("sheep")) {
 			sheepID = changes.getInt("sheep");
-//			fireOwnedSheep(sheepID);
+			// fireOwnedSheep(sheepID);
 		}
 	}
-
 
 	/**
 	 * A slot object added/changed attribute(s).
 	 *
-	 * @param	container	The base container object.
-	 * @param	slotName	The container's slot name.
-	 * @param	object		The base slot object.
-	 * @param	changes		The slot changes.
+	 * @param container
+	 *            The base container object.
+	 * @param slotName
+	 *            The container's slot name.
+	 * @param object
+	 *            The base slot object.
+	 * @param changes
+	 *            The slot changes.
 	 */
-	public void onChangedAdded(final RPObject container, final String slotName, final RPObject object, final RPObject changes) {
-//System.err.println("onChangedAdded() - slotName = " + slotName);
+	public void onChangedAdded(final RPObject container, final String slotName,
+			final RPObject object, final RPObject changes) {
+		// System.err.println("onChangedAdded() - slotName = " + slotName);
 
-		if(slotName.equals("!buddy")) {
+		if (slotName.equals("!buddy")) {
 			processBuddiesAdded(changes);
 		}
 
 	}
 
-
 	/**
 	 * An object removed attribute(s).
 	 *
-	 * @param	object		The base object.
-	 * @param	changes		The changes.
+	 * @param object
+	 *            The base object.
+	 * @param changes
+	 *            The changes.
 	 */
 	public void onChangedRemoved(final RPObject object, final RPObject changes) {
-		if(changes.has("adminlevel")) {
+		if (changes.has("adminlevel")) {
 			adminlevel = 0;
 			fireAdminLevelChanged(adminlevel);
 		}
 
-		if(changes.has("name")) {
+		if (changes.has("name")) {
 			name = null;
 			fireNameChanged(name);
 		}
 
-		if(changes.has("sheep")) {
+		if (changes.has("sheep")) {
 			sheepID = 0;
-//			fireOwnedSheep(sheepID);
+			// fireOwnedSheep(sheepID);
 		}
 	}
-
 
 	/**
 	 * A slot object removed attribute(s).
 	 *
-	 * @param	container	The base container object.
-	 * @param	slotName	The container's slot name.
-	 * @param	object		The base slot object.
-	 * @param	changes		The slot changes.
+	 * @param container
+	 *            The base container object.
+	 * @param slotName
+	 *            The container's slot name.
+	 * @param object
+	 *            The base slot object.
+	 * @param changes
+	 *            The slot changes.
 	 */
-	public void onChangedRemoved(final RPObject container, final String slotName, final RPObject object, final RPObject changes) {
-		if(slotName.equals("!buddy")) {
+	public void onChangedRemoved(final RPObject container,
+			final String slotName, final RPObject object, final RPObject changes) {
+		if (slotName.equals("!buddy")) {
 			processBuddiesRemoved(changes);
 		}
 	}
 
-
 	/**
 	 * An object was removed.
 	 *
-	 * @param	object		The object.
+	 * @param object
+	 *            The object.
 	 */
 	public void onRemoved(final RPObject object) {
 		adminlevel = 0;
@@ -556,12 +542,12 @@ public class UserContext implements RPObjectChangeListener {
 		fireNameChanged(null);
 
 		sheepID = 0;
-//		fireSheepOwned(sheepID);
+		// fireSheepOwned(sheepID);
 
 		/*
 		 * When object goes away, buddies also go
 		 */
-		for(String buddyName : buddies.keySet()) {
+		for (String buddyName : buddies.keySet()) {
 			fireBuddyRemoved(buddyName);
 		}
 
