cd807248b1e5babec4bb9b6f7ea965d9eb56a2dd
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-08-15 10:13:51 +0000

    avoid inner assignment

--- src/games/stendhal/server/entity/creature/Pet.java
+++ src/games/stendhal/server/entity/creature/Pet.java
@@ -12,15 +12,12 @@
  ***************************************************************************/
 package games.stendhal.server.entity.creature;
 
-
 import games.stendhal.server.StendhalRPRuleProcessor;
 import games.stendhal.server.entity.Entity;
 import games.stendhal.server.entity.item.Item;
 import games.stendhal.server.entity.player.Player;
 
 import java.util.Set;
-
-import javax.management.AttributeNotFoundException;
 
 import marauroa.common.Log4J;
 import marauroa.common.Logger;
@@ -30,10 +27,9 @@ import marauroa.common.game.SyntaxException;
 import marauroa.common.game.Definition.Type;
 
 /**
- * A pet is a domestic animal that can be owned by a player.
- * Ideally it eats meat from the ground.
- * They move faster than sheep
- * Ideally pets attack weak animals for you
+ * A pet is a domestic animal that can be owned by a player. Ideally it eats
+ * meat from the ground. They move faster than sheep Ideally pets attack weak
+ * animals for you
  */
 /**
  * @author kymara
@@ -47,17 +43,17 @@ public abstract class Pet extends DomesticAnimal {
 	/**
 	 * The weight at which the pet will stop eating.
 	 */
-	final public int MAX_WEIGHT = 100;
+	public final int MAX_WEIGHT = 100;
 
-	 protected static int HP = 100;
+	protected static int HP = 100;
 
-	 protected static int incHP = 5;
+	protected static int incHP = 5;
 
-	 protected static int ATK = 10;
+	protected static int ATK = 10;
 
-	 protected static int DEF = 20;
+	protected static int DEF = 20;
 
-	 protected static int XP;
+	protected static int XP;
 
 	protected int hunger;
 
@@ -74,6 +70,7 @@ public abstract class Pet extends DomesticAnimal {
 
 	/**
 	 * Creates a new wild Pet.
+	 *
 	 * @throws AttributeNotFoundException
 	 */
 	public Pet() {
@@ -82,12 +79,12 @@ public abstract class Pet extends DomesticAnimal {
 
 	/**
 	 * Creates a new Pet that is owned by a player.
+	 *
 	 * @throws AttributeNotFoundException
 	 */
 	public Pet(Player owner) {
 		super(owner);
-		baseSpeed= 0.5;
-
+		baseSpeed = 0.5;
 
 		setATK(ATK);
 		setDEF(DEF);
@@ -100,21 +97,25 @@ public abstract class Pet extends DomesticAnimal {
 	}
 
 	/**
-	 * Creates a Pet based on an existing pet RPObject, and assigns it to
-	 * a player.
+	 * Creates a Pet based on an existing pet RPObject, and assigns it to a
+	 * player.
+	 *
 	 * @param object
-	 * @param owner The player who should own the pet
+	 * @param owner
+	 *            The player who should own the pet
 	 * @throws AttributeNotFoundException
 	 */
 	public Pet(RPObject object, Player owner) {
 		super(object, owner);
-		baseSpeed= 0.5;
+		baseSpeed = 0.5;
 		hunger = 0;
 	}
 
 	/**
 	 * Is called when the pet dies. Removes the dead pet from the owner.
-	 * @param killer The entity who caused the death
+	 *
+	 * @param killer
+	 *            The entity who caused the death
 	 */
 	@Override
 	public void onDead(Entity killer) {
@@ -122,7 +123,8 @@ public abstract class Pet extends DomesticAnimal {
 			if (owner.hasPet()) {
 				owner.removePet(this);
 			} else {
-				logger.warn("INCOHERENCE: Pet " + this + " isn't owned by " + owner);
+				logger.warn("INCOHERENCE: Pet " + this + " isn't owned by "
+						+ owner);
 			}
 		} else {
 			StendhalRPRuleProcessor.get().removeNPC(this);
@@ -132,54 +134,50 @@ public abstract class Pet extends DomesticAnimal {
 	}
 
 	/**
-	 * Returns the PetFood that is nearest to the pet's current position.
-	 * If there is no PetFood within the given range, returns none.
-	 * @param range The maximum distance to a PetFood
-	 * @return The nearest PetFood, or null if there is none within the
-	 *         given range
+	 * Returns the PetFood that is nearest to the pet's current position. If
+	 * there is no PetFood within the given range, returns none.
+	 *
+	 * @param range
+	 *            The maximum distance to a PetFood
+	 * @return The nearest PetFood, or null if there is none within the given
+	 *         range
 	 */
 	private Item getNearestFood(double range) {
 
 		Set<Item> items = getZone().getItemsOnGround();
-		double squaredDistance = range * range; // This way we save several sqrt operations
-        Item chosen=null;
-		for ( Item i  : items){
-			if (canEat(i)){
-					if (this.squaredDistance(i) < squaredDistance) {
-						chosen = i;
-						squaredDistance = this.squaredDistance(i);
-					}
+		double squaredDistance = range * range; // This way we save several sqrt
+												// operations
+		Item chosen = null;
+		for (Item i : items) {
+			if (canEat(i)) {
+				if (this.squaredDistance(i) < squaredDistance) {
+					chosen = i;
+					squaredDistance = this.squaredDistance(i);
+				}
 			}
 
 		}
 
-
-
 		return chosen;
 	}
 
-	 boolean canEat(Item i) {
+	boolean canEat(Item i) {
 
-			return "meat".equals(i.getItemSubclass());
+		return "meat".equals(i.getItemSubclass());
 
-
-	 }
+	}
 
 	private void eat(Item food) {
-			if (weight < MAX_WEIGHT) {
-				setWeight(weight + 1);
-			}
+		if (weight < MAX_WEIGHT) {
+			setWeight(weight + 1);
+		}
 		food.removeOne();
 		hunger = 0;
 	}
 
-
 	//
 	// RPEntity
 	//
-
-
-
 
 	/**
 	 * Determines what the pet shall do next.
@@ -187,19 +185,18 @@ public abstract class Pet extends DomesticAnimal {
 	@Override
 	public void logic() {
 
-
-		if (!isEnemyNear(20) && (owner == null)) // if there is no player near and none will see us...
-		{
+		if (!isEnemyNear(20) && (owner == null)) {
+			// if there is no player near and none will see us...
 			stop();
-
 			notifyWorldAboutChanges();
 			return;
 		}
 
 		hunger++;
-		Item food = null;
+		Item food = getNearestFood(6);
 
-		if ((hunger > 50) && ((food = getNearestFood(6)) != null) && (weight < MAX_WEIGHT)) {
+		if ((hunger > 50) && (food  != null)
+				&& (weight < MAX_WEIGHT)) {
 			if (nextTo(food)) {
 				logger.debug("Pet eats");
 				setIdea("eat");
@@ -210,7 +207,7 @@ public abstract class Pet extends DomesticAnimal {
 				logger.debug("Pet moves to food");
 				setIdea("food");
 				setMovement(food, 0, 0, 20);
-				//setAsynchonousMovement(food,0,0);
+				// setAsynchonousMovement(food,0,0);
 			}
 		} else if (owner == null) {
 			logger.debug("Pet (ownerless) moves randomly");
@@ -224,7 +221,8 @@ public abstract class Pet extends DomesticAnimal {
 			clearPath();
 		}
 
-		if ((owner != null) && owner.has("text") && owner.get("text").contains("pet")) {
+		if ((owner != null) && owner.has("text")
+				&& owner.get("text").contains("pet")) {
 			clearPath();
 			moveToOwner();
 		}
@@ -238,10 +236,12 @@ public abstract class Pet extends DomesticAnimal {
 		notifyWorldAboutChanges();
 
 	}
-    // Should never be called
+
+	// Should never be called
 	@Override
 	public String describe() {
-		String text = "You see a pet; it looks like it weighs about " + weight + ".";
+		String text = "You see a pet; it looks like it weighs about " + weight
+				+ ".";
 		if (hasDescription()) {
 			text = getDescription();
 		}
