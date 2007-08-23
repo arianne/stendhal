7b02231530e9634ef8e431c31f2950fec901f1b5
Chad F <chad3f@users.sourceforge.net>
2007-08-23 00:59:07 +0000

    Combined Pet and Sheep into DomesticAnimal to match server (and share common code).

--- src/games/stendhal/client/entity/Pet.java
+++ src/games/stendhal/client/entity/Pet.java
@@ -15,182 +15,19 @@ package games.stendhal.client.entity;
 import games.stendhal.client.soundreview.SoundMaster;
 import games.stendhal.common.Rand;
 
-import marauroa.common.game.RPObject;
-
 /** A Pet entity */
-public class Pet extends RPEntity {
-	/**
-	 * Pet idea property.
-	 */
-	public final static Object	PROP_IDEA	= new Object();
-
-	/**
-	 * Pet weight property.
-	 */
-	public final static Object	PROP_WEIGHT	= new Object();
-
-	/**
-	 * The pet's weight (0-100).
-	 */
-	private int weight;
-
-	/**
-	 * The pet's idea.
-	 */
-	private String	idea;
-
-
+public class Pet extends DomesticAnimal {
 	//
-	// Pet
+	// DomesticAnimal
 	//
 
-	/**
-	 * Get the idea setting.
-	 *
-	 * @return	The pet's idea.
-	 */
-	public String getIdea() {
-		return idea;
-	}
-
-
-	/**
-	 * Get the weight.
-	 *
-	 * @return	The pet's weight.
-	 */
-	public int getWeight() {
-		return weight;
-	}
-
-
-	/**
-	 * The idea changed.
-	 *
-	 * @param	idea		The idea, or <code>null</code>.
-	 */
-	protected void onIdea(final String idea) {
-		if(idea == null) {
-			// No "idea" - Do nothing
-		} else if ("eat".equals(idea)) {
-			probableChat(15);
-		} else if ("food".equals(idea)) {
-			probableChat(20);
-		} else if ("walk".equals(idea)) {
-			probableChat(20);
-		} else if ("follow".equals(idea)) {
-			probableChat(20);
-		} else if ("stop".equals(idea)) {
-			// Do nothing
-		}
-	}
-
-
-	private void probableChat(final int chance) {
-
+	@Override
+	protected void probableChat(final int chance) {
 		String[][] soundnames={{"pet-1.wav","pet-3.wav"},{"pet-2.wav","pet-4.wav"}};
 		int which=Rand.rand(2);
 		if (Rand.rand(100)<chance){
-		String token = weight > 50 ? soundnames[0][which]:soundnames[1][which];
+		String token = getWeight() > 50 ? soundnames[0][which]:soundnames[1][which];
 		SoundMaster.play(token,x,y);//playSound(token, 20, 35, chance);
-		}
-	}
-
-
-	//
-	// Entity
-	//
-
-	/**
-	 * Initialize this entity for an object.
-	 *
-	 * @param	object		The object.
-	 *
-	 * @see-also	#release()
-	 */
-	@Override
-	public void initialize(final RPObject object) {
-		super.initialize(object);
-
-		/*
-		 * Idea
-		 */
-		if (object.has("idea")) {
-			idea = object.get("idea");
-		} else {
-			idea = null;
-		}
-
-		/*
-		 * Weight
-		 */
-		if (object.has("weight")){
-			//TODO: find why there are pet without "weight" attribute
-			weight = object.getInt("weight");
-		} else {
-			weight = 0;
-		}
-
-		onIdea(idea);
-	}
-
-
-	//
-	// RPObjectChangeListener
-	//
-
-	/**
-	 * The object added/changed attribute(s).
-	 *
-	 * @param	object		The base object.
-	 * @param	changes		The changes.
-	 */
-	@Override
-	public void onChangedAdded(final RPObject object, final RPObject changes) {
-		super.onChangedAdded(object, changes);
-
-		/*
-		 * Idea
-		 */
-		if (changes.has("idea")) {
-			idea = changes.get("idea");
-			onIdea(idea);
-			fireChange(PROP_IDEA);
-		}
-
-		/*
-		 * Weight
-		 */
-		if (changes.has("weight")) {
-			int oldWeight = weight;
-			weight = changes.getInt("weight");
-
-			if (weight > oldWeight) {
-				SoundMaster.play("eat-1.wav",x,y);
-			}
-
-			fireChange(PROP_WEIGHT);
-		}
-	}
-
-
-	/**
-	 * The object removed attribute(s).
-	 *
-	 * @param	object		The base object.
-	 * @param	changes		The changes.
-	 */
-	@Override
-	public void onChangedRemoved(final RPObject object, final RPObject changes) {
-		super.onChangedRemoved(object, changes);
-
-		/*
-		 * Idea
-		 */
-		if (changes.has("idea")) {
-			idea = null;
-			onIdea(idea);
-			fireChange(PROP_IDEA);
 		}
 	}
 }
--- src/games/stendhal/client/entity/Sheep.java
+++ src/games/stendhal/client/entity/Sheep.java
@@ -15,182 +15,20 @@ package games.stendhal.client.entity;
 import games.stendhal.client.soundreview.SoundMaster;
 import games.stendhal.common.Rand;
 
-import marauroa.common.game.RPObject;
-
 /** A Sheep entity */
-public class Sheep extends RPEntity {
-	/**
-	 * Sheep idea property.
-	 */
-	public final static Object	PROP_IDEA	= new Object();
-
-	/**
-	 * Sheep weight property.
-	 */
-	public final static Object	PROP_WEIGHT	= new Object();
-
-	/**
-	 * The sheep's weight (0-100).
-	 */
-	private int weight;
-
-	/**
-	 * The sheep's idea.
-	 */
-	private String	idea;
-
-
+public class Sheep extends DomesticAnimal {
 	//
-	// Sheep
+	// DomesticAnimal
 	//
 
-	/**
-	 * Get the idea setting.
-	 *
-	 * @return	The sheep's idea.
-	 */
-	public String getIdea() {
-		return idea;
-	}
-
-
-	/**
-	 * Get the weight.
-	 *
-	 * @return	The sheep's weight.
-	 */
-	public int getWeight() {
-		return weight;
-	}
-
-
-	/**
-	 * The idea changed.
-	 *
-	 * @param	idea		The idea, or <code>null</code>.
-	 */
-	protected void onIdea(final String idea) {
-		if(idea == null) {
-			// No "idea" - Do nothing
-		} else if ("eat".equals(idea)) {
-			probableChat(15);
-		} else if ("food".equals(idea)) {
-			probableChat(20);
-		} else if ("walk".equals(idea)) {
-			probableChat(20);
-		} else if ("follow".equals(idea)) {
-			probableChat(20);
-		} else if ("stop".equals(idea)) {
-			// Do nothing
-		}
-	}
-
-
-	private void probableChat(final int chance) {
+	@Override
+	protected void probableChat(final int chance) {
 
 		String[][] soundnames={{"sheep-1.wav","sheep-3.wav"},{"sheep-2.wav","sheep-4.wav"}};
 		int which=Rand.rand(2);
 		if (Rand.rand(100)<chance){
-		String token = weight > 50 ? soundnames[0][which]:soundnames[1][which];
+		String token = getWeight() > 50 ? soundnames[0][which]:soundnames[1][which];
 		SoundMaster.play(token,x,y);//playSound(token, 20, 35, chance);
-		}
-	}
-
-
-	//
-	// Entity
-	//
-
-	/**
-	 * Initialize this entity for an object.
-	 *
-	 * @param	object		The object.
-	 *
-	 * @see-also	#release()
-	 */
-	@Override
-	public void initialize(final RPObject object) {
-		super.initialize(object);
-
-		/*
-		 * Idea
-		 */
-		if (object.has("idea")) {
-			idea = object.get("idea");
-		} else {
-			idea = null;
-		}
-
-		/*
-		 * Weight
-		 */
-		if (object.has("weight")){
-			//TODO: find why there are sheep without "weight" attribute
-			weight = object.getInt("weight");
-		} else {
-			weight = 0;
-		}
-
-		onIdea(idea);
-	}
-
-
-	//
-	// RPObjectChangeListener
-	//
-
-	/**
-	 * The object added/changed attribute(s).
-	 *
-	 * @param	object		The base object.
-	 * @param	changes		The changes.
-	 */
-	@Override
-	public void onChangedAdded(final RPObject object, final RPObject changes) {
-		super.onChangedAdded(object, changes);
-
-		/*
-		 * Idea
-		 */
-		if (changes.has("idea")) {
-			idea = changes.get("idea");
-			onIdea(idea);
-			fireChange(PROP_IDEA);
-		}
-
-		/*
-		 * Weight
-		 */
-		if (changes.has("weight")) {
-			int oldWeight = weight;
-			weight = changes.getInt("weight");
-
-			if (weight > oldWeight) {
-				SoundMaster.play("eat-1.wav",x,y);
-			}
-
-			fireChange(PROP_WEIGHT);
-		}
-	}
-
-
-	/**
-	 * The object removed attribute(s).
-	 *
-	 * @param	object		The base object.
-	 * @param	changes		The changes.
-	 */
-	@Override
-	public void onChangedRemoved(final RPObject object, final RPObject changes) {
-		super.onChangedRemoved(object, changes);
-
-		/*
-		 * Idea
-		 */
-		if (changes.has("idea")) {
-			idea = null;
-			onIdea(idea);
-			fireChange(PROP_IDEA);
 		}
 	}
 }
