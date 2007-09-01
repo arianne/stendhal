23508077a6f6ca5c8ea3da2f5c9dfedb3751fe55
Chad F <chad3f@users.sourceforge.net>
2007-09-01 02:23:39 +0000

    Refactored the *Source entities to use a common base class. Fixed slightly broken equals reqs.

--- src/games/stendhal/server/entity/FishSource.java
+++ src/games/stendhal/server/entity/FishSource.java
@@ -1,169 +1,162 @@
+/*
+ * @(#) src/games/stendhal/server/entity/FishSource.java
+ *
+ * $Id$
+ */
+
 package games.stendhal.server.entity;
+
+//
+//
 
 import games.stendhal.common.Rand;
 import games.stendhal.server.StendhalRPWorld;
 import games.stendhal.server.entity.item.Item;
 import games.stendhal.server.entity.player.Player;
-import games.stendhal.server.events.TurnListener;
-import games.stendhal.server.events.TurnNotifier;
-import games.stendhal.server.events.UseListener;
-
-import java.lang.ref.WeakReference;
 
 import marauroa.common.game.RPClass;
 
 /**
- * A fish source is a spot where a player can fish. He needs a fishing rod, time
- * and luck. Before he catches fish he needs to make a license.
+ * A fish source is a spot where a player can fish. He needs a fishing rod,
+ * time and luck. Before he catches fish he needs to make a license.
  *
- * Fishing takes 8 seconds; during this time, the player keep standing next to
- * the fish source. In fact, the player only has to be there when the
- * prospecting action has finished. Therefore, make sure that two fish sources
- * are always at least 8 sec of walking away from each other, so that the player
- * can't fish at several sites simultaneously.
+ * Fishing takes 5-9 seconds; during this time, the player keep standing next
+ * to the fish source. In fact, the player only has to be there when the
+ * prospecting action has finished. Therefore, make sure that two fish
+ * sources are always at least 8 sec of walking away from each other, so
+ * that the player can't fish at several sites simultaneously.
  *
  * @author dine
- *
  */
-public class FishSource extends Entity implements UseListener {
+public class FishSource extends PlayerActivityEntity {
+	/**
+	 * The equipment needed.
+	 */
 	private static final String NEEDED_EQUIPMENT = "fishing_rod";
 
-	private class Fisher implements TurnListener {
-		WeakReference<Player> playerRef;
-
-		public Fisher(Player bob) {
-			playerRef = new WeakReference<Player>(bob);
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (obj instanceof Fisher) {
-				Fisher newName = (Fisher) obj;
-				return playerRef.get() == newName.playerRef.get();
-			} else {
-				return false;
-			}
-
-		}
-
-		@Override
-		public int hashCode() {
-			return super.hashCode();
-		}
-
-		/**
-		 * This method is called when the turn number is reached. NOTE: The
-		 * <em>message</em> parameter is deprecated.
-		 *
-		 * @param currentTurn
-		 *            The current turn number.
-		 * @param message
-		 *            The string that was used.
-		 */
-		public void onTurnReached(int currentTurn, String message) {
-			Player player = playerRef.get();
-			if (playerRef.get() != null) {
-				// check if the player is still standing next to this fish
-				// source
-				if (nextTo(player, 0.25)) {
-					// roll the dice
-					if (isSuccessful(player)) {
-						Item fish = StendhalRPWorld.get().getRuleManager()
-								.getEntityManager().getItem(itemName);
-						player.equip(fish, true);
-						player.sendPrivateText("You caught a fish.");
-					} else {
-						player.sendPrivateText("You didn't get a fish.");
-					}
-				}
-			}
-		}
-
-	}
-
+	/**
+	 * The name of the item to be caught.
+	 */
 	private String itemName;
 
+
 	/**
-	 * Calculates the probability that the given player catches a fish. This is
-	 * based on the player's fishing skills, however even players with no skills
-	 * at all have a 5 % probability of success.
+	 * Create a fish source.
 	 *
-	 * @param player
-	 * @return
+	 * @param	itemName	The name of the item to be caught.
+	 */
+	public FishSource(String itemName) {
+		this.itemName = itemName;
+
+		setRPClass("fish_source");
+		put("type", "fish_source");
+
+		setDescription("There is something in the water.");
+	}
+
+
+	//
+	// FishSource
+	//
+
+	public static void generateRPClass() {
+		RPClass rpclass = new RPClass("fish_source");
+		rpclass.isA("entity");
+	}
+
+
+	/**
+	 * Calculates the probability that the given player catches a fish.
+	 * This is based on the player's fishing skills, however even players
+	 * with no skills at all have a 5% probability of success.
+	 *
+	 * @param	player		The player,
+	 *
+	 * @return	The probability of success.
 	 */
 	private double getSuccessProbability(Player player) {
+		double probability = 0.05;
+
 		String skill = player.getSkill("fishing");
-		double probability;
 
 		if (skill != null) {
-			probability = Math.max(0.05, Double.parseDouble(skill));
-		} else {
-			probability = 0.05;
+			probability = Math.max(probability, Double.parseDouble(skill));
 		}
 
 		return probability + player.useKarma(0.05);
 	}
 
-	public FishSource(String itemName) {
-		this.itemName = itemName;
-		setDescription("There is something in the water.");
-		setRPClass("fish_source");
-		put("type", "fish_source");
-	}
 
-	public static void generateRPClass() {
-		RPClass grower = new RPClass("fish_source");
-		grower.isA("entity");
-	}
+	//
+	// PlayerActivityEntity
+	//
 
 	/**
-	 * Decides randomly if a prospecting action should be successful.
+	 * Get the time it takes to perform this activity.
 	 *
-	 * @return true iff the prospecting player should get a fish.
+	 * @return	The time to perform the activity (in seconds).
 	 */
-	private boolean isSuccessful(Player player) {
-		int random = Rand.roll1D100();
-		return random <= getSuccessProbability(player) * 100;
+	@Override
+	protected int getDuration() {
+		return 5 + Rand.rand(4);
 	}
 
-	//
-	// UseListener
-	//
 
 	/**
-	 * Is called when a player has started fishing.
+	 * Decides if the activity can be done.
+	 *
+	 * @return	<code>true</code> if successful.
 	 */
-	public boolean onUsed(RPEntity entity) {
-		if (entity instanceof Player) {
-			Player player = (Player) entity;
-			if (player.nextTo(this)) {
-
-				if (player.isEquipped(NEEDED_EQUIPMENT)) {
-					Fisher fisher = new Fisher(player);
-					// You can't start a new prospecting action before
-					// the last one has finished.
-					if (TurnNotifier.get().getRemainingTurns(fisher) == -1) {
-						player.faceToward(this);
-						player.notifyWorldAboutChanges();
-
-						// some feedback is needed.
-						player.sendPrivateText("You have started fishing.");
-						TurnNotifier.get().notifyInSeconds(getDuration(),
-								fisher);
-					}
-				} else {
-					player
-							.sendPrivateText("You need a fishing rod for fishing.");
-				}
-			}
+	@Override
+	protected boolean isPrepared(final Player player) {
+		if(player.isEquipped(NEEDED_EQUIPMENT)) {
+			return true;
 		}
+
+		player.sendPrivateText("You need a fishing rod for fishing.");
 		return false;
 	}
 
+
 	/**
-	 * @return an int between 5 and 8 to represent seconds needed for fishing
+	 * Decides if the activity was successful.
+	 *
+	 * @return	<code>true</code> if successful.
 	 */
-	private int getDuration() {
-		return 5 + Rand.rand(4);
+	@Override
+	protected boolean isSuccessful(final Player player) {
+		int random = Rand.roll1D100();
+		return (random <= (getSuccessProbability(player) * 100));
+	}
+
+
+	/**
+	 * Called when the activity has finished.
+	 *
+	 * @param	player		The player that did the activity.
+	 * @param	successful	If the activity was successful.
+	 */
+	@Override
+	protected void onFinished(final Player player, final boolean successful) {
+		if(successful) {
+			Item item = StendhalRPWorld.get().getRuleManager().getEntityManager().getItem(itemName);
+
+			player.equip(item, true);
+			player.sendPrivateText("You caught a fish.");
+		} else {
+			player.sendPrivateText("You didn't get a fish.");
+		}
+	}
+
+
+	/**
+	 * Called when the activity has started.
+	 *
+	 * @param	player		The player starting the activity.
+	 */
+	@Override
+	protected void onStarted(final Player player) {
+		// some feedback is needed.
+		player.sendPrivateText("You have started fishing.");
 	}
 }
--- src/games/stendhal/server/entity/WellSource.java
+++ src/games/stendhal/server/entity/WellSource.java
@@ -1,4 +1,13 @@
+/*
+ * @(#) src/games/stendhal/server/entity/WellSource.java
+ *
+ * $Id$
+ */
+
 package games.stendhal.server.entity;
+
+//
+//
 
 import games.stendhal.common.Grammar;
 import games.stendhal.common.Rand;
@@ -6,102 +15,28 @@ import games.stendhal.server.StendhalRPWorld;
 import games.stendhal.server.entity.item.Item;
 import games.stendhal.server.entity.item.StackableItem;
 import games.stendhal.server.entity.player.Player;
-import games.stendhal.server.events.TurnListener;
-import games.stendhal.server.events.TurnNotifier;
-import games.stendhal.server.events.UseListener;
-
-import java.lang.ref.WeakReference;
 
 import marauroa.common.game.RPClass;
-import marauroa.common.game.RPObject;
 
 /**
  * A well source is a spot where a player can make a wish to gain an item. He
  * needs time and luck.
  *
- * Wishing takes 10 seconds; during this time, the player keep standing next to
- * the well source. At every well are two sources next to each other, so the
- * player can actually make 2 wishes at once.
+ * Wishing takes 10 seconds; during this time, the player keep standing next
+ * to the well source. At every well are two sources next to each other, so
+ * the player can actually make 2 wishes at once.
  *
  * @author kymara (based on FishSource by daniel)
  *
  */
-public class WellSource extends Entity implements UseListener {
-	private class Wisher implements TurnListener {
-		WeakReference<Player> playerRef;
-
-		public Wisher(Player bob) {
-			playerRef = new WeakReference<Player>(bob);
-		}
-
-		@Override
-		public boolean equals(Object obj) {
-			if (obj instanceof Wisher) {
-				Wisher wisher = (Wisher) obj;
-				return playerRef.get() == wisher.playerRef.get();
-			} else {
-				return false;
-			}
-		}
-
-		@Override
-		public int hashCode() {
-			return super.hashCode();
-		}
-
-		/**
-		 * This method is called when the turn number is reached. NOTE: The
-		 * <em>message</em> parameter is deprecated.
-		 *
-		 * @param currentTurn
-		 *            The current turn number.
-		 * @param message
-		 *            The string that was used.
-		 */
-		public void onTurnReached(int currentTurn, String message) {
-			Player player = playerRef.get();
-			// check if the player is still logged in
-			if (player != null) {
-				// check if the player is still standing next to this well
-				// source
-				if (nextTo(player, 0.25)) {
-					// roll the dice
-					if (isSuccessful(player)) {
-						String itemName = items[Rand.rand(items.length)];
-						Item item = StendhalRPWorld.get().getRuleManager()
-								.getEntityManager().getItem(itemName);
-						// TODO: player bind the better prizes below:
-						// horned_golden_helmet & dark_dagger
-						if (item.getName().equals("dark_dagger")
-								|| item.getName()
-										.equals("horned_golden_helmet")) {
-							/*
-							 * Bound powerful items.
-							 */
-							item.put("bound", player.getName());
-						} else if (item.getName().equals("money")) {
-							/*
-							 * Assign a random amount of money.
-							 */
-							((StackableItem) item)
-									.setQuantity(Rand.roll1D100());
-						}
-
-						player.equip(item, true);
-						player.sendPrivateText("You were lucky and found "
-								+ Grammar.a_noun(itemName));
-					} else {
-						player.sendPrivateText("Your wish didn't come true.");
-					}
-				}
-			}
-		}
-
-	}
-
-	private String[] items = { "money", "wood", "iron_ore", "gold_nugget",
-			"potion", "home_scroll", "greater_potion", "sapphire", "carbuncle",
-			"horned_golden_helmet", "dark_dagger", "present" };
+public class WellSource extends PlayerActivityEntity {
+	/**
+	 * The list of possible rewards.
+	 */
+	private static final String[] items = {
+		"money", "wood", "iron_ore", "gold_nugget", "potion",
+		"home_scroll", "greater_potion", "sapphire", "carbuncle",
+		"horned_golden_helmet", "dark_dagger", "present" };
 
 	/**
 	 * The chance that wishing is successful.
@@ -109,73 +44,121 @@ public class WellSource extends Entity implements UseListener {
 	private static final double FINDING_PROBABILITY = 0.05;
 
 	/**
-	 * How long it takes to wish at a well (in seconds) TODO: randomize this
-	 * number a bit.
+	 * How long it takes to wish at a well (in seconds).
+	 * TODO: randomize this number a bit.
 	 */
 	private static final int DURATION = 10;
 
+
+	/**
+	 * Create a wishing well source.
+	 */
 	public WellSource() {
-		setDescription("You see a wishing well. Something in it catches your eye.");
 		setRPClass("well_source");
 		put("type", "well_source");
 
-		setResistance(0);
-	}
-
-	public WellSource(RPObject object) {
-		super(object);
 		setDescription("You see a wishing well. Something in it catches your eye.");
-		setRPClass("well_source");
-		put("type", "well_source");
-
 		setResistance(0);
 	}
+
+
+	//
+	// WellSource
+	//
 
 	public static void generateRPClass() {
-		RPClass grower = new RPClass("well_source");
-		grower.isA("entity");
+		RPClass rpclass = new RPClass("well_source");
+		rpclass.isA("entity");
 	}
 
 
+	//
+	// PlayerActivityEntity
+	//
+
 	/**
-	 * Decides randomly if a wishing action should be successful.
+	 * Get the time it takes to perform this activity.
 	 *
-	 * @return true iff the wishing player should get a prize.
+	 * @return	The time to perform the activity (in seconds).
 	 */
-	private boolean isSuccessful(Player player) {
-		int random = Rand.roll1D100();
-		return random <= (FINDING_PROBABILITY + player
-				.useKarma(FINDING_PROBABILITY)) * 100;
+	@Override
+	protected int getDuration() {
+		return DURATION;
 	}
 
+
 	/**
-	 * Is called when a player has started wishing.
+	 * Decides if the activity can be done.
+	 *
+	 * @return	<code>true</code> if successful.
 	 */
-	public boolean onUsed(RPEntity entity) {
-		if (entity instanceof Player) {
-			Player player = (Player) entity;
-			if (player.nextTo(this)) {
-				Wisher wish = new Wisher(player);
-				// You can't start a new wishing action before
-				// the last one has finished.
-				if (TurnNotifier.get().getRemainingTurns(wish) == -1) {
-					player.faceToward(this);
-					player.notifyWorldAboutChanges();
-					// remove 30 money from player as they throw a coin into the
-					// well
-					// some feedback is needed.
-					if (player.isEquipped("money", 30)) {
-						player.drop("money", 30);
-						player
-								.sendPrivateText("You throw 30 coins into the well and make a wish.");
-						TurnNotifier.get().notifyInSeconds(DURATION, wish);
-					} else {
-						player
-								.sendPrivateText("You need 30 coins to make a wish.");
-					}
-				}
-			}
+	@Override
+	protected boolean isPrepared(final Player player) {
+		if (player.isEquipped("money", 30)) {
+			return true;
+		} else {
+			player.sendPrivateText("You need 30 coins to make a wish.");
+			return false;
 		}
-		return false;
+	}
+
+
+	/**
+	 * Decides if the activity was successful.
+	 *
+	 * @return	<code>true</code> if successful.
+	 */
+	@Override
+	protected boolean isSuccessful(final Player player) {
+		int random = Rand.roll1D100();
+		return random <= (FINDING_PROBABILITY + player.useKarma(FINDING_PROBABILITY)) * 100;
+	}
+
+
+	/**
+	 * Called when the activity has finished.
+	 *
+	 * @param	player		The player that did the activity.
+	 * @param	successful	If the activity was successful.
+	 */
+	@Override
+	protected void onFinished(final Player player, final boolean successful) {
+		if(successful) {
+			String itemName = items[Rand.rand(items.length)];
+			Item item = StendhalRPWorld.get().getRuleManager().getEntityManager().getItem(itemName);
+
+			// TODO: player bind the better prizes below:
+			// horned_golden_helmet & dark_dagger
+			if (itemName.equals("dark_dagger") || itemName.equals("horned_golden_helmet")) {
+				/*
+				 * Bound powerful items.
+				 */
+				item.put("bound", player.getName());
+			} else if (itemName.equals("money")) {
+				/*
+				 * Assign a random amount of money.
+				 */
+				((StackableItem) item).setQuantity(Rand.roll1D100());
+			}
+
+			player.equip(item, true);
+			player.sendPrivateText("You were lucky and found " + Grammar.a_noun(itemName));
+		} else {
+			player.sendPrivateText("Your wish didn't come true.");
+		}
+	}
+
+
+	/**
+	 * Called when the activity has started.
+	 *
+	 * @param	player		The player starting the activity.
+	 */
+	@Override
+	protected void onStarted(final Player player) {
+		// remove 30 money from player as they throw a coin into the
+		// well
+		player.drop("money", 30);
+		player.sendPrivateText("You throw 30 coins into the well and make a wish.");
 	}
 }
