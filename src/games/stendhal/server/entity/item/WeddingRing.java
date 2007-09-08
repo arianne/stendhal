73918327cbff7f156bb09c82d4140139f2cdfd63
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-09-08 00:39:25 +0000

    backporting check that the source and target zones allow teleporting

--- src/games/stendhal/server/entity/item/WeddingRing.java
+++ src/games/stendhal/server/entity/item/WeddingRing.java
@@ -21,6 +21,7 @@ import games.stendhal.server.entity.player.Player;
 
 import java.util.Map;
 
+import marauroa.common.Log4J;
 import marauroa.common.game.RPObject;
 
 import org.apache.log4j.Logger;
@@ -29,14 +30,14 @@ import org.apache.log4j.Logger;
  * A special ring that allows the owner to teleport to his or her spouse.
  * The spouse's name is engraved into the ring. Technically, the name is
  * stored in the item's infostring.
- * 
+ *
  * Wedding rings should always be bound to the owner.
- * 
+ *
  * @author daniel
  */
 public class WeddingRing extends Ring {
 
-	private static final Logger logger = Logger.getLogger(WeddingRing.class);
+	private static final Logger logger = Log4J.getLogger(WeddingRing.class);
 
 	/**
 	 * Creates a new wedding ring.
@@ -75,30 +76,46 @@ public class WeddingRing extends Ring {
 	/**
 	 * Teleports the given player to his/her spouse, but only if the spouse
 	 * is also wearing the wedding ring.
-	 * 
+	 *
 	 * @param player The ring's owner.
 	 */
 	private void teleportToSpouse(Player player) {
-		if (has("infostring")) {
-			String spouseName = get("infostring");
-			Player spouse = StendhalRPRuleProcessor.get().getPlayer(spouseName);
-			if (spouse == null) {
-				player.sendPrivateText(spouseName + " is not online.");
-			} else if (spouse.isEquipped("wedding_ring")) {
-				StendhalRPZone zone = spouse.getZone();
-				int x = spouse.getX();
-				int y = spouse.getY();
-				Direction dir = spouse.getDirection();
-
-				player.teleport(zone, x, y, dir, player);
-			} else {
-				// This means trouble ;)
-				player.sendPrivateText(spouseName + " is not wearing the wedding ring.");
-			}
-		} else {
+		if (!has("infostring")) {
 			player.sendPrivateText("This wedding ring hasn't been engraved yet.");
 			logger.debug(player.getName() + "tried to use a wedding ring without a spouse name engraving.");
+			return;
 		}
+
+		String spouseName = get("infostring");
+		Player spouse = StendhalRPRuleProcessor.get().getPlayer(spouseName);
+		if (spouse == null) {
+			player.sendPrivateText(spouseName + " is not online.");
+			return;
+		}
+
+		if (!spouse.isEquipped("wedding_ring")) {
+			// This means trouble ;)
+			player.sendPrivateText(spouseName + " is not wearing the wedding ring.");
+			return;
+		}
+
+		StendhalRPZone sourceZone = player.getZone();
+		if (!sourceZone.isTeleportAllowed()) {
+			player.sendPrivateText("The strong anti magic aura in this area prevents the wedding ring from working!");
+			return;
+		}
+
+		StendhalRPZone destinationZone = spouse.getZone();
+		if (!destinationZone.isTeleportAllowed()) {
+			player.sendPrivateText("The strong anti magic aura in the destination area prevents the wedding ring from working!");
+			return;
+		}
+
+		int x = spouse.getX();
+		int y = spouse.getY();
+		Direction dir = spouse.getDirection();
+
+		player.teleport(destinationZone, x, y, dir, player);
 	}
 
 	@Override
