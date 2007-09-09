cabeaca8cfb80dc21025175d2f32e507d04d6360
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2007-09-09 17:35:15 +0000

    javadoc

--- tools/newrp/Dice.java
+++ tools/newrp/Dice.java
@@ -2,22 +2,51 @@ package newrp;
 
 import java.util.Random;
 
+/**
+ * This class represent a Dice.
+ * It provide methods for rolling a 1D100, 1D20 and 1D6.
+ * 
+ * Also it has methods for rolling N 1D6 and N 1D20.
+ * 
+ * A finally it is also able to return a random number between min and max both inclusive.
+ *
+ * @author miguel
+ *
+ */
 public class Dice {
 	static Random rand = new Random();
 
+	/**
+	 * Returns a number between min and max both included.
+	 * @param min
+	 * @param max
+	 * @return
+	 */
 	public static int between(int min, int max) {
 		return min + rand.nextInt(1 + max - min);
 	}
 
+	/**
+	 * Roll a dice of 100 sides.
+	 * @return
+	 */
 	public static int r1D100() {
 		return rand.nextInt(100) + 1;
 
 	}
 
+	/**
+	 * Roll a dice of 20 sides.
+	 * @return
+	 */
 	public static int r1D20() {
 		return rand.nextInt(20) + 1;
 	}
 
+	/**
+	 * Roll a dice of 6 sides.
+	 * @return
+	 */
 	public static int r1D6() {
 		return rand.nextInt(6) + 1;
 	}
