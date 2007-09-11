bbed87b44eb5aefa8bcfba549be4200370989e16
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2007-09-11 08:14:15 +0000

    Trying to write simpler rules for RP.

---
+++ tools/newrp/SimpleRPEntity.java
@@ -0,0 +1,16 @@
+package newrp;
+
+
+/**
+ * This is a simpler, human understandable implementation of the RP.
+ * 
+ * @author miguel
+ *
+ */
+public class SimpleRPEntity {
+	
+    public SimpleRPEntity() {
+	    super();
+    }
+
+}
