a0fc69132e657d8c6b18d225ed80abdc7c8ad8a2
Katie Russell <kymara@users.sourceforge.net>
2007-09-21 14:30:35 +0000

    increase hair number so black cap and brown haired ponytail can be used

--- src/games/stendhal/client/gui/OutfitDialog.java
+++ src/games/stendhal/client/gui/OutfitDialog.java
@@ -68,7 +68,7 @@ public class OutfitDialog extends JDialog {
 
 	private static final int CLOTHES_OUTFIT = 45;
 
-	private static final int HAIR_OUTFITS = 38;
+	private static final int HAIR_OUTFITS = 40;
 
 	private static final int BODY_OUTFITS = 14;
 
