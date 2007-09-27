852e4150ae2e3fb74f9c07de92b32ff8d2b92f0d
Katie Russell <kymara@users.sourceforge.net>
2007-09-27 07:27:05 +0000

    forgot to make her prompt for sewing_machine, even though there was a response for it. tweaked a few responses

--- src/games/stendhal/server/maps/fado/great_cave/BuyerNPC.java
+++ src/games/stendhal/server/maps/fado/great_cave/BuyerNPC.java
@@ -52,15 +52,15 @@ public class BuyerNPC implements ZoneConfigurator {
 			protected void createDialog() {
 			        addGreeting("Hello.");
 			        addJob("I potter around collecting odds and bobs. Sometimes I sell items, but mostly I like to keep them. If you have any relics to #trade, I would be very happy indeed.");
-				addHelp("I could tell you about some of these wonderful items here. The white #pot, #coffins, #dress, #shield, #armor, #tools, #rug, #flowers and #clock are all fascinating!");
-				addReply("pot","Ahh, the oriental pot! That is an original from the oni palace, you know.");
+				addHelp("I could tell you about some of these wonderful items here. The white #pot, #coffins, #dress, #shield, #armor, #tools, #rug, #flowers, #clock and #sewing_machine are all fascinating!");
+				addReply("pot","You mean the white and blue one, the oriental pot, I suppose. That is an original made by the ancient oni people. It's very rare.");
 				addReply("coffins","Those coffins were looted from some underground catacombs, I had to pay a pretty price for that pair.");
 				addReply("dress","I do love that beautiful pink dress. I am told it was worn by the elven princess Tywysoga.");
 				addReply("shield","That is a truly fearsome shield, is it not? There is some enscription on the back about devil knights, but I am afraid I do not understand it.");
-				addReply("rug","That is a genuine rug from the far East. I have never seen one like it, only cheap copies.");
+				addReply("rug","That is a genuine rug from the far East. I have never seen one like it, only cheap copies. Please don't get muddy footprints on it!");
 				addReply("flowers","Ah ha! These are flowers grown with elf magic. I bought them myself from a wonderful florist in Nalwor.");
 				addReply("clock","That grandfather clock is one of my more modern pieces. If you know Woody the Woodcutter, you may recognise the handiwork.");
-				addReply("tools","Those tools on the back wall are a true antique! They were used by the great grandfather of Xoderos, isn't that incredible!");
+				addReply("tools","Those tools on the back wall are a true antique! They were used by the great grandfather of Xoderos of Semos, isn't that incredible!");
 				addReply("armor","Ah, that mighty piece was made in Deniran. I'm afraid I know little more about it.");
 				addReply("sewing_machine","Oh you know that is my favourite. It was made by a man called Zinger, and it still works just as well as the day it was made.");
 				addQuest("I have no favour to ask of you.");
@@ -69,7 +69,7 @@ public class BuyerNPC implements ZoneConfigurator {
  				addGoodbye("Bye.");
 			}
 		};
-		npc.setDescription("You see Vonda, a witch who seems to like clutter..");
+		npc.setDescription("You see Vonda, a witch who seems to like clutter...");
 		zone.assignRPObjectID(npc);
 		npc.setEntityClass("witch2npc");
 		npc.setPosition(4, 12);
