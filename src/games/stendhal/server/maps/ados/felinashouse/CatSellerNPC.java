0634192cc9719821a5648f2a6fd8a99ec5d5b965
Katie Russell <kymara@users.sourceforge.net>
2007-10-09 16:25:49 +0000

    Move Felina into her home in ados

--- src/games/stendhal/server/maps/ados/felinashouse/CatSellerNPC.java
+++ src/games/stendhal/server/maps/ados/felinashouse/CatSellerNPC.java
@@ -1,4 +1,4 @@
-package games.stendhal.server.maps.semos.house;
+package games.stendhal.server.maps.ados.felinashouse;
 
 import games.stendhal.server.StendhalRPZone;
 import games.stendhal.server.config.ZoneConfigurator;
@@ -16,7 +16,7 @@ import java.util.Map;
 
 public class CatSellerNPC implements ZoneConfigurator {
 
-	public static final int BUYING_PRICE = 1;
+	public static final int BUYING_PRICE = 100;
 
 	/**
 	 * Configure a zone.
@@ -25,19 +25,25 @@ public class CatSellerNPC implements ZoneConfigurator {
 	 * @param	attributes	Configuration attributes.
 	 */
 	public void configureZone(StendhalRPZone zone, Map<String, String> attributes) {
-		buildSemosVillageArea(zone);
+		buildHouseArea(zone);
 	}
 
-	private void buildSemosVillageArea(StendhalRPZone zone) {
+	private void buildHouseArea(StendhalRPZone zone) {
 
 		SpeakerNPC npc = new SpeakerNPC("Felina") {
 			@Override
 			protected void createPath() {
 				List<Node> nodes = new LinkedList<Node>();
-				nodes.add(new Node(7, 8));
-				nodes.add(new Node(9, 8));
-				nodes.add(new Node(9, 9));
-				nodes.add(new Node(7, 9));
+				nodes.add(new Node(6, 8));
+				nodes.add(new Node(11, 8));
+				nodes.add(new Node(11, 17));
+				nodes.add(new Node(19, 17));
+				nodes.add(new Node(19, 21));
+				nodes.add(new Node(14, 21));
+				nodes.add(new Node(14, 16));
+				nodes.add(new Node(10, 16));
+				nodes.add(new Node(10, 4));
+				nodes.add(new Node(6, 4));
 				setPath(new FixedPath(nodes, true));
 			}
 
@@ -58,7 +64,7 @@ public class CatSellerNPC implements ZoneConfigurator {
 								seller.say("You don't seem to have enough money.");
 								return false;
 							}
-							seller.say("Here you go, a cute little kitten! Take good care of it, now...");
+							seller.say("Here you go, a cute little kitten! Your kitten will eat any piece of chicken you place on the ground. Enjoy her!");
 
 							Cat cat = new Cat(player);
 
@@ -82,23 +88,23 @@ public class CatSellerNPC implements ZoneConfigurator {
 				items.put("cat", BUYING_PRICE);
 
 				addGreeting();
-				addJob("I sell cats");
-				addHelp("I sell cat. To buy one, just tell me you want to #buy #cat. If you're new to this business, I can tell you how to #travel with her, take #care of her, and finally give you tips on when to #sell her. If you find any wild cat, incidentally, you can make them your #own.");
+				addJob("I sell cats. Well, really they are just little kittens when I sell them to you but if you #care for them well they grow into cats.");
+				addHelp("I sell cats. To buy one, just tell me you want to #buy #cat. If you're new to this business, I can tell you how to #travel with her and take #care of her. If you find any wild cat, incidentally, you can make them your #own.");
 				addGoodbye();
 				addSeller(new CatSellerBehaviour(items));
 				addReply("care",
-						"My cat especially love to eat the red berries that grow on these little bushes. Just stand near one and your cat will walk over to start eating. You can right-click and choose LOOK at any time, to check up on her weight; she will gain one unit of weight for every cherry she eats.");
+						"Cats love chicken. Just place a piece on the ground and your cat will run over to eat it. You can right-click on her and choose 'Look' at any time, to check up on her weight; she will gain one unit of weight for every piece of chicken she eats.");
 				addReply("travel",
-						"You'll need your cat to be close by in order for her to follow you when you change zones; you can say #cat to call her if she's not paying attention. If you decide to abandon her instead, you can right-click on yourself and select LEAVE CAT; but frankly I think that sort of behaviour is disgraceful.");
+						"You'll need your cat to be close by in order for her to follow you when you change zones; you can say #cat to call her if she's not paying attention. If you decide to abandon her instead, you can right-click on yourself and select 'Leave Pet'; but frankly I think that sort of behaviour is disgraceful.");
 				addReply("sell",
-						"Once you've gotten your cat up to a weight of 100, you can take her to Sato in Semos; he will buy her from you.");
+						"Sell??? What kind of a monster are you? Why would you ever sell your beautiful cat?");
 				addReply("own",
-						"If you find any wild or abandoned cat, you can right-click on them and select OWN to tame them. Cats need to be looked after!");
+						"If you find any wild or abandoned cat, you can right-click on them and select 'Own' to tame them. It will start following you immediately. Cats go a bit crazy without an owner!");
 			}
 		};
 
 		npc.setEntityClass("woman_009_npc");
-		npc.setPosition(7, 8);
+		npc.setPosition(6, 8);
 		npc.initHP(100);
 		zone.add(npc);
 	}
