f86269b4d3e00da18f124dd5465eda311ab2cfff
Katie Russell <kymara@users.sourceforge.net>
2007-10-09 07:22:13 +0000

    Healer for magic city and teacher in school

---
+++ src/games/stendhal/server/maps/fado/great_cave/HealerNPC.java
@@ -0,0 +1,69 @@
+package games.stendhal.server.maps.fado.great_cave;
+
+import games.stendhal.server.StendhalRPZone;
+import games.stendhal.server.config.ZoneConfigurator;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.pathfinder.FixedPath;
+import games.stendhal.server.pathfinder.Node;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Builds a Healer NPC for the magic city  
+ *
+ * @author kymara
+ */
+public class HealerNPC implements ZoneConfigurator {
+
+	/**
+	 * Configure a zone.
+	 *
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+	public void configureZone(StendhalRPZone zone, Map<String, String> attributes) {
+		buildNPC(zone);
+	}
+
+	private void buildNPC(StendhalRPZone zone) {
+		SpeakerNPC npc = new SpeakerNPC("Salva Mattori") {
+
+			@Override
+			protected void createPath() {
+				List<Node> nodes = new LinkedList<Node>();
+				// walks along the aqueduct path, roughly
+				nodes.add(new Node(5, 25));
+				nodes.add(new Node(5, 51));
+				nodes.add(new Node(18, 51));
+				nodes.add(new Node(18, 78));
+				nodes.add(new Node(20, 78));
+				nodes.add(new Node(20, 109));
+				// and back again
+				nodes.add(new Node(20, 78));
+				nodes.add(new Node(18, 78));
+				nodes.add(new Node(18, 51));
+				nodes.add(new Node(5, 51));
+				setPath(new FixedPath(nodes, true));
+			}
+
+			@Override
+			protected void createDialog() {
+			        addGreeting("Greetings. Can I #help you?");
+				addJob("I practise alchemy and have the ability to #heal others.");
+				addHealer(500);
+				addReply("magical","We're all capable of magic here. There are different kinds, of course. My favourite is the Sunlight Spell to keep grass and flowers growing underground.");
+				addHelp("I have #magical powers to #heal your ailments.");
+				addQuest("I need nothing, thank you.");
+ 				addGoodbye("Fare thee well.");
+			}
+		};
+
+		npc.setDescription("You see a quiet woman with a benign face.");
+		npc.setEntityClass("cloakedwomannpc");
+		npc.setPosition(5, 25);
+		npc.initHP(100);
+		zone.add(npc);
+	}
+}
---
+++ src/games/stendhal/server/maps/fado/great_cave/TeacherNPC.java
@@ -0,0 +1,58 @@
+package games.stendhal.server.maps.fado.great_cave;
+
+import games.stendhal.server.StendhalRPZone;
+import games.stendhal.server.config.ZoneConfigurator;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.ConversationPhrases;
+import games.stendhal.server.pathfinder.FixedPath;
+import games.stendhal.server.pathfinder.Node;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Builds a Teacher NPC who flies on a broomstick 
+ *
+ * @author kymara
+ */
+public class TeacherNPC implements ZoneConfigurator {
+
+	/**
+	 * Configure a zone.
+	 *
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+	public void configureZone(StendhalRPZone zone, Map<String, String> attributes) {
+		buildNPC(zone);
+	}
+
+	private void buildNPC(StendhalRPZone zone) {
+		SpeakerNPC npc = new SpeakerNPC("Esolte Vietta") {
+
+			@Override
+			protected void createPath() {
+				List<Node> nodes = new LinkedList<Node>();
+				nodes.add(new Node(29, 19));
+				nodes.add(new Node(36, 19));
+				nodes.add(new Node(36, 21));
+				nodes.add(new Node(29, 21));
+				setPath(new FixedPath(nodes, true));
+			}
+
+			@Override
+			    protected void createDialog() {
+				add(ConversationStates.IDLE, ConversationPhrases.GREETING_MESSAGES, null, ConversationStates.IDLE, "Sit down, shut up, and watch me!", null);
+	 	     }
+		    
+		};
+
+		npc.setDescription("You see a witch flying on a broomstick. She appears to be instructing some pupils.");
+		npc.setEntityClass("witch3npc");
+		npc.setPosition(29, 19);
+		npc.initHP(100);
+		zone.add(npc);
+	}
+}
