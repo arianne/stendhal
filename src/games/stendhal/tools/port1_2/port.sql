b0876fbd4815bb699c363f3b8a3b7c73d14cb741
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-10-09 18:04:12 +0000

    remove obsolute attributes

--- src/games/stendhal/tools/port1_2/port.sql
+++ src/games/stendhal/tools/port1_2/port.sql
@@ -1,11 +1,17 @@
+-- DELETE FROM marauroa.rpobject WHERE object_id IN (SELECT object_id FROM characters WHERE charname='Snaketails>oB<');
+DELETE FROM marauroa.characters WHERE charname='Snaketails>oB<';
+DELETE FROM marauroa.player WHERE username='Snaketails>oB<';
+
+DELETE FROM rpattribute WHERE name in ('fullghostmode', 'features');
+
 insert into account(id, username, password, email, timedate, status)
-select id, username, password, email, timedate, status from olddatabase.player;
+select id, username, password, email, timedate, status from marauroa.player;
 
 insert into characters(player_id, charname, object_id)
-select player_id, charname, object_id from olddatabase.characters;
+select player_id, charname, object_id from marauroa.characters;
 
 insert into rpobject(object_id)
-select object_id from olddatabase.characters;
+select object_id from marauroa.characters;
 
 insert into banlist(id, address, mask)
-select id, address, mask from olddatabase.banlist;
+select id, address, mask from marauroa.banlist;
