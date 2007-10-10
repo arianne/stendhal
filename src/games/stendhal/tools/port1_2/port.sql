48a6a3f0baa6727553db7936c74cf42f92e91e3e
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-10-10 18:25:05 +0000

    fixed character, removed banned player with invalid username, improved performance

--- src/games/stendhal/tools/port1_2/port.sql
+++ src/games/stendhal/tools/port1_2/port.sql
@@ -1,14 +1,24 @@
--- DELETE FROM marauroa.rpobject WHERE object_id IN (SELECT object_id FROM characters WHERE charname='Snaketails>oB<');
+-- deleted banned user with invalid characters
+DELETE FROM marauroa.rpobject USING marauroa.rpobject, marauroa.rpattribute
+WHERE rpobject.object_id = rpattribute.object_id AND rpattribute.name LIKE 'name%' AND rpattribute.value='Snaketails>oB<';
 DELETE FROM marauroa.characters WHERE charname='Snaketails>oB<';
 DELETE FROM marauroa.player WHERE username='Snaketails>oB<';
 
-DELETE FROM rpattribute WHERE name in ('fullghostmode', 'features');
+-- delete deprecated attributes
+DELETE FROM marauroa.rpattribute WHERE name in ('fullghostmode', 'features');
 
-insert into account(id, username, password, email, timedate, status)
-select id, username, password, email, timedate, status from marauroa.player;
 
-insert into characters(player_id, charname, object_id)
-select player_id, charname, object_id from marauroa.characters;
+INSERT INTO account(id, username, password, email, timedate, status)
+SELECT id, username, password, email, timedate, status FROM marauroa.player;
+
+alter table marauroa.rpattribute CHANGE value value BINARY(255);
+alter table marauroa.player CHANGE username username BINARY;
+
+INSERT INTO characters(player_id, charname, object_id)
+SELECT player.id, rpattribute.object_id, value 
+FROM marauroa.rpattribute, marauroa.player, marauroa.rpobject 
+WHERE player.username = rpattribute.value AND rpattribute.name LIKE 'name%' 
+AND rpobject.object_id=rpattribute.object_id AND rpobject.slot_id=0;
 
 insert into rpobject(object_id)
 select object_id from marauroa.characters;
