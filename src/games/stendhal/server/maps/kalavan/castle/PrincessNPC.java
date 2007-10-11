9f1f8ec4b5e53b45bea17ada2636b3b7724acb0a
Katie Russell <kymara@users.sourceforge.net>
2007-10-11 08:50:51 +0000

    fixed inconsistencies and added more prompts to say a QUEST_MESSAGE

--- src/games/stendhal/server/maps/kalavan/castle/PrincessNPC.java
+++ src/games/stendhal/server/maps/kalavan/castle/PrincessNPC.java
@@ -56,9 +56,9 @@ public class PrincessNPC implements ZoneConfigurator {
 				addReply("good","Good! Can I help you?");
 				addReply("bad","Oh dear ... Can I help you?");
 				addReply("well","Wonderful! Can I help you?");
-				addJob("I am the princess of this kingdom. To become one of my citizens, speak to Barrett Holmes in the city. He may be able to sell you a house.");
+				addJob("I am the princess of this kingdom. To become one of my citizens, speak to Barrett Holmes in the city. He may be able to sell you a house. But first, I have a #favour to ask of you...");
 				addHelp("Watch out for mad scientists. My father allowed them liberty to do some work in the basement and I am afraid things have got rather out of hand.");
-				addOffer("Sorry, but I do not have anything to offer you.");				
+				addOffer("Sorry, but I do not have anything to offer you. You could do me a #favour, though...");				
 				addGoodbye("Goodbye, and good luck.");
 			}
 		};
