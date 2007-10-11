cc066b8cfe111cc3ee2ed3cc4d47e660f6967a39
johnnnny <johnnnny@users.sourceforge.net>
2007-10-11 21:45:31 +0000

    fixed static fields in the TimedTeleportScroll class

--- src/games/stendhal/server/entity/item/scroll/RainbowBeansScroll.java
+++ src/games/stendhal/server/entity/item/scroll/RainbowBeansScroll.java
@@ -6,39 +6,32 @@ package games.stendhal.server.entity.item.scroll;
 import java.util.Map;
 
 /**
- * Represents the rainbow beans that takes the player to the dream world zone, after which
- * it will teleport player to a random location in 0_semos_plains_s.
- * 
- * infostring attribute in items.xml: 1_dreamscape 77 35 5400 0_semos_plains_s -1 -1
- * where 1_dreamscape is the target zone name;
- * 	77 and 35 are the target x and y position;
- * 	5400 is the number of turns before return;
- * 	0_semos_plains_s is the return zone;
- * 	-1 and -1 are the return x and y positions (negative value means a random position)
+ * Represents the rainbow beans that takes the player to the dream world zone,
+ * after which it will teleport player to a random location in 0_semos_plains_s.
  */
 public class RainbowBeansScroll extends TimedTeleportScroll {
 
 	/**
 	 * Creates a new timed marked teleport scroll
-	 *
+	 * 
 	 * @param name
 	 * @param clazz
 	 * @param subclass
 	 * @param attributes
 	 */
-	public RainbowBeansScroll(String name, String clazz, String subclass, Map<String, String> attributes) {
+	public RainbowBeansScroll(String name, String clazz, String subclass,
+			Map<String, String> attributes) {
 		super(name, clazz, subclass, attributes);
 	}
 
 	/**
 	 * copy constructor
-	 *
+	 * 
 	 * @param item item to copy
 	 */
 	public RainbowBeansScroll(RainbowBeansScroll item) {
 		super(item);
 	}
-
 
 	@Override
 	protected String getBeforeReturnMessage() {
@@ -47,6 +40,7 @@ public class RainbowBeansScroll extends TimedTeleportScroll {
 
 	@Override
 	protected String getAfterReturnMessage() {
-		return "You find yourself in the forest with a bad headache. That was a strange experience.";
+		return "You find yourself in the forest with a bad headache." +
+				" That was a strange experience.";
 	}
 }
--- src/games/stendhal/server/entity/item/scroll/TimedTeleportScroll.java
+++ src/games/stendhal/server/entity/item/scroll/TimedTeleportScroll.java
@@ -18,56 +18,27 @@ import marauroa.common.Log4J;
 import marauroa.common.Logger;
 
 /**
- * Represents a teleport scroll that takes the player to a specified location for a specified time, after which
- * it will teleport the player to given location.
+ * Represents a teleport scroll that takes the player to a specified location
+ * for a specified time, after which it will teleport the player to given
+ * location.
  * 
- * TODO: This class isn't fully self-containing as the LoginHandler (that handles the players logging in the target zone)
- * must be implemented elsewhere.
+ * infostring attribute in items.xml: 1_dreamscape 77 35 5400 0_semos_plains_s
+ * -1 -1 where 1_dreamscape is the target zone name; 77 and 35 are the target x
+ * and y position; 5400 is the number of turns before return; 0_semos_plains_s
+ * is the return zone; -1 and -1 are the return x and y positions (negative
+ * value means a random position)
+ * 
+ * TODO: This class isn't fully self-containing as the LoginHandler (that
+ * handles the players logging in the target zone) must be implemented
+ * elsewhere.
  */
 public class TimedTeleportScroll extends TeleportScroll {
 
-	private static final Logger logger = Log4J.getLogger(TimedTeleportScroll.class);
-
-	/* Note: the values here are for example only, they are overridden with the ones from items.xml infostring.
-	 */
-
-	protected static String targetZoneName = "1_dreamscape";
-	protected static int targetX = -1;
-	protected static int targetY = -1;
-
-	protected static int timeInTurns = 5400;
-
-	protected static String returnZoneName = "0_semos_plains_s";
-	protected static int returnX = -1;
-	protected static int returnY = -1;
-
-	/** set to true after the item has been initialized (infoString parsed) */
-	protected static boolean initialized = false;
+	private static final Logger logger = Log4J
+			.getLogger(TimedTeleportScroll.class);
 
 	/**
-	 * initialize scroll parameters.
-	 */
-	protected void init() {
-		String infoString = getInfoString();
-		if (infoString != null) {
-			StringTokenizer st = new StringTokenizer(infoString);
-			if (st.countTokens() == 7) {
-				targetZoneName = st.nextToken();
-				targetX = Integer.parseInt(st.nextToken());
-				targetY = Integer.parseInt(st.nextToken());
-				timeInTurns = Integer.parseInt(st.nextToken());
-				returnZoneName = st.nextToken();
-				returnX = Integer.parseInt(st.nextToken());
-				returnY = Integer.parseInt(st.nextToken());
-			} else {
-				throw new IllegalArgumentException("the infostring attribute is malformed");
-			}
-		}
-		initialized = true;
-	}
-
-	/**
-	 * Teleport the player back from the dream world.
+	 * Teleport the player back from the target zone.
 	 * 
 	 * @param player
 	 * @param returnZone
@@ -76,12 +47,30 @@ public class TimedTeleportScroll extends TeleportScroll {
 	 * @return true if teleport was successful
 	 */
 	public boolean teleportBack(Player player) {
-		if (!initialized) {
-			init();
+		String targetZoneName = null;
+		String returnZoneName = null;
+		int returnX = 0;
+		int returnY = 0;
+		String infoString = getInfoString();
+		if (infoString != null) {
+			StringTokenizer st = new StringTokenizer(infoString);
+			if (st.countTokens() == 7) {
+				targetZoneName = st.nextToken();
+				st.nextToken(); /* targetX */
+				st.nextToken(); /* targetY */
+				st.nextToken(); /* timeInTurns */
+				returnZoneName = st.nextToken();
+				returnX = Integer.parseInt(st.nextToken());
+				returnY = Integer.parseInt(st.nextToken());
+			} else {
+				throw new IllegalArgumentException(
+						"the infostring attribute is malformed");
+			}
 		}
 		int x = returnX;
 		int y = returnY;
-		if (player == null || player.getZone() == null || targetZoneName == null) {
+		if (player == null || player.getZone() == null
+				|| targetZoneName == null) {
 			return true;
 		}
 
@@ -89,7 +78,8 @@ public class TimedTeleportScroll extends TeleportScroll {
 			return true; /* player is already away from the target zone */
 		}
 
-		StendhalRPZone returnZone = StendhalRPWorld.get().getZone(returnZoneName);
+		StendhalRPZone returnZone = StendhalRPWorld.get().getZone(
+				returnZoneName);
 
 		if (x < 0) {
 			x = Rand.rand(returnZone.getWidth());
@@ -110,55 +100,80 @@ public class TimedTeleportScroll extends TeleportScroll {
 
 	/**
 	 * Creates a new timed marked teleport scroll
-	 *
+	 * 
 	 * @param name
 	 * @param clazz
 	 * @param subclass
 	 * @param attributes
 	 */
-	public TimedTeleportScroll(String name, String clazz, String subclass, Map<String, String> attributes) {
+	public TimedTeleportScroll(String name, String clazz, String subclass,
+			Map<String, String> attributes) {
 		super(name, clazz, subclass, attributes);
 	}
 
 	/**
 	 * copy constructor
-	 *
-	 * @param item item to copy
+	 * 
+	 * @param item
+	 *            item to copy
 	 */
 	public TimedTeleportScroll(TimedTeleportScroll item) {
 		super(item);
 	}
 
 	/**
-	 * Is invoked when a teleporting scroll is used. Tries to put the
-	 * player on the scroll's destination, or near it. 
-	 * @param player The player who used the scroll and who will be teleported
+	 * Is invoked when a teleporting scroll is used. Tries to put the player on
+	 * the scroll's destination, or near it.
+	 * 
+	 * @param player
+	 *            The player who used the scroll and who will be teleported
 	 * @return true iff teleport was successful
 	 */
 	@Override
 	protected boolean useTeleportScroll(Player player) {
-		if (!initialized) {
-			init();
+		String targetZoneName = null;
+		int targetX = 0;
+		int targetY = 0;
+		int timeInTurns = 0;
+		String infoString = getInfoString();
+		if (infoString != null) {
+			StringTokenizer st = new StringTokenizer(infoString);
+			if (st.countTokens() == 7) {
+				targetZoneName = st.nextToken();
+				targetX = Integer.parseInt(st.nextToken());
+				targetY = Integer.parseInt(st.nextToken());
+				timeInTurns = Integer.parseInt(st.nextToken());
+			} else {
+				throw new IllegalArgumentException(
+						"the infostring attribute is malformed");
+			}
 		}
+
 		/* check destination */
-		StendhalRPZone targetZone = StendhalRPWorld.get().getZone(targetZoneName);
+		StendhalRPZone targetZone = StendhalRPWorld.get().getZone(
+				targetZoneName);
 
 		if (targetZone != null) {
 			String beforeReturnMessage = getBeforeReturnMessage();
 			if (beforeReturnMessage != null) {
-				TurnNotifier.get().notifyInTurns((int)(timeInTurns * 0.9),
-						new TimedTeleportWarningTurnListener(player, StendhalRPWorld.get().getZone(targetZoneName),
+				TurnNotifier.get().notifyInTurns(
+						(int) (timeInTurns * 0.9),
+						new TimedTeleportWarningTurnListener(player,
+								StendhalRPWorld.get().getZone(targetZoneName),
 								beforeReturnMessage));
 			}
 			TurnNotifier.get().notifyInTurns(timeInTurns,
 					new TimedTeleportTurnListener(player));
 
-			/* we use the player as teleporter (last parameter) to give feedback if something goes wrong */
+			/*
+			 * we use the player as teleporter (last parameter) to give feedback
+			 * if something goes wrong
+			 */
 			return player.teleport(targetZone, targetX, targetY, null, player);
 		} else {
 			/* invalid zone - only log it */
-			logger.warn("Timed marked scroll to unknown zone " + targetZoneName + " teleported " + player.getName()
-			        + " to Semos instead");
+			logger.warn("Timed teleport scroll to unknown zone: "
+					+ targetZoneName);
 		}
 
 		return false;
@@ -177,6 +192,7 @@ public class TimedTeleportScroll extends TeleportScroll {
 
 	/**
 	 * override this to show a message before teleporting the player back.
+	 * 
 	 * @return the message to shown or null for no message
 	 */
 	protected String getBeforeReturnMessage() {
@@ -185,6 +201,7 @@ public class TimedTeleportScroll extends TeleportScroll {
 
 	/**
 	 * override this to show a message after teleporting the player back.
+	 * 
 	 * @return the message to shown or null for no message
 	 */
 	protected String getAfterReturnMessage() {
@@ -192,7 +209,8 @@ public class TimedTeleportScroll extends TeleportScroll {
 	}
 
 	/**
-	 * TimedTeleportTurnListener class is the implementation of the TurnListener interface for the timed teleport.
+	 * TimedTeleportTurnListener class is the implementation of the TurnListener
+	 * interface for the timed teleport.
 	 */
 	class TimedTeleportTurnListener implements TurnListener {
 
@@ -208,8 +226,9 @@ public class TimedTeleportScroll extends TeleportScroll {
 	}
 
 	/**
-	 * TimedTeleportWarningTurnListener class is the implementation of the TurnListener interface for the timed teleport
-	 * to send a warning message to the player before teleporting back.
+	 * TimedTeleportWarningTurnListener class is the implementation of the
+	 * TurnListener interface for the timed teleport to send a warning message
+	 * to the player before teleporting back.
 	 */
 	class TimedTeleportWarningTurnListener implements TurnListener {
 
@@ -217,7 +236,8 @@ public class TimedTeleportScroll extends TeleportScroll {
 		private final StendhalRPZone zone;
 		private final String warningMessage;
 
-		TimedTeleportWarningTurnListener(Player player, StendhalRPZone zone, String warningMessage) {
+		TimedTeleportWarningTurnListener(Player player, StendhalRPZone zone,
+				String warningMessage) {
 			this.player = player;
 			this.zone = zone;
 			this.warningMessage = warningMessage;
