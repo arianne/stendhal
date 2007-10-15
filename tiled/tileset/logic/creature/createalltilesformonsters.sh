45c1b76a99432a29630884aac97b952e62d6a126
Katie Russell <kymara@users.sourceforge.net>
2007-10-15 13:10:44 +0000

    ran ant update maps

--- tiled/tileset/logic/creature/createalltilesformonsters.sh
+++ tiled/tileset/logic/creature/createalltilesformonsters.sh
@@ -1,4 +1,5 @@
 #/bin/bash
+#./createtileset.sh ../../../../data/sprites/monsters/amazon 48x64+48+128 amazon.png
 #./createtileset.sh ../../../../data/sprites/monsters/animal 48x64+48+128 animal.png
 #./createtileset.sh ../../../../data/sprites/monsters/beholder 48x64+48+128 beholder.png
 #./createtileset.sh ../../../../data/sprites/monsters/chaos 48x64+48+128 chaos.png
@@ -15,16 +16,19 @@
 #./createtileset.sh ../../../../data/sprites/monsters/goblin 48x64+48+128 goblin.png
 #./createtileset.sh ../../../../data/sprites/monsters/golem 48x64+48+128 golem.png
 #./createtileset.sh ../../../../data/sprites/monsters/huge_animal 96x128+96+256 huge_animal.png
+./createtileset.sh ../../../../data/sprites/monsters/huger_animal 144x192+144+384 huger_animal.png
 #./createtileset.sh ../../../../data/sprites/monsters/human 48x64+48+128 human.png
-./createtileset.sh ../../../../data/sprites/monsters/hybrid 48x64+48+128 hybrid.png
+#./createtileset.sh ../../../../data/sprites/monsters/hybrid 48x64+48+128 hybrid.png
 #./createtileset.sh ../../../../data/sprites/monsters/kobold 48x64+48+128 kobold.png
+#./createtileset.sh ../../../../data/sprites/monsters/madaram 48x64+48+128 madaram.png
 #./createtileset.sh ../../../../data/sprites/monsters/minotaur 48x64+48+128 minotaur.png
 #./createtileset.sh ../../../../data/sprites/monsters/mummy 48x64+48+128 mummy.png
 #./createtileset.sh ../../../../data/sprites/monsters/mutant 48x64+48+128 mutant.png
-#./createtileset.sh ../../../../data/sprites/monsters/mythical_animal 194x256+194+512 mythical_animal.png
+./createtileset.sh ../../../../data/sprites/monsters/mythical_animal 194x256+194+512 mythical_animal.png
 #./createtileset.sh ../../../../data/sprites/monsters/orc 48x64+48+128 orc.png
 #./createtileset.sh ../../../../data/sprites/monsters/ratfolk 48x64+48+128 ratfolk.png
 #./createtileset.sh ../../../../data/sprites/monsters/small_animal 32x32+32+64 small_animal.png
 #./createtileset.sh ../../../../data/sprites/monsters/troll 48x64+48+128 troll.png
 #./createtileset.sh ../../../../data/sprites/monsters/undead 48x64+48+128 undead.png
 #./createtileset.sh ../../../../data/sprites/monsters/vampire 48x64+48+128 vampire.png
+
