e5f8ecc5191c84ea485767e56f4fff9fb67b79ee
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-10-21 13:36:54 +0000

    factored minimum length check out of AccountCreator into its own class

---
+++ src/games/stendhal/server/account/MinLengthValidator.java
@@ -0,0 +1,40 @@
+package games.stendhal.server.account;
+
+import marauroa.common.game.Result;
+
+/**
+ * validates that the given parameter is provided has a minimum length
+ *
+ * @author hendrik
+ */
+public class MinLengthValidator implements AccountParameterValidator {
+	private String parameterValue;
+	private int minLength;
+
+	/**
+	 * create a new MinLengthValidator
+	 *
+	 * @param parameterValue value to validate
+	 * @param minLength minimum required length 
+	 */
+	public MinLengthValidator(String parameterValue, int minLength) {
+		this.parameterValue = parameterValue;
+		this.minLength = minLength;
+	}
+
+	public Result validate() {
+
+		// provide a better error message in case the parameter-value is completly empty
+		if (parameterValue.length() == 0)  {
+			return Result.FAILED_EMPTY_STRING;
+		}
+
+		// check minimum length
+		if (parameterValue.length() < minLength)  {
+			return Result.FAILED_STRING_SIZE;
+		}
+
+		return null;
+	}
+	
+}
