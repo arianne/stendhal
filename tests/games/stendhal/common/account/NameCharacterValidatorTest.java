5a57065a900ac36034a54a7cfc898c2a192b5766
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-10-21 22:31:57 +0000

    added @Test annotation so that they are actually called

--- tests/games/stendhal/common/account/NameCharacterValidatorTest.java
+++ tests/games/stendhal/common/account/NameCharacterValidatorTest.java
@@ -3,20 +3,24 @@ package games.stendhal.common.account;
 import games.stendhal.server.account.NameCharacterValidator;
 
 import org.junit.Assert;
+import org.junit.Test;
 
 
 public class NameCharacterValidatorTest {
 
+	@Test
 	public void testSpecialCharcter() {
 		NameCharacterValidator validator = new NameCharacterValidator("asdf_");
 		Assert.assertNotNull(validator.validate());
 	}
 
+	@Test
 	public void testStartingWithNumber() {
 		NameCharacterValidator validator = new NameCharacterValidator("1asdf");
 		Assert.assertNotNull(validator.validate());
 	}
 
+	@Test
 	public void testOKString() {
 		NameCharacterValidator validator = new NameCharacterValidator("asdf");
 		Assert.assertNull(validator.validate());
