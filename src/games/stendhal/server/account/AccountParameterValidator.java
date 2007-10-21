b9e9ce5a59b7040fe0276d511b7bfb168e916431
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-10-21 13:35:53 +0000

    initial checkin of validator interface

---
+++ src/games/stendhal/server/account/AccountParameterValidator.java
@@ -0,0 +1,18 @@
+package games.stendhal.server.account;
+
+import marauroa.common.game.Result;
+
+/**
+ * validates a parameter used during account creation
+ *
+ * @author hendrik
+ */
+public interface AccountParameterValidator {
+
+	/**
+	 * validates a parameter provided for account creation.
+	 *
+	 * @return <code>null</code> in case the parameter is valid, or an error otherwise
+	 */
+	public Result validate();
+}
