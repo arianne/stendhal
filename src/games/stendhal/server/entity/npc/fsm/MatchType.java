d0aafbc041e07f33dddf76e54fde87eb7e07e495
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-10-26 16:51:14 +0000

    JavaDoc

--- src/games/stendhal/server/entity/npc/fsm/MatchType.java
+++ src/games/stendhal/server/entity/npc/fsm/MatchType.java
@@ -4,9 +4,13 @@ package games.stendhal.server.entity.npc.fsm;
  * How strong possible transitions should be matched.
  */
 public enum MatchType {
+
+	/** a transition that can happen from any state */
 	ABSOLUTE_JUMP,
 
+	/** a transition whose exspected input matches excatly to the actual input */
 	EXACT_MATCH,
 
+	/** a transition whose exspected input is very similary to the actual input */
 	SIMILAR_MATCH;
 }
