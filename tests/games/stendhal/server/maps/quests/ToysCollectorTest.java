c1997c1483b3755bff308cd62da37a44549e78e4
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-11-09 22:13:51 +0000

    reduced tests to the valuable ones

--- tests/games/stendhal/server/maps/quests/ToysCollectorTest.java
+++ tests/games/stendhal/server/maps/quests/ToysCollectorTest.java
@@ -65,93 +65,10 @@ public class ToysCollectorTest {
 	}
 
 	@Test
-	public final void testWelcomeBeforeStartingQuest() {
-		assertEquals("Mummy said, we are not allowed to talk to strangers. She is worried about that lost girl. But I'm bored. I want some #toys!",quest.welcomeBeforeStartingQuest());
-
-	}
-
-	@Test
-	public final void testWelcomeDuringActiveQuest() {
-		assertEquals("Hello! I'm still bored. Did you bring me toys?",quest.welcomeDuringActiveQuest());
-
-	}
-
-	@Test
-	public final void testWelcomeAfterQuestIsCompleted() {
-		assertEquals("Hi! I'm busy playing with my toys, no grown ups allowed.",quest.welcomeAfterQuestIsCompleted());
-
-	}
-
-	@Test
 	public final void testShouldWelcomeAfterQuestIsCompleted() {
 		assertTrue(quest.shouldWelcomeAfterQuestIsCompleted());
 	}
 
-	@Test
-	public final void testRespondToQuest() {
-		assertEquals("I'm not sure what toys, but whatever would be fun for me to play with! Will you bring me some please?",quest.respondToQuest());
 
-	}
-
-	@Test
-	public final void testRespondToQuestAfterItHasAlreadyBeenCompleted() {
-		assertEquals("The toys are great! Thanks!",quest.respondToQuestAfterItHasAlreadyBeenCompleted());
-
-	}
-
-	@Test
-	public final void testRespondToQuestAcception() {
-		assertEquals("Hooray! How exciting. See you soon.",quest.respondToQuestAcception());
-	}
-
-	@Test
-	public final void testRespondToQuestRefusal() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public final void testAskForMissingItems() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public final void testRespondToPlayerSayingHeHasNoItems() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public final void testAskForItemsAfterPlayerSaidHeHasItems() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public final void testRespondToItemBrought() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public final void testRespondToLastItemBrought() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public final void testRewardPlayer() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public final void testRespondToOfferOfNotExistingItem() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public final void testRespondToOfferOfNotMissingItem() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public final void testRespondToOfferOfNotNeededItem() {
-		fail("Not yet implemented");
-	}
 
 }
