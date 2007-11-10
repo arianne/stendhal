1bffe57199d49f2dcdf11d8ae38ef63357a5c637
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-11-10 23:06:29 +0000

    initial checkin

---
+++ src/games/stendhal/server/entity/npc/condition/QuestActiveCondition.java
@@ -0,0 +1,27 @@
+package games.stendhal.server.entity.npc.condition;
+
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.player.Player;
+
+/**
+ * Was this quest started but not completed?
+ */
+public class QuestActiveCondition extends SpeakerNPC.ChatCondition {
+
+	private String questname;
+
+	/**
+	 * Creates a new QuestActiveCondition
+	 *
+	 * @param questname name of quest slot
+	 */
+	public QuestActiveCondition(String questname) {
+		this.questname = questname;
+	}
+
+	@Override
+	public boolean fire(Player player, String text, SpeakerNPC engine) {
+		return (player.hasQuest(questname) 
+						&& !player.isQuestCompleted(questname));
+	}
+}
