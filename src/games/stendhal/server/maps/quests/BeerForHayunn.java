c4d99dfa17b01cbb55c120ea19f78aa8b6c77e39
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-11-11 12:45:28 +0000

    rewrote all post conditions (hidden in actions) into pre conditions and used standard interaction classes

--- src/games/stendhal/server/maps/quests/BeerForHayunn.java
+++ src/games/stendhal/server/maps/quests/BeerForHayunn.java
@@ -1,13 +1,23 @@
 package games.stendhal.server.maps.quests;
 
-import games.stendhal.server.StendhalRPWorld;
-import games.stendhal.server.entity.item.StackableItem;
 import games.stendhal.server.entity.npc.ConversationPhrases;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.action.DropItemAction;
+import games.stendhal.server.entity.npc.action.EquipItemAction;
+import games.stendhal.server.entity.npc.action.IncreaseXPAction;
+import games.stendhal.server.entity.npc.action.MultipleActions;
+import games.stendhal.server.entity.npc.action.SetQuestAction;
+import games.stendhal.server.entity.npc.condition.AndCondition;
+import games.stendhal.server.entity.npc.condition.NotCondition;
+import games.stendhal.server.entity.npc.condition.PlayerHasItemWithHimCondition;
+import games.stendhal.server.entity.npc.condition.QuestCompletedCondition;
+import games.stendhal.server.entity.npc.condition.QuestInStateCondition;
+import games.stendhal.server.entity.npc.condition.QuestNotCompletedCondition;
 import games.stendhal.server.entity.player.Player;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -73,19 +83,18 @@ public class BeerForHayunn extends AbstractQuest {
 		SpeakerNPC npc = npcs.get("Hayunn Naratha");
 
 		npc.add(ConversationStates.ATTENDING,
-			ConversationPhrases.QUEST_MESSAGES, null,
-			ConversationStates.QUEST_OFFERED, null,
-			new SpeakerNPC.ChatAction() {
-				@Override
-				public void fire(Player player, String text, SpeakerNPC npc) {
-					if (!player.isQuestCompleted(QUEST_SLOT)) {
-						npc.say("My mouth is dry, but I can't be seen to abandon my post! Could you bring me some #beer from the #tavern?");
-					} else {
-						npc.say("Thanks all the same, but I don't want to get too heavily into drinking; I'm still on duty, you know! I'll need my wits about me if a monster shows up...");
-						npc.setCurrentState(1);
-					}
-				}
-			});
+			ConversationPhrases.QUEST_MESSAGES, 
+			new QuestNotCompletedCondition(QUEST_SLOT),
+			ConversationStates.QUEST_OFFERED, 
+			"My mouth is dry, but I can't be seen to abandon my post! Could you bring me some #beer from the #tavern?",
+			null);
+
+		npc.add(ConversationStates.ATTENDING,
+			ConversationPhrases.QUEST_MESSAGES,
+			new QuestCompletedCondition(QUEST_SLOT),
+			ConversationStates.ATTENDING, 
+			"Thanks all the same, but I don't want to get too heavily into drinking; I'm still on duty, you know! I'll need my wits about me if a monster shows up...",
+			null);
 
 		npc.add(
 			ConversationStates.QUEST_OFFERED,
@@ -93,13 +102,7 @@ public class BeerForHayunn extends AbstractQuest {
 			null,
 			ConversationStates.ATTENDING,
 			"Thanks! I'll be right here, waiting. And guarding, of course.",
-			new SpeakerNPC.ChatAction() {
-				@Override
-				public void fire(Player player, String text,
-						SpeakerNPC npc) {
-					player.setQuest(QUEST_SLOT, "start");
-				}
-			});
+			new SetQuestAction(QUEST_SLOT, "start"));
 
 		npc.add(
 			ConversationStates.QUEST_OFFERED,
@@ -107,13 +110,7 @@ public class BeerForHayunn extends AbstractQuest {
 			null,
 			ConversationStates.ATTENDING,
 			"Oh, well forget it then. I guess I'll just hope for it to start raining, and then stand with my mouth open.",
-			new SpeakerNPC.ChatAction() {
-				@Override
-				public void fire(Player player, String text,
-						SpeakerNPC npc) {
-					player.setQuest(QUEST_SLOT, "rejected");
-				}
-			});
+			new SetQuestAction(QUEST_SLOT, "rejected"));
 
 		npc.add(
 			ConversationStates.QUEST_OFFERED,
@@ -145,57 +142,29 @@ public class BeerForHayunn extends AbstractQuest {
 		SpeakerNPC npc = npcs.get("Hayunn Naratha");
 
 		npc.add(ConversationStates.IDLE, ConversationPhrases.GREETING_MESSAGES,
-			new SpeakerNPC.ChatCondition() {
-				@Override
-				public boolean fire(Player player, String text,
-						SpeakerNPC npc) {
-					return player.hasQuest(QUEST_SLOT)
-							&& player.getQuest(QUEST_SLOT).equals("start");
-				}
-			}, ConversationStates.QUEST_ITEM_BROUGHT, null,
-			new SpeakerNPC.ChatAction() {
-				@Override
-				public void fire(Player player, String text, SpeakerNPC npc) {
-					if (player.isEquipped("beer")) {
-						npc.say("Hey! Is that beer for me?");
-					} else {
-						npc.say("Hey, I'm still waiting for that beer, remember? Anyway, what can I do for you?");
-						npc.setCurrentState(1);
-					}
-				}
-			});
+			new AndCondition(new QuestInStateCondition(QUEST_SLOT, "start"), new PlayerHasItemWithHimCondition("beer")),
+			ConversationStates.QUEST_ITEM_BROUGHT, 
+			"Hey! Is that beer for me?", null);
+
+		npc.add(ConversationStates.IDLE, ConversationPhrases.GREETING_MESSAGES,
+			new AndCondition(new QuestInStateCondition(QUEST_SLOT, "start"), new NotCondition(new PlayerHasItemWithHimCondition("beer"))),
+			ConversationStates.ATTENDING, 
+			"Hey, I'm still waiting for that beer, remember? Anyway, what can I do for you?",
+			null);
+
+		List<SpeakerNPC.ChatAction> reward = new LinkedList<SpeakerNPC.ChatAction>();
+		reward.add(new DropItemAction("beer"));
+		reward.add(new EquipItemAction("money", 20));
+		reward.add(new IncreaseXPAction(10));
+		reward.add(new SetQuestAction(QUEST_SLOT, "done"));
 
 		npc.add(
 			ConversationStates.QUEST_ITEM_BROUGHT,
 			ConversationPhrases.YES_MESSAGES,
-			// make sure the player isn't cheating by putting the
-			// beer away and then saying "yes"
-			new SpeakerNPC.ChatCondition() {
-				@Override
-				public boolean fire(Player player, String text,
-						SpeakerNPC npc) {
-					return player.isEquipped("beer");
-				}
-			},
+			new PlayerHasItemWithHimCondition("beer"),
 			ConversationStates.ATTENDING,
 			"*glug glug* Ah! That hit the spot. Let me know if you need anything, ok?",
-			new SpeakerNPC.ChatAction() {
-				@Override
-				public void fire(Player player, String text,
-						SpeakerNPC npc) {
-					player.drop("beer");
-					StackableItem money = (StackableItem) StendhalRPWorld
-							.get().getRuleManager()
-							.getEntityManager().getItem("money");
-					money.setQuantity(20);
-					player.equip(money);
-
-					player.addXP(10);
-
-					player.notifyWorldAboutChanges();
-					player.setQuest(QUEST_SLOT, "done");
-				}
-			});
+			new MultipleActions(reward));
 
 		npc.add(
 			ConversationStates.QUEST_ITEM_BROUGHT,
