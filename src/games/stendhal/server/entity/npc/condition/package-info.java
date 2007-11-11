13c2dcf088b3bd58ceeb8968b6bc1b0da61e8cc2
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-11-11 12:07:39 +0000

    added package declaration

--- src/games/stendhal/server/entity/npc/condition/package-info.java
+++ src/games/stendhal/server/entity/npc/condition/package-info.java
@@ -1,6 +1,8 @@
+package games.stendhal.server.entity.npc.condition;
+
 /**
  * This package contains conditions frequently used in quests.
- * Please use the conditions that fit best. 
+ * Please use the conditions that fit best.
  * (e. g. use QuestNotStartedCondition instead of Not(QuestStartedCondition)
- * because this will make it easier to read the state transition diagrams. 
+ * because this will make it easier to read the state transition diagrams.
  */
