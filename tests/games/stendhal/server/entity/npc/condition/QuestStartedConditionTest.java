0c78b81e906c03f3b2c763a3f53d0bfd164d0ff1
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-11-11 18:11:49 +0000

    intial commit

---
+++ tests/games/stendhal/server/entity/npc/condition/QuestStartedConditionTest.java
@@ -0,0 +1,71 @@
+package games.stendhal.server.entity.npc.condition;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import games.stendhal.server.entity.player.Player;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import utilities.PlayerTestHelper;
+import utilities.SpeakerNPCTestHelper;
+
+public class QuestStartedConditionTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testFire() {
+		assertFalse(new QuestStartedCondition("questname").fire(
+				PlayerTestHelper.createPlayer(), "testAdminConditionText",
+				SpeakerNPCTestHelper.createSpeakerNPC()));
+		Player bob = PlayerTestHelper.createPlayer();
+
+		bob.setQuest("questname", "done");
+		assertTrue(new QuestStartedCondition("questname").fire(bob,
+				"testAdminConditionText",
+				SpeakerNPCTestHelper.createSpeakerNPC()));
+
+		bob.setQuest("questname", null);
+		assertFalse(new QuestStartedCondition("questname").fire(bob,
+				"testAdminConditionText",
+				SpeakerNPCTestHelper.createSpeakerNPC()));
+
+	}
+
+	@Test
+	public final void testQuestNotStartedCondition() {
+		new QuestStartedCondition("questname");
+	}
+
+	@Test
+	public final void testToString() {
+		assertEquals("QuestStarted <questname>",
+				new QuestStartedCondition("questname").toString());
+	}
+
+	@Test
+	public void testEquals() throws Throwable {
+		assertFalse(new QuestStartedCondition("questname").equals(null));
+
+		QuestStartedCondition obj = new QuestStartedCondition("questname");
+		assertTrue(obj.equals(obj));
+
+		assertFalse(new QuestStartedCondition("questname").equals(new Object()));
+
+		assertTrue(new QuestStartedCondition("questname").equals(new QuestStartedCondition(
+				"questname")));
+		assertFalse(new QuestStartedCondition("questname").equals(new QuestStartedCondition(
+				"questname") {
+		}));
+	}
+
+}
