5dfd06982066f2f0db4130600e6f89fb034825e6
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-11-11 01:28:44 +0000

    initial checkin

---
+++ src/games/stendhal/server/entity/npc/condition/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains conditions frequently used in quests.
+ * Please use the conditions that fit best. 
+ * (e. g. use QuestNotStartedCondition instead of Not(QuestStartedCondition)
+ * because this will make it easier to read the state transition diagrams. 
+ */
