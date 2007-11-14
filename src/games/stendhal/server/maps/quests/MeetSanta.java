bf30543a9b2ff8ed7859f7b27f815efc5f2d27c9
Katie Russell <kymara@users.sourceforge.net>
2007-11-14 16:04:52 +0000

    Make santa give the players who say hi a hat. it doesn't wear off so after xmas players can change back manually, or if they die/do pizza/go to athor it gets reverted. Make semos city use the city_xmas tmx file for 0.65

--- src/games/stendhal/server/maps/quests/MeetSanta.java
+++ src/games/stendhal/server/maps/quests/MeetSanta.java
@@ -2,6 +2,7 @@ package games.stendhal.server.maps.quests;
 
 import games.stendhal.server.StendhalRPWorld;
 import games.stendhal.server.StendhalRPZone;
+import games.stendhal.server.entity.Outfit;
 import games.stendhal.server.entity.npc.ConversationPhrases;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.SpeakerNPC;
@@ -11,6 +12,7 @@ import games.stendhal.server.entity.npc.action.MultipleActions;
 import games.stendhal.server.entity.npc.action.SetQuestAction;
 import games.stendhal.server.entity.npc.condition.QuestCompletedCondition;
 import games.stendhal.server.entity.npc.condition.QuestNotCompletedCondition;
+import games.stendhal.server.entity.player.Player;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -20,9 +22,9 @@ import java.util.List;
  * 
  * PARTICIPANTS: - Santa Claus
  * 
- * STEPS: - Find Santa - Say hi - Get reward
+ * STEPS: - Find Santa - Say hi - Get reward - Get hat
  * 
- * REWARD: - a present which can be opend to obtain a random good reward: food,
+ * REWARD: - a present which can be opened to obtain a random good reward: food,
  * money, potions, items, etc...
  * 
  * REPETITIONS: - None
@@ -59,16 +61,32 @@ public class MeetSanta extends AbstractQuest {
 				List<SpeakerNPC.ChatAction> reward = new LinkedList<SpeakerNPC.ChatAction>();
 				reward.add(new EquipItemAction("present"));
 				reward.add(new SetQuestAction(QUEST_SLOT, "done"));
-				
+				reward.add(new ChatAction() {
+					@Override
+					public void fire(Player player, String text, SpeakerNPC npc) {
+					    /*sorry but I (kymara) don't know how to make a proper thing 
+					     *out of this like for the other rewards. Hope this is ok*/
+				        
+				        // fetch old outfit as we want to know the current hair
+				        Outfit oldoutfit = player.getOutfit();
+					// all santa hat sprites are at 50 + current hair
+					int hatnumber = oldoutfit.getHair() + 50;
+					// the new outfit only changes the hair, rest is null
+				        Outfit newOutfit = new Outfit(hatnumber, null, null, null);
+					    //put it on, and store old outfit.
+				        player.setOutfit(newOutfit.putOver(oldoutfit), true);
+					}
+				}
+					   );
 				add(ConversationStates.IDLE,
 					ConversationPhrases.GREETING_MESSAGES,
 					new QuestNotCompletedCondition(QUEST_SLOT),
 					ConversationStates.ATTENDING,
-					"Merry Christmas! I have a present for you.",
+					"Merry Christmas! I have a present and a hat for you.",
 					new MultipleActions(reward));
 
 				addJob("I am Santa Claus! Where have you been in these years?");
-				addGoodbye();
+				addGoodbye("Good bye, and remember to behave if you want a present next year!");
 			}
 		};
 		npcs.add(santa);
