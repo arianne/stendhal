ee8b1731e9077d9caf65b80c71ff6bfcc3590ef6
timothyb89 <timothyb89@users.sourceforge.net>
2007-11-14 23:05:55 +0000

    Now displays dot output. This (should) create a new temp file in the system tempdir (/tmp, C:\temp) for the RPEvent output and the image, which is then displayed in a JDialog. The path to dot can be configured in the client properties with the stendhal.dotPath property, otherwise we'll just hope its on the PATH. Any terminal output is printed to stdout as well. TODO: image scaling (some npcs, like Monogenes, have to many transitions to fit on the screen) and using the client window as the dialog's parent.

--- src/games/stendhal/client/gui/admin/TransitionDiagram.java
+++ src/games/stendhal/client/gui/admin/TransitionDiagram.java
@@ -1,9 +1,61 @@
 package games.stendhal.client.gui.admin;
 
+import games.stendhal.client.update.ClientGameConfiguration;
+import java.awt.Frame;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import javax.swing.ImageIcon;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import org.apache.log4j.Logger;
+
 public class TransitionDiagram {
 
-	public void showTransitionDiagram(String data) {
-		// TODO
+        private static final Logger logger = Logger.getLogger(TransitionDiagram.class);
+    
+        public void showTransitionDiagram(String data) {
+            showTransitionDiagram(data, null);
+        }
+        
+	public void showTransitionDiagram(String data, Frame parent) {
+		try {
+                        File dat = File.createTempFile("stendhal-graph-data", ".txt");
+                        File image = File.createTempFile("stendhal-graph", ".png");
+
+                        //print the data
+                        PrintStream ps = new PrintStream(new FileOutputStream(dat));
+                        ps.print(data);
+                        ps.close();
+                        
+                        //execute
+                        String dotPath = ClientGameConfiguration.get("stendhal.dotPath"); //enable again when done testing
+                        if (dotPath == null) {
+                            dotPath = "dot";//PATH?
+                        }
+                        
+                        //execute
+                        Process p = Runtime.getRuntime().exec(dotPath + " -Tpng -o " + image.getAbsolutePath() + " " + dat.getAbsolutePath());
+                        
+                        //print output
+                        BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+                        String line;
+                        while ((line = br.readLine()) != null) {
+                            System.out.println("dot output: " + line);
+                        }
+                        
+                        //open up the image
+                        JDialog jd = new JDialog(parent, "NPC Transition Viewer", true);
+                        jd.add(new JLabel(new ImageIcon(image.toURI().toURL())));
+                        jd.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+                        jd.pack();
+                        jd.setVisible(true);
+                } catch (Exception e) {
+                        //logger.error("Failed creating graph!", e);
+                        e.printStackTrace();
+                }
 	}
 	
 	public static void main(String[] args) {
