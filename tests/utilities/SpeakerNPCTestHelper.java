eb9a8774efb1697338fcabec4fe00fe71dea32e2
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-11-18 16:20:10 +0000

    added createSpeakerNpc(String name)

--- tests/utilities/SpeakerNPCTestHelper.java
+++ tests/utilities/SpeakerNPCTestHelper.java
@@ -7,8 +7,13 @@ import games.stendhal.server.entity.npc.SpeakerNPC;
 public class SpeakerNPCTestHelper  {
 
 	public static SpeakerNPC createSpeakerNPC() {
+		return createSpeakerNPC("bob");
+	}
+
+	public static SpeakerNPC createSpeakerNPC(String name) {
 		PlayerHelper.generateNPCRPClasses();
-		return new SpeakerNPC("bob");
+		return new SpeakerNPC(name);
+
 	}
 
 
