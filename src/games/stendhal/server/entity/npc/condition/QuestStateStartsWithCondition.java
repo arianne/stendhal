71de06b637523462c79a5d97f8f716d21d0a2c28
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-11-24 03:06:51 +0000

    initial checkin

---
+++ src/games/stendhal/server/entity/npc/condition/QuestStateStartsWithCondition.java
@@ -0,0 +1,48 @@
+package games.stendhal.server.entity.npc.condition;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.player.Player;
+
+/**
+ * Does the quest state start with the specified String?
+ */
+public class QuestStateStartsWithCondition extends SpeakerNPC.ChatCondition {
+
+	private String questname;
+	private String state;
+
+	/**
+	 * Creates a new QuestStateStartsWithCondition
+	 *
+	 * @param questname name of quest-slot
+	 * @param state start of state-string
+	 */
+	public QuestStateStartsWithCondition(String questname, String state) {
+		this.questname = questname;
+		this.state = state;
+	}
+
+	@Override
+	public boolean fire(Player player, String text, SpeakerNPC engine) {
+		return (player.hasQuest(questname) && player.getQuest(questname).startsWith(state));
+	}
+
+	@Override
+	public String toString() {
+		return "QuestStateStartsWith <" + questname + "," + state + ">";
+	}
+
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj, false, QuestStartedCondition.class);
+	}
+}
