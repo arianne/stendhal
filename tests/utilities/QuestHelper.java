514a4dbe7c27e4c11167d46d25c7b6f766bb7fcc
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-11-30 22:10:36 +0000

    initial checkin

---
+++ tests/utilities/QuestHelper.java
@@ -0,0 +1,26 @@
+package utilities;
+
+import static org.junit.Assert.assertTrue;
+import games.stendhal.server.maps.MockStendhalRPRuleProcessor;
+import games.stendhal.server.maps.MockStendlRPWorld;
+import marauroa.common.Log4J;
+
+import org.junit.BeforeClass;
+
+/**
+ * Helper methods for testing quests
+ *
+ * @author hendrik
+ */
+public class QuestHelper {
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		Log4J.init();
+		PlayerHelper.generatePlayerRPClasses();
+		PlayerHelper.generateItemRPClasses();
+		assertTrue(MockStendhalRPRuleProcessor.get() instanceof MockStendhalRPRuleProcessor);
+		MockStendlRPWorld.get();
+	}
+
+}
