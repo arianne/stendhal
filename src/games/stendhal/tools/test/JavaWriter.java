050667f04f987eb2fb43846dd60bb52a66cd839a
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-11-30 21:53:56 +0000

    added method to writer player and npc spoken text

--- src/games/stendhal/tools/test/JavaWriter.java
+++ src/games/stendhal/tools/test/JavaWriter.java
@@ -2,12 +2,14 @@ package games.stendhal.tools.test;
 
 import java.io.PrintStream;
 
+import org.apache.commons.lang.StringEscapeUtils;
+
 /**
  * writes parts of the java file for a chat test case
  *
  * @author hendrik
  */
-public class JavaWriter {
+class JavaWriter {
 	private PrintStream out;
 	
 	JavaWriter(PrintStream out) {
@@ -22,5 +24,15 @@ public class JavaWriter {
 
 	public void comment(String line) {
 		out.println("\t\t// " + line);
+	}
+
+	public void player(String protagonist, String text) {
+		out.println("\t\ten.step(player, \"" + StringEscapeUtils.escapeJava(text) + "\");");
+	}
+
+	public void npc(String protagonist, String text) {
+		out.println("\t\tassertEquals(\""
+			+ StringEscapeUtils.escapeJava(text)
+			+ "\", " + protagonist + ".get(\"text\"));");
 	}
 }
