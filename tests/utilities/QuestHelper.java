1eb4e42ead35b583c9e8b4cccdd3fad94592b7a4
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-12-02 13:49:47 +0000

    clear NPCList()

--- tests/utilities/QuestHelper.java
+++ tests/utilities/QuestHelper.java
@@ -1,6 +1,7 @@
 package utilities;
 
 import static org.junit.Assert.assertTrue;
+import games.stendhal.server.entity.npc.NPCList;
 import games.stendhal.server.maps.MockStendhalRPRuleProcessor;
 import games.stendhal.server.maps.MockStendlRPWorld;
 import marauroa.common.Log4J;
@@ -22,6 +23,7 @@ public class QuestHelper {
 		PlayerHelper.generateNPCRPClasses();
 		assertTrue(MockStendhalRPRuleProcessor.get() instanceof MockStendhalRPRuleProcessor);
 		MockStendlRPWorld.get();
+		NPCList.get().clear();
 	}
 
 }
