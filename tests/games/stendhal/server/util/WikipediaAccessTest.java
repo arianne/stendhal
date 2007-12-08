d92045f63a922568689ee1bab12fe74d5672b407
Martin Fuchs <martinfuchs@users.sourceforge.net>
2007-12-08 15:15:19 +0000

    add a test for class WikipediaAccess

---
+++ tests/games/stendhal/server/util/WikipediaAccessTest.java
@@ -0,0 +1,36 @@
+package games.stendhal.server.util;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * Tests the WikipediaAccess class
+ *
+ * @author Martin Fuchs
+ */
+public class WikipediaAccessTest {
+
+	@Test
+	public void test() {
+        WikipediaAccess access = new WikipediaAccess("Stendhal");
+
+        access.run();
+
+		if (access.getError() != null) {
+			fail("Wikipedia access was not successfull: "+access.getError());
+		} else if (access.isFinished()) {
+    		if (access.getText()!=null && access.getText().length()>0) {
+    			String result = access.getProcessedText();
+
+    			//System.out.println(result);
+
+    			assertTrue("There should be named the french novelist for the topic Stendhal.", result.contains("Marie-Henri Beyle"));
+    		} else {
+    			fail("Sorry, could not find information on this topic in Wikipedia.");
+    		}
+		}
+	}
+
+} 
