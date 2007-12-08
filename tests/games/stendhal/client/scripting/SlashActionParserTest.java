28697b145a6656cf8291bc30eb0663e9f41fbbef
Martin Fuchs <martinfuchs@users.sourceforge.net>
2007-12-08 11:46:33 +0000

    initialise action repository before running tests

--- tests/games/stendhal/client/scripting/SlashActionParserTest.java
+++ tests/games/stendhal/client/scripting/SlashActionParserTest.java
@@ -3,10 +3,13 @@ package games.stendhal.client.scripting;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import games.stendhal.client.actions.SlashActionRepository;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import utilities.QuestHelper;
 
 /**
  * test SlashActionParser class
@@ -15,31 +18,54 @@ import org.junit.Test;
  */
 public class SlashActionParserTest {
 
+	@BeforeClass
+	public static void setupclass() {
+		QuestHelper.setUpBeforeClass();
+		SlashActionRepository.register();
+	}
+
 	@Test
 	public final void test() {
 		SlashActionCommand cmd = SlashActionParser.parse("who");
 		assertFalse(cmd.getError());
 		assertEquals("who", cmd.getName());
-		assertEquals("", cmd.getRemainder());
 		assertNotNull(cmd.getParams());
-		assertEquals(1, cmd.getParams().length);
-		assertNull(cmd.getParams()[0]);
+		assertEquals(0, cmd.getParams().length);
+		assertEquals("", cmd.getRemainder());
 
 		cmd = SlashActionParser.parse("where ghost");
 		assertFalse(cmd.getError());
 		assertEquals("where", cmd.getName());
-		assertEquals("", cmd.getRemainder());
 		assertNotNull(cmd.getParams());
 		assertEquals(1, cmd.getParams().length);
 		assertEquals("ghost", cmd.getParams()[0]);
+		assertEquals("", cmd.getRemainder());
 
 		cmd = SlashActionParser.parse("where 'player 2'");
 		assertFalse(cmd.getError());
 		assertEquals("where", cmd.getName());
-		assertEquals("", cmd.getRemainder());
 		assertNotNull(cmd.getParams());
 		assertEquals(1, cmd.getParams().length);
 		assertEquals("player 2", cmd.getParams()[0]);
+		assertEquals("", cmd.getRemainder());
+
+		cmd = SlashActionParser.parse("adminlevel player 100");
+		assertFalse(cmd.getError());
+		assertEquals("adminlevel", cmd.getName());
+		assertNotNull(cmd.getParams());
+		assertEquals(2, cmd.getParams().length);
+		assertEquals("player", cmd.getParams()[0]);
+		assertEquals("100", cmd.getParams()[1]);
+		assertEquals("", cmd.getRemainder());
+
+		cmd = SlashActionParser.parse("jail player minutes reason");
+		assertFalse(cmd.getError());
+		assertEquals("jail", cmd.getName());
+		assertNotNull(cmd.getParams());
+		assertEquals(2, cmd.getParams().length);
+		assertEquals("player", cmd.getParams()[0]);
+		assertEquals("minutes", cmd.getParams()[1]);
+		assertEquals("reason", cmd.getRemainder());
 	}
 
 	@Test
