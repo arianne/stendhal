36375c681ca2e38d8cc21c3cee20fefa1670aac8
Martin Fuchs <martinfuchs@users.sourceforge.net>
2007-12-09 14:17:06 +0000

    add test case for selling cheese to the bar maid

--- tests/utilities/PlayerTestHelper.java
+++ tests/utilities/PlayerTestHelper.java
@@ -7,6 +7,10 @@ import marauroa.common.game.RPObject;
 
 public class PlayerTestHelper  {
 
+	/**
+	 * create an unnamed player object
+	 * @return player
+	 */
 	public static Player createPlayer() {
 		PlayerHelper.generatePlayerRPClasses();
 		Player pl = new Player(new RPObject());
@@ -14,18 +18,41 @@ public class PlayerTestHelper  {
 		pl.put("outfit", "01010101");
 		return pl;
 	}
+
+	/**
+	 * create a named player
+	 * @param name
+	 * @return player
+	 */
 	public static Player createPlayer(String name) {
 		Player pl = createPlayer();
 		pl.setName(name);
 		return pl;
 	}
 
+	/**
+	 * equip the player with the given amount of money
+	 * @param player
+	 * @param amount
+	 * @return success flag
+	 */
 	public static boolean equipWithMoney(Player player, int amount) {
+		return equipWithStackableItem(player, "money", amount);
+	}
+
+	/**
+	 * equip the player with the given amount of items
+	 * @param player
+	 * @param clazz
+	 * @param amount
+	 * @return success flag
+	 */
+	public static boolean equipWithStackableItem(Player player, String clazz, int amount) {
 		StendhalRPWorld world = StendhalRPWorld.get();
 
-		StackableItem money = (StackableItem) world.getRuleManager().getEntityManager().getItem("money");
-		money.setQuantity(amount);
+		StackableItem item = (StackableItem) world.getRuleManager().getEntityManager().getItem(clazz);
+		item.setQuantity(amount);
 
-		return player.equip(money);
+		return player.equip(item);
 	}
 }
