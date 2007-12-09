db39727e6ea1ec61c0f45db8ff5b6c4ee626a25a
Martin Fuchs <martinfuchs@users.sourceforge.net>
2007-12-09 21:47:34 +0000

    add new test helper functions

--- tests/utilities/PlayerTestHelper.java
+++ tests/utilities/PlayerTestHelper.java
@@ -1,8 +1,14 @@
 package utilities;
 
 import games.stendhal.server.StendhalRPWorld;
+import games.stendhal.server.StendhalRPZone;
 import games.stendhal.server.entity.item.StackableItem;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.NPCList;
+import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.maps.MockStendhalRPRuleProcessor;
+import games.stendhal.server.maps.MockStendlRPWorld;
 import marauroa.common.game.RPObject;
 
 public class PlayerTestHelper  {
@@ -31,6 +37,84 @@ public class PlayerTestHelper  {
 	}
 
 	/**
+	 * register a player in rule processor, world and zone
+	 * @param player
+	 * @param zoneName
+	 */
+	public static void registerPlayer(Player player, String zoneName) {
+		registerPlayer(player, StendhalRPWorld.get().getZone(zoneName));
+	}
+
+	/**
+	 * register a player in rule processor, world and zone
+	 * @param player
+	 * @param zone
+	 */
+	public static void registerPlayer(Player player, StendhalRPZone zone) {
+		MockStendhalRPRuleProcessor.get().addPlayer(player);
+		MockStendlRPWorld.get().add(player);
+		zone.add(player);
+	}
+
+	/**
+	 * remove a player from rule processor, world and zone
+	 * @param player
+	 * @param zone
+	 */
+	public static void unregisterPlayer(Player player, StendhalRPZone zone) {
+		zone.remove(player);
+		removePlayer(player);
+	}
+
+	/**
+	 * remove a player from rule processor, world and zone
+	 * @param player
+	 * @param zone
+	 */
+	public static void removePlayer(String playerName, StendhalRPZone zone) {
+		Player player = MockStendhalRPRuleProcessor.get().getPlayer(playerName);
+
+		if (player != null) {
+			unregisterPlayer(player, zone);
+		}
+    }
+
+	/**
+	 * remove a player from rule processor, world and zone
+	 * @param playerName
+	 * @param zoneName
+	 */
+	public static void removePlayer(String playerName, String zoneName)
+    {
+		removePlayer(playerName, MockStendlRPWorld.get().getZone(zoneName));
+    }
+
+	/**
+	 * remove a player from world and rule processor
+	 * @param playerName
+	 */
+	public static void removePlayer(String playerName)
+    {
+		Player player = MockStendhalRPRuleProcessor.get().getPlayer(playerName);
+
+		if (player != null) {
+			removePlayer(player);
+		}
+    }
+
+	/**
+	 * remove a player from world and rule processor
+	 * @param player
+	 */
+	public static void removePlayer(Player player)
+    {
+		if (player != null) {
+			MockStendlRPWorld.get().remove(player.getID());
+			MockStendhalRPRuleProcessor.get().removePlayer(player);
+		}
+    }
+
+	/**
 	 * equip the player with the given amount of money
 	 * @param player
 	 * @param amount
@@ -55,4 +139,35 @@ public class PlayerTestHelper  {
 
 		return player.equip(item);
 	}
+
+	/**
+	 * reset the conversation state of the named NPC
+	 * @param string
+	 */
+	public static void resetNPC(String npcName)
+    {
+		SpeakerNPC npc = NPCList.get().get(npcName);
+
+		if (npc != null) {
+			npc.setCurrentState(ConversationStates.IDLE);
+		}
+    }
+
+	/**
+	 * remove the named NPC
+	 * @param npcName
+	 */
+	public static void removeNPC(String npcName)
+    {
+		NPCList.get().remove(npcName);
+    }
+
+	/**
+	 * remove a zone from the world
+	 * @param name
+	 */
+	public static void removeZone(String zoneName)
+    {
+		//TODO
+    }
 }
