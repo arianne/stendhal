1c421e4423a74b8afdcdd54fe007f0dae44d77b5
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-12-09 11:26:58 +0000

    adjusted test script to display map of Semos and added a description text.

--- src/games/stendhal/server/script/ImageViewTest.java
+++ src/games/stendhal/server/script/ImageViewTest.java
@@ -24,10 +24,17 @@ public class ImageViewTest extends ScriptImpl {
         generateRPEvent();
 
         RPEvent event = new RPEvent("examine");
-        event.put("path", "/data/gui/StendhalSplash.jpg");
-        event.put("alt", "Test image");
-        event.put("title", "Image viewer test");
-        
+        event.put("path", "/data/sprites/examine/map-semos-city.png");
+        event.put("alt", "Map of Semos City");
+        event.put("title", "Semos City");
+        event.put("text", "Semos City is your starting point and you will return here often during your journey through the world.<br>"
+    		+ "1&nbsp;Townhall,&nbsp;Tad&nbsp;lives&nbsp;here, "
+    		+ "2&nbsp;Library, 3&nbsp;Bank, 4&nbsp;Storage, 5&nbsp;Bakery, "
+        	+ "6&nbsp;Blacksmith,&nbsp;Carmen, 7&nbsp;Inn,&nbsp;Margaret, "
+        	+ "8&nbsp;Temple,&nbsp;ilisa, 9&nbsp;Dangerous&nbsp;Dungeon<br>"
+        	+ "A&nbsp;Semos&nbsp;Village, B&nbsp;Northern Plains&nbsp;and&nbsp;Mine, "
+        	+ "C&nbsp;Very&nbsp;long&nbsp;path&nbsp;to&nbsp;Ados, "
+        	+ "D&nbsp;Southern&nbsp;Plains&nbsp;and&nbsp;Nalwor&nbsp;Forest");
         admin.addEvent(event);
     }
 }
