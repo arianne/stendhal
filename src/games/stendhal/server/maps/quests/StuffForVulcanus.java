e29bf2cabd6fd672800a833e4b4fb0cfc9732ec6
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-12-12 22:45:37 +0000

    fixed NullPointerException in case the player asked to get something forged before the quest is started (fixes #1848716)

--- src/games/stendhal/server/maps/quests/StuffForVulcanus.java
+++ src/games/stendhal/server/maps/quests/StuffForVulcanus.java
@@ -6,6 +6,7 @@ import games.stendhal.server.entity.item.Item;
 import games.stendhal.server.entity.npc.ConversationPhrases;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.condition.QuestStartedCondition;
 import games.stendhal.server.entity.npc.condition.QuestStateStartsWithCondition;
 import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.util.TimeUtil;
@@ -255,7 +256,8 @@ public class StuffForVulcanus extends AbstractQuest {
 			});
 
 		npc.add(ConversationStates.ATTENDING,
-			Arrays.asList("forge", "missing"), null,
+			Arrays.asList("forge", "missing"),
+			new QuestStartedCondition(QUEST_SLOT),
 			ConversationStates.ATTENDING, null,
 			new SpeakerNPC.ChatAction() {
 				@Override
