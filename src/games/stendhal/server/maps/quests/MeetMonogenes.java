d1b17e0ea9e13115ac5d651d3e18f9779040c7a8
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-12-12 14:58:25 +0000

    Monogenes now offers a map using the examine client window

--- src/games/stendhal/server/maps/quests/MeetMonogenes.java
+++ src/games/stendhal/server/maps/quests/MeetMonogenes.java
@@ -4,6 +4,7 @@ import games.stendhal.server.entity.npc.ConversationPhrases;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.Sentence;
 import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.action.ExamineChatAction;
 import games.stendhal.server.entity.player.Player;
 
 /**
@@ -13,7 +14,7 @@ import games.stendhal.server.entity.player.Player;
  * Monogenes. - Be polite and say "bye" at the end of the conversation to get a
  * small reward.
  * 
- * REWARD: - 10 XP (check that user's level is lesser than 5) - No money
+ * REWARD: broken (- 10 XP (check that user's level is lesser than 2) - No money)
  * 
  * REPETITIONS: - None
  * 
@@ -48,79 +49,84 @@ public class MeetMonogenes extends AbstractQuest {
 					}
 				});
 
-		npc
-				.add(
-						ConversationStates.ATTENDING,
-						ConversationPhrases.HELP_MESSAGES,
-						null,
-						ConversationStates.INFORMATION_1,
-						"I'm Diogenes' older brother and I don't actually remember what I used to do... I'm retired now. I can offer you a few tips on socializing with the residents of Semos, if you like?",
-						null);
+		npc.add(
+			ConversationStates.ATTENDING,
+			ConversationPhrases.HELP_MESSAGES,
+			null,
+			ConversationStates.INFORMATION_1,
+			"I'm Diogenes' older brother and I don't actually remember what I used to do... I'm retired now. I can offer you a few tips on socializing with the residents of Semos, if you like?",
+			null);
 
-		npc
-				.add(
-						ConversationStates.INFORMATION_1,
-						ConversationPhrases.YES_MESSAGES,
-						null,
-						ConversationStates.INFORMATION_2,
-						"First of all, you should introduce yourself by saying HI. After this, try to keep the conversation to the topics they bring up; suitable subjects will be highlighted #like #this. A few generally safe topics of conversation are the person's #job, asking for #help, asking if they have an #offer to make, and asking for a #quest to go on. Anyway, do you want a quick run-down of the buildings in Semos?",
-						null);
+		npc.add(
+			ConversationStates.INFORMATION_1,
+			ConversationPhrases.YES_MESSAGES,
+			null,
+			ConversationStates.INFORMATION_2,
+			"First of all, you should introduce yourself by saying HI. After this, try to keep the conversation to the topics they bring up; suitable subjects will be highlighted #like #this. A few generally safe topics of conversation are the person's #job, asking for #help, asking if they have an #offer to make, and asking for a #quest to go on. Anyway, do you want a quick run-down of the buildings in Semos?",
+			null);
 
-		npc
-				.add(
-						ConversationStates.INFORMATION_1,
-						"no",
-						null,
-						ConversationStates.IDLE,
-						"And how are you supposed to know what's happening? By reading the Semos Tribune? Hah! Bye, then.",
-						null);
+		npc.add(
+			ConversationStates.INFORMATION_1,
+			"no",
+			null,
+			ConversationStates.IDLE,
+			"And how are you supposed to know what's happening? By reading the Semos Tribune? Hah! Bye, then.",
+			null);
 
-		npc
-				.add(
-						ConversationStates.INFORMATION_2,
-						ConversationPhrases.YES_MESSAGES,
-						null,
-						ConversationStates.ATTENDING,
-						"It's a good idea to read the wooden signs to identify places; just right-click on them and choose LOOK. Now, I can direct you to the #bank, the #library, the #tavern, the #temple, the #blacksmith, the #bakery, or the old #village.",
-						null);
+		npc.add(
+			ConversationStates.INFORMATION_2,
+			ConversationPhrases.YES_MESSAGES,
+			null,
+			ConversationStates.ATTENDING,
+			"It's a good idea to read the wooden signs to identify places; just right-click on them and choose LOOK. Now, I can show you a #map or direct you to the #bank, the #library, the #tavern, the #temple, the #blacksmith, the #bakery, or the old #village.",
+			null);
 
-		npc
-				.add(
-						ConversationStates.INFORMATION_2,
-						"no",
-						null,
-						ConversationStates.IDLE,
-						"Already finding your way about, huh? Glad to hear it! Goodbye!",
-						null);
+		npc.add(
+			ConversationStates.INFORMATION_2,
+			"no",
+			null,
+			ConversationStates.IDLE,
+			"Already finding your way about, huh? Glad to hear it! Goodbye!",
+			null);
+		
+		npc.add(
+			ConversationStates.ATTENDING,
+			"map", null, ConversationStates.ATTENDING, "Caption\n"
+			+ "1 Townhall, Tad lives here,   2 Library,   3 Bank,   4 Storage,\n"
+			+ "5 Bakery,   6 Blacksmith, Carmen,   7 Inn, Margaret \n"
+        	+ "8 Temple, ilisa,   9 Dangerous Dungeon \n"
+        	+ "A Semos Village,   B Northern Plains and Mine, \n"
+        	+ "C Very long path to Ados, \n"
+        	+ "D Southern Plains and Nalwor Forest",
+        	new ExamineChatAction("map-semos-city.png", "Semos City", "Map of Semos City"));
 
-		npc
-				.addReply(
-						"bank",
-						"See this big building in front of me, with the giant fake treasure chest? That's it right there. Kinda obvious once you think about it.");
-		npc
-				.addReply(
-						"library",
-						"Follow the path from here to the west; you'll see a building with two doors, and the emblem of a book and quill pen on display.");
-		npc
-				.addReply(
-						"tavern",
-						"Just head southeast along the path, and you can't miss it. It has a large sign that reads INN.");
-		npc
-				.addReply(
-						"temple",
-						"The temple is southeast from here, beside the #tavern. It has a cross on the roof, very distinctive.");
-		npc
-				.addReply(
-						"bakery",
-						"Our local bakery is just east of this square; they have a sign up with a picture of a loaf of bread on it.");
-		npc
-				.addReply(
-						"blacksmith",
-						"Head southwest to reach the smithy. There's a picture of an anvil hanging above the door, you should be able to spot it.");
-		npc
-				.addReply(
-						"village",
-						"Just keep heading southwest, past the #blacksmith, and you will shortly come to the old Semos village. Nishiya still sells sheep there.");
+		npc.addReply(
+			"bank",
+			"See this big building in front of me, with the giant fake treasure chest? That's it right there. Kinda obvious once you think about it.");
+
+		npc.addReply(
+			"library",
+			"Follow the path from here to the west; you'll see a building with two doors, and the emblem of a book and quill pen on display.");
+
+		npc.addReply(
+			"tavern",
+			"Just head southeast along the path, and you can't miss it. It has a large sign that reads INN.");
+
+		npc.addReply(
+			"temple",
+			"The temple is southeast from here, beside the #tavern. It has a cross on the roof, very distinctive.");
+
+		npc.addReply(
+			"bakery",
+			"Our local bakery is just east of this square; they have a sign up with a picture of a loaf of bread on it.");
+
+		npc.addReply(
+			"blacksmith",
+			"Head southwest to reach the smithy. There's a picture of an anvil hanging above the door, you should be able to spot it.");
+
+		npc.addReply(
+			"village",
+			"Just keep heading southwest, past the #blacksmith, and you will shortly come to the old Semos village. Nishiya still sells sheep there.");
 
 		/** Give the reward to the polite newcomer user */
 		// npc.add(ConversationStates.ATTENDING,
@@ -131,7 +137,7 @@ public class MeetMonogenes extends AbstractQuest {
 		// new SpeakerNPC.ChatAction() {
 		// @Override
 		// public void fire(Player player, Sentence sentence, SpeakerNPC engine) {
-		// if (player.getLevel() < 15) {
+		// if (player.getLevel() < 2) {
 		// engine.say("Goodbye! I hope I was of some use to you.");
 		// player.addXP(10);
 		// player.notifyWorldAboutChanges();
