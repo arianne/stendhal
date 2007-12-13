272f378a4f3e3628da0266926ed954a6e9ae72fd
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-12-13 21:46:22 +0000

    initial checkin of stub

---
+++ src/games/stendhal/server/StendhalRPObjectFactory.java
@@ -0,0 +1,31 @@
+package games.stendhal.server;
+
+import marauroa.common.game.RPClass;
+import marauroa.common.game.RPObject;
+import marauroa.server.game.rp.RPObjectFactory;
+
+import org.apache.log4j.Logger;
+
+/**
+ * creates concrete objects of Stendhal classes 
+ *
+ * @author hendrik
+ */
+public class StendhalRPObjectFactory extends RPObjectFactory {
+	private static Logger logger = Logger.getLogger(StendhalRPObjectFactory.class);
+
+	@Override
+	public RPObject transform(RPObject object) {
+		RPClass clazz = object.getRPClass();
+		if (clazz == null) {
+			logger.error("Cannot create concrete object for " + object + " because it does not have an RPClass.");
+			return super.transform(object);
+		}
+		String name = clazz.getName();
+		
+		// TODO: add factory here
+		
+		return super.transform(object);
+	}
+
+}
