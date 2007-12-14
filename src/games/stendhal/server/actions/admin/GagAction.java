41a1a2f9c43dafb86759826f1f932e41cfb91f23
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-12-14 10:52:24 +0000

    use wellknownconstants

--- src/games/stendhal/server/actions/admin/GagAction.java
+++ src/games/stendhal/server/actions/admin/GagAction.java
@@ -6,11 +6,12 @@ import games.stendhal.server.actions.CommandCenter;
 import games.stendhal.server.entity.player.Player;
 import marauroa.common.game.RPAction;
 
+import static games.stendhal.server.actions.WellKnownActionConstants.MINUTES;
+import static games.stendhal.server.actions.WellKnownActionConstants.TARGET;
 public class GagAction extends AdministrationAction {
 	private static final String USAGE_GAG_NAME_MINUTES_REASON = "Usage: /gag name minutes reason";
 	private static final String _REASON = "reason";
-	private static final String _MINUTES = "minutes";
-	private static final String _TARGET = "target";
+
 	private static final String _GAG = "gag";
 
 	public static void register(){
@@ -21,14 +22,14 @@ public class GagAction extends AdministrationAction {
 	@Override
 	public void perform(Player player, RPAction action) {
 	
-		if (action.has(_TARGET) && action.has(_MINUTES)) {
-			String target = action.get(_TARGET);
+		if (action.has(TARGET) && action.has(MINUTES)) {
+			String target = action.get(TARGET);
 			String reason = "";
 			if (action.has(_REASON)) {
 				reason = action.get(_REASON);
 			}
 			try {
-				int minutes = action.getInt(_MINUTES);
+				int minutes = action.getInt(MINUTES);
 				StendhalRPRuleProcessor.get().addGameEvent(player.getName(),
 						_GAG, target, Integer.toString(minutes), reason);
 				GagManager.get().gag(target, player, minutes, reason);
