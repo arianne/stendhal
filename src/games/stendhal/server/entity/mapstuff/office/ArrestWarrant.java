bf4aa89e2b81a96d967054d5dcb169762869eb05
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-12-14 15:04:22 +0000

    new method getCriminal() and javadoc

--- src/games/stendhal/server/entity/mapstuff/office/ArrestWarrant.java
+++ src/games/stendhal/server/entity/mapstuff/office/ArrestWarrant.java
@@ -7,6 +7,11 @@ import marauroa.common.game.RPClass;
 import marauroa.common.game.RPObject;
 import marauroa.common.game.Definition.Type;
 
+/**
+ * are persitent arrest warrant
+ *
+ * @author hendrik
+ */
 public class ArrestWarrant extends Entity {
 	private static final String CRIMINAL = "criminal";
 	private static final String POLICE_OFFICER = "police_officer";
@@ -25,7 +30,15 @@ public class ArrestWarrant extends Entity {
 		clazz.addAttribute(TIMESTAMP, Type.FLOAT, Definition.HIDDEN);
 		clazz.addAttribute(STARTED, Type.FLAG, Definition.HIDDEN);
 	}
-	
+
+	/**
+	 * Creates an ArrestWarrant
+	 *
+	 * @param criminalName  name of criminal to be jailed
+	 * @param policeOfficer name of police officer who issued the /jail command
+	 * @param minutes time of sentence
+	 * @param reason reason
+	 */
 	public ArrestWarrant(String criminalName, Player policeOfficer, int minutes, String reason) {
 		store();
 		put(CRIMINAL, criminalName);
@@ -34,9 +47,24 @@ public class ArrestWarrant extends Entity {
 		put(REASON, reason);
 		put(TIMESTAMP, System.currentTimeMillis());
 	}
-	
+
+	/**
+	 * creates an ArrestWarrant based on a deserialized RPObject; 
+	 * use the other constructor.
+	 *
+	 * @param rpobject RPObject
+	 */
 	public ArrestWarrant(RPObject rpobject) {
 		super(rpobject);
 		store();
+	}
+
+	/**
+	 * gets the name of the criminal
+	 *
+	 * @return name of criminal
+	 */
+	public String getCriminal() {
+		return get(CRIMINAL);
 	}
 }
