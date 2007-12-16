4dbd6922527f9d484d8da19a5680a233b575df56
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-12-16 09:20:52 +0000

    adjusted to newer messages

--- tests/games/stendhal/server/actions/AdministrationActionTest.java
+++ tests/games/stendhal/server/actions/AdministrationActionTest.java
@@ -563,8 +563,7 @@ public class AdministrationActionTest {
 		action.put("minutes", 1);
 
 		assertTrue(CommandCenter.execute(pl, action));
-		assertEquals("this is ok as jailing is tested elsewhere",
-				"Zone -1_semos_jail not found", pl.getPrivateText());
+		assertTrue(pl.getPrivateText().startsWith("You have jailed hugo for 1 minutes. Reason: whynot."));
 	}
 
 	@Test
