3c693a41e8ed8c4d709c9cf8d6304aa395a3eb28
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-12-21 19:57:28 +0000

    use new ExamineEvent class

--- src/games/stendhal/server/entity/npc/action/ExamineChatAction.java
+++ src/games/stendhal/server/entity/npc/action/ExamineChatAction.java
@@ -1,13 +1,13 @@
 package games.stendhal.server.entity.npc.action;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
-
-import marauroa.common.game.RPEvent;
 import games.stendhal.server.entity.npc.Sentence;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.SpeakerNPC.ChatAction;
 import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.events.ExamineEvent;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 
 public class ExamineChatAction extends ChatAction {
 	private String image;
@@ -22,11 +22,7 @@ public class ExamineChatAction extends ChatAction {
 
 	@Override
 	public void fire(Player player, Sentence sentence, SpeakerNPC npc) {
-		RPEvent event = new RPEvent("examine");
-		event.put("path", "/data/sprites/examine/" + image);
-		event.put("title", title);
-		event.put("alt", alt);
-		player.addEvent(event);
+		player.addEvent(new ExamineEvent(image, title, alt));
 	}
 
 	@Override
