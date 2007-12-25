30d4686e288e40d9855295de061c9b0e43fb9ff5
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-12-25 22:39:59 +0000

    created stub for logging method, improved documenation

--- src/games/stendhal/server/core/engine/ItemLogger.java
+++ src/games/stendhal/server/core/engine/ItemLogger.java
@@ -1,16 +1,34 @@
 package games.stendhal.server.core.engine;
 
+import games.stendhal.server.entity.item.Item;
+import games.stendhal.server.entity.player.Player;
+
+import java.sql.SQLException;
+
+import marauroa.server.game.db.JDBCDatabase;
+import marauroa.server.game.db.Transaction;
+
+import org.apache.log4j.Logger;
+
 /**
  * Item Logger
  *
  * @author hendrik
  */
 public class ItemLogger {
+
+	private static Logger logger = Logger.getLogger(ItemLogger.class);
+
 	/*
+	CREATE TABLE itemid (
+	  last_id INTEGER
+	);
+	
 	CREATE TABLE itemlog (
 	  id         SERIAL,
 	  itemid     INTEGER,
-	  player_id  INTEGER,
+	  timedate   TIMESTAMP,
+	  source     VARCHAR(20),
 	  event,     VARCHAR(20),
 	  param1     VARCHAR(50),
 	  param2     VARCHAR(50),
@@ -20,7 +38,7 @@ public class ItemLogger {
 	
 	
 	
-	create             name         quantity          quest-name / killed creature / summon zone x y / summonat target target-slot quantity
+	create             name         quantity          quest-name / killed creature / summon zone x y / summonat target target-slot quantity / olditem
 	slot-to-slot       source       source-slot       target    target-slot
 	ground-to-slot     zone         x         y       target    target-slot
 	slot-to-ground     source       source-slot       zone         x         y
@@ -36,5 +54,31 @@ public class ItemLogger {
 	the last two are redundant pairs to simplify queries
 	 */
 
-	
+	private static void log(Item item, Player player, String event, String param1, String param2, String param3, String param4) {
+		Transaction transaction = JDBCDatabase.getDatabase().getTransaction();
+		try {
+
+			assignIDIfNotPresent(transaction, item);
+			log(transaction, item, player, event, param1, param2, param3, param4);
+
+			transaction.commit();
+		} catch (SQLException e) {
+			logger.error(e, e);
+			try {
+				transaction.rollback();
+			} catch (SQLException e1) {
+				logger.error(e1, e1);
+			}
+		}
+	}
+
+	private static void assignIDIfNotPresent(Transaction transaction, Item item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void log(Transaction transaction, Item item, Player player, String event, String param1, String param2, String param3, String param4) {
+		// TODO Auto-generated method stub
+		
+	}
 }
