0eba6e4d7ba4a922b4d504f595a92cd979fdd52b
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-12-26 01:51:44 +0000

    added comment about not using equals

--- src/games/stendhal/server/entity/slot/PersonalChestSlot.java
+++ src/games/stendhal/server/entity/slot/PersonalChestSlot.java
@@ -21,6 +21,11 @@ public class PersonalChestSlot extends LootableSlot {
 
 	@Override
     public boolean isReachableForTakingThingsOutOfBy(Entity entity) {
+		// Yes, this comparison of references is by design: Two player objects
+		// are equal if they are for the same character but could be from two 
+		// different session. Marauroa is supposted to prevent two session
+		// for the same character being active at the same time, but we should
+		// not depend on this as the banks have had lots of bugs in the past.
 	    if (chest.getAttending() != entity) {
 	    	logger.error(entity + " tried to take stuff out of the bank chest " + chest + " which is currently attenting " + chest.getAttending());
 	    	return false;
