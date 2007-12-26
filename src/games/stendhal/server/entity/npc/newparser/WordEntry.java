55c3da5d566eaef583f72cd5cc9e1107b0cf8e24
Martin Fuchs <martinfuchs@users.sourceforge.net>
2007-12-26 16:39:18 +0000

    add word categorization functions

--- src/games/stendhal/server/entity/npc/newparser/WordEntry.java
+++ src/games/stendhal/server/entity/npc/newparser/WordEntry.java
@@ -10,7 +10,7 @@ import java.io.PrintWriter;
  */
 public class WordEntry {
 	public String	word;	/** word */
-	public String	type;	/** word type, e.g. VER, ADJ, PLU, PLU-ANI, ... */
+	public String	type;	/** word type, e.g. VER, ADJ, NOU, NOU-ANI, ... */
 	public String	plural;	/** pluralised word (or singular for entries of type ...-PLU */
 	public Integer	value;	/** numeric value for words of type NUM */
 
@@ -28,5 +28,45 @@ public class WordEntry {
 	    if (value != null) {
 	    	pw.printf("\t%d", value);
 	    }
+    }
+
+	/**
+	 * determine is the word is a verb
+	 * @return
+	 */
+	public boolean isVerb() {
+	    return type.startsWith("VER");
+    } 
+
+	/**
+	 * determine is the word is a noun
+	 * @return
+	 */
+	public boolean isNoun() {
+	    return type.startsWith("NOU");
+    } 
+
+	/**
+	 * determine is the word is a numeral
+	 * @return
+	 */
+	public boolean isNumeral() {
+	    return type.startsWith("NUM");
+    } 
+
+	/**
+	 * determine is the word is a preposition
+	 * @return
+	 */
+	public boolean isPreposition() {
+	    return type.startsWith("PRE");
+    } 
+
+	/**
+	 * determine is the word is in plural form
+	 * @return
+	 */
+	public boolean isPlural() {
+	    return type.endsWith("-PLU");
     } 
 }
