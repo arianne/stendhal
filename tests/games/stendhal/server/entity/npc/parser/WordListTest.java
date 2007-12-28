24999beddf7d6383e568ae432fbb32c3a2b087ad
Martin Fuchs <martinfuchs@users.sourceforge.net>
2007-12-28 10:31:14 +0000

    use word type constants in word list test

--- tests/games/stendhal/server/entity/npc/parser/WordListTest.java
+++ tests/games/stendhal/server/entity/npc/parser/WordListTest.java
@@ -3,8 +3,6 @@ package games.stendhal.server.entity.npc.parser;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
-import games.stendhal.server.entity.npc.parser.WordEntry;
-import games.stendhal.server.entity.npc.parser.WordList;
 
 import org.junit.Test;
 
@@ -22,37 +20,37 @@ public class WordListTest {
 		WordEntry w = wl.find("house");
 		assertNotNull(w);
 		assertTrue(w.getType().isObject());
-		assertEquals("OBJ", w.getType().getTypeString());
+		assertEquals(WordType.OBJECT, w.getType().getTypeString());
 		assertEquals("houses", w.getPlurSing());
 
 		w = wl.find("man");
 		assertNotNull(w);
 		assertTrue(w.getType().isSubject());
-		assertEquals("SUB", w.getType().getTypeString());
+		assertEquals(WordType.SUBJECT, w.getType().getTypeString());
 		assertEquals("men", w.getPlurSing());
 
 		w = wl.find("carrot");
 		assertNotNull(w);
 		assertTrue(w.getType().isObject());
-		assertEquals("OBJ-FOO", w.getType().getTypeString());
+		assertEquals(WordType.OBJECT+WordType.SUFFIX_FOOD, w.getType().getTypeString());
 		assertEquals("carrots", w.getPlurSing());
 
 		w = wl.find("carrots");
 		assertNotNull(w);
 		assertTrue(w.getType().isObject());
-		assertEquals("OBJ-FOO-PLU", w.getType().getTypeString());
+		assertEquals(WordType.OBJECT+WordType.SUFFIX_FOOD+WordType.SUFFIX_PLURAL, w.getType().getTypeString());
 		assertEquals("carrot", w.getPlurSing());
 
 		w = wl.find("water");
 		assertNotNull(w);
 		assertTrue(w.getType().isObject());
-		assertEquals("OBJ-FLU", w.getType().getTypeString());
+		assertEquals(WordType.OBJECT+WordType.SUFFIX_FLUID, w.getType().getTypeString());
 		assertEquals("waters", w.getPlurSing());
 
 		w = wl.find("she");
 		assertNotNull(w);
 		assertTrue(w.getType().isSubject());
-		assertEquals("SUB", w.getType().getTypeString());
+		assertEquals(WordType.SUBJECT, w.getType().getTypeString());
 		assertEquals("they", w.getPlurSing());
 	}
 
@@ -63,17 +61,17 @@ public class WordListTest {
 		WordEntry w = wl.find("say");
 		assertNotNull(w);
 		assertTrue(w.getType().isVerb());
-		assertEquals("VER", w.getType().getTypeString());
+		assertEquals(WordType.VERB, w.getType().getTypeString());
 
 		w = wl.find("open");
 		assertNotNull(w);
 		assertTrue(w.getType().isVerb());
-		assertEquals("VER", w.getType().getTypeString());
+		assertEquals(WordType.VERB, w.getType().getTypeString());
 
 		w = wl.find("are");
 		assertNotNull(w);
 		assertTrue(w.getType().isVerb());
-		assertEquals("VER-PLU", w.getType().getTypeString());
+		assertEquals(WordType.VERB+WordType.SUFFIX_PLURAL, w.getType().getTypeString());
 	}
 
 	@Test
@@ -83,12 +81,12 @@ public class WordListTest {
 		WordEntry w = wl.find("white");
 		assertNotNull(w);
 		assertTrue(w.getType().isAdjective());
-		assertEquals("ADJ-COL", w.getType().getTypeString());
+		assertEquals(WordType.ADJECTIVE+WordType.SUFFIX_COLOR, w.getType().getTypeString());
 
 		w = wl.find("silvery");
 		assertNotNull(w);
 		assertTrue(w.getType().isAdjective());
-		assertEquals("ADJ-COL", w.getType().getTypeString());
+		assertEquals(WordType.ADJECTIVE+WordType.SUFFIX_COLOR, w.getType().getTypeString());
 	}
 
 	@Test
@@ -98,12 +96,12 @@ public class WordListTest {
 		WordEntry w = wl.find("with");
 		assertNotNull(w);
 		assertTrue(w.getType().isPreposition());
-		assertEquals("PRE", w.getType().getTypeString());
+		assertEquals(WordType.PREPOSITION, w.getType().getTypeString());
 
 		w = wl.find("on");
 		assertNotNull(w);
 		assertTrue(w.getType().isPreposition());
-		assertEquals("PRE", w.getType().getTypeString());
+		assertEquals(WordType.PREPOSITION, w.getType().getTypeString());
 	}
 
 	@Test
