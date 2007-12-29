31d258318d8a495acc57a5767cde34cf1c42adca
Astrid Sawatzky <astridemma@users.sourceforge.net>
2007-12-29 10:02:01 +0000

    initial commit

---
+++ tests/games/stendhal/client/MockStendhalClient.java
@@ -0,0 +1,14 @@
+package games.stendhal.client;
+
+
+
+public class MockStendhalClient extends StendhalClient {
+
+	protected MockStendhalClient(String loggingProperties) {
+		super(loggingProperties);
+		client = this;
+		
+	}
+	
+
+}
