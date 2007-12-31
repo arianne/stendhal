70e5f5950173f48a286b39eb7a70b99ae9d0826b
Hendrik Brummermann <nhnb@users.sourceforge.net>
2007-12-31 13:15:04 +0000

    implemented iterator.next

---
+++ src/games/stendhal/tools/itemlog/LogIterator.java
@@ -0,0 +1,50 @@
+package games.stendhal.tools.itemlog;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Iterator;
+
+import org.apache.log4j.Logger;
+
+/**
+ * iterates over the log entries returned by a database query
+ *
+ * @author hendrik
+ */
+public class LogIterator implements Iterator {
+	private static Logger logger = Logger.getLogger(LogIterator.class);
+	
+	private ResultSet resultSet;
+	private boolean hasNext;
+	private boolean nextCalled;
+
+	public boolean hasNext() {
+		if (nextCalled) {
+			return hasNext;
+		}
+		nextCalled = true;
+		try {
+	        hasNext = resultSet.next();
+        } catch (SQLException e) {
+        	hasNext = false;
+        	logger.error(e, e);
+        }
+		return hasNext;
+	}
+
+	public Object next() {
+		try {
+	        hasNext = resultSet.next();
+        } catch (SQLException e) {
+        	hasNext = false;
+        	logger.error(e, e);
+        }
+		nextCalled = false;
+		return null;
+	}
+
+	public void remove() {
+		// TODO Auto-generated method stub
+
+	}
+}
