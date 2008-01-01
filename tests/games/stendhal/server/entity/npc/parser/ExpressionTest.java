0daa1d69bac153254e2b3c6bab1ef9ad7cada066
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-01-01 15:32:11 +0000

    add test class for npc.parser.Expression

---
+++ tests/games/stendhal/server/entity/npc/parser/ExpressionTest.java
@@ -0,0 +1,33 @@
+package games.stendhal.server.entity.npc.parser;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Test the NPC conversation parser Expression class.
+ *
+ * @author Martin Fuchs
+ */
+public class ExpressionTest {
+
+	@Test
+	public final void testMatch() {
+		Expression expr1 = ConversationParser.createTriggerExpression("cloak");
+		Expression expr2 = ConversationParser.createTriggerExpression("cloaks");
+		Expression expr3 = ConversationParser.createTriggerExpression("trousers");
+
+		assertTrue(expr1.matches(expr1));
+		assertFalse(expr1.matches(expr2));
+		assertFalse(expr1.matches(expr3));
+
+		assertTrue(expr1.matchesNormalized(expr2));
+		assertFalse(expr1.matchesNormalized(expr3));
+
+		assertTrue(expr1.matchesNormalizedBeginning(expr2));
+		assertTrue(expr2.matchesNormalizedBeginning(expr1));
+		assertFalse(expr1.matchesNormalizedBeginning(expr3));
+	}
+
+}
