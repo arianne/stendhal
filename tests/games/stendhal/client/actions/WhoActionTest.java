731fe6d824cae4f2465142ead1f9b0b5fc914c68
Astrid Sawatzky <astridemma@users.sourceforge.net>
2008-01-01 17:28:12 +0000

    initial commit

---
+++ tests/games/stendhal/client/actions/WhoActionTest.java
@@ -0,0 +1,48 @@
+package games.stendhal.client.actions;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import games.stendhal.client.MockStendhalClient;
+
+import marauroa.common.game.RPAction;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class WhoActionTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void testExecute() {
+		new MockStendhalClient("") {
+			@Override
+			public void send(RPAction action) {
+				client = null;
+				for (String attrib : action) {
+					assertEquals("type", attrib);
+					assertEquals("who", (action.get(attrib)));
+				}
+
+			}
+		};
+		WhoAction action = new WhoAction();
+		assertTrue(action.execute(null, null));
+
+	}
+
+	@Test
+	public void testGetMaximumParameters() {
+		WhoAction action = new WhoAction();
+		assertThat(action.getMaximumParameters(), is(0));
+	}
+
+	@Test
+	public void testGetMinimumParameters() {
+		WhoAction action = new WhoAction();
+		assertThat(action.getMinimumParameters(), is(0));
+	}
+
+}
