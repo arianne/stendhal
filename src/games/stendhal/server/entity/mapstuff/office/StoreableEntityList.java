e0c0fea04559a0dda5ab59e5a336c69bcfe91beb
Hendrik Brummermann <nhnb@users.sourceforge.net>
2008-01-02 01:56:50 +0000

    added entities can now automatically be place in a free spot on the world

--- src/games/stendhal/server/entity/mapstuff/office/StoreableEntityList.java
+++ src/games/stendhal/server/entity/mapstuff/office/StoreableEntityList.java
@@ -3,6 +3,8 @@ package games.stendhal.server.entity.mapstuff.office;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.entity.Entity;
 
+import java.awt.Rectangle;
+import java.awt.Shape;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -18,6 +20,7 @@ import marauroa.common.game.RPObject;
 abstract class StoreableEntityList<T extends Entity> {
 	private StendhalRPZone zone;
 	private Class<T> clazz;
+	private Shape shape;
 
 	/**
 	 * creates a new StoreableEntityList
@@ -28,19 +31,57 @@ abstract class StoreableEntityList<T extends Entity> {
 	// the class object is needed, because generic type variables (T)
 	// cannot be used in instanceof.
 	public StoreableEntityList(StendhalRPZone zone, Class<T> clazz) {
-		super();
 		this.zone = zone;
 		this.clazz = clazz;
 	}
+
+	public StoreableEntityList(StendhalRPZone zone, Shape shape, Class<T> clazz) {
+	    this(zone, clazz);
+	    this.shape = shape;
+    }
 
 	/**
      * Adds a storeable entity
      * 
      * @param entity storeable entity
+     * @return true in case the entity was added successfully; 
+     * 				false in case no free spot for it was found
      */
-    public void add(T entity) {
-    	zone.add(entity);
-    	zone.storeToDatabase();
+	public boolean add(T entity) {
+		boolean success = calculatePosition(entity);
+		if (!success) {
+			return false;
+		}
+		zone.add(entity);
+		zone.storeToDatabase();
+		return true;
+	}
+
+	/**
+	 * calculates a free spot to place this entity into.
+	 *
+	 * @param entity entity
+	 * @return true, in case a spot was found or this entity should 
+	 * 				not be place in the zone; false otherwise
+	 */
+	private boolean calculatePosition(T entity) {
+		if (shape == null) {
+			return true;
+		}
+
+		Rectangle rect = shape.getBounds();
+		for (int x = rect.x; x < rect.x + rect.width; x++) {
+			for (int y = rect.y; y < rect.y + rect.height; y++) {
+				if (shape.contains(x, y)) {
+					if (!zone.collides(entity, x, y)) {
+						entity.setPosition(x, y);
+						return true;
+					}
+				}
+			}
+		}
+
+		return false;
     }
 
 	/**
