5315e1838bf97e19041d5177f1ae2d181a01d9cf
Hendrik Brummermann <nhnb@users.sourceforge.net>
2008-01-05 18:08:08 +0000

    splitted code into multiple methods

--- src/games/stendhal/server/actions/chat/TellAction.java
+++ src/games/stendhal/server/actions/chat/TellAction.java
@@ -19,143 +19,165 @@ import marauroa.common.game.RPObject;
  * handles /tell-action (/msg-action). 
  */
 public class TellAction implements ActionListener {
+	private String text;
+	private String senderName;
+	private String receiverName;
+	private Player sender;
+	private Player receiver;
+	
+	private void init(Player player, RPAction action) {
+		text = action.get(TEXT).trim();
+		senderName = player.getName();
+		receiverName = action.get(TARGET);
+		sender = player;
+		receiver = StendhalRPRuleProcessor.get().getPlayer(receiverName);
+	}
 
-	// TODO: split this method into small parts
+	private boolean validateAction(RPAction action) {
+		return action.has(TARGET) && action.has(TEXT);
+	}
+
+	private boolean checkOnline() {
+		if ((receiver == null)
+				|| (receiver.isGhost() && (sender.getAdminLevel() < AdministrationAction.getLevelForCommand("ghostmode")))) {
+			sender.sendPrivateText("No player named \"" + receiverName + "\" is currently active.");
+			return false;
+		}
+		return true;
+	}
+
+	private boolean checkIgnoreList(Player player) {
+		// check ignore list
+		String reply = receiver.getIgnore(senderName);
+		if (reply != null) {
+			// sender is on ignore list
+			if (reply.length() == 0) {
+				tellIgnorePostman(player, Grammar.suffix_s(receiverName)
+					+ " mind is not attuned to yours, so you cannot reach them.");
+			} else {
+				tellIgnorePostman(player, receiverName + " is ignoring you: " + reply);
+			}
+			return false;
+		}
+		return true;
+	}
+
+	private String createFullMessageText() {
+		if (senderName.equals(receiverName)) {
+			return "You mutter to yourself: " + text;
+		} else {
+			return senderName + " tells you: " + text;
+		}
+	}
+
+	private void extractRealSenderFromMessageInCaseItWasSendViaPostman() {
+		// HACK: extract sender from postman messages
+		StringTokenizer st = new StringTokenizer(text, " ");
+		if (senderName.equals("postman") && (st.countTokens() > 2)) {
+			String temp = st.nextToken();
+			String command = st.nextToken();
+			if (command.equals("asked")) {
+				senderName = temp;
+			}
+		}
+	}
+
+	private void tellAboutAwayStatusIfNeccessary() {
+		// Handle /away messages
+		String away = receiver.getAwayMessage();
+		if (away != null) {
+			if (receiver.isAwayNotifyNeeded(senderName)) {
+				// Send away response
+				tellIgnorePostman(sender, receiverName + " is away: " + away);
+			}
+		}
+	}
+
+	private void tellIgnorePostman(Player receiver, String message) {
+		if (!receiver.getName().equals("postman")) {
+			receiver.sendPrivateText(message);
+		}
+	}
+
 	public void onAction(Player player, RPAction action) {
 		if (GagManager.checkIsGaggedAndInformPlayer(player)) {
 			return;
 		}
 
-		String away;
-		String reply;
-		String grumpy;
-
-		// TODO: find a cleaner way to implement it
 		if (Jail.isInJail(player)) {
 			player.sendPrivateText("The strong anti telepathy aura prevents you from getting through. Use /support <text> to contact an admin!");
 			return;
 		}
 
-		if (action.has(TARGET) && action.has(TEXT)) {
-			String text = action.get(TEXT).trim();
-			String message;
-
-			// find the target player
-			String senderName = player.getTitle();
-			String receiverName = action.get(TARGET);
-
-			Player receiver = StendhalRPRuleProcessor.get().getPlayer(
-					receiverName);
-			/*
-			 * If the receiver is not logged or if it is a ghost and you don't
-			 * have the level to see ghosts...
-			 */
-			if ((receiver == null)
-					|| (receiver.isGhost() && (player.getAdminLevel() < AdministrationAction.getLevelForCommand("ghostmode")))) {
-				player.sendPrivateText("No player named \""
-						+ action.get(TARGET) + "\" is currently active.");
-				player.notifyWorldAboutChanges();
-				return;
-			}
-
-			if (senderName.equals(receiverName)) {
-				message = "You mutter to yourself: " + text;
-			} else {
-				message = senderName + " tells you: " + text;
-			}
-
-			// HACK: extract sender from postman messages
-			StringTokenizer st = new StringTokenizer(text, " ");
-			if (senderName.equals("postman") && (st.countTokens() > 2)) {
-				String temp = st.nextToken();
-				String command = st.nextToken();
-				if (command.equals("asked")) {
-					senderName = temp;
-				}
-			}
-
-			// check ignore list
-			reply = receiver.getIgnore(senderName);
-			if (reply != null) {
-				// sender is on ignore list
-				// HACK: do not notify postman
-				if (!senderName.equals("postman")) {
-					if (reply.length() == 0) {
-						player.sendPrivateText(Grammar.suffix_s(receiverName)
-								+ " mind is not attuned to yours, so you cannot reach them.");
-					} else {
-						player.sendPrivateText(receiverName
-								+ " is ignoring you: " + reply);
-					}
-
-					player.notifyWorldAboutChanges();
-				}
-				return;
-			}
-
-			// check grumpiness
-			grumpy = receiver.getGrumpyMessage();
-			if (grumpy != null && receiver.getSlot("!buddy").size() > 0) {
-				RPObject buddies = receiver.getSlot("!buddy").iterator().next();
-				boolean senderFound = false;
-				for (String buddyName : buddies) {
-					// TODO: as in Player.java, remove '_' prefix if ID is made
-					// completely virtual
-					if (buddyName.charAt(0) == '_') {
-						buddyName = buddyName.substring(1);
-					}
-					if (buddyName.equals(senderName)) {
-						senderFound = true;
-						break;
-					}
-				}
-				if (!senderFound) {
-					// sender is not a buddy
-					// HACK: do not notify postman
-					if (!senderName.equals("postman")) {
-						if (grumpy.length() == 0) {
-							player.sendPrivateText(receiverName
-									+ " has a closed mind, and is seeking solitude from all but close friends");
-						} else {
-							player.sendPrivateText(receiverName
-									+ " is seeking solitude from all but close friends: "
-									+ grumpy);
-						}
-						player.notifyWorldAboutChanges();
-					}
-					return;
-				}
-			}
-			// transmit the message
-			receiver.sendPrivateText(message);
-
-			if (!senderName.equals(receiverName)) {
-				player.sendPrivateText("You tell " + receiverName + ": " + text);
-			}
-
-			/*
-			 * Handle /away messages
-			 */
-			away = receiver.getAwayMessage();
-			if (away != null) {
-				if (receiver.isAwayNotifyNeeded(senderName)) {
-					/*
-					 * Send away response
-					 */
-					player.sendPrivateText(receiverName + " is away: " + away);
-
-					player.notifyWorldAboutChanges();
-				}
-			}
-
-			receiver.setLastPrivateChatter(senderName);
-			StendhalRPRuleProcessor.get().addGameEvent(player.getName(), "chat",
-					receiverName, Integer.toString(text.length()),
-					text.substring(0, Math.min(text.length(), 1000)));
-			receiver.notifyWorldAboutChanges();
-			player.notifyWorldAboutChanges();
+		if (!validateAction(action)) {
 			return;
 		}
+
+		init(player, action);
+
+		/* If the receiver is not logged in or if it is a ghost 
+		 * and you don't have the level to see ghosts... */
+		if (!checkOnline()) {
+			return;
+		}
+
+		String message = createFullMessageText();
+		extractRealSenderFromMessageInCaseItWasSendViaPostman();
+
+		if (!checkIgnoreList(player)) {
+			return;
+		}
+
+		// check grumpiness
+		if (!checkGrumpy()) {
+			return;
+		}
+
+		// transmit the message
+		receiver.sendPrivateText(message);
+
+		if (!senderName.equals(receiverName)) {
+			player.sendPrivateText("You tell " + receiverName + ": " + text);
+		}
+
+		tellAboutAwayStatusIfNeccessary();
+
+		receiver.setLastPrivateChatter(senderName);
+		StendhalRPRuleProcessor.get().addGameEvent(player.getName(), "chat",
+			receiverName, Integer.toString(text.length()),
+			text.substring(0, Math.min(text.length(), 1000)));
+	}
+
+	private boolean checkGrumpy() {
+		String grumpy = receiver.getGrumpyMessage();
+		if (grumpy != null && receiver.getSlot("!buddy").size() > 0) {
+			RPObject buddies = receiver.getSlot("!buddy").iterator().next();
+			boolean senderFound = false;
+			for (String buddyName : buddies) {
+				// TODO: as in Player.java, remove '_' prefix if ID is made
+				// completely virtual
+				if (buddyName.charAt(0) == '_') {
+					buddyName = buddyName.substring(1);
+				}
+				if (buddyName.equals(senderName)) {
+					senderFound = true;
+					break;
+				}
+			}
+			if (!senderFound) {
+				// sender is not a buddy
+				if (grumpy.length() == 0) {
+					tellIgnorePostman(sender, 
+						receiverName + " has a closed mind, and is seeking solitude from all but close friends");
+				} else {
+					tellIgnorePostman(sender, 
+						receiverName+ " is seeking solitude from all but close friends: " + grumpy);
+				}
+				return false;
+			}
+		}
+
+		return true;
 	}
 
 }
