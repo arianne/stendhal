232721836ad33c63fa8a8e1547da24ab996dfe8f
Hendrik Brummermann <nhnb@users.sourceforge.net>
2008-01-11 23:20:17 +0000

    fixed sql statement, added debbug output

--- src/games/stendhal/tools/loganalyser/login/Analyser.java
+++ src/games/stendhal/tools/loganalyser/login/Analyser.java
@@ -1,17 +1,13 @@
 package games.stendhal.tools.loganalyser.login;
 
 import games.stendhal.server.core.engine.StendhalPlayerDatabase;
-import games.stendhal.tools.loganalyser.itemlog.consistency.LogEntryIterator;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Date;
-import java.util.List;
 
 import marauroa.common.Log4J;
-import marauroa.common.Pair;
 import marauroa.server.game.db.StringChecker;
 import marauroa.server.game.db.Transaction;
 
@@ -22,21 +18,25 @@ import marauroa.server.game.db.Transaction;
  */
 public class Analyser {
 
-	private static final String SQL = "SELECT timedate, username, address "
+	private static final String SQL = "SELECT username, address, loginEvent.timedate As timedate"
 		+ " FROM loginEvent, account "
 		+ " WHERE account.id = loginEvent.player_id"
-		+ " AND loginEvent.address = '%0$s' AND loginEvent.timedate >= '%1$s'"
+		+ " AND loginEvent.address = '%1$s' AND loginEvent.timedate >= '%2$s'"
 		+ " ORDER BY loginEvent.timedate";
 
 	private void analyse(String address, String timedate) throws SQLException {
 		LoginEventIterator iterator = readLoginsFromAddress(address, timedate);
+		for (LoginEvent event : iterator) {
+			System.out.println(event);
+		}
 	}
 
 	private LoginEventIterator readLoginsFromAddress(String address, String timedate) throws SQLException {
 		Transaction transaction =  StendhalPlayerDatabase.getDatabase().getTransaction();
 		Connection connection = transaction.getConnection();
 		Statement stmt = connection.createStatement();
-		ResultSet resultSet = stmt.executeQuery(String.format(SQL, StringChecker.escapeSQLString(address), StringChecker.escapeSQLString(timedate)));
+		String select = String.format(SQL, StringChecker.escapeSQLString(address), StringChecker.escapeSQLString(timedate));
+		ResultSet resultSet = stmt.executeQuery(select);
 		return new LoginEventIterator(stmt, resultSet);
 	}
 
