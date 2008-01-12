021f143f3e030a74df40417711005dd38f9232da
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-01-12 20:55:01 +0000

    indent /help message by inserting TAB characters

--- src/games/stendhal/client/actions/HelpAction.java
+++ src/games/stendhal/client/actions/HelpAction.java
@@ -23,7 +23,7 @@ class HelpAction implements SlashAction {
 				"For a detailed reference, visit #http://arianne.sourceforge.net/wiki/index.php/StendhalManual",
 				"Here are the most-used commands:",
 				"- /tell <player> <message> \tSends a private message to <player>",
-				"- /answer <message> \t\tSends a private message to the last player who sent a message to you",
+				"- /answer <message> \tSends a private message to the last player who sent a message to you",
 				"- // <message> \t\tSends a private message to the last player you sent a message to",
 				"- /support <message> \tAsk an administrator for help.",
 				"- /who \t\tList all players currently online",
@@ -34,10 +34,10 @@ class HelpAction implements SlashAction {
 				"- /ignore <player> [<minutes>|*|- [<reason...>]] \tAdd <player> to your ignore list",
 				"- /unignore <player> \tRemove <player> from your ignore list",
 				"- /away <message> \tSet an away message",
-				"- /away  \tRemove status away",
-				"- /grumpy <message> \t Sets a message to ignore all non-buddies.",
-				"- /grumpy  \tRemove status grumpy",
-				"- /quit \t\tLeave the game. You will continue where you left off upon your return",
+				"- /away \t\tRemove status away",
+				"- /grumpy <message> \tSets a message to ignore all non-buddies.",
+				"- /grumpy \t\tRemove status grumpy",
+				"- /quit \t\t\tLeave the game. You will continue where you left off upon your return",
 				"- /sound mute <on|off> \tMute or unmute the sounds" };
 
 		for (String line : lines) {
