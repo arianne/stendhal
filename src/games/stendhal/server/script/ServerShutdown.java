1066911daf74f479a973cae6e275e9a35b577ab6
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-01-13 09:06:26 +0000

    add comment about not needing to call arauroad.finish() explicitly

--- src/games/stendhal/server/script/ServerShutdown.java
+++ src/games/stendhal/server/script/ServerShutdown.java
@@ -6,8 +6,6 @@ import games.stendhal.server.entity.player.Player;
 
 import java.util.List;
 
-//import marauroa.server.marauroad;
-
 /**
  * Shuts down the server in a regular fashion. You should warn connected players
  * to logout if that is still possible.
@@ -29,6 +27,8 @@ public class ServerShutdown extends ScriptImpl {
 		new Thread(
 			new Runnable() {
     			public void run() {
+    				//marauroad.finish() is already called using a JRE shutdown hook, so we don't need
+    				// to call it here directly: 
     				//marauroad.getMarauroa().finish();
 
     				System.exit(0);
