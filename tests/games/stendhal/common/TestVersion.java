2ff9b18270ee36c5c425ad6882b00e357124a16c
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-01-15 22:57:42 +0000

    eliminate compiler warnings

--- tests/games/stendhal/common/TestVersion.java
+++ tests/games/stendhal/common/TestVersion.java
@@ -1,12 +1,11 @@
 package games.stendhal.common;
 
-import org.junit.Test;
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import games.stendhal.client.update.Version;
 
-
-import static org.junit.Assert.*;
-import static org.hamcrest.Matcher.*;
+import org.junit.Test;
 
 public class TestVersion {
 
