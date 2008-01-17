b468dcbcab12b228ffa623c1a7b7a68ec3b66641
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-01-17 21:01:40 +0000

    add new options to ConversationParserContext

--- src/games/stendhal/server/entity/npc/parser/ConversationContext.java
+++ src/games/stendhal/server/entity/npc/parser/ConversationContext.java
@@ -13,14 +13,15 @@ public class ConversationContext {
 	static final int CCS_WAIT_FOR_YES_NO = 1;	// wait for a yes/no answer
 	static final int CCS_WAIT_FOR_OBJECT = 2;	// wait for a named object
 
-	private int state = CCS_NONE;			// current state
+	private int state = CCS_NONE;				// current state
 
-	private boolean forMatching = false;	// flag for sentences to be used for matching
+	private boolean forMatching = false;		// flag for sentences to be used for matching
+	private boolean mergeExpressions = true;	// flag to enable Expression merging
+	private boolean persistNewWords = true;		// flag to enable storing new words into the database
 
 	public void setState(int state) {
 		this.state = state;
 	}
-
 	public int getState() {
 		return state;
 	}
@@ -28,9 +29,22 @@ public class ConversationContext {
 	public void setForMatching(boolean forMatching) {
 	    this.forMatching = forMatching;
     }
-
 	public boolean isForMatching() {
 	    return forMatching;
+    }
+
+	public void setMergeExpressions(boolean mergeExpressions) {
+	    this.mergeExpressions = mergeExpressions;
+    }
+	public boolean getMergeExpressions() {
+	    return mergeExpressions;
+    }
+
+	public void setPersistNewWords(boolean persistNewWords) {
+	    this.persistNewWords = persistNewWords;
+    }
+	public boolean getPersistNewWords() {
+	    return persistNewWords;
     }
 
 }
