8340f4b76271de79895a1c34320e0de9d9212d37
Astrid Sawatzky <astridemma@users.sourceforge.net>
2008-01-18 16:08:04 +0000

    initial commit can be added to getRootLogger, and collects all messages send there

---
+++ tests/utilities/Log4JAppender.java
@@ -0,0 +1,32 @@
+package utilities;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+
+public class Log4JAppender extends AppenderSkeleton {
+	private static List<String> messages = new ArrayList<String>();
+	public static final Log4JAppender instance = new Log4JAppender();
+
+	@Override
+	protected void append(LoggingEvent event) {
+		messages.add(event.getRenderedMessage());
+	}
+
+	public void close() {
+	}
+
+	public boolean requiresLayout() {
+		return false;
+	}
+
+	public static String[] getMessages() {
+		return (String[]) messages.toArray(new String[messages.size()]);
+	}
+
+	public static void clear() {
+		messages.clear();
+	}
+
+}
