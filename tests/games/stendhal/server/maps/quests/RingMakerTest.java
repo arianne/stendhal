04bf7e1778145eb0e3bfdaa5ee169adcd4f0f65a
Astrid Sawatzky <astridemma@users.sourceforge.net>
2008-01-18 12:21:40 +0000

    seems to be safer to use Defaultentitymanager for special items than using ItmTestHelper. made the different testCases into different tests.

--- tests/games/stendhal/server/maps/quests/RingMakerTest.java
+++ tests/games/stendhal/server/maps/quests/RingMakerTest.java
@@ -3,16 +3,18 @@ package games.stendhal.server.maps.quests;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.rule.defaultruleset.DefaultEntityManager;
 import games.stendhal.server.entity.item.Item;
+import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.NPCList;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.fsm.Engine;
 import games.stendhal.server.entity.player.Player;
-import games.stendhal.server.maps.MockStendhalRPRuleProcessor;
-import games.stendhal.server.maps.MockStendlRPWorld;
 import games.stendhal.server.maps.fado.weaponshop.RingSmithNPC;
 
+
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -23,35 +25,50 @@ import utilities.QuestHelper;
 public class RingMakerTest {
 
 	private static final String QUEST_SLOT = "fix_emerald_ring";
-	private static Player player = null;
-	private SpeakerNPC npc = null;
-	private Engine en = null;
-
+	
+	private static SpeakerNPC npc;
+	private static Engine en;
+	private Player player;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		QuestHelper.setUpBeforeClass();
 
 		StendhalRPZone zone = new StendhalRPZone("admin_test");
-		new RingSmithNPC().configureZone(zone, null);
+		RingSmithNPC ognir = new RingSmithNPC();
+		ognir.configureZone(zone, null);
 
 		AbstractQuest quest = new RingMaker();
 		quest.addToWorld();
-
-		player = PlayerTestHelper.createPlayer("player");
-
-		zone.add(player);
-		player.setPosition(52, 53);
-		MockStendhalRPRuleProcessor.get().addPlayer(player);
+		npc = NPCList.get().get("Ognir");
+		en = npc.getEngine();
 	}
 
 	@AfterClass
-	public static void afterClass() {
-		PlayerTestHelper.removePlayer(player);
+	public static void tearDownAftereClass() throws Exception {
+		 npc = null;
+		 en = null;
 	}
 
+	@Before
+	public void setUp() {
+		player = PlayerTestHelper.createPlayer("player");
+		en.setCurrentState(ConversationStates.IDLE);
+	}
+	
 	@Test
-	public void testOrderEmeraldRing() {
-
+	public void testHiandBye() throws Exception {
+		en.step(player, "hi");
+		assertEquals("Hi! Can I #help you?", npc.get("text"));
+		assertTrue(en.step(player, "bye"));
+		assertEquals("Bye, my friend.", npc.get("text"));	
+		
+	}
+	
+	
+	@Test
+	public void testOrderEmeraldRingWithoutEnoughmoney() {
+		
 		// **at ringsmith**
 		npc = NPCList.get().get("Ognir");
 		en = npc.getEngine();
@@ -63,7 +80,7 @@ public class RingMakerTest {
 		assertEquals("It is difficult to get the ring of life. Do a favour for a powerful elf in Nal'wor and you may receive one as a reward.", npc.get("text"));
 		assertTrue(en.step(player, "bye"));
 		// -----------------------------------------------
-		Item item = ItemTestHelper.createItem("emerald_ring");
+		Item item = DefaultEntityManager.getInstance().getItem("emerald_ring");
 		player.getSlot("bag").add(item);
 		assertTrue(en.step(player, "hi"));
 		assertEquals("Hi! Can I #help you?", npc.get("text"));
@@ -73,17 +90,27 @@ public class RingMakerTest {
 		assertEquals("I see you already have an emerald ring. If it gets broken, you can come to me to fix it.", npc.get("text"));
 
 	// break the ring don't give them money and make them lie that they have it
-		item.put("amount",0);
+		item.put("amount", 0);
 		assertTrue(en.step(player, "emerald"));
 		assertEquals("What a pity, your emerald ring is broken. I can fix it, for a #price.", npc.get("text"));
 		assertTrue(en.step(player, "price"));
 		assertEquals("The charge for my service is 80000 money, and I need 2 gold bars and 1 emerald to fix the ring. Do you want to pay now?", npc.get("text"));
 		assertTrue(en.step(player, "yes"));
-		assertEquals("Come back when you have both the money, the gem and the gold. Goodbye", npc.get("text"));
-		assertTrue(en.step(player, "bye"));
-
+		assertEquals("Come back when you have the money, the gem and the gold. Goodbye.", npc.get("text"));
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		
+	}
+	
+	@Test
+	public void testOrderEmeraldRingDeny() {
+		
 		// -----------------------------------------------
 		// this time say no they don't want to pay yet
+		npc = NPCList.get().get("Ognir");
+		en = npc.getEngine();
+		Item item = DefaultEntityManager.getInstance().getItem("emerald_ring");
+		item.put("amount", 0);
+		player.getSlot("bag").add(item);
 		assertTrue(en.step(player, "hi"));
 		assertEquals("Hi! Can I #help you?", npc.get("text"));
 		assertTrue(en.step(player, "help"));
@@ -94,15 +121,24 @@ public class RingMakerTest {
 		assertEquals("The charge for my service is 80000 money, and I need 2 gold bars and 1 emerald to fix the ring. Do you want to pay now?", npc.get("text"));
 		assertTrue(en.step(player, "no"));
 		assertEquals("No problem, just come back when you have the money, the emerald, and the gold.", npc.get("text"));
+		en.step(player, "bye");
+		assertEquals("Bye, my friend.", npc.get("text"));
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+	}
 
-		// -----------------------------------------------
-		// this time say yes and give them the stuff
-		item = ItemTestHelper.createItem("money", 80000);
+	@Test
+	public void testOrderEmeraldRing() {
+		
+		Item item = DefaultEntityManager.getInstance().getItem("emerald_ring");
+		item.put("amount", 0);
 		player.getSlot("bag").add(item);
-		item = ItemTestHelper.createItem("gold bar", 2);
-		player.getSlot("bag").add(item);
-		item = ItemTestHelper.createItem("emerald", 1);
-		player.getSlot("bag").add(item);
+		
+		Item money = ItemTestHelper.createItem("money", 80000);
+		player.getSlot("bag").add(money);
+		Item goldbar = ItemTestHelper.createItem("gold bar", 2);
+		player.getSlot("bag").add(goldbar);
+		Item emerald = ItemTestHelper.createItem("emerald", 1);
+		player.getSlot("bag").add(emerald);
 
 		assertTrue(en.step(player, "hi"));
 		assertEquals("Hi! Can I #help you?", npc.get("text"));
@@ -116,18 +152,13 @@ public class RingMakerTest {
 		assertEquals("Okay, that's all I need to fix the ring. Come back in 10 minutes and it will be ready. Bye for now.", npc.get("text"));
 
 		assertTrue(player.getQuest(QUEST_SLOT).startsWith("forging"));
-
-
-		// -----------------------------------------------
-
-
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
 	}
 
 	@Test
 	public void testFetchOrderedEmeraldRing() {
-		npc = NPCList.get().get("Ognir");
-		en = npc.getEngine();
-
+		
+		
 		player.setQuest("fix_emerald_ring", "forging;" + Long.MAX_VALUE);
 		
 		en.step(player, "hi");
@@ -147,9 +178,10 @@ public class RingMakerTest {
 		assertEquals("Hi! Can I #help you?", npc.get("text"));
 		en.step(player, "help");
 		assertEquals("I am an expert on #wedding rings and #emerald rings, sometimes called the ring of #life.", npc.get("text"));
+		int oldXP = player.getXP();
 		en.step(player, "emerald");
 		assertEquals("I'm pleased to say, your ring of life is fixed! It's good as new now.", npc.get("text"));
-		// player earns 500 experience points.
+		assertEquals("player earns 500 experience points.", oldXP + 500, player.getXP());
 		en.step(player, "bye");
 		assertEquals("Bye, my friend.", npc.get("text"));
 
