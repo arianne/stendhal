a64c058bce2124c6adfd1c516b28a7f31f061d86
Astrid Sawatzky <astridemma@users.sourceforge.net>
2008-01-18 13:31:37 +0000

    2 simple script to compile / run a single test classpath especially libs is for sure not complete yet the runtest script relys on tests being compiled by ant once

---
+++ comtest.sh
@@ -0,0 +1,3 @@
+javac -cp .:./:./src/:./tests/:./libs/junit-4.4.jar:./libs/marauroa.jar:./libs/log4j.jar:./libs/commons-lang.jar:./libs/mysql-connector-java-5.1.5-bin.jar:./libs/groovy.jar -d build/build_tests   ./tests/games/stendhal/server/maps/quests/RingMakerTest.java
+
+
