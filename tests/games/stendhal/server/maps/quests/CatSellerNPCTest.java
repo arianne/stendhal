8a433657249ee891f9d0742d1361e17b117f9212
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-01-19 10:16:36 +0000

    add test for CatSellerNPC

---
+++ tests/games/stendhal/server/maps/quests/CatSellerNPCTest.java
@@ -0,0 +1,147 @@
+package games.stendhal.server.maps.quests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import games.stendhal.server.core.engine.StendhalRPWorld;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.NPCList;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.fsm.Engine;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.maps.ados.felinashouse.CatSellerNPC;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import utilities.PlayerTestHelper;
+import utilities.QuestHelper;
+
+/**
+ * Test buying cats.
+ * @author Martin Fuchs
+ */
+public class CatSellerNPCTest {
+
+	private static final String ZONE_NAME = "int_ados_felinas_house";
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		QuestHelper.setUpBeforeClass();
+
+		StendhalRPZone zone = new StendhalRPZone(ZONE_NAME);
+		StendhalRPWorld world = StendhalRPWorld.get();
+		world.addRPZone(zone);
+
+		CatSellerNPC bar = new CatSellerNPC();
+		bar.configureZone(zone, null);
+
+		// load items to handle money
+		world.getRuleManager().getEntityManager();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		SpeakerNPC npc = NPCList.get().get("Felina");
+		if (npc != null) {
+			npc.setCurrentState(ConversationStates.IDLE);
+		}
+	}
+
+	@Test
+	public void testHiAndBye() {
+		Player player = PlayerTestHelper.createPlayer("player");
+
+		SpeakerNPC npc = NPCList.get().get("Felina");
+		assertNotNull(npc);
+		Engine en = npc.getEngine();
+
+		assertTrue(en.step(player, "hi Felina"));
+		assertEquals("Greetings! How may I help you?", npc.get("text"));
+
+		assertTrue(en.step(player, "bye"));
+		assertEquals("Bye.", npc.get("text"));
+	}
+
+	@Test
+	public void testBuyCat() {
+		Player player = PlayerTestHelper.createPlayer("player");
+
+		SpeakerNPC npc = NPCList.get().get("Felina");
+		assertNotNull(npc);
+		Engine en = npc.getEngine();
+
+		assertTrue(en.step(player, "hi"));
+		assertEquals("Greetings! How may I help you?", npc.get("text"));
+
+		assertTrue(en.step(player, "job"));
+		assertEquals("I sell cats. Well, really they are just little kittens when I sell them to you but if you #care for them well they grow into cats.", npc.get("text"));
+
+		assertTrue(en.step(player, "care"));
+		assertEquals("Cats love chicken and fish. Just place a piece on the ground and your cat will run over to eat it. You can right-click on her and choose 'Look' at any time, to check up on her weight; she will gain one unit of weight for every piece of chicken she eats.", npc.get("text"));
+
+		assertTrue(en.step(player, "buy"));
+		assertEquals("Please tell me what you want to buy.", npc.get("text"));
+
+		assertTrue(en.step(player, "buy dog"));
+		assertEquals("Sorry, I don't sell dogs.", npc.get("text"));
+
+		assertTrue(en.step(player, "buy cat"));
+		assertEquals("1 cat will cost 100. Do you want to buy it?", npc.get("text"));
+
+		assertTrue(en.step(player, "no"));
+		assertEquals("Ok, how else may I help you?", npc.get("text"));
+
+		assertTrue(en.step(player, "buy cat"));
+		assertEquals("1 cat will cost 100. Do you want to buy it?", npc.get("text"));
+
+		assertTrue(en.step(player, "yes"));
+		assertEquals("You don't seem to have enough money.", npc.get("text"));
+
+		assertTrue(en.step(player, "buy two cats"));
+		assertEquals("2 cats will cost 200. Do you want to buy them?", npc.get("text"));
+
+		assertTrue(en.step(player, "yes"));
+		assertEquals("Hmm... I just don't think you're cut out for taking care of more than one cat at once.", npc.get("text"));
+
+		// equip with enough money to buy the cat
+		assertTrue(PlayerTestHelper.equipWithMoney(player, 500));
+		assertTrue(en.step(player, "buy cat"));
+		assertEquals("1 cat will cost 100. Do you want to buy it?", npc.get("text"));
+
+		assertFalse(player.hasPet());
+
+		assertTrue(en.step(player, "yes"));
+		assertEquals("Here you go, a cute little kitten! Your kitten will eat any piece of chicken or fish you place on the ground. Enjoy her!", npc.get("text"));
+
+		assertTrue(player.hasPet());
+	}
+
+	@Test
+	public void testSellCat() {
+		Player player = PlayerTestHelper.createPlayer("player");
+
+		SpeakerNPC npc = NPCList.get().get("Felina");
+		assertNotNull(npc);
+		Engine en = npc.getEngine();
+
+		assertTrue(en.step(player, "hi"));
+		assertEquals("Greetings! How may I help you?", npc.get("text"));
+
+		assertTrue(en.step(player, "sell cat"));
+		assertEquals("Sell??? What kind of a monster are you? Why would you ever sell your beautiful cat?", npc.get("text"));
+	}
+}
