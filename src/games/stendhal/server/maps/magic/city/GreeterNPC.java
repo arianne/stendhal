9e51e8cad5a8e9bc8ff174771274dd2b2f1d5caa
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-01-19 14:49:25 +0000

    white space fix

--- src/games/stendhal/server/maps/magic/city/GreeterNPC.java
+++ src/games/stendhal/server/maps/magic/city/GreeterNPC.java
@@ -69,14 +69,15 @@ public class GreeterNPC implements ZoneConfigurator {
 				addReply("magic", "Indeed, enchantments such as our Sunlight Spell to keep the grass and flowers healthy down here. I suppose you are wondering why you have seen traditional enemies such as dark elves and green elves in company together here, let me #explain.");
 				addReply("explain", "As a city for wizards only, we have much to learn from one another. Thus, old quarrels are forgotten and we live here in peace.");
 				addHelp("It is part of my #job to #offer you enchanted scrolls to travel to any major city in Faiumoni. I also have a supply of scrolls you may mark, and some scrolls to summon creatures. Be aware, they do not come cheap.");
-				new SellerAdder().addSeller(this, new SellerBehaviour(shops.get("allscrolls")) {
 
+				new SellerAdder().addSeller(this, new SellerBehaviour(shops.get("allscrolls")) {
 					@Override
 					public int getUnitPrice(String item) {
 						// Player pays 150 % of standard price
 						return (int) (1.50f * priceList.get(item));
 					}
 				});
+
 				addQuest("Neither can live while the other survives! The Dark Lord must be killed...no ... wait... that was some other time. Forgive me for confusing you, I need nothing.");
  				addGoodbye("Adieu.");
 			}
