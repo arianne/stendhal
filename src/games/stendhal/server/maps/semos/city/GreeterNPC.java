6a73afac1bbfe050d40a01363438c1839fc7ab4f
Katie Russell <kymara@users.sourceforge.net>
2008-01-23 22:05:10 +0000

    make another prompt direct to 'buildings' so players don't miss it.

--- src/games/stendhal/server/maps/semos/city/GreeterNPC.java
+++ src/games/stendhal/server/maps/semos/city/GreeterNPC.java
@@ -15,7 +15,7 @@ public class GreeterNPC extends SpeakerNPCFactory {
 	@Override
 	public void createDialog(SpeakerNPC npc) {
 		npc.addJob("I'm Diogenes' older brother and I don't actually remember what I used to do... I'm retired now.");
-		npc.addOffer("I give directions to newcomers to Semos and help them settle in. When I'm in a bad mood I sometimes give misleading directions to amuse myself... hee hee hee! Of course, sometimes I get my wrong directions wrong and they end up being right after all! Ha ha!");
+		npc.addOffer("I give directions to #buildings in Semos, to newcomers settle in. When I'm in a bad mood I sometimes give misleading directions to amuse myself... hee hee hee! Of course, sometimes I get my wrong directions wrong and they end up being right after all! Ha ha!");
 		// All further behaviour is defined in quest classes.
 
 	}
