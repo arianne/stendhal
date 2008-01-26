a15ec79ea4ad01628f12fa3ce9c8a3bcbda33125
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-01-26 00:13:10 +0000

    quote continued bold text

--- src/games/stendhal/client/actions/GMHelpAlterAction.java
+++ src/games/stendhal/client/actions/GMHelpAlterAction.java
@@ -24,7 +24,7 @@ class GMHelpAlterAction implements SlashAction {
 				"/alter <player> <attrib> <mode> <value> \tAlter stat <attrib> of <player> by the given amount; <mode> can be ADD, SUB, or SET",
 				"Examples of <attrib>: atk, def, base_hp, hp, atk_xp, def_xp, xp, outfit",
 				"When modifying 'outfit', you should use SET mode and provide an 8-digit number; the first 2 digits are the 'hair' setting, then 'head', 'outfit', then 'body'",
-				"For example: #/alter #testplayer #outfit #set #12109901",
+				"For example: #'/alter testplayer outfit set 12109901'",
 				"This will make <testplayer> look like danter" };
 
 		for (String line : lines) {
