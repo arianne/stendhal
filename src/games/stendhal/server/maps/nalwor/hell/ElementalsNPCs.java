7e78df7d7e097613f41de59f1d4ca9ffe45098a8
Katie Russell <kymara@users.sourceforge.net>
2008-02-01 00:30:06 +0000

    Add some elemental npcs and an npc who wil eventually control how to leave the zone he controls

---
+++ src/games/stendhal/server/maps/nalwor/hell/ElementalsNPCs.java
@@ -0,0 +1,77 @@
+package games.stendhal.server.maps.nalwor.hell;
+
+import games.stendhal.common.Direction;
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.pathfinder.FixedPath;
+import games.stendhal.server.core.pathfinder.Node;
+import games.stendhal.server.entity.npc.ConversationPhrases;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Creates the elementals npcs in hell
+ *
+ * @author kymara
+ */
+public class ElementalsNPCs implements ZoneConfigurator {
+	/**
+	 * Configure a zone.
+	 *
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+	public void configureZone(StendhalRPZone zone, Map<String, String> attributes) {
+		buildNPCs(zone);
+	}
+
+	private void buildNPCs(StendhalRPZone zone) {
+		String[] names = {"Savanka","Xeoilia","Azira"};
+		Node[] start = new Node[] { new Node(117, 5), new Node(119, 8), new Node(118, 10) };
+		for (int i = 0; i < 3; i++) {
+			SpeakerNPC npc = new SpeakerNPC(names[i]) {
+
+				@Override
+				protected void createPath() {
+					List<Node> nodes = new LinkedList<Node>();
+					nodes.add(new Node(117, 5));
+					nodes.add(new Node(117, 7));
+					nodes.add(new Node(118, 7));
+					nodes.add(new Node(118, 8));
+					nodes.add(new Node(119, 8));
+					nodes.add(new Node(119, 10));
+					nodes.add(new Node(125, 10));
+					nodes.add(new Node(125, 11));
+					nodes.add(new Node(118, 11));
+					nodes.add(new Node(118, 10));
+					nodes.add(new Node(118, 9));
+					nodes.add(new Node(117, 9));
+					nodes.add(new Node(117, 8));
+					nodes.add(new Node(116, 8));
+					nodes.add(new Node(116, 5));
+					setPath(new FixedPath(nodes, true));
+				}
+
+				@Override
+				protected void createDialog() {
+					add(
+			     		ConversationStates.IDLE,
+						ConversationPhrases.GREETING_MESSAGES,
+						ConversationStates.IDLE,
+						"Speak not to us, the harbingers of Hell!",
+						null);
+			
+				}
+			};
+			npc.setEntityClass("fireelementalnpc");
+			npc.setPosition(start[i].getX(), start[i].getY());
+			npc.setDirection(Direction.DOWN);
+			npc.initHP(100);
+			zone.add(npc);
+		}
+	}
+}
---
+++ src/games/stendhal/server/maps/nalwor/hell/ReaperNPC.java
@@ -0,0 +1,58 @@
+package games.stendhal.server.maps.nalwor.hell;
+
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+
+import java.util.Map;
+
+/**
+ * Builds the reaper in hell
+ *
+ * @author kymara
+ */
+public class ReaperNPC implements ZoneConfigurator {
+	//
+	// ZoneConfigurator
+	//
+
+	/**
+	 * Configure a zone.
+	 *
+	 * @param zone
+	 *            The zone to be configured.
+	 * @param attributes
+	 *            Configuration attributes.
+	 */
+	public void configureZone(StendhalRPZone zone,
+			Map<String, String> attributes) {
+		buildNPC(zone, attributes);
+	}
+
+	private void buildNPC(StendhalRPZone zone, Map<String, String> attributes) {
+		SpeakerNPC npc = new SpeakerNPC("Grim Reaper") {
+
+			@Override
+			protected void createPath() {
+				// doesn't move
+				setPath(null);
+			}
+
+			@Override
+			protected void createDialog() {
+				addGreeting("If you seek to #leave this place you must solve a #riddle.");
+				// TODO: The riddles. Will have to be in a quest style file.
+				addReply("leave", "Be patient, kymara hasn't coded it yet.");
+				addReply("riddle", "I will pose a brain teaser for you, if you wish to #leave. Of course, you can rot in hell, if you so desire ... ");
+				addJob("I harvest the souls of the living.");
+				addHelp("I hold the keys to the gates of hell, should you wish to #leave.");
+				addOffer("Unless you wish me to take your soul ... ");
+				addGoodbye("The old order of things has passed away ... ");
+			}
+		};
+		npc.setEntityClass("grim_reaper_npc");
+		npc.setPosition(63, 76);
+		npc.initHP(100);
+		zone.add(npc);
+	}
+}
