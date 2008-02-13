58e7c901fe6f78790d5121ef102a15cf360cb74d
Katie Russell <kymara@users.sourceforge.net>
2008-02-13 19:32:11 +0000

    allow players to leave through asking 2nd npc. tidy up 1st npc for release. drop level requirement of entry to 200.

--- src/games/stendhal/server/maps/nalwor/hell/ReaperNPC.java
+++ src/games/stendhal/server/maps/nalwor/hell/ReaperNPC.java
@@ -7,8 +7,8 @@ import games.stendhal.server.entity.npc.SpeakerNPC;
 import java.util.Map;
 
 /**
- * Builds the reaper in hell.
- *
+ * Builds the reaper in hell
+ * Remaining behaviour will be in games.stendhal.server.maps.quests.SolveRiddles
  * @author kymara
  */
 public class ReaperNPC implements ZoneConfigurator {
@@ -40,12 +40,12 @@ public class ReaperNPC implements ZoneConfigurator {
 
 			@Override
 			protected void createDialog() {
-				addGreeting("If you seek to #leave this place you must solve a #riddle.");
-				// TODO: The riddles. Will have to be in a quest style file.
-				addReply("leave", "Be patient, kymara hasn't coded it yet.");
+				addGreeting("If you seek to #leave this place you must solve a #riddle");
 				addReply("riddle", "I will pose a brain teaser for you, if you wish to #leave. Of course, you can rot in hell, if you so desire ... ");
+				addReply("leave", "A pity for you, I do not have my book of riddles with me! You will have to ask my mirror to let you leave. But it will hurt...");
+				// Remaining behaviour will be in games.stendhal.server.maps.quests.SolveRiddles
 				addJob("I harvest the souls of the living.");
-				addHelp("I hold the keys to the gates of hell, should you wish to #leave.");
+				addHelp("I hold the keys to the gates of hell, should you wish to #leave");
 				addOffer("Unless you wish me to take your soul ... ");
 				addGoodbye("The old order of things has passed away ... ");
 			}
