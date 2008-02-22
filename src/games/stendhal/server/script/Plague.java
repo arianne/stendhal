f368548de54eba9594e0ccd01a76d6696992d603
Astrid Sawatzky <astridemma@users.sourceforge.net>
2008-02-22 21:12:37 +0000

    usage Plague.class [ringcount] creature name

--- src/games/stendhal/server/script/Plague.java
+++ src/games/stendhal/server/script/Plague.java
@@ -25,20 +25,27 @@ public class Plague extends ScriptImpl {
 			return;
 		}
 
+		
+		int ringcount = MathHelper.parseIntDefault(args.get(0), -1);
+		int startArgIndex = 1;
+		if (ringcount == -1){
+			ringcount = 1;
+			startArgIndex = 0;
+		}
+		String creatureClass="";
+		List <String>  templist  =   args.subList(startArgIndex,args.size());
+		for (String part : templist) {
+			creatureClass = creatureClass + part + " "; 
+		}
+		
+		creatureClass  = creatureClass.trim();
 		// extract position of admin
 		StendhalRPZone myZone = sandbox.getZone(admin);
 		int x = admin.getX();
 		int y = admin.getY();
 		sandbox.setZone(myZone);
 
-		// select creature
-		String creatureClass;
-
-		if (args.size() >= 2) {
-			creatureClass = args.get(1);
-		} else {
-			creatureClass = args.get(0);
-		}
+		
 
 		Creature tempCreature = sandbox.getCreature(creatureClass);
 		if (tempCreature == null) {
@@ -46,24 +53,20 @@ public class Plague extends ScriptImpl {
 		} else {
 			Creature creature = new RaidCreature(tempCreature);
 
-			// spawn the specified amout of them
-			if (args.size() == 1) {
-				sandbox.add(creature, x, y);
-			} else {
-				int k = MathHelper.parseIntDefault(args.get(0), 1);
-				if (k <= MAX_RING_COUNT) {
-					for (int dx = -k; dx <= k; dx++) {
-						for (int dy = -k; dy <= k; dy++) {
-							if ((dx != 0) || (dy != 0)) {
-								sandbox.add(creature, x + dx, y + dy + 1);
-							}
+			int k = MathHelper.parseIntDefault(args.get(0), 1);
+			if (k <= MAX_RING_COUNT) {
+				for (int dx = -k; dx <= k; dx++) {
+					for (int dy = -k; dy <= k; dy++) {
+						if ((dx != 0) || (dy != 0)) {
+							sandbox.add(creature, x + dx, y + dy + 1);
 						}
 					}
-				} else {
-					admin.sendPrivateText("That's too many! Please keep <ringcount> less or equal to "
-							+ MAX_RING_COUNT + ".");
 				}
+			} else {
+				admin.sendPrivateText("That's too many! Please keep <ringcount> less or equal to "
+						+ MAX_RING_COUNT + ".");
 			}
+			
 		}
 	}
 }
