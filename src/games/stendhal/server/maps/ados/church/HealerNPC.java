5f4e92a1627f26b83107d554d75d3287ea150255
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-02-27 20:53:19 +0000

    fix Valos' replies

--- src/games/stendhal/server/maps/ados/church/HealerNPC.java
+++ src/games/stendhal/server/maps/ados/church/HealerNPC.java
@@ -17,10 +17,10 @@ public class HealerNPC extends SpeakerNPCFactory {
 	public void createDialog(SpeakerNPC npc) {
 	    npc.addJob("Long ago I was a priest of this church. But my #ideas were not approved of by all."); 
 	    npc.addReply("ideas",
-		        "I have read many texts and learnt of strange ways. My healing powers became so strong I can now #concoct a special #mega potion for warriors like you.");
+		        "I have read many texts and learnt of strange ways. My healing powers became so strong I can now #concoct a special #'mega potion' for warriors like you.");
 	    npc.addReply("giant heart",
 		        "Giants dwell in caves east of here. Good luck slaying those beasts ...");
-	    npc.addReply("heal", "I can #concoct a strong #mega potion for you to use while you travel. I need one special ingredient and a small charge to cover my time.");
+	    npc.addReply("heal", "I can #concoct a strong #'mega potion' for you to use while you travel. I need one special ingredient and a small charge to cover my time.");
 	    npc.addReply("mega potion", "It is a powerful elixir. If you want one, ask me to #'concoct 1 mega' potion.");
 	    npc.addReply("money", "That is your own concern. We of the cloth need not scurry around to make cash.");    
 	    npc.addHelp("If you want to become wise like me, you should visit a library. There is much to learn.");
