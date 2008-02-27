681b9302ad2cdb62febd6b896cd232fc33680ca6
Astrid Sawatzky <astridemma@users.sourceforge.net>
2008-02-27 12:50:35 +0000

    avoid instance of

--- src/games/stendhal/client/entity/Inspector.java
+++ src/games/stendhal/client/entity/Inspector.java
@@ -6,5 +6,5 @@ import marauroa.common.game.RPSlot;
 public interface Inspector {
 
 	EntityContainer inspectMe(Entity entity, RPSlot content,
-			EntityContainer container);
+			EntityContainer container, int width , int height);
 }
