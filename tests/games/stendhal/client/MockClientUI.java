dc5b01b535633ce2a1feb2e6f45de6ecd7bc2128
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-03-01 16:03:59 +0000

    add client UI mock class

---
+++ tests/games/stendhal/client/MockClientUI.java
@@ -0,0 +1,23 @@
+package games.stendhal.client;
+
+import games.stendhal.client.gui.j2DClient;
+import games.stendhal.common.ErrorBuffer;
+import games.stendhal.common.NotificationType;
+
+/**
+ * Mock client as replacement for j2DClient.
+ *
+ * @author Martin Fuchs
+ */
+public class MockClientUI extends j2DClient {
+	private ErrorBuffer buffer = new ErrorBuffer();
+
+	@Override
+	public void addEventLine(final String header, final String text, final NotificationType type) {
+		buffer.setError(text);
+	}
+
+	public String getEventBuffer() {
+		return buffer.getErrorString();
+	}
+}
