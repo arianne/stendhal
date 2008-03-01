7f40948c1af58589046567bdf7c0ade3d243d005
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-03-01 21:19:14 +0000

    tests for /summonat

---
+++ tests/games/stendhal/client/actions/SummonAtActionTest.java
@@ -0,0 +1,88 @@
+package games.stendhal.client.actions;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import games.stendhal.client.MockClientUI;
+import games.stendhal.client.MockStendhalClient;
+import marauroa.common.game.RPAction;
+
+import org.junit.Test;
+
+/**
+ * Test the SummonAtAction class.
+ *
+ * @author Martin Fuchs
+ */
+public class SummonAtActionTest {
+
+	@Test
+	public void testInvalidAmount() {
+		MockClientUI clientUI = new MockClientUI();
+		SummonAtAction action = new SummonAtAction();
+
+		// issue "/summonat bag 5x money"
+		assertTrue(action.execute(new String[]{"player", "bag", "5x"}, "money"));
+		assertEquals("Invalid amount", clientUI.getEventBuffer());
+	}
+
+	@Test
+	public void testExecute() {
+		// create client UI
+		MockClientUI clientUI = new MockClientUI();
+
+		// create client
+		new MockStendhalClient("") {
+			@Override
+			public void send(RPAction action) {
+				client = null;
+				assertEquals("summonat", action.get("type"));
+				assertEquals("player", action.get("target"));
+				assertEquals("bag", action.get("slot"));
+				assertEquals(5, action.getInt("amount"));
+				assertEquals("money", action.get("item"));
+			}
+		};
+
+		// issue "/summonat bag 5 money"
+		SummonAtAction action = new SummonAtAction();
+		assertTrue(action.execute(new String[]{"player", "bag", "5"}, "money"));
+		assertEquals("", clientUI.getEventBuffer());
+	}
+
+	@Test
+	public void testSpaceHandling() {
+		// create client UI
+		MockClientUI clientUI = new MockClientUI();
+
+		// create client
+		new MockStendhalClient("") {
+			@Override
+			public void send(RPAction action) {
+				client = null;
+				assertEquals("summonat", action.get("type"));
+				assertEquals("player", action.get("target"));
+				assertEquals("bag", action.get("slot"));
+				assertEquals(1, action.getInt("amount"));
+				assertEquals("silver sword", action.get("item"));
+			}
+		};
+
+		// issue "/summonat bag silver sword"
+		SummonAtAction action = new SummonAtAction();
+		assertTrue(action.execute(new String[]{"player", "bag", "silver"}, "sword"));
+		assertEquals("", clientUI.getEventBuffer());
+	}
+
+	@Test
+	public void testGetMaximumParameters() {
+		SummonAtAction action = new SummonAtAction();
+		assertEquals(3, action.getMaximumParameters());
+	}
+
+	@Test
+	public void testGetMinimumParameters() {
+		SummonAtAction action = new SummonAtAction();
+		assertEquals(3, action.getMinimumParameters());
+	}
+
+}
