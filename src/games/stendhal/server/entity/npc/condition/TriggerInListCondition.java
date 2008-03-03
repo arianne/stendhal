775478943bca64c2f67c02e165d85d89904f4aa3
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-03-03 21:06:23 +0000

    use TriggerList

--- src/games/stendhal/server/entity/npc/condition/TriggerInListCondition.java
+++ src/games/stendhal/server/entity/npc/condition/TriggerInListCondition.java
@@ -1,13 +1,11 @@
 package games.stendhal.server.entity.npc.condition;
 
 import games.stendhal.server.entity.npc.SpeakerNPC;
-import games.stendhal.server.entity.npc.parser.ConversationParser;
 import games.stendhal.server.entity.npc.parser.Sentence;
-import games.stendhal.server.entity.npc.parser.Expression;
+import games.stendhal.server.entity.npc.parser.TriggerList;
 import games.stendhal.server.entity.player.Player;
 
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.commons.lang.builder.EqualsBuilder;
@@ -17,7 +15,7 @@ import org.apache.commons.lang.builder.HashCodeBuilder;
  * Was one of theses trigger phrases said? (Use with a ""-trigger in npc.add)
  */
 public class TriggerInListCondition extends SpeakerNPC.ChatCondition {
-	private List<Expression> triggers = new LinkedList<Expression>();
+	private TriggerList triggers;
 
 	/**
 	 * Creates a new TriggerInListCondition.
@@ -36,9 +34,7 @@ public class TriggerInListCondition extends SpeakerNPC.ChatCondition {
 	 *            list of trigger
 	 */
 	public TriggerInListCondition(List<String> trigger) {
-		for (String trig : trigger) {
-			this.triggers.add(ConversationParser.createTriggerExpression(trig));
-		}
+		triggers = new TriggerList(trigger);
 	}
 
 	@Override
