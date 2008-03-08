1de11dc09f3e7bb7089af3347d2f8acf08033c11
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-03-08 02:14:51 +0000

    add tests for WordEntry

---
+++ tests/games/stendhal/server/entity/npc/parser/WordEntryTest.java
@@ -0,0 +1,28 @@
+package games.stendhal.server.entity.npc.parser;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Test the NPC conversation WordEntry class.
+ * 
+ * @author Martin Fuchs
+ */
+public class WordEntryTest {
+
+	@Test
+	public final void testNouns() {
+		WordEntry w = new WordEntry();
+		assertEquals("/", w.getNormalizedWithTypeString());
+
+		w.setNormalized("norm");
+		w.setPlurSing("plur");
+		assertEquals("norm/", w.getNormalizedWithTypeString());
+
+		w.setType(new ExpressionType("TYP"));
+		w.setValue(4711);
+		assertEquals("norm/TYP", w.getNormalizedWithTypeString());
+	}
+
+}
