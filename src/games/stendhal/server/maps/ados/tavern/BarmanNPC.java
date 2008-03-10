853ac994a270b4cc15369562f9ddae586b642d00
Katie Russell <kymara@users.sourceforge.net>
2008-03-10 21:58:14 +0000

    add a male barman for the ladies' room of ados tavern. he offers things that girls like ;) make coralia offer things more suited to guys.

--- src/games/stendhal/server/maps/ados/tavern/BarmanNPC.java
+++ src/games/stendhal/server/maps/ados/tavern/BarmanNPC.java
@@ -17,9 +17,9 @@ import java.util.Map;
 /**
  * Ados Tavern (Inside / Level 0).
  *
- * @author hendrik
+ * @author kymara
  */
-public class BarMaidNPC implements ZoneConfigurator {
+public class BarmanNPC implements ZoneConfigurator {
 
 	/**
 	 * Configure a zone.
@@ -32,44 +32,43 @@ public class BarMaidNPC implements ZoneConfigurator {
 	}
 
 	private void buildTavern(StendhalRPZone zone, Map<String, String> attributes) {
-		SpeakerNPC tavernMaid = new SpeakerNPC("Coralia") {
+		SpeakerNPC barman = new SpeakerNPC("Dale") {
 
 			@Override
 			protected void createPath() {
 				List<Node> nodes = new LinkedList<Node>();
-				nodes.add(new Node(13, 9));
-				nodes.add(new Node(13, 7));
-				nodes.add(new Node(6, 7));
-				nodes.add(new Node(6, 12));
-				nodes.add(new Node(10, 12));
-				nodes.add(new Node(10, 5));
-				nodes.add(new Node(17, 5));
-                nodes.add(new Node(17, 3));
-                nodes.add(new Node(3, 3));
-                nodes.add(new Node(3, 6));
-                nodes.add(new Node(13, 6));
+				nodes.add(new Node(27, 2));
+				nodes.add(new Node(23, 2));
+				nodes.add(new Node(23, 5));
+				nodes.add(new Node(27, 5));
+				nodes.add(new Node(27, 8));
+				nodes.add(new Node(24, 8));
+				nodes.add(new Node(24, 12));
+                nodes.add(new Node(28, 12));
+                nodes.add(new Node(28, 6));
+                nodes.add(new Node(23, 6));
+                nodes.add(new Node(23, 2));
 				setPath(new FixedPath(nodes, true));
 			}
 
 			@Override
 			protected void createDialog() {
-				addGreeting();
-				addJob("I am the bar maid for this fair tavern. We sell both imported and local beers, and fine food.");
-				addHelp("This tavern is a great place to take a break and meet new people! Just ask if you want me to #offer you a drink.");
+				addGreeting("Hey, good looking ...");
+				addJob("I keep the ladies happy. How you doin'?");
+				addQuest("Just sit back, relax and enjoy the show.");
+				addHelp("This is the room for the ladies. Guys hang out in the other bar, Coralia serves the gents.");
 				Map<String, Integer> offerings = new HashMap<String, Integer>();
-				offerings.put("beer", 15);
-				offerings.put("pie", 160);
-				offerings.put("bread", 50);
-				offerings.put("cheese", 20);
-				offerings.put("tomato", 30);
+				offerings.put("wine", 20);
+				offerings.put("pina colada", 100);
+				offerings.put("chocolate bar", 100);
 				new SellerAdder().addSeller(this, new SellerBehaviour(offerings));
-				addGoodbye();
+				addGoodbye("See you around, sweetcheeks.");
 			}
 		};
 
-		tavernMaid.setEntityClass("maidnpc");
-		tavernMaid.setPosition(13, 9);
-		tavernMaid.initHP(100);
-		zone.add(tavernMaid);
+		barman.setEntityClass("barman3npc");
+		barman.setPosition(27, 2);
+		barman.initHP(100);
+		zone.add(barman);
 	}
 }
