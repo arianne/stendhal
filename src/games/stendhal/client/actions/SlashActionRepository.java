b89bf3569ee07230db20429e68989d5797281f97
Miguel Angel Blanch Lardin <arianne_rpg@users.sourceforge.net>
2008-03-14 20:28:50 +0000

    Added sentence server side. Tested.

--- src/games/stendhal/client/actions/SlashActionRepository.java
+++ src/games/stendhal/client/actions/SlashActionRepository.java
@@ -39,6 +39,7 @@ public class SlashActionRepository {
 		actions.put("msg", msg);
 		actions.put("quit", new QuitAction());
 		actions.put("remove", new RemoveBuddyAction());
+		actions.put("sentence", new SentenceAction());
 		actions.put("sound", new SoundAction());
 		actions.put("summonat", new SummonAtAction());
 		actions.put("summon", new SummonAction());
