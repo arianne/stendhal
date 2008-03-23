5b776e5e1d5d8a4de4e0bd8a9fb6a0a10bdb47f1
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-03-23 09:39:19 +0000

    add classes for the "Wood" Look and feel configured from XML.

---
+++ src/games/stendhal/client/gui/laf/WoodInternalFrameUI.java
@@ -0,0 +1,17 @@
+package games.stendhal.client.gui.laf;
+
+import javax.swing.JInternalFrame;
+import javax.swing.plaf.basic.BasicInternalFrameUI;
+
+/**
+ * Experimental painter class for internal frames the Wood look and feel.
+ *
+ * @author Martin Fuchs
+ */
+public class WoodInternalFrameUI extends BasicInternalFrameUI {
+
+	public WoodInternalFrameUI(JInternalFrame b) {
+	    super(b);
+    }
+
+}
---
+++ src/games/stendhal/client/gui/laf/WoodLookAndFeel.java
@@ -0,0 +1,55 @@
+package games.stendhal.client.gui.laf;
+
+import javax.swing.UIDefaults;
+import javax.swing.plaf.synth.SynthLookAndFeel;
+
+import data.gui.LFRessourceBase;
+
+/**
+ * Look and feel for the Wood style, configured from XML.
+ *
+ * @author Martin Fuchs
+ */
+@SuppressWarnings("serial")
+public class WoodLookAndFeel extends SynthLookAndFeel {
+
+	public WoodLookAndFeel() {
+		try {
+	        load(WoodLookAndFeel.class.getClassLoader().getResourceAsStream("data/gui/woodskin.xml"), LFRessourceBase.class);
+        } catch(Exception e) {
+	        e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getName() {
+        return "Wood";
+    }
+
+    @Override
+    public String getID() {
+        return "Wood";
+    }
+
+    /**
+     * Load the SystemColors into the defaults table.  The keys
+     * for SystemColor defaults are the same as the names of
+     * the public fields in SystemColor.
+     */
+    protected void initSystemColorDefaults(UIDefaults table)
+    {
+    	super.initSystemColorDefaults(table);
+
+//        table.put("window", WoodStyle.getInstance().getForeground());
+    }
+
+    @Override
+    protected void initClassDefaults(UIDefaults table)
+    {
+        super.initClassDefaults(table);
+
+//        table.put("ButtonUI", "games.stendhal.client.gui.laf.WoodButtonUI");
+//        table.put("InternalFrameUI", "games.stendhal.client.gui.laf.WoodInternalFrameUI");
+    }
+
+}
