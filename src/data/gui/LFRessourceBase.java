19718cf3a9c72ae742b9616e7c77f960841690fd
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-03-23 09:30:13 +0000

    add helper class to load the look and feel configuration

---
+++ src/data/gui/LFRessourceBase.java
@@ -0,0 +1,10 @@
+package data.gui;
+
+/**
+ * Class to be used as base for the class loader to load Look and Feel resources.
+ *
+ * @author Martin Fuchs
+ */
+public final class LFRessourceBase {
+
+}
