f988541c7367db9684f04e28313af302feae72b0
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-03-30 21:31:33 +0000

    change GroundContainer to use Swing's JInternalFrame and adjust to the new client code

--- src/games/stendhal/client/gui/wt/GroundContainer.java
+++ src/games/stendhal/client/gui/wt/GroundContainer.java
@@ -15,227 +15,184 @@ package games.stendhal.client.gui.wt;
 
 import games.stendhal.client.IGameScreen;
 import games.stendhal.client.StendhalClient;
-import games.stendhal.client.StendhalUI;
 import games.stendhal.client.entity.ActionType;
 import games.stendhal.client.entity.Entity;
 import games.stendhal.client.entity.EntityView;
 import games.stendhal.client.entity.Inspector;
+import games.stendhal.client.gui.Desktop;
+import games.stendhal.client.gui.MouseHandlerAdapter;
 import games.stendhal.client.gui.j2d.Text;
 import games.stendhal.client.gui.j2d.entity.Entity2DView;
-import games.stendhal.client.gui.wt.core.WtBaseframe;
-import games.stendhal.client.gui.wt.core.WtDraggable;
-import games.stendhal.client.gui.wt.core.WtDropTarget;
 
+import java.awt.Dimension;
 import java.awt.Point;
+import java.awt.event.MouseEvent;
 import java.awt.geom.Point2D;
+
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
 
 import marauroa.common.game.RPAction;
 import marauroa.common.game.RPSlot;
 
 /**
  * 
- * This container is the ground.
+ * This container is the game play ground.
  * 
  * @author mtotz
  * 
  */
-public class GroundContainer extends WtBaseframe implements WtDropTarget,
-		Inspector {
+@SuppressWarnings("serial")
+public class GroundContainer extends JInternalFrame implements Inspector
+{
 	/** the game client. */
 	private StendhalClient client;
-
-	/**
-	 * The UI.
-	 */
-	private StendhalUI ui;
 
 	/** the game screen. */
 	private IGameScreen screen;
 
-	/** creates a new groundcontainer. */
-	public GroundContainer(final StendhalClient client,
-			final IGameScreen screen, final int width, final int height) {
-		super(width, height);
+	/* The game screen panel */
+	Desktop desktop;
 
-		this.client = client;
-		this.screen = screen;
-
-		ui = StendhalUI.get();
-	}
-
-	/** drags an item from the ground .*/
-	@Override
-	protected WtDraggable getDragged(int x, int y) {
-		WtDraggable other = super.getDragged(x, y);
-
-		if (other != null) {
-			return other;
-		}
-
-		Point2D point = screen.convertScreenViewToWorld(x, y);
-		EntityView view = screen.getMovableEntityViewAt(point.getX(),
-				point.getY());
-
-		// only Items can be dragged
-		if (view != null) {
-			return new MoveableEntityContainer(view.getEntity());
-		}
-
-		return null;
-	}
+	final Dimension size;
 
 	/**
-	 * 
-	 * 
-	 * 
+	 * creates a new GroundContainer. 
 	 */
-	@Override
-	public synchronized boolean onMouseClick(Point p) {
-		// base class checks if the click is within a child
-		if (super.onMouseClick(p)) {
-			// yes, click already processed
-			return true;
-		}
+	public GroundContainer(Desktop desktop, final StendhalClient client, final IGameScreen screen, final Dimension size) {
+		setName("baseframe");
+//		super("baseframe", 0, 0, width, height);
 
-		// get clicked entity
-		Point2D point = screen.convertScreenViewToWorld(p);
+		this.size = size;
+		this.client = client;
+		this.screen = screen;
+		this.desktop = desktop;
 
-		// for the text pop up....
-		Text text = screen.getTextAt(point.getX(), point.getY());
-		if (text != null) {
-			screen.removeText(text);
-			return true;
-		}
-
-		// for the clicked entity....
-		EntityView view = screen.getEntityViewAt(point.getX(), point.getY());
-		if (view != null) {
-			if (ui.isCtrlDown()) {
-				view.onAction();
-				return true;
-			} else if (ui.isShiftDown()) {
-				view.onAction(ActionType.LOOK);
-				return true;
-			}
-		}
-
-		return false;
+		// register native event handler
+		desktop.addMouseListener(new MyMouseHandlerAdapter());
+//		desktop.addMouseMotionListener(this);
 	}
 
-	@Override
-	public synchronized boolean onMouseDoubleClick(Point p) {
-		// base class checks if the click is within a child
-		if (super.onMouseDoubleClick(p)) {
-			// yes, click already processed
-			return true;
-		}
-		// doubleclick is outside of all windows
-		Point2D point = screen.convertScreenViewToWorld(p);
+	class MyMouseHandlerAdapter extends MouseHandlerAdapter {
 
-		// for the text pop up....
-		Text text = screen.getTextAt(point.getX(), point.getY());
-		if (text != null) {
-			screen.removeText(text);
-			return true;
-		}
+    	/** Processes popup menus. */
+    	@Override
+    	protected void onPopup(MouseEvent e) {
+    		Point pt = getMousePoint(e);
+    		if (pt == null) {
+    			return;
+    		}
 
-		EntityView view = screen.getEntityViewAt(point.getX(), point.getY());
+    		Point2D point = screen.convertScreenViewToWorld(pt);
 
-		if (view != null) {
-			// ... do the default action
-			view.onAction();
-			return true;
-		} else {
-			// moveto action
-			RPAction action = new RPAction();
-			action.put("type", "moveto");
-			action.put("x", (int) point.getX());
-			action.put("y", (int) point.getY());
-			client.send(action);
-			// TODO: let action do this
-			return true;
-		}
+    		Entity2DView view = screen.getEntityViewAt(point.getX(), point.getY());
+
+    		if (view != null) {
+    			// ... show context menu (aka command list)
+    			String[] actions = view.getActions();
+
+    			if (actions.length > 0) {
+    				Entity entity = view.getEntity();
+
+    				new CommandList(entity.getType(), actions, view).show(e.getComponent(), e.getX(), e.getY());
+    			}
+    		}
+    	}
+
+    	/** Process double clicks. */
+    	@Override
+    	public void onLDoubleClick(MouseEvent e) {
+    		Point pt = getMousePoint(e);
+    		if (pt == null) {
+    			return;
+    		}
+
+    		Point2D point = screen.convertScreenViewToWorld(pt);
+
+    		// for the text pop up....
+    		Text text = screen.getTextAt(point.getX(), point.getY());
+    		if (text != null) {
+    			screen.removeText(text);
+    			return;
+    		}
+
+    		EntityView view = screen.getEntityViewAt(point.getX(), point.getY());
+
+    		if (view != null) {
+    			// ... do the default action
+    			view.onAction();
+    		} else {
+    			// moveto action
+    			RPAction action = new RPAction();
+    			action.put("type", "moveto");
+    			action.put("x", (int) point.getX());
+    			action.put("y", (int) point.getY());
+    			client.send(action);
+    			// TODO: let action do this
+    		}
+    	}
+
+    	@Override
+    	public synchronized void onLeftClick(MouseEvent e) {
+    		Point pt = getMousePoint(e);
+    		if (pt == null) {
+    			return;
+    		}
+
+    		// get clicked entity
+    		Point2D point = screen.convertScreenViewToWorld(pt);
+
+    		// for the text pop up....
+    		Text text = screen.getTextAt(point.getX(), point.getY());
+    		if (text != null) {
+    			screen.removeText(text);
+    			return;
+    		}
+
+    		// for the clicked entity....
+    		EntityView view = screen.getEntityViewAt(point.getX(), point.getY());
+
+    		if (view != null) {
+    			if (e.isControlDown()) {
+    				view.onAction();
+    			} else if (e.isShiftDown()) {
+    				view.onAction(ActionType.LOOK);
+    			}
+    		}
+    	}
+
 	}
 
-	/** Processes right click. */
-	@Override
-	public synchronized boolean onMouseRightClick(Point p) {
-		// base class checks if the click is within a child
-		if (super.onMouseRightClick(p)) {
-			// yes, click already processed
-			return true;
-		}
-		// doubleclick is outside of all windows
-		Point2D point = screen.convertScreenViewToWorld(p);
+	private Point getMousePoint(MouseEvent e) {
+		Point offset = desktop.getOffset();
 
-		Entity2DView view = screen.getEntityViewAt(point.getX(), point.getY());
+		int x = e.getPoint().x - offset.x;
+		int y = e.getPoint().y - offset.y;
 
-		if (view != null) {
-			// ... show context menu (aka command list)
-			String[] actions = view.getActions();
-
-			if (actions.length > 0) {
-				Entity entity = view.getEntity();
-
-				setContextMenu(new CommandList(entity.getType(), actions, view));
-			}
-			return true;
+		if (x >= size.width || y >= size.height) {
+			return null;
 		}
 
-		return false;
-	}
-
-	//
-	// WtDropTarget
-	//
-
-	/** called when an object is dropped. */
-	public boolean onDrop(final int x, final int y, WtDraggable droppedObject) {
-		// Not an entity?
-		if (!(droppedObject instanceof MoveableEntityContainer)) {
-			return false;
+		if (x < 0 || y < 0) {
+			return null;
 		}
 
-		MoveableEntityContainer container = (MoveableEntityContainer) droppedObject;
-
-		RPAction action = new RPAction();
-
-		if (container.isContained()) {
-			// looks like an drop
-			action.put("type", "drop");
-		} else {
-			// it is a displace
-			action.put("type", "displace");
-		}
-
-		// HACK: if ctrl is pressed, attempt to split stackables
-		if (ui.isCtrlDown()) {
-			action.put("quantity", 1);
-		}
-
-		// fill 'moved from' parameters
-		container.fillRPAction(action);
-
-		// 'move to'
-		Point2D point = screen.convertScreenViewToWorld(x, y);
-		action.put("x", (int) point.getX());
-		action.put("y", (int) point.getY());
-
-		client.send(action);
-		return true;
+		return new Point(x, y);
 	}
 
 	//
 	// Inspector
 	//
 
-	public EntityContainer inspectMe(Entity suspect, RPSlot content,
-			EntityContainer container, int width , int height) {
-		if ((container == null) || !container.isVisible()) {
+	public EntityContainer inspectMe(Entity suspect, RPSlot content, EntityContainer container, int width, int height) {
+		if (container == null || !container.isVisible()) {
 			container = new EntityContainer(suspect.getType(), width, height);
-			
 
-			addChild(container);
+			// immediately free the memory after closing inspect windows
+			container.setClosable(true);
+			container.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+			desktop.add(container);
 
 			container.setSlot(suspect, content.getName());
 			container.setVisible(true);
