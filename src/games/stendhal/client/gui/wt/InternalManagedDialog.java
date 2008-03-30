15f7bc359b176717b70dc97bb99a65dd4b762f5e
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-03-30 21:27:50 +0000

    remove unused code

--- src/games/stendhal/client/gui/wt/InternalManagedDialog.java
+++ src/games/stendhal/client/gui/wt/InternalManagedDialog.java
@@ -22,11 +22,6 @@ public class InternalManagedDialog extends ClientPanel {
 	 */
 	protected Component content;
 
-//	/**
-//	 * The content pane.
-//	 */
-//	protected StyledJPanel contentPane;
-
 	/**
 	 * Create a managed dialog window.
 	 * 
@@ -63,44 +58,6 @@ public class InternalManagedDialog extends ClientPanel {
 		setMinimized(!isMinimized());
 	}
 
-//	/**
-//	 * Do simulated dialog layout.
-//	 */
-//	public void pack() {
-//		Dimension cSize;
-//		int width;
-//
-//		if (content != null) {
-//			cSize = content.getPreferredSize();
-//		} else {
-//			cSize = new Dimension(0, 0);
-//		}
-//
-//		if (content != null) {
-//			content.setBounds(0, tbSize.height, width, cSize.height);
-//
-//			content.validate();
-//		}
-//
-//		if (isMinimized()) {
-//			dialog.setBounds(0, 0, width, tbSize.height);
-//			contentPane.setBounds(0, 0, width, tbSize.height);
-//		} else {
-//			dialog.setSize(width, tbSize.height + cSize.height);
-//
-//			contentPane.setSize(width, tbSize.height + cSize.height);
-//		}
-//	}
-
-//	/**
-//	 * Get the actual dialog.
-//	 * 
-//	 * @return The dialog.
-//	 */
-//	public Container getDialog() {
-//		return dialog;
-//	}
-
 	/**
 	 * Set the content component. For now, if the content wishes to resize the
 	 * dialog, it should set a client property named <code>size-change</code>
@@ -110,20 +67,6 @@ public class InternalManagedDialog extends ClientPanel {
 	 *            A component to implement the content.
 	 */
 	public void setContent(Component content) {
-//		if (this.content != null) {
-////			this.content.removePropertyChangeListener(sizeChangeListener);
-//
-//			contentPane.remove(this.content);
-//		}
-//
-//		this.content = content;
-//
-//		if (content instanceof JComponent) {
-//			contentPane.add(content);
-//		}
-
-//		content.addPropertyChangeListener("size-change", sizeChangeListener);
-
 		add(content);
 
 		pack();
@@ -138,19 +81,5 @@ public class InternalManagedDialog extends ClientPanel {
 		 */
 //		WtWindowManager.getInstance().moveTo(this, getX(), getY());
 	}
-
-//	/**
-//	 * Handle content resize required property change.
-//	 */
-//	protected class ContentSizeChangeCB implements PropertyChangeListener {
-//
-//		//
-//		// PropertyChangeListener
-//		//
-//
-//		public void propertyChange(PropertyChangeEvent ev) {
-//			pack();
-//		}
-//	}
 
 }
