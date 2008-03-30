d043600825b60386f60c90f3da11b52a1202b6d9
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-03-30 20:37:22 +0000

    reorganize imports

--- src/games/stendhal/client/update/HttpClient.java
+++ src/games/stendhal/client/update/HttpClient.java
@@ -7,10 +7,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.SocketTimeoutException;
 import java.net.URL;
-import java.net.ConnectException;
 import java.util.Properties;
 
 /**
