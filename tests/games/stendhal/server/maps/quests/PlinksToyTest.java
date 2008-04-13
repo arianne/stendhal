a9f4fafa63da106e7c25298c3c6988d72852c3ba
Astrid Sawatzky <astridemma@users.sourceforge.net>
2008-04-13 11:14:42 +0000

    removed unused imports

--- tests/games/stendhal/server/maps/quests/PlinksToyTest.java
+++ tests/games/stendhal/server/maps/quests/PlinksToyTest.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 import games.stendhal.server.core.engine.SingletonRepository;
--- tiled/src/tiled/util/TiledConfiguration.java
+++ tiled/src/tiled/util/TiledConfiguration.java
@@ -19,7 +19,6 @@ package tiled.util;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
