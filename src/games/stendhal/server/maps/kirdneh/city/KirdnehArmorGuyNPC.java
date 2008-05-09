ead3c6840a1e76d39ff96742fe342930c5c675ae
tigertoes <tigertoes@users.sourceforge.net>
2008-05-09 08:18:17 +0000

    removed unused imports

--- src/games/stendhal/server/maps/kirdneh/city/KirdnehArmorGuyNPC.java
+++ src/games/stendhal/server/maps/kirdneh/city/KirdnehArmorGuyNPC.java
@@ -8,11 +8,7 @@ import games.stendhal.server.core.pathfinder.Node;
 import games.stendhal.server.entity.npc.ShopList;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.behaviour.adder.BuyerAdder;
-import games.stendhal.server.entity.npc.behaviour.adder.SellerAdder;
 import games.stendhal.server.entity.npc.behaviour.impl.BuyerBehaviour;
-import games.stendhal.server.entity.npc.behaviour.impl.SellerBehaviour;
-
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
