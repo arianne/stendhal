efb075e860f8d19df88788d689e5e3c3c9a7da3e
Astrid Sawatzky <astridemma@users.sourceforge.net>
2008-05-29 14:05:47 +0000

    a bit more sophisticated, (not really though)

--- src/games/stendhal/server/entity/creature/impl/Coward.java
+++ src/games/stendhal/server/entity/creature/impl/Coward.java
@@ -5,9 +5,12 @@ import games.stendhal.server.entity.creature.Creature;
 public class Coward extends HandToHand {
 	@Override
 	public void getBetterAttackPosition(Creature creature) {
-		creature.faceToward(creature.getAttackTarget());
-		creature.setDirection(creature.getDirection().oppositeDirection());
-		creature.setSpeed(creature.getBaseSpeed());
+		super.getBetterAttackPosition(creature);
+		if (creature.isAttacked()){
+			 creature.faceToward(creature.getAttackSources().get(0));
+			 creature.setDirection(creature.getDirection().oppositeDirection());
+			 creature.setSpeed(creature.getBaseSpeed());
+		}
 	}
 
 	
