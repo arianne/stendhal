2adc093f62eb301990bb597d0a3287123ab775fd
Martin Fuchs <martinfuchs@users.sourceforge.net>
2008-05-30 07:15:59 +0000

    fix bug 1978657: Tendertwig> My your travels be pleasant, my fine friend

--- src/games/stendhal/server/maps/ados/entwives/EntwifeNPC.java
+++ src/games/stendhal/server/maps/ados/entwives/EntwifeNPC.java
@@ -35,7 +35,7 @@ public class EntwifeNPC implements ZoneConfigurator {
 				addJob("I guard all i can see. It is a peaceful life.");
 				addHelp("There is lots to see and harvest here. Just wander around.");
 				addOffer("I have nothing to offer but fresh air and sunshine.");
-				addGoodbye("My your travels be pleasant, my fine friend.");
+				addGoodbye("May your travels be pleasant, my fine friend.");
 				addQuest("There is something i wish. But I have no time at present to discuss it. Please come back again later.");
 			}
 		};
