cce095293c6615c416c6bc39e39e0ce91a127928
Hendrik Brummermann <nhnb@users.sourceforge.net>
2008-06-14 23:13:35 +0000

    splitted logUsage into its own method

--- src/games/stendhal/server/actions/UseAction.java
+++ src/games/stendhal/server/actions/UseAction.java
@@ -12,13 +12,12 @@
  ***************************************************************************/
 package games.stendhal.server.actions;
 
+import static games.stendhal.server.actions.WellKnownActionConstants.TARGET;
 import static games.stendhal.server.actions.WellKnownActionConstants._BASEITEM;
 import static games.stendhal.server.actions.WellKnownActionConstants._BASEOBJECT;
 import static games.stendhal.server.actions.WellKnownActionConstants._BASESLOT;
-import static games.stendhal.server.actions.WellKnownActionConstants.TARGET;
 import static games.stendhal.server.actions.WellKnownActionConstants._USE;
 import games.stendhal.server.core.engine.SingletonRepository;
-import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.events.UseListener;
 import games.stendhal.server.entity.Entity;
 import games.stendhal.server.entity.item.Corpse;
@@ -28,7 +27,6 @@ import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.util.EntityHelper;
 import marauroa.common.game.RPAction;
 import marauroa.common.game.RPObject;
-import marauroa.common.game.RPSlot;
 
 public class UseAction implements ActionListener {
 
@@ -58,50 +56,14 @@ public class UseAction implements ActionListener {
 				action.get(TARGET), player);
 
 		if (entity != null) {
-			invokeUseListener(player, entity);
+			tryUse(player, entity);
 		}
 	}
 
 	private void useItemInSlot(Player player, RPAction action) {
-		StendhalRPZone zone = player.getZone();
-
-		int baseObject = action.getInt(_BASEOBJECT);
-
-		RPObject.ID baseobjectid = new RPObject.ID(baseObject, zone.getID());
-		if (!zone.has(baseobjectid)) {
-			return;
-		}
-
-		RPObject base = zone.get(baseobjectid);
-		if (!canAccessSlot(player, base)) {
-			return;
-		}
-
-		Entity baseEntity = (Entity) base;
-
-		if (baseEntity.hasSlot(action.get(_BASESLOT))) {
-			RPSlot slot = baseEntity.getSlot(action.get(_BASESLOT));
-
-			if (slot.size() == 0) {
-				return;
-			}
-
-			RPObject object = null;
-			int item = action.getInt(_BASEITEM);
-			// scan through the slot to find the requested item
-			for (RPObject rpobject : slot) {
-				if (rpobject.getID().getObjectID() == item) {
-					object = rpobject;
-					break;
-				}
-			}
-
-			// no item found...we take the first one
-			if (object == null) {
-				object = slot.iterator().next();
-			}
-
-			invokeUseListener(player, object);
+		Entity object = EntityHelper.entityFromSlot(player, action);
+		if ((object != null) && canAccessSlot(player, object)) {
+			tryUse(player, object);
 		}
 	}
 
@@ -120,7 +82,7 @@ public class UseAction implements ActionListener {
 		return true;
 	}
 
-	private void invokeUseListener(Player player, RPObject object) {
+	private void tryUse(Player player, RPObject object) {
 
 		// HACK: No item transfer in jail (we don't want a jailed player to
 		// use items like home scroll.
@@ -131,23 +93,13 @@ public class UseAction implements ActionListener {
 			return;
 		}
 
-		String name = object.get("type");
-		if (object.has("name")) {
-			name = object.get("name");
-		}
-		String infostring = "";
-		if (object.has("infostring")) {
-			infostring = object.get("infostring");
-		}
-
-		SingletonRepository.getRuleProcessor().addGameEvent(player.getName(), _USE,
-				name, infostring);
+		logUsage(player, object);
 
 		if (object instanceof UseListener) {
 			UseListener listener = (UseListener) object;
 
 			// Make sure nobody uses items bound to someone else.
-			if (listener instanceof Item) {
+			if (object instanceof Item) {
 				Item item = (Item) listener;
 				if (item.has("bound")
 						&& !item.get("bound").equals(player.getName())) {
@@ -161,5 +113,27 @@ public class UseAction implements ActionListener {
 
 			listener.onUsed(player);
 		}
+	}
+
+	/**
+	 * Logs that this entity was used.
+	 *
+	 * @param player player using the entity
+	 * @param object entity being used
+	 */
+	private void logUsage(Player player, RPObject object) {
+		String name = object.get("type");
+		if (object.has("name")) {
+			name = object.get("name");
+		}
+		String infostring = "";
+		if (object.has("infostring")) {
+			infostring = object.get("infostring");
+		}
+
+		SingletonRepository.getRuleProcessor().addGameEvent(player.getName(), _USE,
+				name, infostring);
+		
+		// TODO: log to itemlog, too?
 	}
 }
