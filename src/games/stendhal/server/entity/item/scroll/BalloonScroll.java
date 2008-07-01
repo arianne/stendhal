47eb0d8ad0557f72f46ddbdb5c3a20199cf772e1
Katie Russell <kymara@users.sourceforge.net>
2008-07-01 18:33:14 +0000

    another instance of a timed teleport scroll, this time the scroll is a balloon

---
+++ src/games/stendhal/server/entity/item/scroll/BalloonScroll.java
@@ -0,0 +1,43 @@
+package games.stendhal.server.entity.item.scroll;
+
+import java.util.Map;
+
+/**
+ * Represents the balloon that takes the player to 7 kikareukin clouds,
+ * after which it will teleport player to a random location in 6 kikareuikin islands.
+ */
+public class BalloonScroll extends TimedTeleportScroll {
+
+	/**
+	 * Creates a new timed marked BalloonScroll scroll.
+	 * 
+	 * @param name
+	 * @param clazz
+	 * @param subclass
+	 * @param attributes
+	 */
+	public BalloonScroll(String name, String clazz, String subclass,
+			Map<String, String> attributes) {
+		super(name, clazz, subclass, attributes);
+	}
+
+	/**
+	 * Copy constructor.
+	 * 
+	 * @param item
+	 *            item to copy
+	 */
+	public BalloonScroll(BalloonScroll item) {
+		super(item);
+	}
+
+	@Override
+	protected String getBeforeReturnMessage() {
+		return "It feels like the clouds won't take your weight much longer ... ";
+	}
+
+	@Override
+	protected String getAfterReturnMessage() {
+		return "You fell through a hole in the clouds, back to solid ground.";
+	}
+}
