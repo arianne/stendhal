32940ea135a89b0c5fdf8c0d3c3792a90d108fda
tigertoes <tigertoes@users.sourceforge.net>
2008-07-13 22:14:35 +0000

    basic mad scientist npcs for mithril cloak quest

---
+++ src/games/stendhal/server/maps/kalavan/castle/MadScientist1NPC.java
@@ -0,0 +1,53 @@
+package games.stendhal.server.maps.kalavan.castle;
+
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.pathfinder.FixedPath;
+import games.stendhal.server.core.pathfinder.Node;
+import games.stendhal.server.entity.npc.ConversationPhrases;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Builds a mad scientist NPC who takes your silk glands and gives them to MadScientist2. 
+ *
+ * @author kymara with modifications by tigertoes
+ */
+public class MadScientist1NPC implements ZoneConfigurator {
+
+	/**
+	 * Configure a zone.
+	 *
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		buildNPC(zone);
+	}
+
+	private void buildNPC(final StendhalRPZone zone) {
+		final SpeakerNPC npc = new SpeakerNPC("Vincento Price") {
+
+			@Override
+			protected void createPath() {
+				setPath(null);
+			}
+
+			@Override
+			    protected void createDialog() {
+				add(ConversationStates.IDLE, ConversationPhrases.GREETING_MESSAGES, null, ConversationStates.IDLE, "Ha ha he he woo hoo!!!", null);
+	 	     }
+		    
+		};
+
+		npc.setDescription("You see someone that is somewhat strange. Perhaps you shouldn't bother him?");
+		npc.setEntityClass("madscientistnpc");
+		npc.setPosition(18, 84);
+		npc.initHP(100);
+		zone.add(npc);
+	}
+}
