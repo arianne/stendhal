84ba4c54241c027e5b2488f3ab700960fec20ba0
Hendrik Brummermann <nhnb@users.sourceforge.net>
2008-07-20 14:55:24 +0000

    mock classes to test reflection

---
+++ tests/games/stendhal/server/core/reflectiondebugger/MockChildClass.java
@@ -0,0 +1,11 @@
+package games.stendhal.server.core.reflectiondebugger;
+
+/**
+ * This class is used to test the reflection code.
+ *
+ * @author hendrik
+ */
+public class MockChildClass extends MockParentClass {
+	private float childPrivateFloat = 2.0f;
+	public boolean childPublicBoolean = true;
+}
---
+++ tests/games/stendhal/server/core/reflectiondebugger/MockParentClass.java
@@ -0,0 +1,11 @@
+package games.stendhal.server.core.reflectiondebugger;
+
+/**
+ * This class is used to test the reflection code.
+ *
+ * @author hendrik
+ */
+public class MockParentClass {
+	private int parentPrivateInt = 1;
+	public String parentPublicString = "text";
+}
