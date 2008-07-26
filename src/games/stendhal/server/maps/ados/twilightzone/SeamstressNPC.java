a50381a2b0f1c68ee436314479908d39e19d0410
Katie Russell <kymara@users.sourceforge.net>
2008-07-26 17:28:42 +0000

    add twilight zone which is like ida sewing room but dirty colours effect achieved with haze. make twilight moss send there. add lda npc who is sick (responses are just put in for now and can be changed later to what the quest needs since i don't know what that is)

---
+++ src/games/stendhal/server/maps/ados/twilightzone/SeamstressNPC.java
@@ -0,0 +1,65 @@
+package games.stendhal.server.maps.ados.twilightzone;
+
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.pathfinder.FixedPath;
+import games.stendhal.server.core.pathfinder.Node;
+import games.stendhal.server.entity.npc.ShopList;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.behaviour.adder.BuyerAdder;
+import games.stendhal.server.entity.npc.behaviour.impl.BuyerBehaviour;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/*
+ * Twilight zone is a copy of sewing room in dirty colours with a delirious sick lda (like Ida) in it
+ */
+public class SeamstressNPC implements ZoneConfigurator {
+	private final ShopList shops = SingletonRepository.getShopList();
+
+	/**
+	 * Configure a zone.
+	 *
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		buildSeamstress(zone);
+	}
+
+	private void buildSeamstress(final StendhalRPZone zone) {
+		final SpeakerNPC seamstress = new SpeakerNPC("lda") {
+
+			@Override
+			protected void createPath() {
+				final List<Node> nodes = new LinkedList<Node>();
+				nodes.add(new Node(7, 7));
+				nodes.add(new Node(7, 14));
+				nodes.add(new Node(12, 14));
+				nodes.add(new Node(12, 7));
+				setPath(new FixedPath(nodes, true));
+			}
+
+			@Override
+			protected void createDialog() {
+				addGreeting("Help me, I'm delirious.");
+				addJob("I thought I was a sewing woman .. but that seems like another life now.");
+				addHelp("I don't know what I need."); 
+				addOffer("I'm too confused, I can't do anything...");
+				addGoodbye("Won't you stay and comfort me ...");
+			}
+		};
+		// see through
+		seamstress.setDescription("You see Ida, she looks sick and feverish");
+		seamstress.setVisibility(70);
+		// walk through
+		seamstress.setResistance(0);
+		seamstress.setEntityClass("woman_002_npc");
+		seamstress.setPosition(7, 7);
+		seamstress.initHP(40);
+		zone.add(seamstress);
+	}
+}
