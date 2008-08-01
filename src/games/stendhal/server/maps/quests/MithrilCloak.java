e148696cdff80f4ff60136446902f753cb6219c8
Katie Russell <kymara@users.sourceforge.net>
2008-08-01 12:57:09 +0000

    don't let kampusch remove any items for the mithril thread till you have them all

--- src/games/stendhal/server/maps/quests/MithrilCloak.java
+++ src/games/stendhal/server/maps/quests/MithrilCloak.java
@@ -410,7 +410,7 @@ public class MithrilCloak extends AbstractQuest {
 							
     						if (found) {
 								if (behaviour.getAmount() < 40){
-									npc.say("Do you really want so few? I'm not wasting my time with that! Any decent sized pieces of fabric needs at least 40 spools of thread!");
+									npc.say("Do you really want so few? I'm not wasting my time with that! Any decent sized pieces of fabric needs at least 40 spools of thread! You should at least #make #40.");
 									return;
 								}
     							if (behaviour.getAmount() > 1000) {
@@ -549,7 +549,7 @@ public class MithrilCloak extends AbstractQuest {
 		npc.addReply("balloon","Ah! They are dropped by the charming little baby angels who dwell in Kikareukin Islands. I want one for my daughter.");
 		npc.addReply("silk thread","That is from the silk glands of giant spiders. You need 40 spools of silk thread to make something as large as a cloak, say.");
 		npc.addReply("silk","That is from the silk glands of giant spiders.");
-		npc.addReply("mithril nuggets","You can find them for yourself.");
+i		npc.addReply("mithril nuggets","You can find them for yourself.");
 		npc.addReply("Whiggins","Find the wizard Whiggins inside his house in the magic city.");
 		npc.addReply("scientists","I hear of experiments deep in Kalavan Castle. The scientists are a crazy bunch, but look for the lead researcher, Vincento Price, he may be sane enough to help you.");
 
@@ -565,10 +565,12 @@ public class MithrilCloak extends AbstractQuest {
 			new SpeakerNPC.ChatAction() {
 				@Override
 				public void fire(final Player player, final Sentence sentence, final SpeakerNPC npc) {
-					if (player.drop("silk thread", 40)
-						&& player.drop("mithril nugget", 7)
-						&& player.drop("balloon")) {
-						
+					if (player.isEquipped("silk thread", 40)
+						&& player.isEquipped("mithril nugget", 7)
+						&& player.isEquipped("balloon")) {
+						player.drop("silk thread", 40);
+					    player.drop("mithril nugget", 7);
+						player.drop("balloon");
 						final long timeNow = new Date().getTime();
 						player.setQuest(QUEST_SLOT, "fusingthread;" + timeNow);
 						npc.say("I will fuse 40 mithril thread for you. Please come back in "
