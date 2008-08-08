d9a250386418b3d6f4b015193eefb22963b78e73
Katie Russell <kymara@users.sourceforge.net>
2008-08-08 10:28:52 +0000

    update version number to 0.69 which means **you will need to update your scripts**

--- src/games/stendhal/client/update/Version.java
+++ src/games/stendhal/client/update/Version.java
@@ -10,7 +10,7 @@ package games.stendhal.client.update;
 public class Version {
 
 	/** Version Number. */
-	public static final String VERSION = "0.68.1";
+	public static final String VERSION = "0.69";
 
 	/**
 	 * Extract the specified number of parts from a version-string.
