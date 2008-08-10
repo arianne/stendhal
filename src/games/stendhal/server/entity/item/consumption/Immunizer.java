75c61a7eab3a47caa90a2f88012720a4c18c2596
Katie Russell <kymara@users.sourceforge.net>
2008-08-10 20:53:32 +0000

    apply patch [ 2043475 ] Bug 2018537 antidote messages, provided by madmetzger

--- src/games/stendhal/server/entity/item/consumption/Immunizer.java
+++ src/games/stendhal/server/entity/item/consumption/Immunizer.java
@@ -1,5 +1,6 @@
 package games.stendhal.server.entity.item.consumption;
 
+import games.stendhal.common.NotificationType;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.events.TurnListener;
 import games.stendhal.server.core.events.TurnNotifier;
@@ -9,16 +10,21 @@ import games.stendhal.server.entity.player.Player;
 class Immunizer implements Feeder {
 
 	public boolean feed(final ConsumableItem item, final Player player) {
-		player.setImmune();
-		// set a timer to remove the immunity effect after some time
-		final TurnNotifier notifier = SingletonRepository.getTurnNotifier();
-		// first remove all effects from previously used immunities to
-		// restart the timer
-		final TurnListener tl = new AntidoteEater(player);
-		notifier.dontNotify(tl);
-		notifier.notifyInTurns(item.getAmount(), tl);
-		item.removeOne();
-		return true;
+		if(!player.isImmune()) {
+			player.setImmune();
+			// set a timer to remove the immunity effect after some time
+			final TurnNotifier notifier = SingletonRepository.getTurnNotifier();
+			// first remove all effects from previously used immunities to
+			// restart the timer
+			final TurnListener tl = new AntidoteEater(player);
+			notifier.dontNotify(tl);
+			notifier.notifyInTurns(item.getAmount(), tl);
+			item.removeOne();
+			return true;
+		} else {
+			player.sendPrivateText(NotificationType.INFORMATION,"Antidote was not consumed, because you are still immune.");
+			return false;
+		}
 	}
 
 }
