e464b2292a42f15e6dd2b712b4ed1cbd894c181f
Hendrik Brummermann <nhnb@users.sourceforge.net>
2008-08-14 16:43:54 +0000

    log errors to logfile

--- src/games/stendhal/server/actions/admin/BanAction.java
+++ src/games/stendhal/server/actions/admin/BanAction.java
@@ -1,16 +1,18 @@
 package games.stendhal.server.actions.admin;
 
-import java.sql.SQLException;
-
-import org.apache.log4j.Logger;
-
 import games.stendhal.server.actions.CommandCenter;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.entity.player.Player;
+
+import java.sql.SQLException;
+
 import marauroa.common.game.RPAction;
 import marauroa.server.game.db.IDatabase;
 
+import org.apache.log4j.Logger;
+
 public class BanAction extends AdministrationAction {
+	private static Logger logger = Logger.getLogger(BanAction.class);
 
 	@Override
 	protected void perform(final Player player, final RPAction action) {
@@ -24,7 +26,7 @@ public class BanAction extends AdministrationAction {
 				player.sendPrivateText("you have banned " + bannedName);
 				Logger.getLogger(BanAction.class).info(player.getName() + " has banned " + bannedName);
 			} catch (SQLException e) {
-				e.printStackTrace();
+				logger.error("Error while trying to ban user", e);
 			}
 		}
 
