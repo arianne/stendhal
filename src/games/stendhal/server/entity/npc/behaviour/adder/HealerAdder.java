2b76fa6b1268ba7a641670cd208b5283cce82089
Katie Russell <kymara@users.sourceforge.net>
2008-08-20 13:30:18 +0000

    Healers won't heal player killers (even those who charge for healing)

--- src/games/stendhal/server/entity/npc/behaviour/adder/HealerAdder.java
+++ src/games/stendhal/server/entity/npc/behaviour/adder/HealerAdder.java
@@ -28,24 +28,29 @@ public class HealerAdder {
 						healerBehaviour.setChosenItemName("heal");
 						healerBehaviour.setAmount(1);
 						final int cost = healerBehaviour.getCharge(engine, player);
-
-						if (cost > 0) {
-							engine.say("Healing costs " + cost
-									+ ". Do you have that much?");
-						} else {
-							if ((player.getATK() > 35) || (player.getDEF() > 35)) {
-								engine.say("Sorry, I cannot heal you because you are way too strong for my limited powers");
-							} else if (!player.isNew()
-									&& (player.getLastPVPActionTime() > System.currentTimeMillis()
-											- 2 * MathHelper.MILLISECONDS_IN_ONE_HOUR)) {
-								// ignore the PVP flag for very young characters
-								// (low atk, low def AND low level)
-								engine.say("Sorry, but you have a bad aura, so that I am unable to heal you right now.");
-							} else {
-								engine.say("There, you are healed. How else may I help you?");
-								healerBehaviour.heal(player);
-							}
+						if( player.isBadBoy() ) {
+							// don't heal player killers at all
+							engine.say("You killed another soul recently, giving you an aura of evil. I cannot, and will not, heal you.");
 							engine.setCurrentState(ConversationStates.ATTENDING);
+						} else {
+							if (cost > 0) {
+								engine.say("Healing costs " + cost
+									+ ". Do you have that much?");
+							} else {
+								if ((player.getATK() > 35) || (player.getDEF() > 35)) {
+									engine.say("Sorry, I cannot heal you because you are way too strong for my limited powers");
+								} else if (!player.isNew()
+										   && (player.getLastPVPActionTime() > System.currentTimeMillis()
+											- 2 * MathHelper.MILLISECONDS_IN_ONE_HOUR)) {
+									// ignore the PVP flag for very young characters
+									// (low atk, low def AND low level)
+									engine.say("Sorry, but you have a bad aura, so that I am unable to heal you right now.");
+								} else {
+									engine.say("There, you are healed. How else may I help you?");
+									healerBehaviour.heal(player);
+								}
+								engine.setCurrentState(ConversationStates.ATTENDING);
+							}
 						}
 					}
 				});
