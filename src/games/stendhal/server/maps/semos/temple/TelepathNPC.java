0c21197fabd150cd01caab043f169e0339ec4940
Katie Russell <kymara@users.sourceforge.net>
2008-08-21 20:51:48 +0000

    Io Flotto will notice if PK-er comes ot her and offers to remove it. if they have 1. not done pvp recently, 2. not killed player recently (longer) and 3. good karma (>= 5) then she wil remove it provided that they confirm they are sorry. the numbers for timing can be changed, and extra conditions added, if required.

--- src/games/stendhal/server/maps/semos/temple/TelepathNPC.java
+++ src/games/stendhal/server/maps/semos/temple/TelepathNPC.java
@@ -1,5 +1,6 @@
 package games.stendhal.server.maps.semos.temple;
 
+import games.stendhal.common.MathHelper;
 import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.pathfinder.FixedPath;
@@ -10,6 +11,7 @@ import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.parser.Sentence;
 import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.util.TimeUtil;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -55,8 +57,14 @@ public class TelepathNPC implements ZoneConfigurator {
 
 					        public void fire(final Player player, final Sentence sentence, final SpeakerNPC engine) {
 						        if (player.hasQuest("meet_io")) {
-							        engine.say("Hi again, " + player.getTitle()
+									if(player.isBadBoy()) {
+										// notice pk icon 
+										engine.say("Hi again, " + player.getTitle() + ". I sense have been branded with the mark of a killer. Do you wish to have it removed?");
+										engine.setCurrentState(ConversationStates.QUESTION_1);
+									} else {
+										engine.say("Hi again, " + player.getTitle()
 							                + ". How can I #help you this time? Not that I don't already know...");
+									}
 						        } else {
 							        engine
 							                .say("I awaited you, "
@@ -66,10 +74,43 @@ public class TelepathNPC implements ZoneConfigurator {
 						        }
 					        }
 				        });
+				add(ConversationStates.QUESTION_1, ConversationPhrases.YES_MESSAGES, null, ConversationStates.ATTENDING,
+				        null, new ChatAction() {
+
+					        public void fire(final Player player, final Sentence sentence, final SpeakerNPC engine) {
+						       	if ((player.getLastPVPActionTime() > System.currentTimeMillis()
+											- MathHelper.MILLISECONDS_IN_ONE_WEEK)) {
+									// player attacked another within the last week
+									engine.say("You will have to abstain from even attacking other people for a full week. And remember, I will know if you even think of doing it!");
+								} else if ((player.getLastPlayerKillTime() > System.currentTimeMillis()
+											- 3 * MathHelper.MILLISECONDS_IN_ONE_WEEK)) {
+									// player did a player kill within the last 3 weeks
+									long timeRemaining = player.getLastPlayerKillTime() - System.currentTimeMillis() 
+										+ 3 * MathHelper.MILLISECONDS_IN_ONE_WEEK;
+									engine.say("Your last victim hasn't forgotten that you killed them, and neither have I. Come back in " 
+											   + TimeUtil.approxTimeUntil((int) (timeRemaining / 1000L)) + ", and we will see how sorry you are.");
+								} else if (player.getKarma() < 5) {
+									// player does not have much good karma
+									engine.say("They say what goes around, comes around. A good thing will happen for you when you have good karma again. Which means you, in turn, must do a good deed for someone else. Come back when your karma is better.");
+								} else {
+									// player has fulfilled all requirements to be rehabilitated
+									engine.say("Are you really sorry for what you did?");
+									engine.setCurrentState(ConversationStates.QUESTION_2);
+								}
+							}});
+				// player didn't want pk icon removed, offer other help
+				add(ConversationStates.QUESTION_1, ConversationPhrases.NO_MESSAGES, null, ConversationStates.ATTENDING, "Fine! I can still #help you in other ways if you need it.", null);
+				// player satisfied the pk removal requirements and said yes they were sorry
+				add(ConversationStates.QUESTION_2, ConversationPhrases.YES_MESSAGES, null, ConversationStates.ATTENDING,
+				        "Good, I knew you were.", new ChatAction() {
+
+					        public void fire(final Player player, final Sentence sentence, final SpeakerNPC engine) {
+								player.rehabilitate(); 	
+							}});
+				// player said no they are not really sorry
+				add(ConversationStates.QUESTION_2, ConversationPhrases.NO_MESSAGES, null, ConversationStates.IDLE, "I thought not! Good bye!", null);
 				addJob("I am committed to harnessing the total power of the human mind. I have already made great advances in telepathy and telekinesis; however, I can't yet foresee the future, so I don't know if we will truly be able to destroy Blordrough's dark legion...");
-				addReply(
-				        ConversationPhrases.QUEST_MESSAGES,
-				        "Well, there's not really much that I need anyone to do for me right now. And I... Hey! Were you just trying to read my private thoughts? You should always ask permission before doing that!");
+				addQuest("Well, there's not really much that I need anyone to do for me right now. And I... Hey! Were you just trying to read my private thoughts? You should always ask permission before doing that!");
 				addGoodbye();
 				// further behaviour is defined in the MeetIo quest.
 			}
