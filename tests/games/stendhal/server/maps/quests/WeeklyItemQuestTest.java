e7f32a87af1a2009a9d1049ecf9b825069db38df
Markus <madmetzger@users.sourceforge.net>
2008-08-28 20:26:51 +0000

    assertion changed to react on "7 days" or "1 week"

--- tests/games/stendhal/server/maps/quests/WeeklyItemQuestTest.java
+++ tests/games/stendhal/server/maps/quests/WeeklyItemQuestTest.java
@@ -1,6 +1,7 @@
 package games.stendhal.server.maps.quests;
 
 import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.isOneOf;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
@@ -85,7 +86,7 @@ public class WeeklyItemQuestTest {
 		en.step(player, "hi");
 		assertEquals("Welcome to Kirdneh Museum.", npc.get("text"));
 		en.step(player, "task");
-		assertEquals("The museum can only afford to send you to fetch an item once a week. Please check back in 7 days.", npc.get("text"));
+		assertThat(npc.get("text"),isOneOf("The museum can only afford to send you to fetch an item once a week. Please check back in 7 days.","The museum can only afford to send you to fetch an item once a week. Please check back in 1 week."));
 		en.step(player, "bye");
 		assertEquals("Good bye, it was pleasant talking with you.", npc.get("text"));
 
