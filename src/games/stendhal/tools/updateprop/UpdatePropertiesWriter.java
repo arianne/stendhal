f32c498aea18a0f9b10eee89e1f03575ee5f79a4
Hendrik Brummermann <nhnb@users.sourceforge.net>
2008-09-03 21:12:33 +0000

    completly implemented and useable (although fileSize could be sorted better)

--- src/games/stendhal/tools/updateprop/UpdatePropertiesWriter.java
+++ src/games/stendhal/tools/updateprop/UpdatePropertiesWriter.java
@@ -60,48 +60,24 @@ public class UpdatePropertiesWriter {
 	}
 
 	private void destination() {
-		// TODO Auto-generated method stub
-		
-/*
-# new version after update to calculate multiple updates in a row
-#version.destination.0.66=0.67
- */
+		ps.println("# new version after update to calculate multiple updates in a row");
+		writeKeysWithPrefix("version.destination");
 	}
 	
 
 	private void init() {
-		// TODO Auto-generated method stub
-
-/*
-		# files to download on first install
-		init.file-list=log4j.jar,marauroa-2.5.jar,stendhal-data-0.69.jar,stendhal-0.69.jar
-		init.version=0.69
-*/
+		ps.println("# files to download on first install");
+		writeKeysWithPrefix("init.");
 	}
 
 	private void updateFileList() {
-		// TODO Auto-generated method stub
-
-/*
-		# files to download on update from this version
-		#update-file-list.0.67=stendhal-diff-0.67-0.68.jar,stendhal-data-diff-0.67-0.68.jar,marauroa-2.5.jar,log4j.jar
-*/
+		ps.println("# files to download on update from this version");
+		writeKeysWithPrefix("update-file-list.");
 	}
 
 	private void fileSize() {
-		// TODO Auto-generated method stub
-		
-/*
-# size of complete files
-file-size.log4j.jar=209693
-file-size.marauroa-2.5.jar=245198
-
-file-size.stendhal-0.69.jar=559913
-file-size.stendhal-data-0.69.jar=18336725
-
-# size of update files
-#file-size.stendhal-diff-0.68.1-0.69.jar=428828
-#file-size.stendhal-data-diff-0.68.1-0.69.jar=1010978
- */
+		ps.println("# size of complete files");
+		writeKeysWithPrefix("file-size.");
+		// TODO: Sort this in a nicer way
 	}
 }
