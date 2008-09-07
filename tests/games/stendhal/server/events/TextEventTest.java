98a0d4d1d4f68b7eb3ce07b0944793c18f751d2e
Astrid Sawatzky <astridemma@users.sourceforge.net>
2008-09-07 09:33:35 +0000

    adding test for generateRPClass.

--- tests/games/stendhal/server/events/TextEventTest.java
+++ tests/games/stendhal/server/events/TextEventTest.java
@@ -1,18 +1,30 @@
 package games.stendhal.server.events;
 
+import marauroa.common.game.RPClass;
+
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class TextEventTest {
 
 	@Test
-	public void testTextEvent () {
+	public void testTextEvent() {
 		TextEvent event = new TextEvent("text");
-		assertThat(event,is(TextEvent.class));
-		assertThat(event.get("text"),is("text"));
+		assertThat(event, is(TextEvent.class));
+		assertThat(event.get("text"), is("text"));
 	}
+
+	public void testGenerateRPClass() {
+		assertFalse(RPClass.hasRPClass("text"));
+		TextEvent.generateRPClass();
+		assertTrue(RPClass.hasRPClass("text"));
+	}
+	
+	
 
 }
