0baa443a16a65a3fb7c0692d1ef5aec0c39c40d2
Astrid Sawatzky <astridemma@users.sourceforge.net>
2008-09-20 16:51:21 +0000

    removed unused import

--- tests/games/stendhal/server/entity/player/PlayerTest.java
+++ tests/games/stendhal/server/entity/player/PlayerTest.java
@@ -16,7 +16,6 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import utilities.PlayerTestHelper;
