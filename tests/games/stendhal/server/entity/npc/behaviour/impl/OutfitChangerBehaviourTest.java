453f121687e822ab4834a2186c58e7724e979c6f
Astrid Sawatzky <astridemma@users.sourceforge.net>
2008-11-03 15:05:21 +0000

    regression test for broken equals

---
+++ tests/games/stendhal/server/entity/npc/behaviour/impl/OutfitChangerBehaviourTest.java
@@ -0,0 +1,35 @@
+package games.stendhal.server.entity.npc.behaviour.impl;
+
+import static org.junit.Assert.*;
+
+import games.stendhal.server.entity.npc.behaviour.impl.OutfitChangerBehaviour.OutwearClothes;
+import games.stendhal.server.entity.player.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import utilities.PlayerTestHelper;
+
+public class OutfitChangerBehaviourTest {
+
+	@Test
+	public void testOnWornOff() {
+		Map<String, Integer> pricelist = new HashMap<String, Integer>();
+		pricelist.put("trunks", Integer.valueOf(50));
+		OutfitChangerBehaviour beh = new OutfitChangerBehaviour(pricelist);
+		Player player = PlayerTestHelper.createPlayer("bob");
+		OutwearClothes cloth = beh.new OutwearClothes(player);
+		OutwearClothes cloth2 = beh.new OutwearClothes(player);
+		assertTrue(cloth.equals(cloth2));
+		assertTrue(cloth2.equals(cloth));
+		
+		OutwearClothes cloth3 = beh.new OutwearClothes(PlayerTestHelper.createPlayer("bob"));
+		
+		assertTrue(cloth.equals(cloth3));
+		assertTrue(cloth3.equals(cloth));
+		
+	}
+
+}
