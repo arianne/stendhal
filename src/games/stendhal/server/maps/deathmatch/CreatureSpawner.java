72894cd4b1b7ea74cf98040b6242cc0704b14d89
Katie Russell <kymara@users.sourceforge.net>
2009-02-23 22:15:36 +0000

    make the constructor and a method visible so that it can be used outside

--- src/games/stendhal/server/maps/deathmatch/CreatureSpawner.java
+++ src/games/stendhal/server/maps/deathmatch/CreatureSpawner.java
@@ -39,7 +39,7 @@ public class CreatureSpawner implements TurnListener {
 	/**
 	 * Creates a new CreatureSpawner
 	 */
-	CreatureSpawner() {
+	public CreatureSpawner() {
 		final Collection<Creature> creatures = SingletonRepository.getEntityManager().getCreatures();
 		for (Creature creature : creatures) {
 			if (!creature.isRare()) {
@@ -106,7 +106,7 @@ public class CreatureSpawner implements TurnListener {
 	 * @param questLevel level of creature / deathmatch status
 	 * @return creature template
 	 */
-	private Creature calculateNextCreature(final int questLevel) {
+	public Creature calculateNextCreature(final int questLevel) {
 		final List<Creature> possibleCreaturesToSpawn = new ArrayList<Creature>();
 		int lastLevel = 0;
 
