f2b0b75ff0c2ef0471c0f08162f56d8a40805a26
Katie Russell <kymara@users.sourceforge.net>
2009-02-24 22:56:34 +0000

    Make the fee charged for going to the island be dependent on player level. Try to stop the TurnNotifier on any corpses in the island before we destroy the zone.

--- src/games/stendhal/server/maps/magic/house1/ChallengerNPC.java
+++ src/games/stendhal/server/maps/magic/house1/ChallengerNPC.java
@@ -5,6 +5,8 @@ import games.stendhal.common.Rand;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.events.MovementListener;
+import games.stendhal.server.core.events.TurnListener;
+import games.stendhal.server.core.events.TurnNotifier;
 import games.stendhal.server.core.rp.StendhalRPAction;
 import games.stendhal.server.entity.ActiveEntity;
 import games.stendhal.server.entity.creature.Creature;
@@ -20,7 +22,6 @@ import games.stendhal.server.entity.npc.condition.AndCondition;
 import games.stendhal.server.entity.npc.condition.LevelGreaterThanCondition;
 import games.stendhal.server.entity.npc.condition.LevelLessThanCondition;
 import games.stendhal.server.entity.npc.condition.NotCondition;
-import games.stendhal.server.entity.npc.condition.PlayerHasItemWithHimCondition;
 import games.stendhal.server.entity.npc.condition.TimePassedCondition;
 import games.stendhal.server.entity.npc.parser.Sentence;
 import games.stendhal.server.entity.player.Player;
@@ -30,6 +31,8 @@ import games.stendhal.server.maps.deathmatch.Spot;
 import java.awt.geom.Rectangle2D;
 import java.util.Arrays;
 
+import marauroa.common.game.RPObject; 
+
 import org.apache.log4j.Logger;
 
 public class ChallengerNPC extends SpeakerNPCFactory {
@@ -37,8 +40,8 @@ public class ChallengerNPC extends SpeakerNPCFactory {
  private static final int NUMBER_OF_CREATURES = 5;
  /** lowest level allowed to island*/
  private static final int MIN_LEVEL = 50;
- /** Cost of getting to island */
- private static final int COST = 50000;
+ /** Cost multiplier for getting to island */
+ private static final int COST_FACTOR = 300;
  /** island coordinates for placing monsters */
  private static final int MIN_X = 10;
  /** island coordinates for placing monsters */
@@ -81,11 +84,13 @@ public class ChallengerNPC extends SpeakerNPCFactory {
 			    	entity.put("zoneid","int_magic_house1");
 					entity.put("x","12");
 					entity.put("y","3");
-					// TODO: iterate through all items left in the zone and for the listeners, call
-					/*if (inspected instanceof TurnListener) {
-						*TurnListener listener = (TurnListener) inspected;
-						*TurnNotifier.get().dontNotify(listener);
-					}*/
+					// iterate through all items left in the zone and for the listeners, stop them listening before we remove the zone
+					for (RPObject inspected : zone) {				
+						if (inspected instanceof TurnListener) {
+							TurnListener listener = (TurnListener) inspected;
+							TurnNotifier.get().dontNotify(listener);
+						}
+					}
 					SingletonRepository.getRPWorld().removeZone(zone);
 
 			    }
@@ -98,6 +103,11 @@ public class ChallengerNPC extends SpeakerNPCFactory {
 		}
 
 		public void fire(final Player player, final Sentence sentence, final SpeakerNPC npc) {
+			int cost =  (int)  COST_FACTOR*player.getLevel();
+			if (!player.isEquipped("money", cost)) {
+				npc.say("You don't have enough money with you, the fee at your level is " + cost + " money.");
+				return;
+			}
 			final StendhalRPZone Challengezone = (StendhalRPZone) SingletonRepository
 					.getRPWorld().getRPZone("int_adventure_island");
 			String zoneName = player.getName() + "_adventure_island";
@@ -122,45 +132,53 @@ public class ChallengerNPC extends SpeakerNPCFactory {
 					count++;	
 				}
 			}
+			String message;
 			if (count>=20) {
-				// consider refunding player some of the money :P or at least telling them
+				// if we didn't manage to spawn NUMBER_OF_CREATURES they get a reduction
+				cost =  cost*(i/NUMBER_OF_CREATURES);
+				message = "Haastaja bellows from below: I could only fit " + i + " creatures on the island for you. You have therefore been charged less, a fee of only " + cost + " money. Good luck.";
 				logger.info("Tried too many times to place creatures in adventure island so less than the required number have been spawned");
+			} else { 
+				message = "Haastaja bellows from below: I took the fee of " + cost + " money. Good luck and remember to be careful with your items, as if you place them on the ground and then leave, they are lost. Most of all, take care with your life.";
 			}
 			zone.disallowIn();
 			SingletonRepository.getRPWorld().addRPZone(zone);
-			player.drop("money", COST);
+			player.drop("money", cost);
 			player.setQuest(QUEST_SLOT, Long.toString(System.currentTimeMillis()));
 			player.teleport(zone, 4, 6, Direction.DOWN, player);
-			
+			// send the text afetr we change zone so that the event is not lost on zone change
+			player.sendPrivateText(message);
+			player.notifyWorldAboutChanges();
 		}
 	}
 
 	@Override
 	public void createDialog(final SpeakerNPC npc) {
 		npc.addGreeting("And so, the hero has come.");
-		npc.addQuest("Pay the fee of " + COST + " money and you can #fight my trained magical creatures. There will be " + NUMBER_OF_CREATURES + " in all, at a level to challenge you. Your life force is the power holding the spell - if you die, the island and your corpse and items are all lost." );
-		npc.addHelp("If you are strong enough and will pay, you can #fight " + NUMBER_OF_CREATURES + " of my animals on a private adventure island. I summon it magically and it is held there by your energy; if you die, it vanishes and your corpse and items will be lost there, forever.");
+		npc.addQuest("Pay the #fee and you can #fight my trained magical creatures. There will be " + NUMBER_OF_CREATURES + " in all, at a level to challenge you. Your life force is the power holding the spell - if you die, the island and your corpse and items are all lost." );
+		npc.addHelp("If you are strong enough and will pay the #fee, you can #fight " + NUMBER_OF_CREATURES + " of my animals on a private adventure island. I summon it magically and it is held there by your energy; if you die, it vanishes and your corpse and items will be lost there, forever.");
 		npc.addJob("I train magical animals for fighting and offer warriors the chance to #battle against them on a magical island.");
 		npc.addOffer("To fight against " + NUMBER_OF_CREATURES + " of my trained creatures, chosen for your level, make the #challenge. Just know that if you die inside while on the island your corpse and items are lost forever.");		
 		npc.addGoodbye("Bye.");
+		npc.add(ConversationStates.ANY,"fee",new LevelGreaterThanCondition(MIN_LEVEL - 1), ConversationStates.ATTENDING, null,
+				new ChatAction() {
+				public void fire(final Player player, final Sentence sentence, final SpeakerNPC npc) {
+						npc.say("The fee is your current level, multiplied by " + COST_FACTOR + " and payable in cash. At your level of " + player.getLevel() + " the fee is " + COST_FACTOR*player.getLevel() + " money.");			
+				}
+		});
+			
 		// player meets conditions, first remind them of the dangers and wait for a 'yes'
-		npc.add(ConversationStates.ANY, Arrays.asList("challenge", "fight", "battle"), new AndCondition(new LevelGreaterThanCondition(MIN_LEVEL - 1),new PlayerHasItemWithHimCondition("money", COST), new TimePassedCondition(QUEST_SLOT, DAYS_BEFORE_REPEAT*24*60)) , ConversationStates.QUEST_OFFERED, 
+		npc.add(ConversationStates.ANY, Arrays.asList("challenge", "fight", "battle"), new AndCondition(new LevelGreaterThanCondition(MIN_LEVEL - 1), new TimePassedCondition(QUEST_SLOT, DAYS_BEFORE_REPEAT*24*60)) , ConversationStates.QUEST_OFFERED, 
 				"I accept your challenge. Remember if you die inside you CANNOT return to your corpse to retrieve any items you lost. Likewise if you leave and have left items on the ground, you lose them. Are you still sure you want to enter the adventure island?", 
-				null);
-		// player doesn't have enough money
-		npc.add(ConversationStates.ANY, Arrays.asList("challenge", "fight", "battle"), new NotCondition(new PlayerHasItemWithHimCondition("money", COST)), ConversationStates.ATTENDING, "You don't have the fee, it is " + COST + " money.", 
 				null);
 		// player returns within DAYS_BEFORE_REPEAT days
 		npc.add(ConversationStates.ANY, Arrays.asList("challenge", "fight", "battle"), new NotCondition(new TimePassedCondition(QUEST_SLOT, DAYS_BEFORE_REPEAT*24*60)), ConversationStates.ATTENDING, null, 
 				new StateTimeRemainingAction(QUEST_SLOT, "Your life force will not support the island so soon after you last visited. You will be ready again in", DAYS_BEFORE_REPEAT*24*60));
 		// player below MIN_LEVEL
-		npc.add(ConversationStates.ANY, Arrays.asList("challenge", "fight", "battle"), new LevelLessThanCondition(MIN_LEVEL), ConversationStates.ATTENDING, "You are too weak to fight against " + NUMBER_OF_CREATURES  + " at once. Come back when you are at least Level " + MIN_LEVEL + ".", null);
+		npc.add(ConversationStates.ANY, Arrays.asList("challenge", "fight", "battle", "fee"), new LevelLessThanCondition(MIN_LEVEL), ConversationStates.ATTENDING, "You are too weak to fight against " + NUMBER_OF_CREATURES  + " at once. Come back when you are at least Level " + MIN_LEVEL + ".", null);
 		// all conditions are met and player says yes he wants to fight
-		npc.add(ConversationStates.QUEST_OFFERED, ConversationPhrases.YES_MESSAGES, new AndCondition(new LevelGreaterThanCondition(MIN_LEVEL - 1),new PlayerHasItemWithHimCondition("money", COST)) , ConversationStates.IDLE, null, 
+		npc.add(ConversationStates.QUEST_OFFERED, ConversationPhrases.YES_MESSAGES, new LevelGreaterThanCondition(MIN_LEVEL - 1) , ConversationStates.IDLE, null, 
 				new ChallengeChatAction());
-		// incase player says challenge then drops the money before they say yes
-		npc.add(ConversationStates.QUEST_OFFERED, ConversationPhrases.YES_MESSAGES, new NotCondition(new PlayerHasItemWithHimCondition("money", COST)), ConversationStates.ATTENDING, "You don't have the fee, it is " + COST + " money.", 
-						null);
 		// player was reminded of dangers and he doesn't want to fight
 		npc.add(ConversationStates.QUEST_OFFERED, ConversationPhrases.NO_MESSAGES, null, ConversationStates.ATTENDING, "Fair enough.",null);	
 	}
