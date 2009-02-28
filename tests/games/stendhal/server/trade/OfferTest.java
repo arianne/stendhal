71b14b6b0961e96c0a715c859aeb11f39002d720
Markus <madmetzger@users.sourceforge.net>
2009-02-28 08:28:57 +0000

    restored two tests

---
+++ tests/games/stendhal/server/trade/OfferTest.java
@@ -0,0 +1,13 @@
+package games.stendhal.server.trade;
+
+import org.junit.Test;
+
+public class OfferTest {
+
+	@Test
+	public void testOffer() throws Exception {
+		
+		
+	}
+}
+
---
+++ tests/games/stendhal/server/trade/TradeTest.java
@@ -0,0 +1,103 @@
+package games.stendhal.server.trade;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.entity.item.Item;
+import games.stendhal.server.entity.item.StackableItem;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.maps.MockStendlRPWorld;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import utilities.PlayerTestHelper;
+
+public class TradeTest {
+
+	@BeforeClass
+	public static void beforeClass() {
+		MockStendlRPWorld.get();
+	}
+
+	@AfterClass
+	public static void afterClass() {
+		MockStendlRPWorld.reset();
+	}
+
+	@Test
+	public void testSuccessfullTrade() {
+		Player bob = PlayerTestHelper.createPlayer("bob");
+		Shop edeka = new Shop();
+		Item item = SingletonRepository.getEntityManager().getItem("axe");
+		StackableItem erniesMoney = (StackableItem) SingletonRepository
+				.getEntityManager().getItem("money");
+		Integer price = Integer.valueOf(10);
+		erniesMoney.setQuantity(price);
+		bob.equip(item);
+		Offer offer = edeka.createOffer(bob, item, price);
+		assertThat(offer.getItem(), is(item));
+		assertThat(offer.getPrice(), is(price));
+		assertThat(Boolean.valueOf(bob.isEquipped(item.getName())),
+				is(Boolean.FALSE));
+		Player ernie = PlayerTestHelper.createPlayer("ernie");
+		ernie.equip(erniesMoney);
+		assertThat(ernie.isEquipped("money", price), is(Boolean.TRUE));
+		edeka.acceptOffer(offer, ernie);
+		assertThat(Boolean.valueOf(ernie.isEquipped("axe")), is(Boolean.TRUE));
+		assertThat(ernie.isEquipped("money", price), is(Boolean.FALSE));
+		assertThat(Boolean.valueOf(bob.isEquipped("money")), is(Boolean.FALSE));
+		edeka.fetchEarnings(bob);
+		assertThat(Boolean.valueOf(bob.isEquipped("money", price.intValue())),
+				is(Boolean.TRUE));
+	}
+
+	@Test
+	public void testNonExistingOffer() {
+		Shop edeka = new Shop();
+		Item item = SingletonRepository.getEntityManager().getItem("axe");
+		StackableItem erniesMoney = (StackableItem) SingletonRepository
+				.getEntityManager().getItem("money");
+		Integer price = Integer.valueOf(10);
+		erniesMoney.setQuantity(price);
+		Offer offer = new Offer(item, price, "george");
+
+		Player ernie = PlayerTestHelper.createPlayer("ernie");
+		ernie.equip(erniesMoney);
+		assertThat(ernie.isEquipped("money", price), is(Boolean.TRUE));
+		edeka.acceptOffer(offer, ernie);
+		assertThat(Boolean.valueOf(ernie.isEquipped("axe")), is(Boolean.FALSE));
+		assertThat(ernie.isEquipped("money", price), is(Boolean.TRUE));
+	}
+	
+	
+	@Test
+	public void testPoorBuyer() {
+		Player bob = PlayerTestHelper.createPlayer("bob");
+		Shop edeka = new Shop();
+		Item item = SingletonRepository.getEntityManager().getItem("axe");
+		StackableItem erniesMoney = (StackableItem) SingletonRepository
+				.getEntityManager().getItem("money");
+		Integer price = Integer.valueOf(10);
+		Integer tooFewMoney = price - 1;
+		erniesMoney.setQuantity(tooFewMoney);
+		bob.equip(item);
+		Offer offer = edeka.createOffer(bob, item, price);
+
+		Player ernie = PlayerTestHelper.createPlayer("ernie");
+		ernie.equip(erniesMoney);
+		
+		assertThat(ernie.isEquipped("money", price), is(Boolean.FALSE));
+		edeka.acceptOffer(offer, ernie);
+		assertThat(Boolean.valueOf(ernie.isEquipped("axe")), is(Boolean.FALSE));
+		assertThat(ernie.isEquipped("money", tooFewMoney), is(Boolean.TRUE));
+		assertThat(Boolean.valueOf(bob.isEquipped("money")), is(Boolean.FALSE));
+		edeka.fetchEarnings(bob);
+		assertThat(Boolean.valueOf(bob.isEquipped("money", tooFewMoney.intValue())),
+				is(Boolean.FALSE));
+		assertThat(Boolean.valueOf(bob.isEquipped("money", price.intValue())),
+				is(Boolean.FALSE));
+	}
+
+}
