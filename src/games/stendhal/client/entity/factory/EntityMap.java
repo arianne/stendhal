a159d247bad2d199f12967591b4a58679bb1aa92
Astrid Sawatzky <astridemma@users.sourceforge.net>
2009-03-09 14:00:28 +0000

    let Triple have its own file

--- src/games/stendhal/client/entity/factory/EntityMap.java
+++ src/games/stendhal/client/entity/factory/EntityMap.java
@@ -12,6 +12,7 @@
 
 package games.stendhal.client.entity.factory;
 
+import games.stendhal.client.Triple;
 import games.stendhal.client.entity.Blood;
 import games.stendhal.client.entity.BossCreature;
 import games.stendhal.client.entity.Box;
@@ -46,88 +47,6 @@ import games.stendhal.client.entity.WellSource;
 import java.util.HashMap;
 import java.util.Map;
 
-class Triple<P, S, T> {
-	// they are used in equals and hashcode
-	private final P prim;
-	private final S sec;
-	private final T third;
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		int add;
-
-		if (prim == null) {
-			add = 0;
-		} else {
-			add = prim.hashCode();
-		}
-
-		result = prime * result + add;
-		if (sec == null) {
-			add = 0;
-		} else {
-			add = sec.hashCode();
-		}
-		result = prime * result + add;
-
-		if (third == null) {
-			add = 0;
-		} else {
-			add = third.hashCode();
-		}
-
-		result = prime * result + add;
-		return result;
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public boolean equals(final Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		final Triple<P, S , T> other = (Triple <P, S, T>) obj;
-		if (prim == null) {
-			if (other.prim != null) {
-				return false;
-			}
-		} else if (!prim.equals(other.prim)) {
-			return false;
-		}
-		if (sec == null) {
-			if (other.sec != null) {
-				return false;
-			}
-		} else if (!sec.equals(other.sec)) {
-			return false;
-		}
-		if (third == null) {
-			if (other.third != null) {
-				return false;
-			}
-		} else if (!third.equals(other.third)) {
-			return false;
-		}
-		return true;
-	}
-
-	public Triple(final P prim, final S sec, final T third) {
-		this.prim = prim;
-		this.sec = sec;
-		this.third = third;
-	}
-
-	
-
-}
 
 /**
  * Registers the relationship between Type, eclass and java class of entity
