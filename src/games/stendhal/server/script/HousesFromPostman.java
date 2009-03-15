cb156a8e1ed48ccba088d26b95dd1aa5d9df0650
Kimmo Rundelin <kiheru@users.sourceforge.net>
2009-03-15 22:40:14 +0000

    add a note to the house owner and presets to the chest

--- src/games/stendhal/server/script/HousesFromPostman.java
+++ src/games/stendhal/server/script/HousesFromPostman.java
@@ -3,11 +3,18 @@ package games.stendhal.server.script;
 import java.util.LinkedList;
 import java.util.List;
 
+import marauroa.common.game.RPSlot;
+
 import org.apache.log4j.Logger;
 
+import games.stendhal.common.filter.FilterCriteria;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.scripting.ScriptImpl;
+import games.stendhal.server.entity.Entity;
+import games.stendhal.server.entity.item.Item;
+import games.stendhal.server.entity.item.StackableItem;
+import games.stendhal.server.entity.mapstuff.chest.StoredChest;
 import games.stendhal.server.entity.mapstuff.portal.HousePortal;
 import games.stendhal.server.entity.mapstuff.portal.Portal;
 import games.stendhal.server.entity.player.Player;
@@ -51,8 +58,10 @@ public class HousesFromPostman extends ScriptImpl {
 				// set the tax to be paid in 10 days
 				long time = System.currentTimeMillis();
 				//  ms -> s -> min ->h -> d * 10;
-				time += 1000 * 60 * 60 * 24 * 10;
+				//time += 1000 * 60 * 60 * 24 * 10;
 				portal.setExpireTime(time);
+				
+				fillChest(findChest(portal));
 				
 				logger.debug("Updated house " + number);
 				
@@ -60,6 +69,40 @@ public class HousesFromPostman extends ScriptImpl {
 			}
 		}
 		logger.error("Failed to find house " + number);
+	}
+	
+	private StoredChest findChest(HousePortal portal) {
+		final String zoneName = portal.getDestinationZone();
+		final StendhalRPZone zone = SingletonRepository.getRPWorld().getZone(zoneName);
+		
+		final List<Entity> chests = zone.getFilteredEntities(new FilterCriteria<Entity>() {
+			public boolean passes(Entity object) {
+				return (object instanceof StoredChest);
+			}
+		});
+		
+		if (chests.size() != 1) {
+			logger.error(chests.size() + " chests in " + portal.getDoorId());
+			return null;
+		}
+		
+		return (StoredChest) chests.get(0);
+	}
+	
+	private void fillChest(StoredChest chest) {
+		Item item = SingletonRepository.getEntityManager().getItem("note");
+		item.setDescription("INFORMATION TO THE HOUSE OWNER\n"
+				+ "1. If you do not pay your house taxes, the house and all the items in the chest will be confiscated.\n"
+				+ "2. All people who can get in the house can use the chest.\n");
+		chest.add(item);
+		
+		item = SingletonRepository.getEntityManager().getItem("wine");
+		((StackableItem) item).setQuantity(2);
+		chest.add(item);
+		
+		item = SingletonRepository.getEntityManager().getItem("chocolate bar");
+		((StackableItem) item).setQuantity(2);
+		chest.add(item);
 	}
 	
 	@Override
