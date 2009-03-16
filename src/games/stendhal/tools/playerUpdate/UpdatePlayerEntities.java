d14c6c5750f6297c22b1762d6b21f5a511582fd7
Astrid Sawatzky <astridemma@users.sourceforge.net>
2009-03-16 09:06:42 +0000

    applying patch provided by madmetzger introducing class UpdatePlayerEntities. loads all player rpobject and stores them back, by which the rpobject is updated to the newest version

---
+++ src/games/stendhal/tools/playerUpdate/UpdatePlayerEntities.java
@@ -0,0 +1,44 @@
+package games.stendhal.tools.playerUpdate;
+
+import games.stendhal.server.core.engine.StendhalPlayerDatabase;
+import games.stendhal.server.entity.player.Player;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Iterator;
+
+import marauroa.common.game.RPObject;
+
+/**
+ * Loads all Players from the database, performs update operations and saves afterwards. 
+ * @author madmetzger
+ */
+public class UpdatePlayerEntities {
+
+    /**
+     * Loads a player from database.
+     * @return the player
+     */
+    public void loadAndUpdatePlayers() {
+        StendhalPlayerDatabase spdb = (StendhalPlayerDatabase) StendhalPlayerDatabase.getDatabase();
+        Iterator<RPObject> i = spdb.iterator();
+        while (i.hasNext()) {
+            Player player = Player.create(i.next());
+            try {
+                spdb.storeCharacter(spdb.getTransaction(), player.getName(), player.getName(), player);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        spdb.close();
+    }
+    
+    public static void main(final String[] args) {
+        new UpdatePlayerEntities().loadAndUpdatePlayers();
+    }
+
+}
+
+ 	  	 
