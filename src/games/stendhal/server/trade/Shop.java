6d56f8426888bb438edba705824b7f17ed02f590
Markus <madmetzger@users.sourceforge.net>
2009-04-02 15:33:08 +0000

    shop is no ZoneConfigurator anymore

--- src/games/stendhal/server/trade/Shop.java
+++ src/games/stendhal/server/trade/Shop.java
@@ -1,6 +1,5 @@
 package games.stendhal.server.trade;
 
-import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.entity.item.Item;
@@ -12,12 +11,24 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-public class Shop implements ZoneConfigurator {
+import marauroa.common.game.RPObject;
+
+public class Shop {
 	
 	// TODO Map<String, Set<Earning>> Earning knows offered item
 	private final Map<String, Integer> earnings = new HashMap<String, Integer>();
 	private final List<Offer> offers = new LinkedList<Offer>();
+	
+	public Shop(final StendhalRPZone zone) {
+		for (final RPObject item : zone) {
+			if(item.getRPClass().getName().equals("offer")) {
+				final Offer offer = (Offer) item;
+				this.offers.add(offer);
+			}
+		}
 
+	}
+	
 	public Offer createOffer(final Player offerer, final Item item,
 			final Integer money) {
 		offerer.drop(item);
@@ -28,7 +39,6 @@ public class Shop implements ZoneConfigurator {
 	}
 
 	public void acceptOffer(final Offer offer, final Player player) {
-		
 		if (offers.contains(offer)) {
 			if (player.drop("money", offer.getPrice().intValue())) {
 				player.equipOrPutOnGround(offer.getItem());
@@ -44,13 +54,6 @@ public class Shop implements ZoneConfigurator {
 			item.setQuantity(earnings.get(earner.getName()));
 			earner.equipToInventoryOnly(item);
 		}
-	}
-
-
-	public void configureZone(StendhalRPZone zone,
-			Map<String, String> attributes) {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
