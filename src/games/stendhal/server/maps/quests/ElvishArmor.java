432fc4fbb7a38d5833172f34c0a8afbc043a48eb
Katie Russell <kymara@users.sourceforge.net>
2009-04-05 14:44:02 +0000

    update to use BringListOfItemsQuest logic

--- src/games/stendhal/server/maps/quests/ElvishArmor.java
+++ src/games/stendhal/server/maps/quests/ElvishArmor.java
@@ -20,6 +20,8 @@ import games.stendhal.server.entity.npc.parser.Expression;
 import games.stendhal.server.entity.npc.parser.Sentence;
 import games.stendhal.server.entity.npc.parser.TriggerList;
 import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.maps.quests.logic.BringListOfItemsQuest;
+import games.stendhal.server.maps.quests.logic.BringListOfItemsQuestLogic;
 
 import java.util.Arrays;
 import java.util.LinkedList;
@@ -50,7 +52,8 @@ import java.util.List;
  * <li> None.</li>
  * </ul>
  */
-public class ElvishArmor extends AbstractQuest {
+public class ElvishArmor extends AbstractQuest implements
+		BringListOfItemsQuest {
 
 	private static final String QUEST_SLOT = "elvish_armor";
 
@@ -64,202 +67,137 @@ public class ElvishArmor extends AbstractQuest {
 		return QUEST_SLOT;
 	}
 	
-	/**
-	 * Returns a list of the names of all items that the given player still has
-	 * to bring to fulfill the quest.
-	 *
-	 * @param player
-	 *            The player doing the quest
-	 * @param hash
-	 *            If true, sets a # character in front of every name
-	 * @return A list of item names
-	 */
-	private List<String> missingitems(final Player player, final boolean hash) {
-		final List<String> result = new LinkedList<String>();
-
-		String doneText = player.getQuest(QUEST_SLOT);
-		if (doneText == null) {
-			doneText = "";
-		}
-
-		final List<String> done = Arrays.asList(doneText.split(";"));
-		for (final String itemName : NEEDEDITEMS) {
-			if (!done.contains(itemName)) {
-				if (hash) {
-					result.add("#" + itemName);
-				} else {
-					result.add(itemName);
-				}
-			}
-		}
-
-		return result;
+	@Override
+	public void init(final String name) {
+		super.init(name);
 	}
 
-	private void step_1() {
-		final SpeakerNPC npc = npcs.get("Lupos");
-
-		// player says hi before starting the quest
-		npc.add(
-				ConversationStates.IDLE,
-				ConversationPhrases.GREETING_MESSAGES,
-				new QuestNotStartedCondition(QUEST_SLOT),
-				ConversationStates.ATTENDING,
-				"Greetings, traveller. I see that you have come far to be here. I am interested in anyone who has encountered our kin, the green #elves",
-				null);
-
-		npc.add(
-				ConversationStates.ATTENDING,
-				"elves",
-				new ChatCondition() {
-					public boolean fire(final Player player, final Sentence sentence, final SpeakerNPC engine) {
-						return !player.hasQuest(QUEST_SLOT);
-					}
-				},
-				ConversationStates.QUEST_OFFERED,
-				"Yes, those that dwell in the forest of Nalwor. They guard their #secrets closely, you know.",
-				null);
-
-		// player asks what items are needed
-		npc.add(
-				ConversationStates.QUEST_OFFERED,
-				"secrets",
-				null,
-				ConversationStates.QUEST_OFFERED,
-				"They won't share knowledge of how to create the green armor, shields and the like. You would call them elvish items. I wonder if a traveller like you could bring me any?",
-				null);
-
-		// player says yes
-		npc.add(ConversationStates.QUEST_OFFERED,
-				ConversationPhrases.YES_MESSAGES, 
-				null,
-				ConversationStates.IDLE, 
-				"The secrets of the green elves shall be ours at last! Bring me all elvish equipment you can find, I'll reward you well!", 
-				new MultipleActions(new SetQuestAction(QUEST_SLOT, ""),
-									new IncreaseKarmaAction(5.0)));
-
-		// player is not willing to help
-		npc.add(ConversationStates.QUEST_OFFERED, 
-				ConversationPhrases.NO_MESSAGES, 
-				null,
-				ConversationStates.QUEST_OFFERED, 
-				"Another unhelpful soul, I see.",
-				new DecreaseKarmaAction(5.0));
-
+	private void setupAbstractQuest() {
+		final BringListOfItemsQuest concreteQuest = this;
+		final BringListOfItemsQuestLogic bringItems = new BringListOfItemsQuestLogic(concreteQuest);
+		bringItems.addToWorld();
 	}
 
-	private void step_2() {
-		// Just find the items by killing elves and/or dark elves and bring them to Lupos.
+  	@Override
+	public void addToWorld() {
+		super.addToWorld();
+		offerSteps();
+		setupAbstractQuest();
 	}
 
-	private void step_3() {
-		final SpeakerNPC npc = npcs.get("Lupos");
+	public SpeakerNPC getNPC() {
+		return npcs.get("Lupos");
+	}
 
-		// player returns while quest is still active
-		npc.add(
-				ConversationStates.IDLE,
-				ConversationPhrases.GREETING_MESSAGES,
-				new QuestActiveCondition(QUEST_SLOT),
-				ConversationStates.QUESTION_1,
-				"Hello! I hope your search for elvish #equipment is going well?",
-				null);
+	public List<String> getNeededItems() {
+		return NEEDEDITEMS;
+	}
 
-		// player asks what exactly is missing
-		npc.add(ConversationStates.QUESTION_1, "equipment", null,
-				ConversationStates.QUESTION_1, null,
-				new ChatAction() {
-					public void fire(final Player player, final Sentence sentence, final SpeakerNPC engine) {
-						final List<String> needed = missingitems(player, true);
-						engine.say("I have heard descriptions of "
-								+ Grammar.quantityplnoun(needed.size(), "item")
+	public List<String> getTriggerPhraseToEnumerateMissingItems() {
+		return Arrays.asList("equipment");
+	}
+
+	public List<String> getAdditionalTriggerPhraseForQuest() {
+		return Arrays.asList("secrets");
+	}
+
+	public double getKarmaDiffForQuestResponse() {
+		return 5.0;
+	}
+
+	public boolean shouldWelcomeAfterQuestIsCompleted() {
+		return true;
+	}
+
+	public String welcomeBeforeStartingQuest() {
+		return "Greetings, traveller. I see that you have come far to be here. "
+			+ "I am interested in anyone who has encountered our kin, the green elves of Nalwor. They guard their #secrets closely.";
+	}
+
+	public String respondToQuest() {
+		return "They won't share knowledge of how to create the green armor, shields and the like. You would call them elvish items. "
+			+ "I wonder if a traveller like you could bring me any?";
+	}
+
+	public String respondToQuestAcception() {
+		return "The secrets of the green elves shall be ours at last! Bring me all elvish equipment you can find, I'll reward you well!";
+	}
+
+	public String respondToQuestRefusal() {
+		return "Another unhelpful soul, I see.";
+	}
+
+	public String welcomeDuringActiveQuest() {
+		return "Hello! I hope your search for elvish #equipment is going well?";
+	}
+
+	// this one not actually used here
+	public String firstAskForMissingItems(final List<String> missingItems) {
+		return "I have heard descriptions of "
+								+ Grammar.quantityplnoun(missingItems.size(), "item")
 								+ " in all. They are: "
-								+ Grammar.enumerateCollection(needed)
-								+ ". Have you looted any?");
-					}
-				});
-		// player says he has a required item with him
-		npc.add(ConversationStates.QUESTION_1,
-				ConversationPhrases.YES_MESSAGES, null,
-				ConversationStates.QUESTION_1,
-				"Felicitations! What #equipment did you pillage?", null);
+								+ Grammar.enumerateCollection(missingItems)
+								+ ". Will you collect them?";
+	}
 
-		npc.add(ConversationStates.QUESTION_1, NEEDEDITEMS, null,
-				ConversationStates.QUESTION_1, null,
-				new ChatAction() {
-					public void fire(final Player player, final Sentence sentence, final SpeakerNPC engine) {
-						final Expression item = sentence.getTriggerExpression();
+	public String askForMissingItems(final List<String> missingItems) {
+		return "I have heard descriptions of "
+								+ Grammar.quantityplnoun(missingItems.size(), "item")
+								+ " in all. They are: "
+								+ Grammar.enumerateCollection(missingItems)
+								+ ". Have you looted any?";
+	}
 
-						TriggerList missing = new TriggerList(missingitems(player, false));
+	public String askForItemsAfterPlayerSaidHeHasItems() {
+		return "Felicitations! What #equipment did you pillage?";
+	}
 
-						final Expression found = missing.find(item);
-						if (found != null) {
-							final String itemName = found.getOriginal();
+	public String respondToItemBrought() {
+		return "Excellent work. Is there more that you plundered?";
+	}
 
-							if (player.drop(itemName)) {
-								// register item as done
-								final String doneText = player.getQuest(QUEST_SLOT);
-								player.setQuest(QUEST_SLOT, doneText + ";" + itemName);
 
-								// check if the player has brought all items
-								missing = new TriggerList(missingitems(player, true));
-
-								if (missing.size() > 0) {
-									engine.say("Excellent work. Is there more that you plundered?");
-								} else {
-//									Item blackitem = StendhalRPWorld.get().getRuleManager().getEntityManager().getItem("black_item");
-//									blackitem.setBoundTo(player.getName());
-//									player.equip(blackitem, true);
-									player.addKarma(20.0);
-									player.addXP(20000);
-									engine.say("I will study these! The albino elves owe you a debt of thanks.");
-									player.setQuest(QUEST_SLOT, "done");
-									player.notifyWorldAboutChanges();
-									engine.setCurrentState(ConversationStates.ATTENDING);
-								}
-							} else {
-								engine.say("Liar! You don't really have "
+	public String respondToLastItemBrought() {
+		return "I will study these! The albino elves owe you a debt of thanks.";
+	}
+								
+	public String respondToOfferOfNotExistingItem(final String itemName) {
+		return "Liar! You don't really have "
 										+ Grammar.a_noun(itemName)
-										+ " with you.");
-							}
-						} else {
-							engine.say("You've already brought that elvish item to me.");
-						}
-					}
-		});
+										+ " with you.";
+	}
+	public String respondToOfferOfNotMissingItem() {
+		return "You've already brought that elvish item to me.";
+	}
 
-		npc.add(ConversationStates.QUESTION_1, "",
-				new NotCondition(new TriggerInListCondition(NEEDEDITEMS)),
-				ConversationStates.QUESTION_1,
-				"I don't think that's a piece of elvish armor...", null);
+	public String respondToOfferOfNotNeededItem() {
+		return	"I don't think that's a piece of elvish armor...";
+	}
 
-		// allow to say goodbye while Lupos is listening for item names
-		npc.add(ConversationStates.QUESTION_1, ConversationPhrases.GOODBYE_MESSAGES,
-				null,
-				ConversationStates.IDLE,
-				"Bye.", null);
+	public String respondToPlayerSayingHeHasNoItems(final List<String> missingItems) {
+		return "I understand, the green elves protect themselves well. If there's anything else I can do for you, just say.";
+	}
 
-		npc.add(
-				ConversationStates.ATTENDING,
-				ConversationPhrases.NO_MESSAGES,
-				new QuestNotCompletedCondition(QUEST_SLOT),
-				ConversationStates.ATTENDING,
-				"I understand, the green elves protect themselves well. If there's anything else I can do for you, just say.",
-				null);
+	public String respondToQuestAfterItHasAlreadyBeenCompleted() {
+		return "I'm now busy studying the properties of the elvish armor you brought me. It really is intriguing. Until I can reproduce it, I would buy similar items from you."; 
+	}
 
-		// player says he didn't bring any items to different question
-		npc.add(ConversationStates.QUESTION_1, ConversationPhrases.NO_EXPRESSION,
-				new QuestNotCompletedCondition(QUEST_SLOT), 
-				ConversationStates.ATTENDING,
-				"I see. If there's anything else I can do for you, just say.",
-				null);
+	public void rewardPlayer(final Player player) {
+		player.addKarma(20.0);
+		player.addXP(20000);
+	}
 
-		// player returns after finishing the quest
-		npc.add(ConversationStates.IDLE, 
-				ConversationPhrases.GREETING_MESSAGES,
-				new QuestCompletedCondition(QUEST_SLOT), 
-				ConversationStates.ATTENDING,
-				"Greetings again, old friend.", 
-				null);
+	public String welcomeAfterQuestIsCompleted() {
+		return "Greetings again, old friend.";
+	}
+
+	// the bring list of items quest doesn't include this logic:
+		// player returns when the quest is in progress and says quest
+		//				"As you already know, I seek elvish #equipment.";
+
+
+	private void offerSteps() {
+  		final SpeakerNPC npc = npcs.get("Lupos");
 
 		// player returns after finishing the quest and says offer
 		npc.add(
@@ -270,21 +208,6 @@ public class ElvishArmor extends AbstractQuest {
 				"If you have found any more elvish items, I'd be glad if you would #sell them to me. I would buy elvish armor, shield, legs, boots, cloak or sword. I would also buy a drow sword if you have one.",
 				null);
 
-		// player returns after finishing the quest and says quest
-		npc.add(
-				ConversationStates.ATTENDING,
-				ConversationPhrases.QUEST_MESSAGES,
-				new QuestCompletedCondition(QUEST_SLOT),
-				ConversationStates.ATTENDING,
-				"I'm now busy studying the properties of the elvish armor you brought me. It really is intriguing. Until I can reproduce it, I would buy similar items from you.",
-				null);
-
-		// player returns when the quest is in progress and says quest
-		npc.add(ConversationStates.ATTENDING,
-				ConversationPhrases.QUEST_MESSAGES,
-				new QuestActiveCondition(QUEST_SLOT),
-				ConversationStates.QUESTION_1,
-				"As you already know, I seek elvish #equipment.", null);
 
 		// player returns when the quest is in progress and says offer
 		npc.add(ConversationStates.ATTENDING,
@@ -294,11 +217,4 @@ public class ElvishArmor extends AbstractQuest {
 				"I don't think I trust you well enough yet ... ", null);
 	}
 
-	@Override
-	public void addToWorld() {
-		super.addToWorld();
-		step_1();
-		step_2();
-		step_3();
-	}
 }
