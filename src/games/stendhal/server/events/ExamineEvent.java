f96d4150360b88412e9e76208db5460bb8e31c33
Katie Russell <kymara@users.sourceforge.net>
2009-04-14 04:43:33 +0000

    add logging incase to help with debugging this

--- src/games/stendhal/server/events/ExamineEvent.java
+++ src/games/stendhal/server/events/ExamineEvent.java
@@ -5,7 +5,9 @@ import marauroa.common.game.RPClass;
 import marauroa.common.game.RPEvent;
 import marauroa.common.game.Definition.DefinitionClass;
 import marauroa.common.game.Definition.Type;
+import marauroa.common.game.SyntaxException;
 
+import org.apache.log4j.Logger;
 /**
  * An examine event that opens an image viewer with
  * the specified image in the client.
@@ -18,15 +20,23 @@ public class ExamineEvent extends RPEvent {
 	private static final String ALT = "alt";
 	private static final String TITLE = "title";
 
+	/** the logger instance. */
+	private static final Logger logger = Logger.getLogger(ExamineEvent.class);
+
 	/**
 	 * Creates the rpclass.
 	 */
 	public static void generateRPClass() {
+		try {
 		final RPClass rpclass = new RPClass(RPCLASS_NAME);
 		rpclass.add(DefinitionClass.ATTRIBUTE, PATH, Type.STRING, Definition.PRIVATE);
 		rpclass.add(DefinitionClass.ATTRIBUTE, ALT, Type.STRING, Definition.PRIVATE);
 		rpclass.add(DefinitionClass.ATTRIBUTE, TITLE, Type.STRING, Definition.PRIVATE);
+		} catch (final SyntaxException e) {
+			logger.error("cannot generateRPClass", e);
+		}
 	}
+
 
 	/**
 	 * Creates a new ExamineEvent.
