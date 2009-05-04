dfd7cd995a7fd45bd52c58aefc543f4d2ada08a0
Katie Russell <kymara@users.sourceforge.net>
2009-05-04 13:41:52 +0000

    don't tell the player 'blah was removed from your buddy list' if blah wasn't there. check first, and if not being ignored, tell player so.

--- src/games/stendhal/server/actions/buddy/UnignoreAction.java
+++ src/games/stendhal/server/actions/buddy/UnignoreAction.java
@@ -10,8 +10,10 @@ class UnignoreAction implements ActionListener {
 	public void onAction(final Player player, final RPAction action) {
 		if (action.has(TARGET)) {
 			final String who = action.get(TARGET);
-
-			if (player.removeIgnore(who)) {
+			if (player.getIgnore(who) == null) {
+				player.sendPrivateText(who
+						+ " was not being ignored by you.");
+			} else if (player.removeIgnore(who)) {
 				player.sendPrivateText(who
 						+ " was removed from your ignore list.");
 			}
