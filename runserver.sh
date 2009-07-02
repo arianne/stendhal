8943428bc2d09eab7b3d7a0c481d040d0ef6b1cb
Katie Russell <kymara@users.sourceforge.net>
2009-07-02 21:55:39 +0000

    runserver scripts for linux and windows. the build.xml edit is to the 'init' part; it ensures that when version number changes, these scripts get updated. still to do: add them to the server.zip. (it's easy, i just didnt' do it yet)

---
+++ runserver.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+STENDHAL_VERSION = "0.74.5"
+
+LOCALCLASSPATH=.:data/script/:data/conf/:stendhal-server-$STENDHAL_VERSION.jar:marauroa.jar:mysql-connector.jar:log4j.jar:commons-lang.jar
+
+java -Xmx400m -cp "${LOCALCLASSPATH}" marauroa.server.marauroad -c server.ini -l
+
