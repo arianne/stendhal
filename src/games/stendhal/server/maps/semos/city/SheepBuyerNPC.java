7c019c03db85b2cd75a3367db9798362c59966a0
Kimmo Rundelin <kiheru@users.sourceforge.net>
2009-07-07 15:17:47 +0000

    move the sheep Sato buys to the den in Semos

--- src/games/stendhal/server/maps/semos/city/SheepBuyerNPC.java
+++ src/games/stendhal/server/maps/semos/city/SheepBuyerNPC.java
@@ -1,5 +1,8 @@
 	package games.stendhal.server.maps.semos.city;
 
+import games.stendhal.common.Rand;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.rp.StendhalRPAction;
 import games.stendhal.server.entity.creature.Sheep;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.SpeakerNPCFactory;
@@ -37,6 +40,23 @@ public class SheepBuyerNPC extends SpeakerNPCFactory {
 					return 0;
 				}
 			}
+			
+			/**
+			 * Move a bought sheep to the den if there's space, or remove it 
+			 * from the zone otherwise.
+			 * 
+			 * @param sheep the sheep to be moved
+			 */
+			private void moveSheep(Sheep sheep) {
+				// The area of the sheed den.
+				int x = Rand.randUniform(39, 54);
+				int y = Rand.randUniform(24, 29);
+				StendhalRPZone zone = sheep.getZone();
+				if (!StendhalRPAction.placeat(zone, sheep, x, y)) {
+					// there was no room for the sheep. Simply eat it
+					sheep.getZone().remove(sheep);  
+				}
+			}
 
 			@Override
 			public boolean transactAgreedDeal(final SpeakerNPC seller, final Player player) {
@@ -56,8 +76,8 @@ public class SheepBuyerNPC extends SpeakerNPCFactory {
 						player.removeSheep(sheep);
 
 						player.notifyWorldAboutChanges();
+						moveSheep(sheep);
 
-						sheep.getZone().remove(sheep);
 						return true;
 					}
 				} else {
