8e712e71bf948e784e4abdce4468c26f3ca314c3
Hendrik Brummermann <nhnb@users.sourceforge.net>
2009-07-13 22:25:15 +0000

    fixed deadlock by commit the preparation transaction before doing the testcall

--- tests/games/stendhal/server/actions/admin/BanActionTest.java
+++ tests/games/stendhal/server/actions/admin/BanActionTest.java
@@ -8,9 +8,6 @@ import static org.junit.Assert.assertTrue;
 import games.stendhal.server.actions.CommandCenter;
 import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.maps.MockStendlRPWorld;
-
-import java.sql.SQLException;
-
 import marauroa.common.game.RPAction;
 import marauroa.server.db.DBTransaction;
 import marauroa.server.db.TransactionPool;
@@ -32,60 +29,82 @@ public class BanActionTest {
 	}
 
 	@Test
-	public void testPerform() throws SQLException {
+	public void testPerform() throws Throwable {
 		DBTransaction transaction = TransactionPool.get().beginWork();
-		AccountDAO accountDAO = DAORegister.get().get(AccountDAO.class);
+		try {
+			AccountDAO accountDAO = DAORegister.get().get(AccountDAO.class);
+	
+			BanAction ban = new BanAction();
+			Player player = PlayerTestHelper.createPlayer("bob");
+			RPAction action = new RPAction();
+			action.put("target", player.getName());
+			if (!accountDAO.hasPlayer(transaction, player.getName())) {
+				accountDAO.addPlayer(transaction, player.getName(), new byte[0], "schnubbel");
+			}
+			accountDAO.setAccountStatus(transaction, player.getName(), "active");
+			assertEquals("active", accountDAO.getAccountStatus(transaction, player.getName()));
 
-		BanAction ban = new BanAction();
-		Player player = PlayerTestHelper.createPlayer("bob");
-		RPAction action = new RPAction();
-		action.put("target", player.getName());
-		if (!accountDAO.hasPlayer(transaction, player.getName())) {
-			accountDAO.addPlayer(transaction, player.getName(), new byte[0], "schnubbel");
+			TransactionPool.get().commit(transaction);
+
+			ban.perform(player , action);
+
+			transaction = TransactionPool.get().beginWork();
+
+			assertEquals("banned", accountDAO.getAccountStatus(transaction, player.getName()));
+	
+			// just undo the changes so the next test starts clean
+			TransactionPool.get().rollback(transaction);
+		} catch (Throwable e) {
+			TransactionPool.get().rollback(transaction);
+			throw e;
 		}
-		accountDAO.setAccountStatus(transaction, player.getName(), "active");
-		assertEquals("active", accountDAO.getAccountStatus(transaction, player.getName()));
-		ban.perform(player , action);
-		assertEquals("banned", accountDAO.getAccountStatus(transaction, player.getName()));
-
-		// just undo the changes so the next test starts clean
-		TransactionPool.get().rollback(transaction);
 	}
 	
 	@Test
-	public void testCommandCenterPerform() throws SQLException {
+	public void testCommandCenterPerform() throws Throwable {
 		DBTransaction transaction = TransactionPool.get().beginWork();
-		AccountDAO accountDAO = DAORegister.get().get(AccountDAO.class);
+		try {
+			AccountDAO accountDAO = DAORegister.get().get(AccountDAO.class);
+	
+			Player player = PlayerTestHelper.createPlayer("bobby");
+			Player admin = PlayerTestHelper.createPlayer("admin");
+			RPAction action = new RPAction();
+			action.put("type", "ban");
+			action.put("target", player.getName());
+			action.put("reason", "whynot");
+			if (!accountDAO.hasPlayer(transaction, player.getName())) {
+				accountDAO.addPlayer(transaction, player.getName(), new byte[0], "schnubbel");
+			}
+			accountDAO.setAccountStatus(transaction, player.getName(), "active");
+			if (!accountDAO.hasPlayer(transaction, admin.getName())) {
+				accountDAO.addPlayer(transaction, admin.getName(), new byte[0], "schnubbel");
+			}
+			accountDAO.setAccountStatus(transaction, admin.getName(), "active");
+	
+			assertEquals("active", accountDAO.getAccountStatus(transaction, player.getName()));
+			assertEquals("active", accountDAO.getAccountStatus(transaction, admin.getName()));
 
-		Player player = PlayerTestHelper.createPlayer("bobby");
-		Player admin = PlayerTestHelper.createPlayer("admin");
-		RPAction action = new RPAction();
-		action.put("type", "ban");
-		action.put("target", player.getName());
-		action.put("reason", "whynot");
-		if (!accountDAO.hasPlayer(transaction, player.getName())) {
-			accountDAO.addPlayer(transaction, player.getName(), new byte[0], "schnubbel");
+			TransactionPool.get().commit(transaction);
+
+			assertFalse(CommandCenter.execute(admin , action));
+			admin.clearEvents();
+			admin.setAdminLevel(5000);
+			assertTrue(CommandCenter.execute(admin , action));
+			
+			transaction = TransactionPool.get().beginWork();
+			
+			assertEquals("banned", accountDAO.getAccountStatus(transaction, player.getName()));
+			assertEquals("active", accountDAO.getAccountStatus(transaction, admin.getName()));
+			assertFalse(admin.events().isEmpty());
+			assertThat(admin.events().get(0).toString(), containsString("[private_text=Attributes of Class(): ")); 
+			assertThat(admin.events().get(0).toString(), containsString("[text=You have banned bobby for: whynot]"));
+			assertThat(admin.events().get(0).toString(), containsString("[texttype=PRIVMSG]"));
+	
+			// just undo the changes so the next test starts clean
+			TransactionPool.get().rollback(transaction);
+		} catch (Throwable e) {
+			TransactionPool.get().rollback(transaction);
+			throw e;
 		}
-		accountDAO.setAccountStatus(transaction, player.getName(), "active");
-		if (!accountDAO.hasPlayer(transaction, admin.getName())) {
-			accountDAO.addPlayer(transaction, admin.getName(), new byte[0], "schnubbel");
-		}
-		accountDAO.setAccountStatus(transaction, admin.getName(), "active");
-
-		assertEquals("active", accountDAO.getAccountStatus(transaction, player.getName()));
-		assertEquals("active", accountDAO.getAccountStatus(transaction, admin.getName()));
-		assertFalse(CommandCenter.execute(admin , action));
-		admin.clearEvents();
-		admin.setAdminLevel(5000);
-		assertTrue(CommandCenter.execute(admin , action));
-		assertEquals("banned", accountDAO.getAccountStatus(transaction, player.getName()));
-		assertEquals("active", accountDAO.getAccountStatus(transaction, admin.getName()));
-		assertFalse(admin.events().isEmpty());
-		assertThat(admin.events().get(0).toString(), containsString("[private_text=Attributes of Class(): ")); 
-		assertThat(admin.events().get(0).toString(), containsString("[text=You have banned bobby for: whynot]"));
-		assertThat(admin.events().get(0).toString(), containsString("[texttype=PRIVMSG]"));
-
-		// just undo the changes so the next test starts clean
-		TransactionPool.get().rollback(transaction);
 	}
 }
