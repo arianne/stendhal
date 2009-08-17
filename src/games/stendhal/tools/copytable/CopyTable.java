fa9c669004f251a17cc97d87f3d93efd676f4213
Hendrik Brummermann <nhnb@users.sourceforge.net>
2009-08-17 20:38:28 +0000

    adjusted timings but still creates lag

--- src/games/stendhal/tools/copytable/CopyTable.java
+++ src/games/stendhal/tools/copytable/CopyTable.java
@@ -8,18 +8,19 @@ import marauroa.server.game.db.DatabaseFactory;
 
 public class CopyTable {
 
-	public static void main(String[] args) throws SQLException {
+	public static void main(String[] args) throws SQLException, InterruptedException {
 		new DatabaseFactory().initializeDatabase();
 		TransactionPool transactionPool = TransactionPool.get();
 		
-		for (int i = 0; i < 34600; i++) {
-			System.out.println(i);
+		for (int i = 1122; i < 346000; i++) {
+			System.out.println("> " + i);
 			String cmd = "INSERT INTO itemlog_new (id, timedate, itemid, source, event, param1, param2, param3, param4)"
-				+ " SELECT id, timedate, itemid, source, event, param1, param2, param3, param4 FROM itemlog WHERE id >= " + (i * 1000) + " AND id < "  + ((i+1) * 1000);
+				+ " SELECT id, timedate, itemid, source, event, param1, param2, param3, param4 FROM itemlog WHERE id >= " + (i * 100) + " AND id < "  + ((i+1) * 100);
 			DBTransaction transaction = transactionPool.beginWork();
 			transaction.execute(cmd, null);
 			transactionPool.commit(transaction);
-			System.out.println(i);
+			System.out.println("< " + i);
+			Thread.sleep(3000);
 		}
 	}
 }
