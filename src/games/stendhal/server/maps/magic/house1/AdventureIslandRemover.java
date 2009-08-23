587599bbdff7ac248623a08b36167c183fe6130c
Kimmo Rundelin <kiheru@users.sourceforge.net>
2009-08-23 22:09:20 +0000

    call onRemoved() on corpses before removing the zone to stop timers

--- src/games/stendhal/server/maps/magic/house1/AdventureIslandRemover.java
+++ src/games/stendhal/server/maps/magic/house1/AdventureIslandRemover.java
@@ -1,8 +1,10 @@
 package games.stendhal.server.maps.magic.house1;
 
+import marauroa.common.game.RPObject;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.events.TurnListener;
+import games.stendhal.server.entity.item.Corpse;
 
 /**
  * removes the island
@@ -22,6 +24,13 @@ public class AdventureIslandRemover implements TurnListener {
 
 	public void onTurnReached(int currentTurn) {
 		if (zone.getPlayers().size()==0) {
+			// Tell all corpses they are to be removed
+			// (stops timers)
+			for (RPObject object : zone) {
+				if (object instanceof Corpse) {
+					((Corpse) object).onRemoved(zone);
+				}
+			}
 			SingletonRepository.getRPWorld().removeZone(zone);
 		} 
 	}
