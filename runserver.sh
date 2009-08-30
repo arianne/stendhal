690b36b006ca7c744afba333544067f9b9928f52
Hendrik Brummermann <nhnb@users.sourceforge.net>
2009-08-30 18:37:41 +0000

    increased version number

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-STENDHAL_VERSION="0.76.5"
+STENDHAL_VERSION="0.77"
 
 LOCALCLASSPATH=.:data/script/:data/conf/:stendhal-server-$STENDHAL_VERSION.jar:marauroa.jar:mysql-connector.jar:log4j.jar:commons-lang.jar
 
