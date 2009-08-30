d55bda36575d54363344cee4a5c21e173d89d459
Katie Russell <kymara@users.sourceforge.net>
2009-08-30 15:56:07 +0000

    option to pay 200* playerlevel money instead of take 3% xp loss as a short term offer, because we changed wedding ring behaviour

--- src/games/stendhal/server/maps/quests/marriage/Divorce.java
+++ src/games/stendhal/server/maps/quests/marriage/Divorce.java
@@ -42,13 +42,69 @@ class Divorce {
 				new ChatCondition() {
 					public boolean fire(final Player player, final Sentence sentence, final SpeakerNPC npc) {
 						return (player.isQuestCompleted(marriage.getQuestSlot()))
-								&& player.isEquipped("wedding ring");
+								&& player.isEquipped("wedding ring") && player.isEquipped("money",200*player.getLevel());
 					}
 				}, 
 				ConversationStates.QUESTION_3,
-				"Are you sure you want to divorce?", 
-				null);
+				null,
+			   	new ChatAction() {
+					public void fire(final Player player, final Sentence sentence, final SpeakerNPC npc) {
+						Player husband;
+						Player wife;
+						String partnerName;
+						String additional = "";
+						husband = player;
+						partnerName = husband.getQuest(marriage.getSpouseQuestSlot());
+						wife = SingletonRepository.getRuleProcessor().getPlayer(
+								partnerName);
+						if ((wife != null)
+								&& wife.hasQuest(marriage.getQuestSlot())
+								&& wife.getQuest(marriage.getSpouseQuestSlot()).equals(
+										husband.getName())) {
+							if (wife.isEquipped("money", 200*wife.getLevel())) {
+								additional = partnerName + " has their fee of " + 200*wife.getLevel() + " and will also be charged.";
+							} else {
+								additional = partnerName + " doesn't have their fee of " + 200*wife.getLevel() + " and will lose 3% xp instead.";
+							}
+						}
+						npc.say("There's an offer currently, you can pay to divorce instead of losing xp. It will cost you " + 200* player.getLevel() + " money. " + additional + " Do you want to divorce, and pay the money instead of losing xp?");
+					}
+				});
 
+		clerk.add(ConversationStates.ATTENDING, 
+				  "divorce",
+				  new ChatCondition() {
+					  public boolean fire(final Player player, final Sentence sentence, final SpeakerNPC npc) {
+						  return (player.isQuestCompleted(marriage.getQuestSlot()))
+							  && player.isEquipped("wedding ring") && !player.isEquipped("money",200*player.getLevel());
+					  }
+				  }, 
+				  ConversationStates.QUESTION_3,
+				  null,
+				  new ChatAction() {
+					  public void fire(final Player player, final Sentence sentence, final SpeakerNPC npc) {
+						  Player husband;
+						  Player wife;
+						  String partnerName;
+						  String additional = "";
+						  husband = player;
+						  partnerName = husband.getQuest(marriage.getSpouseQuestSlot());
+						  wife = SingletonRepository.getRuleProcessor().getPlayer(
+																				  partnerName);
+						  if ((wife != null)
+							  && wife.hasQuest(marriage.getQuestSlot())
+							  && wife.getQuest(marriage.getSpouseQuestSlot()).equals(
+																					 husband.getName())) {
+							  if (wife.isEquipped("money", 200*wife.getLevel())) {
+								  additional = partnerName + " has their fee of " + 200*wife.getLevel() + " and will also be charged.";
+							  } else {
+								  additional = partnerName + " doesn't have their fee of " + 200*wife.getLevel() + " and will lose 3% xp instead.";
+							  }
+						  }
+						  npc.say("There's an offer currently, you can pay to divorce instead of losing xp. It would cost you " + 200* player.getLevel() + " money, but you do not have sufficent money here with you. " + additional + " You can take the penalty of losing 3% of your xp. Do you want to divorce, and lose the xp?");
+					  }
+				  });
+		
 		clerk.add(ConversationStates.ATTENDING,
 					"divorce",
 					new ChatCondition() {
@@ -78,7 +134,7 @@ class Divorce {
 				"divorce",
 				new NotCondition(new PlayerHasItemWithHimCondition("wedding ring")),
 				ConversationStates.ATTENDING,
-				"I apologise, but I need your wedding ring in order to divorce you.",
+				"I apologise, but I need your wedding ring in order to divorce you. If you have lost yours, you can go to Ognir to make another.",
 				null);
 
 		// If they say no
@@ -113,8 +169,12 @@ class Divorce {
 							if (wife.isEquipped("wedding ring")) {
 								wife.drop("wedding ring");
 							}
-							final int xp = (int) (wife.getXP() * 0.03);
-							wife.subXP(xp);
+							if (wife.isEquipped("money", 200*wife.getLevel())) {
+								wife.drop("money", 200*wife.getLevel());
+							} else {
+								final int xp = (int) (wife.getXP() * 0.03);
+								wife.subXP(xp);
+							}
 							wife.removeQuest(marriage.getQuestSlot());
 							wife.removeQuest(marriage.getSpouseQuestSlot());
 							wife.sendPrivateText(husband.getName()
@@ -131,8 +191,12 @@ class Divorce {
 										+ " has divorced from you!");
 							}
 						}
-						final int xp = (int) (husband.getXP() * 0.03);
-						husband.subXP(xp);
+						if (husband.isEquipped("money", 200*husband.getLevel())) {
+							husband.drop("money", 200*husband.getLevel());
+						} else {
+							final int xp = (int) (husband.getXP() * 0.03);
+							husband.subXP(xp);
+						}
 						husband.drop("wedding ring");
 						husband.removeQuest(marriage.getQuestSlot());
 						husband.removeQuest(marriage.getSpouseQuestSlot());
