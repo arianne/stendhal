cfb7c69db6af71becb3fb1dc7fe4fab76d472d64
Hendrik Brummermann <nhnb@users.sourceforge.net>
2009-09-17 20:28:54 +0000

    check existence of character to be jailed

--- src/games/stendhal/server/actions/admin/JailAction.java
+++ src/games/stendhal/server/actions/admin/JailAction.java
@@ -3,15 +3,20 @@ package games.stendhal.server.actions.admin;
 import static games.stendhal.common.constants.Actions.JAIL;
 import static games.stendhal.common.constants.Actions.MINUTES;
 import static games.stendhal.common.constants.Actions.TARGET;
+
+import java.sql.SQLException;
+
 import games.stendhal.server.actions.CommandCenter;
 import games.stendhal.server.core.engine.GameEvent;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.entity.player.Player;
 import marauroa.common.game.RPAction;
+import marauroa.server.game.db.CharacterDAO;
+import marauroa.server.game.db.DAORegister;
 
 public class JailAction extends AdministrationAction {
 
-	private static final String USAGE_JAIL_NAME_MINUTES_REASON = "Usage: /jail name minutes reason";
+	private static final String USAGE_JAIL_NAME_MINUTES_REASON = "Usage: /jail <name> <minutes> <reason>";
 
 	public static void register() {
 		CommandCenter.register(JAIL, new JailAction(), 400);
@@ -23,6 +28,16 @@ public class JailAction extends AdministrationAction {
 
 		if (action.has(TARGET) && action.has(MINUTES)) {
 			final String target = action.get(TARGET);
+
+			try {
+				if (!DAORegister.get().get(CharacterDAO.class).hasCharacter(null, target)) {
+					player.sendPrivateText("No character with that name: " + target);
+					return;
+				}
+			} catch (SQLException e) {
+				logger.error(e, e);
+			}
+			
 			String reason = "";
 			if (action.has("reason")) {
 				reason = action.get("reason");
