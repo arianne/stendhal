b8ecd6bbaad3f309d940bf3803937c923ee21cad
Hendrik Brummermann <nhnb@users.sourceforge.net>
2009-10-04 19:59:34 +0000

    moved tower-is-closed-sign into its own class

--- src/games/stendhal/server/maps/quests/SemosMineTownRevivalWeeks.java
+++ src/games/stendhal/server/maps/quests/SemosMineTownRevivalWeeks.java
@@ -1,11 +1,9 @@
 package games.stendhal.server.maps.quests;
 
-import games.stendhal.server.core.engine.SingletonRepository;
-import games.stendhal.server.core.engine.StendhalRPZone;
-import games.stendhal.server.entity.mapstuff.sign.Sign;
 import games.stendhal.server.maps.quests.revivalweeks.DadNPC;
 import games.stendhal.server.maps.quests.revivalweeks.FoundGirl;
 import games.stendhal.server.maps.quests.revivalweeks.OutfitLender2NPC;
+import games.stendhal.server.maps.quests.revivalweeks.TownerClosedSign;
 
 /**
  * <p>Creates a special version of Susi by the semos mine town.
@@ -21,24 +19,13 @@ public class SemosMineTownRevivalWeeks extends AbstractQuest {
 		return QUEST_SLOT;
 	}
 
-	// TODO: move this sign to the normal, non-quest map
-	// This is actually a lie, the real is that we cannot tell apart
-	// if someone is behind the top of the tower or in front of it.
-	private void createSignToCloseTower() {
-		final StendhalRPZone zone = SingletonRepository.getRPWorld().getZone("0_semos_mountain_n2");
-		final Sign sign = new Sign();
-		sign.setPosition(105, 114);
-		sign.setText("Because of the missing guard rail it is too dangerous to enter the tower.");
-		zone.add(sign);
-	}
-
 	@Override
 	public void addToWorld() {
 		super.addToWorld();
 		new FoundGirl().addToWorld();
 		new DadNPC().addToWorld();
 		new OutfitLender2NPC().addToWorld();
-		createSignToCloseTower();
+		new TownerClosedSign().addToWorld();
 	}
 
 	@Override
---
+++ src/games/stendhal/server/maps/quests/revivalweeks/TownerClosedSign.java
@@ -0,0 +1,24 @@
+package games.stendhal.server.maps.quests.revivalweeks;
+
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.mapstuff.sign.Sign;
+
+/**
+ * Add a sign saying the tower is closed
+ *
+ * @author hendrik
+ */
+public class TownerClosedSign {
+
+	// TODO: move this sign to the normal, non-quest map
+	// This is actually a lie, the real reason is that we cannot tell apart
+	// if someone is behind the top of the tower or in front of it.
+	public void addToWorld() {
+		final StendhalRPZone zone = SingletonRepository.getRPWorld().getZone("0_semos_mountain_n2");
+		final Sign sign = new Sign();
+		sign.setPosition(105, 114);
+		sign.setText("Because of the missing guard rail it is too dangerous to enter the tower.");
+		zone.add(sign);
+	}
+}
