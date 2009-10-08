165f85a64ea8f0c9d8af10d9d639fe5377d2dd95
Markus <madmetzger@users.sourceforge.net>
2009-10-08 11:03:55 +0000

    add error handling for wrong sentences

--- src/games/stendhal/server/maps/semos/tavern/marketChatActions/ShowOffersChatAction.java
+++ src/games/stendhal/server/maps/semos/tavern/marketChatActions/ShowOffersChatAction.java
@@ -1,6 +1,7 @@
 package games.stendhal.server.maps.semos.tavern.marketChatActions;
 
 import games.stendhal.server.entity.npc.ChatAction;
+import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.parser.Sentence;
 import games.stendhal.server.entity.player.Player;
@@ -22,31 +23,37 @@ import marauroa.common.game.RPSlot;
 public class ShowOffersChatAction implements ChatAction {
 
 	public void fire(Player player, Sentence sentence, SpeakerNPC npc) {
-		Market market = TradeCenterZoneConfigurator.getShopFromZone(player.getZone());
-		StringBuilder offersMessage = new StringBuilder();
-		int counter = 0;
-		RPSlot offersSlot = market.getSlot(Market.OFFERS_SLOT_NAME);
-		MarketManagerNPC marketNPC = (MarketManagerNPC) npc;
-		marketNPC.getOfferMap().put(player.getName(),new HashMap<String, Offer>());
-		for (RPObject rpObject : offersSlot) {
-			if(rpObject.getRPClass().getName().equals(Offer.OFFER_RPCLASS_NAME)) {
-				Offer o = (Offer) rpObject;
-				counter += 1;
-				offersMessage.append(counter);
-				offersMessage.append(" ");
-				offersMessage.append(o.getItem().getName());
-				offersMessage.append(" for ");
-				offersMessage.append(o.getPrice());
-				offersMessage.append(" money");
-				offersMessage.append("\n");
-				marketNPC.getOfferMap().get(player.getName()).put(Integer.valueOf(counter).toString(), o);
+		if (sentence.hasError()) {
+			npc.say("Sorry, I did not understand you. "
+					+ sentence.getErrorString());
+			npc.setCurrentState(ConversationStates.ATTENDING);
+		} else if (sentence.getExpressions().iterator().next().toString().equals("show")){
+			Market market = TradeCenterZoneConfigurator.getShopFromZone(player.getZone());
+			StringBuilder offersMessage = new StringBuilder();
+			int counter = 0;
+			RPSlot offersSlot = market.getSlot(Market.OFFERS_SLOT_NAME);
+			MarketManagerNPC marketNPC = (MarketManagerNPC) npc;
+			marketNPC.getOfferMap().put(player.getName(),new HashMap<String, Offer>());
+			for (RPObject rpObject : offersSlot) {
+				if(rpObject.getRPClass().getName().equals(Offer.OFFER_RPCLASS_NAME)) {
+					Offer o = (Offer) rpObject;
+					counter += 1;
+					offersMessage.append(counter);
+					offersMessage.append(" ");
+					offersMessage.append(o.getItem().getName());
+					offersMessage.append(" for ");
+					offersMessage.append(o.getPrice());
+					offersMessage.append(" money");
+					offersMessage.append("\n");
+					marketNPC.getOfferMap().get(player.getName()).put(Integer.valueOf(counter).toString(), o);
+				}
 			}
-		}
-		if (counter > 0) {
-			player.sendPrivateText(offersMessage.toString());
-		}
-		if (counter == 0) {
-			player.sendPrivateText("There are currently no offers in the market.");
+			if (counter > 0) {
+				player.sendPrivateText(offersMessage.toString());
+			}
+			if (counter == 0) {
+				player.sendPrivateText("There are currently no offers in the market.");
+			}
 		}
 	}
 	
