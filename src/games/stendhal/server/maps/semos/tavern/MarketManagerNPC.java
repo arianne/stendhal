db57498c73778f7343af4346478662384e1cef35
Markus <madmetzger@users.sourceforge.net>
2009-10-08 10:12:05 +0000

    added new action to accept an offer

--- src/games/stendhal/server/maps/semos/tavern/MarketManagerNPC.java
+++ src/games/stendhal/server/maps/semos/tavern/MarketManagerNPC.java
@@ -1,8 +1,8 @@
 package games.stendhal.server.maps.semos.tavern;
 
-import games.stendhal.server.entity.npc.ChatAction;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.maps.semos.tavern.marketChatActions.AcceptOfferChatAction;
 import games.stendhal.server.maps.semos.tavern.marketChatActions.AddOfferChatAction;
 import games.stendhal.server.maps.semos.tavern.marketChatActions.ShowOffersChatAction;
 import games.stendhal.server.trade.Offer;
@@ -30,6 +30,7 @@ public final class MarketManagerNPC extends SpeakerNPC {
 		addHelp("Add a nice help text here.");
 		add(ConversationStates.ATTENDING, "sell", null, null, ConversationStates.ATTENDING,	null, new AddOfferChatAction());
 		add(ConversationStates.ATTENDING, "show", null, ConversationStates.BUY_PRICE_OFFERED, null, new ShowOffersChatAction());
+		add(ConversationStates.BUY_PRICE_OFFERED, "accept", null, ConversationStates.ATTENDING, null, new AcceptOfferChatAction());
 		addGoodbye("Visit me again to see players offers or put a new offer!");
 	}
 	public Map<String, Map<String, Offer>> getOfferMap() {
---
+++ src/games/stendhal/server/maps/semos/tavern/marketChatActions/AcceptOfferChatAction.java
@@ -0,0 +1,15 @@
+package games.stendhal.server.maps.semos.tavern.marketChatActions;
+
+import games.stendhal.server.entity.npc.ChatAction;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.parser.Sentence;
+import games.stendhal.server.entity.player.Player;
+
+public class AcceptOfferChatAction implements ChatAction {
+
+	public void fire(Player player, Sentence sentence, SpeakerNPC npc) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
