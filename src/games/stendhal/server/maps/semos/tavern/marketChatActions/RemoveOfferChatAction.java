1a8b3c6f6f6e2e4fe5a863e3dcf808ad70b9db75
Markus <madmetzger@users.sourceforge.net>
2009-10-09 10:32:12 +0000

    use super class behaviour

--- src/games/stendhal/server/maps/semos/tavern/marketChatActions/RemoveOfferChatAction.java
+++ src/games/stendhal/server/maps/semos/tavern/marketChatActions/RemoveOfferChatAction.java
@@ -1,20 +1,52 @@
 package games.stendhal.server.maps.semos.tavern.marketChatActions;
 
-import games.stendhal.server.entity.npc.ChatAction;
+import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.parser.Sentence;
 import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.maps.semos.tavern.MarketManagerNPC;
+import games.stendhal.server.maps.semos.tavern.TradeCenterZoneConfigurator;
+import games.stendhal.server.trade.Market;
+import games.stendhal.server.trade.Offer;
 /**
  * removes a certain offer from the market
  * 
  * @author madmetzger
  *
  */
-public class RemoveOfferChatAction implements ChatAction {
+public class RemoveOfferChatAction extends KnownOffersChatAction {
 
 	public void fire(Player player, Sentence sentence, SpeakerNPC npc) {
-		// TODO Auto-generated method stub
-
+		if (sentence.hasError()) {
+			npc.say("Sorry, I did not understand you. "
+					+ sentence.getErrorString());
+			npc.setCurrentState(ConversationStates.ATTENDING);
+		} else if (sentence.getExpressions().iterator().next().toString().equals("sell")){
+			handleSentence(player,sentence,npc);
+		}
 	}
 
+	private void handleSentence(Player player, Sentence sentence, SpeakerNPC npc) {
+		try {
+			MarketManagerNPC manager = (MarketManagerNPC) npc;
+			String offerNumber = getOfferNumberFromSentence(sentence).toString();
+			if(manager.getOfferMap().get(player.getName()).containsKey(offerNumber)) {
+				Offer o = manager.getOfferMap().get(player.getName()).get(offerNumber);
+				if(o.getOffererName().equals(player.getName())) {
+					Market m = TradeCenterZoneConfigurator.getShopFromZone(player.getZone());
+					m.removeOffer(o,player);
+					player.getZone().add(o, true);
+					return;
+				}
+				npc.say("You can only remove your own offers. Please say #show #mine to see only your offers.");
+				npc.setCurrentState(ConversationStates.ATTENDING);
+				return;
+			}
+			npc.say("Sorry, please choose a number from those I told you to remove your offer.");
+			npc.setCurrentState(ConversationStates.BUY_PRICE_OFFERED);
+		} catch (NumberFormatException e) {
+			npc.say("Sorry, please say #remove #number");
+			npc.setCurrentState(ConversationStates.BUY_PRICE_OFFERED);
+		}	
+	}
 }
