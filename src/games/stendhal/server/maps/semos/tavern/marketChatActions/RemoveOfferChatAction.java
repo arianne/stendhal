182aff35cd399313ab36da4e9da1ec0e0005578e
Markus <madmetzger@users.sourceforge.net>
2009-10-09 09:13:23 +0000

    add action for removal of offers

---
+++ src/games/stendhal/server/maps/semos/tavern/marketChatActions/RemoveOfferChatAction.java
@@ -0,0 +1,20 @@
+package games.stendhal.server.maps.semos.tavern.marketChatActions;
+
+import games.stendhal.server.entity.npc.ChatAction;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.parser.Sentence;
+import games.stendhal.server.entity.player.Player;
+/**
+ * removes a certain offer from the market
+ * 
+ * @author madmetzger
+ *
+ */
+public class RemoveOfferChatAction implements ChatAction {
+
+	public void fire(Player player, Sentence sentence, SpeakerNPC npc) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
