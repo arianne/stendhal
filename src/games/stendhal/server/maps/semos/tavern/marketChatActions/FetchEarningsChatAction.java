a0ba29cb333887b01def0c5cb2334038cf2ac7b2
Markus <madmetzger@users.sourceforge.net>
2009-10-09 11:31:14 +0000

    implemented fetching of earnings

--- src/games/stendhal/server/maps/semos/tavern/marketChatActions/FetchEarningsChatAction.java
+++ src/games/stendhal/server/maps/semos/tavern/marketChatActions/FetchEarningsChatAction.java
@@ -1,15 +1,39 @@
 package games.stendhal.server.maps.semos.tavern.marketChatActions;
 
+import java.util.Set;
+
 import games.stendhal.server.entity.npc.ChatAction;
+import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.parser.Sentence;
 import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.maps.semos.tavern.TradeCenterZoneConfigurator;
+import games.stendhal.server.trade.Earning;
+import games.stendhal.server.trade.Market;
 
 public class FetchEarningsChatAction implements ChatAction {
 
 	public void fire(Player player, Sentence sentence, SpeakerNPC npc) {
-		// TODO Auto-generated method stub
+		if (sentence.hasError()) {
+			npc.say("Sorry, I did not understand you. "
+					+ sentence.getErrorString());
+			npc.setCurrentState(ConversationStates.ATTENDING);
+		} else if (sentence.getExpressions().iterator().next().toString().equals("fetch")){
+			handleSentence(player,sentence,npc);
+		}
+	}
 
+	private void handleSentence(Player player, Sentence sentence, SpeakerNPC npc) {
+		Market market = TradeCenterZoneConfigurator.getShopFromZone(player.getZone());
+		Set<Earning> earnings = market.fetchEarnings(player);
+		int collectedSum = 0;
+		for (Earning earning : earnings) {
+			collectedSum += earning.getValue().intValue();
+			player.getZone().add(earning, true);
+		}
+		player.sendPrivateText("You collected "+Integer.valueOf(collectedSum).toString()+" money.");
+		npc.say("Your earnings have been paid to you");
+		npc.setCurrentState(ConversationStates.ATTENDING);
 	}
 
 }
