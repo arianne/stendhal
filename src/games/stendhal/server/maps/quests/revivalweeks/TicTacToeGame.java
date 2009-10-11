3f6d2e8db861a1a7e0ac007bab8e59f211a2a253
Hendrik Brummermann <nhnb@users.sourceforge.net>
2009-10-11 17:12:32 +0000

    added an NPC for the tic tac toe game

--- src/games/stendhal/server/maps/quests/revivalweeks/TicTacToeGame.java
+++ src/games/stendhal/server/maps/quests/revivalweeks/TicTacToeGame.java
@@ -1,8 +1,12 @@
 package games.stendhal.server.maps.quests.revivalweeks;
 
+import games.stendhal.common.Direction;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.entity.mapstuff.game.TicTacToeBoard;
+import games.stendhal.server.entity.npc.ConversationPhrases;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.SpeakerNPC;
 
 /**
  * Add a sign saying the tower is closed
@@ -10,12 +14,64 @@ import games.stendhal.server.entity.mapstuff.game.TicTacToeBoard;
  * @author hendrik
  */
 public class TicTacToeGame {
+	private StendhalRPZone zone;
+	private TicTacToeBoard board;
 
-	public void addToWorld() {
-		StendhalRPZone zone = SingletonRepository.getRPWorld().getZone("0_semos_mountain_n2");
-		TicTacToeBoard board = new TicTacToeBoard();
+	private void addBoard() {
+		board = new TicTacToeBoard();
 		board.setPosition(105, 120);
 		zone.add(board);
 		board.addToWorld();
 	}
+
+	private void addNPC() {
+		// TODO: add name
+		SpeakerNPC npc = new SpeakerNPC("AddNameHere") {
+			@Override
+			protected void createPath() {
+				// NPC doesn't move
+				setPath(null);
+			}
+
+			@Override
+			protected void createDialog() {
+				add(ConversationStates.IDLE, 
+						ConversationPhrases.GREETING_MESSAGES, 
+						ConversationStates.IDLE,
+						"Hi, welcome to our small game of Tic Tac Toe. Your task is to fill a row "
+						+ "(vertical, horizontal, diagonal) with the same type of tokens. "
+						+ "You need an opponent to #play against.",
+						null);
+
+				add(ConversationStates.IDLE, 
+						ConversationPhrases.HELP_MESSAGES, 
+						ConversationStates.IDLE,
+						"You have to stand next to a token in order to move it.",
+						null);
+				add(ConversationStates.IDLE, 
+						ConversationPhrases.JOB_MESSAGES, 
+						ConversationStates.IDLE,
+						"I am the supervisor of this game.",
+						null);
+				add(ConversationStates.IDLE, 
+						ConversationPhrases.GOODBYE_MESSAGES, 
+						ConversationStates.IDLE,
+						"It was nice to meet you.",
+						null);
+			}
+		};
+		// TODO change outfit
+		npc.setEntityClass("oldwizardnpc"); 
+		npc.setPosition(106, 118);
+		npc.setDirection(Direction.DOWN);
+		zone.add(npc);
+	}
+
+	public void addToWorld() {
+		zone = SingletonRepository.getRPWorld().getZone("0_semos_mountain_n2");
+
+		addBoard();
+		addNPC();
+	}
+
 }
