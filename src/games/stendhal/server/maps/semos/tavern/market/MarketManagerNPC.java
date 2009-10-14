d481dd9e873a7b37a19c153d3e03bcf5b3cb535c
Markus <madmetzger@users.sourceforge.net>
2009-10-14 18:12:16 +0000

    added help text

--- src/games/stendhal/server/maps/semos/tavern/market/MarketManagerNPC.java
+++ src/games/stendhal/server/maps/semos/tavern/market/MarketManagerNPC.java
@@ -24,7 +24,14 @@ public final class MarketManagerNPC extends SpeakerNPC {
 	protected void createDialog() {
 		addGreeting("Welcome to Semos trading center. How can I help you?");
 		addJob("I am here to assist you in selling items.");
-		addHelp("Add a nice help text here.");
+		addHelp("I help you in selling your items. Say #sell #item #price to put an offer at the market." +
+				" If you want to buy something, say #show and I will list current open offers with an " +
+				"offer number. If you want to accept one of the offers, say #accept #number to buy the " +
+				"item offered with that number. If you want to remove an offer from the market, tell me " +
+				"#show mine, so you will see only your offers. Say #remove #number afterward to remove a " +
+				"certain offer. If you have expired offers, you can ask for them by saying #show #expired." +
+				" You can prolong an expired offer by saying #prolong #number. If you already sold some items" +
+				"you can say fetch to me and I will pay out your earnings.");
 		add(ConversationStates.ATTENDING, "sell", null, null, ConversationStates.ATTENDING,	null, new AddOfferChatAction());
 		add(ConversationStates.ATTENDING, "show", null, ConversationStates.BUY_PRICE_OFFERED, null, new ShowOffersChatAction());
 		add(ConversationStates.ATTENDING, "fetch", null, ConversationStates.ATTENDING, null, new FetchEarningsChatAction());
