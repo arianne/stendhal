a4ec5d2ff423fbc602d789ba228ac01cfcc09801
Hendrik Brummermann <nhnb@users.sourceforge.net>
2009-10-17 20:08:50 +0000

    some npcs accept the chat trigger in all states

--- src/games/stendhal/server/maps/quests/PaperChase.java
+++ src/games/stendhal/server/maps/quests/PaperChase.java
@@ -33,7 +33,7 @@ import java.util.Map;
 public class PaperChase extends AbstractQuest {
 	private static final String QUEST_SLOT = "paper_chase";
 
-	private static final List<String> NPC_IDLE = Arrays.asList("Thanatos");
+	private static final List<String> NPC_IDLE = Arrays.asList("Thanatos", "Pdiddi", "Monogenes");
 
 	private List<String> points = Arrays.asList("Hayunn Naratha", "Thanatos", "Haizen", "Zara", "Leander", "Sally", 
 												"Plink", "Jef", "Blacksheep Harry", 
