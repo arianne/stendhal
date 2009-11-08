abd1ef58061b323a99d9c2461c52b4d9f7a99ba9
Hendrik Brummermann <nhnb@users.sourceforge.net>
2009-11-08 21:41:46 +0000

    javadoc

--- src/games/stendhal/tools/loganalyser/laganalyser/LagHTMLWriter.java
+++ src/games/stendhal/tools/loganalyser/laganalyser/LagHTMLWriter.java
@@ -14,17 +14,31 @@ import java.io.PrintStream;
 public class LagHTMLWriter {
 	private PrintStream ps;
 
+	/**
+	 * creates a new LagHTMLWriter
+	 *
+	 * @param outputFileName name of file to write
+	 * @throws FileNotFoundException in case 
+	 */
 	public LagHTMLWriter(String outputFileName) throws FileNotFoundException {
 		this.ps = new PrintStream(outputFileName);
 	}
 
+	/**
+	 * writes the header of the html-file (including the content header).
+	 */
 	public void writeHeader() {
 		ps.println("<html>");
 		ps.println("<head><title>Lag</title></head>");
 		ps.println("<body>");
-		ps.println("<table border=\"1\"");
+		ps.println("<table border=\"1\">");
 	}
 
+	/**
+	 * writes a line
+	 *
+	 * @param times times
+	 */
 	public void writeTurnOverflows(int[] times) {
 		ps.print("<tr>");
 		for (int i = 0; i < times.length; i++) {
@@ -33,12 +47,18 @@ public class LagHTMLWriter {
 		ps.println("</tr>");
 	}
 
+	/**
+	 * writes the html footer.
+	 */
 	public void writeFooter() {
 		ps.println("</table>");
 		ps.println("</body>");
 		ps.println("</html>");
 	}
 
+	/**
+	 * closes the output stream.
+	 */
 	public void close() {
 		ps.close();
 	}
