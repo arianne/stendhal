90a3281633c0b0241fcf82c82f1fd3d0d7beaf84
Hendrik Brummermann <nhnb@users.sourceforge.net>
2009-11-08 21:01:14 +0000

    implemented calculation of relative lag

--- src/games/stendhal/tools/loganalyser/laganalyser/LagReader.java
+++ src/games/stendhal/tools/loganalyser/laganalyser/LagReader.java
@@ -4,6 +4,8 @@
 package games.stendhal.tools.loganalyser.laganalyser;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 /**
@@ -13,15 +15,59 @@ import java.io.IOException;
 public class LagReader {
 	private BufferedReader br;
 
-	public LagReader(String inputFileName) {
-		// TODO Auto-generated constructor stub
+	/**
+	 * creates a new LagReader
+	 *
+	 * @param inputFileName name of log file
+	 * @throws FileNotFoundException in case the file is not found
+	 */
+	public LagReader(String inputFileName) throws FileNotFoundException {
+		br = new BufferedReader(new FileReader(inputFileName));
 	}
 
+	/**
+	 * returns the relative times uesed in each step
+	 *
+	 * @return relative times
+	 */
 	public int[] readTurnOverflowRelative() {
+		int[] absolute = readTurnOverflowAbsolute();
+		if (absolute != null) {
+			return calculateRelative(absolute);
+		}
+		return null;
+	}
+
+	/**
+	 * reads the absolute times from the file.
+	 *
+	 * @return array of ints
+	 */
+	public int[] readTurnOverflowAbsolute() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * calcuates the relative offset
+	 *
+	 * @param absolute absolute times
+	 * @return relative times
+	 */
+	int[] calculateRelative(int[] absolute) {
+		int[] res = new int[absolute.length];
+		res[0] = absolute[0];
+		for (int i = 1; i < res.length; i++) {
+			res [i] = absolute[i] - absolute[i-1];
+		}
+		return res;
+	}
+
+	/**
+	 * closes the input stream
+	 *
+	 * @throws IOException in case of an I/O error
+	 */
 	public void close() throws IOException {
 		br.close();
 	}
