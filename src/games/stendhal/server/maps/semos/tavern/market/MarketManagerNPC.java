282fd0f85fda1ac0049ebbefae6c06eee5cfa8cb
Katie Russell <kymara@users.sourceforge.net>
2009-11-11 23:29:54 +0000

    Split the very long help paragraph into #buying and #selling. Fixed bugs:  - [Stendhal Trading] typo in help response # 2895562  - [Stendhal Trading] No response to 'trade' # 2895553

--- src/games/stendhal/server/maps/semos/tavern/market/MarketManagerNPC.java
+++ src/games/stendhal/server/maps/semos/tavern/market/MarketManagerNPC.java
@@ -24,23 +24,26 @@ public final class MarketManagerNPC extends SpeakerNPC {
 
 	@Override
 	protected void createDialog() {
-		addGreeting("Welcome to Semos trading center. How can I help you?");
-		addJob("I am here to assist you in selling items.");
-		addHelp("I help you in selling your items. Say #sell #item #price to put an offer at the market." +
-				" If you want to buy something, say #show and I will list current open offers with an " +
-				"offer number. If you want to accept one of the offers, say #accept #number to buy the " +
-				"item offered with that number. If you want to remove an offer from the market, tell me " +
-				"#show mine, so you will see only your offers. Say #remove #number afterward to remove a " +
-				"certain offer. If you have expired offers, you can ask for them by saying #show #expired." +
-				" You can prolong an expired offer by saying #prolong #number. If you already sold some items" +
-				"you can say fetch to me and I will pay out your earnings.");
+		addGreeting("Welcome to Semos trading center. How can I #help you?");
+		addJob("I am here to #help you sell items.");
+		addOffer("To put an offer on the market, say #sell #item #price - and then anyone else can buy it, " +
+                "even if you are not here. For more details just ask for #help.");
+		addHelp("Would you like help in #buying or help in #selling?");
+		addReply("buying","If you want to buy something, say #show and I will list current open offers with an " +
+				 "offer number. If you want to accept one of the offers, say #accept #number to buy the " +
+				 "item offered with that number.");
+		addReply("selling","Say #sell #item #price to put an offer on the market. If you want to remove an " +
+				 "offer from the market, tell me #'show mine', so you will see only your offers. Say #remove " +
+                 "#number afterward to remove a certain offer. If you have expired offers, you can ask for them " +
+				 "by saying #show #expired. You can prolong an expired offer by saying #prolong #number. If you already sold some items " +
+				 "you can say #fetch to me and I will pay out your earnings.");
 		add(ConversationStates.ATTENDING, "sell", null, null, ConversationStates.ATTENDING,	null, new PrepareOfferChatAction());
 		add(ConversationStates.ATTENDING, "show", null, ConversationStates.BUY_PRICE_OFFERED, null, new ShowOffersChatAction());
 		add(ConversationStates.ATTENDING, "fetch", null, ConversationStates.ATTENDING, null, new FetchEarningsChatAction());
 		add(ConversationStates.BUY_PRICE_OFFERED, "accept", null, ConversationStates.ATTENDING, null, new AcceptOfferChatAction());
 		add(ConversationStates.BUY_PRICE_OFFERED, "remove", null, ConversationStates.ATTENDING, null, new RemoveOfferChatAction());
 		add(ConversationStates.BUY_PRICE_OFFERED, "prolong", null, ConversationStates.ATTENDING, null, new ProlongOfferChatAction());
-		addGoodbye("Visit me again to see players offers, put a new offer or fetch your earnings!");
+		addGoodbye("Visit me again to see available offers, make a new offer or fetch your earnings!");
 	}
 	public Map<String, Map<String, Offer>> getOfferMap() {
 		return offerMap;
