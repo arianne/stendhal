8bd652e79e038f85284c31c28b8cf74b8959aa87
Kimmo Rundelin <kiheru@users.sourceforge.net>
2009-11-17 23:20:54 +0000

    use ItemTransformer

--- src/games/stendhal/server/entity/mapstuff/chest/StoredChest.java
+++ src/games/stendhal/server/entity/mapstuff/chest/StoredChest.java
@@ -3,15 +3,14 @@ package games.stendhal.server.entity.mapstuff.chest;
 import games.stendhal.common.Grammar;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.engine.transformer.ItemTransformer;
 import games.stendhal.server.core.events.TurnListener;
 import games.stendhal.server.entity.item.Item;
-import games.stendhal.server.entity.item.StackableItem;
 import games.stendhal.server.entity.slot.ChestSlot;
 
 import java.util.LinkedList;
 import java.util.List;
 
-import marauroa.common.game.RPClass;
 import marauroa.common.game.RPObject;
 import marauroa.common.game.RPSlot;
 
@@ -75,83 +74,29 @@ public class StoredChest extends Chest {
 
 			final RPSlot newSlot = new ChestSlot(this);
 			addSlot(newSlot);
-			// does this code look familiar to you? well it might, it's from
-			// PlayerRPClass. :*
-			for (final RPObject item : objects) {
+			
+			// Restore the stored items
+			ItemTransformer transformer = new ItemTransformer();
+			for (final RPObject rpobject : objects) {
 				try {
-					// We simply ignore corpses...
-					if (item.get("type").equals("item")) {
-						// TODO: what about items whose names have changed?
-						// the update converter is in player :(
-						final String name = item.get("name");
-						final Item entity = SingletonRepository
-								.getEntityManager().getItem(name);
-
-						// log removed items
-						if (entity == null) {
-							int quantity = 1;
-							if (item.has("quantity")) {
-								quantity = item.getInt("quantity");
-							}
-							logger.warn("Cannot restore " + quantity + " "
-									+ name + " of stored chest "
-									+ " because this item"
-									+ " was removed from items.xml");
-							continue;
+					Item item = transformer.transform(rpobject);
+					
+					// log removed items
+					if (item == null) {
+						int quantity = 1;
+						if (rpobject.has("quantity")) {
+							quantity = rpobject.getInt("quantity");
 						}
-
-						entity.setID(item.getID());
-
-						if (item.has("persistent")
-								&& (item.getInt("persistent") == 1)) {
-							/*
-							 * Keep [new] rpclass
-							 */
-							final RPClass rpclass = entity.getRPClass();
-							entity.fill(item);
-							entity.setRPClass(rpclass);
-
-							// If we've updated the item name we don't want
-							// persistent reverting it
-							entity.put("name", name);
-						}
-
-						if (entity instanceof StackableItem) {
-							int quantity = 1;
-							if (item.has("quantity")) {
-								quantity = item.getInt("quantity");
-							} else {
-								logger.warn("Adding quantity=1 to " + item
-									+ ". Most likely cause is that this item was not stackable in the past");
-							}
-							((StackableItem) entity).setQuantity(quantity);
-
-							if (quantity <= 0) {
-								logger.warn("Ignoring item " + name
-									+ " on restore of stored chest"
-									+ " because this item has an invalid quantity: "
-									+ quantity);
-								continue;
-							}
-						}
-
-						// make sure saved individual information is restored
-						final String[] individualAttributes = { "infostring", "description", "bound", "undroppableondeath" };
-						for (final String attribute : individualAttributes) {
-							if (item.has(attribute)) {
-								entity.put(attribute, item.get(attribute));
-							}
-						}
-
-						if (item.has("logid")) {
-							entity.put("logid", item.get("logid"));
-						}
-						newSlot.add(entity);
-					} else {
-						logger.warn("Non-item object found in stored chest: " + item);
+						logger.warn("Cannot restore " + quantity + " "
+								+ rpobject.get("name") + " of stored chest "
+								+ " because this item"
+								+ " was removed from items.xml");
+						continue;
 					}
+
+					newSlot.add(item);
 				} catch (final Exception e) {
-					logger.error("Error adding " + item + " to stored chest slot", e);
+					logger.error("Error adding " + rpobject + " to stored chest slot", e);
 				}
 			}
 		}
