7ac29c81cf0a5fe431757e5dcdb4f40c3adfbe1a
Kimmo Rundelin <kiheru@users.sourceforge.net>
2009-11-19 12:46:09 +0000

    Do not listen to logins anymore trying to relabel the house doors. All owned houses have the right owner name by now.

--- src/games/stendhal/server/maps/quests/houses/HouseBuyingMain.java
+++ src/games/stendhal/server/maps/quests/houses/HouseBuyingMain.java
@@ -1,16 +1,8 @@
 package games.stendhal.server.maps.quests.houses;
 
-import games.stendhal.common.MathHelper;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
-import games.stendhal.server.core.events.LoginListener;
-import games.stendhal.server.entity.mapstuff.portal.HousePortal;
 import games.stendhal.server.entity.npc.SpeakerNPC;
-import games.stendhal.server.entity.player.Player;
-
-import java.util.List;
-
-import org.apache.log4j.Logger;
 
 /**
  * Controls house buying.
@@ -18,11 +10,8 @@ import org.apache.log4j.Logger;
  * @author kymara
  */
 
-public class HouseBuyingMain implements LoginListener {
+public class HouseBuyingMain {
 	static HouseTax houseTax;
-
-	/** the logger instance. */
-	private static final Logger logger = Logger.getLogger(HouseBuyingMain.class);
 
 	/** Kalavan house seller Zone name. */
 	private static final String KALAVAN_CITY = "0_kalavan_city";
@@ -77,36 +66,10 @@ public class HouseBuyingMain implements LoginListener {
 		athor_island_zone.add(npc4);
 	}
 
-	// we'd like to update houses sold before release of 0.73 with the owner name
-	// when a player logs in we see if they own a house and we get the number from the house slot
-	// this can be removed after all previously owned portals would have expired unless player has logged in to pay tax 
-	// as by then unclaimed houses will be reclaimed by state
-	// this will be 6? months after release of 0.73
-	public void onLoggedIn(final Player player) {
-		final String name = player.getName();
-		if (player.hasQuest(HouseSellerNPCBase.QUEST_SLOT) && !"postman".equals(name)) {
-			
-			// note we default to a DIFFERENT value than the default house number incase neither found for some bad reasons
-			final int id = MathHelper.parseIntDefault(player.getQuest(HouseSellerNPCBase.QUEST_SLOT), -1);
-			logger.debug("Found that " + name + " owns house " + Integer.toString(id));
-			// Now look for the house portal which matches this and update it to have the player name on it
-			final List<HousePortal> portals =  HouseUtilities.getHousePortals();
-			for (final HousePortal houseportal : portals) {
-				final String owner = houseportal.getOwner();
-				if ("an unknown owner".equals(owner)) {
-					final int number = houseportal.getPortalNumber();
-					if (number == id) {
-						houseportal.setOwner(name);
-						logger.debug(name + " owns house " + Integer.toString(id) + " and we labelled the house");
-						return;
-					}
-				}
-			}
-		}
-	}
-
 	public void addToWorld() {
+		// Start collecting taxes as well
 		houseTax = new HouseTax();
+		
 		kalavan_city_zone = SingletonRepository.getRPWorld().getZone(KALAVAN_CITY);
 		createNPC();
 
@@ -118,10 +81,5 @@ public class HouseBuyingMain implements LoginListener {
 
 		athor_island_zone = SingletonRepository.getRPWorld().getZone(ATHOR_ISLAND);
 		createNPC4();
-
-		SingletonRepository.getLoginNotifier().addListener(this);
-		
-		// Start collecting taxes as well
-		
 	}
 }
