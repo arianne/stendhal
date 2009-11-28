558d7ef6a0c04960826302f51b4fe683ceff7b7b
Hendrik Brummermann <nhnb@users.sourceforge.net>
2009-11-28 13:13:07 +0000

    code cleanup

--- src/games/stendhal/client/actions/SoundAction.java
+++ src/games/stendhal/client/actions/SoundAction.java
@@ -5,6 +5,7 @@ import games.stendhal.client.gui.chatlog.StandardEventLine;
 import games.stendhal.client.gui.wt.core.WtWindowManager;
 import games.stendhal.client.sound.SoundSystem;
 import games.stendhal.client.soundreview.SoundMaster;
+import games.stendhal.common.MathHelper;
 
 /**
  * Set sound characteristics.
@@ -25,35 +26,47 @@ class SoundAction implements SlashAction {
 		final String command = params[0];
 
 		if (command.equals("mute")) {
-			boolean play = Boolean.parseBoolean(WtWindowManager.getInstance().getProperty("sound.play", "true"));
-			play = !play;
-			WtWindowManager.getInstance().setProperty("sound.play", Boolean.toString(play));
-			SoundMaster.setMute(!play);
-			SoundSystem.get().setMute(!play);
+			toggleMute();
 		} else if (command.equals("volume")) {
-			if (params.length < 2) {
-				j2DClient.get().addEventLine(new StandardEventLine(
-				"/sound volume <vol>, with <vol> between 0 and 100."));
-			}
-			int vol;
-
-			try {
-				vol = Integer.parseInt(params[1]);
-			} catch (final NumberFormatException ex) {
-				vol = -1;
-			}
-
-			if ((vol < 0) || (vol > 100)) {
-				j2DClient.get().addEventLine(new StandardEventLine(
-						"Volume must be an integer between 0 and 100"));
-			} else {
-				WtWindowManager.getInstance().setProperty("sound.volume",
-						Integer.toString(vol));
-				SoundSystem.get().setVolume(vol);
-			}
+			volume(params);
 		}
 
 		return true;
+	}
+
+	/**
+	 * toggles the mute state of sounds.
+	 */
+	private void toggleMute() {
+		boolean play = Boolean.parseBoolean(WtWindowManager.getInstance().getProperty("sound.play", "true"));
+		play = !play;
+		WtWindowManager.getInstance().setProperty("sound.play", Boolean.toString(play));
+		SoundMaster.setMute(!play);
+		SoundSystem.get().setMute(!play);
+	}
+
+	/**
+	 * sets the volume of sounds
+	 *
+	 * @param params volume, &lt;vol&gt;
+	 */
+	private void volume(final String[] params) {
+		if (params.length < 2) {
+			j2DClient.get().addEventLine(new StandardEventLine(
+			"/sound volume <vol>, with <vol> between 0 and 100."));
+			return;
+		}
+
+		int vol = MathHelper.parseIntDefault(params[1], -1);
+
+		if ((vol < 0) || (vol > 100)) {
+			j2DClient.get().addEventLine(new StandardEventLine(
+					"Volume must be an integer between 0 and 100"));
+			return;
+		}
+
+		WtWindowManager.getInstance().setProperty("sound.volume", Integer.toString(vol));
+		SoundSystem.get().setVolume(vol);
 	}
 
 	/**
