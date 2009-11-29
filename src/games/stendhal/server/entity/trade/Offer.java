437fbd7ee22a6274d93a6cc36361a56b4e0a0fd2
Kimmo Rundelin <kiheru@users.sourceforge.net>
2009-11-29 14:35:23 +0000

    save timestamp with offers

--- src/games/stendhal/server/entity/trade/Offer.java
+++ src/games/stendhal/server/entity/trade/Offer.java
@@ -1,6 +1,5 @@
 package games.stendhal.server.entity.trade;
 
-
 import games.stendhal.server.core.engine.transformer.ItemTransformer;
 import games.stendhal.server.entity.PassiveEntity;
 import games.stendhal.server.entity.item.Item;
@@ -14,28 +13,30 @@ import org.apache.log4j.Logger;
 
 public class Offer extends PassiveEntity {
 	private static final Logger logger = Logger.getLogger(Offer.class);
-	
+
 	private static final String OFFERER_ATTRIBUTE_NAME = "offerer";
 
 	public static final String OFFER_ITEM_SLOT_NAME = "item";
 
 	public static final String OFFER_RPCLASS_NAME = "offer";
+	private static final String TIMESTAMP = "timestamp";
 
 	private Item item;
-	
+
 	private final Integer price;
 
 	private final String offerer;
-	
+
 	public static void generateRPClass() {
 		final RPClass offerRPClass = new RPClass(OFFER_RPCLASS_NAME);
 		offerRPClass.isA("entity");
 		offerRPClass.addAttribute("price", Type.INT);
 		offerRPClass.addAttribute("class", Type.STRING);
 		offerRPClass.addAttribute(OFFERER_ATTRIBUTE_NAME, Type.STRING);
+		offerRPClass.addAttribute(TIMESTAMP, Type.STRING);
 		offerRPClass.addRPSlot(OFFER_ITEM_SLOT_NAME, 1);
 	}
-	
+
 	/**
 	 * @param item
 	 */
@@ -43,25 +44,26 @@ public class Offer extends PassiveEntity {
 		super();
 		setRPClass("offer");
 		hide();
-		if (!this.hasSlot(OFFER_ITEM_SLOT_NAME)) {
+		if (!hasSlot(OFFER_ITEM_SLOT_NAME)) {
 			this.addSlot(OFFER_ITEM_SLOT_NAME);
 		}
 		if (item != null) {
-			this.getSlot(OFFER_ITEM_SLOT_NAME).add(item);
+			getSlot(OFFER_ITEM_SLOT_NAME).add(item);
 			this.item = item;
 		}
 		this.put("price", price.intValue());
 		this.price = price;
 		this.put(OFFERER_ATTRIBUTE_NAME, offerer);
 		this.offerer = offerer;
+		put(TIMESTAMP, Long.toString(System.currentTimeMillis()));
 	}
-	
+
 	public Offer(final RPObject object) {
 		this(null, Integer.valueOf(object.getInt("price")), object.get(OFFERER_ATTRIBUTE_NAME));
-		final RPObject itemObject = object.getSlot(OFFER_ITEM_SLOT_NAME).getFirst(); 
-		
+		final RPObject itemObject = object.getSlot(OFFER_ITEM_SLOT_NAME).getFirst();
+
 		final Item entity = new ItemTransformer().transform(itemObject);
-		
+
 		// log removed items
 		if (entity == null) {
 			int quantity = 1;
@@ -70,33 +72,42 @@ public class Offer extends PassiveEntity {
 			}
 			logger.warn("Cannot restore " + quantity + " "
 					+ itemObject.get("name") + " to offer "
-					+ " because this item"
-					+ " was removed from items.xml");
+					+ " because this item was removed from items.xml");
 			return;
 		}
-		
+
 		item = entity;
 		getSlot(OFFER_ITEM_SLOT_NAME).add(item);
+		put(TIMESTAMP, object.get(TIMESTAMP));
 	}
-
 
 	public final Item getItem() {
 		return item;
 	}
 
-
-
 	public final Integer getPrice() {
 		return price;
 	}
-
-
 
 	public final String getOfferer() {
 		return offerer;
 	}
 
-	
+	/**
+	 * Get the creation or renewal time of the offer.
+	 * 
+	 * @return Timestamp in milliseconds
+	 */
+	public long getTimestamp() {
+		long timeStamp = 0;
+		try {
+			timeStamp = Long.parseLong(get(TIMESTAMP));
+		} catch (final NumberFormatException e) {
+			logger.error("Invalid timestamp: " + get(TIMESTAMP), e);
+		}
+		return timeStamp;
+	}
+
 	@Override
 	public int hashCode() {
 		return HashCodeBuilder.reflectionHashCode(this);
@@ -104,8 +115,7 @@ public class Offer extends PassiveEntity {
 
 	@Override
 	public boolean equals(final Object obj) {
-		return EqualsBuilder.reflectionEquals(this, obj, false,
-				Offer.class);
+		return EqualsBuilder.reflectionEquals(this, obj, false, Offer.class);
 	}
-	
+
 }
