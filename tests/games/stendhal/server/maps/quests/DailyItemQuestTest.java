0204cb57e7722d774efe247da48c73e90a22107d
Kimmo Rundelin <kiheru@users.sourceforge.net>
2009-12-11 14:28:09 +0000

    changed to use SpeakerNPCTestHelper.getReply()

--- tests/games/stendhal/server/maps/quests/DailyItemQuestTest.java
+++ tests/games/stendhal/server/maps/quests/DailyItemQuestTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static utilities.SpeakerNPCTestHelper.getReply;
 
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
@@ -59,13 +60,13 @@ public class DailyItemQuestTest {
 		en = npc.getEngine();
 
 		en.step(player, "hi");
-		assertEquals("On behalf of the citizens of Ados, welcome.", npc.get("text"));
+		assertEquals("On behalf of the citizens of Ados, welcome.", getReply(npc));
 		en.step(player, "task");
-		assertTrue(npc.get("text").startsWith("Ados is in need of supplies. Go fetch "));
+		assertTrue(getReply(npc).startsWith("Ados is in need of supplies. Go fetch "));
 		en.step(player, "complete");
-		assertTrue(npc.get("text").startsWith("You didn't fetch "));
+		assertTrue(getReply(npc).startsWith("You didn't fetch "));
 		en.step(player, "bye");
-		assertEquals("Good day to you.", npc.get("text"));
+		assertEquals("Good day to you.", getReply(npc));
 		
 		player.setQuest(questSlot, "pina colada;100");
 		Item item = ItemTestHelper.createItem("pina colada");
@@ -73,49 +74,49 @@ public class DailyItemQuestTest {
 		final int xp = player.getXP();
 		
 		en.step(player, "hi");
-		assertEquals("On behalf of the citizens of Ados, welcome.", npc.get("text"));
+		assertEquals("On behalf of the citizens of Ados, welcome.", getReply(npc));
 		en.step(player, "complete");
 		assertFalse(player.isEquipped("pina colada"));
 		assertThat(player.getXP(), greaterThan(xp));
 		assertTrue(player.isQuestCompleted(questSlot));
 		// [10:50] kymara earns 455960 experience points. 
-		assertEquals("Good work! Let me thank you on behalf of the people of Ados!", npc.get("text"));
+		assertEquals("Good work! Let me thank you on behalf of the people of Ados!", getReply(npc));
 		en.step(player, "bye");
-		assertEquals("Good day to you.", npc.get("text"));
+		assertEquals("Good day to you.", getReply(npc));
 		
 		en.step(player, "hi");
-		assertEquals("On behalf of the citizens of Ados, welcome.", npc.get("text"));
+		assertEquals("On behalf of the citizens of Ados, welcome.", getReply(npc));
 		en.step(player, "task");
-		assertThat(npc.get("text"), 
+		assertThat(getReply(npc), 
 				isOneOf("I can only give you a new quest once a day. Please check back in 24 hours.",
 						"I can only give you a new quest once a day. Please check back in 1 day."));
 		en.step(player, "bye");
-		assertEquals("Good day to you.", npc.get("text"));
+		assertEquals("Good day to you.", getReply(npc));
 
 		// -----------------------------------------------
 		player.setQuest(questSlot, "done;0");
 		// [10:51] Changed the state of quest 'daily_item' from 'done;1219834233092;1' to 'done;0' 
 		en.step(player, "hi");
-		assertEquals("On behalf of the citizens of Ados, welcome.", npc.get("text"));
+		assertEquals("On behalf of the citizens of Ados, welcome.", getReply(npc));
 		en.step(player, "task");
-		assertTrue(npc.get("text").startsWith("Ados is in need of supplies. Go fetch "));
+		assertTrue(getReply(npc).startsWith("Ados is in need of supplies. Go fetch "));
 		en.step(player, "bye");
-		assertEquals("Good day to you.", npc.get("text"));
+		assertEquals("Good day to you.", getReply(npc));
 
 		// -----------------------------------------------
 
 		// [10:53] Changed the state of quest 'daily_item' from 'dwarf cloak;1219834342834;0' to 'dwarf cloak;0' 
 		player.setQuest(questSlot, "dwarf cloak;0");
 		en.step(player, "hi");
-		assertEquals("On behalf of the citizens of Ados, welcome.", npc.get("text"));
+		assertEquals("On behalf of the citizens of Ados, welcome.", getReply(npc));
 		en.step(player, "task");
-		assertEquals("You're already on a quest to fetch a dwarf cloak. Say #complete if you brought it! Perhaps there are no supplies of that left at all! You could fetch #another item if you like, or return with what I first asked you.", npc.get("text"));
+		assertEquals("You're already on a quest to fetch a dwarf cloak. Say #complete if you brought it! Perhaps there are no supplies of that left at all! You could fetch #another item if you like, or return with what I first asked you.", getReply(npc));
 		en.step(player, "another");
-		assertEquals("I see. Please, ask me for another #quest when you think you can help Ados again.", npc.get("text"));
+		assertEquals("I see. Please, ask me for another #quest when you think you can help Ados again.", getReply(npc));
 		en.step(player, "quest");
-		assertTrue(npc.get("text").startsWith("Ados is in need of supplies. Go fetch "));
+		assertTrue(getReply(npc).startsWith("Ados is in need of supplies. Go fetch "));
 		en.step(player, "bye");
-		assertEquals("Good day to you.", npc.get("text"));
+		assertEquals("Good day to you.", getReply(npc));
 		
 	}
 }
