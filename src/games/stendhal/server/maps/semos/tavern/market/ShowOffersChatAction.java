801b42e98f914379a88111c8e89a79bba012e31f
Kimmo Rundelin <kiheru@users.sourceforge.net>
2009-12-13 13:06:49 +0000

    implemented filtering offers like "show meat" "show black" "show helmets"

--- src/games/stendhal/server/maps/semos/tavern/market/ShowOffersChatAction.java
+++ src/games/stendhal/server/maps/semos/tavern/market/ShowOffersChatAction.java
@@ -1,5 +1,6 @@
 package games.stendhal.server.maps.semos.tavern.market;
 
+import games.stendhal.server.entity.item.Item;
 import games.stendhal.server.entity.npc.ChatAction;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.SpeakerNPC;
@@ -10,6 +11,10 @@ import games.stendhal.server.entity.trade.Market;
 import games.stendhal.server.entity.trade.Offer;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 import marauroa.common.game.RPObject;
 import marauroa.common.game.RPSlot;
@@ -40,6 +45,8 @@ public class ShowOffersChatAction implements ChatAction {
 		boolean filterForMine = false;
 		
 		Market market = TradeCenterZoneConfigurator.getShopFromZone(player.getZone());
+		
+		// Figure out what to look for
 		RPSlot offersSlot = market.getSlot(Market.OFFERS_SLOT_NAME);
 		if (onlyMyExpiredOffers) {
 			offersSlot = market.getSlot(Market.EXPIRED_OFFERS_SLOT_NAME);
@@ -48,14 +55,25 @@ public class ShowOffersChatAction implements ChatAction {
 		if (onlyMyOffers) {
 			filterForMine = true;
 		}
+		String wordFilter = null;
+		if (!filterForMine) {
+			wordFilter = getWordFilter(sentence);
+		}
+		
+		// Get the list of offers, and filter out all that we don't need
+		List<Offer> offers = getOffers(offersSlot);
+		if (filterForMine) {
+			filterForMine(offers, player);
+		}
+		if (wordFilter != null) {
+			filterForWord(offers, wordFilter);
+		}
 		
 		StringBuilder offersMessage = new StringBuilder();
 		MarketManagerNPC marketNPC = (MarketManagerNPC) npc;
 		marketNPC.getOfferMap().put(player.getName(),new HashMap<String, Offer>());
 		
-		int counter = 0;
-		counter = filterSlotContent(player, filterForMine, offersMessage,
-				counter, offersSlot, marketNPC);
+		int counter = buildMessage(offersMessage, offers, marketNPC.getOfferMap().get(player.getName()));
 		if (counter > 0) {
 			player.sendPrivateText(offersMessage.toString());
 		}
@@ -63,33 +81,6 @@ public class ShowOffersChatAction implements ChatAction {
 			String expiredAddition = onlyMyExpiredOffers ? "expired " : "";
 			player.sendPrivateText("There are currently no "+expiredAddition+"offers in the market.");
 		}
-	}
-
-	private int filterSlotContent(Player player, boolean onlyMyOffers,
-			StringBuilder offersMessage, int counter, RPSlot offersSlot,
-			MarketManagerNPC marketNPC) {
-		for (RPObject rpObject : offersSlot) {
-			if (rpObject.getRPClass().getName().equals(Offer.OFFER_RPCLASS_NAME)) {
-				Offer o = (Offer) rpObject;
-				if (onlyMyOffers && !o.getOfferer().equals(player.getName())) {
-					continue;
-				}
-				counter += 1;
-				if (counter > MAX_SHOWN_OFFERS) {
-					offersMessage.append("Only " + MAX_SHOWN_OFFERS + " first offers shown.");
-					return counter;
-				}
-				offersMessage.append(counter);
-				offersMessage.append(": ");
-				offersMessage.append(o.getItem().getName());
-				offersMessage.append(" for ");
-				offersMessage.append(o.getPrice());
-				offersMessage.append(" money");
-				offersMessage.append("\n");
-				marketNPC.getOfferMap().get(player.getName()).put(Integer.valueOf(counter).toString(), o);
-			}
-		}
-		return counter;
 	}
 
 	private boolean checkForMineFilter(Sentence sentence) {
@@ -110,4 +101,89 @@ public class ShowOffersChatAction implements ChatAction {
 		return false;
 	}
 	
+	private String getWordFilter(Sentence sentence) {
+		if (sentence.getObjectCount() > 0) {
+			// A proper recognised item name. Look for those
+			return sentence.getObject(0).getNormalized();
+		} else {
+			List<Expression> expressions = sentence.getExpressions();
+			// Filter for the first other word
+			if (expressions.size() > 1) {
+				return expressions.get(1).getNormalized();
+			}
+		}
+		
+		return null;
+	}
+	
+	private List<Offer> getOffers(RPSlot slot) {
+		LinkedList<Offer> offers = new LinkedList<Offer>();
+		for (RPObject rpObject : slot) {
+			offers.add((Offer) rpObject);
+		}
+		return offers;
+	}
+	
+	/**
+	 * Filter out offers that do not belong to a given player.
+	 * 
+	 * @param offers list of offers to be filtered
+	 * @param player player whose offers should be retained on the list
+	 */
+	private void filterForMine(List<Offer> offers, Player player) {
+		Iterator<Offer> it = offers.iterator();
+		while (it.hasNext()) {
+			if (!it.next().getOfferer().equals(player.getName())) {
+				it.remove();
+			}
+		}
+	}
+	
+	/**
+	 * Filter out offers that do not match a given word.
+	 *  
+	 * @param offers list of offers to be filtered
+	 * @param word a word to check in item name or type
+	 */
+	private void filterForWord(List<Offer> offers, String word) {
+		Iterator<Offer> it = offers.iterator();
+		while (it.hasNext()) {
+			Item item = it.next().getItem();
+			
+			if (!(item.getName().indexOf(word) != -1 || word.equals(item.getItemClass()))) {
+				it.remove();
+			}
+		}
+	}
+	
+	/**
+	 * Format a message out of an offer list, and update an offermap to match it.
+	 * 
+	 * @param message message to fill with the offer list
+	 * @param offers list of offers to format
+	 * @param map offermap to be filled
+	 * 
+	 * @return number of offers added to the list and map
+	 */
+	private int buildMessage(StringBuilder message, List<Offer> offers, Map<String, Offer> map) {
+		int counter = 0;
+		
+		for (Offer offer : offers) {
+			counter++;
+			if (counter > MAX_SHOWN_OFFERS) {
+				message.append("Only " + MAX_SHOWN_OFFERS + " first offers shown.");
+				return counter;
+			}
+			message.append(counter);
+			message.append(": ");
+			message.append(offer.getItem().getName());
+			message.append(" for ");
+			message.append(offer.getPrice());
+			message.append(" money");
+			message.append("\n");
+			map.put(Integer.toString(counter), offer);
+		}
+		
+		return counter;
+	}
 }
