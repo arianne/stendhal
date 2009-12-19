45f767c88026cb78f4c131c06c6111ffd9d2f5bd
Hendrik Brummermann <nhnb@users.sourceforge.net>
2009-12-19 19:17:28 +0000

    display the content of the show_item_list slot

--- src/games/stendhal/client/gui/imageviewer/ItemListImageViewerEvent.java
+++ src/games/stendhal/client/gui/imageviewer/ItemListImageViewerEvent.java
@@ -1,6 +1,7 @@
 package games.stendhal.client.gui.imageviewer;
 
 import java.awt.Dimension;
+import java.net.URL;
 
 import javax.swing.JLabel;
 
@@ -44,16 +45,9 @@ public class ItemListImageViewerEvent extends ViewPanel {
 
 		RPSlot slot = event.getSlot("content");
 		for (RPObject item : slot) {
-			html.append("<tr><td>");
-			html.append("img");
-			html.append("</td><td>");
-			html.append(item.get("price"));
-			html.append("</td><td>");
-			html.append(item.get("description"));
-			html.append("</td></tr>");
+			createRow(html, item);
 		}
-		
-		html.append("<tr><td>Club</td><th>1234</td><td>You see bla</td></tr>");
+
 		
 		html.append("</table></body></html>");
 
@@ -62,5 +56,52 @@ public class ItemListImageViewerEvent extends ViewPanel {
 		add(imageLabel);
 
 		setVisible(true);
+	}
+
+	/**
+	 * creates an item row
+	 *
+	 * @param html  out buffer to append to
+	 * @param item  RPObject representing an item to display
+	 */
+	private void createRow(StringBuilder html, RPObject item) {
+		URL url = getItemImageURL(item);
+		String price = getFormatedPrice(item);
+
+		html.append("<tr><td>");
+		html.append("<img src=\"" + url.toString() + "\">");
+		html.append("</td><td>");
+		html.append(price);
+		html.append("</td><td>");
+		html.append(item.get("description"));
+		html.append("</td></tr>");
+	}
+
+	/**
+	 * formats the price depending on its sign.
+	 *
+	 * @param RPObject representing an item to display 
+	 * @return html code to display price
+	 */
+	private String getFormatedPrice(RPObject item) {
+		String price = "";
+		if (item.has("price")) {
+			int priceInt = item.getInt("price");
+			price = "<span style=\"color: ";
+			if (priceInt < 0) {
+				price = price + "#F00\">";
+			} else {
+				price = price + "#0F0\">";
+			}
+			price = price + Math.abs(priceInt) + "</span>";
+		}
+		return price;
+	}
+
+	private URL getItemImageURL(RPObject item) {
+		String itemName = item.get("class") + "/" + item.get("subclass");
+		String imagePath = "/data/sprites/items/" + itemName + ".png";
+		URL url = this.getClass().getResource(imagePath);
+		return url;
 	}
 }
