c9a066b913e5187557546814fbdcd04a77068feb
Hendrik Brummermann <nhnb@users.sourceforge.net>
2009-12-20 00:12:39 +0000

    disabled defining of show_item_list because older maurauroa version cannot connect when this is defined

--- src/games/stendhal/server/entity/player/PlayerRPClass.java
+++ src/games/stendhal/server/entity/player/PlayerRPClass.java
@@ -118,7 +118,7 @@ public class PlayerRPClass {
 		
 		player.addRPEvent("transition_graph", Definition.PRIVATE);
 		player.addRPEvent("examine", Definition.PRIVATE);
-		player.addRPEventWithSlots("show_item_list", Definition.PRIVATE);
+		// player.addRPEventWithSlots("show_item_list", Definition.PRIVATE);
 	}
 
 }
