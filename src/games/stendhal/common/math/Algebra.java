50ee02a470e105afc327caba8c349936ae39265f
plassy <plassy@users.sourceforge.net>
2009-12-26 19:07:42 +0000

    changed default EPSILON value in common.math.Algebra

--- src/games/stendhal/common/math/Algebra.java
+++ src/games/stendhal/common/math/Algebra.java
@@ -13,7 +13,7 @@ import java.util.Arrays;
  */
 public final class Algebra
 {
-    public static float EPSILON = Float.MIN_VALUE;
+    public static float EPSILON = 0.0000001f;
 
     public static float[] vecf(float ...args)
     {
