f3d245491a16887838c0bf0e10b4a078e6bacdda
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-01-06 11:26:59 +0000

    set verrsion to 0.80

--- src/games/stendhal/client/update/Version.java
+++ src/games/stendhal/client/update/Version.java
@@ -10,7 +10,7 @@ package games.stendhal.client.update;
 public class Version {
 
 	/** Version Number. */
-	public static final String VERSION = "0.79.5";
+	public static final String VERSION = "0.80";
 
 	/**
 	 * Extract the specified number of parts from a version-string.
