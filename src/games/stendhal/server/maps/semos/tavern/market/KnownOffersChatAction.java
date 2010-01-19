b057b5c5d442d0010d2549eb3f3c07851ab9d21b
Kimmo Rundelin <kiheru@users.sourceforge.net>
2010-01-19 12:02:06 +0000

    Accept sentences of form "command number 1" in addition to "command 1"

--- src/games/stendhal/server/maps/semos/tavern/market/KnownOffersChatAction.java
+++ src/games/stendhal/server/maps/semos/tavern/market/KnownOffersChatAction.java
@@ -3,20 +3,27 @@ package games.stendhal.server.maps.semos.tavern.market;
 import games.stendhal.server.entity.npc.ChatAction;
 import games.stendhal.server.entity.npc.parser.Expression;
 import games.stendhal.server.entity.npc.parser.Sentence;
+import games.stendhal.server.entity.npc.parser.SimilarExprMatcher;
 /**
  * abstract class for handling offer numbers in sentences
  *  
  * @author madmetzger
  */
 public abstract class KnownOffersChatAction implements ChatAction {
-
 	protected Integer getOfferNumberFromSentence(Sentence sentence) {
-		Expression expression = sentence.getExpression(1,"");
-		if (expression == null) {
-			throw new NumberFormatException("no number provided");
+		SimilarExprMatcher matcher = new SimilarExprMatcher();
+		final int last = sentence.getExpressions().size();
+		for (Expression expr : sentence.getExpressions().subList(1, last)) {
+			if (matcher.match(expr, new Expression("number"))) {
+				/*
+				 * The player wrote either "command number", "command number <number>",
+				 * or something along those lines. Ignore the "number" parts until
+				 * we get to the actual numeral.
+				 */
+			} else {
+				return Integer.parseInt(expr.toString());
+			}
 		}
-		String offerNumber = expression.toString();
-		return Integer.parseInt(offerNumber);
+		throw new NumberFormatException("no number provided");
 	}
-
 }
