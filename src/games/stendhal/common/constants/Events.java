7917f9e02405311ac16f4fc5d1ff9d23ea173ae6
yoriy <yoriy@users.sourceforge.net>
2010-01-28 12:10:38 +0000

    added descriptions for start and stop attack events

--- src/games/stendhal/common/constants/Events.java
+++ src/games/stendhal/common/constants/Events.java
@@ -7,5 +7,9 @@ public final class Events {
 	public static final String PRIVATE_TEXT = "private_text";
 	// TODO value other then "text" lead to exceptions, fix it.	
 	public static final String PUBLIC_TEXT = "text";
+	
+	public static final String START_ATTACK = "start_attack";
+	
+	public static final String STOP_ATTACK = "stop_attack";
 
 }
