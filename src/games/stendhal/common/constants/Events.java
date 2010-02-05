e7315a84bd279326f450cc5e83c31fab45e458eb
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-02-05 20:58:56 +0000

    removed unused event definition for open_offer_panel

--- src/games/stendhal/common/constants/Events.java
+++ src/games/stendhal/common/constants/Events.java
@@ -5,8 +5,6 @@ package games.stendhal.common.constants;
  */
 public final class Events {
 	
-	public static final String OPEN_OFFER_PANEL = "open_offer_panel";
-	
 	public static final String PRIVATE_TEXT = "private_text";
 	// TODO value other then "text" lead to exceptions, fix it.	
 	public static final String PUBLIC_TEXT = "text";
