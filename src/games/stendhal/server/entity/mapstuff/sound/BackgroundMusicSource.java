59600e52e98227339d1ae30f592171e706fc80ca
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-02-06 00:18:41 +0000

    implemented LoopedAmbientSoundSource and BackgroundMusicSource

--- src/games/stendhal/server/entity/mapstuff/sound/BackgroundMusicSource.java
+++ src/games/stendhal/server/entity/mapstuff/sound/BackgroundMusicSource.java
@@ -7,13 +7,13 @@ import games.stendhal.common.constants.SoundLayer;
  *
  * @author hendrik
  */
-public class LoopedAmbientSoundSource extends LoopedSoundSource {
+public class BackgroundMusicSource extends LoopedSoundSource  {
 
 	/**
 	 * Create an ambient sound area.
 	 */
-	public LoopedAmbientSoundSource(String sound, int radius, int volume) {
-		super(sound, radius, volume, SoundLayer.AMBIENT_SOUND);
+	public BackgroundMusicSource(String sound, int radius, int volume, SoundLayer layer) {
+		super(sound, radius, volume, SoundLayer.BACKGROUND_MUSIC);
 	}
 
 }
---
+++ src/games/stendhal/server/entity/mapstuff/sound/LoopedSoundSource.java
@@ -0,0 +1,51 @@
+package games.stendhal.server.entity.mapstuff.sound;
+
+import games.stendhal.common.constants.SoundLayer;
+import games.stendhal.server.entity.PassiveEntity;
+import marauroa.common.game.RPClass;
+import marauroa.common.game.Definition.DefinitionClass;
+import marauroa.common.game.Definition.Type;
+
+/**
+ * Plays a sound and music in a loop.
+ *
+ * @author hendrik
+ */
+public class LoopedSoundSource extends PassiveEntity {
+	private static final String RPCLASS_NAME = "looped_sound_source";
+
+
+	/**
+	 * Create an ambient sound area.
+	 */
+	public LoopedSoundSource() {
+		setRPClass(RPCLASS_NAME);
+		put("type", RPCLASS_NAME);
+	}
+
+
+	/**
+	 * Create an ambient sound area.
+	 */
+	public LoopedSoundSource(String sound, int radius, int volume, SoundLayer layer) {
+		setRPClass(RPCLASS_NAME);
+		put("type", RPCLASS_NAME);
+		put("sound", sound);
+		put("radius", radius);
+		put("volume", volume);
+	}
+
+
+	/**
+	 * generates the RPClass
+	 */
+	public static void generateRPClass() {
+		final RPClass rpclass = new RPClass(RPCLASS_NAME);
+		rpclass.isA("area");
+		rpclass.addAttribute("sound", Type.STRING);
+		rpclass.addAttribute("radius", Type.INT);
+		rpclass.addAttribute("volume", Type.BYTE);
+		rpclass.add(DefinitionClass.ATTRIBUTE, "layer", Type.BYTE);
+	}
+
+}
