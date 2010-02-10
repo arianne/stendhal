6e296d0a19abff90c045d9e6df50f9c385db792a
yoriy <yoriy@users.sourceforge.net>
2010-02-10 21:39:04 +0000

    removed TODO as obsoleted.

--- src/games/stendhal/common/constants/Events.java
+++ src/games/stendhal/common/constants/Events.java
@@ -6,7 +6,7 @@ package games.stendhal.common.constants;
 public final class Events {
 	
 	public static final String PRIVATE_TEXT = "private_text";
-	// TODO value other then "text" lead to exceptions, fix it.	
+	
 	public static final String PUBLIC_TEXT = "text";
 
 	/** a sound event */
