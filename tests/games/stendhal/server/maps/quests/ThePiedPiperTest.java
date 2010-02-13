26113f8571b63b6591e64b283ecaadaa1ce896ef
yoriy <yoriy@users.sourceforge.net>
2010-02-13 22:29:33 +0000

    removed unused parts, split quest testing

--- tests/games/stendhal/server/maps/quests/ThePiedPiperTest.java
+++ tests/games/stendhal/server/maps/quests/ThePiedPiperTest.java
@@ -27,7 +27,8 @@ public class ThePiedPiperTest {
 	private Player player = null;
 	private SpeakerNPC npc = null;
 	private Engine en = null;
-
+	final static ThePiedPiper quest = new ThePiedPiper();
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		QuestHelper.setUpBeforeClass();
@@ -37,8 +38,6 @@ public class ThePiedPiperTest {
 		final StendhalRPZone zone = new StendhalRPZone("admin_test");
 		new MayorNPC().configureZone(zone, null);
 
-
-		final AbstractQuest quest = new ThePiedPiper();
 		quest.addToWorld();
 
 	}
@@ -81,10 +80,13 @@ public class ThePiedPiperTest {
 		en.step(player, "hi");
 		assertEquals("On behalf of the citizens of Ados, welcome.", getReply(npc));
 		en.step(player, "rats");
-		assertEquals("There are still about 27 rats alive.", getReply(npc));
+		assertEquals("There are still about "+quest.getRatsCount()+" rats alive.", getReply(npc));
 		en.step(player, "bye");
 		assertEquals("Good day to you.", getReply(npc));
-
+	}
+	
+	@Ignore
+	public void testQuest3() {
 
 		// [17:58] Mayor Chalmers shouts: No rats in Ados now, exclude those who always lived in storage and haunted house. Rats hunters are welcome to get their reward.
 		en.step(player, "hi");
@@ -99,7 +101,10 @@ public class ThePiedPiperTest {
 		assertEquals("Here is your 1050 money, thank you very much for your help.", getReply(npc));
 		en.step(player, "bye");
 		assertEquals("Good day to you.", getReply(npc));
-
+	}
+	
+	@Ignore
+	public void testQuest4() {
 		// [18:09] Mayor Chalmers shouts: Ados city is under rats invasion! Anyone who will help to clean up city, will be rewarded!
 
 		en.step(player, "hi");
@@ -118,7 +123,10 @@ public class ThePiedPiperTest {
 		assertEquals("Good day to you.", getReply(npc));
 		// [18:14] rat has been killed by helga
 		// [18:14] helga earns 5 experience points.
-
+	}
+	
+	@Ignore
+	public void testQuest5() {	
 		// [18:19] Mayor Chalmers shouts: Saddanly, rats captured city, they are living now under all Ados buildings. I am now in need of call Piped Piper, rats exterminator. Thank to all who tryed to clean up Ados,  you are welcome to get your reward.
 
 		en.step(player, "hi");
@@ -133,7 +141,10 @@ public class ThePiedPiperTest {
 		assertEquals("Here is your 10 money, thank you very much for your help.", getReply(npc));
 		en.step(player, "bye");
 		assertEquals("Good day to you.", getReply(npc));
-
+	}
+	
+	@Ignore
+	public void testQuest6() {
 		// [19:20] Mayor Chalmers shouts: Thanx gods, rats is gone now, Pied Piper hypnotized them and lead away to dungeons. Those of you, who helped to Ados city with rats problem, can get your reward now.
 		en.step(player, "hi");
 		assertEquals("On behalf of the citizens of Ados, welcome.", getReply(npc));
@@ -156,74 +167,6 @@ public class ThePiedPiperTest {
 		assertEquals("You didn't kill any rats which invaded the city, so you don't deserve a reward.", getReply(npc));
 		en.step(player, "bye");
 		assertEquals("Good day to you.", getReply(npc));
-
-		// [19:31] Mayor Chalmers shouts: Ados city is under rats invasion! Anyone who will help to clean up city, will be rewarded!
-		en.step(player, "hi");
-		assertEquals("On behalf of the citizens of Ados, welcome.", getReply(npc));
-		en.step(player, "reward");
-		assertEquals("Ados is being invaded by rats! "+
-				  "I dont want to reward you now, "+
-  				  " until all rats are dead.", getReply(npc));
-		en.step(player, "bye");
-		assertEquals("Good day to you.", getReply(npc));
-
-		en.step(player, "hi");
-		assertEquals("On behalf of the citizens of Ados, welcome.", getReply(npc));
-		en.step(player, "rats");
-		assertEquals("There are still about 2 rats alive.", getReply(npc));
-		assertEquals("Good day to you.", getReply(npc));
-
-		// [19:42] Mayor Chalmers shouts: No rats in Ados now, exclude those who always lived in storage and haunted house. Rats hunters are welcome to get their reward.
-		en.step(player, "hi");
-		assertEquals("On behalf of the citizens of Ados, welcome.", getReply(npc));
-		en.step(player, "rats");
-		assertEquals("Ados isn't being invaded by rats right now. You can still "+
-				  "get a #reward for the last time you helped. You can ask for #details "+
-				  "if you want.", getReply(npc));
-		en.step(player, "details");
-		assertEquals("Well, from the last reward, you killed 23 rats, 3 caverats, 3 venomrats, a razorrat, a giantrat, and 3 archrats, so I will give you 3510 money as a reward for that job.", getReply(npc));
-		en.step(player, "bye");
-		assertEquals("Good day to you.", getReply(npc));
-
-		// NOTE: this is testing a different quest
-		en.step(player, "hi");
-		assertEquals("On behalf of the citizens of Ados, welcome.", getReply(npc));
-		en.step(player, "quest");
-		assertEquals("Ados is in need of supplies. Go fetch a tuna sandwich and say complete, once you've brought it.", getReply(npc));
-		en.step(player, "done");
-		assertEquals("You didn't fetch a tuna sandwich yet. Go and get it and say complete only once you're done.", getReply(npc));
-		en.step(player, "done");
-		assertEquals("Good work! Let me thank you on behalf of the people of Ados!", getReply(npc));
-		// [19:51] helga earns 1281887 experience points.
-		en.step(player, "bye");
-		assertEquals("Good day to you.", getReply(npc));
-
-		// [19:52] Mayor Chalmers shouts: Ados city is under rats invasion! Anyone who will help to clean up city, will be rewarded!
-		// [19:52] caverat has been killed by helga
-		// [19:52] helga earns 10 experience points.
-
-		en.step(player, "hi");
-		assertEquals("On behalf of the citizens of Ados, welcome.", getReply(npc));
-		en.step(player, "rats");
-		assertEquals("There are still about 37 rats alive.", getReply(npc));
-		en.step(player, "details");
-		assertEquals("Well, from the last reward, you killed 23 rats, 4 caverats, 3 venomrats, a razorrat, a giantrat, and 3 archrats, so I will give you 3530 money as a reward for that job.", getReply(npc));
-		assertEquals("Good day to you.", getReply(npc));
-
-		// [19:52] razorrat has been killed by helga
-		// [19:52] helga earns 80 experience points.
-		en.step(player, "hi");
-		assertEquals("On behalf of the citizens of Ados, welcome.", getReply(npc));
-		en.step(player, "details");
-		assertEquals("Well, from last reward, you killed 23 rats, 4 caverats, 3 venomrats, 2 razorrats, a giantrat, and 3 archrats, so I will give you 3690 money as a reward for that job.", getReply(npc));
-		en.step(player, "reward");
-		assertEquals("Ados is being invaded by rats! "+
-				  "I dont want to reward you now, "+
-  				  " until all rats are dead.", getReply(npc));
-		en.step(player, "bye");
-		assertEquals("Good day to you.", getReply(npc));
-
-		// -----------------------------------------------
-
 	}
+
 }
