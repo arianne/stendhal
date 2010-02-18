85dd793cdac3f51248c8b4fb050a51b11b5b1cd6
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-02-18 18:22:56 +0000

    increased version number

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-STENDHAL_VERSION="0.80"
+STENDHAL_VERSION="0.80.1"
 
 LOCALCLASSPATH=.:data/script/:data/conf/:stendhal-server-$STENDHAL_VERSION.jar:marauroa.jar:mysql-connector.jar:log4j.jar:commons-lang.jar
 
--- src/games/stendhal/client/update/Version.java
+++ src/games/stendhal/client/update/Version.java
@@ -10,7 +10,7 @@ package games.stendhal.client.update;
 public class Version {
 
 	/** Version Number. */
-	public static final String VERSION = "0.80";
+	public static final String VERSION = "0.80.1";
 
 	/**
 	 * Extract the specified number of parts from a version-string.
