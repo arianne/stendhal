1a1d36cc427028c4dc78666912dfaa904a1e7e6d
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-02-18 21:11:51 +0000

    implement Cloneable

--- src/games/stendhal/tools/loganalyser/itemlog/contraband/ItemInfo.java
+++ src/games/stendhal/tools/loganalyser/itemlog/contraband/ItemInfo.java
@@ -5,33 +5,41 @@ package games.stendhal.tools.loganalyser.itemlog.contraband;
  *
  * @author hendrik
  */
-public class ItemInfo {
+public class ItemInfo implements Cloneable {
 
 	private String name;
 	private String itemid;
 	private String quantity;
 	private String owner;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getItemid() {
 		return itemid;
 	}
+
 	public void setItemid(String itemid) {
 		this.itemid = itemid;
 	}
+
 	public String getQuantity() {
 		return quantity;
 	}
+
 	public void setQuantity(String quantity) {
 		this.quantity = quantity;
 	}
+
 	public String getOwner() {
 		return owner;
 	}
+
 	public void setOwner(String owner) {
 		this.owner = owner;
 	}
