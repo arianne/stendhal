a0f5077ab05c891bcf7242d00a5d0a7129296285
plassy <plassy@users.sourceforge.net>
2010-02-24 16:10:07 +0000

    added method to create a homogenous vector. added a method to create a matrix out of a list of vectors. added a method to multiply two matrices.

--- src/games/stendhal/common/math/Algebra.java
+++ src/games/stendhal/common/math/Algebra.java
@@ -17,6 +17,20 @@ public final class Algebra
 
     public static float[] vecf(float ...args)
     {
+		assert args.length > 0;
+        return args;
+    }
+
+	public static float[] homogenousf(float ...args)
+    {
+		assert args.length > 0;
+
+		int   l = args.length - 1;
+		float w = args[l];
+
+		for(int i=0; i<l; ++i)
+			args[i] *= w;
+
         return args;
     }
 
@@ -40,7 +54,9 @@ public final class Algebra
 
     public static void mov_Vecf(float[] result, float[] vec)
     {
-        assert result.length == vec.length;
+		assert result != null: "method argument 1 is null";
+		assert vec    != null: "method argument 2 is null";
+		assert result.length == vec.length;
 
         for(int i=0; i<vec.length; ++i)
             result[i] = vec[i];
@@ -48,12 +64,16 @@ public final class Algebra
 
     public static void mov_Vecf(float[] result, float a)
     {
+		assert result != null: "method argument 1 is null";
         Arrays.fill(result, a);
     }
 
     public static void add_Vecf(float[] result, float[] a, float[] b)
     {
-        assert result.length == a.length && a.length == b.length;
+		assert result != null: "method argument 1 is null";
+		assert a      != null: "method argument 2 is null";
+		assert b      != null: "method argument 3 is null";
+		assert result.length == a.length && a.length == b.length;
 
         for(int i=0; i<a.length; ++i)
             result[i] = a[i] + b[i];
@@ -61,7 +81,9 @@ public final class Algebra
 
     public static void add_Vecf(float[] result, float[] a, float b)
     {
-        assert result.length == a.length;
+		assert result != null: "method argument 1 is null";
+		assert a      != null: "method argument 2 is null";
+		assert result.length == a.length;
 
         for(int i=0; i<a.length; ++i)
             result[i] = a[i] + b;
@@ -69,6 +91,9 @@ public final class Algebra
 
     public static void sub_Vecf(float[] result, float[] a, float[] b)
     {
+		assert result != null: "method argument 1 is null";
+		assert a      != null: "method argument 2 is null";
+		assert b      != null: "method argument 3 is null";
         assert result.length == a.length && a.length == b.length;
 
         for(int i=0; i<a.length; ++i)
@@ -77,6 +102,8 @@ public final class Algebra
 
     public static void sub_Vecf(float[] result, float[] a, float b)
     {
+		assert result != null: "method argument 1 is null";
+		assert a      != null: "method argument 2 is null";
         assert result.length == a.length;
 
         for(int i=0; i<a.length; ++i)
@@ -85,6 +112,9 @@ public final class Algebra
 
     public static void mul_Vecf(float[] result, float[] a, float[] b)
     {
+		assert result != null: "method argument 1 is null";
+		assert a      != null: "method argument 2 is null";
+		assert b      != null: "method argument 3 is null";
         assert result.length == a.length && a.length == b.length;
 
         for(int i=0; i<a.length; ++i)
@@ -93,6 +123,8 @@ public final class Algebra
 
     public static void mul_Vecf(float[] result, float[] a, float b)
     {
+		assert result != null: "method argument 1 is null";
+		assert a      != null: "method argument 2 is null";
         assert result.length == a.length;
 
         for(int i=0; i<a.length; ++i)
@@ -101,6 +133,9 @@ public final class Algebra
 
     public static void div_Vecf(float[] result, float[] a, float[] b)
     {
+		assert result != null: "method argument 1 is null";
+		assert a      != null: "method argument 2 is null";
+		assert b      != null: "method argument 3 is null";
         assert result.length == a.length && a.length == b.length;
 
         for(int i=0; i<a.length; ++i)
@@ -109,6 +144,8 @@ public final class Algebra
 
     public static void div_Vecf(float[] result, float[] a, float b)
     {
+		assert result != null: "method argument 1 is null";
+		assert a      != null: "method argument 2 is null";
         assert result.length == a.length;
 
         for(int i=0; i<a.length; ++i)
@@ -117,6 +154,8 @@ public final class Algebra
 
     public static float dot_Vecf(float[] a, float[] b)
     {
+		assert a      != null: "method argument 1 is null";
+		assert b      != null: "method argument 2 is null";
         assert a.length == b.length;
 
         float result = 0.0f;
@@ -129,6 +168,9 @@ public final class Algebra
 
     public static void cross_Vec3f(float[] result, float[] a, float[] b)
     {
+		assert result != null: "method argument 1 is null";
+		assert a      != null: "method argument 2 is null";
+		assert b      != null: "method argument 3 is null";
         assert result.length == 3 && a.length == 3 && b.length == 3;
 
         float x =  a[1] * b[2] - b[1] * a[2];
@@ -142,11 +184,13 @@ public final class Algebra
 
     public static void cross_Vec2f(float[] result, float[] a)
     {
+		assert result != null: "method argument 1 is null";
+		assert a      != null: "method argument 2 is null";
         assert result.length == 2 && a.length == 2;
 
         float x = a[0];
         float y = a[1];
-        
+
         result[0] =  y;
         result[1] = -x;
     }
@@ -161,18 +205,23 @@ public final class Algebra
         return (float)Math.sqrt(dot_Vecf(v,v));
     }
 
-    public static void normalize_Vecf(float[] result, float[] v)
+
+    public static void normalize_Vecf(float[] result, float[] a)
     {
-        assert result.length == v.length;
+		assert result != null: "method argument 1 is null";
+		assert a      != null: "method argument 2 is null";
+        assert result.length == a.length;
 
-        float length = 1.0f / length_Vecf(v);
+        float length = 1.0f / length_Vecf(a);
 
-        for(int i=0; i<v.length; ++i)
-            result[i] = v[i] * length;
+        for(int i=0; i<a.length; ++i)
+            result[i] = a[i] * length;
     }
 
     public static float distanceSqrt_Vecf(float[] a, float[] b)
     {
+		assert a      != null: "method argument 1 is null";
+		assert b      != null: "method argument 2 is null";
         assert a.length == b.length;
 
         float temp;
@@ -191,4 +240,81 @@ public final class Algebra
     {
         return (float)Math.sqrt(distanceSqrt_Vecf(a, b));
     }
+
+	// ------------------------------------------------------------- //
+
+	public static float[] mat(boolean horizontalVectors, float[] ...vectors)
+	{
+		assert vectors.length    != 0;
+		assert vectors[0]        != null;
+		assert vectors[0].length != 0;
+
+		int     vecLength = vectors[0].length;
+		int     numVecs   = vectors.length;
+		float[] result    = new float[numVecs * vecLength];
+		int     index     = 0;
+
+		if(horizontalVectors)
+		{
+			for(float[] vec: vectors)
+			{
+				assert vec.length == vecLength: "all vectors must have equal length";
+
+				System.arraycopy(vec, 0, result, index, vecLength);
+				index += vecLength;
+			}
+		}
+		else
+		{
+			for(float[] vec: vectors)
+			{
+				assert vec.length == vecLength: "all vectors must have equal length";
+
+				int row = index;
+
+				for(float val: vec)
+				{
+					result[row] = val;
+					row += numVecs;
+				}
+
+				++index;
+			}
+		}
+
+		return result;
+	}
+
+	public static float[] mul_mat(float[] a, int rowsA, int colsA, float[] b, int rowsB, int colsB)
+	{
+		assert colsA == rowsB;
+		assert (rowsA * colsA) != 0;
+		assert (rowsB * colsB) != 0;
+
+		int     resultSize = rowsA * colsB;
+		float[] result     = new float[resultSize];
+		int     ra         = 0; // current row for array a
+		int     rr         = 0; // current row for array result
+
+		Arrays.fill(result, 0.0f);
+
+		while(ra < resultSize)
+		{
+			for(int cb=0; cb<colsB; ++cb)
+			{
+				int ib = cb; // current index for array b
+
+				for(int d=0; d<colsA; ++d)
+				{
+					result[rr + cb] += a[ra + d] * b[ib];
+					ib += colsB;
+				}
+			}
+
+			ra += colsA;
+			rr += colsB;
+		}
+
+		return result;
+	}
 }
