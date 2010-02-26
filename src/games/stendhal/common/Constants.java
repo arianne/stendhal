725e1bca50fb871a720eaae8830a992488fb6843
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-02-26 23:06:22 +0000

    removed unused code

--- src/games/stendhal/common/Constants.java
+++ src/games/stendhal/common/Constants.java
@@ -1,26 +1,12 @@
 package games.stendhal.common;
-
-public interface Constants {
+public final class Constants {
 	
-	String ACCEPT_OFFER_TYPE = "ACCEPT_OFFER";
-	
-	String ADD_OFFER_TYPE = "ADD_OFFER";
-	
-	String OFFER_ITEM = "item";
-	
-	String OFFER_PRICE = "price";
-	
-	String OFFER_GOODS = "goods";
-	
-	String OFFER_OFFERERNAME = "offererName";
-	
-	String ACTION_TYPE = "type";
-
 	/**
 	 * All the slots considered to be "with" the entity. Listed in priority
 	 * order (i.e. bag first).
 	 */
-	String[] CARRYING_SLOTS = { "bag", "head", "rhand",
+	// TODO: let the slots decide that themselves
+	public static final String[] CARRYING_SLOTS = { "bag", "head", "rhand",
 			"lhand", "armor", "finger", "cloak", "legs", "feet", "keyring" };
 
 }
