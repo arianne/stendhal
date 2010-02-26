32c439ae95f8574a64a2c7847bde3f8851053927
Kimmo Rundelin <kiheru@users.sourceforge.net>
2010-02-26 23:14:23 +0000

    made spouse slot static. javadoc.

--- src/games/stendhal/server/maps/quests/marriage/MarriageQuestInfo.java
+++ src/games/stendhal/server/maps/quests/marriage/MarriageQuestInfo.java
@@ -5,25 +5,49 @@ import games.stendhal.server.entity.player.Player;
 class MarriageQuestInfo {
 	private static final String QUEST_SLOT = "marriage";
 
-	// The spouse's name is stored in one of the player's quest slots.
-	// This is necessary to disallow polygamy.
-	private final String SPOUSE_QUEST_SLOT = "spouse";
+	/**
+	 * Name of the spouse slot.
+	 * The spouse's name is stored in one of the player's quest slots.
+	 * This is necessary to disallow polygamy.
+	 */
+	private static final String SPOUSE_QUEST_SLOT = "spouse";
 	
+	/**
+	 * Get the name of the quest slot.
+	 * 
+	 * @return quest slot name
+	 */
 	public String getQuestSlot() {
 		return QUEST_SLOT;
 	}
 	
+	/**
+	 * Get the name of the spouse slot.
+	 * 
+	 * @return spouse slot name
+	 */
 	public String getSpouseQuestSlot() {
 		return SPOUSE_QUEST_SLOT;
 	}
 	
+	/**
+	 * Check if a player is married to another player.
+	 * 
+	 * @param player the player to be checked
+	 * @return true iff the player is married
+	 */
 	public boolean isMarried(final Player player) {
 		return player.hasQuest(SPOUSE_QUEST_SLOT);
 	} 
 	
+	/**
+	 * Check if a player is engaged with another player.
+	 * 
+	 * @param player the player to check
+	 * @return true iff the player is engaged
+	 */
     public boolean isEngaged(final Player player) {
         return (player.hasQuest(QUEST_SLOT) 
         		&& (player.getQuest(QUEST_SLOT).startsWith("engaged") || player.getQuest(QUEST_SLOT).startsWith("forging;")));
     }
-
 }
