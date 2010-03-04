72e5f94fc2806da867807e81c0e926af749d229f
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-03-04 21:13:08 +0000

    added try catch blocks

--- src/games/stendhal/client/sound/SoundSystemFacade.java
+++ src/games/stendhal/client/sound/SoundSystemFacade.java
@@ -36,14 +36,22 @@ public class SoundSystemFacade extends SoundManager implements WorldListener {
 	}
 
 	private SoundSystemFacade() {
-		boolean mute = !Boolean.parseBoolean(WtWindowManager.getInstance().getProperty("sound.play", "true"));
-		super.mute(mute, null);
+		try {
+			boolean mute = !Boolean.parseBoolean(WtWindowManager.getInstance().getProperty("sound.play", "true"));
+			super.mute(mute, null);
+		} catch (RuntimeException e) {
+			logger.error(e, e);
+		}
 	}
 
 	public void playerMoved() {
-		float[] position = Algebra.vecf((float) User.get().getX(), (float) User.get().getY());
-		super.setHearerPosition(position);
-		update();
+		try {
+			float[] position = Algebra.vecf((float) User.get().getX(), (float) User.get().getY());
+			super.setHearerPosition(position);
+			update();
+		} catch (RuntimeException e) {
+			logger.error(e, e);
+		}
 	}
 
 	public void zoneEntered(String zoneName) {
@@ -55,50 +63,85 @@ public class SoundSystemFacade extends SoundManager implements WorldListener {
 	}
 
 	public void stop(String soundName, Time fadeOutDuration) {
-		super.stop(getSound(soundName), fadeOutDuration);
+		try {
+			super.stop(getSound(soundName), fadeOutDuration);
+		} catch (RuntimeException e) {
+			logger.error(e, e);
+		}
 	}
 
 	public Sound getSound(String soundName) {
-		return sounds.get(soundName);
+		try {
+			return sounds.get(soundName);
+		} catch (RuntimeException e) {
+			logger.error(e, e);
+		}
+		return null;
 	}
 
 	public Sound openSound(String fileURI, SoundFile.Type fileType) {
-		return super.openSound(resourceLocator.getResource(fileURI), fileType);
+		try {
+			return super.openSound(resourceLocator.getResource(fileURI), fileType);
+		} catch (RuntimeException e) {
+			logger.error(e, e);
+		}
+		return null;
 	}
 
 	public Sound openSound(String fileURI, SoundFile.Type fileType, int numSamplesPerChunk, boolean enableStreaming) {
-		return super.openSound(resourceLocator.getResource(fileURI), fileType, numSamplesPerChunk, enableStreaming);
+		try {
+			return super.openSound(resourceLocator.getResource(fileURI), fileType, numSamplesPerChunk, enableStreaming);
+		} catch (RuntimeException e) {
+			logger.error(e, e);
+		}
+		return null;
 	}
 
 	public Sound loadSound(String name, String fileURI, SoundFile.Type fileType, boolean enableStreaming) {
-		Sound sound = sounds.get(name);
-
-		if(sound == null) {
-			sound = super.openSound(resourceLocator.getResource(fileURI), fileType, 256, enableStreaming);
-
-			if(sound != null) {
-				sounds.put(name, sound);
+		try {
+			Sound sound = sounds.get(name);
+	
+			if(sound == null) {
+				sound = super.openSound(resourceLocator.getResource(fileURI), fileType, 256, enableStreaming);
+	
+				if(sound != null) {
+					sounds.put(name, sound);
+				}
 			}
+			return sound;
+		} catch (RuntimeException e) {
+			logger.error(e, e);
 		}
-
-		return sound;
+		return null;
 	}
 
 	public void play(final String soundName, final SoundLayer soundLayer, int volume) {
-		Sound sound = loadSound(soundName, "audio:/" + soundName + ".ogg", Type.OGG, false);
-		super.play(sound, Numeric.intToFloat(volume, 100.0f), 0, null, false, new Time());
+		try {
+			Sound sound = loadSound(soundName, "audio:/" + soundName + ".ogg", Type.OGG, false);
+			super.play(sound, Numeric.intToFloat(volume, 100.0f), 0, null, false, new Time());
+		} catch (RuntimeException e) {
+			logger.error(e, e);
+		}
 	}
 
 	public void play(final String soundName, final double x, final double y, final SoundLayer soundLayer, int volume) {
-		AudibleArea area = new AudibleCircleArea(Algebra.vecf((float) x, (float) y), 3, 20);
-		Sound sound = loadSound(soundName, "audio:/" + soundName + ".ogg", Type.OGG, false);
-		super.play(sound, Numeric.intToFloat(volume, 100.0f), 0, area, false, new Time());
+		try {
+			AudibleArea area = new AudibleCircleArea(Algebra.vecf((float) x, (float) y), 3, 20);
+			Sound sound = loadSound(soundName, "audio:/" + soundName + ".ogg", Type.OGG, false);
+			super.play(sound, Numeric.intToFloat(volume, 100.0f), 0, area, false, new Time());
+		} catch (RuntimeException e) {
+			logger.error(e, e);
+		}
 	}
 
 	public void play(final String soundName, final double x, final double y, int radius, final SoundLayer soundLayer, int volume) {
-		AudibleArea area = new AudibleCircleArea(Algebra.vecf((float) x, (float) y), radius / 4.0f, radius);
-		Sound sound = loadSound(soundName, "audio:/" + soundName + ".ogg", Type.OGG, false);
-		super.play(sound, Numeric.intToFloat(volume, 100.0f), 0, area, false, new Time());
+		try {
+			AudibleArea area = new AudibleCircleArea(Algebra.vecf((float) x, (float) y), radius / 4.0f, radius);
+			Sound sound = loadSound(soundName, "audio:/" + soundName + ".ogg", Type.OGG, false);
+			super.play(sound, Numeric.intToFloat(volume, 100.0f), 0, area, false, new Time());
+		} catch (RuntimeException e) {
+			logger.error(e, e);
+		}
 	}
 
 }
