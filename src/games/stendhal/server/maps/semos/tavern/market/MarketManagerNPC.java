ea28c5c029c65ccabdf531008d7edcd033fa581c
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-03-06 15:21:58 +0000

    fixed compiler warnigns

--- src/games/stendhal/server/maps/semos/tavern/market/MarketManagerNPC.java
+++ src/games/stendhal/server/maps/semos/tavern/market/MarketManagerNPC.java
@@ -18,7 +18,7 @@ public final class MarketManagerNPC extends SpeakerNPC {
 
 	@Override
 	protected void createPath() {
-		
+		// npcs is lazy and does not move
 	}
 
 	@Override
@@ -26,7 +26,7 @@ public final class MarketManagerNPC extends SpeakerNPC {
 		addGreeting("Welcome to Semos trading center. How can I #help you?");
 		addJob("I am here to #help you sell items.");
 		addOffer("To put an offer on the market, say #sell #item #price - and then anyone else can buy it, " +
-                "even if you are not here. For more details just ask for #help.");
+				"even if you are not here. For more details just ask for #help.");
 		addHelp("Would you like help in #buying or help in #selling?");
 		addReply("buying", "If you want to buy something, say #show and I will list current open offers with an " +
 				 "offer number. If you want to accept one of the offers, say #accept #number to buy the " +
@@ -34,7 +34,7 @@ public final class MarketManagerNPC extends SpeakerNPC {
 				 "example #show #meat to only see meat related offers.");
 		addReply("selling", "Say #sell #item #price to put an offer on the market. If you want to remove an " +
 				 "offer from the market, tell me #'show mine', so you will see only your offers. Say #remove " +
-                 "#number afterward to remove a certain offer. If you have expired offers, you can ask for them " +
+				 "#number afterward to remove a certain offer. If you have expired offers, you can ask for them " +
 				 "by saying #show #expired. You can prolong an expired offer by saying #prolong #number. If you already sold some items " +
 				 "you can say #fetch to me and I will pay out your earnings.");
 		new PrepareOfferHandler().add(this);
@@ -48,6 +48,7 @@ public final class MarketManagerNPC extends SpeakerNPC {
 		add(ConversationStates.ATTENDING, "examine", null, ConversationStates.ATTENDING, null, new ExamineOfferChatAction());
 		addGoodbye("Visit me again to see available offers, make a new offer or fetch your earnings!");
 	}
+
 	public Map<String, Map<String, Offer>> getOfferMap() {
 		return offerMap;
 	}
