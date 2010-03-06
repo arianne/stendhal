5f555e7478ccfd8739a714f6b5c7d27ddeb7841d
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-03-06 09:18:44 +0000

    extends the portal script to allow admins to name a portal and to define a level checking condition

--- src/games/stendhal/server/script/AdminPortal.java
+++ src/games/stendhal/server/script/AdminPortal.java
@@ -2,6 +2,7 @@
 package games.stendhal.server.script;
 
 import games.stendhal.server.core.scripting.ScriptImpl;
+import games.stendhal.server.entity.mapstuff.portal.LevelCheckingPortal;
 import games.stendhal.server.entity.mapstuff.portal.Portal;
 import games.stendhal.server.entity.player.Player;
 
@@ -17,24 +18,63 @@ public class AdminPortal extends ScriptImpl {
 
 	@Override
 	public void execute(final Player admin, final List<String> args) {
-		if (args.size() == 2) {
-
-			sandbox.setZone(sandbox.getZone(admin));
-			int x = admin.getX();
-			int y = admin.getY();
-
-			Portal portal = new Portal();
-			portal.setPosition(x, y);
-			portal.setDestination(args.get(0), args.get(1));
-
-			// add sign to game
-			sandbox.add(portal);
+		if (args.size() >= 2 && args.size() != 4) {
+			try {
+				createPortal(admin, args);
+			} catch (RuntimeException e) {
+				admin.sendPrivateText(e.toString());
+			}
 		} else {
 			// syntax error, print help text
 			sandbox.privateText(
 					admin,
-					"This script creates portals. /script AdminPortal.class <destination-zone> <destination-ref>");
+					"This script creates portals:\n" + 
+					"/script AdminPortal.class <destination-zone> <destination-ref>\n" + 
+					"/script AdminPortal.class <name> <destination-zone> <destination-ref>\n" + 
+					"/script AdminPortal.class <name> <destination-zone> <destination-ref> level <min-level> <max-level> <reject-message>" + 
+					"");
 		}
 	}
 
+	private void createPortal(final Player admin, final List<String> args) {
+		sandbox.setZone(sandbox.getZone(admin));
+		int x = admin.getX();
+		int y = admin.getY();
+
+		Portal portal = instantiatePortal(args);
+		setPortalName(args, portal);
+		portal.setPosition(x, y);
+		int destinationOffset = getDestinationOffset(args);
+		portal.setDestination(args.get(destinationOffset), args.get(destinationOffset + 1));
+
+		// add sign to game
+		sandbox.add(portal);
+	}
+
+	private int getDestinationOffset(List<String> args) {
+		if (args.size() > 2) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+	private Portal instantiatePortal(List<String> args) {
+		if (args.size() < 4) {
+			return new Portal();
+		} else if (args.get(4).equals("level")) {
+			String rejectMessage = null;
+			if (args.size() == 7) {
+				rejectMessage = args.get(6);
+			}
+			return new LevelCheckingPortal(Integer.parseInt(args.get(5)), Integer.parseInt(args.get(6)), rejectMessage);
+		}
+		throw new IllegalArgumentException("Invalid portal type.");
+	}
+
+	private void setPortalName(final List<String> args, Portal portal) {
+		if (args.size() > 2) {
+			portal.setIdentifier(args.get(0));
+		}
+	}
 }
