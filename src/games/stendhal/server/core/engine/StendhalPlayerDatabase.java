6cc190f421005ce7d3a1fb3d081aacc51d5a29b8
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-03-17 22:24:02 +0000

    converted item logging to DBCommandQueue

--- src/games/stendhal/server/core/engine/StendhalPlayerDatabase.java
+++ src/games/stendhal/server/core/engine/StendhalPlayerDatabase.java
@@ -8,8 +8,6 @@ import games.stendhal.server.core.engine.db.StendhalNPCDAO;
 import games.stendhal.server.core.engine.db.StendhalWebsiteDAO;
 
 import java.sql.SQLException;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import marauroa.server.db.DBTransaction;
 import marauroa.server.db.JDBCSQLHelper;
@@ -20,47 +18,8 @@ import marauroa.server.game.db.DAORegister;
 import org.apache.log4j.Logger;
 
 public class StendhalPlayerDatabase {
-
-	private static boolean shouldStop;
 	
 	private static final Logger logger = Logger.getLogger(StendhalPlayerDatabase.class);
-	private static TimerTask task = new TimerTask() {
-
-		ItemLogger itemLogger = new ItemLogger();
-
-		@Override
-		public void run() {
-			
-			try {
-				processGameEvents();
-				itemLogger.processEntries();
-			} catch (SQLException e) {
-				logger.error(e, e);
-			}
-			
-			if (shouldStop && GameEventQueue.queue.isEmpty()) {
-				this.cancel();
-			}
-		}
-		
-		private void processGameEvents() throws SQLException {
-			final DBTransaction transaction = TransactionPool.get().beginWork();
-			try {
-				for (GameEvent current = GameEventQueue.getGameEvents().poll();
-					current != null; current = GameEventQueue.getGameEvents().poll()) {
-					// DAORegister.get().get(GameEventDAO.class).addGameEvent(transaction, current.source, current.event, current.params);
-					if ("server system".equals(current.source) && "shutdown".equals(current.event)) {
-						shouldStop = true;
-					}
-				}
-				TransactionPool.get().commit(transaction);
-			} catch (SQLException e) {
-				logger.error(e, e);
-				TransactionPool.get().rollback(transaction);
-			}
-		}
-
-	};
 
 	public void initialize() {
 		final DBTransaction transaction = TransactionPool.get().beginWork();
@@ -83,7 +42,5 @@ public class StendhalPlayerDatabase {
 		DAORegister.get().register(StendhalKillLogDAO.class, new StendhalKillLogDAO ());
 		DAORegister.get().register(StendhalNPCDAO.class, new StendhalNPCDAO());
 		DAORegister.get().register(StendhalWebsiteDAO.class, new StendhalWebsiteDAO());
-
-		new Timer().schedule(task , 2000, 300);
 	}
 }
