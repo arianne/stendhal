8d9a05c3b77c7da74ebb39d5135b51ae6055ea9c
Markus <madmetzger@users.sourceforge.net>
2010-03-19 18:29:50 +0000

    added java doc and two other tests

--- tests/games/stendhal/server/entity/trade/EarningTest.java
+++ tests/games/stendhal/server/entity/trade/EarningTest.java
@@ -5,7 +5,11 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-
+/**
+ * test class for earnings entity
+ *  
+ * @author madmetzger
+ */
 public class EarningTest {
 
 	@Test
@@ -15,5 +19,20 @@ public class EarningTest {
 		assertThat(e1,is(e2));
 		assertThat(e1.hashCode(), is(e2.hashCode()));
 	}
+	
+	@Test
+	public void testSellername() {
+		Earning e1 = new Earning(Integer.valueOf(1),"bob", true);
+		Earning e2 = new Earning(Integer.valueOf(1),"bob", true);
+		assertThat(e1.getSeller(),is("bob"));
+		assertThat(e2.getSeller(),is("bob"));
+	}
 
+	@Test
+	public void testValue() {
+		Earning e1 = new Earning(Integer.valueOf(1),"bob", true);
+		Earning e2 = new Earning(Integer.valueOf(1),"bob", true);
+		assertThat(e1.getValue(),is(Integer.valueOf(1)));
+		assertThat(e2.getValue(),is(Integer.valueOf(1)));
+	}
 }
