fafd24e7c53ca21067298c6ca693ade49cee1f16
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-03-23 19:13:55 +0000

    added name to sound group

--- src/games/stendhal/common/constants/SoundLayer.java
+++ src/games/stendhal/common/constants/SoundLayer.java
@@ -8,20 +8,31 @@ package games.stendhal.common.constants;
 public enum SoundLayer {
 
 	/** nice background music which is considered out-of-game */
-	BACKGROUND_MUSIC,
+	BACKGROUND_MUSIC("music"),
 
 	/**
 	 * sounds that are related to the zone and are in-game, 
 	 * like a waterfall, wind, the hammering at the blacksmith
 	 */
-	AMBIENT_SOUND,
+	AMBIENT_SOUND("ambient"),
 
 	/** noise made by creatures and NPCs */
-	CREATURE_NOISE,
+	CREATURE_NOISE("creature"),
 
 	/** noise made by weapons and armor */
-	FIGHTING_NOISE,
+	FIGHTING_NOISE("sfx"),
 
 	/** user interface feedback, opening of windows, private message notification */
-	USER_INTERFACE;
+	USER_INTERFACE("gui");
+
+	public final String groupName;
+
+	/**
+	 * creates an instance of SoundLayer
+	 *
+	 * @param groupName name of group
+	 */
+	private SoundLayer(String groupName) {
+		this.groupName = groupName;
+	}
 }
