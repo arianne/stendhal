ee3de74bac0308951efa52c31029ceaaeb2f523d
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-03-24 20:09:11 +0000

    updated version number

--- runserver.bat
+++ runserver.bat
@@ -1,3 +1,3 @@
-set STENDHAL_VERSION=0.81.5
+set STENDHAL_VERSION=0.82.5
 set LOCALCLASSPATH=.;data\script;data\conf;stendhal-server-%STENDHAL_VERSION%.jar;marauroa.jar;mysql-connector.jar;log4j.jar;commons-lang.jar;h2.jar
 java -Xmx400m -cp "%LOCALCLASSPATH%" marauroa.server.marauroad -c server.ini -l
