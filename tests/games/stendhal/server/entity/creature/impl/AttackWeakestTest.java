ef72b88303124c42c6ee8debadeb4b261626d0ed
Kimmo Rundelin <kiheru@users.sourceforge.net>
2010-03-28 23:40:44 +0000

    added some tests for AttackWeakest

---
+++ tests/games/stendhal/server/entity/creature/impl/AttackWeakestTest.java
@@ -0,0 +1,162 @@
+package games.stendhal.server.entity.creature.impl;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.creature.Creature;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.maps.MockStendlRPWorld;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import utilities.PlayerTestHelper;
+import utilities.RPClass.CreatureTestHelper;
+
+public class AttackWeakestTest {
+	@BeforeClass
+	public static void setUpbeforeClass() throws Exception {
+		MockStendlRPWorld.get();
+		CreatureTestHelper.generateRPClasses();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Tests that a new creature does not have a target.
+	 */
+	@Test
+	public void testhasValidTargetNonAttacker() {
+		AttackStrategy strat = new AttackWeakest();
+		Creature loner = new Creature(); 
+		
+		assertFalse("attacker has no target", strat.hasValidTarget(loner));
+	}
+
+	/**
+	 * Tests that hasValidTarget succeeds for a normal, 
+	 * and fails for invisible target.
+	 */
+	@Test
+	public void testhasValidTargetInvisibleVictim() {
+		AttackStrategy strat = new AttackWeakest();
+
+		Creature creature = new Creature();
+		Player player = PlayerTestHelper.createPlayer("ghost");
+		StendhalRPZone arena = new StendhalRPZone("arena");
+		arena.add(creature);
+		arena.add(player);
+		creature.setTarget(player);
+		assertTrue("has a valid target", strat.hasValidTarget(creature));
+		player.setInvisible(true);
+		assertFalse("has a valid target", strat.hasValidTarget(creature));
+	}
+
+	/**
+	 * Tests that hasValidTarget fails when the target
+	 * is not in the same zone.
+	 */
+	@Test
+	public void testhasValidTargetDifferentZones() {
+		AttackStrategy strat = new AttackWeakest();
+		
+		StendhalRPZone jacuzzi = new StendhalRPZone("jacuzzi");
+		Player player = PlayerTestHelper.createPlayer("elvis");
+		jacuzzi.add(player);
+		
+		Creature creature = new Creature();
+		StendhalRPZone arena = new StendhalRPZone("arena");
+		arena.add(creature);
+
+		creature.setTarget(player);
+		assertFalse("has a valid target", strat.hasValidTarget(creature));
+	}
+
+	/**
+	 * Tests finding a new target
+	 */
+	@Test
+	public void testFindNewTarget() {
+		AttackStrategy strat = new AttackWeakest();
+		
+		/*
+		 *  Need to use a real creature, because the
+		 *  creature needs to be offensive to see the
+		 *  targets. 
+		 */
+		final Creature creature = SingletonRepository.getEntityManager().getCreature("rat");
+		
+		Player veteran = PlayerTestHelper.createPlayer("test dummy");
+		Player newbie = PlayerTestHelper.createPlayer("test dummy2");
+		
+		StendhalRPZone arena = new StendhalRPZone("arena");
+		arena.add(creature);
+		assertFalse("is not attacking", strat.hasValidTarget(creature));
+		arena.add(veteran);
+		arena.add(newbie);
+		
+		creature.setPosition(3, 3);
+		veteran.setPosition(3, 4);
+		
+		// Should pick the nearest: veteran
+		strat.findNewTarget(creature);
+		assertTrue("has a valid target", strat.hasValidTarget(creature));
+		assertEquals("attack nearest", veteran, creature.getAttackTarget());
+	}
+	
+	/**
+	 * Tests switching to a weaker target
+	 */
+	@Test
+	public void testSwitchTargets() {
+		AttackStrategy strat = new AttackWeakest();
+		
+		/*
+		 *  Need to use a real creature, because the
+		 *  creature needs to be offensive to see the
+		 *  targets. 
+		 */
+		final Creature creature = SingletonRepository.getEntityManager().getCreature("rat");
+		
+		Player veteran = PlayerTestHelper.createPlayer("veteran");
+		Player newbie = PlayerTestHelper.createPlayer("newbie");
+		
+		StendhalRPZone arena = new StendhalRPZone("arena");
+		arena.add(creature);
+		assertFalse("is not attacking", strat.hasValidTarget(creature));
+		arena.add(veteran);
+		arena.add(newbie);
+		veteran.addXP(10000);
+		newbie.addXP(100);
+		
+		assertTrue("sanity check for player levels", veteran.getLevel() > newbie.getLevel());
+		
+		creature.setPosition(3, 3);
+		veteran.setPosition(3, 4);
+		
+		// Should pick the nearest: veteran
+		strat.findNewTarget(creature);
+		assertTrue("has a valid target", strat.hasValidTarget(creature));
+		assertEquals("attack nearest", veteran, creature.getAttackTarget());
+		
+		// move newbie near. this should result in switching targets
+		newbie.setPosition(2, 3);
+		assertTrue("has a valid target", strat.hasValidTarget(creature));
+		assertEquals("attack weakest", newbie, creature.getAttackTarget());
+		
+		// move veteran away for a moment
+		veteran.setPosition(3, 5);
+		// should make no difference
+		assertTrue("has a valid target", strat.hasValidTarget(creature));
+		assertEquals("attack weakest", newbie, creature.getAttackTarget());
+		veteran.setPosition(3, 4);
+		// and neither should putting him back (unlike newbie's arrival)
+		assertTrue("has a valid target", strat.hasValidTarget(creature));
+		assertEquals("attack weakest", newbie, creature.getAttackTarget());
+	}
+}
