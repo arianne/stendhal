6d1e06761b73ec162b770adb4455ed1524b6a35e
Katie Russell <kymara@users.sourceforge.net>
2010-03-28 10:30:44 +0000

    Started to rework the Producer code to use standard actions and conditions, where it's not tied in with some  method from the behaviour. (But those could be split up more - they do a lot of conditional checks inside behaviour). Also added comments about each block.

--- src/games/stendhal/server/entity/npc/behaviour/adder/ProducerAdder.java
+++ src/games/stendhal/server/entity/npc/behaviour/adder/ProducerAdder.java
@@ -1,11 +1,12 @@
 package games.stendhal.server.entity.npc.behaviour.adder;
 
 import games.stendhal.server.entity.npc.ChatAction;
-import games.stendhal.server.entity.npc.ChatCondition;
 import games.stendhal.server.entity.npc.ConversationPhrases;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.behaviour.impl.ProducerBehaviour;
+import games.stendhal.server.entity.npc.condition.QuestActiveCondition;
+import games.stendhal.server.entity.npc.condition.QuestNotActiveCondition;
 import games.stendhal.server.entity.npc.fsm.Engine;
 import games.stendhal.server.entity.npc.parser.Sentence;
 import games.stendhal.server.entity.player.Player;
@@ -15,11 +16,20 @@ import org.apache.log4j.Logger;
 public class ProducerAdder {
 	private static Logger logger = Logger.getLogger(ProducerAdder.class);
 
+    /** Adds all the dialogue associated with a Producing NPC */
 	public void addProducer(final SpeakerNPC npc, final ProducerBehaviour behaviour,
 			final String welcomeMessage) {
+
+        /** Which NPC is this? */
 		final Engine engine = npc.getEngine();
 
+        /** What quest slot is the production stored in? */
+        final String QUEST_SLOT = behaviour.getQuestSlot();
+
+        /** How should we greet the player? */
 		final String thisWelcomeMessage = welcomeMessage;
+
+        /* If the NPC is attending another player, say who they are attending */
 		npc.addWaitMessage(null, new ChatAction() {
 			public void fire(final Player player, final Sentence sentence, final SpeakerNPC engine) {
 				engine.say("Please wait! I am attending "
@@ -27,28 +37,25 @@ public class ProducerAdder {
 			}
 		});
 
+        /* The Player greets the NPC.
+        * The NPC is not currently producing for player (not started, is rejected, or is complete) */
 		engine.add(ConversationStates.IDLE,
 				ConversationPhrases.GREETING_MESSAGES,
-				new ChatCondition() {
-					public boolean fire(final Player player, final Sentence sentence,
-							final SpeakerNPC engine) {
-						return !player.hasQuest(behaviour.getQuestSlot())
-								|| player.isQuestCompleted(behaviour.getQuestSlot());
-					}
-				}, ConversationStates.ATTENDING, thisWelcomeMessage, null);
+				new QuestNotActiveCondition(QUEST_SLOT), 
+                ConversationStates.ATTENDING, thisWelcomeMessage, null);
 
-		engine.add(
+        /* In the behaviour a production activity is defined, e.g. 'cast' or 'mill' 
+        * and this is used as the trigger to start the production,
+        * provided that the NPC is not currently producing for player (not started, is rejected, or is complete) */		
+        engine.add(
 				ConversationStates.ATTENDING,
 				behaviour.getProductionActivity(),
-				new ChatCondition() {
-					public boolean fire(final Player player, final Sentence sentence,
-							final SpeakerNPC engine) {
-						return !player.hasQuest(behaviour.getQuestSlot())
-								|| player.isQuestCompleted(behaviour.getQuestSlot());
-					}
-				}, ConversationStates.ATTENDING, null,
+				new QuestNotActiveCondition(QUEST_SLOT), 
+                ConversationStates.ATTENDING, 
+                null,
 				new ChatAction() {
 					public void fire(final Player player, final Sentence sentence, final SpeakerNPC npc) {
+                        // TODO: this can be a part of a separate condition-action block
 						if (sentence.hasError()) {
 							npc.say("Sorry, I did not understand you. "
 									+ sentence.getErrorString());
@@ -86,6 +93,7 @@ public class ProducerAdder {
 					}
 				});
 
+        /* Player agrees to the proposed production deal */
 		engine.add(ConversationStates.PRODUCTION_OFFERED,
 				ConversationPhrases.YES_MESSAGES, null,
 				ConversationStates.ATTENDING, null,
@@ -96,39 +104,35 @@ public class ProducerAdder {
 					}
 				});
 
+        /* Player does not agree to the proposed production deal */
 		engine.add(ConversationStates.PRODUCTION_OFFERED,
 				ConversationPhrases.NO_MESSAGES, null,
 				ConversationStates.ATTENDING, "OK, no problem.", null);
 
+        /* Player says the production trigger word but the NPC is already producing items for that player */
 		engine.add(
 				ConversationStates.ATTENDING,
 				behaviour.getProductionActivity(),
-				new ChatCondition() {
-					public boolean fire(final Player player, final Sentence sentence,
-							final SpeakerNPC engine) {
-						return player.hasQuest(behaviour.getQuestSlot())
-								&& !player.isQuestCompleted(behaviour.getQuestSlot());
-					}
-				}, ConversationStates.ATTENDING, null,
+				new QuestActiveCondition(QUEST_SLOT), 
+                ConversationStates.ATTENDING, null,
 				new ChatAction() {
 					public void fire(final Player player, final Sentence sentence,
 							final SpeakerNPC npc) {
+                        // TODO: check - can the StateRemainingTimeAction be used here? 
 						npc.say("I still haven't finished your last order. Come back in "
 								+ behaviour.getApproximateRemainingTime(player)
 								+ "!");
 					}
 				});
 
+        /* Player greets NPC and the NPC is already producing items for that player
+         * There are two options: the NPC is still busy or he is finished
+         * The method giveProduct(npc, player) used here takes care of both. */
 		engine.add(
 				ConversationStates.IDLE,
 				ConversationPhrases.GREETING_MESSAGES,
-				new ChatCondition() {
-					public boolean fire(final Player player, final Sentence sentence,
-							final SpeakerNPC engine) {
-						return player.hasQuest(behaviour.getQuestSlot())
-								&& !player.isQuestCompleted(behaviour.getQuestSlot());
-					}
-				}, ConversationStates.ATTENDING, null,
+				new QuestActiveCondition(QUEST_SLOT), 
+                ConversationStates.ATTENDING, null,
 				new ChatAction() {
 					public void fire(final Player player, final Sentence sentence,
 							final SpeakerNPC npc) {
