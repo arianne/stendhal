b88fb20bdc767c9e0fb5c585a47708ac0ed7dac4
Kimmo Rundelin <kiheru@users.sourceforge.net>
2010-04-11 13:10:36 +0000

    Configurator support for sub-zone NoTeleportOut.

--- src/games/stendhal/server/core/config/zone/NoTeleportOut.java
+++ src/games/stendhal/server/core/config/zone/NoTeleportOut.java
@@ -8,8 +8,15 @@ import java.util.Map;
 public class NoTeleportOut implements ZoneConfigurator {
 
 	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
-		zone.disallowOut();
-
+		if ((attributes != null) && attributes.containsKey("x")) {
+			int x = Integer.parseInt(attributes.get("x"));
+			int y = Integer.parseInt(attributes.get("y"));
+			int width = Integer.parseInt(attributes.get("width"));
+			int height = Integer.parseInt(attributes.get("height"));
+			
+			zone.disallowOut(x, y, width, height);
+		} else {
+			zone.disallowOut();
+		}
 	}
-
 }
--- tests/games/stendhal/server/core/config/zone/NoTeleportOutTest.java
+++ tests/games/stendhal/server/core/config/zone/NoTeleportOutTest.java
@@ -3,6 +3,10 @@ package games.stendhal.server.core.config.zone;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.StendhalRPZone;
 
@@ -43,4 +47,24 @@ public class NoTeleportOutTest {
 		assertFalse(zone.isTeleportOutAllowed(19, 19));
 	}
 
+	/**
+	 * Tests for configuring only part of the zone
+	 */
+	@Test
+	public void testConfigureSubZone() {
+		StendhalRPZone zone = new StendhalRPZone("testzone",  20, 20);
+		ZoneConfigurator conf = new NoTeleportOut();
+		Map<String, String> attributes = new HashMap<String, String>();
+		attributes.put("x", "1");
+		attributes.put("y", "1");
+		attributes.put("width", "5");
+		attributes.put("height", "5");
+		conf.configureZone(zone, attributes);
+		assertTrue("Outside the blocked area", zone.isTeleportOutAllowed(0, 0));
+		assertFalse("Inside the blocked area", zone.isTeleportOutAllowed(1, 1));
+		assertFalse("Inside the blocked area", zone.isTeleportOutAllowed(5, 5));
+		assertTrue("Outside the blocked area", zone.isTeleportOutAllowed(6, 6));
+		assertTrue(zone.isTeleportInAllowed(0, 0));
+		assertTrue(zone.isTeleportInAllowed(19, 19));
+	}
 }
