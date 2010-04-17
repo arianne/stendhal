b7210149f6f408ea4e79a20c8804d00183ebfc6b
Markus <madmetzger@users.sourceforge.net>
2010-04-17 19:02:33 +0000

    introduce abstract creator for spells

---
+++ src/games/stendhal/server/core/rule/defaultruleset/creator/AbstractSpellCreator.java
@@ -0,0 +1,17 @@
+package games.stendhal.server.core.rule.defaultruleset.creator;
+
+import games.stendhal.server.core.rule.defaultruleset.DefaultSpell;
+import games.stendhal.server.entity.spell.Spell;
+
+import java.lang.reflect.Constructor;
+
+abstract class AbstractSpellCreator extends AbstractCreator<Spell> {
+	
+	protected final DefaultSpell defaultSpell;
+
+	public AbstractSpellCreator(DefaultSpell defaultSpell, Constructor<?> construct) {
+		super(construct);
+		this.defaultSpell = defaultSpell;
+	}
+
+}
