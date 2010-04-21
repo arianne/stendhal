9e7ba4b4a8e6ec0e2fb82836ddc75444de0ce330
Kimmo Rundelin <kiheru@users.sourceforge.net>
2010-04-21 20:41:05 +0000

    client side support for different gate images

--- src/games/stendhal/client/gui/j2d/entity/Gate2DView.java
+++ src/games/stendhal/client/gui/j2d/entity/Gate2DView.java
@@ -1,42 +1,48 @@
 package games.stendhal.client.gui.j2d.entity;
 
 import games.stendhal.client.entity.ActionType;
+import games.stendhal.client.entity.IEntity;
 import games.stendhal.client.sprite.Sprite;
 import games.stendhal.client.sprite.SpriteStore;
 
 import java.awt.Graphics2D;
+import java.util.HashMap;
 import java.util.List;
+
+import marauroa.common.game.RPObject;
 
 import org.apache.log4j.Logger;
 
 public class Gate2DView extends Entity2DView {
-	static final Sprite horizontalClosedGate;
-	static final Sprite horizontalOpenGate;
-	private static final Sprite verticalClosedGate;
-	private static final Sprite verticalOpenGate;
-	static {
-		 final Sprite sprite = SpriteStore.get().getSprite("data/sprites/doors/fence_gate_h.png");
-		 horizontalClosedGate = sprite.createRegion(0, 32 + 96, 96, 64, null);
-		 horizontalOpenGate = sprite.createRegion(0, 32, 96, 64, null);
-		 final Sprite sprite_v = SpriteStore.get().getSprite("data/sprites/doors/fence_gate_v.png");
-		 verticalClosedGate = sprite_v.createRegion(0, 96, 96, 96, null);
-		 verticalOpenGate = sprite_v.createRegion(0, 0, 96, 96, null);
-	
-	}
+	static final HashMap<String, Sprite[]> sprites = new HashMap<String, Sprite[]>();
 
-
-	private String orientation = "orientation";
+	private Sprite openSprite, closedSprite;
 	
 	public Gate2DView() {
-		
-		
-	}
-
-	@Override
-	protected String translate(final String name) {
-		return "data/sprites/doors/fence_gate_" + entity.getRPObject().get("orientation") + ".png";
 	}
 	
+	@Override
+	public void initialize(IEntity entity) {
+		super.initialize(entity);
+		final RPObject rpobject = entity.getRPObject();
+		final String baseImage = rpobject.get("image");
+		final String orientation = rpobject.get("orientation"); 
+		
+		String imageName = "data/sprites/doors/" + baseImage + "_" + orientation +".png";
+		Sprite[] s = sprites.get(imageName);
+		if (s == null) {
+			Sprite sprite = SpriteStore.get().getSprite(imageName);
+			s = new Sprite[2];
+			s[0] = sprite.createRegion(0, 0, 96, 96, null);
+			s[1] = sprite.createRegion(0, 96, 96, 96, null);
+			System.err.println("Caching " + imageName);
+			sprites.put(imageName, s);
+		}
+		
+		openSprite = s[0];
+		closedSprite = s[1];
+	}
+
 	@Override
 	protected void buildActions(final List<String> list) {
 		list.add(ActionType.USE.getRepresentation());
@@ -54,8 +60,6 @@ public class Gate2DView extends Entity2DView {
 			return;
 		}
 
-		
-
 		switch (at) {
 		case USE:
 			at.send(at.fillTargetInfo(entity.getRPObject()));
@@ -66,27 +70,28 @@ public class Gate2DView extends Entity2DView {
 		}
 	}
 	
+	/**
+	 * Check if the gate is open.
+	 * 
+	 * @return <code>true</code> iff the gate is open
+	 */
+	private boolean isOpen() {
+		return entity.getResistance() == 0;
+	}
+	
 	@Override
 	protected void drawEntity(final Graphics2D g2d, final int x, final int y, final int width,
 			final int height) {
-	
-		if (entity.getRPObject().get(orientation).equals("h")) {
-			if (entity.getResistance() == 100) {
-				horizontalClosedGate.draw(g2d, x - 32, y);
-			} else {
-				horizontalOpenGate.draw(g2d, x - 32, y);
-			}
-		} else	if (entity.getResistance() == 100) {
-			verticalClosedGate.draw(g2d, x - 32, y - 32);
+		if (isOpen()) {
+			openSprite.draw(g2d, x - 32, y - 32);
 		} else {
-			verticalOpenGate.draw(g2d, x - 32, y - 32);
+			closedSprite.draw(g2d, x - 32, y - 32);
 		}
 	}
 
 
 	@Override
 	public int getZIndex() {
-		
 		return 0;
 	}
 }
