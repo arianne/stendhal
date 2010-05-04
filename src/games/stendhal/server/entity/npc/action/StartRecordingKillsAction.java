f9f89dc5e263cbbb94f05d298ef97e5f22ab8f64
yoriy <yoriy@users.sourceforge.net>
2010-05-04 05:21:39 +0000

    reworked action logic; using player's quest slot for storing required creatures kills information, instead of cleaning  his kills slot.

--- src/games/stendhal/server/entity/npc/action/StartRecordingKillsAction.java
+++ src/games/stendhal/server/entity/npc/action/StartRecordingKillsAction.java
@@ -5,8 +5,8 @@ import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.parser.Sentence;
 import games.stendhal.server.entity.player.Player;
 
-import java.util.Arrays;
-import java.util.List;
+import marauroa.common.Pair;
+import java.util.HashMap;
 
 import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
@@ -17,37 +17,39 @@ import org.apache.commons.lang.builder.HashCodeBuilder;
  * @author hendrik
  */
 public class StartRecordingKillsAction implements ChatAction {
-	private final List<String> toKill;
-
+	//first number in pair is required solo kills, second is required shared kills
+    private final HashMap<String, Pair<Integer, Integer>> toKill;
+    private final String QUEST_SLOT;
+    private final int KILLS_INDEX;
+	
 	/**
 	 * Creates a new StartRecodingKillsAction.
 	 * 
 	 * @param toKill
 	 *            list of creatures which should be killed by the player
 	 */
-	public StartRecordingKillsAction(final List<String> toKill) {
+	public StartRecordingKillsAction(final String questSlot, final int startIndex, final HashMap<String, Pair<Integer, Integer>> toKill) {
 		this.toKill = toKill;
-	}
-
-	/**
-	 * Creates a new StartRecodingKillsAction.
-	 * 
-	 * @param toKill
-	 *            creatures which should be killed by the player
-	 */
-	public StartRecordingKillsAction(final String... toKill) {
-		this.toKill = Arrays.asList(toKill);
+		this.QUEST_SLOT=questSlot;
+		this.KILLS_INDEX=startIndex;
 	}
 
 	public void fire(final Player player, final Sentence sentence, final SpeakerNPC npc) {
-		for (final String creature : toKill) {
-			player.removeKill(creature);
+		final StringBuilder sb= new StringBuilder("");
+		for (final String creature : toKill.keySet()) {
+			final int requiredSolo=toKill.get(creature).first();
+			final int requiredShared=toKill.get(creature).second();			
+			final int soloKills=player.getSoloKill(creature);
+			final int sharedKills=player.getSharedKill(creature);
+			sb.append(creature+","+requiredSolo+","+requiredShared+","+soloKills+","+sharedKills+",");
 		}
+		final String result=sb.toString().substring(0, sb.toString().length()-1);
+		player.setQuest(QUEST_SLOT, KILLS_INDEX, result);
 	}
 
 	@Override
 	public String toString() {
-		return "StartRecordingKillsActions <" + toKill + ">";
+		return "StartRecordingKillsActions <" + toKill.toString() + ">";
 	}
 
 	@Override
