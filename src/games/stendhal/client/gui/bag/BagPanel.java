58e558e9f4068bae2b53ba2755eb0497053fada8
Kimmo Rundelin <kiheru@users.sourceforge.net>
2010-05-05 21:19:48 +0000

    use stock GridLayout for the experimental bag

--- src/games/stendhal/client/gui/bag/BagPanel.java
+++ src/games/stendhal/client/gui/bag/BagPanel.java
@@ -1,17 +1,14 @@
 package games.stendhal.client.gui.bag;
 
-import static pagelayout.EasyCell.eol;
-import static pagelayout.EasyCell.grid;
 import games.stendhal.client.gui.styled.Style;
 import games.stendhal.client.gui.styled.swing.StyledJPanel;
 
 import java.awt.Component;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.Timer;
-
-import pagelayout.CellGrid;
 
 public class BagPanel extends StyledJPanel {
 
@@ -33,12 +30,10 @@ public class BagPanel extends StyledJPanel {
 		timer = new Timer(1000 / FRAMES_PER_SECOND, timerTask);	
 		timer.start();
 
-		final CellGrid baggrid = grid(panels[0], panels[1], panels[2], eol(),
-				panels[3], panels[4], panels[5], eol(), panels[6], panels[7],
-				panels[8], eol(), panels[9], panels[10], panels[11], eol());
-		baggrid.setFixedWidth(panels, true);
-		baggrid.setFixedHeight(panels, true);
-		baggrid.setComponentGaps(1, 1);
-		baggrid.createLayout(this).setContainerGaps(0, 0);
+		setLayout(new GridLayout(4, 3));
+		
+		for (Component panel : panels) {
+			add(panel);
+		}
 	}
 }
