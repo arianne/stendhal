8bea66fa8fcfd8752a1e02bdb0897f240d4f5d59
Katie Russell <kymara@users.sourceforge.net>
2010-05-08 14:50:40 +0000

    adjust black items prices

--- src/games/stendhal/server/entity/npc/ShopList.java
+++ src/games/stendhal/server/entity/npc/ShopList.java
@@ -401,14 +401,12 @@ public class ShopList {
 		
 		
 		// black items (balduin, when ultimate collector quest completed)
-		// TODO: check prices as these are only here as a stop gap
 		shops.add("buyblack", "black armor", 60000);
-		shops.add("buyblack", "black boots", 20000);
-		shops.add("buyblack", "black cloak", 25000);
-		shops.add("buyblack", "black helmet", 30000);
+		shops.add("buyblack", "black boots", 10000);
+		shops.add("buyblack", "black cloak", 20000);
+		shops.add("buyblack", "black helmet", 15000);
 		shops.add("buyblack", "black legs", 40000);
 		shops.add("buyblack", "black shield", 75000);
-		// TODO: decide: do we want him to also buy these weapons?
 		shops.add("buyblack", "black sword", 20000);
 		shops.add("buyblack", "black scythe", 40000);
 		shops.add("buyblack", "black halberd", 30000);
