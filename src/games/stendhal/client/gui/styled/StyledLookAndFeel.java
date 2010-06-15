2712937e73af41f9d3c659bb4be0001cb0b9577f
Kimmo Rundelin <kiheru@users.sourceforge.net>
2010-06-15 19:03:36 +0000

    A look and feel that configures swing with the various Styled* UI objects. Not very usable yet.

---
+++ src/games/stendhal/client/gui/styled/StyledLookAndFeel.java
@@ -0,0 +1,61 @@
+package games.stendhal.client.gui.styled;
+
+import javax.swing.UIDefaults;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+
+public class StyledLookAndFeel extends MetalLookAndFeel {
+	private static final String pkg = "games.stendhal.client.gui.styled.";
+	
+	private final Style style;
+	
+	public StyledLookAndFeel(Style style) {
+		super();
+		this.style = style;
+	}
+	
+	@Override
+	protected void initClassDefaults(UIDefaults table) {
+		super.initClassDefaults(table);
+
+		Object[] uiDefaults = {
+			// Provide access to the style for the components
+			"StendhalStyle", style,
+			// The component UIs
+			"ButtonUI", pkg + "StyledButtonUI",
+			"PanelUI", pkg + "StyledPanelUI",
+			//"PopupMenuUI", pkg + "StyledPopuMenuUI",
+			"ScrollBarUI", pkg + "StyledScrollBarUI",
+			"ScrollPaneUI", pkg + "StyledScrollPaneUI",
+			"SliderUI", pkg + "StyledSliderUI",
+			"TextFieldUI", pkg + "StyledTextFieldUI",
+		};
+		
+		table.putDefaults(uiDefaults);
+	}
+	
+	@Override
+	public boolean isSupportedLookAndFeel() {
+		// supported everywhere
+		return true;
+	}
+	
+	@Override
+	public boolean isNativeLookAndFeel() {
+		return false;
+	}
+	
+	@Override
+	public String getDescription() {
+		return "Stendhal pixmap look and feel";
+	}
+	
+	@Override
+	public String getID() {
+		return "Stendhal";
+	}
+	
+	@Override
+	public String getName() {
+		return "Stendhal";
+	}
+}
