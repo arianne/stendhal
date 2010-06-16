881405378c96bc958bfb35d60417e00396421239
Markus <madmetzger@users.sourceforge.net>
2010-06-16 19:34:37 +0000

    organize imports

--- src/games/stendhal/server/maps/semos/city/HealerNPC.java
+++ src/games/stendhal/server/maps/semos/city/HealerNPC.java
@@ -1,19 +1,18 @@
 package games.stendhal.server.maps.semos.city;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-
 import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.pathfinder.FixedPath;
 import games.stendhal.server.core.pathfinder.Node;
 import games.stendhal.server.entity.npc.SpeakerNPC;
-import games.stendhal.server.entity.npc.SpeakerNPCFactory;
 import games.stendhal.server.entity.npc.behaviour.adder.HealerAdder;
 import games.stendhal.server.entity.npc.behaviour.adder.SellerAdder;
 import games.stendhal.server.entity.npc.behaviour.impl.SellerBehaviour;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A young lady (original name: Carmen) who heals players without charge. 
