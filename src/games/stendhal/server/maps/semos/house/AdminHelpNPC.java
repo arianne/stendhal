5ebcfd4f59acc688ad623ea6aa913e976e7160ed
Katie Russell <kymara@users.sourceforge.net>
2010-06-26 10:59:01 +0000

    backport add missing setPath statement to let Skye move again

--- src/games/stendhal/server/maps/semos/house/AdminHelpNPC.java
+++ src/games/stendhal/server/maps/semos/house/AdminHelpNPC.java
@@ -4,6 +4,7 @@ package games.stendhal.server.maps.semos.house;
 import games.stendhal.common.Direction;
 import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.pathfinder.FixedPath;
 import games.stendhal.server.core.pathfinder.Node;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.SpeakerNPC;
@@ -50,6 +51,7 @@ public class AdminHelpNPC implements ZoneConfigurator {
 				List<Node> nodes = new LinkedList<Node>();
 				nodes.add(new Node(6, 11));
 				nodes.add(new Node(12,11));
+				setPath(new FixedPath(nodes, true));
 			}
 			
 		};
