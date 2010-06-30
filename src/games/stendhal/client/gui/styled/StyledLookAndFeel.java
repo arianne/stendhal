f2a594cd03b70f590b60e4a5a864d4b6a8d9f9f4
Kimmo Rundelin <kiheru@users.sourceforge.net>
2010-06-30 15:12:17 +0000

    Added a hack for remapping some ctrl -keybindings to native versions. Hopefully fixes [3021985] "copy&paste does not work for OSX in Stendhal 0.85".

--- src/games/stendhal/client/gui/styled/StyledLookAndFeel.java
+++ src/games/stendhal/client/gui/styled/StyledLookAndFeel.java
@@ -1,7 +1,14 @@
 package games.stendhal.client.gui.styled;
 
+import java.awt.Toolkit;
+import java.awt.event.KeyEvent;
+
+import javax.swing.KeyStroke;
 import javax.swing.UIDefaults;
+import javax.swing.plaf.InputMapUIResource;
 import javax.swing.plaf.metal.MetalLookAndFeel;
+
+import org.apache.log4j.Logger;
 
 public class StyledLookAndFeel extends MetalLookAndFeel {
 	private static final String pkg = "games.stendhal.client.gui.styled.";
@@ -40,6 +47,73 @@ public class StyledLookAndFeel extends MetalLookAndFeel {
 		};
 		
 		table.putDefaults(uiDefaults);
+	}
+	
+	@Override
+	protected void initComponentDefaults(UIDefaults table) {
+		super.initComponentDefaults(table);
+		restoreSystemKeymaps(table);
+	}
+	
+	/**
+	 * A hack to work around swing hard coding key bindings, and providing
+	 * no reasonable way to access the native ones. Add new components and
+	 * key bindings here when mac users report them.
+	 * 
+	 * @param table A mess of all UI defaults crammed in one hash map
+	 */
+	private void restoreSystemKeymaps(UIDefaults table) {
+		/*
+		 * Input mappings that need to be tuned. the list is not exhaustive and
+		 * will likely need more entries when new components start to be used.
+		 */
+		String[] keys = { "EditorPane.focusInputMap",
+				"FormattedTextField.focusInputMap",
+				"PasswordField.focusInputMap",
+				"TextArea.focusInputMap",
+				"TextField.focusInputMap",
+				"TextPane.focusInputMap" };
+		
+		// Native modifier key. Ctrl for others, cmd on mac
+		int modifier = Toolkit.getDefaultToolkit().getMenuShortcutKeyMask();
+		
+		for (String key : keys) {
+			Object value = table.get(key);
+			if (value instanceof InputMapUIResource) {
+				InputMapUIResource map = (InputMapUIResource) value;
+				
+				// CUT
+				remapKey(map, KeyEvent.VK_X, KeyEvent.CTRL_DOWN_MASK, modifier);
+				
+				// COPY
+				remapKey(map, KeyEvent.VK_C, KeyEvent.CTRL_DOWN_MASK, modifier);
+				
+				// PASTE
+				remapKey(map, KeyEvent.VK_V, KeyEvent.CTRL_DOWN_MASK, modifier);
+			} else {
+				Logger.getLogger(StyledLookAndFeel.class).error("Can not modify resource: " + key);
+			}
+		}
+	}
+	
+	/**
+	 * Remap a swing default key binding to a native one, if needed.
+	 * 
+	 * @param map keymap to be modified
+	 * @param key
+	 * @param defaultModifier swing default modifier key for the action
+	 * @param nativeModifier native modifier key for the action
+	 */
+	private void remapKey(InputMapUIResource map, int key, int defaultModifier, 
+			int nativeModifier) {
+		KeyStroke defaultKey = KeyStroke.getKeyStroke(key, defaultModifier);
+		Object action = map.get(defaultKey);
+		
+		KeyStroke nativeKey = KeyStroke.getKeyStroke(key, nativeModifier);
+		if (!nativeKey.equals(defaultKey)) {
+			map.remove(defaultKey);
+			map.put(nativeKey, action);
+		}
 	}
 	
 	@Override
