837f6f4e5cceba133908e211d51bcd6da1778655
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-07-07 18:16:12 +0000

    organized imports

--- src/games/stendhal/server/maps/ados/city/WeaponryTraderNPC.java
+++ src/games/stendhal/server/maps/ados/city/WeaponryTraderNPC.java
@@ -6,14 +6,10 @@ import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.pathfinder.FixedPath;
 import games.stendhal.server.core.pathfinder.Node;
-import games.stendhal.server.entity.npc.ShopList;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.behaviour.adder.BuyerAdder;
-import games.stendhal.server.entity.npc.behaviour.adder.OutfitChangerAdder;
 import games.stendhal.server.entity.npc.behaviour.impl.BuyerBehaviour;
-import games.stendhal.server.entity.npc.behaviour.impl.OutfitChangerBehaviour;
 
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
