cb22a7f9042c77cce0dd649a8b69816c306181c2
Katie Russell <kymara@users.sourceforge.net>
2010-07-11 22:44:49 +0000

    when answering support, send a message to postman if the player is offline but did exist. Needed to change how the supportanswer templates were formatted to include the name as it required player to be online but we could have used action.get(TARGET) like for the rest of it.

--- src/games/stendhal/server/actions/admin/SupportAnswerAction.java
+++ src/games/stendhal/server/actions/admin/SupportAnswerAction.java
@@ -2,23 +2,32 @@ package games.stendhal.server.actions.admin;
 
 import games.stendhal.common.Grammar;
 import games.stendhal.common.NotificationType;
+import games.stendhal.server.core.engine.dbcommand.StoreMessageCommand;
 import games.stendhal.common.messages.SupportMessageTemplatesFactory;
 import games.stendhal.server.actions.CommandCenter;
 import games.stendhal.server.core.engine.GameEvent;
+import games.stendhal.server.core.events.TurnListener;
+import games.stendhal.server.core.events.TurnNotifier;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.entity.player.Player;
 
 import java.util.Map;
+
+import marauroa.server.db.command.DBCommand;
+import marauroa.server.db.command.DBCommandQueue;
+import marauroa.server.db.command.ResultHandle;
 
 import marauroa.common.game.RPAction;
 import static games.stendhal.common.constants.Actions.SUPPORTANSWER;
 import static games.stendhal.common.constants.Actions.TARGET;
 import static games.stendhal.common.constants.Actions.TEXT;
 
-public class SupportAnswerAction extends AdministrationAction {
+public class SupportAnswerAction extends AdministrationAction implements TurnListener  {
 	
 	private static final Map<String, String> messageTemplates = new SupportMessageTemplatesFactory().getTemplates();
 
+	private ResultHandle handle = new ResultHandle();
+	
 	public static void register() {
 		CommandCenter.register(SUPPORTANSWER, new SupportAnswerAction(), 50);
 	}
@@ -39,9 +48,7 @@ public class SupportAnswerAction extends AdministrationAction {
 			if (reply.startsWith("$")) {
 				if (messageTemplates.containsKey(reply)) {
 					reply = messageTemplates.get(reply);
-					if (supported != null) {
-						reply = String.format(reply, supported.getTitle());
-					}
+					reply = String.format(reply, action.get(TARGET));
 				} else {
 					player.sendPrivateText(reply + " is not a recognised shortcut. Did you mean $faq, $faqsocial, $ignore, $faqpvp, $wiki, $knownbug, $bugstracker, $rules, $notsupport or $spam?");
 					// send no support answer message if the shortcut wasn't understood
@@ -60,8 +67,45 @@ public class SupportAnswerAction extends AdministrationAction {
 				SingletonRepository.getRuleProcessor().sendMessageToSupporters(message);
 				
 			} else {
-				player.sendPrivateText(action.get(TARGET) + " is not currently logged in.");
+				// that player is not logged in. Do they exist at all or are they just offline? Try sending a message with postman.
+				DBCommand command = new StoreMessageCommand(player.getName(), action.get(TARGET), "In answer to your support question:\n" + reply + " \nIf you wish to reply, use /support.", "S");
+				DBCommandQueue.get().enqueueAndAwaitResult(command, handle);
+				TurnNotifier.get().notifyInTurns(0, this);
 			}
 		}
+	}
+	
+	/**
+	 * Completes handling the supportanswer action.
+	 * 
+	 * @param currentTurn ignored
+	 */
+	public void onTurnReached(int currentTurn) {
+		StoreMessageCommand checkcommand = DBCommandQueue.get().getOneResult(StoreMessageCommand.class, handle);
+		
+		if (checkcommand == null) {
+			TurnNotifier.get().notifyInTurns(0, this);
+			return;
+		}
+
+		boolean characterExists = checkcommand.targetCharacterExists();
+		String adminName = checkcommand.getSource();
+		String target = checkcommand.getTarget();
+		String supportmessage = checkcommand.getMessage();
+		
+		final Player admin = SingletonRepository.getRuleProcessor().getPlayer(adminName);
+		
+		if(!characterExists) {
+				if (admin != null) {
+					// incase admin logged out while waiting we want to avoid NPE
+					admin.sendPrivateText(NotificationType.ERROR, "Sorry, " + target + " could not be found.");
+				}
+				return;
+		} 
+		
+		final String message = adminName + " answers " + Grammar.suffix_s(target)
+		+ " support question using postman: " + supportmessage;
+		
+		SingletonRepository.getRuleProcessor().sendMessageToSupporters(message);
 	}
 }
