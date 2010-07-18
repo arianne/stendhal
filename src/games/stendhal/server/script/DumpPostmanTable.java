fef3dbd1886aa983d3cbd2db97128c6f12faaaa8
Katie Russell <kymara@users.sourceforge.net>
2010-07-18 22:37:49 +0000

    initial try at a script to dump the postman xml file to a format easier imported by mysql. I picked csv but we can change this if wanted. It just writes to System.out for now. checks for double quotes and escape characters already present and replaces them, also replaces new lines with a space.

---
+++ src/games/stendhal/server/script/DumpPostmanTable.java
@@ -0,0 +1,53 @@
+package games.stendhal.server.script;
+
+import java.io.FileInputStream;
+import games.stendhal.server.core.scripting.ScriptImpl;
+import games.stendhal.server.entity.player.Player;
+
+import java.util.Iterator;
+import java.util.Properties;
+import java.util.List;
+
+
+/**
+ * For dumping postman's table straight to the server log in 'csv' format
+ *  
+ */
+
+public class DumpPostmanTable extends ScriptImpl {
+	
+	private static final String STENDHAL_POSTMAN_XML = ".stendhal-postman.xml";
+	private final Properties messages = new Properties();
+	
+	@Override
+	public void execute(final Player admin, final List<String> args) {
+		super.execute(admin, args);
+		try {
+			this.messages.loadFromXML(new FileInputStream(STENDHAL_POSTMAN_XML));
+		} catch (final Exception e) {
+			admin.sendPrivateText("Could not find postman file");
+		}
+		
+		final Iterator< ? > itr = messages.keySet().iterator();
+		while (itr.hasNext()) {
+			final String key = itr.next().toString();
+			String target = key.substring(0,key.indexOf("!"));
+			String source = key.substring(key.indexOf("!") + 1);
+			String message = messages.getProperty(key);
+			
+			// messages got grouped together, but the line breaks will confuse the csv file: replace new lines with spaces.
+			message = message.replace("\n", " ");
+			// replace any double quotes in there with two single quotes so we don't confuse mysql on import
+			message =  message.replace("\"", "''");
+			// replace any escape characters with something else (would normally replace with \\ but java doesn't like \\
+			message =  message.replace("\\", "/");
+			// well, why not clean it now.
+			message = message.trim();
+			System.out.println("\"" + source + "\",\"" + target  + "\",\"" + message + "\"");
+
+			itr.remove();
+
+		}
+	}
+
+}
