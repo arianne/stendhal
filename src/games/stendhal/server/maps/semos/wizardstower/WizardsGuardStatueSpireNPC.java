f32873dc87e34714e68b7df7fe4c4644e5f37f1f
Katie Russell <kymara@users.sourceforge.net>
2010-07-18 17:10:47 +0000

    mostly just formating to make it readable, but also corrected some spelling, a wrong gem name (no emeralds here) and the same descriptions of the wizards which were already corrected in the other file but copied here

--- src/games/stendhal/server/maps/semos/wizardstower/WizardsGuardStatueSpireNPC.java
+++ src/games/stendhal/server/maps/semos/wizardstower/WizardsGuardStatueSpireNPC.java
@@ -39,92 +39,93 @@ public class WizardsGuardStatueSpireNPC implements ZoneConfigurator {
 			@Override
 			protected void createDialog() {
 				addGreeting("Greetings again, adventurer!");
-				addHelp("You are located in the #store. You can enter the spire by the teleporter in front of me. The one behind me teleports you back to the tower entrance.");
+				addHelp("You are stood in the #store. You can enter the spire by the teleporter in front of me. The one behind me teleports you back to the tower entrance.");
 				addJob("I am the guardian and #storekeeper of the #wizards tower.");
 				addGoodbye("So long!");
-				add(
-				        ConversationStates.ATTENDING,
-				        Arrays.asList("store", "storekeeper"),
-				        ConversationStates.ATTENDING,
-				        "I can create #special items with the materials from the store. Just tell me what you want, but for the most items I will need extra ingredients.",
-				        null);
-				add(
-				        ConversationStates.ATTENDING,
-				        Arrays.asList("special"),
-				        ConversationStates.ATTENDING,
-				        "For example I can create a #rift #cloak. I could read in your mind, adventurer. But it is not allowed to me here. So you have to tell me which special item you want and I will tell you, if I can help you.",
-				        null);
-				add(
-				        ConversationStates.ATTENDING,
-				        Arrays.asList("wizard", "wizards"),
-				        ConversationStates.ATTENDING,
-				        "Seven wizards form the wizards circle. These are #Erastus, #Elana, #Ravashack, #Jaer, #Cassandra, #Silvanus and #Malleus",
-				        null);
+				addReply(Arrays.asList("store", "storekeeper"),
+				        "I can create #special items with the materials from the store. Just tell me what you want, but for the most items I will need extra ingredients.");
+				addReply("special",
+				        "For example I can create a #rift #cloak. I could read in your mind, adventurer. But it is not allowed to me here. So you have to tell me which special item you want and I will tell you, if I can help you.");
+				addReply(Arrays.asList("wizard", "wizards"),
+				        "Seven wizards form the wizards circle. These are #Erastus, #Elana, #Ravashack, #Jaer, #Cassandra, #Silvanus and #Malleus");
 				addReply("erastus", "Erastus is the archmage of the wizards circle. He is the grandmaster of all magics and the wisest person that is known. He is the only one without a part in the practical test.");
-				addReply("elana", "Elana is the warmest and friendliest enchantress. She is the protectress of all living creatures and uses divinely magic to save and heal them.");
-				addReply("ravashack", "Ravashack is a very mighty necromancer. He studies the dark magic since ages. Ravashack is a mystery, using dark magic to gain the upper hand on his opponents, but fighting the evil liches, his archenemies.");
-				addReply("jaer", "Jaer is the master of illusion. Charming and flighty like a breeze on a hot summerday. His domain is the air and he has many allies in the plains of mythical ghosts");
-				addReply("cassandra", "Cassandra is a beautifull woman, but first of all a powerfull sorceress. Cassandras domain is the water and she can be cold like ice to achieve her aim.");
-				addReply("silvanus", "Silvanus is a sage druid and perhaps the eldest of all elves. He is a friend of all animals, trees, fairy creatures and ents. His domain is the earth and nature.");
-				addReply("malleus", "Malleus is the powerfull archetype of a magician and the master of destructive magics. His domain is the fire and he rambled the plains of demons for ages, to understand their ambitions.");
+				addReply("elana", "Elana is the warmest and friendliest enchantress. She is the protectress of all living creatures and uses divine magic to save and heal them.");
+				addReply("ravashack", "Ravashack is a very mighty necromancer. He has studied the dark magic for ages. Ravashack is a mystery, using dark magic to gain the upper hand on his opponents, but fighting the evil liches, his arch enemies.");
+				addReply("jaer", "Jaer is the master of illusion. Charming and flighty like a breeze on a hot summer day. His domain is Air and he has many allies in the plains of mythical ghosts.");
+				addReply("cassandra", "Cassandra is a beautiful woman, but foremost a powerful sorceress. Cassandra's domain is Water and she can be cold like ice to achieve her aim.");
+				addReply("silvanus", "Silvanus is a sage druid and perhaps the eldest of all elves. He is a friend of all animals, trees, fairy creatures and ents. His domain is Earth and Nature.");
+				addReply("malleus", "Malleus is the powerful archetype of a magician and the master of destructive magics. His domain is Fire and he rambled the plains of demons for ages, to understand their ambitions.");
 
-	//behavior on special item SCROLL
-				add(
-				        ConversationStates.ATTENDING,
-				        Arrays.asList("scroll", "scrolls"),
-				        ConversationStates.INFORMATION_1,
-				        "I will create a magic scroll for you, but I need eight pieces of wood for that. The scroll will be empty and can be enchanted by wizards. Do you want a magic scroll?",
-				        null);
-		add(ConversationStates.INFORMATION_1, ConversationPhrases.YES_MESSAGES,
-			new NotCondition(new PlayerHasItemWithHimCondition("wood", 8)),
-			ConversationStates.ATTENDING,
-			"You don't have enough wood, I will need eight pieces.", null);
-		add(ConversationStates.INFORMATION_1, ConversationPhrases.YES_MESSAGES,
-			new PlayerHasItemWithHimCondition("wood", 8),
-			ConversationStates.ATTENDING,
-			"There is your magic scroll.",
-			new MultipleActions(new DropItemAction("wood", 8),
-			new EquipItemAction("scroll", 1, true),
-			new IncreaseXPAction(250)));
-		add(ConversationStates.INFORMATION_1, ConversationPhrases.NO_MESSAGES, null,
-			ConversationStates.ATTENDING,
-			"Well, maybe later. Just tell me when you want a magic scroll.", null);
+				// behavior on special item SCROLL
+				add(ConversationStates.ATTENDING,
+				    Arrays.asList("scroll", "scrolls"),
+				    ConversationStates.INFORMATION_1,
+				    "I will create a magic scroll for you, but I need eight pieces of wood for that. The scroll will be empty and can be enchanted by wizards. Do you want a magic scroll?",
+				    null);
+				add(ConversationStates.INFORMATION_1, 
+					ConversationPhrases.YES_MESSAGES,
+					new NotCondition(new PlayerHasItemWithHimCondition("wood", 8)),
+					ConversationStates.ATTENDING,
+					"You don't have enough wood, I will need eight pieces.", 
+					null);
+				add(ConversationStates.INFORMATION_1, 
+					ConversationPhrases.YES_MESSAGES,
+					new PlayerHasItemWithHimCondition("wood", 8),
+					ConversationStates.ATTENDING,
+					"There is your magic scroll.",
+					new MultipleActions(
+						new DropItemAction("wood", 8),
+						new EquipItemAction("scroll", 1, true),
+						new IncreaseXPAction(250)));
+				add(ConversationStates.INFORMATION_1, 
+					ConversationPhrases.NO_MESSAGES, 
+					null,
+					ConversationStates.ATTENDING,
+					"Well, maybe later. Just tell me when you want a magic scroll.", 
+					null);
 
-	//behavior on special item RIFT CLOAK
-				add(
-				        ConversationStates.ATTENDING,
-				        Arrays.asList("rift cloak"),
-				        ConversationStates.INFORMATION_2,
-				        "I will create a rift cloak for you, but I have to spine a carbuncle and an sapphire in the magic. The cloak is useless in the battle and will protect you only one time entering a magical rift."+
+				//behavior on special item RIFT CLOAK
+				add(ConversationStates.ATTENDING,
+				    Arrays.asList("rift cloak"),
+				    ConversationStates.INFORMATION_2,
+				    "I will create a rift cloak for you, but I have to fuse a carbuncle and a sapphire in the magic. The cloak is useless in battle and will protect you only one time, when entering a magical rift."+
 					" The rift disintegrates the cloak instead of you. There is no way to get the cloak back. If you want to enter the rift again, you will need a new rift cloak. Shall I create one for you?",
-				        null);
-		add(ConversationStates.INFORMATION_2, ConversationPhrases.YES_MESSAGES,
-			new AndCondition(
-			new NotCondition(new PlayerHasItemWithHimCondition("carbuncle", 1)),
-			new PlayerHasItemWithHimCondition("sapphire", 1)),
-			ConversationStates.ATTENDING,
-			"You don't have a carbuncle, I will need an sapphire and a carbuncle.", null);
-		add(ConversationStates.INFORMATION_2, ConversationPhrases.YES_MESSAGES,
-			new AndCondition(
-			new NotCondition(new PlayerHasItemWithHimCondition("sapphire", 1)),
-			new PlayerHasItemWithHimCondition("carbuncle", 1)),
-			ConversationStates.ATTENDING,
-			"You don't have an emerald, I will need a carbuncle and an sapphire.", null);
-		add(ConversationStates.INFORMATION_2, ConversationPhrases.YES_MESSAGES,
-			new AndCondition(
-			new PlayerHasItemWithHimCondition("sapphire", 1),
-			new PlayerHasItemWithHimCondition("carbuncle", 1)),
-			ConversationStates.ATTENDING,
-			"There is your rift cloak. Don't forget that it protects you only one time, befor it will be destroyed. So be sure that you are ready for what awaits you in the rift.",
-			new MultipleActions(new DropItemAction("carbuncle", 1),
-			new DropItemAction("sapphire", 1),
-			new EquipItemAction("rift cloak", 1, true),
-			new IncreaseXPAction(5000)));
-		add(ConversationStates.INFORMATION_2, ConversationPhrases.NO_MESSAGES, null,
-			ConversationStates.ATTENDING,
-			"Don't forget that you can't enter a magical rift without a rift cloak.", null);
+				     null);
+				add(ConversationStates.INFORMATION_2, 
+					ConversationPhrases.YES_MESSAGES,
+					new AndCondition(
+							new NotCondition(new PlayerHasItemWithHimCondition("carbuncle", 1)),
+							new PlayerHasItemWithHimCondition("sapphire", 1)),
+					ConversationStates.ATTENDING,
+					"You don't have a carbuncle, I will need a sapphire and a carbuncle.",
+					null);
+				add(ConversationStates.INFORMATION_2, 
+					ConversationPhrases.YES_MESSAGES,
+					new AndCondition(
+							new NotCondition(new PlayerHasItemWithHimCondition("sapphire", 1)),
+							new PlayerHasItemWithHimCondition("carbuncle", 1)),
+					ConversationStates.ATTENDING,
+					"You don't have a sapphire, I will need a carbuncle and a sapphire.",
+					null);
+				add(ConversationStates.INFORMATION_2, ConversationPhrases.YES_MESSAGES,
+						new AndCondition(
+								new PlayerHasItemWithHimCondition("sapphire", 1),
+								new PlayerHasItemWithHimCondition("carbuncle", 1)),
+					ConversationStates.ATTENDING,
+					"There is your rift cloak. Don't forget that it protects you only one time, before it is destroyed. So be sure that you are ready for what awaits you in the rift.",
+					new MultipleActions(
+							new DropItemAction("carbuncle", 1),
+							new DropItemAction("sapphire", 1),
+							new EquipItemAction("rift cloak", 1, true),
+							new IncreaseXPAction(5000)));
+				add(ConversationStates.INFORMATION_2, 
+					ConversationPhrases.NO_MESSAGES, 
+					null,
+					ConversationStates.ATTENDING,
+					"Don't forget that you can't enter a magical rift without a rift cloak.",
+					null);
 
-	//behavior on special item XARUHWAIYZ PHIAL
+				//behavior on special item XARUHWAIYZ PHIAL
 			} //remaining behavior defined in maps.quests.ZekielsPracticalTestQuest
 		};
 
