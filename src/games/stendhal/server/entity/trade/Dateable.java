fcc6c6435270bb8d568f83975dfc31beee431604
Markus <madmetzger@users.sourceforge.net>
2010-07-18 11:35:20 +0000

    corrected java doc a bit

--- src/games/stendhal/server/entity/trade/Dateable.java
+++ src/games/stendhal/server/entity/trade/Dateable.java
@@ -1,5 +1,13 @@
 package games.stendhal.server.entity.trade;
 
+/**
+ * Interface for objects that can give information about a point of time
+ * 
+ * @author kiheru
+ */
 public interface Dateable {
+	/**
+	 * @return point of time relevant for this object
+	 */
 	long getTimestamp();
 }
