b605fd113e49d300802eaf78187430f6d2e1fcb3
laguz <mkvii@users.sourceforge.net>
2010-07-22 17:58:46 +0000

    outlined behave offering items of zekiel and cassandra

--- src/games/stendhal/server/maps/semos/wizardstower/BlueIceSorceressNPC.java
+++ src/games/stendhal/server/maps/semos/wizardstower/BlueIceSorceressNPC.java
@@ -64,31 +64,40 @@ public class BlueIceSorceressNPC implements ZoneConfigurator {
 			@Override
 			protected void createDialog() {
 				addGreeting("Hello dear!");
-				addHelp("I can conjure an #ice #scroll for you. It is able to protect you from dangerous heat, depends on how you use it.");
+
+				//addHelp("I can conjure an #ice #scroll for you. It can't help you in the battle. But it can protect you from natural heat on certain dangerous terrains. You will know when you need it.");
+				addHelp("Excuse me, I am very busy establishing the sorceresses part at the wizards circle.");
+
 				addJob("I am Cassandra, the sorceress of water and ice. I represent #Frostshade at the wizards circle.");
-				addOffer("I can conjure an #ice #scroll for you. It is able to protect you from dangerous heat, depends on how you use it.");
+
+				//addOffer("I can conjure an #ice #scroll for you. It can't help you in the battle. But it can protect you from natural heat on certain dangerous terrains. You will know when you need it.");
+				addOffer("Excuse me, I am very busy establishing the sorceresses part at the wizards circle.");
+
 				addReply("Frostshade", "Frostshade is the school of the magic of water and ice. It lies deep in the northern glaciers.");
-				addReply("scroll", "Zekiel is the storekeeper of the tower, I am sure he can help you.");
+				addReply("blank scroll", "Zekiel is the storekeeper of the tower, I am sure he can help you.");
 				addQuest("The magic in this world has just begun and I am very busy to establish the sorceresses of #Frostshade at the wizards circle. I will tell you in time, when I have a quest for you.");
 				addGoodbye("Bye!");
-				addReply(Arrays.asList("ice scroll", "ice scrolls"),
-				        "I need a #scroll for that. If you bring me one, I will #enchant it for you.");
-				add(
-				        ConversationStates.ATTENDING,
-				        Arrays.asList("enchant"),
-				        new PlayerHasItemWithHimCondition("scroll"),
-				        ConversationStates.ATTENDING,
-				        "I enchanted your scroll to an ice scroll. May it cool off your feets on your travels.",
-				        new MultipleActions(
-				        		new DropItemAction("scroll", 1),
-				        		new EquipItemAction("ice scroll", 1, true),
-				        		new IncreaseXPAction(250)));
-				add(
-				        ConversationStates.ATTENDING,
-				        Arrays.asList("enchant"),
-				    new NotCondition(new PlayerHasItemWithHimCondition("scroll")),
-				        ConversationStates.ATTENDING,
-					"You don't have a #scroll that I could enchant.", null);
+
+/**				addReply(Arrays.asList("ice scroll", "ice scrolls"),
+*				        "I need a #blank #scroll for that. If you bring me one, I will #enchant it for you.");
+*				add(
+*				        ConversationStates.ATTENDING,
+*				        Arrays.asList("enchant"),
+*				        new PlayerHasItemWithHimCondition("blank scroll"),
+*				        ConversationStates.ATTENDING,
+*				        "I enchanted your blank scroll to an ice scroll. May it cool off your feets on your travels.",
+*				        new MultipleActions(
+*				        		new DropItemAction("blank scroll", 1),
+*				        		new EquipItemAction("ice scroll", 1, true),
+*				        		new IncreaseXPAction(250)));
+*				add(
+*				        ConversationStates.ATTENDING,
+*				        Arrays.asList("enchant"),
+*				    new NotCondition(new PlayerHasItemWithHimCondition("blank scroll")),
+*				        ConversationStates.ATTENDING,
+*					"You don't have a #blank #scroll that I could enchant.", null);
+*/
+
 			} //remaining behaviour defined in maps.quests.SorceressCassandraPlainQuest
 		};
 
