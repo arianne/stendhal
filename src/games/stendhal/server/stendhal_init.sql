64d9e88e947ffb7f1ba955aa04bac937f2a1abfd
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-07-22 18:53:27 +0000

    removed database code for experimental achievements

--- src/games/stendhal/server/stendhal_init.sql
+++ src/games/stendhal/server/stendhal_init.sql
@@ -162,24 +162,3 @@ CREATE TABLE IF NOT EXISTS buddy (
 );
 
 /* CREATE INDEX i_buddy_charname ON buddy (charname); */
-
-CREATE TABLE IF NOT EXISTS achievement (
-    id INTEGER AUTO_INCREMENT NOT NULL,
-    identifier VARCHAR(64),
-    title VARCHAR(64),
-    category VARCHAR(64),
-    description VARCHAR(64),
-    PRIMARY KEY(id)
-);
-/* CREATE UNIQUE INDEX i_achievement_identifier ON achievement(identifier);*/
-
-CREATE TABLE IF NOT EXISTS reached_achievement (
-    id INTEGER AUTO_INCREMENT NOT NULL,
-    charname VARCHAR(32),
-    timedate TIMESTAMP default CURRENT_TIMESTAMP,
-    achievement_id INTEGER,
-    PRIMARY KEY(id)
-);
-
-
-/* CREATE INDEX i_reached_achievement_charname ON reached_achievement(charname);*/
