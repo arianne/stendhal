fe8a525ae5096f8e3bb037ec072d53db0e64b248
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-07-27 19:40:55 +0000

    changed java download url to http://java.com (and claim that java 6 is needed, but check is only for 5)

--- src/games/stendhal/client/update/Starter.java
+++ src/games/stendhal/client/update/Starter.java
@@ -33,11 +33,10 @@ public class Starter {
 			final String version = System.getProperty("java.specification.version");
 			if (Float.parseFloat(version) < 1.5f) {
 				JOptionPane.showMessageDialog(
-						null,
-						"You need at least Java 1.5.0 (also known as 5.0) but you only have "
-								+ version
-								+ ". You can download it at http://java.sun.com");
-				System.exit(-1);
+					null,
+					"You need at least Java 6 (also known as 1.6.0) but you only have "
+					+ version
+					+ ". You can download it at http://java.com");
 			}
 		} catch (final RuntimeException e) {
 			// ignore
