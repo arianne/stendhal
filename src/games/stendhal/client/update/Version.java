4764de1e31498655051777c5979369416fc38cfb
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-07-27 17:25:00 +0000

    updated version number

--- src/games/stendhal/client/update/Version.java
+++ src/games/stendhal/client/update/Version.java
@@ -10,7 +10,7 @@ package games.stendhal.client.update;
 public class Version {
 
 	/** Version Number. */
-	public static final String VERSION = "0.86";
+	public static final String VERSION = "0.86.1";
 
 	/**
 	 * Extract the specified number of parts from a version-string.
