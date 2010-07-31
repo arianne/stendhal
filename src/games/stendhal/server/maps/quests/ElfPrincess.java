509fabc64947d3ada4a619d699a2e64a677450ec
Markus <madmetzger@users.sourceforge.net>
2010-07-31 18:16:21 +0000

    add a time limit to the quest (fixes #2949443)

--- src/games/stendhal/server/maps/quests/ElfPrincess.java
+++ src/games/stendhal/server/maps/quests/ElfPrincess.java
@@ -8,10 +8,16 @@ import games.stendhal.server.entity.npc.ConversationPhrases;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.EventRaiser;
 import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.action.DecreaseKarmaAction;
+import games.stendhal.server.entity.npc.action.DropItemAction;
 import games.stendhal.server.entity.npc.action.EquipItemAction;
+import games.stendhal.server.entity.npc.action.IncreaseKarmaAction;
+import games.stendhal.server.entity.npc.action.IncreaseXPAction;
+import games.stendhal.server.entity.npc.action.IncrementQuestAction;
 import games.stendhal.server.entity.npc.action.MultipleActions;
+import games.stendhal.server.entity.npc.action.SayTimeRemainingAction;
 import games.stendhal.server.entity.npc.action.SetQuestAction;
-import games.stendhal.server.entity.npc.action.SetQuestAndModifyKarmaAction;
+import games.stendhal.server.entity.npc.action.SetQuestToTimeStampAction;
 import games.stendhal.server.entity.npc.condition.AndCondition;
 import games.stendhal.server.entity.npc.condition.NotCondition;
 import games.stendhal.server.entity.npc.condition.OrCondition;
@@ -20,6 +26,7 @@ import games.stendhal.server.entity.npc.condition.QuestCompletedCondition;
 import games.stendhal.server.entity.npc.condition.QuestInStateCondition;
 import games.stendhal.server.entity.npc.condition.QuestNotInStateCondition;
 import games.stendhal.server.entity.npc.condition.QuestNotStartedCondition;
+import games.stendhal.server.entity.npc.condition.TimePassedCondition;
 import games.stendhal.server.entity.npc.parser.Sentence;
 import games.stendhal.server.entity.player.Player;
 
@@ -80,13 +87,21 @@ public class ElfPrincess extends AbstractQuest {
 			new QuestCompletedCondition("QUEST_SLOT"),
 			ConversationStates.ATTENDING,
 			"I have plenty of blooms now thank you.", null);
-
+		
 		npc.add(ConversationStates.ATTENDING,
 			ConversationPhrases.QUEST_MESSAGES,
-			new QuestInStateCondition(QUEST_SLOT, "flower_brought"),
+			new AndCondition(new QuestInStateCondition(QUEST_SLOT, 0, "flower_brought"), new TimePassedCondition(QUEST_SLOT, 5, 1)),
 			ConversationStates.QUEST_OFFERED,
 			"The last Rhosyd you brought me was so lovely. Will you find me another from Rose Leigh?",
 			null);
+		
+		//last brought flower is less than 5 minutes ago
+		npc.add(ConversationStates.ATTENDING,
+				ConversationPhrases.QUEST_MESSAGES,
+				new AndCondition(new QuestInStateCondition(QUEST_SLOT, 0, "flower_brought"), new NotCondition(new TimePassedCondition(QUEST_SLOT, 5, 1))),
+				ConversationStates.QUEST_OFFERED,
+				null,
+				new SayTimeRemainingAction(QUEST_SLOT,"I need to put the lovely flower into my collection before you can bring me a new one. Please check back in", 5, 1));
 
 		npc.add(ConversationStates.ATTENDING,
 			ConversationPhrases.QUEST_MESSAGES,
@@ -100,13 +115,15 @@ public class ElfPrincess extends AbstractQuest {
 			ConversationPhrases.YES_MESSAGES, null,
 			ConversationStates.ATTENDING,
 			"Thank you! Once you find it, say #flower to me so I know you have it. I'll be sure to give you a nice reward.",
-			new SetQuestAndModifyKarmaAction(QUEST_SLOT, "start", 10.0));
-
+			new MultipleActions(new SetQuestAction(QUEST_SLOT, 0, "start"),
+								new IncreaseKarmaAction(10.0)));
+		//new SetQuestAndModifyKarmaAction(QUEST_SLOT, "start", 10.0)
 		// Player says no, they've lost karma.
 		npc.add(ConversationStates.QUEST_OFFERED,
 			ConversationPhrases.NO_MESSAGES, null, ConversationStates.IDLE,
 			"Oh, never mind. Bye then.",
-			new SetQuestAndModifyKarmaAction(QUEST_SLOT, "rejected", -10.0));
+			new MultipleActions(new SetQuestAction(QUEST_SLOT, 0, "rejected"),
+					new DecreaseKarmaAction(10.0)));
 	}
 
 	private void getFlowerStep() {
@@ -114,14 +131,14 @@ public class ElfPrincess extends AbstractQuest {
 
 		rose.add(ConversationStates.IDLE,
 			ConversationPhrases.GREETING_MESSAGES,
-			new QuestInStateCondition(QUEST_SLOT, "start"),
+			new QuestInStateCondition(QUEST_SLOT, 0, "start"),
 			ConversationStates.IDLE, 
 			"Hello dearie. My far sight tells me you need a pretty flower for some fair maiden. Here ye arr, bye now.",
-			new MultipleActions(new EquipItemAction("rhosyd", 1, true), new SetQuestAction(QUEST_SLOT, "got_flower")));
+			new MultipleActions(new EquipItemAction("rhosyd", 1, true), new SetQuestAction(QUEST_SLOT, 0, "got_flower")));
 
 	rose.add(ConversationStates.IDLE,
 			ConversationPhrases.GREETING_MESSAGES,
-			new QuestNotInStateCondition(QUEST_SLOT, "start"),
+			new QuestNotInStateCondition(QUEST_SLOT, 0, "start"),
 			ConversationStates.IDLE,
 			"I've got nothing for you today, sorry dearie. I'll be on my way now, bye.", 
 			null);
@@ -129,27 +146,27 @@ public class ElfPrincess extends AbstractQuest {
 
 	private void bringFlowerStep() {
 		final SpeakerNPC npc = npcs.get("Tywysoga");
+		ChatAction addRandomNumberOfItemsAction = new ChatAction() {
+			public void fire(final Player player, final Sentence sentence, final EventRaiser npc) {
+				//add random number of goldbars
+				final StackableItem goldbars = (StackableItem) SingletonRepository.getEntityManager()
+						.getItem("gold bar");
+				int goldamount;
+				goldamount = 5 * Rand.roll1D6();
+				goldbars.setQuantity(goldamount);
+				// goldbars.setBoundTo(player.getName()); <- not sure
+				// if these should get bound or not.
+				player.equipOrPutOnGround(goldbars);
+				npc.say("Thank you! Take these " + Integer.toString(goldamount) + " gold bars, I have plenty. And, listen: If you'd ever like to get me another, be sure to ask me first. Rose Leigh is superstitious, she won't give the bloom unless she senses you need it.");
+			}
+		};
 		npc.add(ConversationStates.ATTENDING,
 				Arrays.asList("flower", "Rhosyd"),
-				new AndCondition(new QuestInStateCondition(QUEST_SLOT, "got_flower"), new PlayerHasItemWithHimCondition("rhosyd")),
+				new AndCondition(new QuestInStateCondition(QUEST_SLOT, 0, "got_flower"), new PlayerHasItemWithHimCondition("rhosyd")),
 				ConversationStates.ATTENDING, null,
-				new ChatAction() {
-					public void fire(final Player player, final Sentence sentence, final EventRaiser npc) {
-						player.drop("rhosyd");
-						player.addXP(5000);
-						player.addKarma(15);
-						final StackableItem goldbars = (StackableItem) SingletonRepository.getEntityManager()
-								.getItem("gold bar");
-						int goldamount;
-						goldamount = 5 * Rand.roll1D6();
-						goldbars.setQuantity(goldamount);
-						// goldbars.setBoundTo(player.getName()); <- not sure
-						// if these should get bound or not.
-						player.equipOrPutOnGround(goldbars);
-						npc.say("Thank you! Take these " + Integer.toString(goldamount) + " gold bars, I have plenty. And, listen: If you'd ever like to get me another, be sure to ask me first. Rose Leigh is superstitious, she won't give the bloom unless she senses you need it.");
-						player.setQuest(QUEST_SLOT, "flower_brought");
-					}
-				});
+				new MultipleActions(new DropItemAction("rhosyd"), new IncreaseXPAction(5000), new IncreaseKarmaAction(15),
+									addRandomNumberOfItemsAction, new SetQuestAction(QUEST_SLOT, 0, "flower_brought"), new SetQuestToTimeStampAction(QUEST_SLOT, 1),
+									new IncrementQuestAction(QUEST_SLOT, 2, 1)));
 
 		npc.add(ConversationStates.ATTENDING,
 			Arrays.asList("flower", "Rhosyd"),
