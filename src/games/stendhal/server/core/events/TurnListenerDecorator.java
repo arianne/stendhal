e785ca18bbc8eeee1112597b65cff6429deede58
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-07-31 19:55:09 +0000

    created a new class TurnListenerDecorator which allows to put add turn listeners multiple times (#3037636)

---
+++ src/games/stendhal/server/core/events/TurnListenerDecorator.java
@@ -0,0 +1,39 @@
+/***************************************************************************
+ *                    (C) Copyright 2003-2010 - Stendhal                   *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.core.events;
+
+/**
+ * a TurnListener decorator. Multiple instance can point to the same
+ * real TurnListener but will be treated as unique listeners by the 
+ * set in the TurnNotifier.
+ *
+ * @author hendrik
+ */
+public class TurnListenerDecorator implements TurnListener {
+	private TurnListener turnListener;
+
+	/**
+	 * creates a new TurnListenerDecorator
+	 *
+	 * @param turnListener the real turnlistener to delegate to
+	 */
+	public TurnListenerDecorator(TurnListener turnListener) {
+		this.turnListener = turnListener;
+	}
+
+	public void onTurnReached(int currentTurn) {
+		if (turnListener != null) {
+			turnListener.onTurnReached(currentTurn);
+		}
+	}
+
+}
