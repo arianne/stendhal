f13970be45e645c686313c4ac2b1d6a59bd69c53
Katie Russell <kymara@users.sourceforge.net>
2010-07-31 21:05:28 +0000

    zone.toString() starts with "zone" hence got : Entity added while in another zone zone IRPZone.ID in a logger warning, fixed the warning.

--- src/games/stendhal/server/entity/Entity.java
+++ src/games/stendhal/server/entity/Entity.java
@@ -475,7 +475,7 @@ public abstract class Entity extends RPObject {
 	 */
 	public void onAdded(final StendhalRPZone zone) {
 		if (this.zone != null) {
-			logger.error("Entity added while in another zone " + zone + ": " + this, new Throwable());
+			logger.error("Entity added while in another " + zone + ": " + this, new Throwable());
 		}
 
 		this.zone = zone;
