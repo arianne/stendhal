805a5ef1b3c247844220c157d140aeff2ee0b4e7
Markus <madmetzger@users.sourceforge.net>
2010-07-31 13:02:12 +0000

    make factory nicer

--- src/games/stendhal/server/core/events/achievements/QuestAchievementFactory.java
+++ src/games/stendhal/server/core/events/achievements/QuestAchievementFactory.java
@@ -13,47 +13,37 @@ public class QuestAchievementFactory extends AchievementFactory {
 	 * 
 	 * @return all available quest achievements in a collection
 	 */
+	@Override
 	public Collection<Achievement> createAchievements() {
 		List<Achievement> questAchievements = new LinkedList<Achievement>();
 		//daily monster quest achievements
-		questAchievements.add(new Achievement("quest.special.dmq.10", "Semos' Protector",
-												getCategory(),  "Finish daily monster quest 10 times", Achievement.EASY_BASE_SCORE,
-												new QuestStateGreaterThanCondition("daily", 2, 9)));
-		questAchievements.add(new Achievement("quest.special.dmq.50", "Semos' Guardian",
-												getCategory(),  "Finish daily monster quest 50 times", Achievement.EASY_BASE_SCORE,
-												new QuestStateGreaterThanCondition("daily", 2, 49)));
-		questAchievements.add(new Achievement("quest.special.dmq.100", "Semos' Hero",
-												getCategory(),  "Finish daily monster quest 100 times", Achievement.MEDIUM_BASE_SCORE,
-												new QuestStateGreaterThanCondition("daily", 2, 99)));
-		questAchievements.add(new Achievement("quest.special.dmq.250", "Semos' Champion",
-												getCategory(),  "Finish daily monster quest 250 times", Achievement.MEDIUM_BASE_SCORE,
-												new QuestStateGreaterThanCondition("daily", 2, 249)));
-		questAchievements.add(new Achievement("quest.special.dmq.500", "Semos' Vanquisher",
-												getCategory(),  "Finish daily monster quest 500 times", Achievement.HARD_BASE_SCORE,
-												new QuestStateGreaterThanCondition("daily", 2, 499)));
+		questAchievements.add(createAchievement("quest.special.dmq.10", "Semos' Protector", "Finish daily monster quest 10 times",
+												Achievement.EASY_BASE_SCORE, new QuestStateGreaterThanCondition("daily", 2, 9)));
+		questAchievements.add(createAchievement("quest.special.dmq.50", "Semos' Guardian", "Finish daily monster quest 50 times", 
+												Achievement.EASY_BASE_SCORE, new QuestStateGreaterThanCondition("daily", 2, 49)));
+		questAchievements.add(createAchievement("quest.special.dmq.100", "Semos' Hero", "Finish daily monster quest 100 times", 
+												Achievement.MEDIUM_BASE_SCORE, new QuestStateGreaterThanCondition("daily", 2, 99)));
+		questAchievements.add(createAchievement("quest.special.dmq.250", "Semos' Champion", "Finish daily monster quest 250 times", 
+												Achievement.MEDIUM_BASE_SCORE, new QuestStateGreaterThanCondition("daily", 2, 249)));
+		questAchievements.add(createAchievement("quest.special.dmq.500", "Semos' Vanquisher", "Finish daily monster quest 500 times", 
+												Achievement.HARD_BASE_SCORE, new QuestStateGreaterThanCondition("daily", 2, 499)));
 		//daily item quest achievements
-		questAchievements.add(new Achievement("quest.special.diq.10", "Ados' Supporter",
-												getCategory(),  "Finish daily item quest 10 times", Achievement.EASY_BASE_SCORE,
-												new QuestStateGreaterThanCondition("daily_item", 2, 9)));
-		questAchievements.add(new Achievement("quest.special.diq.50", "Ados' Provider",
-												getCategory(),  "Finish daily item quest 50 times", Achievement.EASY_BASE_SCORE,
-												new QuestStateGreaterThanCondition("daily_item", 2, 49)));
-		questAchievements.add(new Achievement("quest.special.diq.100", "Ados' Supplier",
-												getCategory(),  "Finish daily item quest 100 times", Achievement.MEDIUM_BASE_SCORE,
-												new QuestStateGreaterThanCondition("daily_item", 2, 99)));
-		questAchievements.add(new Achievement("quest.special.diq.250", "Ados' Stockpiler",
-												getCategory(),  "Finish daily item quest 250 times", Achievement.MEDIUM_BASE_SCORE,
-												new QuestStateGreaterThanCondition("daily_item", 2, 249)));
-		questAchievements.add(new Achievement("quest.special.diq.50", "Ados' Hoarder",
-												getCategory(),  "Finish daily item quest 50 times", Achievement.HARD_BASE_SCORE,
-												new QuestStateGreaterThanCondition("daily_item", 2, 499)));
+		questAchievements.add(createAchievement("quest.special.diq.10", "Ados' Supporter", "Finish daily item quest 10 times", 
+												Achievement.EASY_BASE_SCORE, new QuestStateGreaterThanCondition("daily_item", 2, 9)));
+		questAchievements.add(createAchievement("quest.special.diq.50", "Ados' Provider", "Finish daily item quest 50 times", 
+												Achievement.EASY_BASE_SCORE, new QuestStateGreaterThanCondition("daily_item", 2, 49)));
+		questAchievements.add(createAchievement("quest.special.diq.100", "Ados' Supplier", "Finish daily item quest 100 times",
+												Achievement.MEDIUM_BASE_SCORE, new QuestStateGreaterThanCondition("daily_item", 2, 99)));
+		questAchievements.add(createAchievement("quest.special.diq.250", "Ados' Stockpiler", "Finish daily item quest 250 times", 
+												Achievement.MEDIUM_BASE_SCORE, new QuestStateGreaterThanCondition("daily_item", 2, 249)));
+		questAchievements.add(createAchievement("quest.special.diq.50", "Ados' Hoarder", "Finish daily item quest 50 times", 
+												Achievement.HARD_BASE_SCORE, new QuestStateGreaterThanCondition("daily_item", 2, 499)));
 		//weekly item quest achievement
-		questAchievements.add(new Achievement("quest.special.wiq.5", "Archaeologist",
-												getCategory(),  "Finish weekly item quest 5 times", Achievement.HARD_BASE_SCORE,
-												new QuestStateGreaterThanCondition("weekly_item", 2, 4)));
+		questAchievements.add(createAchievement("quest.special.wiq.5", "Archaeologist", "Finish weekly item quest 5 times", 
+												Achievement.HARD_BASE_SCORE, new QuestStateGreaterThanCondition("weekly_item", 2, 4)));
 		return questAchievements;
 	}
-	
+
 	@Override
 	protected Category getCategory() {
 		return Category.QUEST;
