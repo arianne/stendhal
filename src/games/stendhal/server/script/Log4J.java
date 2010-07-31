aa5f7b6ddc459be3399535eb7982f867849be3e3
Markus <madmetzger@users.sourceforge.net>
2010-07-31 19:49:51 +0000

    add a script to change the log level for certain loggers at runtime (fixes #3037632)

---
+++ src/games/stendhal/server/script/Log4J.java
@@ -0,0 +1,47 @@
+package games.stendhal.server.script;
+
+import java.util.List;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+
+import games.stendhal.server.core.scripting.ScriptImpl;
+import games.stendhal.server.entity.player.Player;
+/**
+ * Script to change log level of certain loggers at runtime
+ * 
+ * Parameters:
+ * 1. full qualified class name where the logger is used
+ * 2. log level as defined by log4j
+ *    - INFO
+ *    - DEBUG
+ *    - WARN
+ *    - ERROR
+ *    - FATAL
+ *    - OFF
+ *    - TRACE
+ *    - ALL
+ *    
+ * For log level names see also the log4j documentation
+ *  
+ * @author madmetzger
+ */
+public class Log4J extends ScriptImpl {
+	
+	@Override
+	public void execute(Player admin, List<String> args) {
+		super.execute(admin, args);
+		if(args.size() != 2) {
+			admin.sendPrivateText("Needed parameters: [full qualified class name] [log level]");
+			return;
+		}
+		Level levelToSet = Level.toLevel(args.get(1), Level.INFO);
+		try {
+			Class<?> clazz = Class.forName(args.get(0));
+			Logger.getLogger(clazz).setLevel(levelToSet);
+		} catch (ClassNotFoundException e) {
+			admin.sendPrivateText("The class '"+args.get(0)+"' could not be found.");
+		}
+	}
+
+}
