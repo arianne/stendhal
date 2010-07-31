a6ed03fb5fc19fecf79da43258dd7467f699fccf
Markus <madmetzger@users.sourceforge.net>
2010-07-31 18:17:24 +0000

    add an achievement for the elf princess quest

--- src/games/stendhal/server/core/events/achievements/QuestAchievementFactory.java
+++ src/games/stendhal/server/core/events/achievements/QuestAchievementFactory.java
@@ -40,6 +40,9 @@ public class QuestAchievementFactory extends AchievementFactory {
 		//weekly item quest achievement
 		questAchievements.add(createAchievement("quest.special.wiq.5", "Archaeologist", "Finish weekly item quest 5 times", 
 												Achievement.HARD_BASE_SCORE, new QuestStateGreaterThanCondition("weekly_item", 2, 4)));
+		//elf princess quest achievement
+		questAchievements.add(createAchievement("quest.special.rhosyd.25", "Casanova", "Finish elf princess quest 25 times", 
+												Achievement.MEDIUM_BASE_SCORE, new QuestStateGreaterThanCondition("elf_princess", 2, 24)));
 		return questAchievements;
 	}
 
