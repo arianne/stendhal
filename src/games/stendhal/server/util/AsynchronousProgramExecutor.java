64d81e257885ea588467d73335a24e01d5a2b9f5
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-08-14 16:47:00 +0000

    execute an external tool specified in server.ini

--- src/games/stendhal/server/util/AsynchronousProgramExecutor.java
+++ src/games/stendhal/server/util/AsynchronousProgramExecutor.java
@@ -1,18 +1,13 @@
 package games.stendhal.server.util;
 
-import games.stendhal.common.Base64;
-
 import java.io.IOException;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 import marauroa.common.Configuration;
 
 import org.apache.log4j.Logger;
 
 /**
- * Accesses twitter
+ * Executes an external program
  * 
  * @author hendrik
  */
@@ -22,7 +17,7 @@ public class AsynchronousProgramExecutor extends Thread {
 	private String account;
 
 	/**
-	 * Sends a tweet
+	 * Creates a new AsynchronousProgramExecutor
 	 * 
 	 * @param account
 	 *            account to use
@@ -35,8 +30,7 @@ public class AsynchronousProgramExecutor extends Thread {
 	}
 
 	/**
-	 * Sends a tweet. Use "start()" for asynchronous access.
-	 * 
+	 * Executes the program. Use "start()" for asynchronous access.
 	 */
 	public void run() {
 		Configuration configuration;
@@ -48,44 +42,29 @@ public class AsynchronousProgramExecutor extends Thread {
 		}
 
 		// check that a password for this account was configured
-		if (!configuration.has("stendhal.twitter." + account + ".password")) {
+		if (!configuration.has("stendhal.program." + account)) {
 			return;
 		}
 
-		// get username and password from configuratoin
-		String username = configuration.get("stendhal.twitter." + account + ".username", account);
-		String password = configuration.get("stendhal.twitter." + account + ".password");
-
-		// send the tweet
-		send(username, password, message);
+		String cmd = configuration.get("stendhal.program." + account);
+		send(cmd, message);
 	}
 
 	/**
 	 * sends the message to the twitter account
 	 *
-	 * @param username username of the twitter account
-	 * @param password password of the twitter account
+	 * @param cmd command
 	 * @param message message to tweet
 	 */
-	private void send(String username, String password, String message) {
+	private void send(String cmd, String message) {
 		try {
-			URL url = new URL("https://api.twitter.com/1/statuses/update.xml");
-			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-			// Setup the header for the request
-			connection.setRequestMethod("POST");
-			connection.setRequestProperty("User-Agent", "Stendhal - http://stendhalgame.org");
-			String input = username + ":" + password;
-			connection.setRequestProperty("Authorization", "Basic " + new String(Base64.encode(input.getBytes("UTF-8"))));
-
-			// write the message
-			connection.setDoOutput(true);
-			OutputStream os = connection.getOutputStream();
-			os.write(("status=" + message.substring(0, Math.min(message.length(), 139))).getBytes("UTF-8"));
-			os.close();
-
-			// send the request
-			connection.getInputStream().close();
+			String[] args = new String[2];
+			args[0] = cmd;
+			args[1] = message;
+			Process p = Runtime.getRuntime().exec(args);
+			p.getErrorStream().close();
+			p.getOutputStream().close();
+			p.getInputStream().close();
 		} catch (IOException e) {
 			logger.error(e, e);
 		}
