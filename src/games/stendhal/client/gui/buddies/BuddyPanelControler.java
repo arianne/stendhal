128edcd8bc4154e4869649e0bbd75944a3f7d9f9
yoriy <yoriy@users.sourceforge.net>
2010-08-17 07:04:27 +0000

    retrieve warnings from getOldValue()

--- src/games/stendhal/client/gui/buddies/BuddyPanelControler.java
+++ src/games/stendhal/client/gui/buddies/BuddyPanelControler.java
@@ -89,7 +89,6 @@ public class BuddyPanelControler implements PropertyChangeListener {
 	
 	class MapBasedBuddyPropertyChangeListener implements PropertyChangeListener {
 
-		@SuppressWarnings("unchecked")
 		public void propertyChange(PropertyChangeEvent evt) {
 			if (evt == null) {
 				return;
