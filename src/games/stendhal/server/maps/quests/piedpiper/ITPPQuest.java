fbd8a97e3683e694779a30f22b01464b2035dc59
yoriy <yoriy@users.sourceforge.net>
2010-08-17 06:45:48 +0000

    interface for quest phases switching

---
+++ src/games/stendhal/server/maps/quests/piedpiper/ITPPQuest.java
@@ -0,0 +1,49 @@
+package games.stendhal.server.maps.quests.piedpiper;
+
+public interface ITPPQuest extends ITPPQuestConstants {
+	
+	/**
+	 * function will change phase to next phase 
+	 */
+	void phaseToNextPhase(ITPPQuest nextPhase);
+	
+	/**
+	 * function will reset quest state to default phase (INACTIVE).
+	 */
+	void phaseToDefaultPhase();
+	
+	/**
+	 * function return shout message when quest going to next phase 
+	 * @return
+	 */
+	String getSwitchingToNextPhaseMessage();
+	
+	/**
+	 * function return shout message when quest going to default phase
+	 * @return
+	 */
+	String getSwitchingToDefPhaseMessage();
+	
+	/**
+	 * return minimal timeout period for quest phase
+	 * @return
+	 */
+	int getMinTimeOut();
+	
+	/**
+	 * return maximal timeout period for quest phase
+	 * @return
+	 */
+	int getMaxTimeOut();
+	
+	/**
+	 * return quest phase
+	 */
+	TPP_Phase getPhase();
+
+	/**
+	 * function will perform necessary actions at phase start
+	 */
+	void prepare();
+}
+
