a91245b7101cd077105bcfc42a5b816e0f9d14ac
Bluelads4 <bluelads99@users.sourceforge.net>
2010-08-23 12:07:22 +0000

    Added new description for Semos NPC Phalk

--- src/games/stendhal/server/maps/semos/mines/DwarfGuardianNPC.java
+++ src/games/stendhal/server/maps/semos/mines/DwarfGuardianNPC.java
@@ -62,6 +62,7 @@ public class DwarfGuardianNPC implements ZoneConfigurator {
 		});
 
 		npc.setEntityClass("dwarf_guardiannpc");
+		npc.setDescription("You see Phalk. He is a soldier who doesn't want to abandon adventurers to their fate.");
 		npc.setPosition(118, 26);
 		npc.setDirection(Direction.RIGHT);
 		npc.initHP(25);
