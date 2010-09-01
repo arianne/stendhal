b546533dd2161396f64c4c831deee19337773699
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-09-01 09:42:07 +0000

    made bell on chain useable to play a sound (fixes #3029116)

---
+++ src/games/stendhal/server/entity/mapstuff/useable/PlaySoundEntity.java
@@ -0,0 +1,53 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.entity.mapstuff.useable;
+
+import games.stendhal.common.constants.SoundLayer;
+import games.stendhal.server.entity.RPEntity;
+import games.stendhal.server.events.SoundEvent;
+
+/**
+ * Plays a sound on "use".
+ *
+ * @author hendrik
+ */
+public class PlaySoundEntity extends UseableEntity {
+	private String[] sounds;
+	private int radius;
+	private int volume;
+
+	/**
+	 * creates a new PlaySoundEntity
+	 *
+	 * @param sound   name of sound (or sounds) to play
+	 * @param radius  hearing radius
+	 * @param volume  volume
+	 */
+	public PlaySoundEntity(String sound, int radius, int volume) {
+		this.sounds = sound.split(", *");
+		this.radius = radius;
+		this.volume = volume;
+	}
+
+	/**
+	 * plays a sound
+	 */
+	public boolean onUsed(RPEntity user) {
+		String sound = sounds[(int) (Math.random() * sounds.length)];
+		SoundEvent event = new SoundEvent(sound, radius, volume, SoundLayer.AMBIENT_SOUND);
+		this.addEvent(event);
+		this.notifyWorldAboutChanges();
+		return true;
+	}
+
+}
---
+++ src/games/stendhal/server/entity/mapstuff/useable/PlaySoundEntityFactory.java
@@ -0,0 +1,38 @@
+/*
+ * @(#) src/games/stendhal/server/entity/area/CreatureProtectionAreaFactory.java
+ *
+ * $Id$
+ */
+
+package games.stendhal.server.entity.mapstuff.useable;
+
+import games.stendhal.server.core.config.factory.ConfigurableFactoryContext;
+import games.stendhal.server.entity.mapstuff.sound.LoopedAmbientSoundSource;
+import games.stendhal.server.entity.mapstuff.sound.SoundSourceFactory;
+
+/**
+ * A factory for PeriodicAmbientSoundSource.
+ */
+public class PlaySoundEntityFactory extends SoundSourceFactory {
+
+	/**
+	 * Create an PlaySoundEntity.
+	 * 
+	 * @param ctx
+	 *            Configuration context.
+	 * 
+	 * @return An PlaySoundEntity.
+	 * 
+	 * @throws IllegalArgumentException
+	 *             If there is a problem with the attributes.
+	 * @see LoopedAmbientSoundSource
+	 */
+	public PlaySoundEntity create(final ConfigurableFactoryContext ctx) {
+		PlaySoundEntity source;
+
+		source = new PlaySoundEntity(getSound(ctx), getRadius(ctx), getVolume(ctx));
+
+		return source;
+	}
+
+}
