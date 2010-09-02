8eff3f23efda3b8be5747e38c2cde27fc6217121
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-09-02 04:15:39 +0000

    started to implement the 9 switches game

---
+++ src/games/stendhal/server/entity/mapstuff/game/NineSwitchesGameBoard.java
@@ -0,0 +1,168 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.entity.mapstuff.game;
+
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.RPEntity;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+
+import java.util.ArrayList;
+
+/**
+ * The game board for the 9 switches game.
+ *
+ * @author hendrik
+ */
+public class NineSwitchesGameBoard {
+	private StendhalRPZone zone;
+	private int x;
+	private int y;
+
+	private SpeakerNPC npc;
+	private ArrayList<NineSwitchesGameSwitch> switches;
+
+	private String playerName;
+
+	/**
+	 * creates a new NineSwitcheGameBoard.
+	 *
+	 * @param zone zone the board is placed into
+	 * @param x x-coordinate
+	 * @param y y-coordinate
+	 */
+	public NineSwitchesGameBoard(StendhalRPZone zone, int x, int y) {
+		this.zone = zone;
+		this.x = x;
+		this.y = y;
+		createSwitches();
+	}
+
+	/**
+	 * use the specified switch.
+	 *
+	 * @param user player who used the switch
+	 * @param gameSwitch switch which was used
+	 */
+	public void usedSwitch(RPEntity user, NineSwitchesGameSwitch gameSwitch) {
+		if (playerName == null) {
+			npc.say(user.getName() + ", please talk to me to start a game.");
+			return;
+		}
+
+		if (!user.getName().equals(playerName)) {
+			user.sendPrivateText("Hey " + user.getName() + ", " + playerName + " is currently playing. Please wait a little.");
+			return;
+		}
+
+		switchGameSwitch(gameSwitch);
+		boolean completed = checkBoard();
+		if (completed) {
+			// TODO: reward
+			npc.say("Gongratiolations, " + user.getName() + " you won");
+			// TODO: cancel time
+			playerName = null;
+		}
+	}
+
+	/**
+	 * creates the switches
+	 */
+	private void createSwitches() {
+		switches = new ArrayList<NineSwitchesGameSwitch>();
+		for (int iRow = 0; iRow < 3; iRow++) {
+			for (int iCol = 0; iCol < 3; iCol++) {
+				NineSwitchesGameSwitch gameSwitch = new NineSwitchesGameSwitch(this);
+				gameSwitch.setPosition(x + iCol, y + iRow);
+				zone.add(gameSwitch);
+				switches.add(gameSwitch);
+			}
+		}
+	}
+
+
+	/**
+	 * toggles the switches linked to the used one.
+	 *
+	 * @param gameSwitch used switch
+	 */
+	private void switchGameSwitch(NineSwitchesGameSwitch gameSwitch) {
+		int index = switches.indexOf(gameSwitch);
+		int row = index / 3;
+		int col = index % 3;
+		
+		if (row > 0) {
+			switches.get((row - 1) * 3 + col).toggle();
+		}
+		if (col > 0) {
+			switches.get(row * 3 + col - 1).toggle();
+		}
+		gameSwitch.toggle();
+		if (row < 2) {
+			switches.get((row + 1) * 3 + col).toggle();
+		}
+		if (col < 2) {
+			switches.get(row * 3 + col + 1).toggle();
+		}
+	}
+
+	/**
+	 * check if all arrows point to the right.
+	 *
+	 * @return true if the game was completed, false otherwise
+	 */
+	private boolean checkBoard() {
+		for (NineSwitchesGameSwitch gameSwitch : switches) {
+			if (gameSwitch.getState() == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * resets all switches to point back to the left.
+	 */
+	private void resetBoard() {
+		for (NineSwitchesGameSwitch gameSwitch : switches) {
+			gameSwitch.setState(0);
+		}
+	}
+
+	/**
+	 * sets the name of the active player or <code>null</code> if no game is active.
+	 *
+	 * @param playerName name of player
+	 */
+	public void setPlayerName(String playerName) {
+		this.playerName = playerName;
+		resetBoard();
+	}
+
+	/**
+	 * gets the name of the active player.
+	 *
+	 * @return name of player, or <code>null</code> if no game is active.
+	 */
+	public String getPlayerName() {
+		return this.playerName;
+	}
+
+	/**
+	 * sets the SpeakerNPC who manages this game.
+	 *
+	 * @param npc SpeakerNPC
+	 */
+	public void setNPC(SpeakerNPC npc) {
+		this.npc = npc;
+	}
+}
---
+++ src/games/stendhal/server/entity/mapstuff/game/NineSwitchesGameSwitch.java
@@ -0,0 +1,51 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.entity.mapstuff.game;
+
+import games.stendhal.server.entity.RPEntity;
+import games.stendhal.server.entity.mapstuff.useable.UseableEntity;
+
+/**
+ * A switch in the 9 switches game
+ *
+ * @author hendrik
+ */
+public class NineSwitchesGameSwitch extends UseableEntity{
+	private NineSwitchesGameBoard board;
+
+	/**
+	 * creates a new Switch for the 9 switches game.
+	 * 
+	 * @param board
+	 */
+	public NineSwitchesGameSwitch(NineSwitchesGameBoard board) {
+		this.board = board;
+		put("class", "switch");
+		put("name", "arrow_switch");
+	}
+
+	/**
+	 * someone clicked a switch.
+	 */
+	public boolean onUsed(RPEntity user) {
+		board.usedSwitch(user, this);
+		return true;
+	}
+
+	/**
+	 * toggles the state of the switch.
+	 */
+	public void toggle() {
+		setState((getState() + 1) % 2);
+	}
+}
