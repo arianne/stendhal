3f1f8e20a89d3c98eeff02fb03170f28a8009051
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-09-03 23:22:45 +0000

    sized chatlog to full screen

--- src/games/stendhal/bot/curses/CursesWindow.java
+++ src/games/stendhal/bot/curses/CursesWindow.java
@@ -18,6 +18,7 @@ import java.util.StringTokenizer;
 
 import jcurses.event.ActionEvent;
 import jcurses.event.ActionListener;
+import jcurses.system.Toolkit;
 import jcurses.widgets.Button;
 import jcurses.widgets.GridLayoutManager;
 import jcurses.widgets.List;
@@ -51,11 +52,12 @@ public class CursesWindow extends Window implements ActionListener {
         button = new Button("Send");
         button.addListener(this);
 
-        GridLayoutManager manager = new GridLayoutManager(1, 3);
+        int innerHeight = height - 5;
+        GridLayoutManager manager = new GridLayoutManager(1, innerHeight);
         super.getRootPanel().setLayoutManager(manager);
-        manager.addWidget(chatLog, 0, 0, 1, 1, WidgetsConstants.ALIGNMENT_CENTER, WidgetsConstants.ALIGNMENT_CENTER);
-        manager.addWidget(textField, 0, 1, 1, 1, WidgetsConstants.ALIGNMENT_CENTER, WidgetsConstants.ALIGNMENT_CENTER);
-        manager.addWidget(button, 0, 2, 1, 1, WidgetsConstants.ALIGNMENT_CENTER, WidgetsConstants.ALIGNMENT_CENTER);
+        manager.addWidget(chatLog  , 0,  0, 1, innerHeight - 2, WidgetsConstants.ALIGNMENT_CENTER, WidgetsConstants.ALIGNMENT_CENTER);
+        manager.addWidget(textField, 0, innerHeight - 2, 1, 1, WidgetsConstants.ALIGNMENT_CENTER, WidgetsConstants.ALIGNMENT_CENTER);
+        manager.addWidget(button   , 0, innerHeight - 1, 1, 1, WidgetsConstants.ALIGNMENT_CENTER, WidgetsConstants.ALIGNMENT_CENTER);
         
     }
 
