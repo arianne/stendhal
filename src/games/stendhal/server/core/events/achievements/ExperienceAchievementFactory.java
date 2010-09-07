a8639de48fbfc09c8afaa2396a833a235abdb4fc
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-09-07 21:36:14 +0000

    use full form for internal names of achievements (fixes #3061510)

--- src/games/stendhal/server/core/events/achievements/ExperienceAchievementFactory.java
+++ src/games/stendhal/server/core/events/achievements/ExperienceAchievementFactory.java
@@ -20,21 +20,21 @@ public class ExperienceAchievementFactory extends AchievementFactory {
 	@Override
 	public Collection<Achievement> createAchievements() {
 		List<Achievement> xpAchievements = new LinkedList<Achievement>();
-		xpAchievements.add(createAchievement("xp.lvl.10", "Greenhorn", "Reach level 10", Achievement.EASY_BASE_SCORE,
+		xpAchievements.add(createAchievement("xp.level.10", "Greenhorn", "Reach level 10", Achievement.EASY_BASE_SCORE,
 												new LevelGreaterThanCondition(9)));
-		xpAchievements.add(createAchievement("xp.lvl.50", "Novice", "Reach level 50", Achievement.EASY_BASE_SCORE,
+		xpAchievements.add(createAchievement("xp.level.50", "Novice", "Reach level 50", Achievement.EASY_BASE_SCORE,
 												new LevelGreaterThanCondition(49)));
-		xpAchievements.add(createAchievement("xp.lvl.100", "Apprentice", "Reach level 100", Achievement.EASY_BASE_SCORE, 
+		xpAchievements.add(createAchievement("xp.level.100", "Apprentice", "Reach level 100", Achievement.EASY_BASE_SCORE, 
 												new LevelGreaterThanCondition(99)));
-		xpAchievements.add(createAchievement("xp.lvl.200", "Adventurer", "Reach level 200", Achievement.MEDIUM_BASE_SCORE, 
+		xpAchievements.add(createAchievement("xp.level.200", "Adventurer", "Reach level 200", Achievement.MEDIUM_BASE_SCORE, 
 												new LevelGreaterThanCondition(199)));
-		xpAchievements.add(createAchievement("xp.lvl.300", "Experienced Adventurer", "Reach level 100", Achievement.MEDIUM_BASE_SCORE, 
+		xpAchievements.add(createAchievement("xp.level.300", "Experienced Adventurer", "Reach level 100", Achievement.MEDIUM_BASE_SCORE, 
 												new LevelGreaterThanCondition(299)));
-		xpAchievements.add(createAchievement("xp.lvl.400", "Master Adventurer", "Reach level 400", Achievement.MEDIUM_BASE_SCORE, 
+		xpAchievements.add(createAchievement("xp.level.400", "Master Adventurer", "Reach level 400", Achievement.MEDIUM_BASE_SCORE, 
 												new LevelGreaterThanCondition(399)));
-		xpAchievements.add(createAchievement("xp.lvl.500", "Stendhal Master", "Reach level 500", Achievement.HARD_BASE_SCORE, 
+		xpAchievements.add(createAchievement("xp.level.500", "Stendhal Master", "Reach level 500", Achievement.HARD_BASE_SCORE, 
 												new LevelGreaterThanCondition(499)));
-		xpAchievements.add(createAchievement("xp.lvl.597", "Stendhal High Master", "Reach level 597", Achievement.HARD_BASE_SCORE, 
+		xpAchievements.add(createAchievement("xp.level.597", "Stendhal High Master", "Reach level 597", Achievement.HARD_BASE_SCORE, 
 												new LevelGreaterThanCondition(596)));
 		return xpAchievements;
 	}
