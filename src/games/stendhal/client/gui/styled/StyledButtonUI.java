e83a60a78945e8b0c8ccda88f1ecb5dea83dcb7b
Kimmo Rundelin <kiheru@users.sourceforge.net>
2010-09-09 10:12:34 +0000

    Avoid using JComponent.getMousePosition, which is for some reason extremely expensive

--- src/games/stendhal/client/gui/styled/StyledButtonUI.java
+++ src/games/stendhal/client/gui/styled/StyledButtonUI.java
@@ -4,6 +4,8 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Insets;
 import java.awt.Rectangle;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.AbstractButton;
 import javax.swing.JComponent;
@@ -14,18 +16,13 @@ import javax.swing.plaf.basic.BasicButtonUI;
  * ButtonUI implementation for drawing {@link WoodStyle} style buttons. 
  */
 public class StyledButtonUI extends BasicButtonUI {
-	private static StyledButtonUI instance;
-	
 	private final Style style;
+	/** <code>true</code> if the mouse is over the button */
+	private boolean mouseOver;
 	
-	// Required by UIManager, not necessarily called from EDT
-	public static synchronized ComponentUI createUI(JComponent button) {
-		// Button UIs can be shared
-		if (instance == null) {
-			instance = new StyledButtonUI(StyleUtil.getStyle());
-		}
-		
-		return instance;
+	// Required by UIManager
+	public static ComponentUI createUI(JComponent button) {
+		return new StyledButtonUI(StyleUtil.getStyle());
 	}
 	
 	/**
@@ -41,6 +38,7 @@ public class StyledButtonUI extends BasicButtonUI {
 	public void paint(Graphics graphics, JComponent button) {
 		AbstractButton btn = (AbstractButton) button;
 		paintBackground(graphics, button);
+		
 		// Restore normal look after pressing ends, if needed
 		if (!btn.getModel().isPressed()) {
 			// Try to avoid switching borders if the buttom has none or custom 
@@ -49,10 +47,11 @@ public class StyledButtonUI extends BasicButtonUI {
 				button.setBorder(style.getBorder());
 			}
 		}
-		if (button.getMousePosition() != null) {
+
+		if (mouseOver) {
 			hilite(graphics, button);
 		}
-
+		
 		super.paint(graphics, button);
 	}
 	
@@ -130,5 +129,24 @@ public class StyledButtonUI extends BasicButtonUI {
 		super.installUI(button);
 		button.setForeground(style.getForeground());
 		button.setBorder(style.getBorder());
+		button.addMouseListener(new MouseOverListener());
+	}
+	
+	/**
+	 * Follow mouse enter and leave events to the button. Highlighting
+	 * is implemented this way instead of sharing the same ButtonUI for all
+	 * buttons because button.getMousePosition() is for some reason extremely
+	 * expensive. (Used 20 times the time used to actually draw the button).
+	 */
+	private class MouseOverListener extends MouseAdapter {
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			mouseOver = true;
+		}
+		
+		@Override
+		public void mouseExited(MouseEvent e) {
+			mouseOver = false;
+		}
 	}
 }
