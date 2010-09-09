bcccab911c722de54c223bd20a30c78b7ce12a1b
Kimmo Rundelin <kiheru@users.sourceforge.net>
2010-09-09 19:23:04 +0000

    Use InternalManagedWindow for the image views. Fixes [2917794] "[Stendhal] Moving Examine and Item List window cause flicker" and [3048038] "[Stendhal client] cvs; shopsign scrollbars broken"

--- src/games/stendhal/client/gui/imageviewer/ImageViewWindow.java
+++ src/games/stendhal/client/gui/imageviewer/ImageViewWindow.java
@@ -1,56 +1,51 @@
 package games.stendhal.client.gui.imageviewer;
 
 import games.stendhal.client.stendhal;
+import games.stendhal.client.gui.InternalManagedWindow;
 import games.stendhal.client.gui.j2DClient;
-import games.stendhal.client.gui.wt.InternalManagedDialog;
 
 import java.awt.Dimension;
-import java.awt.Point;
+
+import javax.swing.SwingUtilities;
 
 /**
  * Opens an image resource at a given URL, and displays it in the client.
  * 
  * @author timothyb89
  */
-public class ImageViewWindow extends InternalManagedDialog {
-
+public class ImageViewWindow extends InternalManagedWindow {
 	/**
 	 * The padding of the window, in pixels, when generating the maximum size.
 	 */
-	public static final int PADDING = 100;
-
-	private ViewPanel viewPanel;
+	private static final int PADDING = 100;
 
 	public ImageViewWindow(String title, ViewPanel viewPanel) {
 		super("examine", title);
-		this.viewPanel = viewPanel;
-		init();
-	}
-
-	public void init() {
+		
 		viewPanel.prepareView(genMaxSize());
 		setContent(viewPanel);
-		j2DClient.get().addWindow(this);
-		final Point center = genCenterPoint();
-		this.moveTo(center.x, center.y);
-		view();
+		
+		/*
+		 * Generating the window was likely triggered an event from the network
+		 * thread; do the rest of the work in EDT.
+		 */
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				j2DClient.get().addWindow(ImageViewWindow.this);
+				center();
+				getParent().validate();
+			}
+		});
 	}
 
-	public void view() {
-		setVisible(true);
-	}
-
-	public Dimension genMaxSize() {
+	/**
+	 * Calculate maximum size for a window that fits on the game screen.
+	 * 
+	 * @return size
+	 */
+	private Dimension genMaxSize() {
 		final int width = (int) (stendhal.screenSize.getWidth() - PADDING);
 		final int height = (int) (stendhal.screenSize.getHeight() - PADDING);
 		return new Dimension(width, height);
 	}
-
-	public Point genCenterPoint() {
-		final int x = (int) ((stendhal.screenSize.getWidth()  - this.getDialog().getWidth()) / 2);
-		final int y = (int) ((stendhal.screenSize.getHeight() - this.getDialog().getHeight()) / 2);
-
-		return new Point(x, y);
-	}
-
 }
--- src/games/stendhal/client/gui/imageviewer/ViewPanel.java
+++ src/games/stendhal/client/gui/imageviewer/ViewPanel.java
@@ -1,15 +1,16 @@
 package games.stendhal.client.gui.imageviewer;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 
-import javax.swing.JPanel;
+import javax.swing.JComponent;
 
 /**
  * an abstract base class for all ViewPanels
  *
  * @author hendrik
  */
-public abstract class ViewPanel extends JPanel {
+public abstract class ViewPanel extends JComponent {
 
 	private static final long serialVersionUID = 7442185832293104642L;
 
@@ -17,6 +18,8 @@ public abstract class ViewPanel extends JPanel {
 	 * creates a new ViewPanel
 	 */
 	public ViewPanel() {
+		setLayout(new BorderLayout());
+		setOpaque(false);
 	}
 
 	/**
@@ -27,5 +30,4 @@ public abstract class ViewPanel extends JPanel {
 	public void prepareView(Dimension maxSize) {
 		// do nothing
 	}
-
 }
