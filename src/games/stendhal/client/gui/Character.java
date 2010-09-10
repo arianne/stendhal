38f63d423398e05de6998702d0ef142fe3ca8f19
Kimmo Rundelin <kiheru@users.sourceforge.net>
2010-09-10 18:33:06 +0000

    Added swing implementation of the character window

---
+++ src/games/stendhal/client/gui/Character.java
@@ -0,0 +1,160 @@
+package games.stendhal.client.gui;
+
+import games.stendhal.client.entity.EntityChangeListener;
+import games.stendhal.client.entity.IEntity;
+import games.stendhal.client.entity.User;
+import games.stendhal.client.entity.factory.EntityFactory;
+import games.stendhal.client.gui.layout.SBoxLayout;
+import games.stendhal.client.sprite.SpriteStore;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.swing.Box;
+import javax.swing.JComponent;
+
+import marauroa.common.game.RPObject;
+import marauroa.common.game.RPSlot;
+
+/**
+ * Window for showing the equipment the player is wearing.
+ */
+public class Character extends InternalManagedWindow implements EntityChangeListener {
+	/** Padding between the ItemPanels */
+	private static final int PADDING = 1;
+	/** The pixel amount the hand slots should be below the armor slot */
+	private static final int HAND_YSHIFT = 10;
+	
+	/** ItemPanels searchable by the respective slot name */
+	private final Map<String, ItemPanel> slotPanels = new HashMap<String, ItemPanel>();
+	private User player;
+	
+	/**
+	 * Create a new character window.
+	 */
+	public Character() {
+		super("character", "Character");
+		createLayout();
+		// Don't allow the user close this. There's no way to get it back. 
+		setCloseable(false);
+	}
+	
+	/**
+	 * sets the player entity.
+	 * 
+	 * @param userEntity
+	 */
+	public void setPlayer(final User userEntity) {
+		player = userEntity;
+		userEntity.addChangeListener(this);
+		refreshContents();
+	}
+	
+	/**
+	 * Create the content layout and add the ItemPanels.
+	 */
+	private void createLayout() {
+		// Layout containers
+		JComponent row = SBoxLayout.createContainer(SBoxLayout.HORIZONTAL, PADDING);
+		JComponent left = SBoxLayout.createContainer(SBoxLayout.VERTICAL, PADDING);
+		JComponent middle = SBoxLayout.createContainer(SBoxLayout.VERTICAL, PADDING);
+		JComponent right = SBoxLayout.createContainer(SBoxLayout.VERTICAL, PADDING);
+		left.setAlignmentY(CENTER_ALIGNMENT);
+		right.setAlignmentY(CENTER_ALIGNMENT);
+		row.add(left);
+		row.add(middle);
+		row.add(right);
+
+		/*
+		 * Fill the left column
+		 *
+		 * Add filler to shift the hand slots down. Shift * 2 because centering
+		 * the column uses the other half at the bottom.
+		 */
+		left.add(Box.createVerticalStrut(HAND_YSHIFT * 2)); 
+		SpriteStore store = SpriteStore.get();
+		ItemPanel panel = new ItemPanel("rhand", store.getSprite("data/gui/weapon-slot.png"));
+		slotPanels.put("rhand", panel);
+		left.add(panel);
+		
+		panel = new ItemPanel("finger", store.getSprite("data/gui/ring-slot.png"));
+		slotPanels.put("finger", panel);
+		left.add(panel);
+		
+		// Fill the middle column
+		panel = new ItemPanel("head", store.getSprite("data/gui/helmet-slot.png"));
+		slotPanels.put("head", panel);
+		middle.add(panel);
+		
+		panel = new ItemPanel("armor", store.getSprite("data/gui/armor-slot.png"));
+		slotPanels.put("armor", panel);
+		middle.add(panel);
+		
+		panel = new ItemPanel("legs", store.getSprite("data/gui/legs-slot.png"));
+		slotPanels.put("legs", panel);
+		middle.add(panel);
+		
+		panel = new ItemPanel("feet", store.getSprite("data/gui/boots-slot.png"));
+		slotPanels.put("feet", panel);
+		middle.add(panel);
+	
+		/*
+		 *  Fill the right column
+		 *
+		 * Add filler to shift the hand slots down. Shift * 2 because centering
+		 * the column uses the other half at the bottom.
+		 */
+		right.add(Box.createVerticalStrut(HAND_YSHIFT * 2));
+		panel = new ItemPanel("lhand", store.getSprite("data/gui/shield-slot.png"));
+		slotPanels.put("lhand", panel);
+		right.add(panel);
+		
+		panel = new ItemPanel("cloak", store.getSprite("data/gui/cloak-slot.png"));
+		slotPanels.put("cloak", panel);
+		right.add(panel);
+		
+		setContent(row);
+	}
+	
+	/**
+	 * Updates the player slot panels.
+	 */
+	private void refreshContents() {
+		// traverse all displayed slots
+		for (final Entry<String, ItemPanel> entry : slotPanels.entrySet()) {
+			final RPSlot slot = player.getSlot(entry.getKey());
+
+			if (slot == null) {
+				continue;
+			}
+
+			final ItemPanel entitySlot = entry.getValue();
+
+			if (entitySlot != null) {
+				entitySlot.setParent(player);
+
+				final Iterator<RPObject> iter = slot.iterator();
+
+				if (iter.hasNext()) {
+					final RPObject object = iter.next();
+
+					IEntity entity = EntityFactory.createEntity(object);
+
+					entitySlot.setEntity(entity);
+				} else {
+					entitySlot.setEntity(null);
+				}
+			}
+		}
+
+		setTitle(player.getName());
+	}
+
+	public void entityChanged(IEntity entity, Object property) {
+		if (property == IEntity.PROP_CONTENT) {
+			refreshContents();
+		}
+	}
+}
