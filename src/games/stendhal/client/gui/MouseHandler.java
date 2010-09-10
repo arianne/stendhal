ec35981c827266133a859bbee59c1c9f92da55a8
Kimmo Rundelin <kiheru@users.sourceforge.net>
2010-09-10 18:28:56 +0000

    Added an abstract class for GameScreen style mouse handling

---
+++ src/games/stendhal/client/gui/MouseHandler.java
@@ -0,0 +1,140 @@
+package games.stendhal.client.gui;
+
+import java.awt.Point;
+import java.awt.event.InputEvent;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.util.Locale;
+
+/**
+ * Sane mouse handling for components that want to implement drag and drop or
+ * pass mouse events to EntityViews.
+ */
+abstract class MouseHandler implements MouseListener, MouseMotionListener {
+	/** <code>true</code> if the right mouse button is down */
+	private boolean rightMouseButtonPressed;
+	/** The time at which a mouse button was last pressed. */
+	private long timeOfLastMousePress = System.currentTimeMillis();
+	/** <code>true</code> if dragging is in progress */
+	private boolean dragging;
+	/**
+	 * <code>true</code> if the last single click was handled; it can't be a 
+	 * part of a double click.
+	 */
+	private boolean lastClickWasHandled;
+	
+	/**
+	 * Called on left mouse single click.
+	 * 
+	 * @param point location
+	 * @return <code>true</code> if the click was handled
+	 */
+	protected abstract boolean onMouseClick(Point point);
+	
+	/**
+	 * Called on left double click.
+	 * 
+	 * @param point location
+	 * @return <code>true</code> if the click was handled
+	 */
+	protected abstract boolean onMouseDoubleClick(Point point);
+	
+	/**
+	 * Called on right mouse single click.
+	 * 
+	 * @param point location
+	 */
+	protected abstract void onMouseRightClick(Point point);
+	
+	/**
+	 * Called when mouse dragging starts.
+	 * 
+	 * @param point location
+	 */
+	protected abstract void onDragStart(Point point);
+	
+	/**
+	 * Java's official mouseClick handler; we don't use this because it doesn't
+	 * register clicks while the mouse is moving at all.
+	 * 
+	 * @param e
+	 */
+	public void mouseClicked(MouseEvent e) {
+		// do nothing
+	}
+
+	public void mouseEntered(MouseEvent e) {
+		// do nothing
+	}
+
+	public void mouseExited(MouseEvent e) {
+		// do nothing
+	}
+
+	public void mousePressed(MouseEvent e) {
+		if (e.getButton() == MouseEvent.BUTTON3) {
+			rightMouseButtonPressed = true;
+		}
+
+		timeOfLastMousePress = System.currentTimeMillis();
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		// be sure to stop dragging operations when the left button is released
+		if (e.getButton() == MouseEvent.BUTTON1) {
+			dragging = false;
+		}
+
+		if (e.getButton() == MouseEvent.BUTTON3) {
+			rightMouseButtonPressed = false;
+		}
+
+		// Handle as a click if the button wasn't held for > a second
+		if ((System.currentTimeMillis() - timeOfLastMousePress) < 1000) {
+			onMouseClick(e);
+			timeOfLastMousePress = 0;
+		}
+	}
+
+	public void mouseDragged(MouseEvent e) {
+		if (rightMouseButtonPressed) {
+			// Disallow dragging with right button.
+			return;
+		}
+
+		if (!dragging) {
+			onDragStart(e.getPoint());
+			dragging = true;
+		}
+	}
+
+	public void mouseMoved(MouseEvent arg0) {
+		// do nothing
+	}
+
+	private void onMouseClick(final MouseEvent e) {
+		final Point p = e.getPoint();
+
+		// Added support for ctrl + click for Mac OS X intensifly@gmx.com
+
+		final int onmask = InputEvent.CTRL_DOWN_MASK;
+		if (System.getProperty("os.name").toLowerCase(Locale.ENGLISH).contains("os x")
+				&& ((e.getModifiersEx() & onmask) == onmask)) {
+			onMouseRightClick(p);
+		} else if (e.getButton() == MouseEvent.BUTTON1) {
+			if (e.getClickCount() == 1) {
+				lastClickWasHandled = onMouseClick(p);
+			} else if (e.getClickCount() >= 2) {
+				if (lastClickWasHandled) {
+					lastClickWasHandled = onMouseClick(p);
+				} else {
+					lastClickWasHandled = onMouseDoubleClick(p);
+				}
+			}
+		} else if (e.getButton() == MouseEvent.BUTTON3) {
+			// no double rightclick supported
+			onMouseRightClick(p);
+		}
+	}
+}
