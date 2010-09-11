9d527019701f77ccf58099608a98907a9ea2b8e9
Kimmo Rundelin <kiheru@users.sourceforge.net>
2010-09-11 19:05:46 +0000

    Use SlotGrid to implement SlotWindow

--- src/games/stendhal/client/gui/SlotWindow.java
+++ src/games/stendhal/client/gui/SlotWindow.java
@@ -1,44 +1,22 @@
 package games.stendhal.client.gui;
 
-import games.stendhal.client.entity.EntityChangeListener;
 import games.stendhal.client.entity.IEntity;
 import games.stendhal.client.entity.User;
-import games.stendhal.client.entity.factory.EntityFactory;
 
 import java.awt.Graphics;
-import java.awt.GridLayout;
 import java.awt.geom.Rectangle2D;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
-import javax.swing.JComponent;
-
-import marauroa.common.game.RPObject;
-import marauroa.common.game.RPSlot;
-
-import org.apache.log4j.Logger;
 
 /**
  * A window for showing contents of an entity's slot in a grid of ItemPanels
  */
-public class SlotWindow extends InternalManagedWindow implements EntityChangeListener {
-	/** Padding between the ItemPanels */
-	private static final int PADDING = 1;
+public class SlotWindow extends InternalManagedWindow {
 	/**
 	 * when the player is this far away from the container, the panel is closed.
 	 */
 	private static final int MAX_DISTANCE = 4;
-	private static final Logger logger = Logger.getLogger(SlotWindow.class);
 	
-	/** All shown item panels */
-	private final List<ItemPanel> panels;
-	/** The parent entity of the shown slot */
+	private final SlotGrid content;
 	private IEntity parent;
-	/** Name of the shown slot */
-	private String slotName;
-	/** A slot containing the shown entities */
-	private RPSlot shownSlot;
 	
 	/**
 	 * Create a new EntityContainer.
@@ -50,15 +28,7 @@ public class SlotWindow extends InternalManagedWindow implements EntityChangeLis
 	public SlotWindow(String title, int width, int height) {
 		super(title, title);
 		
-		JComponent content = new JComponent() {};
-		content.setLayout(new GridLayout(height, width, PADDING, PADDING));
-		panels = new ArrayList<ItemPanel>();
-		
-		for (int i = 0; i < width * height; i++) {
-			ItemPanel panel = new ItemPanel(null, null);
-			panels.add(panel);
-			content.add(panel);
-		}
+		content = new SlotGrid(width, height);
 		setContent(content);
 	}
 	
@@ -70,81 +40,13 @@ public class SlotWindow extends InternalManagedWindow implements EntityChangeLis
 	 */
 	public void setSlot(final IEntity parent, final String slot) {
 		this.parent = parent;
-		this.slotName = slot;
-
-		/*
-		 * Reset the container info for all holders
-		 */
-		for (final ItemPanel panel : panels) {
-			panel.setParent(parent);
-			panel.setName(slot);
-		}
-
-		parent.addChangeListener(this);
-		shownSlot = null;
-		rescanSlotContent();
+		content.setSlot(parent, slot);
 	}
 	
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		checkDistance();
-	}
-	
-	/**
-	 * Rescans the content of the slot.
-	 * 
-	 * @param gameScreen
-	 */
-	private void rescanSlotContent() {
-		if ((parent == null) || (slotName == null)) {
-			return;
-		}
-
-		final RPSlot rpslot = parent.getSlot(slotName);
-		
-		// Skip if not changed
-		if ((shownSlot != null) && shownSlot.equals(rpslot)) {
-			return;
-		}
-
-		final Iterator<ItemPanel> iter = panels.iterator();
-
-		/*
-		 * Fill from contents
-		 */
-		if (rpslot != null) {
-			RPSlot newSlot = (RPSlot) rpslot.clone();
-
-			for (final RPObject object : newSlot) {
-				if (!iter.hasNext()) {
-					logger.error("More objects than slots: " + slotName);
-					break;
-				}
-
-				IEntity entity = EntityFactory.createEntity(object);
-
-				if (entity == null) {
-					logger.warn("Unable to find entity for: " + object,
-							new Throwable("here"));
-					entity = EntityFactory.createEntity(object);
-				}
-
-				iter.next().setEntity(entity);
-			}
-			
-			shownSlot = newSlot;
-		} else {
-			shownSlot = null;
-			logger.error("No slot found: " + slotName);
-		}
-
-		/*
-		 * Clear remaining holders
-		 */
-		while (iter.hasNext()) {
-			iter.next().setEntity(null);
-		}
 	}
 	
 	/**
@@ -187,25 +89,13 @@ public class SlotWindow extends InternalManagedWindow implements EntityChangeLis
 		final int px = (int) x;
 		final int py = (int) y;
 
-		final int ix = (int) parent.getX();
-		final int iy = (int) parent.getY();
-
 		final Rectangle2D orig = parent.getArea();
 		orig.setRect(orig.getX() - MAX_DISTANCE, orig.getY() - MAX_DISTANCE,
 				orig.getWidth() + MAX_DISTANCE * 2, orig.getHeight()
 						+ MAX_DISTANCE * 2);
 
 		if (!orig.contains(px, py)) {
-			logger.debug("Closing " + slotName + " container because " + px
-					+ "," + py + " is too far from (" + ix + "," + iy + "):"
-					+ orig);
 			close();
-		}
-	}
-	
-	public void entityChanged(IEntity entity, Object property) {
-		if (property == IEntity.PROP_CONTENT) {
-			rescanSlotContent();
 		}
 	}
 }
