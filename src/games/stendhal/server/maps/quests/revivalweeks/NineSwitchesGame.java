65c14db46dd877fe11596e7869be0134a6f09574
Katie Russell <kymara@users.sourceforge.net>
2010-09-15 19:56:28 +0000

    cleaned up add methods used and corrected some text

--- src/games/stendhal/server/maps/quests/revivalweeks/NineSwitchesGame.java
+++ src/games/stendhal/server/maps/quests/revivalweeks/NineSwitchesGame.java
@@ -5,7 +5,6 @@ import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.entity.mapstuff.game.NineSwitchesGameBoard;
 import games.stendhal.server.entity.npc.ChatAction;
-import games.stendhal.server.entity.npc.ConversationPhrases;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.EventRaiser;
 import games.stendhal.server.entity.npc.SpeakerNPC;
@@ -15,7 +14,7 @@ import games.stendhal.server.entity.player.Player;
 import java.util.Arrays;
 
 /**
- * A Tic Tac Toe game for two players
+ * A Game about Nine switches game for one player
  *
  * @author hendrik
  */
@@ -38,31 +37,16 @@ public class NineSwitchesGame {
 
 			@Override
 			protected void createDialog() {
-				add(ConversationStates.IDLE, 
-						ConversationPhrases.GREETING_MESSAGES, 
-						ConversationStates.IDLE,
-						"Hi, welcome to our small game of nine switches. Your task is to let all arrows point to the right."
-						+ "Easy? Well, there is a #catch.",
-						null);
-				add(ConversationStates.IDLE, 
-						Arrays.asList("catch"), 
-						ConversationStates.IDLE,
-						"Each switch is linked to it neighbour and will toggle them as well. You have one minutes to solve the puzzle."
-						+ "Do you want to #play?.",
-						null);
-				add(ConversationStates.IDLE, 
-						ConversationPhrases.JOB_MESSAGES, 
-						ConversationStates.IDLE,
-						"I am the supervisor of this game.",
-						null);
-				add(ConversationStates.IDLE, 
-						ConversationPhrases.GOODBYE_MESSAGES, 
-						ConversationStates.IDLE,
-						"It was nice to meet you.",
-						null);
-				add(ConversationStates.IDLE,
+				addGreeting("Hi, welcome to our small game of nine switches. Your task is to make all arrows point to the right."
+						+ "Easy? Well, there is a #catch.");
+				addReply("catch", 
+						"Each switch is linked to its neighbour and will change them as well. You have one minute to solve the puzzle."
+						+ "Do you want to #play?.");
+				addJob("I am the supervisor of this game.");
+				addGoodbye("It was nice to meet you.");
+				add(ConversationStates.ATTENDING,
 						Arrays.asList("play", "play?", "game", "yes"),
-						ConversationStates.IDLE,
+						ConversationStates.ATTENDING,
 						"Good luck.",
 						new PlayAction(board));
 			}
