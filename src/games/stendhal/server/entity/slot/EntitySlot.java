a157cb95fa8f89d86135f3e6019bb35ad85356a4
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-09-18 00:51:37 +0000

    add the possibility for slots to store an error message

--- src/games/stendhal/server/entity/slot/EntitySlot.java
+++ src/games/stendhal/server/entity/slot/EntitySlot.java
@@ -9,6 +9,7 @@ import marauroa.common.game.RPSlot;
  * @author hendrik
  */
 public class EntitySlot extends RPSlot implements Slot {
+	private String errorMessage;
 
 	/**
 	 * Creates an uninitialized EntitySlot.
@@ -29,6 +30,7 @@ public class EntitySlot extends RPSlot implements Slot {
 	}
 
 	public boolean isReachableForTakingThingsOutOfBy(final Entity entity) {
+		setErrorMessage("The " + getName() + " of " + ((Entity) getOwner()).getDescriptionName(true) + " is too far away.");
 		return false;
 	}
 
@@ -39,12 +41,37 @@ public class EntitySlot extends RPSlot implements Slot {
 	public boolean isItemSlot() {
 		return true;
 	}
-	
+
 	public RPSlot getWriteableSlot() {
 		return this;
 	}
 
 	public boolean isTargetBoundCheckRequired() {
 		return false;
+	}
+
+	/**
+	 * sets the error message
+	 *
+	 * @param errorMessage error message to set
+	 */
+	protected void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+
+	/**
+	 * gets the last error message
+	 *
+	 * @return error message or <code>null</code>
+	 */
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	/**
+	 * clears the last error message
+	 */
+	public void clearErrorMessage() {
+		errorMessage = null;
 	}
 }
