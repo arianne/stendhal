0e6d1c2d59995b9c4f68c34dd72347ef5a88d55b
Bluelads4 <bluelads99@users.sourceforge.net>
2010-09-19 17:06:56 +0000

    Added new NPC Haunchy Meatock to Ados Market

---
+++ src/games/stendhal/server/maps/ados/city/BBQGrillmasterNPC.java
@@ -0,0 +1,81 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.maps.ados.city;
+
+import games.stendhal.common.Direction;
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.pathfinder.FixedPath;
+import games.stendhal.server.core.pathfinder.Node;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Builds a npc in Ados (name:Haunchy Meatoch) who is a grillmaster on the market
+ * 
+ * @author storyteller (idea) and Vanessa Julius (implemented)
+ *
+ */
+public class BBQGrillmasterNPC implements ZoneConfigurator {
+
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		buildNPC(zone);
+	}
+
+	private void buildNPC(final StendhalRPZone zone) {
+		final SpeakerNPC npc = new SpeakerNPC("Haunchy Meatoch") {
+		    
+			@Override
+			protected void createPath() {
+				final List<Node> nodes = new LinkedList<Node>();
+				nodes.add(new Node(14, 25));
+				nodes.add(new Node(15, 25));
+                nodes.add(new Node(12, 25));
+                nodes.add(new Node(16, 25));  
+                nodes.add(new Node(16, 24));
+                nodes.add(new Node(16, 25)); 
+                nodes.add(new Node(15, 25)); 
+                nodes.add(new Node(12, 25));
+                nodes.add(new Node(17, 25));
+                nodes.add(new Node(17, 24));
+                nodes.add(new Node(17, 25));
+                nodes.add(new Node(13, 25));
+                nodes.add(new Node(14, 25));
+               	setPath(new FixedPath(nodes, true));
+
+			}
+
+			@Override
+			protected void createDialog() {
+				addGreeting("Hey! Isn't it a nice day for having a BBQ?");
+				addHelp("Unfortunetly the steaks aren't ready yet... If you are hungry and can't wait, you could check some offers in the near out like the Blacksheep offers near the fisherhuts in Ados or you can take a ferry to Athor for getting some nice snacks...");
+				
+				addQuest("I can't think about jobs for you at the moment, I have to take care of the steaks."); 
+				addJob("I am a grillmaster as you can prolly see. I love the smell of fresh grilled meat!");
+				addOffer("I hope that my steaks will be ready soon. Please be a bit patient or have some other snacks first.");
+				addGoodbye("A nice day to you! Always keep your fire burning!");
+				
+			}
+		};
+
+		npc.setDescription("You see Haunchy Meatoch. He is surrounded by a nice smell of fresh grilled meat.");
+		npc.setEntityClass("bbqgrillmasternpc");
+		npc.setPosition(14, 25);
+		npc.setDirection(Direction.RIGHT);
+		npc.initHP(100);
+		zone.add(npc);
+	}
+}
