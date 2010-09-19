66e783f57bcefc27ed8d06b5149290e1f46e589f
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-09-19 02:41:45 +0000

    added licence template

--- tests/games/stendhal/client/actions/AlterActionTest.java
+++ tests/games/stendhal/client/actions/AlterActionTest.java
@@ -1,3 +1,15 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package games.stendhal.client.actions;
 
 import static org.hamcrest.CoreMatchers.is;
--- tests/games/stendhal/server/entity/npc/parser/ExpressionMatcherTest.java
+++ tests/games/stendhal/server/entity/npc/parser/ExpressionMatcherTest.java
@@ -1,3 +1,15 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package games.stendhal.server.entity.npc.parser;
 
 import static org.junit.Assert.assertEquals;
