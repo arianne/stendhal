f6ee9ce2dcc8e7f4b948dfedd794ec5dce4b8f51
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-09-19 01:14:30 +0000

    added licence block

--- src/games/stendhal/bot/postman/Postman.java
+++ src/games/stendhal/bot/postman/Postman.java
@@ -1,6 +1,18 @@
 /* CC-BY Hendrik Brummermann <nhb_web@nexgo.de>
  * (But becomes GPL because it is required to link against GPL code).
  */
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package games.stendhal.bot.postman;
 
 import java.util.Date;
