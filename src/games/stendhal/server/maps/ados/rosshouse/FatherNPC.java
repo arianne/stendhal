9d5c6dbc5246bfcaea266cfa645fe29edb21b0b0
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-09-21 21:13:15 +0000

    code format

--- src/games/stendhal/server/maps/ados/rosshouse/FatherNPC.java
+++ src/games/stendhal/server/maps/ados/rosshouse/FatherNPC.java
@@ -52,12 +52,12 @@ public class FatherNPC implements ZoneConfigurator {
 
 			@Override
 			protected void createDialog() {
-			    addGreeting("Hi there.");
-			    addJob("I'm looking after my daughter Susi.");
-			    addHelp("If you need help finding any buildings in Ados, the guard Julius will give you a map. He is by the city entrance.");
-			    addOffer("Sorry I do not have anything to offer you, but there are two places to eat in Ados - the tavern and a bar.");
-			    addQuest("At the end of October we will be visiting the #Semos #Mine #Town #Revival #Weeks");
-			    addGoodbye("Bye, nice to meet you.");
+				addGreeting("Hi there.");
+				addJob("I'm looking after my daughter Susi.");
+				addHelp("If you need help finding any buildings in Ados, the guard Julius will give you a map. He is by the city entrance.");
+				addOffer("Sorry I do not have anything to offer you, but there are two places to eat in Ados - the tavern and a bar.");
+				addQuest("At the end of October we will be visiting the #Semos #Mine #Town #Revival #Weeks");
+				addGoodbye("Bye, nice to meet you.");
 
 				// Revival Weeks
 				add(
