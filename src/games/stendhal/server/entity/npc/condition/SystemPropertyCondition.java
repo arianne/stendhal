3827e6f647f4bb97a735e4355c4484aa20b28a64
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-09-21 16:09:37 +0000

    added a new condition which checks system properties

---
+++ src/games/stendhal/server/entity/npc/condition/SystemPropertyCondition.java
@@ -0,0 +1,72 @@
+package games.stendhal.server.entity.npc.condition;
+
+import games.stendhal.server.entity.Entity;
+import games.stendhal.server.entity.npc.ChatCondition;
+import games.stendhal.server.entity.npc.parser.Sentence;
+import games.stendhal.server.entity.player.Player;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+/**
+ * Is the specified system property set?
+ */
+public class SystemPropertyCondition implements ChatCondition {
+
+	private final String key;
+	private final String value;
+
+	/**
+	 * Creates a new SystemPropertyCondition
+	 *
+	 * @param key key to check
+	 */
+	public SystemPropertyCondition(String key) {
+		this.key = key;
+		this.value = null;
+	}
+
+	/**
+	 * Creates a new SystemPropertyCondition
+	 *
+	 * @param key key to check
+	 * @param value value the key has to have.
+	 */
+	public SystemPropertyCondition(String key, String value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public boolean fire(final Player player, final Sentence sentence, final Entity entity) {
+		if (value == null) {
+			return System.getProperty(key) != null;
+		} else {
+			return value.equals(System.getProperty(key));
+		}
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("system <");
+		sb.append(key);
+		if (value != null) {
+			sb.append("=");
+			sb.append(value);
+		}
+		sb.append(">");
+		return sb.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj, false,
+				SystemPropertyCondition.class);
+	}
+
+}
