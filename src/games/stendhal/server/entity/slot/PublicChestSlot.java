c75a4fae35928e6902635f98466e246cc49064a5
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-09-28 20:33:00 +0000

    creates a new class PublicChestSlot which will not accept bound items

---
+++ src/games/stendhal/server/entity/slot/PublicChestSlot.java
@@ -0,0 +1,37 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.entity.slot;
+
+import games.stendhal.server.entity.mapstuff.chest.Chest;
+
+/**
+ * A slot of a chest which is only accessible, if the chest is open.
+ * 
+ * @author hendrik
+ */
+public class PublicChestSlot extends ChestSlot {
+
+	/**
+	 * creates a new PublicChestSlot
+	 *
+	 * @param owner the chest owning this slot
+	 */
+	public PublicChestSlot(Chest owner) {
+		super(owner);
+	}
+
+	@Override
+	public boolean isTargetBoundCheckRequired() {
+		return true;
+	}
+}
