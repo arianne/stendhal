83ffb4f5196cfc33f07a25dc00b1f055a774bf1b
Katie Russell <kymara@users.sourceforge.net>
2010-10-12 06:48:08 +0000

    correcting some texts

--- src/games/stendhal/server/maps/ados/market/OldFishermanNPC.java
+++ src/games/stendhal/server/maps/ados/market/OldFishermanNPC.java
@@ -64,13 +64,13 @@ public class OldFishermanNPC implements ZoneConfigurator {
 			protected void createDialog() {
 				addGreeting("Ahoy!");
 				addHelp("Harr my friend, my #whole #life long I was sitting in a boat, trying to catch some fish...");
-				addReply("whole life","The feeling you have when fishies hang on your rod is awesome! Did you ever try to catch some by #yourself?");
-				addReply("yourself", "You can ask my friend Santiago for help, he lives in a fisherhut somewhere in the city!");
+				addReply("whole life","The feeling you have when fish hang on your rod is awesome! Did you ever try to catch some by #yourself?");
+				addReply("yourself", "You can ask my friend Santiago for help, he lives in a hut somewhere in the city!");
 				addQuest("Ahoy Matey! I don't have any tasks for you but others do!"); 
-				addJob("I am a fisherman. For many, many years I went fishing every day. There have been dangerous storms I had to get over and some reeeeeally big fish which almost drown my ship, but of course I always was able to #catch them!");
+				addJob("I am a fisherman. For many, many years I went fishing every day. There have been dangerous storms I had to get through and some reeeeeally big fish which almost drown my ship, but of course I always was able to #catch them!");
 				addReply("catch","Hehe! I hope that I will be able to deliver some fish to the market one time...");
-				addOffer("My fishing places on the sea are catched by some ugly #storm I can't drive out at the moment.");
-				addReply("storm", "The huge storm halfway destroyed my boat! Sorry matey, I can't offer you anything at the moment...");
+				addOffer("My fishing places on the sea are caught in some ugly #storm so I can't venture out at the moment.");
+				addReply("storm", "The huge storm nearly destroyed my boat! Sorry matey, I can't offer you anything at the moment...");
 				addGoodbye("See you, matey! And be careful there on this slippy ground!");
 				
 			}
