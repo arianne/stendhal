3646402304ba364d90cd54ef43f087ffa1abde1b
 omero <nylon0700@users.sourceforge.net>
2010-10-14 11:47:26 +0000

    added cherry pie producer NPC

---
+++ src/games/stendhal/server/maps/kalavan/cottage_2/ConfectionerCherryPieNPC.java
@@ -0,0 +1,100 @@
+package games.stendhal.server.maps.kalavan.cottage_2;
+
+import games.stendhal.common.Direction;
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.pathfinder.FixedPath;
+import games.stendhal.server.core.pathfinder.Node;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.behaviour.adder.ProducerAdder;
+import games.stendhal.server.entity.npc.behaviour.impl.ProducerBehaviour;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Provides Gertha, the cherry pies confectioner NPC.
+ * She has a twin sister: Martha, the apple pies confectioner NPC.
+ *
+ * @author omero
+ */
+public class ConfectionerCherryPieNPC implements ZoneConfigurator {
+
+	/**
+	 * Configure a zone.
+	 *
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		buildNPC(zone, attributes);
+	}
+
+	private void buildNPC(final StendhalRPZone zone, final Map<String, String> attributes) {
+		final SpeakerNPC npc = new SpeakerNPC("Gertha") {
+
+			@Override
+			protected void createPath() {
+				final List<Node> nodes = new LinkedList<Node>();
+				nodes.add(new Node(10, 7));
+				nodes.add(new Node(4, 7));
+				nodes.add(new Node(9, 7));
+				nodes.add(new Node(9, 3));
+				nodes.add(new Node(6, 3));
+				nodes.add(new Node(10, 3));
+				setPath(new FixedPath(nodes, true));
+			}
+
+			@Override
+			protected void createDialog() {
+				
+				addJob("I live here with my twin sister #Martha and our passion is to #bake delicious fruit pies!");
+
+				addReply("martha",
+					"She's my twin sister and we live here together... Like me, she also likes to #bake fruit pies!");
+				addReply("honey",
+					"Try asking #Martha where she finds it.");
+				addReply("milk",
+					"I suppose you can find that in a farm!");
+				addReply("flour",
+					"I'd look for some at a mill...");
+				addReply("egg",
+					"Find some hens and you'll easily find some eggs too!");
+				addReply("cherry",
+					"Mmm... Those are sometimes hard to get. Did you already ask in some tavern if they can offer any?");
+
+				addHelp("If that can help, I could #bake a cherry pie for you!");
+				addOffer("I'd be happy to #bake a cherry pie for you. Why don't you just ask me?!");
+
+				addQuest("I'd love to try and bake a strawberry pie once in a while... But Alas! Strawberries are nowhere to be found...");
+
+				addGoodbye("Take care!");
+
+				// (uses sorted TreeMap instead of HashMap)
+				final Map<String, Integer> requiredResources = new TreeMap<String, Integer>();
+				requiredResources.put("flour", Integer.valueOf(2));
+				requiredResources.put("honey", Integer.valueOf(1));
+				requiredResources.put("milk", Integer.valueOf(1));
+				requiredResources.put("egg", Integer.valueOf(1));
+				requiredResources.put("cherry", Integer.valueOf(2));
+
+				final ProducerBehaviour behaviour = new ProducerBehaviour("gertha_bake_cherrypie", "bake", "cherry pie",
+				        requiredResources, 15 * 60);
+
+				new ProducerAdder().addProducer(this, behaviour,
+				        "Hello! Did you come for tasting one my fabulous cherry pies? I could #bake one for you happily!");
+			}
+		};
+
+		npc.setEntityClass("confectionercherrypienpc");
+		npc.setDirection(Direction.DOWN);
+		npc.setPosition(10, 6);
+		npc.initHP(100);
+		npc.setDescription("You see Gertha");
+		zone.add(npc);
+	}
+
+}
