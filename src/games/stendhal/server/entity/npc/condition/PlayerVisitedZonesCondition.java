e95298e4485d36b1da0d920fb298a2e8f8685006
Markus <madmetzger@users.sourceforge.net>
2010-10-27 13:21:16 +0000

    add a condition to check if a player has visited a certain list of zones

---
+++ src/games/stendhal/server/entity/npc/condition/PlayerVisitedZonesCondition.java
@@ -0,0 +1,57 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.entity.npc.condition;
+
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.Entity;
+import games.stendhal.server.entity.npc.ChatCondition;
+import games.stendhal.server.entity.npc.parser.Sentence;
+import games.stendhal.server.entity.player.Player;
+
+import java.util.LinkedList;
+import java.util.List;
+/**
+ * Checks if player has visited a list of certain zones
+ *  
+ * @author madmetzger
+ */
+public class PlayerVisitedZonesCondition implements ChatCondition {
+
+	private final List<String> zoneNames;
+	
+	/**
+	 * Creates a new PlayerVisitedZonesCondition
+	 * 
+	 * @param zones the zone names
+	 */
+	public PlayerVisitedZonesCondition(String... zones) {
+		this.zoneNames = new LinkedList<String>();
+		for (String zone : zones) {
+			zoneNames.add(zone);
+		}
+	}
+
+	public boolean fire(Player player, Sentence sentence, Entity npc) {
+		for(String zone : zoneNames) {
+			StendhalRPZone zoneObject = SingletonRepository.getRPWorld().getZone(zone);
+			if (zoneObject != null) {
+				if(!player.hasVisitedZone(zoneObject)) {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+}
