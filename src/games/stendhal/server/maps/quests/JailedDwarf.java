0d0a19b3c826c61ec12de2b229b648826ec4326f
Katie Russell <kymara@users.sourceforge.net>
2010-10-28 20:40:37 +0000

    use standard conditions and actions

--- src/games/stendhal/server/maps/quests/JailedDwarf.java
+++ src/games/stendhal/server/maps/quests/JailedDwarf.java
@@ -12,13 +12,17 @@
  ***************************************************************************/
 package games.stendhal.server.maps.quests;
 
-import games.stendhal.server.entity.npc.ChatAction;
 import games.stendhal.server.entity.npc.ConversationPhrases;
 import games.stendhal.server.entity.npc.ConversationStates;
-import games.stendhal.server.entity.npc.EventRaiser;
 import games.stendhal.server.entity.npc.SpeakerNPC;
-import games.stendhal.server.entity.npc.parser.Sentence;
-import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.entity.npc.action.IncreaseXPAction;
+import games.stendhal.server.entity.npc.action.MultipleActions;
+import games.stendhal.server.entity.npc.action.SetQuestAction;
+import games.stendhal.server.entity.npc.condition.AndCondition;
+import games.stendhal.server.entity.npc.condition.NotCondition;
+import games.stendhal.server.entity.npc.condition.PlayerHasItemWithHimCondition;
+import games.stendhal.server.entity.npc.condition.QuestCompletedCondition;
+import games.stendhal.server.entity.npc.condition.QuestNotCompletedCondition;
 
 /**
  * QUEST: Jailed Dwarf
@@ -45,26 +49,25 @@ public class JailedDwarf extends AbstractQuest {
 	}
 	private void step_1() {
 		final SpeakerNPC npc = npcs.get("Hunel");
-		   // TODO: Use standard actions and conditions
+		
 		npc.add(ConversationStates.IDLE, ConversationPhrases.GREETING_MESSAGES,
-				null, ConversationStates.ATTENDING, null,
-				new ChatAction() {
-					public void fire(final Player player, final Sentence sentence, final EventRaiser raiser) {
-						if (player.isQuestCompleted(QUEST_SLOT)) {
-							raiser.say("Hi. As you see, I am still too nervous to leave ...");
-						} else {
-							if (player.isEquipped("kanmararn prison key")) {
-								player.setQuest(QUEST_SLOT, "done");
-								player.addXP(2000);
-								raiser.say("You got the key to unlock me! *mumble*  Errrr ... it doesn't look too safe out there for me ... I think I'll just stay here ... perhaps someone could #offer me some good equipment ... ");
-							} else {
-								raiser.say("Help! The duergars have raided the prison and locked me up! I'm supposed to be the Guard! It's a shambles.");
-								player.setQuest(QUEST_SLOT, "start");
-								raiser.setCurrentState(ConversationStates.IDLE);
-							}
-						}
-					}
-				});
+				new QuestCompletedCondition(QUEST_SLOT),
+				 ConversationStates.ATTENDING,
+				 "Hi. As you see, I am still too nervous to leave ...",
+				 null);
+		
+		npc.add(ConversationStates.IDLE, ConversationPhrases.GREETING_MESSAGES,
+				new AndCondition(new QuestNotCompletedCondition(QUEST_SLOT), new NotCondition(new PlayerHasItemWithHimCondition("kanmararn prison key"))),
+				 ConversationStates.IDLE,
+				 "Help! The duergars have raided the prison and locked me up! I'm supposed to be the Guard! It's a shambles.",
+				 new SetQuestAction(QUEST_SLOT, "start"));
+		
+		npc.add(ConversationStates.IDLE, ConversationPhrases.GREETING_MESSAGES,
+				new AndCondition(new QuestNotCompletedCondition(QUEST_SLOT), new PlayerHasItemWithHimCondition("kanmararn prison key")),
+				 ConversationStates.ATTENDING,
+				 "You got the key to unlock me! *mumble*  Errrr ... it doesn't look too safe out there for me ... I think I'll just stay here ... perhaps someone could #offer me some good equipment ... ",
+				 new MultipleActions(new SetQuestAction(QUEST_SLOT, "done"),
+						 			 new IncreaseXPAction(2000)));
 
 	}
 
