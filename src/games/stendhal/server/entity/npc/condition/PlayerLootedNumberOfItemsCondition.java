919c4ec4776805ff74cb020279d4f2947ce50fa1
Markus <madmetzger@users.sourceforge.net>
2010-10-29 13:59:10 +0000

    condition to check how much a player looted of given items

---
+++ src/games/stendhal/server/entity/npc/condition/PlayerLootedNumberOfItemsCondition.java
@@ -0,0 +1,45 @@
+package games.stendhal.server.entity.npc.condition;
+
+import games.stendhal.server.entity.Entity;
+import games.stendhal.server.entity.npc.ChatCondition;
+import games.stendhal.server.entity.npc.parser.Sentence;
+import games.stendhal.server.entity.player.Player;
+
+import java.util.LinkedList;
+import java.util.List;
+/**
+ * Checks if a player has looted a minimum number of item(s).
+ *  
+ * @author madmetzger
+ */
+public class PlayerLootedNumberOfItemsCondition implements ChatCondition {
+	
+	private final List<String> items;
+	
+	private final int number;
+	
+	/**
+	 * Create a new PlayerLootedNumberOfItemsCondition
+	 * 
+	 * @param item
+	 */
+	public PlayerLootedNumberOfItemsCondition(int number, String... item) {
+		this.number = number;
+		items = new LinkedList<String>();
+		if(item != null) {
+			for (String string : item) {
+				items.add(string);
+			}
+		}
+	}
+
+	public boolean fire(Player player, Sentence sentence, Entity npc) {
+		for(String item : items) {
+			if(player.getNumberOfLootsForItem(item) < number) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
