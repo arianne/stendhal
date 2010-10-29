15849a7546e5b04e4dcc1707164b35c23063fb3a
Markus <madmetzger@users.sourceforge.net>
2010-10-29 15:03:22 +0000

    add a first example achievement

--- src/games/stendhal/server/core/events/achievements/ItemAchievementFactory.java
+++ src/games/stendhal/server/core/events/achievements/ItemAchievementFactory.java
@@ -1,5 +1,7 @@
 package games.stendhal.server.core.events.achievements;
 
+import games.stendhal.server.entity.npc.condition.PlayerLootedNumberOfItemsCondition;
+
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
@@ -14,6 +16,9 @@ public class ItemAchievementFactory extends AchievementFactory {
 	@Override
 	public Collection<Achievement> createAchievements() {
 		List<Achievement> itemAchievements = new LinkedList<Achievement>();
+		itemAchievements.add(createAchievement("item.money.100", "First pocket money", "Loot 100 money from creatures", 
+												Achievement.EASY_BASE_SCORE,
+												new PlayerLootedNumberOfItemsCondition(100, "money")));
 		return itemAchievements;
 	}
 
