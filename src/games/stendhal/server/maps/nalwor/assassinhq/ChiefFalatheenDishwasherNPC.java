9b83d4169f650d67b31a5612b0d350e5bf435519
Katie Russell <kymara@users.sourceforge.net>
2010-10-31 18:41:50 +0000

    put Chief Falatheen into pure java not mixed java-xml

--- src/games/stendhal/server/maps/nalwor/assassinhq/ChiefFalatheenDishwasherNPC.java
+++ src/games/stendhal/server/maps/nalwor/assassinhq/ChiefFalatheenDishwasherNPC.java
@@ -12,29 +12,50 @@
  ***************************************************************************/
 package games.stendhal.server.maps.nalwor.assassinhq;
 
+import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.entity.npc.ShopList;
 import games.stendhal.server.entity.npc.SpeakerNPC;
-import games.stendhal.server.entity.npc.SpeakerNPCFactory;
 import games.stendhal.server.entity.npc.behaviour.adder.BuyerAdder;
 import games.stendhal.server.entity.npc.behaviour.impl.BuyerBehaviour;
+
+import java.util.Map;
 
 /**
  * Inside Nalwor Assassin Headquarters - cellar .
  */
-//TODO: take NPC definition elements which are currently in XML and include here
-public class ChiefFalatheenDishwasherNPC extends SpeakerNPCFactory {
+public class ChiefFalatheenDishwasherNPC implements ZoneConfigurator  {
+
 	private final ShopList shops = SingletonRepository.getShopList();
+
+	public void configureZone(StendhalRPZone zone,
+			Map<String, String> attributes) {
+		buildNPC(zone);
+	}
+
+	private void buildNPC(StendhalRPZone zone) {
+		final SpeakerNPC npc = new SpeakerNPC("Chief Falatheen Humble Dishwasher") {
+
 			@Override
-			public void createDialog(final SpeakerNPC dishwasher) {
-				dishwasher.addGreeting("You better have a good excuse for bothering me. I'm up to my neck in dishwater!");
-				dishwasher.addJob("It is my job to wash all the dishes for all these pesky little brats.");
-				dishwasher.addHelp("I can buy your vegetables and herbs.  Please see blackboards on wall for what I need.");
-				dishwasher.addOffer("Look at blackboards on wall to see my prices.");
-				dishwasher.addQuest("You could try to help me #escape from these hoodlums. Well... maybe not.");
-				dishwasher.addGoodbye("Don't forget where I am now. Come back and see me some time. I do get lonely.");
-				dishwasher.addReply("escape", "Yes! I want to pursue my dream. Mother Helena offered me a most wonderful job.  She needs a dishwasher. Lots of complaining customers!!!");
-				new BuyerAdder().add(dishwasher, new BuyerBehaviour(shops.get("buyveggiesandherbs")), false);			    
-			    
-			   	}
+			protected void createPath() {
+				setPath(null);
+			}
+
+			@Override
+			public void createDialog() {
+				addGreeting("You better have a good excuse for bothering me. I'm up to my neck in dishwater!");
+				addJob("It is my job to wash all the dishes for all these pesky little brats.");
+				addHelp("I can buy your vegetables and herbs.  Please see blackboards on wall for what I need.");
+				addOffer("Look at blackboards on wall to see my prices.");
+				addQuest("You could try to help me #escape from these hoodlums. Well... maybe not.");
+				addGoodbye("Don't forget where I am now. Come back and see me some time. I do get lonely.");
+				addReply("escape", "Yes! I want to pursue my dream. Mother Helena offered me a most wonderful job.  She needs a dishwasher. Lots of complaining customers!!!");
+				new BuyerAdder().add(this, new BuyerBehaviour(shops.get("buyveggiesandherbs")), false);			    
+			}};
+			npc.setPosition(20, 3);
+			npc.setEntityClass("chieffalatheennpc");
+			zone.add(npc);		
+	}
 }
+			 
