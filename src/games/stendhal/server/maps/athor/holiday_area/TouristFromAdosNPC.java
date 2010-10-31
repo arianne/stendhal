767b4287fe0d63b9aad04e9c136610684596d041
Katie Russell <kymara@users.sourceforge.net>
2010-10-31 11:08:06 +0000

    put Zara into pure java not mixed java-xml

--- src/games/stendhal/server/maps/athor/holiday_area/TouristFromAdosNPC.java
+++ src/games/stendhal/server/maps/athor/holiday_area/TouristFromAdosNPC.java
@@ -12,29 +12,48 @@
  ***************************************************************************/
 package games.stendhal.server.maps.athor.holiday_area;
 
+import games.stendhal.common.Direction;
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.entity.npc.SpeakerNPC;
-import games.stendhal.server.entity.npc.SpeakerNPCFactory;
-//TODO: take NPC definition elements which are currently in XML and include here
-public class TouristFromAdosNPC extends SpeakerNPCFactory {
 
-	@Override
-	protected SpeakerNPC instantiate(final String name) {
-		final SpeakerNPC npc = new SpeakerNPC(name) {
+import java.util.Map;
+
+public class TouristFromAdosNPC implements ZoneConfigurator  {
+
+	public void configureZone(StendhalRPZone zone,
+			Map<String, String> attributes) {
+		buildNPC(zone);
+	}
+
+	private void buildNPC(StendhalRPZone zone) {
+		final SpeakerNPC npc = new SpeakerNPC("Zara") {
 			@Override
 			public void say(final String text) {
 				// She doesn't move around because she's "lying" on her towel.
 				say(text, false);
 			}
+
+			@Override
+			protected void createPath() {
+				// doesn't move
+				setPath(null);
+			}
+			
+			@Override
+			public void createDialog() {
+				addGreeting("Nice to meet you!");
+				addJob("I'm on holiday! Let's talk about anything else!");
+				addHelp("Be careful! On this island is a desert where many adventurers found their death...");
+				addGoodbye("I hope to see you soon!");
+				// more dialog is defined in the SuntanCreamForZara quest.
+			}
+
 		};
-		return npc;
-	}
-	
-	@Override
-	public void createDialog(final SpeakerNPC npc) {
-		npc.addGreeting("Nice to meet you!");
-		npc.addJob("I'm on holiday! Let's talk about anything else!");
-		npc.addHelp("Be careful! On this island is a desert where many adventurers found their death...");
-		npc.addGoodbye("I hope to see you soon!");
-		// more dialog is defined in the SuntanForZara quest.
+		npc.setPosition(60, 33);
+		npc.setEntityClass("swimmer8npc");
+		npc.setDirection(Direction.DOWN);
+		npc.setDescription("You see Zara, sunbathing, and worry that she is looking a little hot.");
+		zone.add(npc);		
 	}
 }
