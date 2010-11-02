cd88a7f2b3fdec8e4ff1ebfddb5efabdc3b174f0
Katie Russell <kymara@users.sourceforge.net>
2010-11-02 20:02:12 +0000

    put Jackie and the Captain into pure java not mixed java-xml also removed the todo on jackie's sailor type speech as she already has sailor type speech...

--- src/games/stendhal/server/maps/athor/ship/CaptainNPC.java
+++ src/games/stendhal/server/maps/athor/ship/CaptainNPC.java
@@ -13,77 +13,85 @@
 package games.stendhal.server.maps.athor.ship;
 
 import games.stendhal.common.Direction;
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.entity.npc.ChatAction;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.EventRaiser;
 import games.stendhal.server.entity.npc.SpeakerNPC;
-import games.stendhal.server.entity.npc.SpeakerNPCFactory;
 import games.stendhal.server.entity.npc.parser.Sentence;
 import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.maps.athor.ship.AthorFerry.Status;
 
+import java.util.Map;
+
 /** Factory for the captain of Athor Ferry. */
-//TODO: take NPC definition elements which are currently in XML and include here
-public class CaptainNPC extends SpeakerNPCFactory {
 
-private Status ferrystate;
-	@Override
-	protected SpeakerNPC instantiate(final String name) {
-		// The NPC is defined as a ferry announcer because he notifies
-		// passengers when the ferry arrives or departs.
-		final SpeakerNPC npc = new SpeakerNPC(name) {
+public class CaptainNPC implements ZoneConfigurator  {
 
+	private Status ferrystate;
+
+	public void configureZone(StendhalRPZone zone,
+			Map<String, String> attributes) {
+		buildNPC(zone);
+	}
+
+	private void buildNPC(StendhalRPZone zone) {
+		final SpeakerNPC npc = new SpeakerNPC("Captain") {
+
+			@Override
+			public void createDialog() {
+				addGreeting("Yo-ho-ho, me bucko!");
+				addGoodbye("So long...");
+				// if you can make up a help message that is more helpful to the,
+				// player, feel free to replace this one.
+				addHelp("Never look up when a sea gull is flying over ye head!");
+				addJob("I'm th' captain of me boat.");
+				add(ConversationStates.ATTENDING,
+						"status",
+						null,
+						ConversationStates.ATTENDING,
+						null,
+						new ChatAction() {
+					public void fire(final Player player, final Sentence sentence, final EventRaiser npc) {
+						npc.say(ferrystate.toString());
+						//.getCurrentDescription());
+					}
+				});
+
+			}
 
 			@Override
 			protected void onGoodbye(final Player player) {
 				// Turn back to the wheel
 				setDirection(Direction.DOWN);
-			}
+			}	
 		};
-		return npc;
-	}
-
-	@Override
-	public void createDialog(final SpeakerNPC npc) {
-		npc.addGreeting("Yo-ho-ho, me bucko!");
-		npc.addGoodbye("So long...");
-		// if you can make up a help message that is more helpful to the,
-		// player, feel free to replace this one.
-		npc.addHelp("Never look up when a sea gull is flying over ye head!");
-		npc.addJob("I'm th' captain of me boat.");
-
-		npc.add(ConversationStates.ATTENDING,
-				"status",
-				null,
-				ConversationStates.ATTENDING,
-				null,
-				new ChatAction() {
-					public void fire(final Player player, final Sentence sentence, final EventRaiser npc) {
-						npc.say(ferrystate.toString());
-								//.getCurrentDescription());
-					}
-				});
 
 		new AthorFerry.FerryListener() {
-
 			
 			public void onNewFerryState(final Status status) {
 				ferrystate = status;
-					switch (status) {
-					case ANCHORED_AT_MAINLAND:
-					case ANCHORED_AT_ISLAND:
-						// capital letters symbolize shouting
-						npc.say("LET GO ANCHOR!");
-						break;
+				switch (status) {
+				case ANCHORED_AT_MAINLAND:
+				case ANCHORED_AT_ISLAND:
+					// capital letters symbolize shouting
+					npc.say("LET GO ANCHOR!");
+					break;
 
-					default:
-						npc.say("ANCHORS AWEIGH! SET SAIL!");
-						break;
-					}
-					// Turn back to the wheel
-					npc.setDirection(Direction.DOWN);
+				default:
+					npc.say("ANCHORS AWEIGH! SET SAIL!");
+					break;
+				}
+				// Turn back to the wheel
+				npc.setDirection(Direction.DOWN);
 
 			}
-			};
+		};
+		
+		npc.setPosition(22, 38);
+		npc.setEntityClass("piratenpc");
+		npc.setDirection(Direction.DOWN);
+		zone.add(npc);	
 	}
 }
--- src/games/stendhal/server/maps/athor/ship/CoastConveyerNPC.java
+++ src/games/stendhal/server/maps/athor/ship/CoastConveyerNPC.java
@@ -13,6 +13,7 @@
 package games.stendhal.server.maps.athor.ship;
 
 import games.stendhal.common.Direction;
+import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.entity.npc.ChatAction;
@@ -20,16 +21,22 @@ import games.stendhal.server.entity.npc.ConversationPhrases;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.EventRaiser;
 import games.stendhal.server.entity.npc.SpeakerNPC;
-import games.stendhal.server.entity.npc.SpeakerNPCFactory;
 import games.stendhal.server.entity.npc.parser.Sentence;
 import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.maps.athor.ship.AthorFerry.Status;
 
 import java.util.Arrays;
+import java.util.Map;
 
 /** Factory for cargo worker on Athor Ferry. */
-//TODO: take NPC definition elements which are currently in XML and include here
-public class CoastConveyerNPC extends SpeakerNPCFactory {
+
+public class CoastConveyerNPC implements ZoneConfigurator  {
+
+	public void configureZone(StendhalRPZone zone,
+			Map<String, String> attributes) {
+		buildNPC(zone);
+	}
+
 	private static StendhalRPZone islandDocksZone;
 	private static StendhalRPZone mainlandDocksZone;
 
@@ -45,30 +52,38 @@ public class CoastConveyerNPC extends SpeakerNPCFactory {
 
 	private Status ferryState;
 
-	@Override
-	public void createDialog(final SpeakerNPC npc) {
-		//TODO: sailor-style language
-		npc.addGoodbye("Goodbye!"); 
-		npc.addGreeting("Ahoy, Matey! How can I #help you?");
-		npc.addHelp("Ye can #disembark, but only when we're anchored a harbor. Just ask me for the #status if ye have no idea where we are.");
-		npc.addJob("I'm taking passengers who want to #disembark to the coast with me rowing boat.");
+	private void buildNPC(StendhalRPZone zone) {
+		final SpeakerNPC npc = new SpeakerNPC("Jackie") {
 
-		npc.add(ConversationStates.ATTENDING, "status",
-				null,
-				ConversationStates.ATTENDING,
-				null,
-				new ChatAction() {
+			@Override
+			protected void createPath() {
+				setPath(null);
+			}
+
+			@Override
+			public void createDialog() {
+
+				addGoodbye("Goodbye!"); 
+				addGreeting("Ahoy, Matey! How can I #help you?");
+				addHelp("Ye can #disembark, but only when we're anchored a harbor. Just ask me for the #status if ye have no idea where we are.");
+				addJob("I'm taking passengers who want to #disembark to the coast with me rowing boat.");
+
+				add(ConversationStates.ATTENDING, "status",
+						null,
+						ConversationStates.ATTENDING,
+						null,
+						new ChatAction() {
 					public void fire(final Player player, final Sentence sentence, final EventRaiser npc) {
 						npc.say(ferryState.toString());
 					}
 				});
 
-		npc.add(ConversationStates.ATTENDING,
-				Arrays.asList("disembark", "leave"),
-				null,
-				ConversationStates.ATTENDING,
-				null,
-				new ChatAction() {
+				add(ConversationStates.ATTENDING,
+						Arrays.asList("disembark", "leave"),
+						null,
+						ConversationStates.ATTENDING,
+						null,
+						new ChatAction() {
 					public void fire(final Player player, final Sentence sentence, final EventRaiser npc) {
 						switch (ferryState) {
 						case ANCHORED_AT_MAINLAND:
@@ -82,18 +97,18 @@ public class CoastConveyerNPC extends SpeakerNPCFactory {
 
 						default:
 							npc.say(ferryState.toString()
-								+ " Ye can only get off the boat when it's anchored near a harbor.");
+									+ " Ye can only get off the boat when it's anchored near a harbor.");
 
 						}
-				}
-			});
+					}
+				});
 
 
-		npc.add(ConversationStates.SERVICE_OFFERED,
-				ConversationPhrases.YES_MESSAGES,
-				null,
-				ConversationStates.ATTENDING, null,
-				new ChatAction() {
+				add(ConversationStates.SERVICE_OFFERED,
+						ConversationPhrases.YES_MESSAGES,
+						null,
+						ConversationStates.ATTENDING, null,
+						new ChatAction() {
 					public void fire(final Player player, final Sentence sentence, final EventRaiser npc) {
 						switch (ferryState) {
 						case ANCHORED_AT_MAINLAND:
@@ -113,30 +128,37 @@ public class CoastConveyerNPC extends SpeakerNPCFactory {
 					}
 				});
 
-		npc.add(ConversationStates.SERVICE_OFFERED,
-				ConversationPhrases.NO_MESSAGES,
-				null,
-				ConversationStates.ATTENDING,
-				"Aye, matey!", null);
-		new AthorFerry.FerryListener() {
+				add(ConversationStates.SERVICE_OFFERED,
+						ConversationPhrases.NO_MESSAGES,
+						null,
+						ConversationStates.ATTENDING,
+						"Aye, matey!", null);
 
-			
-			public void onNewFerryState(final Status status) {
-				ferryState = status;
-				switch (status) {
-				case ANCHORED_AT_MAINLAND:
-					npc.say("Attention: The ferry has arrived at the mainland! You can now #disembark.");
-					break;
-				case ANCHORED_AT_ISLAND:
-					npc.say("Attention: The ferry has arrived at the island! You can now #disembark.");
-					break;
-				default:
-					npc.say("Attention: The ferry has set sail.");
-					break;
+			}};
+			new AthorFerry.FerryListener() {
+
+
+				public void onNewFerryState(final Status status) {
+					ferryState = status;
+					switch (status) {
+					case ANCHORED_AT_MAINLAND:
+						npc.say("Attention: The ferry has arrived at the mainland! You can now #disembark.");
+						break;
+					case ANCHORED_AT_ISLAND:
+						npc.say("Attention: The ferry has arrived at the island! You can now #disembark.");
+						break;
+					default:
+						npc.say("Attention: The ferry has set sail.");
+						break;
+					}
+
 				}
-
-			}
 			};
+
+			npc.setPosition(29, 34);
+			npc.setEntityClass("pirate_sailor2npc");
+			npc.setDirection(Direction.LEFT);
+			zone.add(npc);	
 	}
 
 	private static StendhalRPZone getMainlandDocksZone() {
