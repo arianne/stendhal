d34e2e3713b33dea4521e05a56a402160635446d
 omero <nylon0700@users.sourceforge.net>
2010-11-03 17:32:27 +0000

    adds a spawner for an empty eared bottle in Ados bakery

---
+++ src/games/stendhal/server/maps/ados/bakery/ItemsOnTable.java
@@ -0,0 +1,41 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.maps.ados.bakery;
+
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.mapstuff.spawner.PassiveEntityRespawnPoint;
+
+import java.util.Map;
+
+/**
+ * Creates items in Ados bakery
+ */
+public class ItemsOnTable implements ZoneConfigurator {
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		buildBakeryArea(zone);
+	}
+
+	private void buildBakeryArea(final StendhalRPZone zone) {
+
+		// grower for an empty eared bottle (24h)
+		final PassiveEntityRespawnPoint bottleGrower1 = new PassiveEntityRespawnPoint("eared bottle", 288000);
+		bottleGrower1.setPosition(8, 13);
+		bottleGrower1.setDescription("Scientists often put bottles down here.");
+		zone.add(bottleGrower1);
+
+		bottleGrower1.setToFullGrowth();
+
+	}
+
+}
