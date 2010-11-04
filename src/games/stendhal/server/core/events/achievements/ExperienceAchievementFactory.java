dcdaff35750f25ead537fd810757209a8474be9d
Markus <madmetzger@users.sourceforge.net>
2010-11-04 21:58:54 +0000

    fix a typo in an achievement description

--- src/games/stendhal/server/core/events/achievements/ExperienceAchievementFactory.java
+++ src/games/stendhal/server/core/events/achievements/ExperienceAchievementFactory.java
@@ -40,7 +40,7 @@ public class ExperienceAchievementFactory extends AchievementFactory {
 												new LevelGreaterThanCondition(99)));
 		xpAchievements.add(createAchievement("xp.level.200", "Adventurer", "Reach level 200", Achievement.MEDIUM_BASE_SCORE, 
 												new LevelGreaterThanCondition(199)));
-		xpAchievements.add(createAchievement("xp.level.300", "Experienced Adventurer", "Reach level 100", Achievement.MEDIUM_BASE_SCORE, 
+		xpAchievements.add(createAchievement("xp.level.300", "Experienced Adventurer", "Reach level 300", Achievement.MEDIUM_BASE_SCORE, 
 												new LevelGreaterThanCondition(299)));
 		xpAchievements.add(createAchievement("xp.level.400", "Master Adventurer", "Reach level 400", Achievement.MEDIUM_BASE_SCORE, 
 												new LevelGreaterThanCondition(399)));
