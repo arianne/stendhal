5884bc1f5792b5d2520f7109357d5c4df25dfc77
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-11-05 23:42:28 +0000

    added comments

--- src/games/stendhal/client/update/UpdateProgressBarWindowListener.java
+++ src/games/stendhal/client/update/UpdateProgressBarWindowListener.java
@@ -5,11 +5,18 @@ import java.awt.event.WindowListener;
 
 import javax.swing.JOptionPane;
 
+/**
+ * Ask for confirmation and exists the JVM if the user closes the progress bar window.
+ */
 public class UpdateProgressBarWindowListener implements WindowListener {
 
 	public void windowOpened(WindowEvent e) {
+		// do nothing
 	}
 
+	/**
+	 * Ask for confirmation and closes the JVM
+	 */
 	public void windowClosing(WindowEvent e) {
 		int result = JOptionPane.showConfirmDialog(e.getWindow(),
 				"Are you sure you want to cancel download?", "Confirmation",
@@ -21,17 +28,22 @@ public class UpdateProgressBarWindowListener implements WindowListener {
 	}
 
 	public void windowClosed(WindowEvent e) {
+		// do nothing
 	}
 
 	public void windowIconified(WindowEvent e) {
+		// do nothing
 	}
 
 	public void windowDeiconified(WindowEvent e) {
+		// do nothing
 	}
 
 	public void windowActivated(WindowEvent e) {
+		// do nothing
 	}
 
 	public void windowDeactivated(WindowEvent e) {
+		// do nothing
 	}
 }
