7649120cbbc6ad4b7749db7487c9bf1e3f08155a
 omero <nylon0700@users.sourceforge.net>
2010-11-08 13:20:16 +0000

    added wood as required item for brewing

--- src/games/stendhal/server/maps/ados/market/FierywaterDistillerNPC.java
+++ src/games/stendhal/server/maps/ados/market/FierywaterDistillerNPC.java
@@ -48,13 +48,15 @@ public class FierywaterDistillerNPC implements ZoneConfigurator {
                 addGreeting("Yo!");
                 addHelp("I'm quite new to this area myself. I cannot help you much, sorry!");
                 addQuest("Oh, well... I'm not much into those kind of things... I'm a simple man with simple needs, but thank you for asking."); 
-                addJob("I can #brew #fierywater for you, if you bring to me enough #canes!");
+                addJob("I can #brew #fierywater for you, if you bring to me enough #canes and #wood!");
                 addOffer("If you need #fierywater, just ask me to #brew some for you!");
                 addReply("fierywater",
                     "That's my specialty! Mix that with other ingredients and you'll have an execellent beverage."
                     +   " Gulp that down 100% pure and most likely you will not survive to tell the experience!");
                 addReply(Arrays.asList("cane", "canes"),
-                    "I get all my #canes imported directly from Athor island.");
+                    "I get all my canes imported directly from Athor island.");
+                addReply("wood",
+                    "You find plenty of wood logs near trees and forests are the best place to look for those!");
                 addGoodbye("Enjoy the market!");
             }
 
@@ -76,7 +78,7 @@ public class FierywaterDistillerNPC implements ZoneConfigurator {
         npc.setDescription("You see Uncle Dag. He runs the distillery stand in Ados market.");
         npc.setEntityClass("fierywaterdistillernpc");
         npc.setPosition(35, 30);
-        
+        npc.setDirection(Direction.DOWN);
         npc.initHP(100);
         zone.add(npc);
     }
