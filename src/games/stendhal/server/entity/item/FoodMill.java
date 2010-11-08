7b5650c9104eda024b1888fb81fdcca559874b40
 omero <nylon0700@users.sourceforge.net>
2010-11-08 13:16:41 +0000

    added notification messages on usage of the tool(s), added sugar mill

--- src/games/stendhal/server/entity/item/FoodMill.java
+++ src/games/stendhal/server/entity/item/FoodMill.java
@@ -22,46 +22,72 @@ import marauroa.common.game.RPObject;
 
 public class FoodMill extends Item implements UseListener {
 
-	public FoodMill(final String name, final String clazz,
-			final String subclass, final Map<String, String> attributes) {
-		super(name, clazz, subclass, attributes);
-	}
+    public FoodMill(final String name, final String clazz,
+            final String subclass, final Map<String, String> attributes) {
+        super(name, clazz, subclass, attributes);
+    }
 
-	public FoodMill(final FoodMill item) {
-		super(item);
-	}
+    public FoodMill(final FoodMill item) {
+        super(item);
+    }
 
-	public boolean onUsed(final RPEntity user) {
-		if (isContained()) {
-			final String slotName = getContainerSlot().getName();
-			if (slotName.endsWith("hand")) {
-				String otherhand;
-				if ("rhand".equals(slotName)) {
-					otherhand = "lhand";
-				} else {
-					otherhand = "rhand";
-				}
-				final RPObject first = user.getSlot(otherhand).getFirst();
-				if (first != null) {
-					if ("apple".equals(first.get("name"))) {
-						if (user.isEquipped("flask")) {
-
-							final Item item = SingletonRepository
-									.getEntityManager().getItem("apple juice");
-
-						
-							user.drop("apple");
-							user.drop("flask");
-							user.equipOrPutOnGround(item);
-						}
-					}
-				}
-
-			}
-			return true;
-		} else {
-			return false;
-		}
-	}
-
+    public boolean onUsed(final RPEntity user) {
+        if (isContained()) {
+            final String slotName = getContainerSlot().getName();
+            if (slotName.endsWith("hand")) {
+                String otherhand;
+                if ("rhand".equals(slotName)) {
+                    otherhand = "lhand";
+                } else {
+                    otherhand = "rhand";
+                }
+                final RPObject first = user.getSlot(otherhand).getFirst();
+                if (first != null) {
+                    if ("sugar mill".equals(getName())) {
+                        /**
+                         * the player needs to equip at least a sugar cane in his other hand
+                         * and have an empty sack in his inventory
+                         */
+                        if ("cane".equals(first.get("name"))) {
+                            if (user.isEquipped("empty sack")) {
+                                final Item item = SingletonRepository.getEntityManager().getItem("sugar");
+                                user.drop("cane");
+                                user.drop("empty sack");
+                                user.equipOrPutOnGround(item);
+                            } else {
+                                user.sendPrivateText("You don't have an empty sack with you");
+                            }
+                        } else {
+                            user.sendPrivateText("You need to have at least a sugar cane in your other hand");
+                        }
+                    } else {
+                        /**
+                         * the player needs to equip at least a sugar cane in his other hand
+                         * and have an empty flask in his inventory
+                         */
+                        if ("apple".equals(first.get("name"))) {
+                            if (user.isEquipped("flask")) {
+                                final Item item = SingletonRepository.getEntityManager().getItem("apple juice");
+                                user.drop("apple");
+                                user.drop("flask");
+                                user.equipOrPutOnGround(item);
+                            } else {
+                                user.sendPrivateText("You don't have an empty flask with you");
+                            }
+                        } else {
+                            user.sendPrivateText("You need to have at least an apple in your other hand");
+                        }
+                    }
+                } else {
+                    user.sendPrivateText("Your other hand looks empty.");
+                }
+            } else {
+                user.sendPrivateText("You should first equip the " + getName() + " in either hand in order to use it.");
+            }
+            return true;
+        } else {
+            user.sendPrivateText("You should be carrying the " + getName() + " in order to use it.");
+            return false;
+        }
+    }
 }
