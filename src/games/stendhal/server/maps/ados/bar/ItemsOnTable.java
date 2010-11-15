bdbdc710f62ee6d718b3321f20aa4b4bd02674df
 omero <nylon0700@users.sourceforge.net>
2010-11-15 15:13:48 +0000

    added a slim and eared bottle in ados bar ground floor

---
+++ src/games/stendhal/server/maps/ados/bar/ItemsOnTable.java
@@ -0,0 +1,47 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.maps.ados.bar;
+
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.mapstuff.spawner.PassiveEntityRespawnPoint;
+
+import java.util.Map;
+
+/**
+ * Creates bottles in Ados bar
+ */
+public class ItemsOnTable implements ZoneConfigurator {
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		buildBarArea(zone);
+	}
+
+	private void buildBarArea(final StendhalRPZone zone) {
+
+		// grower for an empty slim bottle (24h)
+		final PassiveEntityRespawnPoint bottleGrower1 = new PassiveEntityRespawnPoint("slim bottle", 288000);
+		bottleGrower1.setPosition(5, 15);
+		bottleGrower1.setDescription("This seems a good spot were a bottle could be standing.");
+		zone.add(bottleGrower1);
+
+		bottleGrower1.setToFullGrowth();
+
+		// grower for an empty eared bottle (24h)
+		final PassiveEntityRespawnPoint bottleGrower2 = new PassiveEntityRespawnPoint("eared bottle", 288000);
+		bottleGrower2.setPosition(22, 4);
+		bottleGrower2.setDescription("This seems a good spot were a bottle could be standing.");
+		zone.add(bottleGrower2);
+
+		bottleGrower2.setToFullGrowth();
+	}
+
+}
