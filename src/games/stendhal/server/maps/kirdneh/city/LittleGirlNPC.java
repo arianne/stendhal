ab1116795f3ebb2b2f888dd9bbea7c3c3cfc44f6
Bluelads4 <bluelads99@users.sourceforge.net>
2010-11-21 16:09:02 +0000

    Bringing new NPCs Elisabeth (little girl) and her mom Carey in (Kirdneh playground)

---
+++ src/games/stendhal/server/maps/kirdneh/city/LittleGirlNPC.java
@@ -0,0 +1,72 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.maps.kirdneh.city;
+
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+
+import java.util.Map;
+
+/**
+ * Builds a little girl NPC (Elisabeth) in Kirdneh city. 
+ *
+ * @author Vanessa Julius idea by miasma
+ */
+public class LittleGirlNPC implements ZoneConfigurator {
+	//
+	// ZoneConfigurator
+	//
+
+	/**
+	 * Configure a zone.
+	 *
+	 * @param zone
+	 *            The zone to be configured.
+	 * @param attributes
+	 *            Configuration attributes.
+	 */
+	public void configureZone(final StendhalRPZone zone,
+			final Map<String, String> attributes) {
+		buildNPC(zone, attributes);
+	}
+
+	private void buildNPC(final StendhalRPZone zone, final Map<String, String> attributes) {
+		final SpeakerNPC npc = new SpeakerNPC("Elisabeth") {
+
+			protected void createPath() {
+				setPath(null);
+		
+			}
+
+			@Override
+			protected void createDialog() {
+				// greeting message in quest given (ChocolateForElisabeth)
+				addJob("Job? I like it to play with my #toys :)");
+				addReply("toys", "There is a young boy who lives in Semos and gave me one of his teddies once :) So sweet!");
+				addHelp("Ask my #mommy, maybe she can help you...");
+				addReply("mommy", "She sits on the bench over there and enjoys the sun");
+				addOffer("I can't offer you anything, I'm just a #child.");
+				addReply("child", "I'm 5, yay!");
+				addQuest("I want chocolate :( Mommy wanted to take care of that.");
+				addGoodbye("Boiboi :)");
+			}
+		};
+
+		npc.setEntityClass("littlegirl2npc");
+		npc.setPosition(92, 15);
+		npc.initHP(100);
+		npc.setDescription("You see Elisabeth. She seems to be hungry.");
+		zone.add(npc);
+	}
+}
---
+++ src/games/stendhal/server/maps/kirdneh/city/MummyNPC.java
@@ -0,0 +1,72 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.maps.kirdneh.city;
+
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+
+import java.util.Map;
+
+/**
+ * Builds a information giving NPC in Kirdneh city. 
+ *
+ * @author Vanessa Julius idea by miasma
+ */
+public class MummyNPC implements ZoneConfigurator {
+	//
+	// ZoneConfigurator
+	//
+
+	/**
+	 * Configure a zone.
+	 *
+	 * @param zone
+	 *            The zone to be configured.
+	 * @param attributes
+	 *            Configuration attributes.
+	 */
+	public void configureZone(final StendhalRPZone zone,
+			final Map<String, String> attributes) {
+		buildNPC(zone, attributes);
+	}
+
+	private void buildNPC(final StendhalRPZone zone, final Map<String, String> attributes) {
+		final SpeakerNPC npc = new SpeakerNPC("Carey") {
+
+			@Override
+			protected void createPath() {
+				setPath(null);
+			}
+
+			@Override
+			protected void createDialog() {
+				//Greeting message in quest given (ChocolateForElisabeth)
+				addJob("I'm a fulltime mom and I love it.");
+				addHelp("I've heard that some of the #houses around the town are still for sale.");
+				addReply("houses", "They are huge! A friend of mine owns one and invited me a few times.");
+				addOffer("Oh I have no #offers for you, sorry.");
+				addReply("offers", "Did you visit the Kirdneh market already? It's smelling nice there");
+				addQuest("I don't have a quest for you but my daughter #Elisabeth searches for chocolate.");
+				addReply("Elisabeth", "She is such a lovely child, I will always care of her!");
+				addGoodbye("Thank you for meeting us here.");
+			}
+		};
+
+		npc.setEntityClass("mothernpc");
+		npc.setPosition(84, 9);
+		npc.initHP(100);
+		npc.setDescription("You see Carey. She takes care of her daughter Elisabeth.");
+		zone.add(npc);
+	}
+}
