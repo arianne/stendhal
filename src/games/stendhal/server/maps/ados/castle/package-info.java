3328472b301aacd5fe75deea1335e6ce56bb7f02
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-11-29 22:53:36 +0000

    package-info.java for ados maps

---
+++ src/games/stendhal/server/maps/ados/castle/package-info.java
@@ -0,0 +1,19 @@
+/**
+ * logic for Ados castle.
+ */
+package games.stendhal.server.maps.ados.castle;
+
+
+
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
