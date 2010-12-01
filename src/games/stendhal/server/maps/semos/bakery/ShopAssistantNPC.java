eaa2de3cf324a09a142d502d09da94eb53dbbf8e
Katie Russell <kymara@users.sourceforge.net>
2010-12-01 20:19:25 +0000

    erna lends out tools for free but if you lose them you pay. using scythe and dagger for testing while sugar mill and pestle and mortar isn't working.

--- src/games/stendhal/server/maps/semos/bakery/ShopAssistantNPC.java
+++ src/games/stendhal/server/maps/semos/bakery/ShopAssistantNPC.java
@@ -12,14 +12,40 @@
  ***************************************************************************/
 package games.stendhal.server.maps.semos.bakery;
 
+import games.stendhal.common.Grammar;
 import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.pathfinder.FixedPath;
 import games.stendhal.server.core.pathfinder.Node;
+import games.stendhal.server.entity.item.Item;
+import games.stendhal.server.entity.npc.ChatAction;
+import games.stendhal.server.entity.npc.ConversationPhrases;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.EventRaiser;
 import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.action.DecreaseKarmaAction;
+import games.stendhal.server.entity.npc.action.DropItemAction;
+import games.stendhal.server.entity.npc.action.DropRecordedItemAction;
+import games.stendhal.server.entity.npc.action.MultipleActions;
+import games.stendhal.server.entity.npc.action.SetQuestAction;
 import games.stendhal.server.entity.npc.behaviour.adder.ProducerAdder;
 import games.stendhal.server.entity.npc.behaviour.impl.ProducerBehaviour;
+import games.stendhal.server.entity.npc.condition.AndCondition;
+import games.stendhal.server.entity.npc.condition.LevelGreaterThanCondition;
+import games.stendhal.server.entity.npc.condition.LevelLessThanCondition;
+import games.stendhal.server.entity.npc.condition.NotCondition;
+import games.stendhal.server.entity.npc.condition.PlayerHasItemWithHimCondition;
+import games.stendhal.server.entity.npc.condition.PlayerHasRecordedItemWithHimCondition;
+import games.stendhal.server.entity.npc.condition.QuestActiveCondition;
+import games.stendhal.server.entity.npc.condition.QuestCompletedCondition;
+import games.stendhal.server.entity.npc.condition.QuestNotActiveCondition;
+import games.stendhal.server.entity.npc.condition.QuestNotCompletedCondition;
+import games.stendhal.server.entity.npc.condition.TriggerInListCondition;
+import games.stendhal.server.entity.npc.parser.Sentence;
+import games.stendhal.server.entity.player.Player;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -28,10 +54,18 @@ import java.util.TreeMap;
 /**
  * A woman who bakes bread for players.
  * 
- * @author daniel
+ * Erna will lend tools.
+ * 
+ * @author daniel / kymara
  */
 public class ShopAssistantNPC implements ZoneConfigurator  {
 
+	private static final int COST = 3000;
+	private static final String QUEST_SLOT = "borrow_kitchen_equipment";
+	// this should be sugar mill, and pestle and mortar, but it doesn't work.
+	private static final List<String> ITEMS = Arrays.asList("scythe", "dagger");
+
+			
 	public void configureZone(StendhalRPZone zone,
 			Map<String, String> attributes) {
 		buildNPC(zone);
@@ -72,6 +106,134 @@ public class ShopAssistantNPC implements ZoneConfigurator  {
 
 				new ProducerAdder().addProducer(this, behaviour,
 				"Welcome to the Semos bakery! We'll #bake fine bread for anyone who helps bring our #flour delivery from the mill.");
+				
+				addOffer("Our pizza delivery team can #borrow some kitchen equipment from me.");
+				 
+				add(ConversationStates.ATTENDING, "borrow", 
+				    new LevelLessThanCondition(6), 
+				    ConversationStates.ATTENDING, 
+				    "Oh sorry, I don't lend equipment to people with so little experience as you.",
+				    null);
+				 
+				add(ConversationStates.ATTENDING, "borrow", 
+				    new AndCondition(new LevelGreaterThanCondition(5), new QuestNotCompletedCondition("pizza_delivery")),  
+				    ConversationStates.ATTENDING, 
+				    "You'll have to speak to Leander and ask if you can help with the pizza before I'm allowed to lend you anything.",
+				    null);
+				 
+				add(ConversationStates.ATTENDING, "borrow", 
+				    new AndCondition(
+				        new LevelGreaterThanCondition(5), 
+				        new QuestCompletedCondition("pizza_delivery"),
+				        new QuestNotActiveCondition(QUEST_SLOT)), 
+				    ConversationStates.ATTENDING, 
+				    // TODO: # in front of each item
+				    "I lend out " + Grammar.enumerateCollection(ITEMS) + ". If you're interested, please say which you want.", 
+				    null);
+				 
+				// player already has borrowed something it didn't return and will pay for it
+				add(ConversationStates.ATTENDING, "borrow", 
+				    new AndCondition(new QuestActiveCondition(QUEST_SLOT), new NotCondition(new PlayerHasRecordedItemWithHimCondition(QUEST_SLOT))),  
+				    ConversationStates.QUESTION_1, 
+				    "You didn't return what I last lent you! Do you want to pay for it at a cost of " + COST + " money?",
+				    null);
+				 
+				// player already has borrowed something it didn't return and will return it
+				add(ConversationStates.ATTENDING, "borrow", 
+				    new AndCondition(new QuestActiveCondition(QUEST_SLOT), new PlayerHasRecordedItemWithHimCondition(QUEST_SLOT)),  
+				    ConversationStates.QUESTION_2, 
+				    "You didn't return what I last lent you! Do you want to return it now?",
+				    null);
+				 
+				// player wants to pay for previous item
+				final List<ChatAction> payment = new LinkedList<ChatAction>();
+				payment.add(new DropItemAction("money", COST));
+				payment.add(new SetQuestAction(QUEST_SLOT, "done"));
+				payment.add(new DecreaseKarmaAction(10));
+				add(ConversationStates.QUESTION_1, 
+				    ConversationPhrases.YES_MESSAGES, 
+				    new PlayerHasItemWithHimCondition("money", COST),  
+				    ConversationStates.ATTENDING, 
+				    "Thanks. Just let me know if you want to #borrow any tools again.",
+				    new MultipleActions(payment));
+				 
+				// player already has borrowed something and wants to return it
+				final List<ChatAction> returnitem = new LinkedList<ChatAction>();
+				returnitem.add(new DropRecordedItemAction(QUEST_SLOT));
+				returnitem.add(new SetQuestAction(QUEST_SLOT, "done"));
+				add(ConversationStates.QUESTION_2, 
+				    ConversationPhrases.YES_MESSAGES, 
+				    new PlayerHasRecordedItemWithHimCondition(QUEST_SLOT),  
+				    ConversationStates.ATTENDING, 
+				    "Thank you! Just let me know if you want to #borrow any tools again.",
+				    new MultipleActions(returnitem));
+				 
+				// don't want to pay for it now
+				add(ConversationStates.QUESTION_1, 
+				    ConversationPhrases.NO_MESSAGES, 
+				    null,  
+				    ConversationStates.ATTENDING, 
+				    "No problem. Take as long as you need, but you can't borrow other tools till you return the last, or pay for it.",
+				    new MultipleActions(payment));
+				 
+				// don't want to return it now
+				add(ConversationStates.QUESTION_2, 
+				    ConversationPhrases.NO_MESSAGES, 
+				    null,  
+				    ConversationStates.ATTENDING, 
+				    "No problem. Take as long as you need, but you can't borrow other tools till you return the last, or pay for it.",
+				    null);
+				 
+				 
+				// saying the item name and storing that item name into the quest slot, and giving the item
+				add(ConversationStates.ATTENDING,
+				    ITEMS,
+				    new AndCondition(
+				    	new TriggerInListCondition(ITEMS),
+				        new LevelGreaterThanCondition(5), 
+				        new QuestCompletedCondition("pizza_delivery"),
+				        new QuestNotActiveCondition(QUEST_SLOT)),
+				    ConversationStates.ATTENDING, 
+				    null,
+				    new ChatAction(){
+					public void fire(final Player player, final Sentence sentence, final EventRaiser npc) {
+						final String itemName = sentence.getTriggerExpression().toString();
+						final Item item =  SingletonRepository.getEntityManager().getItem(itemName);
+						if (item == null) {
+							npc.say("Sorry, something went wrong. Can you say correctly the item, please?");
+							return;
+						}
+						
+						player.equipOrPutOnGround(item);
+						player.setQuest(QUEST_SLOT,itemName);
+						npc.say("Here you are! Don't forget to #return it or you have to pay!");
+					}
+				});
+				 
+				// say the item name when not in correct conditions should give some appropriate message - or use null condition for a generic message about the item?
+				addReply(ITEMS,"Sorry, you can't borrow from me right now.");
+				
+				// player asks about pay from attending state
+				add(ConversationStates.ATTENDING, "pay", 
+				    new QuestActiveCondition(QUEST_SLOT),  
+				    ConversationStates.QUESTION_1, 
+				    "If you lost what I lent you, you can pay " + COST + " money. Do you want to pay now?",
+				    null);
+				 
+				// player asks about return from attending state
+				add(ConversationStates.ATTENDING, "return", 
+				    new AndCondition(new QuestActiveCondition(QUEST_SLOT), new PlayerHasRecordedItemWithHimCondition(QUEST_SLOT)),  
+				    ConversationStates.QUESTION_2, 
+				    "Do you want to return what you borrowed now?",
+				    null);
+				
+				// player asks about return from attending state
+				add(ConversationStates.ATTENDING, "return", 
+				    new AndCondition(new QuestActiveCondition(QUEST_SLOT), new NotCondition(new PlayerHasRecordedItemWithHimCondition(QUEST_SLOT))),  
+				    ConversationStates.QUESTION_1, 
+				    "You don't have it with you! Do you want to pay " + COST + " money for it now?",
+				    null);
+				
 			}};
 			npc.setPosition(26, 9);
 			npc.setEntityClass("housewifenpc");
