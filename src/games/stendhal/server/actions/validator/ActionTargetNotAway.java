6976f7cda14579eafca369510a3b6a2daee322a4
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-12-01 22:37:50 +0000

    action validators for targetPlayer being online and not away

---
+++ src/games/stendhal/server/actions/validator/ActionTargetNotAway.java
@@ -0,0 +1,53 @@
+/* $Id$ */
+/***************************************************************************
+ *                      (C) Copyright 2003 - Marauroa                      *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.actions.validator;
+
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.entity.player.Player;
+import marauroa.common.game.RPAction;
+
+/**
+ * checks that the target player is not away.
+ *
+ * @author hendrik
+ */
+public class ActionTargetNotAway implements ActionValidator {
+	private String targetAttribute;
+
+	/**
+	 * creates a new ActionTargetNotAway
+	 *
+	 * @param targetAttribute name of attribute containing the target player name
+	 */
+	public ActionTargetNotAway(String targetAttribute) {
+		this.targetAttribute = targetAttribute;
+	}
+
+	/**
+	 * validates an RPAction.
+	 *
+	 * @param player Player
+	 * @param action RPAction to validate
+	 * @return <code>null</code> if the action is valid; an error message otherwise
+	 */
+	public String validate(Player player, RPAction action) {
+		String playerName = action.get(targetAttribute);
+		Player targetPlayer = SingletonRepository.getRuleProcessor().getPlayer(playerName);
+		String awayMessage = targetPlayer.getAwayMessage();
+		if (awayMessage != null) {
+			return targetPlayer.getName() + " is away from keyboard: " + awayMessage;
+		}
+		return null;
+	}
+
+}
---
+++ src/games/stendhal/server/actions/validator/ActionTargetOnline.java
@@ -0,0 +1,52 @@
+/* $Id$ */
+/***************************************************************************
+ *                      (C) Copyright 2003 - Marauroa                      *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.actions.validator;
+
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.entity.player.Player;
+import marauroa.common.game.RPAction;
+
+/**
+ * checks that the target player is onlined.
+ *
+ * @author hendrik
+ */
+public class ActionTargetOnline implements ActionValidator {
+	private String targetAttribute;
+
+	/**
+	 * creates a new ActionTargetOnline
+	 *
+	 * @param targetAttribute name of attribute containing the target player name
+	 */
+	public ActionTargetOnline(String targetAttribute) {
+		this.targetAttribute = targetAttribute;
+	}
+
+	/**
+	 * validates an RPAction.
+	 *
+	 * @param player Player
+	 * @param action RPAction to validate
+	 * @return <code>null</code> if the action is valid; an error message otherwise
+	 */
+	public String validate(Player player, RPAction action) {
+		String playerName = action.get(targetAttribute);
+		Player targetPlayer = SingletonRepository.getRuleProcessor().getPlayer(playerName);
+		if (targetPlayer == null) {
+			return "No player called " + playerName + " is online.";
+		}
+		return null;
+	}
+
+}
