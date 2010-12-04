2cc9f99575c471cca27d35d8810233b2801c6fe0
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-12-04 23:44:57 +0000

    stub for ProgressStatusEvents

--- src/games/stendhal/server/entity/player/PlayerRPClass.java
+++ src/games/stendhal/server/entity/player/PlayerRPClass.java
@@ -137,7 +137,9 @@ public class PlayerRPClass {
 		player.addRPEvent("show_item_list", Definition.PRIVATE);
 		player.addRPEvent(Events.VIEW_CHANGE, Definition.PRIVATE);
 		player.addRPEvent(Events.GROUP_CHANGE, Definition.PRIVATE);
+		player.addRPEvent(Events.PROGRESS_STATUS_CHANGE, Definition.PRIVATE);
 
+		// Maps
 		player.addAttribute("source_usage", Type.MAP, Definition.HIDDEN);
 	}
 
