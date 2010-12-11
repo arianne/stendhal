4b5810db8c2874e6c4f25bb1c2072292f8e75319
Martin Fuchs <martinfuchs@users.sourceforge.net>
2010-12-11 00:31:33 +0000

    fix runpte script on windows

--- runpte.bat
+++ runpte.bat
@@ -1,11 +1,7 @@
-#!/bin/sh
-set STENDHAL_VERSION="0.90.5"
-
+@echo off
 call ant compile_stendhaltools server_build
 
 cd build\build_stendhaltools
-
-set LOCALCLASSPATH=.;../../build/lib/stendhal-server-$STENDHAL_VERSION.jar;../../libs/marauroa.jar;../../libs/log4j.jar;../../libs/swing-layout.jar
-
+set LOCALCLASSPATH=.;..\..\build\build_server;..\..\libs\marauroa.jar;..\..\libs\log4j.jar;..\..\libs\swing-layout.jar
 javaw -cp "%LOCALCLASSPATH%" games.stendhal.tools.npcparsertestenv.TestEnvDlg
-
+cd ..\..
--- runpte.sh
+++ runpte.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-STENDHAL_VERSION="0.90.5"
-
 ant compile_stendhaltools server_build
 
 cd build/build_stendhaltools
-
-LOCALCLASSPATH=.:../../build/lib/stendhal-server-$STENDHAL_VERSION.jar:../../libs/marauroa.jar:../../libs/log4j.jar:../../libs/swing-layout.jar
-
+LOCALCLASSPATH=.:../../build/build_server:../../libs/marauroa.jar:../../libs/log4j.jar:../../libs/swing-layout.jar
 java -cp "${LOCALCLASSPATH}" games.stendhal.tools.npcparsertestenv.TestEnvDlg
-
