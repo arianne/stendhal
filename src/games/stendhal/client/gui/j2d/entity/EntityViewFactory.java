44571819b7722104a87acaaabd8111652c0a4aae
Katie Russell <kymara@users.sourceforge.net>
2010-12-12 18:34:36 +0000

    [Stendhal] scroll eraser doesn't have a Use option 3136020

--- src/games/stendhal/client/gui/j2d/entity/EntityViewFactory.java
+++ src/games/stendhal/client/gui/j2d/entity/EntityViewFactory.java
@@ -227,7 +227,8 @@ public class EntityViewFactory {
 		register("item", "drink", null,  UseableItem2DView.class);
 		register("item", "tool", "foodmill",  UseableItem2DView.class);
 		register("item", "tool", "sugarmill",  UseableItem2DView.class);
-
+		register("item", "tool", "scrolleraser",  UseableItem2DView.class);
+		
 		register("item", "ring", null, UseableRing2DView.class);
 
 		register("useable_entity", null, null, UseableEntity2DView.class);
