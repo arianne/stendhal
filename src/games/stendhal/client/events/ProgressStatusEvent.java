203ab1e92b6757a2dc934733ca3bce0f83867aee
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-12-18 14:18:44 +0000

    removed debug output

--- src/games/stendhal/client/events/ProgressStatusEvent.java
+++ src/games/stendhal/client/events/ProgressStatusEvent.java
@@ -36,7 +36,6 @@ public class ProgressStatusEvent extends Event<RPEntity> {
 			} else if (!event.has("item")) {
 				ProgressLogController.get().showCategorySummary(event.get("progress_type"), event.getList("data"));
 			} else {
-				System.out.println(event.getList("data"));
 				ProgressLogController.get().showDescription(event.get("progress_type"), event.get("item"), event.get("description"), event.getList("data"));
 			}
 		} catch (RuntimeException e) {
