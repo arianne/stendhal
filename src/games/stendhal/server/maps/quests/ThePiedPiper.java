a8d424655e177673354366fd63913478e206fecc
Katie Russell <kymara@users.sourceforge.net>
2010-12-19 13:34:12 +0000

    grammar fix in getHistory

--- src/games/stendhal/server/maps/quests/ThePiedPiper.java
+++ src/games/stendhal/server/maps/quests/ThePiedPiper.java
@@ -424,10 +424,10 @@ import org.apache.log4j.Logger;
 		}	
 		final String questState = player.getQuest(QUEST_SLOT, 0);
 		if ("rats".equals(questState)) {
-			history.add("I have killed some rats in Ados city already, and trying to kill more.");
+			history.add("I have killed some rats in Ados city already, and am trying to kill more.");
 		}
 		if ("done".equals(questState)) {
-			history.add("I have killed some rats in Ados city and got reward from Mayor Chalmers!");
+			history.add("I have killed some rats in Ados city and got a reward from Mayor Chalmers!");
 		}
 		return history; 		
  	}
