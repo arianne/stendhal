7d2c4fb9c46bdaf38ecd6fe0c01d839c744e98c4
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-12-26 21:09:16 +0000

    initial checkin

---
+++ src/games/stendhal/server/StendhalServer.java
@@ -0,0 +1,31 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server;
+
+/**
+ * Starts a Stendhal server
+ *
+ * @author hendrik
+ */
+public class StendhalServer {
+
+	/**
+	 * Starts a Stendhal server
+	 *
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		marauroa.server.marauroad.main(args);
+	}
+
+}
