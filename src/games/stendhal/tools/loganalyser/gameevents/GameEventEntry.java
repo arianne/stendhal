24139597bd8508db18627672b7da4d9f8753b0a1
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-12-28 17:26:58 +0000

    helper classes to iterate over gameEvents

---
+++ src/games/stendhal/tools/loganalyser/gameevents/GameEventEntry.java
@@ -0,0 +1,89 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.tools.loganalyser.gameevents;
+
+/**
+ * represents an entry of the gameEvent table.
+ *
+ * @author hendrik
+ */
+public class GameEventEntry {
+	private final String timestamp;
+	private final String id;
+	private final String source;
+	private final String event;
+	private final String param1;
+	private final String param2;
+
+	/**
+	 * Creates a new GameEventEntry.
+	 *
+	 * @param timestamp timestamp
+	 * @param itemid id of the item
+	 * @param source name of player
+	 * @param event  name of event
+	 * @param param1 additional param1
+	 * @param param2 additional param2
+	 * @param param3 additional param3
+	 * @param param4 additional param4
+	 */
+	public GameEventEntry(final String id, final String timestamp,
+			final String source, final String event, final String param1, final String param2) {
+		this.id = id;
+		this.timestamp = timestamp;
+		this.source = source;
+		this.event = event;
+		this.param1 = param1;
+		this.param2 = param2;
+	}
+
+	public String getEvent() {
+		return event;
+	}
+
+	public String getParam1() {
+		return param1;
+	}
+
+	public String getParam2() {
+		return param2;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder();
+		sb.append(timestamp);
+		sb.append('\t');
+		sb.append(source);
+		sb.append('\t');
+		sb.append(event);
+		sb.append('\t');
+		sb.append(param1);
+		sb.append('\t');
+		sb.append(param2);
+
+		return sb.toString();
+	}
+
+	public String getId() {
+		return id;
+	}
+}
---
+++ src/games/stendhal/tools/loganalyser/gameevents/GameEventEntryIterator.java
@@ -0,0 +1,62 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.tools.loganalyser.gameevents;
+
+import games.stendhal.tools.loganalyser.util.ResultSetIterator;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Iterates over the log entries returned by a database query.
+ *
+ * @author hendrik
+ */
+public class GameEventEntryIterator extends ResultSetIterator<GameEventEntry> {
+	private static Logger logger = Logger.getLogger(GameEventEntryIterator.class);
+
+	/**
+	 * Creates a new LogEntryIterator.
+	 *
+	 * @param resultSet resultSet
+	 */
+	public GameEventEntryIterator(final ResultSet resultSet) {
+		super(resultSet);
+	}
+
+	/**
+	 * Creates a new LogEntryIterator.
+	 *
+	 * @param statement statement
+	 * @param resultSet resultSet
+	 */
+	public GameEventEntryIterator(final Statement statement, final ResultSet resultSet) {
+		super(statement, resultSet);
+	}
+
+	@Override
+	protected GameEventEntry createObject() {
+		try {
+			return new GameEventEntry(resultSet.getString("id"), resultSet.getString("timedate"),
+					resultSet.getString("source"), resultSet.getString("event"), 
+					resultSet.getString("param1"), resultSet.getString("param2"));
+		} catch (final SQLException e) {
+			logger.error(e, e);
+			return null;
+		}
+	}
+
+}
