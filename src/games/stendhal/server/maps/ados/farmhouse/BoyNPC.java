e302050ae34f272a4256bfa9ec76e0f6e94c06dd
Katie Russell <kymara@users.sourceforge.net>
2010-12-29 22:36:53 +0000

    add two new npcs, a little boy who is sick and his worried mum

---
+++ src/games/stendhal/server/maps/ados/farmhouse/BoyNPC.java
@@ -0,0 +1,57 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.maps.ados.farmhouse;
+
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+
+import java.util.Map;
+
+/**
+ * Boy NPC
+ *
+ * @author kymara
+ */
+public class BoyNPC implements ZoneConfigurator {
+
+	/**
+	 * Configure a zone.
+	 *
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		buildNPC(zone, attributes);
+	}
+
+	private void buildNPC(final StendhalRPZone zone, final Map<String, String> attributes) {
+		final SpeakerNPC npc = new SpeakerNPC("Hughie") {
+
+			@Override
+			protected void createPath() {
+				setPath(null);
+			}
+
+			@Override
+			protected void createDialog() {
+				// says nothing at all
+			}
+		};
+		npc.setEntityClass("kid8npc");
+		npc.setPosition(33, 6);
+		npc.initHP(100);
+		npc.setDescription("You see a little boy who looks as though he might be suffering from fever.");
+	    zone.add(npc);
+	}
+}
---
+++ src/games/stendhal/server/maps/ados/farmhouse/MotherNPC.java
@@ -0,0 +1,69 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.maps.ados.farmhouse;
+
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.pathfinder.FixedPath;
+import games.stendhal.server.core.pathfinder.Node;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Mother NPC
+ *
+ * @author kymara
+ */
+public class MotherNPC implements ZoneConfigurator {
+
+	/**
+	 * Configure a zone.
+	 *
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		buildNPC(zone, attributes);
+	}
+
+	private void buildNPC(final StendhalRPZone zone, final Map<String, String> attributes) {
+		final SpeakerNPC npc = new SpeakerNPC("Anastasia") {
+
+			@Override
+			protected void createPath() {
+				final List<Node> nodes = new LinkedList<Node>();
+				nodes.add(new Node(30, 4));
+				nodes.add(new Node(41, 4));
+				nodes.add(new Node(41, 9));
+				nodes.add(new Node(30, 9));
+				setPath(new FixedPath(nodes, true));
+			}
+
+			@Override
+			protected void createDialog() {
+				addGreeting("Hello. I'm sorry I don't have much time as my boy is ill.");
+				addJob("My brother runs this farm. I just look after my son here.");
+				addHelp("Philomena can sell you milk and butter.");
+				addGoodbye("Goodbye.");
+			}
+		};
+		npc.setEntityClass("woman_006_npc");
+		npc.setPosition(30, 9);
+		npc.initHP(100);
+		npc.setDescription("You see Anastasia, looking worried.");
+	    zone.add(npc);
+	}
+}
