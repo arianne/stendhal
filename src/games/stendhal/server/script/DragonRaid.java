df8a0197dd47c2f04e6ddac28777af56eedc9e3e
Katie Russell <kymara@users.sourceforge.net>
2011-01-02 15:39:04 +0000

    fix dragon name

--- src/games/stendhal/server/script/DragonRaid.java
+++ src/games/stendhal/server/script/DragonRaid.java
@@ -28,7 +28,7 @@ public class DragonRaid extends CreateRaid {
 		attackArmy.put("red dragon", 2);
 		attackArmy.put("green dragon", 2);
 		attackArmy.put("bone dragon", 3);
-		attackArmy.put("twinheaded dragon", 2);
+		attackArmy.put("twin headed dragon", 2);
 		attackArmy.put("blue dragon", 3);
 		attackArmy.put("chaos red dragonrider", 2);
 		attackArmy.put("chaos green dragonrider", 2);
