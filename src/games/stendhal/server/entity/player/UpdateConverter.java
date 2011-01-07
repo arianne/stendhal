6b0e6681d769779d741dff494a6eedbfbf4969b4
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-01-07 22:50:44 +0000

    added license block

--- src/games/stendhal/server/entity/player/UpdateConverter.java
+++ src/games/stendhal/server/entity/player/UpdateConverter.java
@@ -1,4 +1,15 @@
-
+/* $Id$ */
+/***************************************************************************
+ *					(C) Copyright 2003-2011 - Stendhal					   *
+ ***************************************************************************
+ ***************************************************************************
+ *																		   *
+ *	 This program is free software; you can redistribute it and/or modify  *
+ *	 it under the terms of the GNU General Public License as published by  *
+ *	 the Free Software Foundation; either version 2 of the License, or	   *
+ *	 (at your option) any later version.								   *
+ *																		   *
+ ***************************************************************************/
 package games.stendhal.server.entity.player;
 
 import games.stendhal.common.ItemTools;
@@ -21,6 +32,10 @@ import marauroa.common.game.RPSlot;
 
 import org.apache.log4j.Logger;
 
+/**
+ * converts player objects to the most recent version by adding attributes,
+ * transforming quest states and similar migrations.
+ */
 public abstract class UpdateConverter {
 	private static Logger logger = Logger.getLogger(UpdateConverter.class);
 
