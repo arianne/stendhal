a5b6a2733be2691186e324be2cabacc58c314f1f
Martin Fuchs <martinfuchs@users.sourceforge.net>
2011-01-09 21:31:12 +0000

    fix bug id 3075982 - rework colouring rules in displaying text in the client

--- src/games/stendhal/client/gui/FormatTextParser.java
+++ src/games/stendhal/client/gui/FormatTextParser.java
@@ -1,6 +1,6 @@
 /* $Id$ */
 /***************************************************************************
- *                   (C) Copyright 2003-2010 - Stendhal                    *
+ *                   (C) Copyright 2003-2011 - Stendhal                    *
  ***************************************************************************
  ***************************************************************************
  *                                                                         *
@@ -12,6 +12,7 @@
  ***************************************************************************/
 package games.stendhal.client.gui;
 
+
 /**
  * Parsing of formated text. 
  *
@@ -19,7 +20,7 @@ package games.stendhal.client.gui;
  */
 public abstract class FormatTextParser {
 	/**
-	 * Write text optionally colored, and strips '\' from the front of the '#' signs.
+	 * Write text optionally coloured, and strips '\' from the front of the '#' signs.
 	 * 
 	 * @param text the text to write
 	 * @param color true if the text should be coloured
@@ -45,33 +46,58 @@ public abstract class FormatTextParser {
 				done = true;
 			} else {
 				quotedIndex = text.indexOf("\\#", startIndex);
+
 				if (quotedIndex != -1 && quotedIndex == (unquotedIndex - 1)) {
 					// the next match is \#. skip it and let writeText replace it
 					startIndex = quotedIndex + 2;
 				} else {
-					// found a lone #. start coloring
+					// found a lone #. start colouring
 					// Write the text before the #
 					if (unquotedIndex != 0) {
 						//writeText(text.substring(0, unquotedIndex - 1), false);
 						writeText(text.substring(0, unquotedIndex), false);
 					}
-					
-					// Find the region to color
-					char endMarker = ' ';
-					int shift = 0;
+
+					// Find the region to colour
+					char[] endMarkers;
+					int shift;
+
 					if ((text.length() > (unquotedIndex + 1)) && (text.charAt(unquotedIndex + 1) == '\'')) {
-						endMarker = '\'';
+						// stop at the terminating apostrophe character
 						shift = 1;
+						endMarkers = new char[] {'\''};
+					} else {
+						// stop at the next space or line feed
+						shift = 0;
+						endMarkers = new char[] {' ', '\n', ',', '.', '!', '?'};
 					}
+
 					// skip the #, and possible quoting
 					text = text.substring(unquotedIndex + 1 + shift);
-					final int stopAt = text.indexOf(endMarker);
-					if (stopAt  == -1) {
-						// No end marker found. the rest is colored (colors improperly quoted strings) 
+
+					// search for the first matching stop marker
+					int stopAt = -1;
+					for(char c : endMarkers) {
+						int idx = text.indexOf(c);
+
+						if (idx != -1) {
+							if (c == '\'' ||	// always stop at the terminating apostrophe
+								java.lang.Character.isWhitespace(c) ||	// always stop on white space markers
+								idx>=text.length()-1 ||	// Punctation only terminates the colouring, if there follows white space.
+										java.lang.Character.isWhitespace(text.charAt(idx+1))) {
+								if ((stopAt == -1) || (idx < stopAt)) {
+									stopAt = idx;
+								}
+							}
+						}
+					}
+
+					if (stopAt == -1) {
+						// No end marker found. the rest is coloured (colours improperly quoted strings) 
 						writeText(text, true);
 						done = true;
 					} else {
-						// color until the endMarker, and skip the possible quoting
+						// colour until the endMarker, and skip the possible quoting
 						writeText(text.substring(0, stopAt), true);
 						text = text.substring(stopAt + shift);
 					}
