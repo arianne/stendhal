ee3e20f3091881bcc85de733aef9b0d9c026ca2f
Bluelads4 <bluelads99@users.sourceforge.net>
2011-01-10 00:38:21 +0000

    Fixed own made grammar fault in description (-ing again.)

--- src/games/stendhal/server/maps/ados/wall/HolidayingBoyNPC.java
+++ src/games/stendhal/server/maps/ados/wall/HolidayingBoyNPC.java
@@ -67,7 +67,7 @@ public class HolidayingBoyNPC implements ZoneConfigurator {
 		npc.setEntityClass("boynpc");
 		npc.setPosition(114, 77);
 		npc.initHP(100);
-		npc.setDescription("You see Finn Farmer. He is a cute looking boy who is having fun while playing in the backyard.");
+		npc.setDescription("You see Finn Farmer. He is a cute looking boy who has fun while playing in the backyard.");
 		zone.add(npc);
 	}
 }
