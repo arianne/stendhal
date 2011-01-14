7e5c7751fce36589e0d0bfcca20c6fb2cb9046e2
Markus <madmetzger@users.sourceforge.net>
2011-01-14 15:09:55 +0000

    draw an image for the category of the reached achievement

--- src/games/stendhal/client/gui/j2d/AchievementBoxFactory.java
+++ src/games/stendhal/client/gui/j2d/AchievementBoxFactory.java
@@ -13,8 +13,16 @@ import java.awt.RenderingHints;
 import java.awt.Transparency;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Locale;
+
+import javax.imageio.ImageIO;
+
+import org.apache.log4j.Logger;
 
 public class AchievementBoxFactory {
+	private static final String ACHIEVEMENT_IMAGE_FOLDER = "data/sprites/achievements/";
 	/** Background image. */
 	private static final String BACKGROUND = "data/gui/banner_background.png";
 	// These are dependent on the sprite
@@ -39,18 +47,26 @@ public class AchievementBoxFactory {
 	 * @return the drawn sprite
 	 */
 	public Sprite createAchievementBox(String title, String description, String category) {
+		final GraphicsConfiguration gc = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration();
+		// Get the category image
+		// initialize category image with empty image in case loading the image fails
+		BufferedImage categoryImage = gc.createCompatibleImage(32, 32, Transparency.BITMASK);
+		try {
+			categoryImage = ImageIO.read(new File(ACHIEVEMENT_IMAGE_FOLDER+category.toLowerCase(Locale.ENGLISH)+".png"));
+		} catch (IOException e) {
+			Logger.getLogger(AchievementBoxFactory.class).error("Error loading achievement box image.", e);
+		}
 		// Calculate size for the message box
 		Font font = graphics.getFont();
 		Font largeFont = font.deriveFont(20f);
 		Rectangle2D titleRect = largeFont.getStringBounds(title, graphics.getFontRenderContext());
 		Rectangle2D textRect = font.getStringBounds(description, graphics.getFontRenderContext());
-		int width = (int) Math.max(titleRect.getWidth(), textRect.getWidth());
-		int height = (int) (titleRect.getHeight() + textRect.getHeight());
-		width += 2 * SIDE_MARGIN;
+		int width = (int) Math.max(titleRect.getWidth(), textRect.getWidth())+categoryImage.getWidth();
+		int height = (int) Math.max(categoryImage.getHeight(), (titleRect.getHeight() + textRect.getHeight()));
+		width += 3 * SIDE_MARGIN;
 		height += TOP_MARGIN + BOTTOM_MARGIN;
 		
 		// Create the background sprite
-		final GraphicsConfiguration gc = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration();
 		final BufferedImage image = gc.createCompatibleImage(width, height, Transparency.BITMASK);
 		final Graphics2D g2d = image.createGraphics();
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
@@ -59,12 +75,15 @@ public class AchievementBoxFactory {
 		bp.paint(g2d, width, height);
 		
 		// Draw the texts
-		g2d.setColor(Color.BLACK);
+		g2d.setColor(Color.BLACK); 
 		g2d.setFont(largeFont);
-		g2d.drawString(title, SIDE_MARGIN, TOP_MARGIN + (int) titleRect.getHeight());
+		g2d.drawString(title, SIDE_MARGIN + SIDE_MARGIN + categoryImage.getWidth(), TOP_MARGIN + (int) titleRect.getHeight());
 		
 		g2d.setFont(font);
-		g2d.drawString(description, SIDE_MARGIN, height - BOTTOM_MARGIN);
+		g2d.drawString(description, SIDE_MARGIN + SIDE_MARGIN + categoryImage.getWidth(), height - BOTTOM_MARGIN);
+		
+		// Draw the image
+		g2d.drawImage(categoryImage, SIDE_MARGIN, TOP_MARGIN, null);
 		
 		g2d.dispose();
 		
