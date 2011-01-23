7d6b5382547dce92e40dcedba0dc7b0f48d3b86c
Kimmo Rundelin <kiheru@users.sourceforge.net>
2011-01-23 15:42:01 +0000

    Added a convenience scroll pane that hides child component border when scroll bars are not needed

---
+++ src/games/stendhal/client/gui/ScrolledViewport.java
@@ -0,0 +1,83 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.client.gui;
+
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
+
+import javax.swing.JComponent;
+import javax.swing.JScrollPane;
+import javax.swing.border.Border;
+
+/**
+ * A JScrollPane variant that hides the child components border completely when
+ * the scroll bars are not shown.
+ */
+public class ScrolledViewport {
+	final JScrollPane scrollPane;
+	final JComponent view;
+	final Border originalBorder;
+	
+	/**
+	 * Create a new ScrolledViewport.
+	 * 
+	 * @param view child component. The border that view has at the moment when
+	 * 	the ScrolledViewport is created is used when ant least one of the scroll
+	 * 	bars is visible.
+	 */
+	public ScrolledViewport(JComponent view) {
+		this.view = view;
+		originalBorder = view.getBorder();
+		scrollPane = new JScrollPane(view);
+		ComponentListener listener = new ScrollBarVisibilityChangeListener();
+		scrollPane.getHorizontalScrollBar().addComponentListener(listener);
+		scrollPane.getVerticalScrollBar().addComponentListener(listener);
+	}
+	
+	/**
+	 * Get the enclosing component.
+	 * 
+	 * @return JScrollPane containing the view
+	 */
+	public JComponent getComponent() {
+		return scrollPane;
+	}
+	
+	/**
+	 * Listener for following scroll bar visibility changes.
+	 */
+	private class ScrollBarVisibilityChangeListener extends ComponentAdapter {
+		@Override
+		public void componentHidden(ComponentEvent e) {
+			updateBorder();
+		}
+		
+		@Override
+		public void componentShown(ComponentEvent e) {
+			updateBorder();
+		}
+		
+		/**
+		 * Show the border if at least one of the scroll bars is visible, hide
+		 * it otherwise.
+		 */
+		private void updateBorder() {
+			if (scrollPane.getHorizontalScrollBar().isVisible()
+				|| scrollPane.getVerticalScrollBar().isVisible()) {
+				view.setBorder(originalBorder);
+			} else {
+				view.setBorder(null);
+			}
+		}
+	}
+}
