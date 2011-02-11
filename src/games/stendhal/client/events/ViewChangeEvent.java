554d1fc9504cf9a7d2f1a1d19eeb7cc14643e5d0
Kimmo Rundelin <kiheru@users.sourceforge.net>
2011-02-11 19:12:50 +0000

    Modify GameScreen in EDT

--- src/games/stendhal/client/events/ViewChangeEvent.java
+++ src/games/stendhal/client/events/ViewChangeEvent.java
@@ -11,6 +11,8 @@
  ***************************************************************************/
 package games.stendhal.client.events;
 
+import javax.swing.SwingUtilities;
+
 import games.stendhal.client.GameScreen;
 import games.stendhal.client.entity.Entity;
 
@@ -20,9 +22,13 @@ import games.stendhal.client.entity.Entity;
 public class ViewChangeEvent extends Event<Entity> {
 	@Override
 	public void execute() {
-		int x = event.getInt("x");
-		int y = event.getInt("y");
+		final int x = event.getInt("x");
+		final int y = event.getInt("y");
 		
-		GameScreen.get().positionChanged(x, y);
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				GameScreen.get().positionChanged(x, y);
+			}
+		});
 	}
 }
