ade39affbd01ae4cb4d3521fe1837a8f55e405c4
Katie Russell <kymara@users.sourceforge.net>
2011-02-12 14:11:07 +0000

    add oni raid

---
+++ src/games/stendhal/server/script/OniRaid.java
@@ -0,0 +1,37 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.script;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author kymara
+ * 
+ * Less safe for players below level 70
+ */
+public class OniRaid extends CreateRaid {
+
+	@Override
+	protected Map<String, Integer> createArmy() {
+		final Map<String, Integer> attackArmy = new HashMap<String, Integer>();
+		attackArmy.put("oni warrior", 7);
+		attackArmy.put("oni archer", 5);
+		attackArmy.put("oni queen", 3);
+		attackArmy.put("oni king", 3);
+		attackArmy.put("oni priest", 6);
+
+
+		return attackArmy;
+	}
+}
