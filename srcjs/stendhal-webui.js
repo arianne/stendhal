81b49a69a4f8fbaf77e439d1d6bf1c8d26a00d20
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-02-19 21:34:54 +0000

    chatbar with history

--- srcjs/stendhal-webui.js
+++ srcjs/stendhal-webui.js
@@ -14,9 +14,83 @@ stendhal.ui = {
 		}
 	},
 
-	/**
-	 * chat and eventlog
-	 */
+
+
+	//*************************************************************************
+	//                                 Chat Bar                                
+	//*************************************************************************
+
+
+	chatBar: {
+		history: [],
+		historyIndex: 0,
+
+		clear: function() {
+			document.getElementById('chatbar').value = '';
+		},
+
+		fromHistory: function(i) {
+			this.historyIndex = this.historyIndex + i;
+			if (this.historyIndex < 0) {
+				this.historyIndex = 0;
+			}
+			if (this.historyIndex >= this.history.length) {
+				this.historyIndex = this.history.length;
+				this.clear();
+			} else {
+				document.getElementById('chatbar').value = this.history[this.historyIndex];
+			}
+		},
+
+		keydown: function(e) {
+			var event = e
+			if (!event) {
+				event = window.event;
+			}
+			if (event.shiftKey) {
+				var code;
+				if (event.which) {
+					code = event.which;
+				} else {
+					code = e.keyCode;
+				}
+				if (code == 38) {
+					stendhal.ui.chatBar.fromHistory(-1);
+				} else if (code == 40){
+					stendhal.ui.chatBar.fromHistory(1);
+				}
+			}
+		},
+		remember: function(text) {
+			if (this.history.length > 100) {
+				this.history.shift();
+			}
+			this.history[this.history.length] = text;
+			this.historyIndex = this.history.length;
+		},
+
+		send: function() {
+			var val = document.getElementById('chatbar').value;
+			var array = val.split(" ");
+			if (array[0] == "/choosecharacter") {
+				marauroa.clientFramework.chooseCharacter(array[1]);
+			} else if (val == '/close') {
+				marauroa.clientFramework.close();
+			} else {
+				if (stendhal.slashActionRepository.execute(val)) {
+					this.remember(val);
+				}
+			}
+			this.clear();
+		}
+	},
+
+
+
+	//*************************************************************************
+	//                                 Chat Log                                
+	//*************************************************************************
+
 	chatLog: {
 		addLine: function(type, msg) {
 			var e = document.createElement('p');
