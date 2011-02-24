6b4c98ba2ca1d6b4f179ee5151b9b898f8236080
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-02-24 19:12:48 +0000

    backported merge of santa and bunny achievement and move to friend category (by kymara)

--- src/games/stendhal/server/core/engine/StendhalPlayerDatabase.java
+++ src/games/stendhal/server/core/engine/StendhalPlayerDatabase.java
@@ -135,7 +135,8 @@ public class StendhalPlayerDatabase {
 			+ "'age.week.one', 'age.month.one', 'age.month.two', 'age.month.three', "
 			+ "'age.month.four', 'age.month.five', 'age.month.six', 'age.month.seven', "
 			+ "'age.month.eight', 'age.month.nine', 'age.month.ten', 'age.month.eleven', "
-			+ "'age.year.one', 'quest.special.dm.025', 'quest.special.susi', 'item.produce.flour')", null);
+			+ "'age.year.one', 'quest.special.dm.025', 'quest.special.susi', 'item.produce.flour', "
+			+ "'quest.special.santa', 'quest.special.bunny'", null);
 		transaction.execute("UPDATE achievement SET identifier='xp.level.010' WHERE identifier='xp.level.10'", null);
 		transaction.execute("UPDATE achievement SET identifier='xp.level.050' WHERE identifier='xp.level.50'", null);
 	}
--- src/games/stendhal/server/core/rp/achievement/factory/QuestAchievementFactory.java
+++ src/games/stendhal/server/core/rp/achievement/factory/QuestAchievementFactory.java
@@ -22,54 +22,48 @@ import java.util.LinkedList;
 import java.util.List;
 /**
  * Factory for quest achievements
- *  
+ *
  * @author madmetzger
  */
 public class QuestAchievementFactory extends AbstractAchievementFactory {
-	
+
 	@Override
 	public Collection<Achievement> createAchievements() {
 		List<Achievement> questAchievements = new LinkedList<Achievement>();
 		//daily monster quest achievements
 		questAchievements.add(createAchievement("quest.special.daily.0010", "Semos' Protector", "Finish daily monster quest 10 times",
 												Achievement.EASY_BASE_SCORE, new QuestStateGreaterThanCondition("daily", 2, 9)));
-		questAchievements.add(createAchievement("quest.special.daily.0050", "Semos' Guardian", "Finish daily monster quest 50 times", 
+		questAchievements.add(createAchievement("quest.special.daily.0050", "Semos' Guardian", "Finish daily monster quest 50 times",
 												Achievement.EASY_BASE_SCORE, new QuestStateGreaterThanCondition("daily", 2, 49)));
-		questAchievements.add(createAchievement("quest.special.daily.0100", "Semos' Hero", "Finish daily monster quest 100 times", 
+		questAchievements.add(createAchievement("quest.special.daily.0100", "Semos' Hero", "Finish daily monster quest 100 times",
 												Achievement.MEDIUM_BASE_SCORE, new QuestStateGreaterThanCondition("daily", 2, 99)));
-		questAchievements.add(createAchievement("quest.special.daily.0250", "Semos' Champion", "Finish daily monster quest 250 times", 
+		questAchievements.add(createAchievement("quest.special.daily.0250", "Semos' Champion", "Finish daily monster quest 250 times",
 												Achievement.MEDIUM_BASE_SCORE, new QuestStateGreaterThanCondition("daily", 2, 249)));
-		questAchievements.add(createAchievement("quest.special.daily.0500", "Semos' Vanquisher", "Finish daily monster quest 500 times", 
+		questAchievements.add(createAchievement("quest.special.daily.0500", "Semos' Vanquisher", "Finish daily monster quest 500 times",
 												Achievement.HARD_BASE_SCORE, new QuestStateGreaterThanCondition("daily", 2, 499)));
-		
+
 		//elf princess quest achievement
-		questAchievements.add(createAchievement("quest.special.elf_princess.0025", "Faiumoni's Casanova", "Finish elf princess quest 25 times", 
+		questAchievements.add(createAchievement("quest.special.elf_princess.0025", "Faiumoni's Casanova", "Finish elf princess quest 25 times",
 												Achievement.MEDIUM_BASE_SCORE, new QuestStateGreaterThanCondition("elf_princess", 2, 24)));
-		
+
 		//Maze
-		questAchievements.add(createAchievement("quest.special.maze", "Pathfinder", "Finish the maze", 
+		questAchievements.add(createAchievement("quest.special.maze", "Pathfinder", "Finish the maze",
 												Achievement.EASY_BASE_SCORE, new QuestStateGreaterThanCondition("maze", 2, 0)));
-		// Meet Santa Claus
-		questAchievements.add(createAchievement("quest.special.santa", "Santa likes me!", "Find Santa Claus",
-												Achievement.EASY_BASE_SCORE, new QuestWithPrefixCompletedCondition("meet_santa_")));
-		// Meet Easter Bunny
-		questAchievements.add(createAchievement("quest.special.bunny", "Happy Easter, Bunny!", "Find Easter Bunny",
-												Achievement.EASY_BASE_SCORE, new QuestWithPrefixCompletedCondition("meet_bunny_")));
 
 		// Ados Deathmatch
-		// disabled. Currently the wrong index is being checked (it would be index 6) 
+		// disabled. Currently the wrong index is being checked (it would be index 6)
 		// and as per bug report https://sourceforge.net/tracker/?func=detail&aid=3148365&group_id=1111&atid=101111 the count is not saved anyway
 		// questAchievements.add(createAchievement("quest.special.dm.025", "Gladiator", "Fight 25 Deathmatches",
 		//		Achievement.HARD_BASE_SCORE, new QuestStateGreaterThanCondition("deathmatch", 1, 24)));
-		
+
 		// have completed all quests in Semos City?
 		questAchievements.add(createAchievement("quest.special.semos", "Aide to Semos folk", "Complete all quests in Semos City",
 				Achievement.MEDIUM_BASE_SCORE, new QuestsInRegionCompletedCondition(Region.SEMOS_CITY)));
-		
+
 		// have completed all quests in Ados City?
 		questAchievements.add(createAchievement("quest.special.ados", "Helper of Ados city dwellers", "Complete all quests in Ados City",
 				Achievement.MEDIUM_BASE_SCORE, new QuestsInRegionCompletedCondition(Region.ADOS_CITY)));
-		
+
 		return questAchievements;
 	}
 
