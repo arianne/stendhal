0e34019089d3fe6ae0f7664b6e71545934af64eb
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-02-28 23:20:28 +0000

    parent class for stendhal "rpobjects" is "entity".

--- srcjs/stendhal-entities.js
+++ srcjs/stendhal-entities.js
@@ -2,22 +2,38 @@ marauroa.rpobjectFactory.entity = function() {}
 marauroa.rpobjectFactory.entity.minimapStyle = "rgb(200,255,200)";
 marauroa.rpobjectFactory.entity.prototype = marauroa.rpobjectFactory._default;
 
+marauroa.rpobjectFactory.item = function() {
+	this.minimapStyle = "rgb(0,255,0)";
+};
+marauroa.rpobjectFactory.item.prototype = new marauroa.rpobjectFactory.entity;
+
 marauroa.rpobjectFactory.player = function() {
 	this.minimapStyle = "rgb(255,255,255)";
 };
-marauroa.rpobjectFactory.player.prototype = marauroa.rpobjectFactory._default;
+marauroa.rpobjectFactory.player.prototype = new marauroa.rpobjectFactory.entity;
 
 marauroa.rpobjectFactory.creature = function() {
-	this.minimapStyle = "rgb(255,0,0)";
+	this.minimapStyle = "rgb(255,255,0)";
 };
-marauroa.rpobjectFactory.creature.prototype = marauroa.rpobjectFactory._default;
+marauroa.rpobjectFactory.creature.prototype = new marauroa.rpobjectFactory.entity;
 
 marauroa.rpobjectFactory.portal = function() {
 	this.minimapStyle = "rgb(0,0,0)";
 };
-marauroa.rpobjectFactory.portal.prototype = marauroa.rpobjectFactory._default;
+marauroa.rpobjectFactory.portal.prototype = new marauroa.rpobjectFactory.entity;
 
 marauroa.rpobjectFactory.npc = function() {
 	this.minimapStyle = "rgb(0,0,255)";
 };
-marauroa.rpobjectFactory.npc.prototype = marauroa.rpobjectFactory._default;
+marauroa.rpobjectFactory.npc.prototype = new marauroa.rpobjectFactory.entity;
+
+
+
+marauroa.rpobjectFactory.createRPObject = function(rpclass) {
+	var ctor = this.entity;
+	if (typeof(this[rpclass]) != "undefined") {
+		ctor = this[rpclass];
+	}
+	return new ctor;
+}
+
