a7edc8e514f509262d4d7b0122e234e7e66e0e50
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-02-28 20:53:44 +0000

    workaround for nested prototype issue

--- srcjs/stendhal-entities.js
+++ srcjs/stendhal-entities.js
@@ -1,24 +1,23 @@
-marauroa.rpobjectFactory.entity = function() {
-	this.minimapStyle = "rgb(128,128,128)";
-};
+marauroa.rpobjectFactory.entity = function() {}
+marauroa.rpobjectFactory.entity.minimapStyle = "rgb(128,128,128)";
 marauroa.rpobjectFactory.entity.prototype = marauroa.rpobjectFactory._default;
 
 marauroa.rpobjectFactory.player = function() {
 	this.minimapStyle = "rgb(0,0,0)";
 };
-marauroa.rpobjectFactory.player.prototype = marauroa.rpobjectFactory.entity;
+marauroa.rpobjectFactory.player.prototype = marauroa.rpobjectFactory._default;
 
 marauroa.rpobjectFactory.creature = function() {
 	this.minimapStyle = "rgb(255,255,0)";
 };
-marauroa.rpobjectFactory.creature.prototype = marauroa.rpobjectFactory.entity;
+marauroa.rpobjectFactory.creature.prototype = marauroa.rpobjectFactory._default;
 
 marauroa.rpobjectFactory.food = function() {
 	this.minimapStyle = "rgb(255,0,0)";
 };
-marauroa.rpobjectFactory.food.prototype = marauroa.rpobjectFactory.entity;
+marauroa.rpobjectFactory.food.prototype = marauroa.rpobjectFactory._default;
 
 marauroa.rpobjectFactory.npc = function() {
 	this.minimapStyle = "rgb(0,0,255)";
 };
-marauroa.rpobjectFactory.npc.prototype = marauroa.rpobjectFactory.entity;
+marauroa.rpobjectFactory.npc.prototype = marauroa.rpobjectFactory._default;
