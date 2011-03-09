0323237102dfe587e800c8ef18598d56c04ffc3b
Kimmo Rundelin <kiheru@users.sourceforge.net>
2011-03-09 19:40:21 +0000

    Added an interface for listening to entities' slot content changes

---
+++ src/games/stendhal/client/entity/ContentChangeListener.java
@@ -0,0 +1,34 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.client.entity;
+
+import marauroa.common.game.RPSlot;
+
+/**
+ * Listener for content changes in an Entity's slots.
+ */
+public interface ContentChangeListener {
+	/**
+	 * Called when entities are added in slots, or the attributes of the
+	 * entities are changed.
+	 * 
+	 * @param added object changes
+	 */
+	void contentAdded(RPSlot added);
+	/**
+	 * Called when entities are removed from slots, of attributes of the
+	 * entities are removed.
+	 * 
+	 * @param removed object changes
+	 */
+	void contentRemoved(RPSlot removed);
+}
