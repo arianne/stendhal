69cc2b8cd902b6757cab9d1cc32ed2561692e258
Bluelads4 <bluelads99@users.sourceforge.net>
2011-03-13 22:47:43 +0000

    Added Mircea, Imorgens mother into Imorgens house (quest will be made soon)

---
+++ src/games/stendhal/server/maps/fado/forest/OldWitchNPC.java
@@ -0,0 +1,67 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.maps.fado.forest;
+
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+
+import java.util.Map;
+
+/**
+ * Creates Mircea, an old witch who lives in Imorgens house, Fado forest.
+ *
+ * @author Vanessa Julius
+ */
+public class OldWitchNPC implements ZoneConfigurator {
+
+	/**
+	 * Configure a zone.
+	 *
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		buildNPC(zone, attributes);
+	}
+
+	private void buildNPC(final StendhalRPZone zone, final Map<String, String> attributes) {
+		final SpeakerNPC npc = new SpeakerNPC("Mircea") {
+
+			@Override
+			protected void createPath() {
+				setPath(null);
+			
+			}
+			
+
+			@Override
+			protected void createDialog() {
+				addGreeting("Hello *Cough*");
+				addJob("I had been a powerful witch before I risked too much and lost my powers while fighting against some #werwolfes.");
+				addReply("werwolfes", "They are really dangerous! I met some on the way to Kikareukin earlier and have been deeply poisoned. That is why I have to rest here.");
+				addQuest("Oh dear, you seem to be really nice and trustful but I can't give you any work at the moment.");
+				addHelp("My daughter #Imorgen outside can maybe help you. She knows lots of people around Faiumoni.");
+				addReply("Imorgen", "She will hopefully become a powerful witch like me in the future. But she still has to learn a lot.");
+				addOffer("I would like to sell you one of my famous drinks but I can't *cough*.");
+				addGoodbye("I hope we'll see us again soon *sigh*.");
+			}
+		};
+
+		npc.setEntityClass("oldwitchnpc");
+		npc.setPosition(12, 4);
+		npc.initHP(100);
+		npc.setDescription("You see Mircea. She is an old asthenic witch who seems to become weaken each second.");
+		zone.add(npc);
+	}
+}
