4548cc606edf0a01aed73cd91ebf6b2f52802ceb
Bluelads4 <bluelads99@users.sourceforge.net>
2011-03-13 20:52:11 +0000

    Added new NPC Imorgen, a young witch... She will placed into Fado forrest soon

---
+++ src/games/stendhal/server/maps/fado/forest/WitchNPC.java
@@ -0,0 +1,73 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.maps.fado.forest;
+
+import games.stendhal.common.Direction;
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.RPEntity;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+
+import java.util.Map;
+
+/**
+ * Creates Imorgen, a young witch who lives in Fado Forest.
+ *
+ * @author Vanessa Julius
+ */
+public class WitchNPC implements ZoneConfigurator {
+
+	/**
+	 * Configure a zone.
+	 *
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		buildNPC(zone, attributes);
+	}
+
+	private void buildNPC(final StendhalRPZone zone, final Map<String, String> attributes) {
+		final SpeakerNPC npc = new SpeakerNPC("Imorgen") {
+
+			@Override
+			protected void createPath() {
+				setPath(null);
+			
+			}
+			
+			protected void onGoodbye(RPEntity player) {
+				setDirection(Direction.DOWN);
+			}
+
+			@Override
+			protected void createDialog() {
+				addGreeting("Hello wanderer!");
+				addJob("I'm a witch and pratice each day to become a powerful one soon.");
+				addQuest("My grandma inside is quite concerned... She is ill and afraid and I need to help her and will figure out, #how...");
+				addReply("how", "The question really is what may help here... Maybe I'll ask her later about it, I first have to finish my new #receipe...");
+				addReply("receipe", "Oh I doubt that you can eat or drink what I prepare at the moment... But I know two really nice women who can make some tasty #soup for you.");
+				addReply("soup", "Florence Boullabaisse and Mother Helena are awesome soup cooks. You'll find them on the Ados market and in the Fado tavern.");
+				addHelp("Aldrin sells some honey, you will love to eat it with bread.");
+				addOffer("I'm sorry but I can't sell you anything.");
+				addGoodbye("Bye bye and take care around here!");
+			}
+		};
+
+		npc.setEntityClass("youngwitchnpc");
+		npc.setPosition(59, 29);
+		npc.initHP(100);
+		npc.setDescription("You see Imorgin. She is a young witch whose mystical aura can be felt over a long distance.");
+		zone.add(npc);
+	}
+}
