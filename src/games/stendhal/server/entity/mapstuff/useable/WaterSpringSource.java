a327c78776b1b133df6a6b1a2cc2954d8d63ed36
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-03-27 11:48:04 +0000

    converted WaterSpringSource to UseableEntity

--- src/games/stendhal/server/entity/mapstuff/useable/WaterSpringSource.java
+++ src/games/stendhal/server/entity/mapstuff/useable/WaterSpringSource.java
@@ -48,11 +48,11 @@ public class WaterSpringSource extends PlayerActivityEntity {
 	 * Create a water spring source.
 	 */
 	public WaterSpringSource() {
-		setRPClass("water_source");
-		put("type", "water_source");
-
 		setDescription("You see some bubbles in the water. Seems like you found a spring.");
 		setResistance(0);
+		setMenu("Fill");
+		put("class", "source");
+		put("name", "water_source");
 	}
 	
 	/**
