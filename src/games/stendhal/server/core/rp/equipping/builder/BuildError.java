e242b247aab6754b4eb313649080970633be4650
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-05-01 13:34:00 +0000

    a builder that just sets an error flag

---
+++ src/games/stendhal/server/core/rp/equipping/builder/BuildError.java
@@ -0,0 +1,29 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2011 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.core.rp.equipping.builder;
+
+import games.stendhal.server.core.rp.equipping.EquipmentActionData;
+import games.stendhal.server.entity.player.Player;
+import marauroa.common.game.RPAction;
+
+/**
+ * builds an error
+ *
+ * @author hendrik
+ */
+public class BuildError implements PartialBuilder {
+
+	public void build(EquipmentActionData data, Player player, RPAction action) {
+		data.setErrorMessage("");
+	}
+
+}
