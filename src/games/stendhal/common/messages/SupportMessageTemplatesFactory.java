752d9bd8580fc7da25dad585a2b687341d927b13
Katie Russell <kymara@users.sourceforge.net>
2011-05-04 20:27:00 +0000

    add new support answer templates

--- src/games/stendhal/common/messages/SupportMessageTemplatesFactory.java
+++ src/games/stendhal/common/messages/SupportMessageTemplatesFactory.java
@@ -45,6 +45,7 @@ public class SupportMessageTemplatesFactory {
 		addTemplate("$faqsocial", "Hi %s, sorry to hear about that. But unfortunately support is not here to help you with social problems unless it gets way out of hand. This issue is discussed further on the stendhal FAQ and how to deal with it is described there. Please open the FAQ via #/faq and read carefully #http://stendhalgame.org/wiki/StendhalFAQ#Player_social_problems - hopefully the rest of your Stendhal experience will be more pleasant.");
 		addTemplate("$firewallserver", "Hi %s, I am sorry but we cannot help you with the configuration of your router or firewall. It is rather dangerous to modify those settings without knowing exactly what you are doing. So this should only be done by an experienced network administrator who will find instructions in the manual that came with the hardware router or operating system.");
 		addTemplate("$gmreview", "Hi %s, that issue is closed for discussion with live support as it is more complex. Please email #gm-review@stendhalgame.org to discuss that issue.");
+		addTemplate("$gmreviewreply", "Hi %s, email responses from #gm-review@stendhalgame.org can take up to four weeks.");
 		addTemplate("$ignore","Hi %s, sorry to hear that you have had some problems with another player. Please try to ignore them. You can use #/ignore #playername to prevent chat messages.");
 		addTemplate("$investigation","Hi %s, Stendhal is a game developed and run by volunteers. These kinds of investigations waste a lot of time. We're sorry but we cannot take this issue further.");
 		addTemplate("$knownbug","Hi %s, thank you for telling us about this bug, we have found it ourselves too and it's already reported. Thank you though and please do keep reporting bugs if you see them!");
@@ -52,6 +53,7 @@ public class SupportMessageTemplatesFactory {
 		addTemplate("$password","Hi %s, you can change your password by logging in on #http://stendhalgame.org. When you are logged in on that page you will find a link called change password in the upper right area of the page. There you can change your password.");
 		addTemplate("$rules","Hi %s, please read the Stendhal Rules at #http://stendhalgame.org/wiki/StendhalRuleSystem - thank you.");
 		addTemplate("$spam","Hi %s, repeatedly saying the same thing over and over again is considered spamming, and this is against the rules of the game. Please do not spam, and please read #http://stendhalgame.org/wiki/StendhalRuleSystem, thank you.");
+		addTemplate("$spamsupport","Hi %s, we use a special tool for support, so you don't have to worry about messages being lost. Please note that sending a lot of support messages will lower the priority of your query.");
 		addTemplate("$thief","Hi %s, please ask Dagobert about #trading to learn how to trade safely and securely. Support cannot recover items lost through carelessness.");
 		addTemplate("$wiki","Hi %s, this is a question which is answered on the Stendhal wiki, please look on #http://stendhalgame.org/wiki/Stendhal as this is full of useful information. Thanks for playing Stendhal.");
 	}
