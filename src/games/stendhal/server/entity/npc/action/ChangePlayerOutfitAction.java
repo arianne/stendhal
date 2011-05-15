387970406e30e520400a41e42e3a3ed2e54a3ed0
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-05-15 08:57:37 +0000

    code cleanup

--- src/games/stendhal/server/entity/npc/action/ChangePlayerOutfitAction.java
+++ src/games/stendhal/server/entity/npc/action/ChangePlayerOutfitAction.java
@@ -23,60 +23,57 @@ import org.apache.commons.lang.builder.HashCodeBuilder;
 
 
 /**
- * 
- * @author jackrabbit
- *
  *	Modify parts or all of a players outfit.
+ 
+ * @author jackrabbit
  */
 public class ChangePlayerOutfitAction implements ChatAction {
 
 	private final Outfit outfitChange;
-	private final boolean removeOrAdd;
-	
+	private final boolean addOutfit;
+
 	/**
 	 * Creates a new ChangePlayerOutfitAction
 	 * 
 	 * @param outfit
 	 * 			the (part of) the outfit to be changed (i.e. removed or added)
-	 * @param removeOrAdd
-	 * 			removeOrAdd = true: Add (part of) the outfit to the players current outfit
-	 * 			removeOrAdd = false: Remove (part of) the outfit the player currently wears
+	 * @param addOutfit
+	 * 			addOutfit = true: Add (part of) the outfit to the players current outfit
+	 * 			addOutfit = false: Remove (part of) the outfit the player currently wears
 	 */
-	public ChangePlayerOutfitAction(Outfit outfit, boolean removeOrAdd) {
+	public ChangePlayerOutfitAction(Outfit outfit, boolean addOutfit) {
 		this.outfitChange = outfit;
-		this.removeOrAdd = removeOrAdd;
+		this.addOutfit = addOutfit;
 	}
-	
-	
+
 	public void fire(final Player player, final Sentence sentence, final EventRaiser npc) {
 		final Outfit outfit = player.getOutfit();
-		
+
 		// Depending on if you want to add or remove part of the outfit...
-		if (removeOrAdd == true) {
+		if (addOutfit) {
 			//... either put the new outfit over the old one
 			// and set the players outfit to the combination
 			Outfit tempOutfit = this.outfitChange.putOver(outfit);
 			player.setOutfit(tempOutfit, true);
-		}
-		else {
+		} else {
 			//... or remove (parts of) the players outfit
 			// and set the players outfit to this combination
 			Outfit tempOutfit = outfit.removeOutfit(this.outfitChange);
 			player.setOutfit(tempOutfit, true);
 		}
 	}
-	
+
 	public String toString() {
 		return "Outfit used for changing: " + outfitChange.toString()
 		+ " If false it should be romved, if true it should be added from/to the current outfit: "
-		+ removeOrAdd;
+		+ addOutfit;
 	}
-	
+
 	@Override
 	public int hashCode() {
 		return HashCodeBuilder.reflectionHashCode(this);
 	}
-	
+
 	public boolean equals(final Object obj) {
 		if (this == obj) {
 			return true;
@@ -91,7 +88,7 @@ public class ChangePlayerOutfitAction implements ChatAction {
 		if (outfitChange != other.outfitChange) {
 			return false;
 		}
-		if (removeOrAdd != other.removeOrAdd) {
+		if (addOutfit != other.addOutfit) {
 			return false;
 		}
 		return true;
