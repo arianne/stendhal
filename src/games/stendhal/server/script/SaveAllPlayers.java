1d2750b2c23db3ef84c11171fa1fa028b86565db
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-06-12 17:39:45 +0000

    a script to trigger saving of all online players

---
+++ src/games/stendhal/server/script/SaveAllPlayers.java
@@ -0,0 +1,50 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2011 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.script;
+
+import games.stendhal.server.core.scripting.ScriptImpl;
+import games.stendhal.server.entity.player.Player;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import marauroa.server.game.container.PlayerEntry;
+import marauroa.server.game.container.PlayerEntryContainer;
+
+import org.apache.log4j.Logger;
+
+/**
+ * save all players
+ *
+ * @author hendrik
+ */
+public class SaveAllPlayers extends ScriptImpl {
+	private static Logger logger = Logger.getLogger(SaveAllPlayers.class);
+
+	@Override
+	public void execute(Player admin, List<String> args) {
+		super.execute(admin, args);
+		for (PlayerEntry entry : PlayerEntryContainer.getContainer()) {
+			try {
+				entry.storeRPObject(entry.object);
+			} catch (SQLException e) {
+				logger.error(e, e);
+			} catch (IOException e) {
+				logger.error(e, e);
+			}
+		}
+	}
+
+	
+}
