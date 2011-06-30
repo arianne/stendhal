ff8e29eb35528b7d4642268acca5accb6268cfba
Markus <madmetzger@users.sourceforge.net>
2011-06-30 19:42:55 +0000

    implement transaction aggreed method

--- src/games/stendhal/server/entity/npc/behaviour/impl/RepairerBehaviour.java
+++ src/games/stendhal/server/entity/npc/behaviour/impl/RepairerBehaviour.java
@@ -1,13 +1,18 @@
 package games.stendhal.server.entity.npc.behaviour.impl;
 
 import games.stendhal.common.grammar.ItemParserResult;
+import games.stendhal.common.parser.Sentence;
+import games.stendhal.server.entity.Entity;
 import games.stendhal.server.entity.item.Item;
+import games.stendhal.server.entity.npc.ChatAction;
+import games.stendhal.server.entity.npc.ChatCondition;
 import games.stendhal.server.entity.npc.EventRaiser;
+import games.stendhal.server.entity.npc.action.SayTextWithPlayerNameAction;
 import games.stendhal.server.entity.npc.behaviour.impl.prices.PriceCalculationStrategy;
 import games.stendhal.server.entity.player.Player;
 
-import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Behaviour for NPCs repairing items
@@ -22,9 +27,10 @@ public class RepairerBehaviour extends TransactionBehaviour {
 	 * Create a new RepairerBehaviour with a given price calculation strategy
 	 * 
 	 * @param calculator the price calculator
+	 * @param items the items that can be repaired
 	 */
-	public RepairerBehaviour(PriceCalculationStrategy calculator) {
-		super(new HashSet<String>());
+	public RepairerBehaviour(PriceCalculationStrategy calculator, Set<String> items) {
+		super(items);
 		priceCalculator = calculator;
 	}
 
@@ -39,19 +45,79 @@ public class RepairerBehaviour extends TransactionBehaviour {
 					int price = priceCalculator.calculatePrice(item, player);
 					if (player.isEquipped("money", price)) {
 						item.repair();
+						player.drop("money", price);
 						return true;
+					} else {
+						seller.say("You cannot afford to repair your "+res.getChosenItemName());
+						return false;
 					}
+				} else {
+					seller.say("Your "+res.getChosenItemName()+" is not damaged.");
+					return false;
 				}
 			} else {
-				//TODO think about reasonable behaviour here? repair twice and account twice? check how many items are to repair in there?
-				return false;
+				Item toRepair = equipped.iterator().next();
+				for(Item i : equipped) {
+					if(i.getDeterioration() > toRepair.getDeterioration()) {
+						toRepair = i;
+					}
+				}
+				if(toRepair.getDeterioration() > 0) {
+					int price = priceCalculator.calculatePrice(toRepair, player);
+					if (player.isEquipped("money", price)) {
+						toRepair.repair();
+						player.drop("money", price);
+						seller.say("You do carry more than one "+res.getChosenItemName()+" with you. So I repaired the most damaged one.");
+						return true;
+					}
+					seller.say("You cannot afford to repair your "+res.getChosenItemName());
+					return false;
+				} else {
+					seller.say("Your "+res.getChosenItemName()+" is not damaged.");
+					return false;
+				}
 			}
 		}
+		seller.say("You do not carry a "+res.getChosenItemName()+" with you.");
 		return false;
 	}
 	
+	@Override
+	public ChatCondition getTransactionCondition() {
+		return new ChatCondition() {
+			public boolean fire(Player player, Sentence sentence, Entity npc) {
+				ItemParserResult res = parse(sentence);
+				return canDealWith(res.getChosenItemName());
+			}
+		};
+	}
+
+	@Override
+	public ChatAction getRejectedTransactionAction() {
+		return new SayTextWithPlayerNameAction("I am sorry, [name], but I cannot repair your item.");
+	}
+
+	/**
+	 * Calculate the price for the given item
+	 * @param item the item to repair
+	 * @param player the player wanting to repair
+	 * @return the price for the player
+	 */
 	public int getPrice(String item, Player player) {
 		return priceCalculator.calculatePrice(item, player);
+	}
+
+	/**
+	 * Check if this NPC can repair this item
+	 * 
+	 * @param chosen the item to repair
+	 * @return true iff this NPC is able to repair the item
+	 */
+	public boolean canDealWith(String chosen) {
+		if(!getItemNames().isEmpty()) {
+			return getItemNames().contains(chosen);
+		}
+		return true;
 	}
 
 }
