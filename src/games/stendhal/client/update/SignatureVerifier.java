52383dd85c5ef89a37cba872634b51b6e4fa2eaf
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-07-12 20:21:10 +0000

    SignatureVerifier can now handle hexadecimal strings with an uneven number of digits

--- src/games/stendhal/client/update/SignatureVerifier.java
+++ src/games/stendhal/client/update/SignatureVerifier.java
@@ -103,8 +103,17 @@ public class SignatureVerifier {
 	}
 
 	// http://stackoverflow.com/questions/140131/convert-a-string-representation-of-a-hex-dump-to-a-byte-array-using-java/140861#140861
-	private static byte[] hexStringToByteArray(String s) {
+	static byte[] hexStringToByteArray(String hexString) {
+		String s = hexString;
+
+		// handle uneven number of hex digits
 		int len = s.length();
+		if (len % 2 == 1) {
+			s = "0" + s;
+			len++;
+		}
+
+		// decode
 		byte[] data = new byte[len / 2];
 		for (int i = 0; i < len; i += 2) {
 			data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character
