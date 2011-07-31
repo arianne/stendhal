a59f23f14394139eeed0ad1b0af51939eacd2c3e
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-07-31 22:12:40 +0000

    load language files manually instead of using Properties.load to support unicode and don't require spaces to be escaped

--- src/games/stendhal/server/core/engine/Translate.java
+++ src/games/stendhal/server/core/engine/Translate.java
@@ -11,10 +11,12 @@
  ***************************************************************************/
 package games.stendhal.server.core.engine;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 
 import marauroa.common.io.UnicodeSupportingInputStreamReader;
 
@@ -40,14 +42,30 @@ public class Translate {
 		init("de");
 	}
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
+	/**
+	 * loads a lauguage file
+	 *
+	 * @param language language
+	 */
 	private static void init(String language) {
 		try {
-			UnicodeSupportingInputStreamReader reader = new UnicodeSupportingInputStreamReader(Translate.class.getClassLoader().getResourceAsStream("data/language/" + language + ".txt"));
-			Properties prop = new Properties();
-			prop.load(reader);
+			InputStream is = Translate.class.getClassLoader().getResourceAsStream("data/language/" + language + ".txt");
+			BufferedReader reader = new BufferedReader(new UnicodeSupportingInputStreamReader(is));
+
+			// we cannot use Properties.load because that does a) not support unicode and b) splits on the first space
+			Map<String, String> map = new HashMap<String, String>();
+			String line = reader.readLine();
+			while (line != null) {
+				String[] tokens = line.split("=", 2);
+				if (tokens.length != 2) {
+					System.err.println(line + "///" + Arrays.toString(tokens));
+				} else {
+					map.put(tokens[0], tokens[1]);
+				}
+				line = reader.readLine();
+			}
 			reader.close();
-			texts.put(language, new HashMap(prop));
+			texts.put(language, map);
 		} catch (IOException e) {
 			logger.error(e, e);
 		}
