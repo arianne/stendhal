738b1c82b804cdafc42cfd1b52d2cdafb61dfa9e
Kimmo Rundelin <kiheru@users.sourceforge.net>
2011-08-12 23:02:23 +0000

    Added an always false condition

---
+++ src/games/stendhal/server/entity/npc/condition/AlwaysFalseCondition.java
@@ -0,0 +1,45 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2011 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.entity.npc.condition;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import games.stendhal.common.parser.Sentence;
+import games.stendhal.server.entity.Entity;
+import games.stendhal.server.entity.npc.ChatCondition;
+import games.stendhal.server.entity.player.Player;
+
+/**
+ * This condition returns always false.
+ */
+public class AlwaysFalseCondition implements ChatCondition {
+	public boolean fire(final Player player, final Sentence sentence, final Entity entity) {
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "false";
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj, false,
+				AlwaysTrueCondition.class);
+	}
+}
