78fb0ff6b3b9f9fb561a549d027e5b55e2a3e54b
Kimmo Rundelin <kiheru@users.sourceforge.net>
2011-09-01 21:09:37 +0000

    Added a test for colors

--- tests/games/stendhal/server/actions/OutfitActionTest.java
+++ tests/games/stendhal/server/actions/OutfitActionTest.java
@@ -14,6 +14,7 @@ package games.stendhal.server.actions;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import games.stendhal.server.entity.player.Player;
 import marauroa.common.game.RPAction;
@@ -73,4 +74,30 @@ public class OutfitActionTest {
 		assertEquals("invalid player outfit", "1", player.get("outfit"));
 	}
 
+	/**
+	 * Tests colors
+	 */
+	@Test
+	public void testColors() {
+		final OutfitAction oa = new OutfitAction();
+		final Player player = PlayerTestHelper.createPlayer("player");
+		final RPAction action = new RPAction();
+		assertNotNull(player.get("outfit"));
+		action.put("value", 1);
+		action.put("hair", 0xfeed);
+		action.put("dress", 0xf00d);
+		// valid color attribute, but not settable by the player
+		action.put("detail", 0xbadf00d);
+		oa.onAction(player, action);
+		assertTrue(player.has("outfit"));
+		assertEquals("1", player.get("outfit"));
+
+		action.put("value", 51515151);
+		oa.onAction(player, action);
+		assertTrue(player.has("outfit"));
+		assertEquals("invalid player outfit", "1", player.get("outfit"));
+		assertEquals("invalid hair color", 0xfeed, player.getInt("outfit_colors", "hair"));
+		assertEquals("invalid dress color", 0xf00d, player.getInt("outfit_colors", "dress"));
+		assertFalse("invalid attribute", player.has("outfit_colors", "detail"));
+	}
 }
