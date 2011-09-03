edcb09b24db51eb007441686aa6015d4949099ad
Kimmo Rundelin <kiheru@users.sourceforge.net>
2011-09-03 18:51:27 +0000

    Added a HSL space color selector

---
+++ src/games/stendhal/client/gui/ColorSelector.java
@@ -0,0 +1,404 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2011 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.client.gui;
+
+import games.stendhal.client.gui.j2d.Blend;
+import games.stendhal.client.gui.layout.SBoxLayout;
+import games.stendhal.client.gui.layout.SLayout;
+import games.stendhal.client.gui.styled.Style;
+import games.stendhal.client.gui.styled.StyleUtil;
+import games.stendhal.client.sprite.Sprite;
+import games.stendhal.client.sprite.SpriteStore;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.colorchooser.ColorSelectionModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**
+ * A HSL space color selector that should be small enough to fit in the outfit
+ * selection dialog.
+ */
+class ColorSelector extends JPanel {
+	private static final String HUE_SATURATION_IMAGE = "data/gui/colors.png";
+	final HSLSelectionModel model;
+	final JComponent hueSaturationSelector;
+	final JComponent lightnessSelector;
+
+
+	/**
+	 * Create a new ColorSelector.
+	 */
+	ColorSelector() {
+		model = new HSLSelectionModel();
+		setBorder(null);
+		setLayout(new SBoxLayout(SBoxLayout.VERTICAL, SBoxLayout.COMMON_PADDING));
+		hueSaturationSelector = new HueSaturationSelector(model);
+		add(hueSaturationSelector);
+		lightnessSelector = new LightnessSelector(model);
+		add(lightnessSelector, SBoxLayout.constraint(SLayout.EXPAND_X));
+	}
+
+	@Override
+	public void setEnabled(boolean enabled) {
+		super.setEnabled(enabled);
+		hueSaturationSelector.setEnabled(enabled);
+		lightnessSelector.setEnabled(enabled);
+	}
+
+	/**
+	 * Get the selection model.
+	 * 
+	 * @return selection model
+	 */
+	ColorSelectionModel getSelectionModel() {
+		return model;
+	}
+
+	/**
+	 * Base class for the color selector sliders.
+	 */
+	private static abstract class Selector extends JComponent implements ChangeListener {
+		/** Model to adjust and listen to. */
+		final HSLSelectionModel model;
+
+		/**
+		 * Create a new Selector.
+		 * 
+		 * @param model selection model
+		 */
+		Selector(HSLSelectionModel model) {
+			this.model = model;
+			model.addChangeListener(this);
+			setOpaque(true);
+			applyStyle();
+			addMouseMotionListener(new MouseMotionAdapter() {
+				@Override
+				public void mouseDragged(MouseEvent ev) {
+					if (isEnabled()) {
+						select(ev.getPoint());
+					}
+				}
+			});
+
+			addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent ev) {
+					if (isEnabled()) {
+						select(ev.getPoint());
+					}
+				}
+			});
+		}
+
+		public void stateChanged(ChangeEvent e) {
+			// Colors changed
+			repaint();
+		}
+
+		/**
+		 * Apply Stendhal style.
+		 */
+		private void applyStyle() {
+			Style style = StyleUtil.getStyle();
+			if (style != null) {
+				setBorder(style.getBorderDown());
+			}
+		}
+
+		/**
+		 * User clicked a point, or dragged the adjuster to it. The component
+		 * should recalculate the colors.
+		 * 
+		 * @param point
+		 */
+		abstract void select(Point point);
+	}
+
+	/**
+	 * Hue-Saturation part of the selector component.
+	 */
+	private static class HueSaturationSelector extends Selector {
+		Sprite hueSprite;
+
+		/**
+		 * Create a new HueSaturationSelector.
+		 * 
+		 * @param model selection model. Should be the same as for the whole
+		 * 	selector
+		 */
+		HueSaturationSelector(HSLSelectionModel model) {
+			super(model);
+		}
+
+		/**
+		 * Get the color gradient sprite.
+		 * 
+		 * @return background sprite
+		 */
+		private Sprite getHueSprite() {
+			if (hueSprite == null) {
+				hueSprite = SpriteStore.get().getSprite(HUE_SATURATION_IMAGE);
+			}
+
+			return hueSprite;
+		}
+
+		@Override
+		public Dimension getPreferredSize() {
+			Sprite s = getHueSprite();
+			int width = s.getWidth();
+			int height = s.getHeight();
+			Insets ins = getInsets();
+			width += ins.left + ins.right;
+			height += ins.top + ins.bottom;
+			return new Dimension(width, height);
+		}
+
+		@Override
+		public void paintComponent(Graphics g) {
+			Insets ins = getInsets();
+			Sprite sprite = getHueSprite();
+			sprite.draw(g, ins.left, ins.right);
+
+			// draw a cross
+			g.setColor(Color.BLACK);
+			int x = (int) (model.getHue() * sprite.getWidth()) + ins.left;
+			int y = (int) ((1f - model.getSaturation()) * sprite.getHeight()) + ins.left;
+			g.drawLine(x, 0, x, getHeight());
+			g.drawLine(0, y, getWidth(), y);
+		}
+
+		@Override
+		void select(Point point) {
+			Insets ins = getInsets();
+			Sprite sprite = getHueSprite();
+			int width = sprite.getWidth();
+			int height = sprite.getHeight();
+			int xDiff = point.x - ins.left;
+			xDiff = Math.min(width, Math.max(0, xDiff));
+			float hue = xDiff / (float) width;
+			int yDiff = point.y - ins.top;
+			yDiff = Math.min(height, Math.max(0, yDiff));
+			float saturation = 1f - yDiff / (float) height;
+			model.setHS(hue, saturation);
+		}
+	}
+
+	/**
+	 * Lightness part of the selector.
+	 */
+	private static class LightnessSelector extends Selector {
+		/** Height of the gradient bar. */
+		private static final int BAR_HEIGHT = 10;
+
+		/**
+		 * Create a new LightnessSelector.
+		 * 
+		 * @param model selection model. Should be the same as for the entire
+		 * 	selector component.
+		 */
+		LightnessSelector(HSLSelectionModel model) {
+			super(model);
+		}
+
+		@Override
+		public Dimension getPreferredSize() {
+			Insets ins = getInsets();
+			// insane value, but we do not actually use it
+			int width = BAR_HEIGHT + ins.left + ins.right;
+			int height = BAR_HEIGHT + ins.top + ins.bottom;
+			return new Dimension(width, height);
+		}
+
+		@Override
+		public void paintComponent(Graphics g) {
+			Insets ins = getInsets();
+			int width = getWidth() - ins.left - ins.right;
+			int height = getWidth() - ins.left - ins.right;
+
+			// calculate start and end colors
+			float[] hsl = new float[3];
+			int[] rgb = new int[4];
+			rgb[0] = 0xff; // alpha
+
+			hsl[0] = model.getHue();
+			// to compensate for the incorrect color mode (see below), we adjust
+			// the saturation a bit. This results in imaginary oversaturated
+			// colors but the color space transformations can take care of that.
+			hsl[1] = 5f * model.getSaturation();
+			// 0 would be black, and have no color
+			hsl[2] = 0.08f;
+			Blend.hsl2rgb(hsl, rgb);
+			Color startColor = new Color(Blend.mergeRgb(rgb));
+			// 1 would be white, and have no color
+			hsl[2] = 0.92f;
+			Blend.hsl2rgb(hsl, rgb);
+			Color endColor = new Color(Blend.mergeRgb(rgb));
+			/*
+			 * A gradient paint is a bit fake, as it won't use the same color
+			 * model for the shift as we actually do. However, that should not 
+			 * be a problem because we never show the user the actual selected
+			 * color, so she won't be able to see the slight difference.
+			 */
+			Graphics2D g2d = (Graphics2D) g;
+			GradientPaint p = new GradientPaint(ins.left, ins.top, startColor, width, ins.top, endColor);
+			g2d.setPaint(p);
+			g2d.fillRect(ins.left, ins.top, width, height);
+
+			// Draw a line. white is not visible on black, and the vice versa,
+			// so draw them both
+			g.setColor(Color.BLACK);
+			int x = (int) (model.getLightness() * width) + ins.left;
+			g.drawLine(x, 0, x, getHeight());
+			g.setColor(Color.WHITE);
+			x++;
+			g.drawLine(x, 0, x, getHeight());
+		}
+
+		@Override
+		void select(Point point) {
+			Insets ins = getInsets();
+			int width = getWidth() - ins.left - ins.right;
+			int xDiff = point.x - ins.left;
+			xDiff = Math.min(width, Math.max(0, xDiff));
+			float lightness = xDiff / (float) width;
+			/*
+			 * Limit lightness a bit, so that the gradient does not become
+			 * confusingly desaturated at the ends.
+			 */
+			lightness = Math.max(0.01f, Math.min(0.99f, lightness));
+			model.setL(lightness);
+		}
+	}
+
+	/**
+	 * Color selection model that is capable of returning, and accepting HSL
+	 * space color data in addition of the usual RGB.
+	 */
+	private static class HSLSelectionModel implements ColorSelectionModel {
+		/** Listeners following this model. */
+		private List<ChangeListener> listeners = new ArrayList<ChangeListener>();
+		/** Current color. */
+		private Color color;
+		/** Current color in HSL space. */
+		private float[] hsl = new float[3];
+
+		public void addChangeListener(ChangeListener listener) {
+			listeners.add(listener);
+		}
+
+		public Color getSelectedColor() {
+			return color;
+		}
+
+		public void removeChangeListener(ChangeListener listener) {
+			listeners.remove(listener);
+		}
+
+		public void setSelectedColor(Color color) {
+			if (color != null) {
+				this.color = color;
+			} else {
+				// Something with a sane lightness value
+				this.color = Color.GRAY;
+			}
+			int[] rgb = new int[4];
+			Blend.splitRgb(this.color.getRGB(), rgb);
+			Blend.rgb2hsl(rgb, hsl);
+			fireChanged();
+		}
+
+		/**
+		 * Set hue and saturation.
+		 * 
+		 * @param hue
+		 * @param saturation
+		 */
+		void setHS(float hue, float saturation) {
+			hsl[0] = hue;
+			hsl[1] = saturation;
+			updateColor();
+		}
+
+		/**
+		 * Set lightness
+		 * 
+		 * @param lightness
+		 */
+		void setL(float lightness) {
+			hsl[2] = lightness;
+			updateColor();
+		}
+
+		/**
+		 * Get hue.
+		 * 
+		 * @return hue
+		 */
+		float getHue() {
+			return hsl[0];
+		}
+
+		/**
+		 * Get saturation.
+		 * 
+		 * @return saturation
+		 */
+		float getSaturation() {
+			return hsl[1];
+		}
+
+		/**
+		 * Get lightness.
+		 * 
+		 * @return lightness
+		 */
+		float getLightness() {
+			return hsl[2];
+		}
+
+		/**
+		 * Recalculate color based on the HSL data.
+		 */
+		private void updateColor() {
+			int[] rgb = new int[4];
+			Blend.hsl2rgb(hsl, rgb);
+			rgb[0] = 0xff;
+			color = new Color(Blend.mergeRgb(rgb));
+			fireChanged();
+		}
+
+		/**
+		 * Notify listeners about changed color.
+		 */
+		private void fireChanged() {
+			for (ChangeListener listener : listeners) {
+				listener.stateChanged(new ChangeEvent(this));
+			}
+		}
+	}
+}
