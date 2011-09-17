1501ae0a93782ac953085d8dd0ef28e13711ad85
yoriy <yoriy@users.sourceforge.net>
2011-09-17 20:28:06 +0000

    added scripts for handling ThePiedPiper quest state

---
+++ src/games/stendhal/server/script/TPPNextPhase.java
@@ -0,0 +1,45 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.script;
+
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPRuleProcessor;
+import games.stendhal.server.core.events.TurnNotifier;
+import games.stendhal.server.core.rp.StendhalQuestSystem;
+import games.stendhal.server.core.scripting.ScriptImpl;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.maps.quests.IQuest;
+import games.stendhal.server.maps.quests.ThePiedPiper;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import marauroa.common.game.RPSlot;
+
+/**
+ * Showing what is current ThePiedPiper quest state, and when it will switch to next.
+ * 
+ * @author yoriy
+ */
+public class TPPNextPhase extends ScriptImpl {
+
+	@Override
+	public void execute(final Player admin, final List<String> args) {
+
+		//ThePiedPiper TPP = (ThePiedPiper) StendhalQuestSystem.get().getQuest("ThePiedPiper");
+        ThePiedPiper.switchToNextPhase();
+		final String Phase = ThePiedPiper.getPhase().toString();
+        admin.sendPrivateText("Switched to "+Phase);
+	};
+}
---
+++ src/games/stendhal/server/script/TPPShowQuestState.java
@@ -0,0 +1,57 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.script;
+
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPRuleProcessor;
+import games.stendhal.server.core.events.TurnNotifier;
+import games.stendhal.server.core.rp.StendhalQuestSystem;
+import games.stendhal.server.core.scripting.ScriptImpl;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.maps.quests.IQuest;
+import games.stendhal.server.maps.quests.ThePiedPiper;
+import games.stendhal.server.util.TimeUtil;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import marauroa.common.game.RPSlot;
+
+/**
+ * Showing what is current ThePiedPiper quest state, and when it will switch to next.
+ * 
+ * @author yoriy
+ */
+public class TPPShowQuestState extends ScriptImpl {
+
+	@Override
+	public void execute(final Player admin, final List<String> args) {
+		
+		StringBuilder sb = new StringBuilder();
+		ThePiedPiper TPP = (ThePiedPiper) StendhalQuestSystem.get().getQuest("ThePiedPiper");
+		sb.append("The Pied Piper quest state:\n");
+        sb.append("Quest phase: "+ThePiedPiper.getPhase().toString()+"\n");
+		sb.append("Next phase : "+ThePiedPiper.getNextPhase(ThePiedPiper.getPhase()).toString()+"\n");
+		int turns=TPP.getRemainingTurns();
+		int seconds=TPP.getRemainingSeconds();
+		sb.append("Remaining turns: ");
+		sb.append(turns);
+		sb.append("\n");
+		sb.append("Remaining seconds: ");
+        sb.append(seconds);
+        sb.append(" ("+TimeUtil.timeUntil(seconds, true)+")");
+		//sb.append("\n");
+        admin.sendPrivateText(sb.toString());
+	};
+}
