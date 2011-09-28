c8fc2e0bcd86255458e90137f984d64f0761ccc2
Markus <madmetzger@users.sourceforge.net>
2011-09-28 17:15:13 +0000

    added new achievement

--- src/games/stendhal/server/core/rp/achievement/factory/ObtainAchievementsFactory.java
+++ src/games/stendhal/server/core/rp/achievement/factory/ObtainAchievementsFactory.java
@@ -2,6 +2,7 @@ package games.stendhal.server.core.rp.achievement.factory;
 
 import games.stendhal.server.core.rp.achievement.Achievement;
 import games.stendhal.server.core.rp.achievement.Category;
+import games.stendhal.server.entity.npc.condition.PlayerHasHarvestedNumberOfItemsGreaterThanCondition;
 import games.stendhal.server.entity.npc.condition.PlayerHasObtainedNumberOfItemsFromWellGreaterThanCondition;
 import games.stendhal.server.entity.npc.condition.QuestCompletedCondition;
 
@@ -26,6 +27,9 @@ public class ObtainAchievementsFactory extends AbstractAchievementFactory {
 		List<Achievement> achievements = new LinkedList<Achievement>();
 		achievements.add(createAchievement("obtain.wish", "A wish came true", "Get an item from the wishing well",
 				Achievement.EASY_BASE_SCORE, true, new PlayerHasObtainedNumberOfItemsFromWellGreaterThanCondition(0)));
+		achievements.add(createAchievement("obtain.harvest.vegetable", "Vegetarian", "Harvest 3 of all vegetables in Faiumoni.",
+				Achievement.EASY_BASE_SCORE, true, new PlayerHasHarvestedNumberOfItemsGreaterThanCondition(3, "salad",
+														"spinach", "tomato", "leek", "courgette", "broccoli","carrot")));
 		//ultimate collector quest achievement
 		achievements.add(createAchievement("quest.special.collector", "Ultimate Collector", "Finish ultimate collector quest",
 												Achievement.HARD_BASE_SCORE, true, new QuestCompletedCondition("ultimate_collector")));
