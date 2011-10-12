6188e565d736769c9a7c792abe4c237d03517e53
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-10-12 18:57:45 +0000

    fixed Excellent Stuff and Farmer achievements (by kymara)

--- src/games/stendhal/server/core/rp/achievement/factory/ObtainAchievementsFactory.java
+++ src/games/stendhal/server/core/rp/achievement/factory/ObtainAchievementsFactory.java
@@ -29,7 +29,7 @@ public class ObtainAchievementsFactory extends AbstractAchievementFactory {
 				Achievement.EASY_BASE_SCORE, true, new PlayerHasObtainedNumberOfItemsFromWellGreaterThanCondition(0)));
 		achievements.add(createAchievement("obtain.harvest.vegetable", "Farmer", "Harvest 3 of all vegetables that grow in Faiumoni.",
 				Achievement.EASY_BASE_SCORE, true, new PlayerHasHarvestedNumberOfItemsGreaterThanCondition(3, "carrot",
-						"salad", "broccoli", "cauliflower", "chinese cabbage","leek", "onion", "courgette", "spinach", "collard", "garlic", "artichoke")));
+						"salad", "broccoli", "cauliflower", "leek", "onion", "courgette", "spinach", "collard", "garlic", "artichoke")));
 		//ultimate collector quest achievement
 		achievements.add(createAchievement("quest.special.collector", "Ultimate Collector", "Finish ultimate collector quest",
 				Achievement.HARD_BASE_SCORE, true, new QuestCompletedCondition("ultimate_collector")));
