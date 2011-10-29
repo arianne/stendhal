445c3dec8c0f7b0c20f25bec9035fdf4344c96cd
Markus <madmetzger@users.sourceforge.net>
2011-10-29 23:10:45 +0000

    add a script to enable a feature for a player

---
+++ src/games/stendhal/server/script/EnableFeature.java
@@ -0,0 +1,27 @@
+package games.stendhal.server.script;
+
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.scripting.ScriptImpl;
+import games.stendhal.server.entity.npc.action.EnableFeatureAction;
+import games.stendhal.server.entity.player.Player;
+
+import java.util.List;
+/**
+ * Script to enable a feature like keyring for a player
+ *
+ * @author madmetzger
+ */
+public class EnableFeature extends ScriptImpl {
+
+	@Override
+	public void execute(final Player admin, final List<String> args) {
+		if(args != null && args.size() != 2) {
+			admin.sendPrivateText("Usage of EnableFeature: [player] [feature]");
+		}
+		final String feature = args.get(1);
+		final String name = args.get(0);
+		final Player player = SingletonRepository.getRuleProcessor().getPlayer(name);
+		new EnableFeatureAction(feature).fire(player, null, null);
+	}
+
+}
