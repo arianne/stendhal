081a49610b6b5e0ec66e64c8b4030fad98225daa
Markus <madmetzger@users.sourceforge.net>
2011-10-30 10:35:48 +0000

    add a functionality to disable a feature

---
+++ src/games/stendhal/server/entity/npc/action/DisableFeatureAction.java
@@ -0,0 +1,52 @@
+package games.stendhal.server.entity.npc.action;
+
+import games.stendhal.common.parser.Sentence;
+import games.stendhal.server.entity.npc.ChatAction;
+import games.stendhal.server.entity.npc.EventRaiser;
+import games.stendhal.server.entity.player.Player;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+/**
+ * Action to disable a feature
+ * 
+ * @author madmetzger
+ */
+public class DisableFeatureAction implements ChatAction {
+
+	private final String feature;
+
+	/**
+	 * Creates a new DisableFeatureAction.
+	 * 
+	 * @param feature name of feature
+	 */
+	public DisableFeatureAction(final String feature) {
+		this.feature = feature;
+	}
+
+	public void fire(final Player player, final Sentence sentence, final EventRaiser npc) {
+		player.setFeature(this.feature, null);
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder();
+		sb.append("disable feature <");
+		sb.append(this.feature);
+		sb.append(">");
+		return sb.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj, false,
+				DisableFeatureAction.class);
+	}
+
+}
---
+++ src/games/stendhal/server/script/DisableFeature.java
@@ -0,0 +1,27 @@
+package games.stendhal.server.script;
+
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.scripting.ScriptImpl;
+import games.stendhal.server.entity.npc.action.DisableFeatureAction;
+import games.stendhal.server.entity.player.Player;
+
+import java.util.List;
+/**
+ * Script to disable a feature like keyring for a player
+ *  
+ * @author madmetzger
+ */
+public class DisableFeature extends ScriptImpl {
+
+	@Override
+	public void execute(final Player admin, final List<String> args) {
+		if(args != null && args.size() != 2) {
+			admin.sendPrivateText("Usage of DisableFeature: [player] [feature]");
+		}
+		final String feature = args.get(1);
+		final String name = args.get(0);
+		final Player player = SingletonRepository.getRuleProcessor().getPlayer(name);
+		new DisableFeatureAction(feature).fire(player, null, null);
+	}
+
+}
