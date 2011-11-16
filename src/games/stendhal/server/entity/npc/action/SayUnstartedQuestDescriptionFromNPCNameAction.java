3b1908a502486d4e23486e377ad55b954b427bf3
Katie Russell <kymara@users.sourceforge.net>
2011-11-16 22:59:42 +0000

    if the npc name said was one matching an unstarted quest in that region, say the quest description (or list them if there is more than one)

---
+++ src/games/stendhal/server/entity/npc/action/SayUnstartedQuestDescriptionFromNPCNameAction.java
@@ -0,0 +1,66 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2011 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.entity.npc.action;
+
+import java.util.List;
+
+import games.stendhal.common.grammar.Grammar;
+import games.stendhal.common.parser.Sentence;
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.entity.npc.ChatAction;
+import games.stendhal.server.entity.npc.EventRaiser;
+import games.stendhal.server.entity.player.Player;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+/**
+ * Gives description for unstarted quest based on npc name
+ */
+public class SayUnstartedQuestDescriptionFromNPCNameAction implements ChatAction {
+	private final String region;
+
+	/**
+	 * Creates a new SayUnstartedQuestDescriptionFromNPCNameAction.
+	 * 
+	 * @param region region of NPC
+	 */
+	public SayUnstartedQuestDescriptionFromNPCNameAction(String region) {
+		this.region = region;
+	}
+
+	public void fire(final Player player, final Sentence sentence, final EventRaiser raiser) {
+		List<String> npcnames = SingletonRepository.getStendhalQuestSystem().getNPCNamesForUnstartedQuestsInRegionForLevel(player, region);
+		for (String name :  npcnames) {
+			if (name.equalsIgnoreCase(sentence.getTriggerExpression().toString())) {
+				raiser.say(Grammar.enumerateCollection(SingletonRepository.getStendhalQuestSystem().getQuestDescriptionForUnstartedQuestInRegionFromNPCName(player,region,name)));
+			}
+		} 	
+	}
+
+	@Override
+	public String toString() {
+		return "SayUnstartedQuestDescriptionFromNPCNameAction in region <" + region +  ">";
+	}
+
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj, false,
+				SayUnstartedQuestDescriptionFromNPCNameAction.class);
+	}
+}
