3758281918a0600076784597870554c3cccff4e8
Markus <madmetzger@users.sourceforge.net>
2011-11-17 18:05:15 +0000

    deactivate item damaging

--- src/games/stendhal/server/entity/item/Item.java
+++ src/games/stendhal/server/entity/item/Item.java
@@ -31,13 +31,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.apache.log4j.Logger;
-
 import marauroa.common.game.Definition;
 import marauroa.common.game.Definition.Type;
 import marauroa.common.game.RPClass;
 import marauroa.common.game.RPObject;
 import marauroa.common.game.RPSlot;
+
+import org.apache.log4j.Logger;
 
 /**
  * This is an item.
@@ -59,7 +59,7 @@ public class Item extends PassiveEntity implements TurnListener, EquipListener {
 	/**
 	 * mean time an item is constantly usable in hours
 	 */
-	private static final long MEAN_LIFETIME = 12 * MathHelper.MILLISECONDS_IN_ONE_HOUR;
+	private static final long MEAN_LIFETIME = 1 * MathHelper.MILLISECONDS_IN_ONE_HOUR;
 	
 	// 10 minutes
 	public static final int DEGRADATION_TIMEOUT = 10 * MathHelper.SECONDS_IN_ONE_MINUTE;
@@ -159,7 +159,7 @@ public class Item extends PassiveEntity implements TurnListener, EquipListener {
 		entity.addAttribute("def", Type.SHORT, Definition.HIDDEN);
 
 		//Some items can be damaged in combat or during use. This rises the degree of deterioration
-		entity.addAttribute("deterioration", Type.INT, (byte) (Definition.HIDDEN | Definition.VOLATILE));
+		entity.addAttribute("deterioration", Type.INT, Definition.HIDDEN);
 
 		// Some items(food) have amount of something
 		// (a bottle, a piece of meat).
@@ -304,7 +304,7 @@ public class Item extends PassiveEntity implements TurnListener, EquipListener {
 		double propabilityForMeanExp = Rand.propabilityForMeanExp(MEAN_LIFETIME / 300 * this.getAttackRate());
 		if(Rand.flipCoin(propabilityForMeanExp) && getDeterioration() <= MAX_DETERIORATION) {
 			Logger.getLogger(getClass()).debug("The item"+ this.getName() +"deteriorated from "+this.getDeterioration()+".");
-			this.add("deterioration", 1);
+//			this.add("deterioration", 1);
 		} else {
 			Logger.getLogger(getClass()).debug("The item"+ this.getName() +"did not deteriorate from "+this.getDeterioration()+".");
 		}
