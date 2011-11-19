3c1a283a0124c1b436d537bfe785ae7cc4a5c693
Markus <madmetzger@users.sourceforge.net>
2011-11-19 15:19:08 +0000

    add slowdown spell

---
+++ src/games/stendhal/server/entity/spell/SlowDownSpell.java
@@ -0,0 +1,31 @@
+package games.stendhal.server.entity.spell;
+
+import games.stendhal.common.constants.Nature;
+import games.stendhal.server.entity.Entity;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.entity.spell.effect.ModifySpeedEffect;
+import marauroa.common.game.RPObject;
+/**
+ * Slow down spell 
+ * 
+ * @author madmetzger
+ */
+public class SlowDownSpell extends Spell {
+
+	public SlowDownSpell(final String name, final Nature nature, final int amount, final int atk,
+			final int cooldown, final int def, final double lifesteal, final int mana,
+			final int minimumlevel, final int range, final int rate, final int regen, double modifier) {
+		super(name, nature, amount, atk, cooldown, def, lifesteal, mana, minimumlevel,
+				range, rate, regen, modifier);
+	}
+
+	public SlowDownSpell(final RPObject object) {
+		super(object);
+	}
+
+	@Override
+	protected void doEffects(final Player caster, final Entity target) {
+		new ModifySpeedEffect(getNature(), getAmount(), getAtk(), getDef(), getLifesteal(), getRate(), getRegen(), getModifier()).act(caster, target);
+	}
+
+}
---
+++ src/games/stendhal/server/entity/spell/effect/ModifySpeedEffect.java
@@ -0,0 +1,29 @@
+package games.stendhal.server.entity.spell.effect;
+
+import games.stendhal.common.constants.Nature;
+import games.stendhal.server.entity.Entity;
+import games.stendhal.server.entity.GuidedEntity;
+import games.stendhal.server.entity.player.Player;
+
+import java.util.Date;
+/**
+ * Slow down an entity
+ * @author madmetzger
+ */
+public class ModifySpeedEffect extends AbstractEffect {
+
+	public ModifySpeedEffect(final Nature nature, final int amount, final int atk, final int def,
+			final double lifesteal, final int rate, final int regen, double modifier) {
+		super(nature, amount, atk, def, lifesteal, rate, regen, modifier);
+	}
+
+	public void act(final Player caster, final Entity target) {
+		actInternal(caster, (GuidedEntity) target);
+	}
+
+	private void actInternal(Player caster, GuidedEntity target) {
+		Date expire = new Date(System.currentTimeMillis() + getAmount()*1000);
+		target.addSpeedModifier(expire, getModifier());
+	}
+
+}
