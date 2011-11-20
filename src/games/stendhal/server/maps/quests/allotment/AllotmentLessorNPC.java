93a5dc16388fb1758f945224730c536d30510edc
filinep <filinep@users.sourceforge.net>
2011-11-20 15:30:25 +0000

    Added an allotment lessor and a utility class to aid in allotment rentals, wip.

---
+++ src/games/stendhal/server/maps/quests/allotment/AllotmentLessorNPC.java
@@ -0,0 +1,179 @@
+package games.stendhal.server.maps.quests.allotment;
+
+import games.stendhal.common.MathHelper;
+import games.stendhal.common.parser.ExpressionType;
+import games.stendhal.common.parser.JokerExprMatcher;
+import games.stendhal.common.parser.Sentence;
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.item.GateKey;
+import games.stendhal.server.entity.item.Item;
+import games.stendhal.server.entity.npc.ChatAction;
+import games.stendhal.server.entity.npc.ConversationPhrases;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.EventRaiser;
+import games.stendhal.server.entity.npc.action.SetQuestAction;
+import games.stendhal.server.entity.npc.condition.*;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.player.Player;
+ 
+import java.util.Arrays;
+import java.util.Map;
+ 
+/**
+ * Builds an allotment lessor NPC for Semos. 
+ *
+ * @author kymara
+ */
+public class AllotmentLessorNPC implements ZoneConfigurator {
+	private static String QUEST_SLOT = "allotment rental";
+	private AllotmentUtilities rentHelper;
+ 
+	/**
+	 * Configure a zone.
+	 *
+	 * @param zone
+	 *            The zone to be configured.
+	 * @param attributes
+	 *            Configuration attributes.
+	 */
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		rentHelper = AllotmentUtilities.get();
+		buildNPC(zone, attributes);
+	}
+ 
+	private void buildNPC(final StendhalRPZone zone, final Map<String, String> attributes) {
+		final SpeakerNPC npc = new SpeakerNPC("Jef's_twin") {
+
+			@Override
+			protected void createPath() {
+				/*final List<Node> nodes = new LinkedList<Node>();
+
+				nodes.add(new Node(70, 19));
+                nodes.add(new Node(86,19));
+                nodes.add(new Node(86,3));
+                nodes.add(new Node(87,3));
+                nodes.add(new Node(87,19));
+                nodes.add(new Node(106,19));
+                nodes.add(new Node(106,3));
+                nodes.add(new Node(107,3));
+                nodes.add(new Node(107,19));
+                nodes.add(new Node(69,19));
+
+				setPath(new FixedPath(nodes, true));*/
+				setPath(null);
+			}
+ 
+			@Override
+			protected void createDialog() {
+				addGreeting("Heya!");
+				addJob("Um, not sure what you mean. Right now I'm waiting for my mum to get back from the #shops.");
+				addHelp("I have some #news about the bazaar over there.");
+				addOffer("I don't sell stuff, I'm just waiting for my mum. But I have some #news if you wanna hear it.");
+				// quest: FindJefsMom , quest sentence given there
+				addReply("news", "Some more shopkeepers will be at the market soon! It'll be cool, it's kind of empty round here at the moment.");
+				addReply("shops", "Yeah she's had to go out of town. All we have here is that flower seller! There's #news about our bazaar, though ...");
+				addGoodbye("See you around.");
+
+				// if player already has one rented ask how may help
+				add(ConversationStates.ATTENDING,
+						Arrays.asList("rent", "allotment"),
+						new QuestActiveCondition(QUEST_SLOT),
+						ConversationStates.QUEST_STARTED,
+						"So what can I do you for? I hope the ground you rented is working well for you.",
+						null);
+				
+				// if allotment not rented and there are available then offer one or say none available
+				add(ConversationStates.ATTENDING,
+					Arrays.asList("rent", "allotment"),
+					new QuestNotActiveCondition("QUEST_SLOT"),
+					ConversationStates.QUEST_OFFERED,
+					null,
+					new ChatAction() {
+						public void fire(Player player, Sentence sentence, EventRaiser npc) {
+							String reply = "";
+							for (String s : rentHelper.getAvailableAllotments(zone.getName())) {
+								reply += s + ", ";
+							}
+						
+							if (reply.length() > 0) {
+								npc.say("Would you like to rent one of the allotments? Allotments " + reply + "are available.");
+							} else {
+								npc.say("Unfortunately there aren't any allotments available at the moment. Please come back later.");
+								npc.setCurrentState(ConversationStates.ATTENDING);
+							}
+						}
+				});
+				
+				// if offer rejected
+				add(ConversationStates.QUEST_OFFERED,
+					ConversationPhrases.NO_MESSAGES,
+					null,
+					ConversationStates.ATTENDING,
+					"Ok, how else may I help you?",
+					new SetQuestAction(QUEST_SLOT, 0, "rejected"));
+				
+				// if accepts to rent allotment
+				add(ConversationStates.QUEST_OFFERED,
+					ConversationPhrases.YES_MESSAGES,
+					null,
+					ConversationStates.QUESTION_1,
+					"Which one?",
+					null);
+				
+				// do business
+				add(ConversationStates.QUESTION_1,
+					"",
+					new TextHasNumberCondition(),
+					ConversationStates.ATTENDING, 
+					null,
+					new ChatAction() {
+						public void fire(Player player, Sentence sentence, EventRaiser npc) {
+							final int number = sentence.getNumeral().getAmount();
+							final String allotmentNumber = Integer.toString(number);
+							
+							if (!rentHelper.hasAllotment(zone.getName(), allotmentNumber)) {
+								npc.say("I'm sorry, that allotment does not exist, which one would you like?");
+							} else {
+								if (rentHelper.getAvailableAllotments(zone.getName()).contains(allotmentNumber)) {
+									if(rentHelper.setExpirationTime(zone.getName(), allotmentNumber)) {
+										npc.say("Here's your key");
+										player.equipToInventoryOnly(rentHelper.getKey(zone.getName(), allotmentNumber));
+									} else {
+										npc.say("Uh oh!");
+										// error?
+									}
+								} else {
+									npc.say("I'm sorry, that allotment is already taken.");
+								}
+							}
+						}					
+					}
+				);
+				
+				//for testing purposes
+				add(ConversationStates.ATTENDING, "key", null, ConversationStates.ATTENDING, null, new ChatAction() {
+					public void fire(Player player, Sentence sentence, EventRaiser npc) {
+						final Item key = SingletonRepository.getEntityManager().getItem("gate key");
+						
+						((GateKey) key).setup("Allotment 0", MathHelper.MILLISECONDS_IN_ONE_MINUTE + System.currentTimeMillis());
+						if (player.equipToInventoryOnly(key)) {
+							npc.say("Here's a key");
+						} else {
+							npc.say("Nope sorry");
+						}
+
+						rentHelper.setExpirationTime(zone.getName(), "0");
+					}
+				});
+			}
+		};
+ 
+		npc.setEntityClass("kid6npc");
+		npc.setPosition(85, 11);
+		npc.initHP(100);
+		npc.setDescription("You see jefs clone. He seems like waiting for someone.");
+		zone.add(npc);
+	}
+}
---
+++ src/games/stendhal/server/maps/quests/allotment/AllotmentUtilities.java
@@ -0,0 +1,313 @@
+package games.stendhal.server.maps.quests.allotment;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import games.stendhal.common.MathHelper;
+import games.stendhal.common.filter.FilterCriteria;
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.events.TurnListener;
+import games.stendhal.server.entity.Entity;
+import games.stendhal.server.entity.item.GateKey;
+import games.stendhal.server.entity.mapstuff.ExpirationTracker;
+import games.stendhal.server.entity.mapstuff.area.Allotment;
+import games.stendhal.server.entity.mapstuff.portal.Gate;
+import games.stendhal.server.entity.player.Player;
+
+public class AllotmentUtilities implements TurnListener {
+	/**
+	 * Prefix that allotment names have
+	 */
+	private static String ALLOTMENT_PREFIX = "Allotment ";
+	
+	/**
+	 * Time interval to check if any allotment rentals have expired
+	 */
+	private static int EXPIRATION_CHECKING_PERIOD = MathHelper.SECONDS_IN_ONE_MINUTE;
+
+	/**
+	 * Amount of time which an allotment can be rented out
+	 */
+	private static long RENTAL_TIME = MathHelper.MILLISECONDS_IN_ONE_MINUTE * 2;
+	
+	/**
+	 * 
+	 */
+	private static int WARN_TIME = MathHelper.SECONDS_IN_ONE_MINUTE / 2;
+
+	/**
+	 * List of zones which have allotments that can be rented
+	 */
+	private static String[] zones = { "0_semos_mountain_w2" };
+
+	/**
+	 * Singleton instance to return
+	 */
+	private static AllotmentUtilities instance = null;
+
+	/**
+	 * Singleton getter
+	 * 
+	 * @return the instance of this singleton
+	 */
+	public static AllotmentUtilities get() {
+		if (instance == null) {
+			instance = new AllotmentUtilities();
+		}
+
+		return instance;
+	}
+
+	/**
+	 * Private constructor, starts periodically checking for rental expirations
+	 */
+	private AllotmentUtilities() {
+		SingletonRepository.getTurnNotifier().notifyInSeconds(1, this);
+	}
+
+	/**
+	 * Gets the specified zone
+	 * 
+	 * @param zoneName the name of the zone to get.
+	 * @return the zone with the given name
+	 */
+	private StendhalRPZone getZone(final String zoneName) {
+		return (StendhalRPZone) SingletonRepository.getRPWorld().getRPZone(zoneName);
+	}
+
+	/**
+	 * Helper method to get all allotments in a given zone
+	 * 
+	 * @param zoneName the zone for which to get allotments
+	 * @return a list of allotments in the zone
+	 */
+	private List<Entity> getAllotments(final String zoneName) {
+		return getZone(zoneName).getFilteredEntities(new FilterCriteria<Entity>() {
+			public boolean passes(Entity o) {
+				return o instanceof Allotment;
+			}
+		});
+	}
+
+	/**
+	 * Helper method to get all gates in a given zone
+	 * 
+	 * @param zoneName the zone for which to get gates
+	 * @return a list of gates in the zone
+	 */
+	private List<Entity> getGates(final String zoneName) {
+		return getZone(zoneName).getFilteredEntities(new FilterCriteria<Entity>() {
+			public boolean passes(Entity o) {
+				return o instanceof Gate;
+			}
+		});
+	}
+	
+	/**
+	 * Helper method to get all expiration trackers in a given zone
+	 * 
+	 * @param zoneName the zone for which to get trackers
+	 * @return a list of trackers in the zone
+	 */
+	private List<Entity> getTrackers(final String zoneName) {
+		return getZone(zoneName).getFilteredEntities(new FilterCriteria<Entity>() {
+			public boolean passes(Entity o) {
+				return o instanceof ExpirationTracker;
+			}
+		});
+	}
+
+	/**
+	 * Helper method to get all players in a zone
+	 * 
+	 * @param zoneName the zone for which to get players
+	 * @return a list of players in the zone
+	 */
+	private List<Player> getPlayers(final String zoneName) {
+		return getZone(zoneName).getPlayers();
+	}
+
+	/**
+	 * Sets the new expiration time for an allotment
+	 * 
+	 * @param zoneName the zone where the allotment is
+	 * @param allotmentName the name of the allotment (found in the zone's xml file)
+	 * 
+	 * @return true if successful, false otherwise
+	 */
+	public boolean setExpirationTime(final String zoneName, final String allotment) {
+		for (Entity entity : getTrackers(zoneName)) {
+			ExpirationTracker tracker = (ExpirationTracker) entity;
+
+			if (tracker.getIdentifier().equals(ALLOTMENT_PREFIX + allotment)) {
+				tracker.setExpirationTime(System.currentTimeMillis() + RENTAL_TIME);
+				
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Gets a key to an allotment that has been rented out
+	 * 
+	 * @param zoneName the zone the allotment is in
+	 * @param allotment the number of the allotment
+	 * 
+	 * @return a key to the allotment
+	 */
+	public GateKey getKey(final String zoneName, final String allotment) {
+		for (Entity e : getTrackers(zoneName)) {
+			if (((ExpirationTracker) e).getIdentifier().equals(ALLOTMENT_PREFIX + allotment)) {
+				final GateKey key = (GateKey) SingletonRepository.getEntityManager().getItem("gate key");
+				key.setup(ALLOTMENT_PREFIX + allotment, ((ExpirationTracker) e).getExpirationTime());
+				
+				return key;
+			}
+		}
+			
+		return null;
+	}
+	
+	/**
+	 * Checks if an allotment exists
+	 * 
+	 * @param zoneName the zone where the allotment is in
+	 * @param allotment
+	 * @return
+	 */
+	public boolean hasAllotment(final String zoneName, final String allotment) {
+		for (Entity e : getTrackers(zoneName)) {
+			if (((ExpirationTracker) e).getIdentifier().equals(ALLOTMENT_PREFIX + allotment)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Get all available allotments in a zone
+	 * 
+	 * @param zoneName the zone to get allotments for
+	 * @return a list of available allotment numbers
+	 */
+	public Set<String> getAvailableAllotments(final String zoneName) {
+		Set<String> available = new HashSet<String>();
+		
+		for (Entity e : getTrackers(zoneName)) {
+			if (((ExpirationTracker) e).getIdentifier().startsWith(ALLOTMENT_PREFIX) &&
+				((ExpirationTracker) e).getExpirationTime() < System.currentTimeMillis()) {	
+				
+				available.add(((ExpirationTracker) e).getIdentifier().replace(ALLOTMENT_PREFIX, ""));
+			}
+		}
+		
+		return available;
+	}
+	
+	/**
+	 * Callback to kick players when rental time expires
+	 */
+	private TurnListener kickPlayers = new TurnListener() {
+		public void onTurnReached(int currentTurn) {
+			// Kick players if they're in an allotment that has expired
+			for (String zoneName : zones) {
+				for (Entity trackerEntity : getTrackers(zoneName)) {
+					ExpirationTracker tracker = (ExpirationTracker) trackerEntity;
+					
+					if (tracker.getExpirationTime() <= System.currentTimeMillis()) {
+						final String name = tracker.getIdentifier();
+						
+						//close the gate
+						for (Entity gateEntity : getGates(zoneName)) {
+							if (name.equals(gateEntity.get("identifier"))) {
+								((Gate) gateEntity).close();
+							}
+						}
+
+						//clear the players
+						for (Entity allotmentEntity : getAllotments(zoneName)) {
+							Allotment allotment = (Allotment) allotmentEntity;
+							
+							if (allotment.has("name") && name.equals(allotment.getName())) {
+								for (Player player : getPlayers(zoneName)) {
+									if (player.getArea().intersects(allotment.getArea())) {
+										player.teleport(getZone(zoneName), tracker.getX(), tracker.getY(), null, null);
+									}
+								}
+							}
+						}
+					}
+				}
+			}			
+		}
+	};
+	
+	/**
+	 * Callback to warn players that rental time is expiring
+	 */
+	private TurnListener warnPlayers = new TurnListener() {
+		public void onTurnReached(int currentTurn) {
+			// Kick players if they're in an allotment that has expired
+			for (String zoneName : zones) {
+				for (Entity trackerEntity : getTrackers(zoneName)) {
+					ExpirationTracker tracker = (ExpirationTracker) trackerEntity;
+					
+					if (tracker.getExpirationTime() - WARN_TIME * 1000 <= System.currentTimeMillis()) {
+						final String name = tracker.getIdentifier();
+						
+						//warn the players
+						for (Entity allotmentEntity : getAllotments(zoneName)) {
+							Allotment allotment = (Allotment) allotmentEntity;
+							
+							if (allotment.has("name") && name.equals(allotment.getName())) {
+								for (Player player : getPlayers(zoneName)) {
+									if (player.getArea().intersects(allotment.getArea())) {
+										player.sendPrivateText("You have " + WARN_TIME + " minutes left before your rental time expires. You are kindly asked to finish up and exit in an orderly fashion. Kind regards, Semos Allotment Rentals Staff.");
+									}
+								}
+							}
+						}
+					}
+				}
+			}			
+		}
+	};
+
+	/**
+	 * Callback for TurnNotifier - periodically checks if any allotment has
+	 * expired and kicks players if it has
+	 */
+	public void onTurnReached(int currentTurn) {
+		//Set up the next check
+		SingletonRepository.getTurnNotifier().notifyInSeconds(EXPIRATION_CHECKING_PERIOD, this);
+
+		for (String zoneName : zones) {
+			for (Entity trackerEntity : new HashSet<Entity>(getTrackers(zoneName))) {
+				ExpirationTracker tracker = (ExpirationTracker) trackerEntity;
+						
+				// if a warning happens within the next checking period schedule a warn
+				if (tracker.getExpirationTime() - WARN_TIME * 1000 <= System.currentTimeMillis()
+						+ EXPIRATION_CHECKING_PERIOD * 1000 &&
+					tracker.getExpirationTime() - WARN_TIME * 1000 > System.currentTimeMillis()) {
+					
+					long diff = tracker.getExpirationTime() - WARN_TIME * 1000 - System.currentTimeMillis();
+					SingletonRepository.getTurnNotifier().notifyInSeconds((int) diff / 1000, warnPlayers);
+				}
+				
+				// if an expiration happens within the next checking period schedule a kick
+				if (tracker.getExpirationTime() <= System.currentTimeMillis()
+						+ EXPIRATION_CHECKING_PERIOD  * 1000 &&
+					tracker.getExpirationTime() > System.currentTimeMillis()) {
+					
+					long diff = tracker.getExpirationTime() - System.currentTimeMillis();
+					SingletonRepository.getTurnNotifier().notifyInSeconds((int) diff / 1000, kickPlayers);
+				}
+			}
+		}
+	}
+}
