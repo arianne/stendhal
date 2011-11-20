86b79a85f379bc8907c4bab2ea4bd3ab1785f29b
Markus <madmetzger@users.sourceforge.net>
2011-11-20 12:33:19 +0000

    java doc and modifying method for def

--- src/games/stendhal/server/entity/modifier/RPEntityModifierHandler.java
+++ src/games/stendhal/server/entity/modifier/RPEntityModifierHandler.java
@@ -11,12 +11,30 @@ public class RPEntityModifierHandler extends AbstractModifierHandler {
 		super(affectedEntity);
 	}
 
+	/**
+	 * Calculate the modified value of base hp
+	 * @param hp
+	 * @return modified base hp
+	 */
 	public int modifyHp(int hp) {
 		double cumulatedModifier = 1d;
 		for (AttributeModifier m : this.getModifiers()) {
 			cumulatedModifier = cumulatedModifier * (1d + m.getHpModifier());
 		}
 		return Double.valueOf(Math.ceil(cumulatedModifier * hp)).intValue();
+	}
+
+	/**
+	 * Calculate the modified value of def
+	 * @param def
+	 * @return modified def
+	 */
+	public int modifyDef(int def) {
+		double cumulatedModifier = 1d;
+		for (AttributeModifier m : this.getModifiers()) {
+			cumulatedModifier = cumulatedModifier * (1d + m.getDefModifier());
+		}
+		return Double.valueOf(Math.ceil(cumulatedModifier * def)).intValue();
 	}
 
 }
