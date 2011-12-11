202045da06f40e0a8c3a11fc6000bddaeff5b9b9
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-12-11 16:19:17 +0000

    created DaylightCondition

---
+++ src/games/stendhal/server/entity/npc/condition/DaylightCondition.java
@@ -0,0 +1,49 @@
+package games.stendhal.server.entity.npc.condition;
+
+import games.stendhal.common.parser.Sentence;
+import games.stendhal.server.core.rp.DaylightPhase;
+import games.stendhal.server.entity.Entity;
+import games.stendhal.server.entity.npc.ChatCondition;
+import games.stendhal.server.entity.player.Player;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+/**
+ * Checks the current day light phase.
+ *
+ * @author hendrik
+ */
+public class DaylightCondition implements ChatCondition {
+
+	private final List<DaylightPhase> daylightPhases;
+
+	/**
+	 * creates a new DaytimeCondition
+	 *
+	 * @param daylightPhases one or more daylight phases
+	 */
+	public DaylightCondition(final DaylightPhase... daylightPhases) {
+		super();
+		this.daylightPhases = Arrays.asList(daylightPhases);
+	}
+
+	public boolean fire(final Player player, final Sentence sentence, final Entity npc) {
+		return daylightPhases.contains(DaylightPhase.current());
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj, false,
+				DaylightCondition.class);
+	}
+
+}
