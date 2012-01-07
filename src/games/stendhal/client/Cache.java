181d41ab608c674b672578e5279b753f46f6fdac
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-01-07 18:12:51 +0000

    removed depracted code for prefilled cache, added support for hash based identity check

--- src/games/stendhal/client/Cache.java
+++ src/games/stendhal/client/Cache.java
@@ -12,192 +12,51 @@
  ***************************************************************************/
 package games.stendhal.client;
 
+import games.stendhal.common.CRC;
+
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.URL;
-import java.util.Properties;
+import java.util.Arrays;
 
-import marauroa.common.Configuration;
-import marauroa.common.ConfigurationParams;
-import marauroa.common.io.Persistence;
+import marauroa.common.crypto.Hash;
 import marauroa.common.net.message.TransferContent;
 
 import org.apache.log4j.Logger;
 
 /**
- * <p>
- * Manages a two level cache which one or both levels are optional:
- * </p>
- * 
- * <p>
- * The first level is prefilled readonly cache in a .jar file on class path. At
- * the time of writing we use this for Webstart because we are unsure how large
- * the webstart PersistenceService may grow.
- * </p>
- * 
- * <p>
- * The second level is a normal cache on filesystem.
- * </p>
+ * Manages a cache for content files such as zone data transmitted by the server
  */
 public class Cache {
-	private static final String VERSION_KEY = "_VERSION";
 	private static Logger logger = Logger.getLogger(Cache.class);
-	private Configuration cacheManager;
-	private Properties prefilledCacheManager;
-
-	/**
-	 * Return the client configuration instance
-	 * (content of "stendhal.cache").
-	 * 
-	 * @return the singleton instance
-	 */
-	public Configuration getConfiguration() {
-		return cacheManager;
-	}
 
 	/**
 	 * Inits the cache.
 	 */
 	protected void init() {
 		try {
-			prefilledCacheManager = new Properties();
-			final URL url = this.getClass().getClassLoader().getResource("cache/stendhal.cache");
-			if (url != null) {
-				final InputStream is = url.openStream();
-				prefilledCacheManager.load(is);
-				is.close();
-			}
 
-			// init caching-directory
-			if (!stendhal.WEB_START_SANDBOX) {
-				// Create file object
-				File file = new File(stendhal.getGameFolder());
-				if (!file.exists() && !file.mkdirs()) {
-					logger.error("Can't create " + file.getAbsolutePath()
-							+ " folder");
-				} else if (file.exists() && file.isFile()) {
-					if (!file.delete() || !file.mkdirs()) {
-						logger.error("Can't removing file "
-								+ file.getAbsolutePath()
-								+ " and creating a folder instead.");
-					}
-				}
-
-				file = new File(stendhal.getGameFolder() + "cache");
-				if (!file.exists() && !file.mkdir()) {
-					logger.error("Can't create " + file.getAbsolutePath()
-							+ " folder");
+			// Create file object
+			File file = new File(stendhal.getGameFolder());
+			if (!file.exists() && !file.mkdirs()) {
+				logger.error("Can't create " + file.getAbsolutePath() + " folder");
+			} else if (file.exists() && file.isFile()) {
+				if (!file.delete() || !file.mkdirs()) {
+					logger.error("Can't removing file " + file.getAbsolutePath() + " and creating a folder instead.");
 				}
 			}
-			initCacheManager();
-			cleanCacheOnUpdate();
-			cacheManager.set(VERSION_KEY, stendhal.VERSION);
-		} catch (final IOException e) {
-			logger.error("cannot create StendhalClient - IO exception", e);
+
+			file = new File(stendhal.getGameFolder() + "cache");
+			if (!file.exists() && !file.mkdir()) {
+				logger.error("Can't create " + file.getAbsolutePath() + " folder");
+			}
 		} catch (final RuntimeException e) {
-			logger.error("cannot create StendhalClient", e);
+			logger.error("cannot create cach folder", e);
 		}
-	}
-
-	/**
-	 * Empty cache on update.
-	 * 
-	 * Stendhal is known to crash, if incompatible stuff is in cache.
-	 * 
-	 * @throws IOException
-	 *             in case the cache folder is not writeable
-	 */
-	private void cleanCacheOnUpdate() throws IOException {
-		if (!cacheManager.has(VERSION_KEY)
-				|| !stendhal.VERSION.equals(cacheManager.get(VERSION_KEY))) {
-			cleanCache();
-			cacheManager.clear();
-			initCacheManager();
-		}
-	}
-
-	/**
-	 * initializes the low level cache manager.
-	 * 
-	 * @throws IOException
-	 *             in case the cache folder is not readable
-	 */
-	private boolean initCacheManager() throws IOException {
-		final String cacheFile = stendhal.getGameFolder() + "cache"
-			+ File.separator + "stendhal.cache";
-
-		// create a new cache file if doesn't exist already
-		boolean ret = new File(cacheFile).createNewFile();
-
-		cacheManager = new Configuration(new ConfigurationParams(
-				false, stendhal.getGameFolder(), "cache/stendhal.cache"));
-
-		return ret;
-	}
-
-	/**
-	 * Deletes the cache.
-	 */
-	private void cleanCache() {
-		final File cacheDir = new File(stendhal.getGameFolder() + "cache");
-		if (cacheDir.isDirectory()) {
-			final File[] files = cacheDir.listFiles();
-			for (final File file : files) {
-				if (!file.delete()) {
-					logger.debug("File was not deleted while cleaning cache:" +
-								 file.getAbsolutePath());
-				}
-			}
-		}
-	}
-
-	private InputStream getItemFromPrefilledCache(final TransferContent item) {
-		final String name = "cache/" + item.name;
-
-		// note: timestamp may contain a checksum. So we have to do an
-		// "equal"-compare.
-		final String timestamp = prefilledCacheManager.getProperty(item.name);
-		if ((timestamp != null)
-				&& (Integer.parseInt(timestamp) == item.timestamp)) {
-
-			// get the stream
-			final URL url = this.getClass().getClassLoader().getResource(name);
-			if (url != null) {
-				try {
-					logger.debug("Content " + item.name
-							+ " is in prefilled cache.");
-					return url.openStream();
-				} catch (final IOException e) {
-					logger.error(e, e);
-				}
-			}
-
-		}
-		return null;
-	}
-
-	private InputStream getItemFromCache(final TransferContent item) {
-		try {
-			// check cache
-			if (cacheManager.has(item.name)
-					&& (Integer.parseInt(cacheManager.get(item.name)) == item.timestamp)) {
-				logger.debug("Content " + item.name
-						+ " is on cache. We save transfer");
-
-				// get the stream
-				try {
-					return Persistence.get().getInputStream(false, stendhal.getGameFolder() + "cache", item.name);
-				} catch (final IOException e) {
-					logger.warn("Cannot read cache file: " + item.name);
-				}
-			}
-		} catch (final NumberFormatException e) {
-			logger.error("Broken cache entry: " + item.name, e);
-		}
-		
-		return null;
 	}
 
 	/**
@@ -212,15 +71,63 @@ public class Cache {
 			logger.error("Cannot get item from cache because .. is not allowed in name " + item.name);
 			return null;
 		}
-
-		// 1. try to read it from stendhal-prefilled-cache.jar
-		InputStream is = getItemFromPrefilledCache(item);
-
-		// 2. try to read from our cache (if not in sandbox)
-		if (is == null) {
-			is = getItemFromCache(item);
+		String filename = stendhal.getGameFolder() + "cache/" + item.name;
+		byte[] data = readFileContent(filename);
+		if (data == null) {
+			return null;
 		}
-		return is;
+
+		// Check hash, if provided by the server
+		byte[] expectedHash = item.getTransmittedHash();
+		if (expectedHash != null) {
+			byte[] actualHash = Hash.hash(data);
+			if (Arrays.equals(expectedHash, actualHash)) {
+				return new ByteArrayInputStream(data);
+			} else {
+				return null;
+			}
+		}
+
+		// Otherwise check CRC for Stendhal up to 0.97
+		int expectedCRC = item.timestamp;
+		int actualCRC = CRC.cmpCRC(data);
+
+		if (expectedCRC == actualCRC) {
+			return new ByteArrayInputStream(data);
+		}
+		return null;
+	}
+
+	/**
+	 * reads a file into a byte array
+	 *
+	 * @param filename name of file
+	 * @return byte-array or <code>null</code> in case the file cannot be read
+	 */
+	private static byte[] readFileContent(String filename) {
+		File file = new File(filename);
+		if (!file.exists()) {
+			return null;
+		}
+		int offset = 0;
+		int numRead = 0;
+		int size = (int) file.length();
+		byte[] res = new byte[size];
+
+		// we need that loop because java does not garantee, 
+		// that read(...) returns the complete remaining stream at once
+		try {
+			InputStream is = new FileInputStream(file);
+			while ((offset < size) && numRead > -1) {
+				numRead = is.read(res, offset, size - offset);
+				offset += numRead;
+			}
+			is.close();
+		} catch (IOException e) {
+			logger.warn(e, e);
+			return null;
+		}
+		return res;
 	}
 
 	/**
@@ -237,16 +144,13 @@ public class Cache {
 				logger.error("Cannot store item to cache because .. is not allowed in name " + item.name);
 				return;
 			}
-			final OutputStream os = Persistence.get().getOutputStream(false,
-					stendhal.getGameFolder() + "cache", item.name);
+			String filename = stendhal.getGameFolder() + "cache/" + item.name;
+			OutputStream os = new FileOutputStream(filename);
 			os.write(data);
 			os.close();
 
-			logger.debug("Content " + item.name + " cached now. Timestamp: "
-					+ Integer.toString(item.timestamp));
-
-			cacheManager.set(item.name, Integer.toString(item.timestamp));
-		} catch (final java.io.IOException e) {
+			logger.debug("Content " + item.name + " cached now.");
+		} catch (IOException e) {
 			logger.error("store", e);
 		}
 	}
