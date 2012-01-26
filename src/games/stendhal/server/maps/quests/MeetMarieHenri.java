8be9199ee9d30db55358e6265d1d890765e9b405
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-01-26 06:04:47 +0000

    added to-do comment about standard conditions and actions

--- src/games/stendhal/server/maps/quests/MeetMarieHenri.java
+++ src/games/stendhal/server/maps/quests/MeetMarieHenri.java
@@ -6,24 +6,30 @@ import games.stendhal.common.parser.JokerExprMatcher;
 import games.stendhal.common.parser.Sentence;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.entity.item.Item;
-import games.stendhal.server.entity.npc.*;
-import games.stendhal.server.entity.npc.action.*;
+import games.stendhal.server.entity.npc.ChatAction;
+import games.stendhal.server.entity.npc.ConversationPhrases;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.EventRaiser;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.action.SetQuestAndModifyKarmaAction;
 import games.stendhal.server.entity.npc.condition.AndCondition;
 import games.stendhal.server.entity.npc.condition.NotCondition;
 import games.stendhal.server.entity.npc.condition.QuestInStateCondition;
-import games.stendhal.server.entity.player.*;
+import games.stendhal.server.entity.player.Player;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * QUEST: Meet Marie-Henri
  * <p>
- * 
+ *
  * PARTICIPANTS:
  * <ul>
  * <li>Marie-Henrie, the famous french writer in Ados library</li>
  * </ul>
- * 
+ *
  * STEPS:
  * <ul>
  * <li>Marie-Henri asks you to find out the pseudonym he uses when writing
@@ -32,18 +38,18 @@ import java.util.*;
  * <li>Name the pseudonym to Marie-Henrie</li>
  * <li>Marie-Henri gives you a reward</li>
  * </ul>
- * 
+ *
  * REWARD:
  * <ul>
  * <li>Karma +5</li>
  * <li>An empty scroll</li>
  * </ul>
- * 
+ *
  * REPETITIONS:
  * <ul>
  * <li>No repetitions.</li>
  * </ul>
- * 
+ *
  * @author RedQueen
  */
 public class MeetMarieHenri extends AbstractQuest {
@@ -69,6 +75,7 @@ public class MeetMarieHenri extends AbstractQuest {
 		return "MeetMarieHenri";
 	}
 
+	@Override
 	public List<String> getHistory(final Player player) {
 		final List<String> res = new ArrayList<String>();
 		if (!player.hasQuest(QUEST_SLOT)) {
@@ -91,6 +98,9 @@ public class MeetMarieHenri extends AbstractQuest {
 	private void createSteps() {
 		// player is asking for a quest
 		SpeakerNPC npc = npcs.get("Marie-Henri");
+
+
+		// TODO: rewrite this to use standard conditions and actions
 		npc.add(ConversationStates.ATTENDING,
 				ConversationPhrases.QUEST_MESSAGES, null,
 				ConversationStates.QUEST_OFFERED, null, new ChatAction() {
@@ -137,7 +147,7 @@ public class MeetMarieHenri extends AbstractQuest {
 
 		// player wants to answer the question
 		npc.add(ConversationStates.ATTENDING,
-				Arrays.asList("pseudonym", "answer", "question"), 
+				Arrays.asList("pseudonym", "answer", "question"),
 				new AndCondition(new QuestInStateCondition(QUEST_SLOT, "start"), new NotCondition(new QuestInStateCondition(QUEST_SLOT, "done"))),
 				ConversationStates.QUESTION_1,
 				"Have you already found out my pseudonym?", null);
@@ -162,6 +172,8 @@ public class MeetMarieHenri extends AbstractQuest {
 				"Maybe the books on that table will give you a clue...", null);
 
 		// analyzing the answer
+
+		// TODO: rewrite this to use standard conditions and actions
 		npc.addMatching(ConversationStates.QUESTION_2, Expression.JOKER,
 				new JokerExprMatcher(), null, ConversationStates.ATTENDING,
 				null, new ChatAction() {
