37babb1eb3191b4a15dac04c91544cba09cac8ef
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-02-26 10:39:57 +0000

    don't handle chat bucket in AnswerAction because it is done again in the invoked TellAction

--- src/games/stendhal/server/actions/chat/AnswerAction.java
+++ src/games/stendhal/server/actions/chat/AnswerAction.java
@@ -20,15 +20,11 @@ import games.stendhal.server.entity.player.Player;
 import marauroa.common.game.RPAction;
 
 /**
- * handles /tell-action (/msg-action). 
+ * handles /tell-action (/msg-action).
  */
 class AnswerAction implements ActionListener {
 
 	public void onAction(final Player player, final RPAction action) {
-		if (!player.getChatBucket().checkAndAdd()) {
-			return;
-		}
-
 		if (action.has(TEXT)) {
 			if (player.getLastPrivateChatter() != null) {
 				// convert the action to a /tell action
