45ae48098701b98a15fe1b0b714ba7aea04b3066
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-03-26 19:47:30 +0000

    code format

--- tests/games/stendhal/server/entity/RPEntityTest.java
+++ tests/games/stendhal/server/entity/RPEntityTest.java
@@ -50,7 +50,6 @@ import org.junit.Test;
 import utilities.PlayerTestHelper;
 import utilities.RPClass.ItemTestHelper;
 
-
 public class RPEntityTest {
 
 	private class MockRPentity extends RPEntity {
@@ -92,7 +91,7 @@ public class RPEntityTest {
 		assertThat(RPEntity.applyDistanceAttackModifiers(damage, 36, 7), is(75));
 		assertThat(RPEntity.applyDistanceAttackModifiers(damage, 49, 7), is(43));
 		assertThat(RPEntity.applyDistanceAttackModifiers(damage, 64, 7), is(0));
-		
+
 		// same with non standard distances.
 		// a short range weapon first
 		assertThat(RPEntity.applyDistanceAttackModifiers(damage, 0, 4), is(80));
@@ -101,7 +100,7 @@ public class RPEntityTest {
 		assertThat(RPEntity.applyDistanceAttackModifiers(damage, 9, 4), is(96));
 		assertThat(RPEntity.applyDistanceAttackModifiers(damage, 16, 4), is(64));
 		assertThat(RPEntity.applyDistanceAttackModifiers(damage, 25, 4), is(0));
-		
+
 		// and a long range weapon (dunno if we actually have anything for
 		// this long range)
 		assertThat(RPEntity.applyDistanceAttackModifiers(damage, 0, 10), is(80));
@@ -138,89 +137,49 @@ public class RPEntityTest {
 		};
 		int defenderDEF = 1;
 		int attackerATK = 1;
-		assertThat(entity.calculateRiskForCanHit(1, defenderDEF, attackerATK),
-				is(19));
-		assertThat(entity.calculateRiskForCanHit(2, defenderDEF, attackerATK),
-				is(18));
-		assertThat(entity.calculateRiskForCanHit(3, defenderDEF, attackerATK),
-				is(17));
-		assertThat(entity.calculateRiskForCanHit(4, defenderDEF, attackerATK),
-				is(16));
-		assertThat(entity.calculateRiskForCanHit(5, defenderDEF, attackerATK),
-				is(15));
-		assertThat(entity.calculateRiskForCanHit(6, defenderDEF, attackerATK),
-				is(14));
-		assertThat(entity.calculateRiskForCanHit(7, defenderDEF, attackerATK),
-				is(13));
-		assertThat(entity.calculateRiskForCanHit(8, defenderDEF, attackerATK),
-				is(12));
-		assertThat(entity.calculateRiskForCanHit(9, defenderDEF, attackerATK),
-				is(11));
-		assertThat(entity.calculateRiskForCanHit(10, defenderDEF, attackerATK),
-				is(10));
-		assertThat(entity.calculateRiskForCanHit(11, defenderDEF, attackerATK),
-				is(9));
-		assertThat(entity.calculateRiskForCanHit(12, defenderDEF, attackerATK),
-				is(8));
-		assertThat(entity.calculateRiskForCanHit(13, defenderDEF, attackerATK),
-				is(7));
-		assertThat(entity.calculateRiskForCanHit(14, defenderDEF, attackerATK),
-				is(6));
-		assertThat(entity.calculateRiskForCanHit(15, defenderDEF, attackerATK),
-				is(5));
-		assertThat(entity.calculateRiskForCanHit(16, defenderDEF, attackerATK),
-				is(4));
-		assertThat(entity.calculateRiskForCanHit(17, defenderDEF, attackerATK),
-				is(3));
-		assertThat(entity.calculateRiskForCanHit(18, defenderDEF, attackerATK),
-				is(2));
-		assertThat(entity.calculateRiskForCanHit(19, defenderDEF, attackerATK),
-				is(1));
-		assertThat(entity.calculateRiskForCanHit(20, defenderDEF, attackerATK),
-				is(0));
+		assertThat(entity.calculateRiskForCanHit(1, defenderDEF, attackerATK), is(19));
+		assertThat(entity.calculateRiskForCanHit(2, defenderDEF, attackerATK), is(18));
+		assertThat(entity.calculateRiskForCanHit(3, defenderDEF, attackerATK), is(17));
+		assertThat(entity.calculateRiskForCanHit(4, defenderDEF, attackerATK), is(16));
+		assertThat(entity.calculateRiskForCanHit(5, defenderDEF, attackerATK), is(15));
+		assertThat(entity.calculateRiskForCanHit(6, defenderDEF, attackerATK), is(14));
+		assertThat(entity.calculateRiskForCanHit(7, defenderDEF, attackerATK), is(13));
+		assertThat(entity.calculateRiskForCanHit(8, defenderDEF, attackerATK), is(12));
+		assertThat(entity.calculateRiskForCanHit(9, defenderDEF, attackerATK), is(11));
+		assertThat(entity.calculateRiskForCanHit(10, defenderDEF, attackerATK), is(10));
+		assertThat(entity.calculateRiskForCanHit(11, defenderDEF, attackerATK), is(9));
+		assertThat(entity.calculateRiskForCanHit(12, defenderDEF, attackerATK), is(8));
+		assertThat(entity.calculateRiskForCanHit(13, defenderDEF, attackerATK), is(7));
+		assertThat(entity.calculateRiskForCanHit(14, defenderDEF, attackerATK), is(6));
+		assertThat(entity.calculateRiskForCanHit(15, defenderDEF, attackerATK), is(5));
+		assertThat(entity.calculateRiskForCanHit(16, defenderDEF, attackerATK), is(4));
+		assertThat(entity.calculateRiskForCanHit(17, defenderDEF, attackerATK), is(3));
+		assertThat(entity.calculateRiskForCanHit(18, defenderDEF, attackerATK), is(2));
+		assertThat(entity.calculateRiskForCanHit(19, defenderDEF, attackerATK), is(1));
+		assertThat(entity.calculateRiskForCanHit(20, defenderDEF, attackerATK), is(0));
 
 		defenderDEF = 10;
 		attackerATK = 5;
-		assertThat(entity.calculateRiskForCanHit(1, defenderDEF, attackerATK),
-				is(90));
-		assertThat(entity.calculateRiskForCanHit(2, defenderDEF, attackerATK),
-				is(80));
-		assertThat(entity.calculateRiskForCanHit(3, defenderDEF, attackerATK),
-				is(70));
-		assertThat(entity.calculateRiskForCanHit(4, defenderDEF, attackerATK),
-				is(60));
-		assertThat(entity.calculateRiskForCanHit(5, defenderDEF, attackerATK),
-				is(50));
-		assertThat(entity.calculateRiskForCanHit(6, defenderDEF, attackerATK),
-				is(40));
-		assertThat(entity.calculateRiskForCanHit(7, defenderDEF, attackerATK),
-				is(30));
-		assertThat(entity.calculateRiskForCanHit(8, defenderDEF, attackerATK),
-				is(20));
-		assertThat(entity.calculateRiskForCanHit(9, defenderDEF, attackerATK),
-				is(10));
-		assertThat(entity.calculateRiskForCanHit(10, defenderDEF, attackerATK),
-				is(0));
-		assertThat(entity.calculateRiskForCanHit(11, defenderDEF, attackerATK),
-				is(-10));
-		assertThat(entity.calculateRiskForCanHit(12, defenderDEF, attackerATK),
-				is(-20));
-		assertThat(entity.calculateRiskForCanHit(13, defenderDEF, attackerATK),
-				is(-30));
-		assertThat(entity.calculateRiskForCanHit(14, defenderDEF, attackerATK),
-				is(-40));
-		assertThat(entity.calculateRiskForCanHit(15, defenderDEF, attackerATK),
-				is(-50));
-		assertThat(entity.calculateRiskForCanHit(16, defenderDEF, attackerATK),
-				is(-60));
-		assertThat(entity.calculateRiskForCanHit(17, defenderDEF, attackerATK),
-				is(-70));
-		assertThat(entity.calculateRiskForCanHit(18, defenderDEF, attackerATK),
-				is(-80));
-		assertThat(entity.calculateRiskForCanHit(19, defenderDEF, attackerATK),
-				is(-90));
-		assertThat(entity.calculateRiskForCanHit(20, defenderDEF, attackerATK),
-				is(-100));
+		assertThat(entity.calculateRiskForCanHit(1, defenderDEF, attackerATK), is(90));
+		assertThat(entity.calculateRiskForCanHit(2, defenderDEF, attackerATK), is(80));
+		assertThat(entity.calculateRiskForCanHit(3, defenderDEF, attackerATK), is(70));
+		assertThat(entity.calculateRiskForCanHit(4, defenderDEF, attackerATK), is(60));
+		assertThat(entity.calculateRiskForCanHit(5, defenderDEF, attackerATK), is(50));
+		assertThat(entity.calculateRiskForCanHit(6, defenderDEF, attackerATK), is(40));
+		assertThat(entity.calculateRiskForCanHit(7, defenderDEF, attackerATK), is(30));
+		assertThat(entity.calculateRiskForCanHit(8, defenderDEF, attackerATK), is(20));
+		assertThat(entity.calculateRiskForCanHit(9, defenderDEF, attackerATK), is(10));
+		assertThat(entity.calculateRiskForCanHit(10, defenderDEF, attackerATK), is(0));
+		assertThat(entity.calculateRiskForCanHit(11, defenderDEF, attackerATK), is(-10));
+		assertThat(entity.calculateRiskForCanHit(12, defenderDEF, attackerATK), is(-20));
+		assertThat(entity.calculateRiskForCanHit(13, defenderDEF, attackerATK), is(-30));
+		assertThat(entity.calculateRiskForCanHit(14, defenderDEF, attackerATK), is(-40));
+		assertThat(entity.calculateRiskForCanHit(15, defenderDEF, attackerATK), is(-50));
+		assertThat(entity.calculateRiskForCanHit(16, defenderDEF, attackerATK), is(-60));
+		assertThat(entity.calculateRiskForCanHit(17, defenderDEF, attackerATK), is(-70));
+		assertThat(entity.calculateRiskForCanHit(18, defenderDEF, attackerATK), is(-80));
+		assertThat(entity.calculateRiskForCanHit(19, defenderDEF, attackerATK), is(-90));
+		assertThat(entity.calculateRiskForCanHit(20, defenderDEF, attackerATK), is(-100));
 	}
 
 	/**
@@ -246,8 +205,7 @@ public class RPEntityTest {
 		entity.addSlot(new PlayerSlot("rhand"));
 
 		assertThat(entity.getItemAtk(), is(0f));
-		final Item item = SingletonRepository.getEntityManager().getItem(
-				"dagger");
+		final Item item = SingletonRepository.getEntityManager().getItem("dagger");
 		entity.getSlot("lhand").add(item);
 		assertThat(entity.getItemAtk(), is((float) item.getAttack()));
 		entity.getSlot("rhand").add(item);
@@ -281,8 +239,7 @@ public class RPEntityTest {
 		entity.addSlot(new PlayerSlot("rhand"));
 
 		assertThat(entity.getItemAtk(), is(0f));
-		final Item item = SingletonRepository.getEntityManager().getItem(
-				"cheese");
+		final Item item = SingletonRepository.getEntityManager().getItem("cheese");
 		entity.getSlot("lhand").add(item);
 		assertThat(entity.getItemAtk(), is(0f));
 		entity.getSlot("rhand").add(item);
@@ -315,17 +272,14 @@ public class RPEntityTest {
 		entity.addSlot(new PlayerSlot("rhand"));
 
 		assertThat(entity.getItemAtk(), is(0f));
-		final Item lefthanditem = SingletonRepository.getEntityManager()
-				.getItem("l hand sword");
+		final Item lefthanditem = SingletonRepository.getEntityManager().getItem("l hand sword");
 		entity.getSlot("lhand").add(lefthanditem);
 		assertThat(entity.getItemAtk(), is(0f));
 
-		final Item righthanditem = SingletonRepository.getEntityManager()
-				.getItem("r hand sword");
+		final Item righthanditem = SingletonRepository.getEntityManager().getItem("r hand sword");
 		entity.getSlot("rhand").add(righthanditem);
 		assertThat(entity.getItemAtk(),
-				is((float) (lefthanditem.getAttack() + righthanditem
-						.getAttack())));
+				is((float) (lefthanditem.getAttack() + righthanditem.getAttack())));
 	}
 
 	/**
@@ -354,13 +308,11 @@ public class RPEntityTest {
 
 		assertThat(entity.getItemAtk(), is(0f));
 
-		final Item lefthanditem = SingletonRepository.getEntityManager()
-				.getItem("l hand sword");
+		final Item lefthanditem = SingletonRepository.getEntityManager().getItem("l hand sword");
 		entity.getSlot("rhand").add(lefthanditem);
 		assertThat(entity.getItemAtk(), is(0f));
 
-		final Item righthanditem = SingletonRepository.getEntityManager()
-				.getItem("r hand sword");
+		final Item righthanditem = SingletonRepository.getEntityManager().getItem("r hand sword");
 		entity.getSlot("lhand").add(righthanditem);
 		assertThat(entity.getItemAtk(), is(0f));
 
@@ -375,7 +327,7 @@ public class RPEntityTest {
 		MockStendlRPWorld.get();
 		final StendhalRPZone zone = new StendhalRPZone("testzone");
 		final RPEntity attacker = new RPEntity() {
-			
+
 			@Override
 			protected void dropItemsOn(final Corpse corpse) {
 				// do nothing
@@ -388,7 +340,8 @@ public class RPEntityTest {
 			}
 
 			@Override
-			public int damageDone(final RPEntity defender, double attackingWeaponsValue, Nature damageType) {
+			public int damageDone(final RPEntity defender, double attackingWeaponsValue,
+					Nature damageType) {
 				return 0;
 			}
 
@@ -465,7 +418,8 @@ public class RPEntityTest {
 			}
 
 			@Override
-			public int damageDone(final RPEntity defender, double attackingWeaponsValue, Nature damageType) {
+			public int damageDone(final RPEntity defender, double attackingWeaponsValue,
+					Nature damageType) {
 				return 30;
 			}
 
@@ -514,7 +468,7 @@ public class RPEntityTest {
 		assertTrue(attacker.attack());
 
 		assertNotNull(attacker.getAttackTarget());
-		
+
 		AttackEvent attack = null;
 		for (RPEvent ev : attacker.events()) {
 			if (ev instanceof AttackEvent) {
@@ -657,19 +611,18 @@ public class RPEntityTest {
 		verify(item);
 	}
 
-	
 	@Test
 	public void testgetDroppables() {
-		
+
 		RPEntity entity = new MockRPentity();
-		Item cheese     = SingletonRepository.getEntityManager().getItem("cheese");
-		Item flag       = new CaptureTheFlagFlag();
-		
+		Item cheese = SingletonRepository.getEntityManager().getItem("cheese");
+		Item flag = new CaptureTheFlagFlag();
+
 		List<Item> droppables;
-		
+
 		droppables = entity.getDroppables();
 		assertEquals(null, droppables);
-		
+
 		entity.addSlot(new PlayerSlot("lhand"));
 		entity.addSlot(new PlayerSlot("rhand"));
 		entity.addSlot(new PlayerSlot("bag"));
@@ -693,22 +646,21 @@ public class RPEntityTest {
 		droppables = entity.getDroppables();
 		assertEquals(1, droppables.size());
 		assertEquals("flag", droppables.get(0).get("name"));
-		
+
 		// remove flag, and we have no droppables any more
 		entity.getSlot("rhand").remove(flag.getID());
-		
+
 		droppables = entity.getDroppables();
 		assertEquals(null, droppables);
 	}
 
-	
 	@Test
 	public void testdropDroppableItem() {
 
 		// MockStendlRPWorld.get();
 
-		Player     player      = PlayerTestHelper.createPlayer("player");
-		Item       flag        = new CaptureTheFlagFlag();
+		Player player = PlayerTestHelper.createPlayer("player");
+		Item flag = new CaptureTheFlagFlag();
 		List<Item> droppables;
 
 		MockStendhalRPRuleProcessor.get().addPlayer(player);
@@ -716,26 +668,25 @@ public class RPEntityTest {
 		StendhalRPZone zone = new StendhalRPZone("testzone", 100, 100);
 
 		zone.add(player);
-		
+
 		player.getSlot("rhand").add(flag);
 
 		droppables = player.getDroppables();
 		assertEquals(1, droppables.size());
 		assertEquals("flag", droppables.get(0).get("name"));
-		
+
 		player.dropDroppableItem(flag);
-		
+
 		droppables = player.getDroppables();
 		assertEquals(null, droppables);
 	}
-	
 
 	@Test
 	public void testmaybeDropDroppables() {
-		
-		Player     player      = PlayerTestHelper.createPlayer("player");
-		Player     attacker    = PlayerTestHelper.createPlayer("attacker");
-		Item       flag        = new CaptureTheFlagFlag();
+
+		Player player = PlayerTestHelper.createPlayer("player");
+		Player attacker = PlayerTestHelper.createPlayer("attacker");
+		Item flag = new CaptureTheFlagFlag();
 		List<Item> droppables;
 
 		MockStendhalRPRuleProcessor.get().addPlayer(player);
@@ -744,25 +695,25 @@ public class RPEntityTest {
 		StendhalRPZone zone = new StendhalRPZone("testzone", 100, 100);
 
 		zone.add(player);
-		
+
 		player.getSlot("rhand").add(flag);
 
 		// XXX i don't really know a better way to test this.
-		//     in theory, this could be in a loop forever.
-		//     but that won't happen
-		
+		// in theory, this could be in a loop forever.
+		// but that won't happen
+
 		while (true) {
-			
+
 			player.maybeDropDroppables(attacker);
-			
+
 			droppables = player.getDroppables();
 
-			if (droppables == null) { 
+			if (droppables == null) {
 				break;
 			}
 		}
 	}
-	
+
 	@Test
 	public void testModifiedBaseHP() throws Exception {
 		final RPEntity entity = new RPEntity() {
@@ -770,6 +721,7 @@ public class RPEntityTest {
 			protected void dropItemsOn(final Corpse corpse) {
 				// do nothing
 			}
+
 			@Override
 			public void logic() {
 				// do nothing
@@ -778,15 +730,15 @@ public class RPEntityTest {
 		assertThat(entity.getBaseHP(), is(0));
 		entity.initHP(100);
 		assertThat(entity.getBaseHP(), is(100));
-		long expireTimestampAsLong = System.currentTimeMillis()+1000;
+		long expireTimestampAsLong = System.currentTimeMillis() + 1000;
 		entity.addBaseHpModifier(new Date(expireTimestampAsLong), 0.5d);
 		assertThat(entity.getBaseHP(), is(150));
-		while(expireTimestampAsLong >= System.currentTimeMillis()) {
+		while (expireTimestampAsLong >= System.currentTimeMillis()) {
 			// just to wait till modifier is expired
 		}
 		assertThat(entity.getBaseHP(), is(100));
 	}
-	
+
 	@Test
 	public void testModifiedDef() throws Exception {
 		final RPEntity entity = new RPEntity() {
@@ -794,6 +746,7 @@ public class RPEntityTest {
 			protected void dropItemsOn(final Corpse corpse) {
 				// do nothing
 			}
+
 			@Override
 			public void logic() {
 				// do nothing
@@ -802,10 +755,10 @@ public class RPEntityTest {
 		assertThat(entity.getDef(), is(0));
 		entity.setDef(100);
 		assertThat(entity.getDef(), is(100));
-		long expireTimestampAsLong = System.currentTimeMillis()+1000;
+		long expireTimestampAsLong = System.currentTimeMillis() + 1000;
 		entity.addDefModifier(new Date(expireTimestampAsLong), 0.5d);
 		assertThat(entity.getDef(), is(150));
-		while(expireTimestampAsLong >= System.currentTimeMillis()) {
+		while (expireTimestampAsLong >= System.currentTimeMillis()) {
 			// just to wait till modifier is expired
 		}
 		assertThat(entity.getDef(), is(100));
