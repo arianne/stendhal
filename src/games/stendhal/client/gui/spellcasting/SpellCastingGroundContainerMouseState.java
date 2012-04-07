29e3eeec326dea75ee15e1e424c2e4d1b6438891
Markus <madmetzger@users.sourceforge.net>
2012-04-07 08:03:59 +0000

    java doc and comments

--- src/games/stendhal/client/gui/spellcasting/SpellCastingGroundContainerMouseState.java
+++ src/games/stendhal/client/gui/spellcasting/SpellCastingGroundContainerMouseState.java
@@ -10,14 +10,21 @@ import java.awt.Point;
 import java.awt.geom.Point2D;
 
 import marauroa.common.game.RPObject;
-
+/**
+ * State of the GroundContainer that is switched to, when a spell is selected.
+ * 
+ * @author madmetzger
+ */
 public class SpellCastingGroundContainerMouseState extends
 		GroundContainerMouseState {
 	
-//	private static final Logger _LOGGER = Logger.getLogger(SpellCastingGroundContainerMouseState.class);
-	
 	private RPObject spell;
 
+	/**
+	 * Create a new SpellCastingGroundContainerMouseState acting on a given GroundContainer
+	 * 
+	 * @param ground the GroundContainer
+	 */
 	public SpellCastingGroundContainerMouseState(GroundContainer ground) {
 		super(ground);
 	}
@@ -33,10 +40,12 @@ public class SpellCastingGroundContainerMouseState extends
 		Point2D point2d = screen.convertScreenViewToWorld(point);
 		double x = point2d.getX();
 		double y = point2d.getY();
+		// determine target
 		EntityView<?> target = screen.getEntityViewAt(x, y);
 		if(target == null) {
 			target = screen.getMovableEntityViewAt(x, y);
 		}
+		// when target found construct and execute spellcasting action
 		if(target != null) {
 			String[] params = {String.valueOf(spell.getID().getObjectID()), String.valueOf(target.getEntity().getID().getObjectID())};
 			new CastSpellAction().execute(params, null);
@@ -61,6 +70,11 @@ public class SpellCastingGroundContainerMouseState extends
 		this.switchState();
 	}
 
+	/**
+	 * Sets the previously selected spell, which will be casted on click on an entity
+	 * 
+	 * @param spell the selected spell
+	 */
 	public void setSpell(RPObject spell) {
 		this.spell = spell;
 	}
@@ -70,8 +84,10 @@ public class SpellCastingGroundContainerMouseState extends
 		Point2D point2 = ground.getScreen().convertScreenViewToWorld(point);
 		final EntityView<?> view = ground.getScreen().getEntityViewAt(point2.getX(), point2.getY());
 		if(view != null) {
+			// use magic symbol when cursor aims at an entity 
 			return StendhalCursor.SPELLCASTING;
 		}
+		// default to normal cursor which signals no behaviour in this state
 		return StendhalCursor.NORMAL;
 	}
 
