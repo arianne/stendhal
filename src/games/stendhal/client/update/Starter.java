8ed57dc0d61b20bc75612aefd6ae2a93c19d8a25
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-04-08 12:09:45 +0000

    include java 5.0 in the outdated version check.

--- src/games/stendhal/client/update/Starter.java
+++ src/games/stendhal/client/update/Starter.java
@@ -7,7 +7,7 @@
 // in order to display a dialogbox to the user in case an old version of java
 // is used. As we compile it with Java 1.2 no new features may be used in this
 // class.
-// 
+//
 //*****************************************************************************
 //*****************************************************************************
 
@@ -29,21 +29,21 @@ import javax.swing.JOptionPane;
 /**
  * This class can be compiled with a lower version of Java and will display an
  * error message if the java version is too old.
- * 
+ *
  * @author hendrik
  */
 public class Starter {
 
 	/**
 	 * Starts stendhal.
-	 * 
+	 *
 	 * @param args
 	 *            args
 	 */
 	public static void main(final String[] args) {
 		try {
 			final String version = System.getProperty("java.specification.version");
-			if (Float.parseFloat(version) < 1.5f) {
+			if (Float.parseFloat(version) < 1.6f) {
 				JOptionPane.showMessageDialog(
 						null,
 						"You need at least Java 6 (also known as 1.6.0) but you only have "
