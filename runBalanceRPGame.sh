6fe0f9e6d6f1ffb531ff15e7fc9725e0dda98a5b
 omero <nylon0700@users.sourceforge.net>
2012-04-21 10:02:31 +0000

    provide a helper shell script for running the creature balancer tool

---
+++ runBalanceRPGame.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ant compile_stendhaltools server_build
+#
+# log4j wants a proper configuration file log4j.properties in the classpath
+# here one is provided by appending data/conf directory.
+# set log4j.appender.File.File to a path with the right write permissions or an error will occur
+#
+#
+# run:
+# sh ./runBalanceRPGame.sh
+# sh ./runBalanceRPGame.sh <creature>
+# 
+# CAVEAT: it currently doesn't handle <creature> like 'killer bat' or 'giant spider'
+LOCALCLASSPATH=build/build_stendhaltools:build/build_server:build/build_server_maps:libs/marauroa.jar:libs/log4j.jar:libs/commons-lang.jar:libs/groovy.jar:libs/mysql-connector-java-5.1.5-bin.jar:libs/h2.jar:libs/swing-layout.jar:build/lib:data/conf:.
+java -cp "${LOCALCLASSPATH}" games.stendhal.tools.BalanceRPGame $1
