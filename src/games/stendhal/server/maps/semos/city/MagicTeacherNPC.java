cadaf44226e9d23543c2db2231b6268410d8bcda
Markus <madmetzger@users.sourceforge.net>
2012-05-12 20:24:44 +0000

    add an npc to easily enable a normal player testing magic

---
+++ src/games/stendhal/server/maps/semos/city/MagicTeacherNPC.java
@@ -0,0 +1,97 @@
+package games.stendhal.server.maps.semos.city;
+
+import games.stendhal.common.parser.Sentence;
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.rule.EntityManager;
+import games.stendhal.server.entity.item.StackableItem;
+import games.stendhal.server.entity.npc.ChatAction;
+import games.stendhal.server.entity.npc.ConversationPhrases;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.EventRaiser;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.entity.spell.Spell;
+
+import java.util.Collection;
+import java.util.Map;
+
+import marauroa.common.game.RPSlot;
+
+import org.apache.log4j.Logger;
+/**
+ * An NPC for testing purposes, that easily enables a player to play around with magic 
+ * 
+ * @author madmetzger
+ */
+public class MagicTeacherNPC implements ZoneConfigurator {
+	
+	/**
+	 * This ChatAction prepares a player with all he needs to play around with magic
+	 * 
+	 * @author madmetzger
+	 */
+	private final class TeachMagicAction implements ChatAction {
+		
+		public void fire(Player player, Sentence sentence, EventRaiser npc) {
+			enableSpellsFeature(player);
+			boostMana(player);
+			equipSpells(player);
+			equipManaPotions(player);
+		}
+		
+		private void equipManaPotions(Player player) {
+			StackableItem potion = (StackableItem) SingletonRepository.getEntityManager().getItem("mana");
+			potion.setQuantity(1000);
+			player.equipOrPutOnGround(potion);
+		}
+
+		private void equipSpells(Player player) {
+			EntityManager em = SingletonRepository.getEntityManager();
+			RPSlot slot = player.getSlot("spells");
+			Collection<String> spells = em.getConfiguredSpells();
+			for (String spellName : spells) {
+				Spell s = em.getSpell(spellName);
+				slot.add(s);
+			}
+		}
+
+		private void boostMana(Player player) {
+			player.setBaseMana(1000);
+			player.setMana(1000);
+		}
+
+		private void enableSpellsFeature(Player player) {
+			player.setFeature("spells", true);
+		}
+		
+	}
+
+	private static final Logger logger = Logger.getLogger(MagicTeacherNPC.class);
+
+	public void configureZone(StendhalRPZone zone,
+			Map<String, String> attributes) {
+		if(System.getProperty("stendhal.testserver") != null) {
+			logger.debug("stendhal.testserver active -> Adding Test NPC for magic");
+			SpeakerNPC npc = new SpeakerNPC("Mirlen") {
+
+				@Override
+				protected void createDialog() {
+					add(ConversationStates.ATTENDING, "teach", null, ConversationStates.SERVICE_OFFERED, "Do you want to learn about magic?", null);
+					add(ConversationStates.SERVICE_OFFERED, ConversationPhrases.YES_MESSAGES, null, ConversationStates.ATTENDING, null, new TeachMagicAction());
+				}
+				
+			};
+			npc.addGreeting("Hello, I am the magic teacher! I can #teach you about magic.");
+			npc.addJob("My job is to #teach you a bit about magic, so you can try it out here.");
+			npc.addHelp("If you need further help #https://stendhalgame.org/wiki/Ideas_for_Stendhal/Magic or you can ask in #'#arianne' for help.");
+			npc.addOffer("I can offer to #teach you about magic.");
+			npc.addGoodbye("Stay magical!");
+			npc.setPosition(20, 26);
+			npc.setEntityClass("blueoldwizardnpc");
+			zone.add(npc);
+		}
+	}
+
+}
