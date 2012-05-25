971802c12bb3f41a9a35be31d3f351185dd37668
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-05-25 18:39:46 +0000

    improved comments

--- src/games/stendhal/common/Direction.java
+++ src/games/stendhal/common/Direction.java
@@ -1,6 +1,6 @@
 /* $Id$ */
 /***************************************************************************
- *                      (C) Copyright 2003 - Marauroa                      *
+ *                   (C) Copyright 2003-2012 - Stendhal                    *
  ***************************************************************************
  ***************************************************************************
  *                                                                         *
@@ -12,7 +12,13 @@
  ***************************************************************************/
 package games.stendhal.common;
 
+/**
+ * a direction to face or walk to
+ *
+ * @author hendrik
+ */
 public enum Direction {
+	/** do not move */
 	STOP(0, 0, 0) {
 		@Override
 		public Direction nextDirection() {
@@ -24,6 +30,7 @@ public enum Direction {
 		}
 
 	},
+	/** up, away from the screen */
 	UP(1, 0, -1) {
 		@Override
 		public Direction nextDirection() {
@@ -34,6 +41,7 @@ public enum Direction {
 			return DOWN;
 		}
 	},
+	/** to the right */
 	RIGHT(2, 1, 0) {
 		@Override
 		public Direction nextDirection() {
@@ -44,6 +52,7 @@ public enum Direction {
 			return LEFT;
 		}
 	},
+	/** down, facing the player */
 	DOWN(3, 0, 1) {
 		@Override
 		public Direction nextDirection() {
@@ -54,6 +63,7 @@ public enum Direction {
 			return UP;
 		}
 	},
+	/** to the left */
 	LEFT(4, -1, 0) {
 		@Override
 		public Direction nextDirection() {
@@ -69,6 +79,12 @@ public enum Direction {
 	private final int dx;
 	private final int dy;
 
+	/**
+	 * converts an integer to a Direction
+	 *
+	 * @param val int
+	 * @return Direction
+	 */
 	public static Direction build(final int val) {
 		switch (val) {
 		case 1:
@@ -89,29 +105,66 @@ public enum Direction {
 		}
 	}
 
+	/**
+	 * gets the delta on the x-axis
+	 *
+	 * @return delta x
+	 */
 	public int getdx() {
 		return dx;
 	}
 
+	/**
+	 * gets the delta on the y-axis
+	 *
+	 * @return dy
+	 */
 	public int getdy() {
 		return dy;
 	}
 
+	/**
+	 * gets a random direction
+	 *
+	 * @return Direction
+	 */
 	public static Direction rand() {
 		return Direction.values()[Rand.rand(4) + 1];
 	}
 
+	/**
+	 * creates a direction
+	 *
+	 * @param val index
+	 * @param dx  delta on the x-axis
+	 * @param dy  delta on the y-axis
+	 */
 	Direction(final int val, final int dx, final int dy) {
 		this.val = val;
 		this.dx = dx;
 		this.dy = dy;
 	}
 
+	/**
+	 * gets the index
+	 *
+	 * @return index
+	 */
 	public int get() {
 		return val;
 	}
 
+	/**
+	 * gets the opposite direction
+	 *
+	 * @return Direction
+	 */
 	public abstract Direction oppositeDirection();
 
+	/**
+	 * gets the next direction clockwise
+	 *
+	 * @return Direction
+	 */
 	public abstract Direction nextDirection();
 }
