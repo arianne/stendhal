1d4c949088c1b42fd2d1669b05b5a60e2c920193
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-06-03 15:16:23 +0000

    revert

--- src/games/stendhal/common/Direction.java
+++ src/games/stendhal/common/Direction.java
@@ -19,13 +19,152 @@ package games.stendhal.common;
  */
 public enum Direction {
 	/** do not move */
-	STOP,
+	STOP(0, 0, 0) {
+		@Override
+		public Direction nextDirection() {
+			return LEFT;
+		}
+		@Override
+		public Direction oppositeDirection() {
+			return STOP;
+		}
+
+	},
 	/** up, away from the screen */
-	UP,
+	UP(1, 0, -1) {
+		@Override
+		public Direction nextDirection() {
+			return RIGHT;
+		}
+		@Override
+		public Direction oppositeDirection() {
+			return DOWN;
+		}
+	},
 	/** to the right */
-	RIGHT,
+	RIGHT(2, 1, 0) {
+		@Override
+		public Direction nextDirection() {
+			return DOWN;
+		}
+		@Override
+		public Direction oppositeDirection() {
+			return LEFT;
+		}
+	},
 	/** down, facing the player */
-	DOWN,
+	DOWN(3, 0, 1) {
+		@Override
+		public Direction nextDirection() {
+			return LEFT;
+		}
+		@Override
+		public Direction oppositeDirection() {
+			return UP;
+		}
+	},
 	/** to the left */
-	LEFT;
+	LEFT(4, -1, 0) {
+		@Override
+		public Direction nextDirection() {
+			return UP;
+		}
+		@Override
+		public Direction oppositeDirection() {
+			return RIGHT;
+		}
+	};
+
+	private final int val;
+	private final int dx;
+	private final int dy;
+
+	/**
+	 * converts an integer to a Direction
+	 *
+	 * @param val int
+	 * @return Direction
+	 */
+	public static Direction build(final int val) {
+		switch (val) {
+		case 1:
+			return UP;
+
+		case 2:
+			return RIGHT;
+
+		case 3:
+			return DOWN;
+
+		case 4:
+			return LEFT;
+
+		default:
+			return STOP;
+
+		}
+	}
+
+	/**
+	 * gets the delta on the x-axis
+	 *
+	 * @return delta x
+	 */
+	public int getdx() {
+		return dx;
+	}
+
+	/**
+	 * gets the delta on the y-axis
+	 *
+	 * @return dy
+	 */
+	public int getdy() {
+		return dy;
+	}
+
+	/**
+	 * gets a random direction
+	 *
+	 * @return Direction
+	 */
+	public static Direction rand() {
+		return Direction.values()[Rand.rand(4) + 1];
+	}
+
+	/**
+	 * creates a direction
+	 *
+	 * @param val index
+	 * @param dx  delta on the x-axis
+	 * @param dy  delta on the y-axis
+	 */
+	Direction(final int val, final int dx, final int dy) {
+		this.val = val;
+		this.dx = dx;
+		this.dy = dy;
+	}
+
+	/**
+	 * gets the index
+	 *
+	 * @return index
+	 */
+	public int get() {
+		return val;
+	}
+
+	/**
+	 * gets the opposite direction
+	 *
+	 * @return Direction
+	 */
+	public abstract Direction oppositeDirection();
+
+	/**
+	 * gets the next direction clockwise
+	 *
+	 * @return Direction
+	 */
+	public abstract Direction nextDirection();
 }
