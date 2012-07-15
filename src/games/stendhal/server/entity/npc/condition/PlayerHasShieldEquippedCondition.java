51c6b70cf34a70f5d7084dd6e9a9d8c120836b0f
Kimmo Rundelin <kiheru@users.sourceforge.net>
2012-07-15 10:39:52 +0000

    Added shield checking condition by Lumocra

---
+++ src/games/stendhal/server/entity/npc/condition/PlayerHasShieldEquippedCondition.java
@@ -0,0 +1,48 @@
+/***************************************************************************
+ *                    (C) Copyright 2003-2012 - Stendhal                   *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.entity.npc.condition;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+
+import games.stendhal.common.parser.Sentence;
+import games.stendhal.server.core.config.annotations.Dev;
+import games.stendhal.server.core.config.annotations.Dev.Category;
+import games.stendhal.server.entity.Entity;
+import games.stendhal.server.entity.npc.ChatCondition;
+import games.stendhal.server.entity.player.Player;
+
+/**
+ * Checks whether the player has a shield equipped
+ * 
+ * @author Lumocra
+ *
+ */
+@Dev(category=Category.ITEMS_OWNED)
+public class PlayerHasShieldEquippedCondition implements ChatCondition {
+
+	public boolean fire(Player player, Sentence sentence, Entity npc) {
+		return player.hasShield();
+	}
+	
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this);
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj, false,
+				PlayerHasShieldEquippedCondition.class);
+	}
+	
+}
