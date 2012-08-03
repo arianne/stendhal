f208b35626dc4ce114f6738a927cb51da4688c60
Kimmo Rundelin <kiheru@users.sourceforge.net>
2012-08-03 16:07:59 +0000

    Added another maze style

--- src/games/stendhal/server/maps/quests/maze/MazePainter.java
+++ src/games/stendhal/server/maps/quests/maze/MazePainter.java
@@ -1,6 +1,6 @@
 /* $Id$ */
 /***************************************************************************
- *                   (C) Copyright 2003-2010 - Stendhal                    *
+ *                   (C) Copyright 2003-2012 - Stendhal                    *
  ***************************************************************************
  ***************************************************************************
  *                                                                         *
@@ -12,50 +12,81 @@
  ***************************************************************************/
 package games.stendhal.server.maps.quests.maze;
 
+import games.stendhal.common.Rand;
 import games.stendhal.common.tiled.LayerDefinition;
 import games.stendhal.common.tiled.StendhalMapStructure;
 import games.stendhal.common.tiled.TileSetDefinition;
 
+/**
+ * Makes a pretty maze map based on the generated collision layer.
+ */
 public class MazePainter {
-	private static class Style {
-		protected static final int GROUND_INDEX = 42;
-		public static final int PORTAL_INDEX = 59;
-		protected final int[] wall = { 2, 4, 20, 3, 5, 15, 19, 16, 36, 32, 35, 31, 1 };
+	/**
+	 * Rules for selecting the tiles.
+	 */
+	private static abstract class Style {
+		private final int[] wall = getWallIndices();
 
-		public Style(StendhalMapStructure map) {
-			TileSetDefinition set = new TileSetDefinition("filler", 1);
-			set.setSource("../../tileset/ground/gravel.png");
-			map.addTileset(set);
+		/**
+		 * Get the tile indices used for walls. <br>
+		 * Wall tile indices:
+		 * <ol>
+		 * 	<li>top left convex corner</li>
+		 * 	<li>bottom left convex corner</li>
+		 * 	<li>left vertical wall</li>
+		 * 	<li>top right convex corner</li>
+		 *  <li>bottom right convex corner</li>
+		 *  <li>right vertical wall</li>
+		 *  <li>top horizontal wall</li>
+		 *  <li>bottom horizontal wall</li>
+		 *  <li>top left concave corner</li>
+		 *  <li>bottom left concave corner</li>
+		 *	<li>top right concave corner</li>
+		 *  <li>bottom right concave corner</li>
+		 * 	<li>outside</li>
+		 * </ol>
+		 * 
+		 * @return list of indices
+		 */
+		abstract int[] getWallIndices();
+		/**
+		 * Get the index of the ground tile.
+		 * 
+		 * @return ground tile index
+		 */
+		abstract int getGroundIndex();
+		/**
+		 * Get the index of the portal tile.
+		 * 
+		 * @return portal tile index
+		 */
+		abstract int getPortalIndex();
 
-			set = new TileSetDefinition("outercorners", 2);
-			set.setSource("../../tileset/building/wall/int_wall_dark_red_corners_2.png");
-			map.addTileset(set);
-
-			set = new TileSetDefinition("wall", 10);
-			set.setSource("../../tileset/building/wall/int_wall_dark_red.png");
-			map.addTileset(set);
-
-			set = new TileSetDefinition("innercorners", 26);
-			set.setSource("../../tileset/building/wall/int_wall_dark_red_corners.png");
-			map.addTileset(set);
-
-			set = new TileSetDefinition("paving", 42);
-			set.setSource("../../tileset/ground/brown_paving.png");
-			map.addTileset(set);
-
-			set = new TileSetDefinition("portal", 44);
-			set.setSource("../../tileset/building/decoration/floor_sparkle.png");
-			map.addTileset(set);
-		}
-
+		/**
+		 * Select tile to a location.
+		 * 
+		 * @param collision collision layer
+		 * @param x
+		 * @param y
+		 * @return tile index
+		 */
 		public int selectTile(LayerDefinition collision, int x, int y) {
 			if (collision.getTileAt(x, y) == 0) {
-				return GROUND_INDEX;
+				// nothing
+				return 0;
 			} else {
 				return selectWall(collision, x, y);
 			}
 		}
 
+		/**
+		 * Select a wall tile.
+		 * 
+		 * @param collision collision layer
+		 * @param x
+		 * @param y
+		 * @return tile index
+		 */
 		private int selectWall(LayerDefinition collision, int x, int y) {
 			if (!collides(collision, x - 1, y)) {
 				if (!collides(collision, x, y - 1)) {
@@ -102,6 +133,16 @@ public class MazePainter {
 			return wall[wall.length - 1];
 		}
 
+		/**
+		 * Check if the tile at location (x, y) is a collision or outside the
+		 * map.
+		 * 
+		 * @param collision
+		 * @param x
+		 * @param y
+		 * @return <code>true</code> if the location is a collision, otherwise
+		 * 	<code>false</code>
+		 */
 		private boolean collides(LayerDefinition collision, int x, int y) {
 			if (x < 0 || y < 0 || x >= collision.getWidth() || y >= collision.getWidth()) {
 				return true;
@@ -110,31 +151,168 @@ public class MazePainter {
 			}
 		}
 	}
+	
+	/**
+	 * Style that paints the walls using the int_wall_dark_red_* set.
+	 */
+	private static class RedWallStyle extends Style {
+		public RedWallStyle(StendhalMapStructure map) {
+			TileSetDefinition set = new TileSetDefinition("filler", 1);
+			set.setSource("../../tileset/ground/gravel.png");
+			map.addTileset(set);
 
+			set = new TileSetDefinition("outercorners", 2);
+			set.setSource("../../tileset/building/wall/int_wall_dark_red_corners_2.png");
+			map.addTileset(set);
+
+			set = new TileSetDefinition("wall", 10);
+			set.setSource("../../tileset/building/wall/int_wall_dark_red.png");
+			map.addTileset(set);
+
+			set = new TileSetDefinition("innercorners", 26);
+			set.setSource("../../tileset/building/wall/int_wall_dark_red_corners.png");
+			map.addTileset(set);
+
+			set = new TileSetDefinition("paving", 42);
+			set.setSource("../../tileset/ground/brown_paving.png");
+			map.addTileset(set);
+
+			set = new TileSetDefinition("portal", 44);
+			set.setSource("../../tileset/building/decoration/floor_sparkle.png");
+			map.addTileset(set);
+		}
+
+		@Override
+		int[] getWallIndices() {
+			return new int[] { 2, 4, 20, 3, 5, 15, 19, 16, 36, 32, 35, 31, 1 };
+		}
+
+		@Override
+		int getGroundIndex() {
+			return 42;
+		}
+
+		@Override
+		int getPortalIndex() {
+			return 59;
+		}
+	}
+	
+	/**
+	 * Style that paints the walls using the grey decorated wall tiles.
+	 */
+	private static class GreyWallStyle extends Style {
+		// Pick a random one from the base ground colors
+		private int groundIndex = 122 + Rand.rand(3);
+		
+		public GreyWallStyle(StendhalMapStructure map) {
+			TileSetDefinition set = new TileSetDefinition("wall", 1);
+			set.setSource("../../tileset/building/wall/int_grey.png");
+			map.addTileset(set);
+			
+			set = new TileSetDefinition("floor", 122);
+			set.setSource("../../tileset/ground/indoor/floor.png");
+			map.addTileset(set);
+			
+			set = new TileSetDefinition("portal", 125);
+			set.setSource("../../tileset/building/decoration/floor_sparkle.png");
+			map.addTileset(set);
+		}
+
+		@Override
+		int[] getWallIndices() {
+			return new int[] { 84, 40, 65, 82, 38, 57, 105, 17, 106, 18, 104, 16, 23 };
+		}
+
+		@Override
+		int getGroundIndex() {
+			return groundIndex;
+		}
+
+		@Override
+		int getPortalIndex() {
+			return 138;
+		}
+	}
+	
+	private Style style;
+
+	/**
+	 * Paint tiles to a map according to a maps collision layer.
+	 * 
+	 * @param map
+	 */
 	public void paint(StendhalMapStructure map) {
 		LayerDefinition collision = map.getLayer("collision");
-		LayerDefinition ground = map.getLayer("0_floor");
+		
+		int i = Rand.rand(2);
+		switch (i) {
+		case 0: style = new GreyWallStyle(map);
+		break;
+		case 1: 
+		default:
+			style = new RedWallStyle(map);
+		}
 
+		LayerDefinition ground = map.getLayer("0_floor");
 		// prepare the floor data arrays for for painting
 		ground.build();
-
-		Style style = new Style(map);
-
-		drawFloor(style, ground, collision);
+		fillFloor(style, ground);
+		
+		// Walls
+		ground = map.getLayer("1_terrain");
+		ground.build();
+		drawTerrain(style, ground, collision);
 	}
 
+	/**
+	 * Paint the portal tile at specified location. Can not be called before
+	 * paint().
+	 * 
+	 * @param map
+	 * @param x
+	 * @param y
+	 */
 	public void paintPortal(StendhalMapStructure map, int x, int y) {
+		if (style == null) {
+			throw new IllegalStateException("paint() must be called before paintPortal().");
+		}
 		LayerDefinition ground = map.getLayer("0_floor");
-		ground.set(x, y, Style.PORTAL_INDEX);
+		ground.set(x, y, style.getPortalIndex());
 	}
-
-	private void drawFloor(Style style, LayerDefinition floor, LayerDefinition collision) {
+	
+	/**
+	 * Fill the ground layer with the ground tile.
+	 * 
+	 * @param style
+	 * @param floor
+	 */
+	private void fillFloor(Style style, LayerDefinition floor) {
 		int width = floor.getWidth();
 		int height = floor.getHeight();
 
+		int ground = style.getGroundIndex();
 		for (int x = 0; x < width; x++) {
 			for (int y = 0; y < height; y++) {
-				floor.set(x, y, style.selectTile(collision, x, y));
+				floor.set(x, y, ground);
+			}
+		}
+	}
+
+	/**
+	 * Paint the walls.
+	 * 
+	 * @param style
+	 * @param terrain
+	 * @param collision
+	 */
+	private void drawTerrain(Style style, LayerDefinition terrain, LayerDefinition collision) {
+		int width = terrain.getWidth();
+		int height = terrain.getHeight();
+
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				terrain.set(x, y, style.selectTile(collision, x, y));
 			}
 		}
 	}
