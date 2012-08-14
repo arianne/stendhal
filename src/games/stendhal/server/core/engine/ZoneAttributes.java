086e52b15927dbbf94f88bb25839c04247c8bcbe
yoriy <yoriy@users.sourceforge.net>
2012-08-14 19:11:23 +0000

    private class extracted from ZoneAttribute

--- src/games/stendhal/server/core/engine/ZoneAttributes.java
+++ src/games/stendhal/server/core/engine/ZoneAttributes.java
@@ -12,15 +12,10 @@
 package games.stendhal.server.core.engine;
 
 import games.stendhal.common.CRC;
-import games.stendhal.server.core.events.TurnListener;
-import games.stendhal.server.core.rp.DaylightPhase;
-import games.stendhal.server.core.rp.StendhalRPAction;
-import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.core.rp.DaylightUpdater;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -82,7 +77,7 @@ public class ZoneAttributes {
 	 *
 	 * @return zone
 	 */
-	StendhalRPZone getZone() {
+	public StendhalRPZone getZone() {
 		return zone;
 	}
 
@@ -120,7 +115,7 @@ public class ZoneAttributes {
 	 *
 	 * @param key attribute name
 	 */
-	void remove(String key) {
+	public void remove(String key) {
 		attr.remove(key);
 		invalidate();
 	}
@@ -174,105 +169,4 @@ public class ZoneAttributes {
 		valid = true;
 	}
 
-	/**
-	 * Manager for daylight colored zones.
-	 */
-	private static class DaylightUpdater implements TurnListener {
-		/** Time between checking if the color should be changed. Seconds. */
-		private static final int CHECK_INTERVAL = 61;
-		/** Singleton instance. */
-		private static final DaylightUpdater instance = new DaylightUpdater();
-		/** Color corresponding to the current time. */
-		Integer currentColor;
-
-		/** Managed zones, and their attributes */
-		private final List<ZoneAttributes> zones = new ArrayList<ZoneAttributes>();
-
-		/**
-		 * Create a new Daylight instance. Do not use this.
-		 */
-		private DaylightUpdater() {
-			onTurnReached(0);
-		}
-
-		/**
-		 * Get the Daylight instance.
-		 *
-		 * @return singleton instance
-		 */
-		static DaylightUpdater get() {
-			return instance;
-		}
-
-		/**
-		 * Make a zone color managed by the daylight colorer.
-		 *
-		 * @param attr attributes of the zone
-		 */
-		void manageAttributes(ZoneAttributes attr) {
-			zones.add(attr);
-			// Set the initial values
-			attr.put("color_method", "multiply");
-			setZoneColor(attr, currentColor);
-		}
-
-		public void onTurnReached(int currentTurn) {
-			updateDaytimeColor();
-			SingletonRepository.getTurnNotifier().notifyInSeconds(CHECK_INTERVAL, this);
-		}
-
-		/**
-		 * Update the zone color according to the hour.
-		 */
-		private void updateDaytimeColor() {
-			setZoneColors(DaylightPhase.current().getColor());
-		}
-
-		/**
-		 * Set the current daylight color. Notifies all the managed zones if
-		 * the color has changed.
-		 *
-		 * @param color
-		 */
-		private void setZoneColors(Integer color) {
-			if (((color == null) && (currentColor != null))
-					|| ((color != null) && !color.equals(currentColor))) {
-				for (ZoneAttributes attr : zones) {
-					setZoneColor(attr, color);
-					if (color != null) {
-						attr.put("blend_method", "bleach");
-					} else {
-						attr.remove("blend_method");
-					}
-				}
-			}
-			currentColor = color;
-		}
-
-		/**
-		 * Set the color of a zone. Sets the blend mode of the effect layers to
-		 * bleach, if needed. Notifies all the players with a recent enough
-		 * client.
-		 *
-		 * @param attr attributes of the zone
-		 * @param color new color value
-		 */
-		private void setZoneColor(ZoneAttributes attr, Integer color) {
-			if (color == null) {
-				attr.remove("color");
-				attr.remove("blend_method");
-			} else {
-				attr.put("color", color.toString());
-				attr.put("blend_method", "bleach");
-			}
-			// Notify resident players about the changed color
-			for (Player player : attr.getZone().getPlayers()) {
-				// Old clients do not understand content transfer that just
-				// update the old map, and end up with no entities on the screen
-				if (player.isClientNewerThan("0.97")) {
-					StendhalRPAction.transferContent(player);
-				}
-			}
-		}
-	}
 }
---
+++ src/games/stendhal/server/core/rp/DaylightUpdater.java
@@ -0,0 +1,112 @@
+package games.stendhal.server.core.rp;
+
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.ZoneAttributes;
+import games.stendhal.server.core.events.TurnListener;
+import games.stendhal.server.entity.player.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Manager for daylight colored zones.
+ */
+public class DaylightUpdater implements TurnListener {
+	/** Time between checking if the color should be changed. Seconds. */
+	private static final int CHECK_INTERVAL = 61;
+	/** Singleton instance. */
+	private static final DaylightUpdater instance = new DaylightUpdater();
+	/** Color corresponding to the current time. */
+	Integer currentColor;
+
+	/** Managed zones, and their attributes */
+	private final List<ZoneAttributes> zones = new ArrayList<ZoneAttributes>();
+
+	/**
+	 * Create a new Daylight instance. Do not use this.
+	 */
+	private DaylightUpdater() {
+		onTurnReached(0);
+	}
+
+	/**
+	 * Get the Daylight instance.
+	 *
+	 * @return singleton instance
+	 */
+	public static DaylightUpdater get() {
+		return instance;
+	}
+
+	/**
+	 * Make a zone color managed by the daylight colorer.
+	 *
+	 * @param attr attributes of the zone
+	 */
+	public void manageAttributes(ZoneAttributes attr) {
+		zones.add(attr);
+		// Set the initial values
+		attr.put("color_method", "multiply");
+		setZoneColor(attr, currentColor);
+	}
+
+	public void onTurnReached(int currentTurn) {
+		updateDaytimeColor();
+		SingletonRepository.getTurnNotifier().notifyInSeconds(CHECK_INTERVAL, this);
+	}
+
+	/**
+	 * Update the zone color according to the hour.
+	 */
+	private void updateDaytimeColor() {
+		setZoneColors(DaylightPhase.current().getColor());
+	}
+
+	/**
+	 * Set the current daylight color. Notifies all the managed zones if
+	 * the color has changed.
+	 *
+	 * @param color
+	 */
+	private void setZoneColors(Integer color) {
+		if (((color == null) && (currentColor != null))
+				|| ((color != null) && !color.equals(currentColor))) {
+			for (ZoneAttributes attr : zones) {
+				setZoneColor(attr, color);
+				if (color != null) {
+					attr.put("blend_method", "bleach");
+				} else {
+					attr.remove("blend_method");
+				}
+			}
+		}
+		currentColor = color;
+	}
+
+	/**
+	 * Set the color of a zone. Sets the blend mode of the effect layers to
+	 * bleach, if needed. Notifies all the players with a recent enough
+	 * client.
+	 *
+	 * @param attr attributes of the zone
+	 * @param color new color value
+	 */
+	private void setZoneColor(ZoneAttributes attr, Integer color) {
+		if (color == null) {
+			attr.remove("color");
+			attr.remove("blend_method");
+		} else {
+			attr.put("color", color.toString());
+			attr.put("blend_method", "bleach");
+		}
+		// Notify resident players about the changed color
+		for (Player player : attr.getZone().getPlayers()) {
+			// Old clients do not understand content transfer that just
+			// update the old map, and end up with no entities on the screen
+			if (player.isClientNewerThan("0.97")) {
+				StendhalRPAction.transferContent(player);
+			}
+		}
+	}
+}
+
