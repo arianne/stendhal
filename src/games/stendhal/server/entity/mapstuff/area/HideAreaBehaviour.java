c2c2cd70d71c9f55add477a5863c5c081525c041
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-08-15 00:40:33 +0000

    factored NoLoginAreaBehaviour and HideAreaBehaviour out of NoLoginArea

---
+++ src/games/stendhal/server/entity/mapstuff/area/HideAreaBehaviour.java
@@ -0,0 +1,29 @@
+/***************************************************************************
+ *                (C) Copyright 2003-2012 - Faiumoni e. V.                 *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.entity.mapstuff.area;
+
+/**
+ * hides the area from the client
+ *
+ * @author hendrik
+ */
+public class HideAreaBehaviour implements AreaBehaviour {
+
+	public void addToWorld(AreaEntity parentAreaEntity) {
+		parentAreaEntity.hide();
+	}
+
+	public void removeFromWorld() {
+		return;
+	}
+
+}
---
+++ src/games/stendhal/server/entity/mapstuff/area/NoLoginAreaBehaviour.java
@@ -0,0 +1,66 @@
+/***************************************************************************
+ *                (C) Copyright 2003-2012 - Faiumoni e. V.                 *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.entity.mapstuff.area;
+
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.events.LoginListener;
+import games.stendhal.server.entity.player.Player;
+
+/**
+ * prevents logging in inside the specified area
+ *
+ * @author hendrik
+ */
+public class NoLoginAreaBehaviour implements LoginListener, AreaBehaviour {
+	private AreaEntity areaEntity;
+	private String message;
+	private int newX;
+	private int newY;
+
+	/**
+	 * Create a nologin area.
+	 * 
+	 * @param newX
+	 *            x position to place the player at
+	 * @param newY
+	 *            y position to place the player at
+	 * @param message
+	 *            The message to send to the user when repositioned.
+	 */
+	public NoLoginAreaBehaviour(final int newX, final int newY, final String message) {
+		this.newX = newX;
+		this.newY = newY;
+		this.message = message;
+	}
+
+	public void addToWorld(AreaEntity parentAreaEntity) {
+		this.areaEntity = parentAreaEntity;
+		SingletonRepository.getLoginNotifier().addListener(this);
+	}
+
+	public void removeFromWorld() {
+		SingletonRepository.getLoginNotifier().removeListener(this);
+	}
+
+	public void onLoggedIn(final Player player) {
+		if (player.getZone().equals(areaEntity.getZone())) {
+			if (areaEntity.getArea().contains(player.getX(), player.getY())) {
+				player.setPosition(newX, newY);
+
+				if (message != null) {
+					player.sendPrivateText(message);
+				}
+			}
+		}
+	}
+
+}
