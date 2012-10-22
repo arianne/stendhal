4a963d4356a9c07fc7e3e9c38e2f4584d2288f38
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-10-22 20:12:55 +0000

    set path of npc

---
+++ src/games/stendhal/server/script/SetPathNPC.java
@@ -0,0 +1,45 @@
+package games.stendhal.server.script;
+
+import games.stendhal.common.MathHelper;
+import games.stendhal.common.NotificationType;
+import games.stendhal.server.core.pathfinder.FixedPath;
+import games.stendhal.server.core.pathfinder.Node;
+import games.stendhal.server.core.scripting.ScriptImpl;
+import games.stendhal.server.entity.npc.NPCList;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.player.Player;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/**
+ * sets the path of an NPC
+ *
+ * @author hendrik
+ */
+public class SetPathNPC extends ScriptImpl {
+
+	@Override
+	public void execute(Player admin, List<String> args) {
+		super.execute(admin, args);
+		if (args.size() != 2) {
+			admin.sendPrivateText(NotificationType.ERROR, "/script SetPathNPC npc \"x1 y1 x2 y2 x3 y3\"");
+			return;
+		}
+		List<Node> nodes = new LinkedList<Node>();
+		StringTokenizer st = new StringTokenizer(args.get(1));
+		while (st.hasMoreElements()) {
+			String x = st.nextToken();
+			if (!st.hasMoreElements()) {
+				break;
+			}
+			String y = st.nextToken();
+			nodes.add(new Node(MathHelper.parseInt(x), MathHelper.parseInt(y)));
+		}
+		SpeakerNPC npc = NPCList.get().get(args.get(0));
+		FixedPath path = new FixedPath(nodes, true);
+		npc.setPath(path);
+	}
+
+}
