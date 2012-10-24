fcc581359fd2f09047a624ea2d91cfb5ac227b6b
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-10-24 22:31:32 +0000

    parse cia mails

--- src/games/stendhal/bot/postman/CiaHandler.java
+++ src/games/stendhal/bot/postman/CiaHandler.java
@@ -4,6 +4,19 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 /**
  * listens for udp messages
@@ -19,17 +32,105 @@ public class CiaHandler {
 	 * @return IRC command
 	 * @throws IOException in case of an input/output error
 	 */
-	public String process(InputStream is) throws IOException {
+	public List<CiaMessage> read(InputStream is) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
 		try {
 			skipHeader(br);
+			Element root = readXml(br);
 
-			// TODO: parse xml
-			// TODO: format message
+			// read header
+			Element source = getChild(root, "source");
+			String module = getChildText(source, "module");
+			String branch = getChildText(source, "branch");
 
-			return null;
+			// read commits
+			List<CiaMessage> list = new LinkedList<CiaMessage>();
+			NodeList nodes = getChild(root, "body").getChildNodes();
+			for (int i = 0; i < nodes.getLength(); i++) {
+				Node node = nodes.item(i);
+				if ((node instanceof Element) && node.getLocalName().equals("commit")) {
+					CiaMessage msg = new CiaMessage();
+					msg.setModule(module);
+					msg.setBranch(branch);
+					msg.setAuthor(getChildText(node, "author"));
+					msg.setRevision(getChildText(node, "revision"));
+
+					NodeList fileNodes = node.getChildNodes();
+					for (int j = 0; i < fileNodes.getLength(); i++) {
+						Node fileNode = nodes.item(j);
+						if ((fileNode instanceof Element) && node.getLocalName().equals("file")) {
+							msg.addFile(fileNode.getNodeValue().trim());
+						}
+					}
+					msg.setMessage(getChildText(node, "log"));
+					list.add(msg);
+				}
+			}
+
+			return list;
 		} finally {
 			br.close();
+		}
+	}
+
+	/**
+	 * gets a child element from an XML structure
+	 *
+	 * @param element Element
+	 * @param name name of child
+	 * @return Element
+	 * @throws IOException if the child does not exist
+	 */
+	private static Element getChild(Element element, String name) throws IOException {
+		NodeList list = element.getChildNodes();
+		for (int i = 0; i < list.getLength(); i++) {
+			Node node = list.item(i);
+			if ((node instanceof Element) && node.getLocalName().equals(name)) {
+				return (Element) list.item(i);
+			}
+		}
+		throw new IOException("Element " + element.getLocalName() + " does not have expected child " + name);
+	}
+
+	/**
+	 * gets the content of a child node or null
+	 *
+	 * @param element parent element
+	 * @param name name of child
+	 * @return content
+	 */
+	private static String getChildText(Node element, String name) {
+		NodeList list = element.getChildNodes();
+		for (int i = 0; i < list.getLength(); i++) {
+			Node node = list.item(i);
+			if ((node instanceof Element) && node.getLocalName().equals(name)) {
+				Element e = (Element) list.item(i);
+				String value = e.getNodeValue();
+				if (value != null) {
+					value = value.trim();
+				}
+				return value;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @param br
+	 * @return
+	 * @throws IOException
+	 * @throws SAXException
+	 */
+	private Element readXml(BufferedReader br) throws IOException {
+		try {
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder builder = dbf.newDocumentBuilder();
+			Document doc = builder.parse(new InputSource(br));
+			return doc.getDocumentElement();
+		} catch (ParserConfigurationException e) {
+			throw new IOException(e);
+		} catch (SAXException e) {
+			throw new IOException(e);
 		}
 	}
 
---
+++ src/games/stendhal/bot/postman/CiaMessage.java
@@ -0,0 +1,180 @@
+package games.stendhal.bot.postman;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * a CIA message
+ *
+ * @author hendrik
+ */
+public class CiaMessage {
+	private String author;
+	private String branch;
+	private String revision;
+	private String module;
+	private final List<String> files = new LinkedList<String>();
+	private String message;
+
+	/**
+	 * @return the author
+	 */
+	protected String getAuthor() {
+		return author;
+	}
+
+	/**
+	 * @param author the author to set
+	 */
+	protected void setAuthor(String author) {
+		this.author = author;
+	}
+
+	/**
+	 * @return the branch
+	 */
+	protected String getBranch() {
+		return branch;
+	}
+
+	/**
+	 * @param branch the branch to set
+	 */
+	protected void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+	/**
+	 * @return the revision
+	 */
+	protected String getRevision() {
+		return revision;
+	}
+
+	/**
+	 * @param revision the revision to set
+	 */
+	protected void setRevision(String revision) {
+		this.revision = revision;
+	}
+
+	/**
+	 * @return the module
+	 */
+	protected String getModule() {
+		return module;
+	}
+
+	/**
+	 * @param module the module to set
+	 */
+	protected void setModule(String module) {
+		this.module = module;
+	}
+
+	/**
+	 * @return the files
+	 */
+	protected String getFiles() {
+		// TODO: format nicely
+		return files.toString();
+	}
+
+	/**
+	 * @param file to add
+	 */
+	protected void addFile(String file) {
+		this.files.add(file);
+	}
+
+	/**
+	 * @return the message
+	 */
+	protected String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	protected void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((branch == null) ? 0 : branch.hashCode());
+		result = prime * result + ((files == null) ? 0 : files.hashCode());
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
+		result = prime * result + ((module == null) ? 0 : module.hashCode());
+		result = prime * result + ((revision == null) ? 0 : revision.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		CiaMessage other = (CiaMessage) obj;
+		if (author == null) {
+			if (other.author != null) {
+				return false;
+			}
+		} else if (!author.equals(other.author)) {
+			return false;
+		}
+		if (branch == null) {
+			if (other.branch != null) {
+				return false;
+			}
+		} else if (!branch.equals(other.branch)) {
+			return false;
+		}
+		if (files == null) {
+			if (other.files != null) {
+				return false;
+			}
+		} else if (!files.equals(other.files)) {
+			return false;
+		}
+		if (message == null) {
+			if (other.message != null) {
+				return false;
+			}
+		} else if (!message.equals(other.message)) {
+			return false;
+		}
+		if (module == null) {
+			if (other.module != null) {
+				return false;
+			}
+		} else if (!module.equals(other.module)) {
+			return false;
+		}
+		if (revision == null) {
+			if (other.revision != null) {
+				return false;
+			}
+		} else if (!revision.equals(other.revision)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "CiaMessage [author=" + author + ", branch=" + branch + ", revision=" + revision + ", module="
+				+ module + ", files=" + files + ", message=" + message + "]";
+	}
+
+}
