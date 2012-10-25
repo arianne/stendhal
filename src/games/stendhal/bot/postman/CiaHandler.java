05c7667fd93d808ce03b2f6511139fd6f913472f
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-10-25 17:36:34 +0000

    for CiaMessage for IRC

--- src/games/stendhal/bot/postman/CiaHandler.java
+++ src/games/stendhal/bot/postman/CiaHandler.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.jibble.pircbot.Colors;
 import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
 
@@ -60,7 +61,46 @@ public class CiaHandler {
 //	20:33 < CIA-18> arianne_rpg: madmetzger * r464dd9c4d5bb marauroa/ (18 files in 13 dirs): Merge branch 'master' of ssh://madmetzger@arianne.git.sourceforge.net/gitroot/arianne/marauroa
 //	18:15 < CIA-27> arianne_rpg: kiheru * stendhal/src/games/stendhal/client/gui/ (6 files in 2 dirs): Autoinspect containers. Removed the old, more complicated, approach
 
+	/**
+	 * formats a CiaMessage for IRC
+	 *
+	 * @param msg CiaMessage
+	 * @return formatted irc line
+	 */
+	public String format(CiaMessage msg) {
+		StringBuilder res = new StringBuilder();
+		addFormattedToken(res, Colors.GREEN, msg.getAuthor());
+		if ((msg.getBranch() != null) && (!msg.getBranch().trim().isEmpty())) {
+			res.append(" ");
+			addFormattedToken(res, Colors.BROWN, msg.getBranch());
+		}
+		res.append(" *");
+		if ((msg.getRevision() != null) && (!msg.getRevision().trim().isEmpty())) {
+			res.append(" r");
+			addFormattedToken(res, Colors.BOLD, msg.getRevision());
+		}
+		res.append(" ");
+		addFormattedToken(res, Colors.CYAN, msg.getModule());
+		res.append("/");
+		res.append(msg.getFiles());
+		addFormattedToken(res, Colors.BOLD, ":");
+		res.append(" ");
+		res.append(msg.getMessage());
+		return res.toString();
+	}
 
+	/**
+	 * formats a token
+	 *
+	 * @param out    output buffer
+	 * @param format color or format to use
+	 * @param token  message
+	 */
+	private void addFormattedToken(StringBuilder out, String format, String token) {
+		out.append(format);
+		out.append(Colors.removeFormattingAndColors(token));
+		out.append(Colors.NORMAL);
+	}
 
 	/**
 	 * skips the mail header
