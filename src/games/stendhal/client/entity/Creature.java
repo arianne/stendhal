76966803d668802c6e41149169878ba8c60c00ce
Kimmo Rundelin <kiheru@users.sourceforge.net>
2013-01-19 21:53:18 +0000

    Removed client side generation of creature sounds

--- src/games/stendhal/client/entity/Creature.java
+++ src/games/stendhal/client/entity/Creature.java
@@ -94,50 +94,12 @@ public class Creature extends AudibleEntity {
 	@Override
 	public void initialize(final RPObject object) {
 		super.initialize(object);
-		
-		final String type = getType();
 
 		if (object.has("name")) {
-			final String name = object.get("name");
-			
 			addSounds(SoundLayer.FIGHTING_NOISE.groupName, "attack",
 				"punch-1"   , "punch-2", "punch-3",
 				"punch-4"   , "punch-5", "punch-6",
 				"swingaxe-1", "slap-1" , "arrow-1");
-
-			if (type.startsWith("creature")) {
-				if (name.equals("wolf")) {
-					addSounds(SoundLayer.CREATURE_NOISE.groupName, "move", "bark-1", "howl-5", "howl-2", "howl-11");
-				} else if (name.equals("rat") || name.equals("caverat") || name.equals("venomrat")) {
-					addSounds(SoundLayer.CREATURE_NOISE.groupName, "move", "rats-2", "rats-41"/*, "rats-3*/);
-				} else if (name.equals("razorrat")) {
-					addSounds(SoundLayer.CREATURE_NOISE.groupName, "move", "rats-1");
-				} else if (name.equals("gargoyle")) {
-					addSounds(SoundLayer.CREATURE_NOISE.groupName, "move", "hyena-1", "hyena-2", "hyena-3");
-				} else if (name.equals("boar")) {
-					addSounds(SoundLayer.CREATURE_NOISE.groupName, "move", "pig-1", "pig-2");
-				} else if (name.equals("bear")) {
-					addSounds(SoundLayer.CREATURE_NOISE.groupName, "move", "bear-1", "bear-2", "bear-3");
-				} else if (name.equals("giantrat")) {
-					addSounds(SoundLayer.CREATURE_NOISE.groupName, "move", "bobcat-1", "leopard-11");
-				} else if (name.equals("cobra")) {
-					addSounds(SoundLayer.CREATURE_NOISE.groupName, "move", "snake-1");
-				} else if (name.equals("kobold")) {
-					addSounds(SoundLayer.CREATURE_NOISE.groupName, "move", "panda-1", "panda-2", "racoon-1", "lama-1");
-				} else if (name.equals("goblin")) {
-					addSounds(SoundLayer.CREATURE_NOISE.groupName, "move", "saur-3", "saur-4");
-				} else if (name.equals("troll")) {
-					addSounds(SoundLayer.CREATURE_NOISE.groupName, "move", "gorilla-1", "gorilla-2", "gorilla-3", "gorilla-4", "gorilla-5");
-				} else if (name.equals("orc")) {
-					addSounds(SoundLayer.CREATURE_NOISE.groupName, "move", "lion-11", "lion-22");
-				} else if (name.equals("ogre")) {
-					addSounds(SoundLayer.CREATURE_NOISE.groupName, "move", "yell-1", "groan-1", "moan-1", "fart-1");
-				} else if (name.equals("skeleton")) {
-					addSounds(SoundLayer.CREATURE_NOISE.groupName, "move", "bones-1", "evillaugh-3", "evillaugh-5", "ghost-1", "ghost-2");
-				} else if (name.equals("cyclops")) {
-					addSounds(SoundLayer.CREATURE_NOISE.groupName, "move", "yell-1", "laugh-33", "evillaugh-4", "grunt-1", "grunt-2");
-				}
-			}
 		}
 
 		if (object.has("metamorphosis")) {
@@ -145,20 +107,6 @@ public class Creature extends AudibleEntity {
 		} else {
 			metamorphosis = null;
 		}
-	}
-
-	/**
-	 * When the entity's position changed.
-	 * 
-	 * @param x
-	 *            The new X coordinate.
-	 * @param y
-	 *            The new Y coordinate.
-	 */
-	@Override
-	protected void onPosition(final double x, final double y) {
-		super.onPosition(x, y);
-		playRandomSoundFromGroup("creature", "move", 10000);
 	}
 
 	@Override
