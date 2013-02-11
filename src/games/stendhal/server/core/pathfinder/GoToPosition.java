e4e7aab883b53a3038fd377f3cf829d3c7c6c83b
yoriy <yoriy@users.sourceforge.net>
2013-02-11 20:01:26 +0000

    added checks for how long path-to-walk is

--- src/games/stendhal/server/core/pathfinder/GoToPosition.java
+++ src/games/stendhal/server/core/pathfinder/GoToPosition.java
@@ -43,6 +43,17 @@ public class GoToPosition implements Observer {
 		finishnotifier.setObserver(o);		
 		this.position = position;
 	}
+	
+	/**
+	 * final part of travel
+	 */
+	public void finish()
+	{
+		// removing ourselves from npc's path end notifications
+		ent.pathnotifier.removeObserver(this);
+		finishnotifier.setChanges();
+		finishnotifier.notifyObservers();
+	}
 
 	/**
 	 * update function
@@ -50,21 +61,30 @@ public class GoToPosition implements Observer {
 	 * @param arg - arguments, not used
 	 */
 	public void update(Observable o, Object arg) {
-		// do we started already?
-		if(!s) {
-			// no, adding ourselves to npc's path end notifications
-			ent.pathnotifier.setObserver(this);
-			path = new FixedPath(Path.searchPath(ent, position.getX(), position.getY()),false);
-			ent.setPath(path);
-			s=true;
+	    // are we at our destination? 
+		Node current = new Node(ent.getX(), ent.getY());
+		if(current.equals(position)) {
+			finish();
 		} else {
-			// yes, removing ourselves from npc's path end notifications ... 
-			ent.pathnotifier.removeObserver(this);
-			// ... and notifying our observers.
-			finishnotifier.setChanges();
-			finishnotifier.notifyObservers();
-			s=false;
+			// do we need to walk?
+		    path = new FixedPath(Path.searchPath(ent, position.getX(), position.getY()),false);
+			if(path.getNodeList().size()==0) {
+				finish();
+			} else {
+				// do we started already?
+				if(s) {
+					// yes, finishing
+					s=false;
+					finish();
+				} else {
+					// no, adding ourselves to npc's path end notifications
+					ent.pathnotifier.setObserver(this);
+					ent.setPath(path);
+					s=true;
+				}
+			}
 		}
-	}	
+	}
 }
+
 
