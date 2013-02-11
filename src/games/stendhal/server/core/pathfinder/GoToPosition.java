3c64e4a22baceb5664791c5593d5f8a75d885e50
yoriy <yoriy@users.sourceforge.net>
2013-02-11 18:42:40 +0000

    added javadoc and commits

--- src/games/stendhal/server/core/pathfinder/GoToPosition.java
+++ src/games/stendhal/server/core/pathfinder/GoToPosition.java
@@ -27,6 +27,7 @@ public class GoToPosition implements Observer {
 		private final Registrator finishnotifier = new Registrator();
 		private FixedPath path;
 		private final Node position;
+		// flag for checking started state
 		private boolean s = false;
 	
 	/**
@@ -43,15 +44,23 @@ public class GoToPosition implements Observer {
 		this.position = position;
 	}
 
-	
+	/**
+	 * update function
+	 * @param o - not used
+	 * @param arg - arguments, not used
+	 */
 	public void update(Observable o, Object arg) {
+		// do we started already?
 		if(!s) {
+			// no, adding ourselves to npc's path end notifications
 			ent.pathnotifier.setObserver(this);
 			path = new FixedPath(Path.searchPath(ent, position.getX(), position.getY()),false);
 			ent.setPath(path);
 			s=true;
 		} else {
+			// yes, removing ourselves from npc's path end notifications ... 
 			ent.pathnotifier.removeObserver(this);
+			// ... and notifying our observers.
 			finishnotifier.setChanges();
 			finishnotifier.notifyObservers();
 			s=false;
