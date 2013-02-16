fb049708964535e5429db210e86fd86ed6e9a436
Jordan Irwin <antumdeluge@users.sourceforge.net>
2013-02-16 03:50:52 +0000

    Updated giant cobra stats and added special drop (venom gland) in snake pit.

---
+++ src/games/stendhal/server/maps/ados/snake_pit/GiantCobraCreature.java
@@ -0,0 +1,47 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.maps.ados.snake_pit;
+
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.rule.EntityManager;
+import games.stendhal.server.entity.creature.Creature;
+import games.stendhal.server.entity.creature.ItemGuardCreature;
+import games.stendhal.server.entity.mapstuff.spawner.CreatureRespawnPoint;
+
+import java.util.Map;
+
+public class GiantCobraCreature implements ZoneConfigurator {
+
+	/**
+	 * Configure a zone.
+	 * 
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		buildSnakePit(zone, attributes);
+	}
+
+	private void buildSnakePit(final StendhalRPZone zone, final Map<String, String> attributes) {
+		final EntityManager manager = SingletonRepository.getEntityManager();
+
+		final Creature creature = new ItemGuardCreature(manager.getCreature("giant cobra"), "venom gland");
+
+		final CreatureRespawnPoint point = new CreatureRespawnPoint(zone, 106, 28, creature, 1);
+
+		zone.add(point);
+	}
+}
