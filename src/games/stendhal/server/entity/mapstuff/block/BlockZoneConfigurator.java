c66718674665b96806ffd7936df9de4b99caa7a6
Markus <madmetzger@users.sourceforge.net>
2013-03-03 12:38:34 +0000

    optional parameter multi documented and implemented a default logic

--- src/games/stendhal/server/entity/mapstuff/block/BlockZoneConfigurator.java
+++ src/games/stendhal/server/entity/mapstuff/block/BlockZoneConfigurator.java
@@ -15,6 +15,9 @@ import java.util.Map;
  * start-x = initial x coordinate, where the block also will be resetted to
  * start-y = initial y coordinate, where the block also will be resetted to
  * 
+ * optional parameters:
+ * multi = boolean flag to allow the block being pushed more than once from its initial position, defaults to false
+ * 
  * @author madmetzger
  */
 public class BlockZoneConfigurator implements ZoneConfigurator {
@@ -24,7 +27,11 @@ public class BlockZoneConfigurator implements ZoneConfigurator {
 			Map<String, String> attributes) {
 		int x = Integer.parseInt(attributes.get("start-x"));
 		int y = Integer.parseInt(attributes.get("start-y"));
-		boolean multiPush = Boolean.parseBoolean(attributes.get("multi"));
+		boolean multiPush = false;
+		final String multiString = attributes.get("multi");
+		if(multiString != null) {
+			multiPush = Boolean.parseBoolean(multiString);
+		}
 		Block b = new Block(x, y, multiPush);
 		zone.add(b, false);
 		zone.addMovementListener(b);
