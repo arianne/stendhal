87746c25956c511941457cd77517cc0925d509d8
Jordan Irwin <antumdeluge@users.sourceforge.net>
2013-04-01 19:34:06 +0000

    Highlighting "Athor" in response to "Klaas".

--- src/games/stendhal/server/maps/semos/apothecary_lab/ApothecaryNPC.java
+++ src/games/stendhal/server/maps/semos/apothecary_lab/ApothecaryNPC.java
@@ -52,7 +52,7 @@ public class ApothecaryNPC implements ZoneConfigurator {
 	            addJob("I used to be an #apothecary, but now I have retired.");
 	            addHelp("I'm sorry, but I don't think there is anything I can help you with.");
 	            addOffer("I have nothing to offer.");
-	            addReply("Klaas", "Oh yes, my good old friend. I used to travel to Athor quite often to gather the very rare #kokuda herb. I got to know Klaas very well as a result.");
+	            addReply("Klaas", "Oh yes, my good old friend. I used to travel to #Athor quite often to gather the very rare #kokuda herb. I got to know Klaas very well as a result.");
 	            addReply("kokuda", "The kokuda is an herb that can only be found inside #Athor Island's labrynth");
 	            addReply("Athor", "You mean you haven't visited Athor? It is a beautiful island. A great place to get away. But stay away from the cannibals' territory. If they invite you for dinner you might never make it home.");
 	            addReply("Apothecary", "I was head researcher of a team that worked for one of Faimouni's most powerful leaders. However this leader became corrupt and demanded that I use my skills to make deadly weapons of war. Anyway, I escaped and have been hiding out here ever since.");
