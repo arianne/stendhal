0fea24d78e5f57688616d3b2eb10c35d8ddb0f55
Markus <madmetzger@users.sourceforge.net>
2013-04-09 18:47:34 +0000

    close never closed reader

--- src/games/stendhal/tools/copytable/ReadTable.java
+++ src/games/stendhal/tools/copytable/ReadTable.java
@@ -31,35 +31,39 @@ public class ReadTable {
 		TransactionPool transactionPool = TransactionPool.get();
 
 		BufferedReader br = new BufferedReader(new FileReader(args[0]));
-		String line = br.readLine();
-		int i = 0;
-		StringBuilder cmd = new StringBuilder();
-		while (line != null) {
-			System.out.println("> " + i);
-			if (line.startsWith("--")) {
+		try {
+			String line = br.readLine();
+			int i = 0;
+			StringBuilder cmd = new StringBuilder();
+			while (line != null) {
+				System.out.println("> " + i);
+				if (line.startsWith("--")) {
+					line = br.readLine();
+					i++;
+					continue;
+				}
+				cmd.append(" " + line);
+				
+				if (cmd.indexOf(";") > -1) {
+					DBTransaction transaction = transactionPool.beginWork();
+					try {
+						if (cmd.indexOf("DROP TABLE") != 0 && cmd.indexOf("CREATE TABLE") != 0) {
+							transaction.execute(line, null);
+						}
+					} catch (SQLException e) {
+						logger.error(cmd, e);
+					}
+					transactionPool.commit(transaction);
+					cmd = new StringBuilder();
+					Thread.sleep(3000);
+				}
+				
+				System.out.println("< " + i);
 				line = br.readLine();
 				i++;
-				continue;
 			}
-			cmd.append(" " + line);
-			
-			if (cmd.indexOf(";") > -1) {
-				DBTransaction transaction = transactionPool.beginWork();
-				try {
-					if (cmd.indexOf("DROP TABLE") != 0 && cmd.indexOf("CREATE TABLE") != 0) {
-						transaction.execute(line, null);
-					}
-				} catch (SQLException e) {
-					logger.error(cmd, e);
-				}
-				transactionPool.commit(transaction);
-				cmd = new StringBuilder();
-				Thread.sleep(3000);
-			}
-
-			System.out.println("< " + i);
-			line = br.readLine();
-			i++;
+		} finally {
+			br.close();
 		}
 	}
 }
