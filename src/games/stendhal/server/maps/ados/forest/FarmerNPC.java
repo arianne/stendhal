851047566ca5052725e5adc85bc24da20c9d2283
Markus <madmetzger@users.sourceforge.net>
2013-04-11 18:07:09 +0000

    Adjust Karl's path to make him not run into the targets and be inconveniently in the way.

--- src/games/stendhal/server/maps/ados/forest/FarmerNPC.java
+++ src/games/stendhal/server/maps/ados/forest/FarmerNPC.java
@@ -54,7 +54,7 @@ public class FarmerNPC implements ZoneConfigurator {
 			@Override
 			protected void createPath() {
 				final List<Node> nodes = new LinkedList<Node>();
-				nodes.add(new Node(64, 75));
+				nodes.add(new Node(64, 76));
 				nodes.add(new Node(64, 86));	
 				nodes.add(new Node(68, 86));	
 				nodes.add(new Node(68, 84));
@@ -90,7 +90,7 @@ public class FarmerNPC implements ZoneConfigurator {
 
 		npc.setDescription("You see Karl, a friendly elderly farmer.");
 		npc.setEntityClass("beardmannpc");
-		npc.setPosition(64, 75);
+		npc.setPosition(64, 76);
 		npc.initHP(100);
 		zone.add(npc);
 	}
