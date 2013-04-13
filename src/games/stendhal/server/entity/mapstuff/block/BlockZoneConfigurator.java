e0e67f911d139ea8bd4947abf6b219cca2cdac95
Markus <madmetzger@users.sourceforge.net>
2013-04-13 16:27:29 +0000

    Implement configuration possibility for sounds

--- src/games/stendhal/server/entity/mapstuff/block/BlockZoneConfigurator.java
+++ src/games/stendhal/server/entity/mapstuff/block/BlockZoneConfigurator.java
@@ -6,6 +6,8 @@ package games.stendhal.server.entity.mapstuff.block;
 import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.StendhalRPZone;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -18,7 +20,9 @@ import java.util.Map;
  * optional parameters:
  * multi = boolean flag to allow the block being pushed more than once from its initial position, defaults to false
  * class = defines the client side representation differing from the default boulder, defaults to block
- * descriptin = override default description
+ * shape = defines special shape to make block fit only on special shaped targets
+ * description = override default description
+ * sounds = comma separated list of sounds which should be played clients side on push
  * 
  * @author madmetzger
  */
@@ -32,6 +36,13 @@ public class BlockZoneConfigurator implements ZoneConfigurator {
 		boolean multiPush = false;
 		final String multiString = attributes.get("multi");
 		final String description = attributes.get("description");
+		final String sounds = attributes.get("sounds");
+		final String shape = attributes.get("shape");
+		
+		List<String> soundList = null;
+		if(sounds!= null) {
+			soundList = Arrays.asList(sounds.split(","));
+		}
 		
 		if(multiString != null) {
 			multiPush = Boolean.parseBoolean(multiString);
@@ -40,10 +51,11 @@ public class BlockZoneConfigurator implements ZoneConfigurator {
 		String style = attributes.get("class");
 		Block b;
 		
+		
 		if(style == null) {
 			b = new Block(x, y, multiPush);
 		} else {
-			b = new Block(x, y, multiPush, style);
+			b = new Block(x, y, multiPush, style, shape, soundList);
 		}
 		
 		if(description != null) {
