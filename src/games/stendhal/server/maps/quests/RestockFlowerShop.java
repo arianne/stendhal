2a062e226f2d0176e376cbe889ae9ba05274f69e
Jordan Irwin <antumdeluge@users.sourceforge.net>
2013-04-29 02:32:22 +0000

    Added more dialogue to prepareBringingStep() and other fixes. (Work in progress).

--- src/games/stendhal/server/maps/quests/RestockFlowerShop.java
+++ src/games/stendhal/server/maps/quests/RestockFlowerShop.java
@@ -21,14 +21,16 @@ import games.stendhal.server.entity.npc.action.IncreaseKarmaAction;
 import games.stendhal.server.entity.npc.action.IncreaseXPAction;
 import games.stendhal.server.entity.npc.action.MultipleActions;
 import games.stendhal.server.entity.npc.action.SayRequiredItemsFromCollectionAction;
+import games.stendhal.server.entity.npc.action.SayTextAction;
+import games.stendhal.server.entity.npc.action.SayTimeRemainingAction;
 import games.stendhal.server.entity.npc.action.SetQuestAction;
 import games.stendhal.server.entity.npc.action.SetQuestAndModifyKarmaAction;
 import games.stendhal.server.entity.npc.action.SetQuestToTimeStampAction;
 import games.stendhal.server.entity.npc.action.StartItemsCollectionWithLimitsAction;
 import games.stendhal.server.entity.npc.condition.AndCondition;
 import games.stendhal.server.entity.npc.condition.NotCondition;
+import games.stendhal.server.entity.npc.condition.PlayerHasItemWithHimCondition;
 import games.stendhal.server.entity.npc.condition.QuestActiveCondition;
-import games.stendhal.server.entity.npc.condition.QuestInStateCondition;
 import games.stendhal.server.entity.npc.condition.TimePassedCondition;
 import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.maps.Region;
@@ -38,7 +40,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 /**
  * QUEST: Restock the Flower Shop
@@ -74,7 +75,7 @@ public class RestockFlowerShop extends AbstractQuest {
 	
 	// Different types of flowers needed in quest
 	private static final List<String> flowerTypes = Arrays.asList(
-			"daisies", "lilia", "pansy", "rose", "zantedesc");
+			"daisies", "lilia", "pansy", "rose", "zantedeschia");
 	public static List<Integer> requestedQuantities = Arrays.asList();
 	
 	private int MAX_FLOWERS = flowerTypes.size() * 15;
@@ -82,7 +83,7 @@ public class RestockFlowerShop extends AbstractQuest {
 	private static String requestedFlowers;
 	
 	// Time player must wait to repeat quest (3 days)
-	private static final int WAIT_TIME = 60 * 24 * 3;
+	private static final int WAIT_TIME = 2;// 60 * 24 * 3;
 	
 	// Quest NPC
 	private final SpeakerNPC npc = npcs.get("Seremela");
@@ -97,7 +98,7 @@ public class RestockFlowerShop extends AbstractQuest {
 		if (player.isQuestInState(QUEST_SLOT, 0, "rejected")) {
 			res.add("Flowers make me sneeze.");
 		}
-		if (player.isQuestInState(QUEST_SLOT, 0, "start")) {
+		else if (!player.isQuestInState(QUEST_SLOT, 0, "done")) {
 			res.add("I have offered to help " + npc.getName() + " restock the flower shop.");
 			
 			final ItemCollection remaining = new ItemCollection();
@@ -106,19 +107,11 @@ public class RestockFlowerShop extends AbstractQuest {
 			// Check to avoid ArrayIndexOutOfBoundsException
 			if (questState.split(";").length > 1) {
 				requestedFlowers = "I still need to bring the following flowers: " + Grammar.enumerateCollection(remaining.toStringList()) + ".";
-				/*
-				for (int f = 0; f <= flowerTypes.size(); f++) {
-					if (player.isQuestInState(QUEST_SLOT, f+1, flowerTypes.get(f))) {
-						flowersBrought += " " + flowerTypes.get(f);
-						if (f != flowerTypes.size()) {
-							flowersBrought += ",";
-						} else {
-							flowersBrought += ".";
-						}
-					}
-				}*/
 				res.add(requestedFlowers);
 			}
+		}
+		else {
+			res.add(npc.getName() + " now has a good supply of flowers.");
 		}
 		
 		return res;
@@ -131,9 +124,8 @@ public class RestockFlowerShop extends AbstractQuest {
 		npc.add(ConversationStates.ATTENDING,
 				ConversationPhrases.QUEST_MESSAGES,
 				new AndCondition(
-						new NotCondition(new QuestInStateCondition(QUEST_SLOT, 0, "start")),
-						new TimePassedCondition(QUEST_SLOT, flowerTypes.size()+1, WAIT_TIME)
-						),
+						new NotCondition(new QuestActiveCondition(QUEST_SLOT)),
+						new TimePassedCondition(QUEST_SLOT, 1, WAIT_TIME)),
 				ConversationStates.QUEST_OFFERED,
 				"The flower shop is running low on flowers. Will help me restock it?",
 				null);
@@ -141,10 +133,18 @@ public class RestockFlowerShop extends AbstractQuest {
 		// Player requests quest after started
 		npc.add(ConversationStates.ATTENDING,
 				ConversationPhrases.QUEST_MESSAGES,
-				new QuestInStateCondition(QUEST_SLOT, 0, "start"),
+				new QuestActiveCondition(QUEST_SLOT),
 				ConversationStates.ATTENDING,
 				"You still haven't brought me the flowers I asked for.",
 				null);
+		
+		// Player requests quest before wait period ended
+		npc.add(ConversationStates.ATTENDING,
+				ConversationPhrases.QUEST_MESSAGES,
+				new NotCondition(new TimePassedCondition(QUEST_SLOT, 1, WAIT_TIME)),
+				ConversationStates.ATTENDING,
+				null,
+				new SayTimeRemainingAction(QUEST_SLOT, 1, "The flowers you brought are selling quickly. I may need your help again in"));
 		
 		// Player accepts quest
 		npc.add(ConversationStates.QUEST_OFFERED,
@@ -153,8 +153,8 @@ public class RestockFlowerShop extends AbstractQuest {
 				ConversationStates.ATTENDING,
 				null,
 				new MultipleActions(
-						new SetQuestAndModifyKarmaAction(QUEST_SLOT, "start", 5.0),
-						new StartItemsCollectionWithLimitsAction(QUEST_SLOT, flowerTypes, MAX_FLOWERS),
+						new IncreaseKarmaAction(5.0),
+						new StartItemsCollectionWithLimitsAction(QUEST_SLOT, 0, flowerTypes, MAX_FLOWERS),
 						new SayRequiredItemsFromCollectionAction(QUEST_SLOT, "Great! Here is what I need: [items]"))
 		);
 		
@@ -169,33 +169,83 @@ public class RestockFlowerShop extends AbstractQuest {
 	
 	
 	private void prepareBringingStep() {
+		
 		final List<ChatAction> reward = new LinkedList<ChatAction>();
 		reward.add(new IncreaseXPAction(1000));
 		reward.add(new IncreaseKarmaAction(25.0));
 		reward.add(new SetQuestAction(QUEST_SLOT, "done"));
-		reward.add(new SetQuestToTimeStampAction(QUEST_SLOT));
+		reward.add(new SetQuestToTimeStampAction(QUEST_SLOT, 1));
+		reward.add(new SayTextAction("Thank you so much! Now I can fill all of my orders."));
 		
 		ChatAction rewardAction = new MultipleActions(reward);
 		
 		/* add triggers for the item names */
-		final ItemCollection items = new ItemCollection();
-		items.addFromQuestStateString(QUEST_SLOT);
-		for (final Map.Entry<String, Integer> item : items.entrySet()) {
+		for (String flower : flowerTypes) {
 			npc.add(ConversationStates.QUESTION_2,
-					item.getKey(),
+					flower,
 					new QuestActiveCondition(QUEST_SLOT),
 					ConversationStates.QUESTION_2,
 					null,
 					new CollectRequestedItemsAction(
-							item.getKey(),
+							flower,
 							QUEST_SLOT,
-							"Thank you! Did you bring any other flowers?",
+							"Thank you! What other flowers did you bring?",
 							"I don't need any more of those flowers.",
 							rewardAction,
 							ConversationStates.IDLE
-							)
-			);
+							));
 		}
+		
+		// NPC sees that player is carrying flowers
+		for (String flower : flowerTypes) {
+			npc.add(ConversationStates.IDLE,
+					ConversationPhrases.GREETING_MESSAGES,
+					new AndCondition(
+							new QuestActiveCondition(QUEST_SLOT),
+							new PlayerHasItemWithHimCondition(flower)),
+					ConversationStates.QUESTION_1,
+					"Did you bring any flowers for the shop?",
+					null);
+		}
+		
+		// Player confirms brought flowers
+		npc.add(ConversationStates.QUESTION_1,
+				ConversationPhrases.YES_MESSAGES,
+				null,
+				ConversationStates.QUESTION_2,
+				"What kind of flowers did you bring?",
+				null);
+		
+		// Player didn't bring flowers
+		npc.add(ConversationStates.QUESTION_1,
+				ConversationPhrases.NO_MESSAGES,
+				null,
+				ConversationStates.ATTENDING,
+				"Please keep looking. Orders are getting backed up.",
+				null);
+		
+		// player offers item that wasn't requested
+		npc.add(ConversationStates.QUESTION_2,
+				"",
+			new QuestActiveCondition(QUEST_SLOT),
+			ConversationStates.QUESTION_2,
+			"I don't think that would look good in the shop.",
+			null);
+		
+		// Player says "bye" or "no" while listing flowers
+		List<String> endDiscussionPhrases = new ArrayList<String>();
+		for (String phrase : ConversationPhrases.NO_MESSAGES) {
+			endDiscussionPhrases.add(phrase);
+		}
+		for (String phrase : ConversationPhrases.GOODBYE_MESSAGES) {
+			endDiscussionPhrases.add(phrase);
+		}
+		npc.add(ConversationStates.QUESTION_2,
+				endDiscussionPhrases,
+				null,
+				ConversationStates.IDLE,
+				"Please come back when you have found some flowers.",
+				null);
 	}
 	
 	
