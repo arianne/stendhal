2aef69b17655040e09b5937186bc0f15fadc0b4b
Jordan Irwin <antumdeluge@users.sourceforge.net>
2013-05-07 00:28:29 +0000

    Fixed checking for null string in setSounds method.

--- src/games/stendhal/server/entity/creature/Creature.java
+++ src/games/stendhal/server/entity/creature/Creature.java
@@ -370,7 +370,7 @@ public class Creature extends NPC {
 	public void setSounds(List<String> sounds) {
 		this.sounds = new ArrayList<String>(sounds);
 		// FIXME: movementSound loops currently not working so adding movement sounds to regular sound list
-		if (!movementSound.equals(null)) {
+		if (movementSound != null) {
 		    this.sounds.add(movementSound);
 		}
 	}
