9b4640f9e4a6be1b45f1a089ff2f80c83e2d58a5
Jordan Irwin <antumdeluge@users.sourceforge.net>
2013-05-23 13:01:27 +0000

    Added second constructor.

--- src/games/stendhal/server/core/config/NPCGroupsXMLLoader.java
+++ src/games/stendhal/server/core/config/NPCGroupsXMLLoader.java
@@ -4,6 +4,7 @@ import games.stendhal.server.core.rule.defaultruleset.DefaultNPC;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -32,9 +33,23 @@ public class NPCGroupsXMLLoader extends DefaultHandler {
     }
     
     /**
-     * Loads creatures
+     * Create an xml based loader of NPC groups.
      * 
-     * @return list of all creatures.
+     * @param uri
+     *            The location of the configuration file.
+     */
+    public NPCGroupsXMLLoader(final String uri) {
+        try {
+            this.uri = new URI(uri);
+        } catch (URISyntaxException e) {
+            logger.error(e, e);
+        }
+    }
+    
+    /**
+     * Loads NPCs
+     * 
+     * @return list of all NPCs.
      */
     public List<DefaultNPC> load() {
         final GroupsXMLLoader groupsLoader = new GroupsXMLLoader(uri);
