bccca8a87547a27c7f3c61739e78f3cb556446bf
Hendrik Brummermann <nhnb@users.sourceforge.net>
2013-09-16 20:25:41 +0000

    added a convenient constructor for eating/poison

--- src/games/stendhal/server/entity/npc/action/InflictStatusOnNPCAction.java
+++ src/games/stendhal/server/entity/npc/action/InflictStatusOnNPCAction.java
@@ -3,10 +3,14 @@ package games.stendhal.server.entity.npc.action;
 import games.stendhal.common.parser.Sentence;
 import games.stendhal.server.core.config.annotations.Dev;
 import games.stendhal.server.core.config.annotations.Dev.Category;
+import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.entity.RPEntity;
+import games.stendhal.server.entity.item.ConsumableItem;
 import games.stendhal.server.entity.npc.ChatAction;
 import games.stendhal.server.entity.npc.EventRaiser;
 import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.entity.status.EatStatus;
+import games.stendhal.server.entity.status.PoisonStatus;
 import games.stendhal.server.entity.status.Status;
 
 /**
@@ -17,6 +21,17 @@ import games.stendhal.server.entity.status.Status;
 @Dev(category = Category.STATS, label="Status")
 public class InflictStatusOnNPCAction implements ChatAction {
 	private Status status;
+	private String itemName;
+
+	/**
+	 * InflictStatusOnNPCAction
+	 *
+	 * @param itemName name of item;
+	 */
+	@Dev
+	public InflictStatusOnNPCAction(String itemName) {
+		this.itemName = itemName;
+	}
 
 	/**
 	 * InflictStatusOnNPCAction
@@ -28,9 +43,22 @@ public class InflictStatusOnNPCAction implements ChatAction {
 	}
 
 	public void fire(Player player, Sentence sentence, EventRaiser npc) {
-		if (npc.getEntity() instanceof RPEntity) {
-			((RPEntity) npc.getEntity()).getStatusList().inflictStatus(status, null);
+		if (! (npc.getEntity() instanceof RPEntity)) {
+			return;
 		}
+
+		Status myStatus = status;
+		ConsumableItem item = null;
+		if (itemName != null) {
+			item = (ConsumableItem) SingletonRepository.getEntityManager().getItem(itemName);
+			if (item.getAmount() > 0) {
+				myStatus = new EatStatus(item.getAmount(), item.getFrecuency(), item.getRegen());
+			} else {
+				myStatus = new PoisonStatus(item.getAmount(), item.getFrecuency(), item.getRegen());
+			}
+		}
+
+		((RPEntity) npc.getEntity()).getStatusList().inflictStatus(myStatus, item);
 	}
 
 }
