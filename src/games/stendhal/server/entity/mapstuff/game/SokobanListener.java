d1b489e6ff4da50c67070b190ab7f9fe8c194565
Hendrik Brummermann <nhnb@users.sourceforge.net>
2013-09-27 07:18:53 +0000

    a listener for evens from the sokoban board

---
+++ src/games/stendhal/server/entity/mapstuff/game/SokobanListener.java
@@ -0,0 +1,36 @@
+/***************************************************************************
+ *                   (C) Copyright 2013 - Faiumoni e. V.                   *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.entity.mapstuff.game;
+
+/**
+ * listens to events from the Sokoban board
+ *
+ * @author hendrik
+ */
+public interface SokobanListener {
+
+	/**
+	 * level was completed successfully
+	 *
+	 * @param playerName name of player
+	 * @param level completed level
+	 */
+	public void onSuccess(String playerName, int level);
+
+	/**
+	 * level timed out
+	 *
+	 * @param playerName name of player
+	 * @param level failed level
+	 */
+	public void onTimeout(String playerName, int level);
+}
