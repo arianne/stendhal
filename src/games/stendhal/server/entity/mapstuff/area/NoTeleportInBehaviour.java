ccc19be4479101175b42941c1219fe0c88bf5945
Hendrik Brummermann <nhnb@users.sourceforge.net>
2013-10-03 18:06:58 +0000

    created AreaEntity behaviours to prevent teleporting in and out respectifly

---
+++ src/games/stendhal/server/entity/mapstuff/area/NoTeleportInBehaviour.java
@@ -0,0 +1,31 @@
+/***************************************************************************
+ *                  (C) Copyright 2013 - Faiumoni e. V.                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.entity.mapstuff.area;
+
+
+/**
+ * prevents teleporting into the specified area
+ *
+ * @author hendrik
+ */
+public class NoTeleportInBehaviour implements AreaBehaviour {
+
+	@Override
+	public void addToWorld(AreaEntity area) {
+		area.getZone().disallowIn(area.getX(), area.getY(), (int) area.getWidth(), (int) area.getHeight());
+	}
+
+	@Override
+	public void removeFromWorld() {
+		// TODO: implement me
+	}
+}
