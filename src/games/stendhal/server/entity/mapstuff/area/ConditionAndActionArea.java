421cd7558be6ab103ad40b0d2e0fa36941057b75
Jordan Irwin <antumdeluge@users.sourceforge.net>
2013-10-23 08:37:11 +0000

    New area type similar to ConditionAndActionPortal.

---
+++ src/games/stendhal/server/entity/mapstuff/area/ConditionAndActionArea.java
@@ -0,0 +1,65 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2013 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.entity.mapstuff.area;
+
+import games.stendhal.server.entity.npc.ChatAction;
+import games.stendhal.server.entity.npc.ChatCondition;
+import games.stendhal.server.entity.player.Player;
+
+public class ConditionAndActionArea extends AreaEntity {
+    private ChatAction action;
+    
+    private ChatCondition condition;
+    
+    private String soundStart;
+    private String soundSuccess;
+    private String soundFail;
+    
+    ConditionAndActionArea(final ChatCondition condition, final ChatAction action) {
+        this.action = action;
+        this.condition = condition;
+        System.out.println("\n\nCREATING!\n\n");
+    }
+    
+    ConditionAndActionArea(final ChatCondition condition, final ChatAction action, final int width, final int height) {
+        super(width, height);
+        this.action = action;
+        this.condition = condition;
+        System.out.println("\n\nCREATING!\n\n");
+    }
+    
+    public boolean use(Player player) {
+        if (action != null) {
+            if ((condition == null) || (condition.fire(player, null, null))) {
+                action.fire(player, null, null);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public ChatAction getAction() {
+        return action;
+    }
+    
+    public ChatCondition getCondition() {
+        return condition;
+    }
+    
+    public void setAction(ChatAction action) {
+        this.action = action;
+    }
+    
+    public void setCondition(ChatCondition condition) {
+        this.condition = condition;
+    }
+}
---
+++ src/games/stendhal/server/entity/mapstuff/area/ConditionAndActionAreaFactory.java
@@ -0,0 +1,96 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2013 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.entity.mapstuff.area;
+
+import org.codehaus.groovy.control.CompilationFailedException;
+
+import games.stendhal.server.core.config.factory.ConfigurableFactory;
+import games.stendhal.server.core.config.factory.ConfigurableFactoryContext;
+import games.stendhal.server.entity.npc.ChatAction;
+import games.stendhal.server.entity.npc.ChatCondition;
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+
+/**
+ * A base factory for <code>ConditionAndActionArea</code> objects.
+ */
+public class ConditionAndActionAreaFactory implements ConfigurableFactory {
+    
+	/**
+	 * Extracts the height from context.
+	 * 
+	 * @param ctx
+	 *            The configuration context.
+	 * @return The height, 1 if unspecified.
+	 */
+	protected int getHeight(final ConfigurableFactoryContext ctx) {
+		return ctx.getInt("height", 1);
+	}
+
+	/**
+	 * Extracts the width from context.
+	 * 
+	 * @param ctx
+	 *            The configuration context.
+	 * @return The width, 1 if unspecified.
+	 */
+	protected int getWidth(final ConfigurableFactoryContext ctx) {
+		return ctx.getInt("width", 1);
+	}
+	
+    protected ChatAction getAction(final ConfigurableFactoryContext ctx) {
+        String value = ctx.getString("action", null);
+        if (value == null) {
+            return null;
+        }
+        Binding groovyBinding = new Binding();
+        final GroovyShell interp = new GroovyShell(groovyBinding);
+        try {
+            String code = "import games.stendhal.server.entity.npc.action.*;\r\n"
+                + value;
+            return (ChatAction) interp.evaluate(code);
+        } catch (CompilationFailedException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+    
+    protected ChatCondition getCondition(final ConfigurableFactoryContext ctx) {
+        String value = ctx.getString("condition", null);
+        if (value == null) {
+            return null;
+        }
+        Binding groovyBinding = new Binding();
+        final GroovyShell interp = new GroovyShell(groovyBinding);
+        try {
+            String code = "import games.stendhal.server.entity.npc.condition.*;\r\n"
+                + value;
+            return (ChatCondition) interp.evaluate(code);
+        } catch (CompilationFailedException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+    
+	/**
+	 * Create a damaging area.
+	 * 
+	 * @param ctx
+	 *            Configuration context.
+	 * 
+	 * @return A OnePlayerArea.
+	 * 
+	 * @see OnePlayerArea
+	 */
+	@Override
+	public Object create(final ConfigurableFactoryContext ctx) {
+		return new ConditionAndActionArea(getCondition(ctx), getAction(ctx), getWidth(ctx), getHeight(ctx));
+	}
+}
