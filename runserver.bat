1ff21299c9722e71adcc0b6cd3098e4c135e5d1d
Jordan Irwin <antumdeluge@users.sourceforge.net>
2013-12-21 20:44:38 +0000

    Changed version to 1.12.

--- runserver.bat
+++ runserver.bat
@@ -1,3 +1,3 @@
-set STENDHAL_VERSION=1.11.5
+set STENDHAL_VERSION=1.12.5
 set LOCALCLASSPATH=.;data\script;data\conf;stendhal-server-%STENDHAL_VERSION%.jar;marauroa.jar;mysql-connector.jar;log4j.jar;commons-lang.jar;h2.jar
 java -Xmx400m -cp "%LOCALCLASSPATH%" games.stendhal.server.StendhalServer -c server.ini -l
