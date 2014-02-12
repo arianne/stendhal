e8e7fda342d6d77fb93a0e9e0460235aca4bef60
Hendrik Brummermann <nhnb@users.sourceforge.net>
2014-02-12 23:08:13 +0000

    create search index entries for npcs

---
+++ src/games/stendhal/server/core/rp/searchindex/SearchIndexManager.java
@@ -0,0 +1,70 @@
+/***************************************************************************
+ *                    (C) Copyright 2014 - Faiumoni e. V.                  *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.core.rp.searchindex;
+
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+import com.google.common.collect.Sets;
+
+/**
+ * manages the search index
+ *
+ * @author hendrik
+ */
+public class SearchIndexManager {
+	private final HashSet<SearchIndexEntry> index = Sets.newHashSet();
+
+	public void generate() {
+		npcs();
+	}
+
+	private void npcs() {
+		for (SpeakerNPC npc : SingletonRepository.getNPCList()) {
+			addName(npc.getName(), SearchIndexEntryType.NPC);
+			addDescription(npc.getName(), npc.getDescription(), SearchIndexEntryType.NPC, 10);
+			addDescription(npc.getName(), npc.getJob(), SearchIndexEntryType.NPC, 10);
+		}
+	}
+
+	/**
+	 * adds the search index entries for a name variable
+	 *
+	 * @param name        name of entity
+	 * @param entityType  type of entity
+	 */
+	private void addName(String name, SearchIndexEntryType type) {
+		index.add(new SearchIndexEntry(name, type.getEntityType(), name, 30));
+		addDescription(name, name, type, 20 + type.getMinorScore());
+	}
+
+
+	/**
+	 * adds the search index entries for a name variable
+	 *
+	 * @param name        name of entity
+	 * @param entityType  type of entity
+	 * @param minorScore  base score of the match type
+	 */
+	private void addDescription(String name, String description, SearchIndexEntryType type, int baseScore) {
+		if (description == null) {
+			return;
+		}
+		StringTokenizer st = new StringTokenizer(description);
+		while (st.hasMoreTokens()) {
+			index.add(new SearchIndexEntry(st.nextToken(), type.getEntityType(), name, baseScore +  + type.getMinorScore()));
+		}
+	}
+}
