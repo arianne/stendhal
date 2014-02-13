a928d4d944b25c5db5eee1cd28fc3dcbde10d693
Hendrik Brummermann <nhnb@users.sourceforge.net>
2014-02-13 23:16:00 +0000

    a database command to update the search index

---
+++ src/games/stendhal/server/core/engine/dbcommand/UpdateSearchIndexCommand.java
@@ -0,0 +1,49 @@
+/***************************************************************************
+ *                    (C) Copyright 2014 - Faiumoni e. V.                  *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.core.engine.dbcommand;
+
+import games.stendhal.server.core.engine.db.StendhalSearchIndexDAO;
+import games.stendhal.server.core.rp.searchindex.SearchIndexEntry;
+import games.stendhal.server.core.rp.searchindex.SearchIndexManager;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Set;
+
+import marauroa.server.db.DBTransaction;
+import marauroa.server.db.command.AbstractDBCommand;
+import marauroa.server.game.db.DAORegister;
+
+import org.apache.log4j.Logger;
+
+/**
+ * updates the search index
+ *
+ * @author hendrik
+ */
+public class UpdateSearchIndexCommand extends AbstractDBCommand {
+	private static Logger logger = Logger.getLogger(UpdateSearchIndexCommand.class);
+
+	@Override
+	public void execute(DBTransaction transaction) throws SQLException, IOException {
+		long start = System.currentTimeMillis();
+
+		SearchIndexManager manager = new SearchIndexManager();
+		Set<SearchIndexEntry> index = manager.generateIndex();
+
+		StendhalSearchIndexDAO dao = DAORegister.get().get(StendhalSearchIndexDAO.class);
+		dao.updateSearchIndex(transaction, index);
+
+		logger.info("Completed dumping of search index in " + (System.currentTimeMillis() - start) + " milliseconds.");
+	}
+
+}
