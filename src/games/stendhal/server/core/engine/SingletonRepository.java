bc441bed0442db417856c2b808b617e3316e961b
Markus <madmetzger@users.sourceforge.net>
2014-02-21 20:14:43 +0000

    delete method not used anymore

--- src/games/stendhal/server/core/engine/SingletonRepository.java
+++ src/games/stendhal/server/core/engine/SingletonRepository.java
@@ -16,7 +16,6 @@ import games.stendhal.server.core.events.LoginNotifier;
 import games.stendhal.server.core.events.TurnNotifier;
 import games.stendhal.server.core.rp.StendhalQuestSystem;
 import games.stendhal.server.core.rp.achievement.AchievementNotifier;
-import games.stendhal.server.core.rp.economy.StendhalEconomy;
 import games.stendhal.server.core.rp.group.GroupManager;
 import games.stendhal.server.core.rule.EntityManager;
 import games.stendhal.server.core.rule.defaultruleset.DefaultEntityManager;
@@ -187,15 +186,6 @@ public class SingletonRepository {
 		return groupManager;
 	}
 	
-	/**
-	 * gets the StendhalEconomy
-	 * 
-	 * @return the stendhal economy
-	 */
-	public static StendhalEconomy getEconomy() {
-		return StendhalEconomy.get();
-	}
-
 	/**
 	 * Sets a new EntityManager 
 	 * @param entityManager
