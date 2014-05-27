bfb0a723d458b600c2f1dca7cbadab6b216b2d2f
Markus <madmetzger@users.sourceforge.net>
2014-05-27 20:38:49 +0000

    add new ChatAction for resetting Blocks

---
+++ src/games/stendhal/server/entity/npc/action/ResetBlockChatAction.java
@@ -0,0 +1,33 @@
+package games.stendhal.server.entity.npc.action;
+
+import games.stendhal.common.parser.Sentence;
+import games.stendhal.server.entity.mapstuff.block.Block;
+import games.stendhal.server.entity.npc.ChatAction;
+import games.stendhal.server.entity.npc.EventRaiser;
+import games.stendhal.server.entity.player.Player;
+
+/**
+ * Resets a block to its initial position
+ * 
+ * @author madmetzger
+ */
+public class ResetBlockChatAction implements ChatAction {
+
+    private final Block blockToReset;
+
+    /**
+     * Create a new Action to reset the given Block
+     * 
+     * @param block 
+     *          The Block to reset
+     */
+    public ResetBlockChatAction(Block block) {
+        this.blockToReset = block;
+    }
+
+    @Override
+    public void fire(Player player, Sentence sentence, EventRaiser npc) {
+        this.blockToReset.reset();
+    }
+
+}
