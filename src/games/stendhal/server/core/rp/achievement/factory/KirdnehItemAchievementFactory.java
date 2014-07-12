2500ac50520771e5126f9d5925502a9073d82340
Hendrik Brummermann <nhnb@users.sourceforge.net>
2014-07-12 18:43:37 +0000

    activated Master Archaeologist and Senior Archaeologist

--- src/games/stendhal/server/core/rp/achievement/factory/KirdnehItemAchievementFactory.java
+++ src/games/stendhal/server/core/rp/achievement/factory/KirdnehItemAchievementFactory.java
@@ -19,10 +19,10 @@ public class KirdnehItemAchievementFactory extends AbstractAchievementFactory {
 		LinkedList<Achievement> achievements = new LinkedList<Achievement>();
 		achievements.add(createAchievement("quest.special.weekly_item.0005", "Archaeologist", "Finish weekly item quest 5 times",
 				Achievement.HARD_BASE_SCORE, true, new QuestStateGreaterThanCondition("weekly_item", 2, 4)));
-		achievements.add(createAchievement("quest.special.weekly_item.0050", "Senior Archaeologist", "Finish weekly item quest 50 times", 
-				Achievement.HARD_BASE_SCORE, false, new QuestStateGreaterThanCondition("weekly_item", 2, 49)));
-		achievements.add(createAchievement("quest.special.weekly_item.0100", "Master Archaeologist", "Finish weekly item quest 100 times", 
-				Achievement.HARD_BASE_SCORE, false, new QuestStateGreaterThanCondition("weekly_item", 2, 99)));
+		achievements.add(createAchievement("quest.special.weekly_item.0050", "Senior Archaeologist", "Finish weekly item quest 50 times",
+				Achievement.HARD_BASE_SCORE, true, new QuestStateGreaterThanCondition("weekly_item", 2, 49)));
+		achievements.add(createAchievement("quest.special.weekly_item.0100", "Master Archaeologist", "Finish weekly item quest 100 times",
+				Achievement.HARD_BASE_SCORE, true, new QuestStateGreaterThanCondition("weekly_item", 2, 99)));
 		return achievements;
 	}
 
