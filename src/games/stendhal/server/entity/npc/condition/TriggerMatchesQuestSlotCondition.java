b079cd193878f57e8490fa3c87837e8df40be237
Hendrik Brummermann <nhnb@users.sourceforge.net>
2014-08-11 22:12:28 +0000

    implemented TriggerMatchesQuestSlotCondition with tests

---
+++ src/games/stendhal/server/entity/npc/condition/TriggerMatchesQuestSlotCondition.java
@@ -0,0 +1,54 @@
+package games.stendhal.server.entity.npc.condition;
+
+import games.stendhal.common.parser.Sentence;
+import games.stendhal.server.core.config.annotations.Dev;
+import games.stendhal.server.core.config.annotations.Dev.Category;
+import games.stendhal.server.entity.Entity;
+import games.stendhal.server.entity.npc.ChatCondition;
+import games.stendhal.server.entity.player.Player;
+
+/**
+ * was the content of a quest slot said?
+ */
+@Dev(category=Category.CHAT, label="\"questtext\"?")
+public class TriggerMatchesQuestSlotCondition implements ChatCondition {
+	private String questname;
+	private int index;
+
+	/**
+	 * creates a TriggerMatchesQuestSlotCondition
+	 *
+	 * @param questname name of quest
+	 * @param index index of questslot
+	 */
+	public TriggerMatchesQuestSlotCondition(String questname, int index) {
+		this.questname = questname;
+		this.index = index;
+	}
+
+	public boolean fire(Player player, Sentence sentence, Entity npc) {
+		return sentence.matchesNormalized(player.getQuest(questname, index));
+	}
+
+	@Override
+	public String toString() {
+		return "questtext? <" + questname + "[" + index + "]>";
+	}
+
+	@Override
+	public int hashCode() {
+		return 5021 * questname.hashCode() + index;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (!(obj instanceof TriggerMatchesQuestSlotCondition)) {
+			return false;
+		}
+		TriggerMatchesQuestSlotCondition other = (TriggerMatchesQuestSlotCondition) obj;
+		if (this.index != other.index) {
+			return false;
+		}
+		return questname.equals(other.questname);
+	}
+}
---
+++ tests/games/stendhal/server/entity/npc/condition/TriggerMatchesQuestSlotConditionTest.java
@@ -0,0 +1,54 @@
+package games.stendhal.server.entity.npc.condition;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import games.stendhal.common.parser.ConversationParser;
+import games.stendhal.server.entity.player.Player;
+
+import org.junit.Test;
+
+import utilities.PlayerTestHelper;
+
+public class TriggerMatchesQuestSlotConditionTest {
+	private static final String QUEST_SLOT = "questslot";
+
+	@Test
+	public void testFire() {
+		final Player player = PlayerTestHelper.createPlayer("player");
+		player.setQuest(QUEST_SLOT, "The banana rests in the fireplace.");
+		assertTrue(new TriggerMatchesQuestSlotCondition(QUEST_SLOT, -1)
+			.fire(player, ConversationParser.parse("The banana rests in the fireplace."), null));
+		assertTrue(new TriggerMatchesQuestSlotCondition(QUEST_SLOT, -1)
+			.fire(player, ConversationParser.parse("The banana rests in the fireplace"), null));
+		assertTrue(new TriggerMatchesQuestSlotCondition(QUEST_SLOT, -1)
+			.fire(player, ConversationParser.parse("Banana rests in the fireplace"), null));
+		assertTrue(new TriggerMatchesQuestSlotCondition(QUEST_SLOT, -1)
+			.fire(player, ConversationParser.parse("Banana rests in  fireplace"), null));
+		assertTrue(new TriggerMatchesQuestSlotCondition(QUEST_SLOT, -1)
+			.fire(player, ConversationParser.parse("banana Rests in Fireplace"), null));
+		assertFalse(new TriggerMatchesQuestSlotCondition(QUEST_SLOT, -1)
+			.fire(player, ConversationParser.parse("Banana rests in the building"), null));
+	}
+
+	@Test
+	public void testToString() {
+		assertThat(new TriggerMatchesQuestSlotCondition("questname", 1).toString(),
+			equalTo("questtext? <questname[1]>"));
+	}
+
+	@Test
+	public void testEqualsObject() {
+		assertThat(new TriggerMatchesQuestSlotCondition("questname", 1),
+				equalTo(new TriggerMatchesQuestSlotCondition("questname", 1)));
+		assertThat(new TriggerMatchesQuestSlotCondition("questname", 1),
+				not(equalTo(new TriggerMatchesQuestSlotCondition("questname2", 1))));
+		assertThat(new TriggerMatchesQuestSlotCondition("questname", 1),
+				not(equalTo(new TriggerMatchesQuestSlotCondition("questname", 0))));
+		assertThat(new TriggerMatchesQuestSlotCondition("questname", 1),
+				not(equalTo(null)));
+	}
+
+}
