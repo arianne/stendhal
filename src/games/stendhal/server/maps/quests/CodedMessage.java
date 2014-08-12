581f4b2a50a146081715c2537a60a330d22fbe48
Hendrik Brummermann <nhnb@users.sourceforge.net>
2014-08-12 06:56:47 +0000

    check coded message

--- src/games/stendhal/server/maps/quests/CodedMessage.java
+++ src/games/stendhal/server/maps/quests/CodedMessage.java
@@ -1,14 +1,26 @@
 package games.stendhal.server.maps.quests;
 
 import games.stendhal.common.Rand;
+import games.stendhal.common.parser.Sentence;
+import games.stendhal.server.entity.Entity;
+import games.stendhal.server.entity.npc.ChatCondition;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.action.MultipleActions;
+import games.stendhal.server.entity.npc.action.SetQuestAction;
+import games.stendhal.server.entity.npc.condition.AndCondition;
+import games.stendhal.server.entity.npc.condition.NotCondition;
+import games.stendhal.server.entity.npc.condition.QuestInStateCondition;
+import games.stendhal.server.entity.npc.condition.TriggerMatchesQuestSlotCondition;
 import games.stendhal.server.entity.player.Player;
 
 import java.util.List;
 
 
 public class CodedMessage extends AbstractQuest {
-	private static String QUEST_SLOT = "coded_message";
 
+
+	private static String QUEST_SLOT = "coded_message";
 
 	@Override
 	public String getSlotName() {
@@ -58,10 +70,79 @@ public class CodedMessage extends AbstractQuest {
 		return res.toString().trim();
 	}
 
-	public static void main(String[] args) {
-		CodedMessage quest = new CodedMessage();
-		for (int i = 0; i < 100; i++) {
-			System.out.println(quest.generateRandomMessage());
+	private void step1() {
+		// TODO
+	}
+	
+	private void step2() {
+		final SpeakerNPC npc = npcs.get("lil johnnnny");
+
+		MultipleActions reward = new MultipleActions(
+			new SetQuestAction(QUEST_SLOT, 0, "done")
+			// TODO
+		);
+		npc.add(ConversationStates.ATTENDING, 
+			"", 
+			new AndCondition(
+				new TriggerMatchesQuestSlotCondition(QUEST_SLOT, 1),
+				new QuestInStateCondition(QUEST_SLOT, 0, "deliver")
+			), 
+			ConversationStates.ATTENDING,
+			"",
+			reward);
+
+		npc.add(ConversationStates.ATTENDING, 
+			"", 
+			new AndCondition(
+				new QuestInStateCondition(QUEST_SLOT, 0, "deliver"),
+				new NotCondition(new TriggerMatchesQuestSlotCondition(QUEST_SLOT, 1)),
+				new TriggerMightbeACodedMessageCondition()
+			),
+			ConversationStates.ATTENDING,
+			"Oh? That doesn't make any sense at all!",
+			null);
+	}
+
+	@Override
+	public void addToWorld() {
+		super.addToWorld();
+		step1();
+		step2();
+	}
+
+
+
+	/**
+	 * does the sentence look like a possible coded message?
+	 *
+	 * @author hendrik
+	 */
+	public class TriggerMightbeACodedMessageCondition implements ChatCondition {
+
+		public boolean fire(Player player, Sentence sentence, Entity npc) {
+			String originalText = sentence.getOriginalText();
+			int counter = 0;
+			for (int i = 0; i < originalText.length(); i++) {
+				if (originalText.charAt(i) == ' ') {
+					counter++;
+				}
+			}
+			return counter >= 4;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			return obj instanceof TriggerMightbeACodedMessageCondition;
+		}
+
+		@Override
+		public int hashCode() {
+			return -37;
+		}
+
+		@Override
+		public String toString() {
+			return "codedmessage?";
 		}
 	}
 }
