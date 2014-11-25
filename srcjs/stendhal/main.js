76f8f4e626338e4616702d7a31493588ab96f611
Hendrik Brummermann <nhnb@users.sourceforge.net>
2014-11-25 21:44:58 +0000

    cleaned up main.js and removed body-onload

--- srcjs/stendhal/main.js
+++ srcjs/stendhal/main.js
@@ -1,48 +1,73 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2014 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
-	marauroa.clientFramework.onDisconnect = function(reason, error){
-		stendhal.ui.chatLog.addLine("error", "Disconnected: " + error);
-	}
+"use strict";
+window.stendhal = window.stendhal || {};
+
+stendhal.main = {
+
+	registerMarauroaEventHandlers: function() {
+		marauroa.clientFramework.onDisconnect = function(reason, error){
+			stendhal.ui.chatLog.addLine("error", "Disconnected: " + error);
+		}
 	
-	marauroa.clientFramework.onLoginRequired = function() {
-		window.location = "/index.php?id=content/account/login&url="
-			+ escape(window.location.pathname + window.location.hash);
-	}
-
-	marauroa.clientFramework.onLoginFailed = function(reason, text) {
-		alert("Login failed. Please login on the Stendhal website first and make sure you open the client on an https://-URL");
-		marauroa.clientFramework.close();
-		document.getElementById("chatbar").disabled = true;
-		document.getElementById("chat").style.backgroundColor = "#AAA";
-	}
-
-	marauroa.clientFramework.onAvailableCharacterDetails = function(characters) {
-		if (window.location.hash) {
-			marauroa.clientFramework.chooseCharacter(window.location.hash.substring(1));
-		} else {
-			marauroa.clientFramework.chooseCharacter(marauroa.util.first(characters).a.name);
+		marauroa.clientFramework.onLoginRequired = function() {
+			window.location = "/index.php?id=content/account/login&url="
+				+ escape(window.location.pathname + window.location.hash);
 		}
 
-		var body = document.getElementById("body")
-		body.style.cursor = "auto";
-		stendhal.ui.chatLog.addLine("client", "Loading world...");
-	}
+		marauroa.clientFramework.onLoginFailed = function(reason, text) {
+			alert("Login failed. Please login on the Stendhal website first and make sure you open the client on an https://-URL");
+			marauroa.clientFramework.close();
+			document.getElementById("chatbar").disabled = true;
+			document.getElementById("chat").style.backgroundColor = "#AAA";
+		}
+
+		marauroa.clientFramework.onAvailableCharacterDetails = function(characters) {
+			if (window.location.hash) {
+				marauroa.clientFramework.chooseCharacter(window.location.hash.substring(1));
+			} else {
+				marauroa.clientFramework.chooseCharacter(marauroa.util.first(characters).a.name);
+			}
+	
+			var body = document.getElementById("body")
+			body.style.cursor = "auto";
+			stendhal.ui.chatLog.addLine("client", "Loading world...");
+		}
 
 
-	marauroa.clientFramework.onTransferREQ = function(items) {
-		for (var i in items) {
-			if (typeof(items[i].name) != "undefined" && items[i].name.match(".collision$")) {
-				items[i].ack = true;
+		marauroa.clientFramework.onTransferREQ = function(items) {
+			for (var i in items) {
+				if (typeof(items[i].name) != "undefined" && items[i].name.match(".collision$")) {
+					items[i].ack = true;
+				}
 			}
 		}
+	},
+	
+	registerBrowserEventHandlers: function() {
+		document.onkeydown=stendhal.ui.chatBar.keydown;
+		document.onkeyup=stendhal.ui.chatBar.keyup;
+	},
+
+	startup: function() {
+		stendhal.ui.chatLog.addLine("client", "Client loaded. Connecting...");
+		var body = document.getElementById("body")
+		body.style.cursor = "wait";
+
+		stendhal.main.registerMarauroaEventHandlers();
+		stendhal.main.registerBrowserEventHandlers();
+		marauroa.clientFramework.connect(null, null);
 	}
-
-	document.onkeydown=stendhal.ui.chatBar.keydown;
-	document.onkeyup=stendhal.ui.chatBar.keyup;
-
-
-function stendhalStartClient() {
-	stendhal.ui.chatLog.addLine("client", "Client loaded. Connecting...");
-	var body = document.getElementById("body")
-	body.style.cursor = "wait";
-	marauroa.clientFramework.connect(null, null);
 }
+
+document.addEventListener('DOMContentLoaded', stendhal.main.startup);
