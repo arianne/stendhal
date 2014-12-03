4d259f852c1a19345f2d952cbe061d4a0dc95bf9
Hendrik Brummermann <nhnb@users.sourceforge.net>
2014-12-03 22:00:05 +0000

    fixed invocation of Array.splice

--- srcjs/stendhal/ui/stendhal-buddylist/stendhal-buddylist.js
+++ srcjs/stendhal/ui/stendhal-buddylist/stendhal-buddylist.js
@@ -4,13 +4,11 @@ Polymer("stendhal-buddylist", {
 	"buddies": [],
 	
 	"setBuddyStatus": function(buddy, status) {
-		if(this.hasBuddy(buddy)) {
-			this.removeBuddy(buddy);
-		}
-		var newEntry = {"name": buddy, "status":status};
+		this.removeBuddy(buddy);
+		var newEntry = {"name": buddy, "status": status};
 		this.buddies.push(newEntry);
 	},
-	
+
     "clear": function() {
     	this.buddies = [];
     },
@@ -26,16 +24,11 @@ Polymer("stendhal-buddylist", {
     
     "removeBuddy": function(buddy) {
     	var arrayLength = this.buddies.length;
-    	var found = false;
-    	var indexFound;
     	for (var i = 0; i < arrayLength; i++) {
-    		if(this.buddies[i].name === buddy) {
-    			indexFound = i;
-    			found = true;
+    		if (this.buddies[i].name === buddy) {
+    			this.buddies.splice(i, 1);
+    			return;
     		}
-    	}
-    	if(found) {
-    		this.buddies = this.buddies.splice(indexFound, 1);
     	}
     }
     
