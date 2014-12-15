303994efec223130ede8238117c8611a7b286d9d
Kimmo Rundelin <kiheru@users.sourceforge.net>
2014-12-15 20:05:54 +0000

    Use InfiniteAudibleArea for events that do not specify radius

--- src/games/stendhal/client/events/SoundEvent.java
+++ src/games/stendhal/client/events/SoundEvent.java
@@ -1,6 +1,6 @@
 /* $Id$ */
 /***************************************************************************
- *                   (C) Copyright 2003-2010 - Stendhal                    *
+ *                   (C) Copyright 2003-2014 - Stendhal                    *
  ***************************************************************************
  ***************************************************************************
  *                                                                         *
@@ -14,7 +14,9 @@ package games.stendhal.client.events;
 
 import games.stendhal.client.ClientSingletonRepository;
 import games.stendhal.client.entity.Entity;
+import games.stendhal.client.sound.facade.AudibleArea;
 import games.stendhal.client.sound.facade.AudibleCircleArea;
+import games.stendhal.client.sound.facade.InfiniteAudibleArea;
 import games.stendhal.client.sound.facade.SoundFileType;
 import games.stendhal.client.sound.facade.SoundGroup;
 import games.stendhal.common.constants.SoundLayer;
@@ -22,13 +24,13 @@ import games.stendhal.common.math.Algebra;
 import games.stendhal.common.math.Numeric;
 
 /**
- * plays a sound
+ * Plays a sound.
  *
  * @author hendrik
  */
 class SoundEvent extends Event<Entity> {
 	/**
-	 * executes the event
+	 * Executes the event.
 	 */
 	@Override
 	public void execute() {
@@ -41,14 +43,16 @@ class SoundEvent extends Event<Entity> {
 		if (event.has("volume")) {
 			volume = Numeric.intToFloat(event.getInt("volume"), 100.0f);
 		}
-		int radius = 100000;
+		String soundName = event.get("sound");
+		AudibleArea area;
 		if (event.has("radius")) {
-			radius = event.getInt("radius");
+			int radius = event.getInt("radius");
+			area = new AudibleCircleArea(Algebra.vecf((float) entity.getX(), (float) entity.getY()), radius / 4.0f, radius);
+		} else {
+			area = new InfiniteAudibleArea();
 		}
 
 		SoundGroup group = ClientSingletonRepository.getSound().getGroup(layer.groupName);
-		String soundName = event.get("sound");
-		AudibleCircleArea area = new AudibleCircleArea(Algebra.vecf((float)entity.getX(), (float)entity.getY()), radius/4.0f, radius);
 		group.loadSound(soundName, soundName + ".ogg", SoundFileType.OGG, false);
 		group.play(soundName, volume, 0, area, null, false, true);
 	}
