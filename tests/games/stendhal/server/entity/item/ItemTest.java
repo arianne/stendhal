f251283ce753124ae7d9e0c14e93f8a2ce4e6c59
Kimmo Rundelin <kiheru@users.sourceforge.net>
2015-03-28 22:00:49 +0200

    Removed antipoison from testDescribe()

--- tests/games/stendhal/server/entity/item/ItemTest.java
+++ tests/games/stendhal/server/entity/item/ItemTest.java
@@ -134,12 +134,11 @@ public class ItemTest {
 		item.put("amount", 5);
 		item.put("range", 6);
 		item.put("lifesteal", 7);
-		item.put("antipoison", 8);
 		
-		assertThat(item.describe(), equalTo("Description. It is a special reward for hero, and cannot be used by others. Stats are (ATK: 2 DEF: 3 RATE: 4 HP: 5 RANGE: 6 LIFESTEAL: 7 ANTIPOISON: 8 MIN-LEVEL: 1)."));
+		assertThat(item.describe(), equalTo("Description. It is a special reward for hero, and cannot be used by others. Stats are (ATK: 2 DEF: 3 RATE: 4 HP: 5 RANGE: 6 LIFESTEAL: 7 MIN-LEVEL: 1)."));
 
 		item.setDamageType(Nature.FIRE);
-		assertThat(item.describe(), equalTo("Description. It is a special reward for hero, and cannot be used by others. Stats are (ATK: 2 [FIRE] DEF: 3 RATE: 4 HP: 5 RANGE: 6 LIFESTEAL: 7 ANTIPOISON: 8 MIN-LEVEL: 1)."));
+		assertThat(item.describe(), equalTo("Description. It is a special reward for hero, and cannot be used by others. Stats are (ATK: 2 [FIRE] DEF: 3 RATE: 4 HP: 5 RANGE: 6 LIFESTEAL: 7 MIN-LEVEL: 1)."));
 	}
 
 
