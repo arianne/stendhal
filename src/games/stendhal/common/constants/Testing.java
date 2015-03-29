f279c9785ed1f36a203b337ec4f37a45ccd9e7ba
Jordan Irwin <antumdeluge@gmail.com>
2015-03-28 18:59:25 -0700

    Defined constants for testing purposes.

---
+++ src/games/stendhal/common/constants/Testing.java
@@ -0,0 +1,25 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2015 - Arianne                     *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.common.constants;
+
+/**
+ * Constants defined for testing purposes.
+ * 
+ * @author AntumDeluge
+ */
+public class Testing {
+	/** Testing combat system */
+	public final String OUTFIT_TESTING_PROPERTY = "testing.outfits";
+	
+	/** Testing outfit system */
+	public final String COMBAT_TESTING_PROPERTY = "testing.combat";
+}
