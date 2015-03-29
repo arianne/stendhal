81653d067f0e43a2e36336df7560ab3a3083e02c
Jordan Irwin <antumdeluge@gmail.com>
2015-03-28 21:13:55 -0700

    Add constructors and methods for new outfit system.

--- src/games/stendhal/client/OutfitStore.java
+++ src/games/stendhal/client/OutfitStore.java
@@ -18,6 +18,7 @@ import games.stendhal.client.sprite.ImageSprite;
 import games.stendhal.client.sprite.Sprite;
 import games.stendhal.client.sprite.SpriteCache;
 import games.stendhal.client.sprite.SpriteStore;
+import games.stendhal.common.constants.Testing;
 
 import java.awt.Color;
 import java.awt.Composite;
@@ -163,6 +164,90 @@ public class OutfitStore {
 	}
 
 	/**
+	 * Build an outfit sprite.
+	 * 
+	 * The body and outfit are described by a "body code" and an "outfit code".
+	 * 
+	 * The body code is a 10-digit integer of the form RREEMMHHBB where RR is
+	 * is the number of the hair graphics (optional), EE is the number of the
+	 * eyes graphics, MM is the number of the mouth graphics (OPTIONAL), HH is
+	 * the number of the head graphics, and BB for the body.
+	 * 
+	 * The outfit code is a 4-digit integer of the form TTDD where TT is the
+	 * number for the detail graphics (optional) and DD is for the dress/outfit
+	 * (optional).
+	 * 
+	 * @param bodyCode
+	 * 		Numerical value of body parts
+	 * @param outfitCode
+	 * 		Numerical value of outfit and detail parts
+	 * @param color
+	 * 		Coloring data
+	 * @return
+	 * 		A walking state tileset
+	 */
+	private Sprite buildOutfit(int bodyCode, int outfitCode, OutfitColor color) {
+		
+		// Body
+		int bodycode = bodyCode % 100;
+		bodyCode /= 100;
+		
+		int headcode = bodyCode % 100;
+		bodyCode /= 100;
+		
+		int mouthcode = bodyCode % 100;
+		bodyCode /= 100;
+		
+		int eyescode = bodyCode % 100;
+		bodyCode /= 100;
+		
+		int haircode = bodyCode % 100;
+		bodyCode /= 100;
+		
+		// Outfit and detail
+		int dresscode = outfitCode % 100;
+		outfitCode /= 100;
+		
+		int detailcode = outfitCode % 100;
+		
+		// Body layer
+		Sprite layer = getBodySprite(bodycode, color);
+		if (layer == null) {
+			throw new IllegalArgumentException(
+					"No body image found for outfit: " + bodycode);
+		}
+
+		final ImageSprite sprite = new ImageSprite(layer);
+		final Graphics g = sprite.getGraphics();
+
+		// Dress layer
+		layer = getDressSprite(dresscode, color);
+		layer.draw(g, 0, 0);
+
+		// Head layer
+		layer = getHeadSprite(headcode, color);
+		layer.draw(g, 0, 0);
+		
+		// Mouth layer
+		layer = getMouthSprite(mouthcode);
+		layer.draw(g, 0, 0);
+		
+		// Eyes layer
+		layer = getEyesSprite(eyescode, color);
+		layer.draw(g,  0, 0);
+
+		// Hair layer
+		layer = getHairSprite(haircode, color);
+		layer.draw(g, 0, 0);
+		
+		// Item layer
+		layer = getDetailSprite(detailcode, color);
+		layer.draw(g, 0, 0);
+
+		return sprite;
+	}
+
+	/**
 	 * Get the shared instance.
 	 * 
 	 * @return The shared [singleton] instance.
@@ -178,6 +263,9 @@ public class OutfitStore {
 	 * 		The sprite index number
 	 * @return
 	 * 		A string value of the format xxx
+	 * 
+	 * FIXME:	Probably not necessary since there are no more than 100
+	 * 			sprites for each group.
 	 */
 	public String getSpriteSuffix(final int index) {
 		String suffix;
@@ -244,7 +332,7 @@ public class OutfitStore {
 	private Sprite getEmptySprite() {
 		return store.getEmptySprite();
 	}
-
+	
 	/**
 	 * Get the failsafe outfit.
 	 * 
@@ -252,13 +340,17 @@ public class OutfitStore {
 	 */
 	public Sprite getFailsafeOutfit() {
 		try {
-			return getOutfit(0, OutfitColor.PLAIN);
+			if (Testing.enabled(Testing.OUTFITS)) {
+				return getOutfit(0, 0, OutfitColor.PLAIN);
+			} else {
+				return getOutfit(0, OutfitColor.PLAIN);
+			}
 		} catch (RuntimeException e) {
 			logger.warn("Cannot build failsafe outfit. Trying to use standard failsafe sprite.", e);
 			return store.getFailsafe();
 		}
 	}
-
+	
 	/**
 	 * Get the hair sprite tileset.
 	 * 
@@ -361,6 +453,113 @@ public class OutfitStore {
 		return store.getColoredSprite(ref, color.getColor(OutfitColor.DETAIL));
 	}
 	
+	/**
+	 * Get an outfit sprite.
+	 * 
+	 * The outfit is described by an "outfit code". It is an 10-digit integer of
+	 * the form TTRRHHDDBB where where TT is the number of the detail graphics (optional)
+	 * RR is the number of the hair graphics, HH for the
+	 * head, DD for the dress, and BB for the body.
+	 * 
+	 * @param bodyCode
+	 * 		Numerical value of body parts
+	 * @param outfitCode
+	 * 		Numerical value of outfit and detail parts
+	 * @param color
+	 * 		Colors for coloring some body and outfit parts
+	 * @return
+	 * 		A walking state tileset
+	 */
+	private Sprite getOutfit(final int bodyCode, final int outfitCode,
+			OutfitColor color) {
+		// Use the normalized string for the reference
+		final String reference = buildReference(bodyCode, outfitCode, color.toString());
+		return getOutfit(bodyCode, outfitCode, color, reference);
+	}
+	
+	/**
+	 * Get outfit for a known outfit reference.
+	 * 
+	 * @param bodyCode
+	 * 		Numerical value of body parts
+	 * @param outfitCode
+	 * 		Numerical value of outfit and detail parts
+	 * @param color
+	 * 		Color information for body and outfit parts
+	 * @param reference
+	 * 		Body and outfit reference
+	 * @return
+	 * 		Body and outfit
+	 */
+	private Sprite getOutfit(final int bodyCode, final int outfitCode,
+			OutfitColor color, String reference) {
+		final SpriteCache cache = SpriteCache.get();
+		Sprite sprite = cache.get(reference);
+
+		if (sprite == null) {
+			sprite = buildOutfit(bodyCode, outfitCode, color);
+			cache.add(reference, sprite);
+		}
+
+		return sprite;
+	}
+	
+	/**
+	 * Get an outfit with color adjustment, such as a player in colored light.
+	 * 
+	 * @param bodyCode
+	 * 		Numerical value of body parts
+	 * @param outfitCode
+	 * 		Numerical value of outfit and detail parts
+	 * @param color
+	 * 		Color information for body and outfit parts
+	 * @param adjColor
+	 * 		Adjustment color for the entire outfit
+	 * @param blend
+	 * 		Blend mode for applying the adjustment color
+	 * @return
+	 * 		Color adjusted outfit
+	 */
+	public Sprite getAdjustedOutfit(final int bodyCode, final int outfitCode,
+			OutfitColor color, Color adjColor, Composite blend) {
+		if ((adjColor == null) || (blend == null)) {
+			return getOutfit(bodyCode, outfitCode, color);
+		} else {
+			final SpriteCache cache = SpriteCache.get();
+			// Use the normalized string for the reference
+			final String reference = buildReference(bodyCode, outfitCode,
+					color.toString());
+			String fullRef = reference + ":" + adjColor.getRGB() + blend.toString();
+			Sprite sprite = cache.get(fullRef);
+			if (sprite == null) {
+				Sprite plain = getOutfit(bodyCode, outfitCode, color);
+				SpriteStore store = SpriteStore.get();
+				sprite = store.modifySprite(plain, adjColor, blend, fullRef);
+				
+			}
+			return sprite;
+		}
+	}
+
+	/**
+	 * Create an unique reference for an outfit.
+	 * 
+	 * @param bodyCode
+	 * 		Numerical value of body parts
+	 * @param outfitcode
+	 * 		Numerical value of outfit and detail parts
+	 * @param colorCode
+	 * 		Color information for body and outfit parts
+	 * @return
+	 * 		Body and outfit reference
+	 */
+	private String buildReference(final int bodyCode, final int outfitCode,
+			final String colorCode) {
+		return "OUTFIT:" + bodyCode + "_" + outfitCode + "@" + colorCode;
+	}
+	
+	
+	// TODO: Remove old methods when new outfit system is finished ------
 	/**
 	 * Get an outfit sprite.
 	 * 
