96fd67ad6b88f0fcbc25378986a520f708f77b77
Jordan Irwin <antumdeluge@gmail.com>
2015-03-28 21:03:49 -0700

    Add eyes to list of colorable components.

--- src/games/stendhal/server/entity/player/Player.java
+++ src/games/stendhal/server/entity/player/Player.java
@@ -75,7 +75,7 @@ public class Player extends RPEntity implements UseListener {
 
 	private static final String LAST_PLAYER_KILL_TIME = "last_player_kill_time";
 	private static final String[] RECOLORABLE_OUTFIT_PARTS = { "detail",
-		"dress", "hair", "body", "head" };
+		"dress", "hair", "body", "head", "eyes" };
 
 	/** the logger instance. */
 	private static final Logger logger = Logger.getLogger(Player.class);
