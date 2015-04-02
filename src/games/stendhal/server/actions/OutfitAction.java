1203b116d9d51fd458e9f8ac0c5a3bcfb7bd2153
Jordan Irwin <antumdeluge@gmail.com>
2015-04-02 12:31:28 -0700

    Replace "System.getProperty()" check with "Testing.OUFITS".

--- src/games/stendhal/server/actions/OutfitAction.java
+++ src/games/stendhal/server/actions/OutfitAction.java
@@ -16,6 +16,7 @@ import static games.stendhal.common.constants.Actions.OUTFIT;
 import static games.stendhal.common.constants.Actions.VALUE;
 import games.stendhal.common.MathHelper;
 import games.stendhal.common.constants.SkinColor;
+import games.stendhal.common.constants.Testing;
 import games.stendhal.server.core.engine.GameEvent;
 import games.stendhal.server.entity.Outfit;
 import games.stendhal.server.entity.player.Player;
@@ -61,7 +62,7 @@ public class OutfitAction implements ActionListener {
 					player.remove(COLOR_MAP, "dress");
 				}
 				
-				if (System.getProperty("testing.outfits") != null) {
+				if (Testing.OUTFITS) {
 					// Players may change skin color
 					color = action.get("skin");
 					if (color != null) {
