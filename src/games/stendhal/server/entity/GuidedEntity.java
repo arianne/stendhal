bfd9509bb54b351bea9509732528ca6c6047c880
Jordan Irwin <antumdeluge@gmail.com>
2015-04-03 15:56:26 -0700

    If entity does not have a previous speed set it in method "setBaseSpeed()". Should be the first time the character moves.

--- src/games/stendhal/server/entity/GuidedEntity.java
+++ src/games/stendhal/server/entity/GuidedEntity.java
@@ -91,6 +91,12 @@ public abstract class GuidedEntity extends ActiveEntity {
 	 */
 	public final void setBaseSpeed(final double bs) {
 		this.baseSpeed = bs;
+		
+		/* If the character has not moved yet set previousSpeed as baseSpeed.
+		 */
+		if (Double.valueOf(this.getPreviousSpeed()) == null) {
+			this.setPreviousSpeed(this.baseSpeed);
+		}
 	}
 	
 	//
