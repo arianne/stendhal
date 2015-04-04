ae3e6364b731ca295c96364c29979e2d153a33fe
Jordan Irwin <antumdeluge@gmail.com>
2015-04-03 23:16:47 -0700

    Add author and copyright notice.

--- src/games/stendhal/server/actions/move/WalkAction.java
+++ src/games/stendhal/server/actions/move/WalkAction.java
@@ -1,3 +1,14 @@
+/***************************************************************************
+ *                     (C) Copyright 2003 - Arianne                        *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package games.stendhal.server.actions.move;
 
 import static games.stendhal.common.constants.Actions.WALK;
@@ -7,6 +18,12 @@ import games.stendhal.server.actions.CommandCenter;
 import games.stendhal.server.entity.player.Player;
 import marauroa.common.game.RPAction;
 
+/**
+ * Action that sets character speed and begins moving in faced direction.
+ * 
+ * @author
+ *		AntumDeluge
+ */
 public class WalkAction implements ActionListener {
 	
 	/**
