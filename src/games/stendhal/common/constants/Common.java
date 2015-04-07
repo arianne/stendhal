44e05d0296dc151cad9b0470b4bcfa52b6967bd9
Jordan Irwin <antumdeluge@gmail.com>
2015-04-07 04:22:41 -0700

    Add license header and PATHSET constant.

--- src/games/stendhal/common/constants/Common.java
+++ src/games/stendhal/common/constants/Common.java
@@ -1,6 +1,27 @@
+/***************************************************************************
+ *                      (C) Copyright 2003 - Arianne                       *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package games.stendhal.common.constants;
 
+/**
+ * Generic constants.
+ * 
+ * @author
+ *         AntumDeluge
+ */
 public class Common {
 
+	/* Property indicating entity is using auto-walk. */
 	public final static String AUTOWALK = "autowalk";
+
+	/* Property indicating entity has a path set. */
+	public final static String PATHSET = "pathset";
 }
