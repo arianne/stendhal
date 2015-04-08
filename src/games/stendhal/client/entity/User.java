18d082ae54741fe6be7e71cfeaa46f3d52287741
Jordan Irwin <antumdeluge@gmail.com>
2015-04-07 22:40:20 -0700

    Add "stop()" method to send RPAction (WalkAction "stop") to server.

--- src/games/stendhal/client/entity/User.java
+++ src/games/stendhal/client/entity/User.java
@@ -12,15 +12,14 @@
  ***************************************************************************/
 package games.stendhal.client.entity;
 
-import static games.stendhal.common.constants.Common.AUTOWALK;
-import static games.stendhal.common.constants.Common.PATHSET;
+import static games.stendhal.common.constants.Actions.MODE;
+import static games.stendhal.common.constants.Actions.TYPE;
+import static games.stendhal.common.constants.Actions.WALK;
 import games.stendhal.client.ClientSingletonRepository;
 import games.stendhal.client.GameObjects;
-import games.stendhal.client.StendhalClient;
 import games.stendhal.client.gui.chatlog.HeaderLessEventLine;
 import games.stendhal.common.Direction;
 import games.stendhal.common.NotificationType;
-import games.stendhal.common.constants.Testing;
 import games.stendhal.common.grammar.Grammar;
 
 import java.awt.event.KeyEvent;
@@ -29,6 +28,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import marauroa.common.game.RPAction;
 import marauroa.common.game.RPObject;
 import marauroa.common.game.RPSlot;
 
@@ -198,21 +198,6 @@ public class User extends Player {
 	 */
 	@Override
 	public void onChangedAdded(final RPObject object, final RPObject changes) {
-		/* TODO: Remove condition when walking bug fix is finished. */
-		if (Testing.MOVEMENT) {
-			if (object != null) {
-				if (!this.stopped()) {
-					if (!object.has(PATHSET) && !object.has(AUTOWALK)
-							&& !StendhalClient.get().keyIsPressed(
-									this.directionToKeyCode(
-											this.getDirection()))) {
-						/* Stop the character's movement. */
-						StendhalClient.get().stop();
-					}
-				}
-			}
-		}
-
 		super.onChangedAdded(object, changes);
 
 		// The first time we ignore it.
@@ -509,5 +494,17 @@ public class User extends Player {
 		default:
 			return null;
 		}
+	}
+
+	/**
+	 * Stop the user's movement.
+	 */
+	public void stop() {
+		final RPAction stopAction = new RPAction();
+
+		stopAction.put(TYPE, WALK);
+		stopAction.put(MODE, "stop");
+
+		ClientSingletonRepository.getClientFramework().send(stopAction);
 	}
 }
