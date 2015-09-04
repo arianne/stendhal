54957306696b57edb48155a32578c20a8acb912b
Kimmo Rundelin <kiheru@users.sourceforge.net>
2015-09-04 16:32:05 +0300

    Added copyright header. Prevent instantiation.

--- src/games/stendhal/client/gui/TransparencyMode.java
+++ src/games/stendhal/client/gui/TransparencyMode.java
@@ -1,3 +1,14 @@
+/***************************************************************************
+ *                (C) Copyright 2003-2015 - Faiumoni e.V.                  *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package games.stendhal.client.gui;
 
 import games.stendhal.client.gui.wt.core.WtWindowManager;
@@ -15,9 +26,15 @@ import org.apache.log4j.Logger;
  * "translucent" means that full alpha transparency is used. Mode "auto", means
  * that the appropriate mode is decided based on a speed test. 
  */
-public class TransparencyMode {
+public final class TransparencyMode {
 	/** The transparency mode that should be used for images. */
 	public static final int TRANSPARENCY = getMode();
+	
+	/**
+	 * Prevent instantiation.
+	 */
+	private TransparencyMode() {
+	}
 	
 	/**
 	 * Determine the mode.
