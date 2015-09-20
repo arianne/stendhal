1d4c825ad659bb58226b10785a1e50be3c55a857
Kimmo Rundelin <kiheru@users.sourceforge.net>
2015-09-20 11:50:43 +0300

    Added signs

---
+++ srcjs/stendhal/entity/sign.js
@@ -0,0 +1,20 @@
+marauroa.rpobjectFactory.sign = marauroa.util.fromProto(marauroa.rpobjectFactory.entity, {
+	zIndex: 5000,
+	"class": "default",
+	
+	draw: function(ctx) {
+		if (!this.imagePath) {
+			this.imagePath = "/data/sprites/signs/" + this["class"] + ".png";
+		}
+		var image = stendhal.data.sprites.get(this.imagePath);
+		if (image.complete) {
+			var localX = this.x * 32;
+			var localY = this.y * 32;
+			ctx.drawImage(image, localX, localY);
+		}
+	},
+	
+	isVisibleToAction: function(filter) {
+		return true;
+	},
+});
