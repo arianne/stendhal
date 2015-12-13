49bd1023e28a7aeef7285c97f511e9320725ec95
Hendrik Brummermann <nhnb@users.sourceforge.net>
2015-12-13 20:51:19 +0100

    scripts are now found in "ant tests" environments, too

--- src/games/stendhal/server/script/ListRaids.java
+++ src/games/stendhal/server/script/ListRaids.java
@@ -12,17 +12,20 @@
  ***************************************************************************/
 package games.stendhal.server.script;
 
-import games.stendhal.server.core.scripting.ScriptImpl;
-import games.stendhal.server.entity.player.Player;
-
-import java.io.File;
-import java.net.URL;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 import org.apache.log4j.Logger;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.reflect.ClassPath;
+import com.google.common.reflect.ClassPath.ClassInfo;
+
+import games.stendhal.server.core.scripting.ScriptImpl;
+import games.stendhal.server.entity.player.Player;
 
 /**
  * Lists raid scripts.
@@ -65,43 +68,18 @@ public class ListRaids extends ScriptImpl {
 	 * @throws ClassNotFoundException if getting the class loader or reading the
 	 * 	script resources fail
 	 */
-	private static ArrayList<Class<?>> getClasses(final String pckgname)
-			throws ClassNotFoundException {
+	private static ArrayList<Class<?>> getClasses(final String pckgname) throws ClassNotFoundException {
 		final ArrayList<Class<?>> classes = new ArrayList<Class<?>>();
-
-		// Get a File object for the package
-		File directory = null;
 		try {
-			final ClassLoader cld = Thread.currentThread().getContextClassLoader();
-			if (cld == null) {
-				throw new ClassNotFoundException("Can't get class loader.");
+			ClassLoader classLoader = ListRaids.class.getClassLoader();
+			String packageName = "games.stendhal.server.script";
+			ImmutableSet<ClassInfo> infos = ClassPath.from(classLoader).getTopLevelClasses(packageName);
+			for (ClassInfo info : infos) {
+				classes.add(info.load());
 			}
-			final String path = pckgname.replace('.', '/');
-			final URL resource = cld.getResource(path);
-			if (resource == null) {
-				throw new ClassNotFoundException("No resource for " + path);
-			}
-			directory = new File(resource.getFile());
-		} catch (final NullPointerException x) {
-			throw new ClassNotFoundException(pckgname + " (" + directory
-					+ ") does not appear to be a valid package");
+			return classes;
+		} catch (IOException e) {
+			throw new ClassNotFoundException("failed to list classes");
 		}
-		if (directory.exists()) {
-			// Get the list of the files contained in the package
-			final String[] files = directory.list();
-			for (int i = 0; i < files.length; i++) {
-				// we are only interested in .class files
-				if (files[i].endsWith(".class")) {
-					// removes the .class extension
-					classes.add(Class.forName(pckgname + '.'
-							+ files[i].substring(0, files[i].length() - 6)));
-				}
-			}
-		} else {
-			throw new ClassNotFoundException(pckgname
-					+ " does not appear to be a valid package");
-		}
-
-		return classes;
 	}
 }
