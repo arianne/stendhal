440248ff22f0c6d72d5ca94334acf287c9256241
Hendrik Brummermann <nhnb@users.sourceforge.net>
2016-01-23 01:43:07 +0100

    converted PuzzleBuildingBlock from interface to aggregation

--- src/games/stendhal/server/entity/mapstuff/puzzle/PuzzleBuildingBlock.java
+++ src/games/stendhal/server/entity/mapstuff/puzzle/PuzzleBuildingBlock.java
@@ -11,38 +11,69 @@
  ***************************************************************************/
 package games.stendhal.server.entity.mapstuff.puzzle;
 
+import java.util.LinkedList;
 import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
+import games.stendhal.server.entity.Entity;
 
 /**
  * a puzzle building block
  *
  * @author hendrik
  */
-public interface PuzzleBuildingBlock {
+public class PuzzleBuildingBlock {
+	private String zoneName;
+	private String name;
+	private Entity entity;
+	private List<String> dependencies;
+
+	/**
+	 * creates a PuzzleBuildingBlock
+	 *
+	 * @param zoneName name of zone
+	 * @param name unique name of entity in zone
+	 * @param entity Entity
+	 */
+	public PuzzleBuildingBlock(String zoneName, String name, Entity entity) {
+		this.zoneName = zoneName;
+		this.name = name;
+		this.entity = entity;
+		this.dependencies = new LinkedList<>();
+	}
 
 	/**
 	 * gets the name of the zone
 	 *
 	 * @return zone
 	 */
-	public String getZoneName();
+	public String getZoneName() {
+		return zoneName;
+	}
 
 	/**
 	 * gets the name of building block
 	 *
 	 * @return name
 	 */
-	public String getName();
+	public String getName() {
+		return name;
+	}
 
 	/**
 	 * listens to input change events
 	 */
-	public void onInputChanged();
+	public void onInputChanged() {
+		entity.update();
+	}
 
 	/**
 	 * gets a list of dependencies that this entity needs to listen to
 	 *
 	 * @return list of dependencies
 	 */
-	public List<String> getDependencies();
+	public List<String> getDependencies() {
+		return ImmutableList.copyOf(dependencies);
+	}
 }
