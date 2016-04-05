22c1a9008bd40ce4f03962f2d104ee97fe4d31bb
soniccuz <soniccuz@users.sourceforge.net>
2016-04-05 17:26:43 -0400

    Basic Summon Pet Scrolls.

---
+++ src/games/stendhal/server/entity/item/scroll/BlankPetScroll.java
@@ -0,0 +1,87 @@
+/* $Id$
+ /***************************************************************************
+ *                      (C) Copyright 2003 - Marauroa                      *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.entity.item.scroll;
+
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.creature.Pet;
+import games.stendhal.server.entity.item.Item;
+import games.stendhal.server.entity.player.Player;
+//import games.stendhal.server.entity.player.PetOwner;;
+
+import java.util.Map;
+
+/**
+ * Represents an empty/blank pet scroll.
+ */
+public class BlankPetScroll extends Scroll {
+
+	// private static final Logger logger = Logger.getLogger(EmptyScroll.class);
+
+	/**
+	 * Creates a new blank pet scroll.
+	 * 
+	 * @param name
+	 * @param clazz
+	 * @param subclass
+	 * @param attributes
+	 */
+	public BlankPetScroll(final String name, final String clazz, final String subclass,
+			final Map<String, String> attributes) {
+		super(name, clazz, subclass, attributes);
+	}
+
+	/**
+	 * Copy constructor.
+	 * 
+	 * @param item
+	 *            item to copy
+	 */
+	public BlankPetScroll(final BlankPetScroll item) {
+		super(item);
+	}
+
+	/**
+	 * Use a blank pet scroll.
+	 * 
+	 * @param player
+	 * @return always true
+	 */
+	@Override
+	protected boolean useScroll(final Player player) {
+		final StendhalRPZone zone = player.getZone();
+		final String petName = player.getPet().getName();
+
+		if (zone.isTeleportInAllowed(player.getX(), player.getY())) {
+			final Item summonPetScroll = SingletonRepository.getEntityManager().getItem(
+					"summon pet scroll");
+			summonPetScroll.setInfoString(petName);
+			player.equipOrPutOnGround(summonPetScroll);
+			
+			final Pet pet = player.getPet();
+			
+
+			if (pet != null) {
+				//petOwner.storePet(pet);
+				pet.getZone().remove(pet);
+				player.sendPrivateText("Amazingly your pet melds with the scroll. It just walked right into the page!");
+			}
+			
+			return true;
+		} else {
+			player.sendPrivateText("The strong anti magic aura in this area prevents the scroll from working!");
+			return false;
+		}
+	}
+	
+}
---
+++ src/games/stendhal/server/entity/item/scroll/SummonPetScroll.java
@@ -0,0 +1,152 @@
+/* $Id$
+ /***************************************************************************
+ *                      (C) Copyright 2003 - Marauroa                      *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.entity.item.scroll;
+
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPZone;
+//import games.stendhal.server.core.rp.StendhalRPAction;
+//import games.stendhal.server.core.rule.EntityManager;
+//import games.stendhal.server.entity.creature.Creature;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.entity.creature.Cat;
+import games.stendhal.server.entity.creature.BabyDragon;
+import games.stendhal.server.entity.creature.PurpleDragon;
+//import games.stendhal.server.entity.creature.Sheep;
+//import games.stendhal.server.core.engine.transformer.PlayerTransformer;
+
+import games.stendhal.server.entity.item.Item;
+
+
+//import java.util.ArrayList;
+//import java.util.Collection;
+//import java.util.HashMap;
+//import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Represents a creature summon pet scroll.
+ */
+public class SummonPetScroll extends Scroll {
+
+	private static final int MAX_ZONE_NPCS = 50;
+
+	private static final Logger logger = Logger.getLogger(SummonPetScroll.class);
+
+	/**
+	 * Creates a new summon pet scroll.
+	 * 
+	 * @param name
+	 * @param clazz
+	 * @param subclass
+	 * @param attributes
+	 */
+	public SummonPetScroll(final String name, final String clazz, final String subclass,
+			final Map<String, String> attributes) {
+		super(name, clazz, subclass, attributes);
+	}
+
+	/**
+	 * Copy constructor.
+	 * 
+	 * @param item
+	 *            item to copy
+	 */
+	public SummonPetScroll(final SummonPetScroll item) {
+		super(item);
+	}
+
+	/**
+	 * Is invoked when a summon pet scroll is used.
+	 * 
+	 * @param player
+	 *            The player who used the scroll
+	 * @return true iff summoning was successful
+	 */
+	@Override
+	protected boolean useScroll(final Player player) {
+		final StendhalRPZone zone = player.getZone();
+
+		if (zone.isInProtectionArea(player)) {
+			player.sendPrivateText("The aura of protection in this area prevents the scroll from working!");
+			return false;
+		}
+
+		if (zone.getNPCList().size() >= MAX_ZONE_NPCS) {
+			player.sendPrivateText("Mysteriously, the scroll does not function! Perhaps this area is too crowded...");
+			logger.info("Too many npcs to summon another creature");
+			return false;
+		}
+
+		final String type = getInfoString();
+
+		if (type == null) {
+			player.sendPrivateText("This scroll does not seem to work. You should talk to the magician who created it.");
+			return false;
+		}
+
+		// create it 
+		if (type == "cat")
+		{
+			//PlayerTransformer.placeSheepAndPetIntoWorld(player);
+			final Cat cat = new Cat(player);
+			player.setPet(cat);
+			cat.setPosition(player.getX(), player.getY() + 1);
+			dropBlank(player);
+		}
+		else if (type == "baby_dragon")
+		{
+			//PlayerTransformer.placeSheepAndPetIntoWorld(player);
+			final BabyDragon babydragon = new BabyDragon(player);
+			player.setPet(babydragon);
+			babydragon.setPosition(player.getX(), player.getY() + 1);
+			dropBlank(player);
+		}
+		else if (type == "purple_dragon")
+		{
+			//PlayerTransformer.placeSheepAndPetIntoWorld(player);
+			final PurpleDragon purpledragon = new PurpleDragon(player);
+			player.setPet(purpledragon);
+			purpledragon.setPosition(player.getX(), player.getY() + 1);
+			dropBlank(player);
+		}
+		
+		return true;
+	}
+	
+	@Override
+	public String describe() {
+		String text = super.describe();
+
+		final String infostring = getInfoString();
+
+		if (infostring != null) {
+			text += " It will summon a " + infostring  + ".";
+		}
+		return (text);
+	}
+	
+	public boolean dropBlank (Player player) {
+		//revert to blank
+		final Item blankPetScroll = SingletonRepository.getEntityManager().getItem(
+				"blank pet scroll");
+		
+		player.equipOrPutOnGround(blankPetScroll);
+		player.sendPrivateText("You summon your pet back onto this plane. Faint smoke lingers from the page.");
+
+		return true;
+	}
+	
+	
+}
