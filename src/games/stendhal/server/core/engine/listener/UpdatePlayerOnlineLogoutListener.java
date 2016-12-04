345b017a58aed7d0f2c7a87906c721d1a7a95843
Markus <madmetzger@users.sf.net>
2016-12-04 11:15:43 +0100

    Pull logout logic out of StendhalRPRuleProcessor

---
+++ src/games/stendhal/server/core/engine/listener/UpdatePlayerOnlineLogoutListener.java
@@ -0,0 +1,28 @@
+package games.stendhal.server.core.engine.listener;
+
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.Task;
+import games.stendhal.server.core.events.LogoutListener;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.events.PlayerLoggedOutEvent;
+/**
+ * UpdatePlayerOnlineLogoutListener is responsible for sending PlayerLoggedOutEvents on Logout
+ * 
+ * @author markus
+ */
+public class UpdatePlayerOnlineLogoutListener implements LogoutListener{
+
+	@Override
+	public void onLoggedOut(Player player) {
+		SingletonRepository.getRuleProcessor().getOnlinePlayers().forAllPlayersExecute(new Task<Player>() {
+			@Override
+			public void execute(final Player player) {
+				player.addEvent(new PlayerLoggedOutEvent(player.getName()));
+				player.notifyWorldAboutChanges();
+			}
+		});
+	}
+
+	
+	
+}
