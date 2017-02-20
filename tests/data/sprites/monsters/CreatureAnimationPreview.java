fb1fae9c8ddf4bbfbc206b0ba157648b1d6582d3
Kimmo Rundelin <kiheru@users.sourceforge.net>
2017-02-20 13:07:13 +0200

    Simplified code.

--- tests/data/sprites/monsters/CreatureAnimationPreview.java
+++ tests/data/sprites/monsters/CreatureAnimationPreview.java
@@ -29,99 +29,54 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.SwingUtilities;
+import javax.swing.event.TreeSelectionListener;
 
 public class CreatureAnimationPreview {
 	private static final int NUMBER_OF_ROWS = 4;
 	private static final int NUMBER_OF_FRAMES = 3;
 	
-	private JFrame jFrame;
-	private JSplitPane jSplitPane;
-	private JScrollPane jScrollPane;
-	private FileTree jTree;
-	private JPanel jPanel;
+	private final JFrame jFrame = new JFrame();
 	private final JLabel[] animationPanel = new JLabel[NUMBER_OF_ROWS];
 	private final JLabel mainPanel = new JLabel();
-	private AnimationRunner[] animations;
+	private final AnimationRunner[] animations = new AnimationRunner[NUMBER_OF_ROWS];
+	private final JSplitPane split = new JSplitPane();
 
+	private CreatureAnimationPreview() {
+		jFrame.setSize(new Dimension(818, 470));
+		JScrollPane scroll = new JScrollPane();
+		scroll.setViewportView(createJTree());
+		split.setLeftComponent(scroll);
+		
+		JPanel rightPanel = new JPanel();
+		JComponent row = new JComponent() {};
+		row.setLayout(new BoxLayout(row, BoxLayout.X_AXIS));
+		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
 
-	/**
-	 * This method initializes jPanel.
-	 * 
-	 * @return javax.swing.JPanel
-	 */
-	@SuppressWarnings("serial")
-	private JPanel getJPanel() {
-		if (jPanel == null) {
-			jPanel = new JPanel();
-			JComponent row = new JComponent() {};
-			row.setLayout(new BoxLayout(row, BoxLayout.X_AXIS));
-			jPanel.setLayout(new BoxLayout(jPanel, BoxLayout.Y_AXIS));
-
-			for (int i = 0; i < NUMBER_OF_ROWS; i++) {
-				JLabel l = new JLabel();
-				animationPanel[i] = l;
-				row.add(l);
-			}
-			jPanel.add(row);
-			jPanel.add(mainPanel);
+		for (int i = 0; i < NUMBER_OF_ROWS; i++) {
+			JLabel l = new JLabel();
+			animationPanel[i] = l;
+			animations[i] = new AnimationRunner(l);
+			row.add(l);
 		}
-		return jPanel;
+
+		rightPanel.add(row);
+		rightPanel.add(mainPanel);
+		
+		split.setRightComponent(rightPanel);
+		jFrame.setContentPane(split);
+		jFrame.setTitle("animated Monsters test");
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		jFrame.setVisible(true);
 	}
 
 	public static void main(final String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
-				new CreatureAnimationPreview().getJFrame().setVisible(true);	
+				new CreatureAnimationPreview();	
 			}
 		});
-	}
-
-	
-	/**
-	 * This method initializes jFrame.
-	 * 
-	 * @return javax.swing.JFrame
-	 */
-	private JFrame getJFrame() {
-		if (jFrame == null) {
-			jFrame = new JFrame();
-
-			jFrame.setSize(new Dimension(818, 470));
-
-			jFrame.setContentPane(getJSplitPane());
-			jFrame.setTitle("animated Monsters test");
-			jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		}
-
-		return jFrame;
-	}
-
-	/**
-	 * This method initializes jSplitPane.
-	 * 
-	 * @return javax.swing.JSplitPane
-	 */
-	private JSplitPane getJSplitPane() {
-		if (jSplitPane == null) {
-			jSplitPane = new JSplitPane();
-			jSplitPane.setLeftComponent(getJScrollPane());
-			jSplitPane.setRightComponent(getJPanel());
-		}
-		return jSplitPane;
-	}
-
-	/**
-	 * This method initializes jScrollPane.
-	 * 
-	 * @return javax.swing.JScrollPane
-	 */
-	private JScrollPane getJScrollPane() {
-		if (jScrollPane == null) {
-			jScrollPane = new JScrollPane();
-			jScrollPane.setViewportView(getJTree());
-		}
-		return jScrollPane;
 	}
 
 	/**
@@ -129,92 +84,80 @@ public class CreatureAnimationPreview {
 	 * 
 	 * @return data.sprites.monsters.FileTree
 	 */
-	private FileTree getJTree() {
-		if (jTree == null) {
-			try {
-				final Preferences pref = Preferences.userNodeForPackage(AnimationRunner.class);
-				File lastDirectory = null;
-				if (pref.get("lastpath", null) != null) {
-					lastDirectory = new File(pref.get("lastpath", null));
-
-				}
-
-				final JFileChooser fc = new JFileChooser(lastDirectory);
-
-				fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-				final int returnVal = fc.showOpenDialog(jScrollPane);
-
-				if (returnVal == JFileChooser.APPROVE_OPTION) {
-					final File file = fc.getSelectedFile();
-					Preferences.userNodeForPackage(AnimationRunner.class).put("lastpath", file.getAbsolutePath());
-
-					jTree = new FileTree(file.getPath());
-				} else {
-					System.exit(0);
-				}
-
-			} catch (final FileNotFoundException e1) {
-				e1.printStackTrace();
-			} catch (final SecurityException e1) {
-				e1.printStackTrace();
+	private FileTree createJTree() {
+		FileTree jTree = null;
+		try {
+			final Preferences pref = Preferences.userNodeForPackage(AnimationRunner.class);
+			File lastDirectory = null;
+			if (pref.get("lastpath", null) != null) {
+				lastDirectory = new File(pref.get("lastpath", null));
 			}
-			if (jTree != null) {
-				jTree.addTreeSelectionListener(new javax.swing.event.TreeSelectionListener() {
-					@Override
-					public void valueChanged(final javax.swing.event.TreeSelectionEvent e) {
-						jFrame.setTitle(e.getNewLeadSelectionPath().getLastPathComponent().toString());
 
-						BufferedImage buf = null;
-						try {
-							final File file = new File(e.getNewLeadSelectionPath().getLastPathComponent().toString());
-							if (file.isDirectory()) {
-								return;
-							}
+			final JFileChooser fc = new JFileChooser(lastDirectory);
 
-							buf = ImageIO.read(file);
-							if (buf == null) {
-								return;
-							}
-						} catch (final IOException e1) {
-							// should never happen;
-							e1.printStackTrace();
-							return;
-						}
+			fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			final int returnVal = fc.showOpenDialog(jFrame);
 
-						mainPanel.setIcon(new ImageIcon(buf));
-						if (animations == null) {
-							animations = new AnimationRunner[4];
-							for (int i = 0; i < NUMBER_OF_ROWS; i++) {
-								animations[i] = createAnimation(animationPanel[i]);
-							}
-						}
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				final File file = fc.getSelectedFile();
+				Preferences.userNodeForPackage(AnimationRunner.class).put("lastpath", file.getAbsolutePath());
 
-						animations[0].startAnimation(buffersCreate(buf, 0));
-						animations[1].startAnimation(buffersCreate(buf, 1));
-						animations[2].startAnimation(buffersCreate(buf, 2));
-						animations[3].startAnimation(buffersCreate(buf, 3));
-					}
-				});
+				jTree = new FileTree(file.getPath());
+				jTree.addTreeSelectionListener(new TreeSelector());
+			} else {
+				System.exit(0);
 			}
+
+		} catch (final FileNotFoundException e1) {
+			e1.printStackTrace();
+		} catch (final SecurityException e1) {
+			e1.printStackTrace();
 		}
+
 		return jTree;
 	}
-
-	private AnimationRunner createAnimation(JLabel viewer) {
-		return new AnimationRunner(viewer);
-	}
-
+	
 	private BufferedImage[] buffersCreate(final BufferedImage buf, final int row) {
 		final BufferedImage[] buffers = new BufferedImage[NUMBER_OF_FRAMES];
 		final int framewidth = buf.getWidth() / CreatureAnimationPreview.NUMBER_OF_FRAMES;
 
 		final int frameheight = buf.getHeight() / CreatureAnimationPreview.NUMBER_OF_ROWS;
 		for (int i = 0; i < NUMBER_OF_FRAMES; i++) {
-
 			buffers[i] = buf.getSubimage(i * framewidth, row * frameheight, framewidth, frameheight);
-
 		}
 		return buffers;
 	}
 
+	/**
+	 * Handler for tree selection changes.
+	 */
+	private class TreeSelector implements TreeSelectionListener {
+		@Override
+		public void valueChanged(final javax.swing.event.TreeSelectionEvent e) {
+			jFrame.setTitle(e.getNewLeadSelectionPath().getLastPathComponent().toString());
+
+			BufferedImage buf = null;
+			try {
+				final File file = new File(e.getNewLeadSelectionPath().getLastPathComponent().toString());
+				if (file.isDirectory()) {
+					return;
+				}
+
+				buf = ImageIO.read(file);
+				if (buf == null) {
+					return;
+				}
+			} catch (final IOException e1) {
+				// should never happen;
+				e1.printStackTrace();
+				return;
+			}
+
+			mainPanel.setIcon(new ImageIcon(buf));
+			for (int i = 0; i < NUMBER_OF_ROWS; i++) {
+				animations[i].startAnimation(buffersCreate(buf, i));
+			}
+			split.resetToPreferredSizes();
+		}
+	}
 }
