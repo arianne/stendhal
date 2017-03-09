b667246e94db5acd617e15d95546bd68877d7838
Hendrik Brummermann <nhnb@users.sourceforge.net>
2017-03-09 23:07:03 +0100

    updated version number

--- src/games/stendhal/common/Debug.java
+++ src/games/stendhal/common/Debug.java
@@ -14,7 +14,7 @@ package games.stendhal.common;
 
 /**
  * Gathers all Debug constants in one place.
- *
+ * 
  * @author mtotz
  */
 public class Debug {
@@ -26,7 +26,7 @@ public class Debug {
 
 	/** version. */
 	// Note: This line is updated by build.xml using a regexp so be sure to adjust it in case you modify this line.
-	public static final String VERSION = "1.26.5";
+	public static final String VERSION = "1.24.5";
 
 	/** pre release suffix */
 	// Note: This line is updated by build.xml using a regexp so be sure to adjust it in case you modify this line.
@@ -36,7 +36,7 @@ public class Debug {
 	 * Log list sizes to find the memory leak. It must be somewhere...
 	 */
 	public static final boolean SHOW_LIST_SIZES = false;
-
+	
 	/**
 	 * it shows entity's view area
 	 */
