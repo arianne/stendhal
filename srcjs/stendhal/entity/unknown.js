135f4f66a0eb63e3937100df6a439838841ff704
Hendrik Brummermann <nhnb@users.sourceforge.net>
2017-03-18 13:16:47 +0100

    log unhandled entities and make them inspectable

--- srcjs/stendhal/entity/unknown.js
+++ srcjs/stendhal/entity/unknown.js
@@ -1,35 +1,34 @@
 /***************************************************************************
  *                   (C) Copyright 2003-2017 - Stendhal                    *
  ***************************************************************************
+ ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Affero General Public License as        *
- *   published by the Free Software Foundation; either version 3 of the    * 
- *   License, or (at your option) any later version.                       *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
 
 "use strict";
 
-var marauroa = window.marauroa = window.marauroa || {};
-
 /**
- * Unknown entity
+ * Item
  */
-marauroa.rpobjectFactory["unknown"] = marauroa.util.fromProto(marauroa.rpobjectFactory["entity"], {
+marauroa.rpobjectFactory.unknown = marauroa.util.fromProto(marauroa.rpobjectFactory.entity, {
 	zIndex: 1,
 
 	init: function() {
-		marauroa.rpobjectFactory["unknown"].proto.init.apply(this, arguments);
+		marauroa.rpobjectFactory.unknown.proto.init.apply(this, arguments);
 		var that = this;
 		setTimeout(function() {
-			console.log("Unknown entity", that["_rpclass"], that["x"], that["y"]);
+			console.log("Unknown entity", that._rpclass, that.x, that.y);
 		}, 1);
 	},
 	
 	isVisibleToAction: function(filter) {
-		return (marauroa.me["adminlevel"] && marauroa.me["adminlevel"] >= 600);
+		return (marauroa.me.adminlevel && marauroa.me.adminlevel >= 600);
 	}
 });
 
-marauroa.rpobjectFactory["_default"] = marauroa.rpobjectFactory["unknown"];
+marauroa.rpobjectFactory._default = marauroa.rpobjectFactory.unknown;
