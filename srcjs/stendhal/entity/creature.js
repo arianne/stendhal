d0b9f4f7e3ad4f5ae3a285d48db500199bede349
Hendrik Brummermann <nhnb@users.sourceforge.net>
2017-03-20 23:57:19 +0100

    added growing spawner (grain, carrots, etc...)

--- srcjs/stendhal/entity/creature.js
+++ srcjs/stendhal/entity/creature.js
@@ -1,22 +1,21 @@
 /***************************************************************************
  *                   (C) Copyright 2003-2017 - Stendhal                    *
  ***************************************************************************
+ ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Affero General Public License as        *
- *   published by the Free Software Foundation; either version 3 of the    * 
- *   License, or (at your option) any later version.                       *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
 
 "use strict";
 
-var marauroa = window.marauroa = window.marauroa || {};
-
 /**
  * Creature
  */
-marauroa.rpobjectFactory["creature"] = marauroa.util.fromProto(marauroa.rpobjectFactory["rpentity"], {
+marauroa.rpobjectFactory.creature = marauroa.util.fromProto(marauroa.rpobjectFactory.rpentity, {
 	minimapStyle: "rgb(255,255,0)",
 	spritePath: "monsters",
 	titleStyle: "#ffc8c8",
@@ -24,7 +23,7 @@ marauroa.rpobjectFactory["creature"] = marauroa.util.fromProto(marauroa.rpobject
 	onclick: function(x, y) {
 		var action = {
 				"type": "attack", 
-				"target": "#" + this["id"]
+				"target": "#" + this.id
 			};
 		marauroa.clientFramework.sendAction(action);
 	},
--- srcjs/stendhal/entity/growingspawner.js
+++ srcjs/stendhal/entity/growingspawner.js
@@ -1,23 +1,21 @@
 /***************************************************************************
  *                   (C) Copyright 2003-2017 - Stendhal                    *
  ***************************************************************************
+ ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Affero General Public License as        *
- *   published by the Free Software Foundation; either version 3 of the    * 
- *   License, or (at your option) any later version.                       *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
 
 "use strict";
 
-var marauroa = window.marauroa = window.marauroa || {};
-var stendhal = window.stendhal = window.stendhal || {};
-
 /**
  * Creature
  */
-marauroa.rpobjectFactory["growing_entity_spawner"] = marauroa.util.fromProto(marauroa.rpobjectFactory["entity"], {
+marauroa.rpobjectFactory.growing_entity_spawner = marauroa.util.fromProto(marauroa.rpobjectFactory.entity, {
 	zIndex: 3000,
 
 	/**
@@ -37,13 +35,13 @@ marauroa.rpobjectFactory["growing_entity_spawner"] = marauroa.util.fromProto(mar
 				type: "use"
 			});
 		}
-		marauroa.rpobjectFactory["rpentity"].proto.buildActions.apply(this, arguments);
+		marauroa.rpobjectFactory.rpentity.proto.buildActions.apply(this, arguments);
 	},
 
 	onclick: function(x, y) {
 		var action = {
 			"type": "use", 
-			"target": "#" + this["id"]
+			"target": "#" + this.id
 		};
 		marauroa.clientFramework.sendAction(action);
 	},
@@ -52,8 +50,8 @@ marauroa.rpobjectFactory["growing_entity_spawner"] = marauroa.util.fromProto(mar
 	 * draw RPEntities
 	 */
 	draw: function(ctx) {
-		var localX = this["x"] * 32;
-		var localY = this["y"] * 32;
+		var localX = this.x * 32;
+		var localY = this.y * 32;
 		var image = stendhal.data.sprites.get("data/sprites/" + this["class"] + ".png");
 		if (image.height) { // image.complete is true on missing image files
 			var count = parseInt(this["max_ripeness"], 10) + 1;
