cfd7c18d9f508dac3caec7f252fba974d3576b0b
Hendrik Brummermann <nhnb@users.sourceforge.net>
2017-03-21 23:01:48 +0100

    use image.height instead of image.complete to ensure an image is loaded

--- srcjs/stendhal/entity/food.js
+++ srcjs/stendhal/entity/food.js
@@ -1,32 +1,18 @@
-/***************************************************************************
- *                   (C) Copyright 2003-2017 - Stendhal                    *
- ***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Affero General Public License as        *
- *   published by the Free Software Foundation; either version 3 of the    * 
- *   License, or (at your option) any later version.                       *
- *                                                                         *
- ***************************************************************************/
-
 "use strict";
 
-var marauroa = window.marauroa = window.marauroa || {};
-var stendhal = window.stendhal = window.stendhal || {};
-
-marauroa.rpobjectFactory["food"] = marauroa.util.fromProto(marauroa.rpobjectFactory["entity"], {
+marauroa.rpobjectFactory.food = marauroa.util.fromProto(marauroa.rpobjectFactory.entity, {
 	set: function(key, value) {
-		marauroa.rpobjectFactory["entity"].set.apply(this, arguments);
+		marauroa.rpobjectFactory.entity.set.apply(this, arguments);
 		if (key === "amount") {
-			this._amount = parseInt(value, 10);
+			this._amount = parseInt(value);
 		}
 	},
 	
 	draw: function(ctx) {
 		var image = stendhal.data.sprites.get("/data/sprites/food.png");
 		if (image.height) {
-			var localX = this["x"] * 32;
-			var localY = this["y"] * 32;
+			var localX = this.x * 32;
+			var localY = this.y * 32;
 			var offset = this._amount * 32;
 			ctx.drawImage(image, 0, offset, 32, 32, localX, localY, 32, 32);
 		}
@@ -36,7 +22,7 @@ marauroa.rpobjectFactory["food"] = marauroa.util.fromProto(marauroa.rpobjectFact
 		console.log(this, x, y);
 		var action = {
 				"type": "look",
-				"target": "#" + this["id"]
+				"target": "#" + this.id
 			};
 		marauroa.clientFramework.sendAction(action);
 	},
