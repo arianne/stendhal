dfb7cb646a1e555e177bb204a4c47a77b12c4bb5
Hendrik Brummermann <nhnb@users.sourceforge.net>
2017-03-24 00:07:59 +0100

    update stats panel on changes only

--- srcjs/stendhal/entity/player.js
+++ srcjs/stendhal/entity/player.js
@@ -1,38 +1,29 @@
 /***************************************************************************
  *                   (C) Copyright 2003-2017 - Stendhal                    *
  ***************************************************************************
+ ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Affero General Public License as        *
- *   published by the Free Software Foundation; either version 3 of the    * 
- *   License, or (at your option) any later version.                       *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
 
 "use strict";
 
-var marauroa = window.marauroa = window.marauroa || {};
-var stendhal = window.stendhal = window.stendhal || {};
-
 /**
  * Player
  */
-marauroa.rpobjectFactory["player"] = marauroa.util.fromProto(marauroa.rpobjectFactory["rpentity"], {
+marauroa.rpobjectFactory.player = marauroa.util.fromProto(marauroa.rpobjectFactory.rpentity, {
 
 	minimapShow: true,
 	minimapStyle: "rgb(255, 255, 255)",
 	dir: 3,
 	
 	set: function(key, value) {
-		marauroa.rpobjectFactory["rpentity"].proto.set.apply(this, arguments);
-		if (key === "text") {
-			this.say(value);
-		} else if (key === "ghostmode") {
-			this.minimapShow = false;
-		}
-		
-		// stats
-		if (marauroa.me !== this) {
+		marauroa.rpobjectFactory.rpentity.proto.set.apply(this, arguments);
+		if (marauroa.me != this) {
 			return;
 		}
 		if (stendhal.ui.stats.keys.indexOf(key) > -1) {
@@ -44,11 +35,11 @@ marauroa.rpobjectFactory["player"] = marauroa.util.fromProto(marauroa.rpobjectFa
 	 * Is this player an admin?
 	 */
 	isAdmin: function() {
-		return (typeof(this["adminlevel"]) !== "undefined" && this["adminlevel"] > 600);
+		return (typeof(this.adminlevel) != "undefined" && this.adminlevel > 600);
 	},
 
 	buildActions: function(list) {
-		marauroa.rpobjectFactory["rpentity"].proto.buildActions.apply(this, arguments);
+		marauroa.rpobjectFactory.rpentity.proto.buildActions.apply(this, arguments);
 	/*
 		boolean hasBuddy = User.hasBuddy(entity.getName());
 		if (!hasBuddy) {
@@ -75,8 +66,8 @@ marauroa.rpobjectFactory["player"] = marauroa.util.fromProto(marauroa.rpobjectFa
 	 */
 	isInHearingRange: function(entity) {
 		return (this.isAdmin() 
-			|| ((Math.abs(this["x"] - entity["x"]) < 15) 
-				&& (Math.abs(this["y"] - entity["y"]) < 15)));
+			|| ((Math.abs(this.x - entity.x) < 15) 
+				&& (Math.abs(this.y - entity.y) < 15)));
 	}
 });
 
