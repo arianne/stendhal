6de8587f3b1e0050ec8b02d51a18b28d464f4065
Hendrik Brummermann <nhnb@users.sourceforge.net>
2017-03-24 00:45:55 +0100

    zone change by clicking near the zone boarder

--- srcjs/stendhal/entity/ground.js
+++ srcjs/stendhal/entity/ground.js
@@ -1,18 +1,17 @@
 /***************************************************************************
- *                   (C) Copyright 2003-2017 - Stendhal                    *
+ *                   (C) Copyright 2003-2014 - Stendhal                    *
+ ***************************************************************************
  ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Affero General Public License as        *
- *   published by the Free Software Foundation; either version 3 of the    * 
- *   License, or (at your option) any later version.                       *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
 
 "use strict";
-var marauroa = window.marauroa = window.marauroa || {};
-var stendhal = window.stendhal = window.stendhal || {};
-
+window.stendhal = window.stendhal || {};
 stendhal.zone = stendhal.zone || {};
 
 /**
@@ -29,11 +28,11 @@ stendhal.zone.ground = {
 	 * Calculates whether the click was close enough to a zone border to trigger
 	 * a zone change.
 	 *
-	 * @param x x of click point in world coordinates
-	 * @param y y of click point in world coordinates
+	 * @param point click point in world coordinates
 	 * @return Direction of the zone to change to, <code>null</code> if no zone change should happen
 	 */
 	calculateZoneChangeDirection: function(x, y) {
+		console.log(x, y, stendhal.data.map.zoneSizeX, stendhal.data.map.zoneSizeY)
 		if (x < 15) {
 			return "4"; // LEFT
 		}
@@ -61,6 +60,7 @@ stendhal.zone.ground = {
 		if (extend) {
 			action.extend = extend;
 		}
+		console.log(action);
 		marauroa.clientFramework.sendAction(action);
 	}
 
