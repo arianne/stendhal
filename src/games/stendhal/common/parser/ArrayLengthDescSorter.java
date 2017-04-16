9bc77265376f19f7b147a1f0207f2cde2cd7531e
Hendrik Brummermann <nhnb@users.sourceforge.net>
2017-04-16 23:33:09 +0200

    fixed Parser to use the longest candidate CompoundName, that matches

--- src/games/stendhal/common/parser/ArrayLengthDescSorter.java
+++ src/games/stendhal/common/parser/ArrayLengthDescSorter.java
@@ -15,26 +15,15 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 /**
- * sorts a array lists based on the length, the order of
- * list with the same length is undefined but consistent.
- *
+ * sorts an array list
+ * 
  * @param <T> ArrayList
  */
 public class ArrayLengthDescSorter<T extends ArrayList<?>> implements Comparator<T> {
 
 	@Override
 	public int compare(T o1, T o2) {
-		int res = o2.size() - o1.size();
-		if (res != 0) {
-			return res;
-		}
-
-		// Both arrays have equal length, so we don't care which one to put first.
-		// But, we may not return 0 as that means that both arguments are within
-		// the same equivalence class, thus only one object will survive in a Set.
-		// Therefore we distinguish ArrayLists with the same length based
-		// on other properties.
-		return o1.toString().compareTo(o2.toString());
+		return o2.size() - o1.size();
 	}
 
 }
