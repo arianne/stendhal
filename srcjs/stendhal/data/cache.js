581ccd858e77e1bce5b9026c8c48a50db353ce1a
Hendrik Brummermann <nhnb@users.sourceforge.net>
2017-04-23 17:44:14 +0200

    implemented cache infrastructure

--- srcjs/stendhal/data/cache.js
+++ srcjs/stendhal/data/cache.js
@@ -1,11 +1,12 @@
 /***************************************************************************
  *                   (C) Copyright 2017 - Faiumoni e. V.                   *
  ***************************************************************************
+ ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU Affero General Public License as        *
- *   published by the Free Software Foundation; either version 3 of the    * 
- *   License, or (at your option) any later version.                       *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
 
@@ -23,56 +24,55 @@ stendhal.data.cache = {
 
 		// https://dzone.com/articles/getting-all-stored-items
 		function requestAllItems(storeName, callback) {
-			var tx = stendhal.data.cache.db.transaction(storeName, IDBTransaction.READ_ONLY);
-			var store = tx.objectStore(storeName);
-			var items = [];
-			
-			tx.oncomplete = function(evt) {  
-				callback(items);
-			};
-			var cursorRequest = store.openCursor();
-			cursorRequest.onerror = function(error) {
-				console.log(error);
-			};
-			cursorRequest.onsuccess = function(evt) {                    
-				var cursor = evt.target.result;
-				if (cursor) {
-					items.push(cursor.value);
-					cursor.continue();
-				}
-			};
+		    var tx = stendhal.data.cache.db.transaction(storeName, IDBTransaction.READ_ONLY);
+		    var store = tx.objectStore(storeName);
+		    var items = [];
+
+		    tx.oncomplete = function(evt) {  
+		        callback(items);
+		    };
+		    var cursorRequest = store.openCursor();
+		    cursorRequest.onerror = function(error) {
+		        console.log(error);
+		    };
+		    cursorRequest.onsuccess = function(evt) {                    
+		        var cursor = evt.target.result;
+		        if (cursor) {
+		            items.push(cursor.value);
+		            cursor.continue();
+		        }
+		    };
 		}
 
 		stendhal.data.cache.sync = {
 			"cid": "noIndexedDB"
-		};
-
+		}
 		if (!window.indexedDB) {
 			return;
 		}
 		var open = indexedDB.open("stendhal", 1);
 
 		open.onupgradeneeded = function() {
-			var db = open.result;
-			db.createObjectStore("cache", {keyPath: "key"});
+		    var db = open.result;
+		    db.createObjectStore("cache", {keyPath: "key"});
 		};
 
 		open.onsuccess = function() {
 			stendhal.data.cache.db = open.result;
 			requestAllItems("cache", function(items) {
 				var len = items.length;
-				for (var i = 0; i < len; i += 1) {
-					stendhal.data.cache.sync[items[i].key] = items[i].data;
-				}
-				if (stendhal.data.cache.sync["cid"] === "noIndexedDB") {
-					stendhal.data.cache.put("cid", (Math.random()*1e48).toString(36));
-				}
+			    for (var i = 0; i < len; i += 1) {
+			    	stendhal.data.cache.sync[items[i].key] = items[i].data;
+			    }
+			    if (stendhal.data.cache.sync["cid"] == "noIndexedDB") {
+			    	stendhal.data.cache.put("cid", (Math.random()*1e48).toString(36));
+			    }
 			});
 		};
 		open.onerror = function(event) {
-			marauroa = {};
-			stendhal = {};
-			alert("Could not initialize cache.");
+			  marauroa = {};
+			  stendhal = {};
+			  alert("Could not initialize cache.");
 		};
 	},
 
@@ -81,14 +81,14 @@ stendhal.data.cache = {
 	},
 
 	put: function(key, value) {
-		if (stendhal.data.cache.sync[key] === value) {
+		if (stendhal.data.cache.sync[key] == value) {
 			return;
 		}
 		stendhal.data.cache.sync[key] = value;
 
-		var tx = stendhal.data.cache.db.transaction("cache", "readwrite");
-		var store = tx.objectStore("cache");
-		store.put({key: key, data: value});
+	    var tx = stendhal.data.cache.db.transaction("cache", "readwrite");
+	    var store = tx.objectStore("cache");
+	    store.put({key: key, data: value});
 	}
 }
 
