a7ccc47375573b1f1a59cef22fac56ef4e4649ae
Hendrik Brummermann <nhnb@users.sourceforge.net>
2017-05-28 00:24:34 +0200

    added or updated license header

--- srcjs/stendhal/data/cache.js
+++ srcjs/stendhal/data/cache.js
@@ -1,12 +1,11 @@
 /***************************************************************************
  *                   (C) Copyright 2017 - Faiumoni e. V.                   *
  ***************************************************************************
- ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *   it under the terms of the GNU Affero General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    * 
+ *   License, or (at your option) any later version.                       *
  *                                                                         *
  ***************************************************************************/
 
