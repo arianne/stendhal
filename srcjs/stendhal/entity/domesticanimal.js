992343aba0559e82cff81219aa6dbc7ff3dd780e
Hendrik Brummermann <nhnb@users.sourceforge.net>
2017-05-28 00:24:34 +0200

    added or updated license header

--- srcjs/stendhal/entity/domesticanimal.js
+++ srcjs/stendhal/entity/domesticanimal.js
@@ -1,3 +1,14 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2017 - Stendhal                    *
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Affero General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    * 
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
 "use strict";
 
 marauroa.rpobjectFactory["domesticanimal"] = marauroa.util.fromProto(marauroa.rpobjectFactory["rpentity"], {
--- srcjs/stendhal/ui/html.js
+++ srcjs/stendhal/ui/html.js
@@ -1,12 +1,11 @@
 /***************************************************************************
- *                   (C) Copyright 2003-2014 - Stendhal                    *
- ***************************************************************************
+ *                   (C) Copyright 2003-2017 - Stendhal                    *
  ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *   it under the terms of the GNU Affero General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    * 
+ *   License, or (at your option) any later version.                       *
  *                                                                         *
  ***************************************************************************/
 
