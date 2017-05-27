99325c172d73bcf276cb014c734d7e219808acf7
Hendrik Brummermann <nhnb@users.sourceforge.net>
2017-05-27 18:25:47 +0200

    fixed searchCompoundName failing on  words the equal prefix and length

--- src/games/stendhal/common/parser/ArrayLengthDescSorter.java
+++ src/games/stendhal/common/parser/ArrayLengthDescSorter.java
@@ -15,7 +15,8 @@ import java.util.ArrayList;
 import java.util.Comparator;
 
 /**
- * sorts an array list
+ * sorts a array lists based on the length, the order of
+ * list with the same length is undefined but consistent.
  * 
  * @param <T> ArrayList
  */
@@ -23,7 +24,17 @@ public class ArrayLengthDescSorter<T extends ArrayList<?>> implements Comparator
 
 	@Override
 	public int compare(T o1, T o2) {
-		return o2.size() - o1.size();
+		int res = o2.size() - o1.size();
+		if (res != 0) {
+			return res;
+		}
+
+		// Both arrays have equal length, so we don't care which one to put first.
+		// But, we may not return 0 as that means that both arguments are within
+		// the same equivalence class, thus only one object will survive in a Set.
+		// Therefore we distinguish ArrayLists with the same length based
+		// on other properties.
+		return o1.toString().compareTo(o2.toString());
 	}
 
 }
