222932663d7495dc6138671972b4731f528e3e57
Snowflake <mudmagicworld@gmail.com>
2017-06-04 19:33:45 +0200

    added soldiers to Ados wall (feature #1411)

---
+++ src/games/stendhal/server/maps/ados/wall/WallSoldier1NPC.java
@@ -0,0 +1,76 @@
+/***************************************************************************
+ *                     (C) Copyright 2017 - Stendhal                       *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.maps.ados.wall;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.pathfinder.FixedPath;
+import games.stendhal.server.core.pathfinder.Node;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+
+/**
+ * Soldiers on the wall
+ *
+ * @author snowflake
+ * @author hendrik
+ */
+public class WallSoldier1NPC implements ZoneConfigurator {
+	/**
+	 * Configure a zone.
+	 *
+	 * @param	zone		The zone to be configured.
+	 * @param	attributes	Configuration attributes.
+	 */
+	@Override
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		buildAdosWallSoldier(zone);
+	}
+
+	/**
+	 * Creatures a soldier on the city wall
+	 *
+	 * @param zone StendhalRPZone
+	 */
+	private void buildAdosWallSoldier(final StendhalRPZone zone) {
+
+		final SpeakerNPC npc = new SpeakerNPC("Galercus") {
+
+			@Override
+			protected void createPath() {
+				final List<Node> path = new LinkedList<Node>();
+				path.add(new Node(79,  1));
+				path.add(new Node(79, 17));
+				path.add(new Node(76, 17));
+				path.add(new Node(76,  1));
+				setPath(new FixedPath(path, true));
+			}
+
+			@Override
+			protected void createDialog() {
+				addGreeting("Hi, welcome to Ados City!");
+				addJob("A job? Are you blind? I'm a guard! Isn't this obvious? Mow move along, citizen. I don't have time for chatting.");
+				addHelp("If you need a map to guide you around Ados, just soldier Julius near the city gate.");
+				addGoodbye("I hope you will enjoy your visit to Ados.");
+			}
+		};
+
+		npc.setEntityClass("youngsoldiernpc");
+		npc.setPosition(79,  1);
+		npc.initHP(100);
+		npc.setDescription("You see Galercus, a soldier who guards the city wall of Ados.");
+		zone.add(npc);
+	}
+}
