d59f8b7018df082438b14009dad4f5827a82f923
Jordan Irwin <antumdeluge@gmail.com>
2017-10-06 01:03:09 -0700

    New 'granmanpc' sprite (CC BY 3.0):
    From 24x32 characters with faces (big pack) by Svetlana Kushnariova:
    https://opengameart.org/node/24823

--- src/games/stendhal/server/maps/nalwor/city/GuessKillsNPC.java
+++ src/games/stendhal/server/maps/nalwor/city/GuessKillsNPC.java
@@ -99,7 +99,7 @@ public class GuessKillsNPC implements ZoneConfigurator {
             }
         };
 
-        npc.setEntityClass("noimagenpc"); /* granmanpc */
+        npc.setEntityClass("granmanpc");
         npc.setDescription("You see Crearid, a very attentive old lady.");
         npc.setPosition(30, 83);
         npc.initHP(100);
