3809a3283ef59b5736456d4b462094e54baa2913
Jordan Irwin <antumdeluge@gmail.com>
2017-10-12 23:30:42 -0700

    New 'gypsywomannpc' (Rose Leigh) sprite (CC BY 3.0):
    Based on work by Svetlana Kushnariova: https://opengameart.org/node/72969

--- src/games/stendhal/server/maps/semos/house/FlowerSellerNPC.java
+++ src/games/stendhal/server/maps/semos/house/FlowerSellerNPC.java
@@ -49,7 +49,7 @@ public class FlowerSellerNPC implements ZoneConfigurator {
 			}
 		};
 
-		rose.setEntityClass("noimagenpc"); /* gypsywomannpc */
+		rose.setEntityClass("gypsywomannpc");
 		rose.initHP(100);
 		rose.setCollisionAction(CollisionAction.REVERSE);
 		rose.setDescription("You see Rose Leigh. She jumps from place to place with a basket filled with lovely rhosyds.");
