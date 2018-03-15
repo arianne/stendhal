24f53ba881381b8caa7e5512b803a8aa1cf30ee4
onu <onu@users.sourceforge.net>
2018-03-15 03:47:43 -0700

    Dagobert (youngnpc) redone (OGA BY 3.0)...
    ...using Townfolk-Adult-M-002 light.png and Townfolk-Adult-M-003
    light.png from 24x32 characters pack extended by diamonddmgirl and
    similar to old colors.
    
    GitHub issue 87: https://github.com/arianne/stendhal/issues/87

--- src/games/stendhal/server/maps/fado/bank/TellerNPC.java
+++ src/games/stendhal/server/maps/fado/bank/TellerNPC.java
@@ -67,7 +67,7 @@ public class TellerNPC implements ZoneConfigurator {
 			}
 		};
 
-		bankNPC.setEntityClass("noimagenpc"); /* youngnpc */
+		bankNPC.setEntityClass("youngnpc");
 		bankNPC.setPosition(15, 3);
 		bankNPC.initHP(1000);
 		bankNPC.setDescription("Yance is the Fado bank manager. He can give advice on how to use the chests.");
