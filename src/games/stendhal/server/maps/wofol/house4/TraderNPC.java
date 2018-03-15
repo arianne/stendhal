36048a4009acc4e50361c24a50e660806149e261
Jordan Irwin <antumdeluge@gmail.com>
2018-03-15 02:26:32 -0700

    Use veteran_kobold sprite for koboldnpc (Wrvil):
    Will create new outfit later.

--- src/games/stendhal/server/maps/wofol/house4/TraderNPC.java
+++ src/games/stendhal/server/maps/wofol/house4/TraderNPC.java
@@ -72,7 +72,7 @@ public class TraderNPC implements ZoneConfigurator {
 			}
 		};
 
-		trader.setEntityClass("noimagenpc"); /* koboldnpc */
+		trader.setEntityClass("koboldnpc");
 		trader.setPosition(4, 4);
 		trader.initHP(100);
 		trader.setDescription("You see Wrvil. He can equip and maybe make you rich at the same time.");
