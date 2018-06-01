ada192bed882d8defdbcbc2656af620f4ad304e1
Hendrik Brummermann <nhnb@users.sourceforge.net>
2018-06-01 22:51:00 +0200

    updated version number to 1.27

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-STENDHAL_VERSION="1.26.5"
+STENDHAL_VERSION="1.27"
 
 LOCALCLASSPATH=.:data/script/:data/conf/:stendhal-server-$STENDHAL_VERSION.jar:marauroa.jar:mysql-connector.jar:log4j.jar:commons-lang.jar:h2.jar
 
