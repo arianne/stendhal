acb1f033d61ccba99c56cb2e7ebe0c6feab2e2a4
Jordan Irwin <antumdeluge@gmail.com>
2018-07-08 17:42:55 -0700

    Zoo Food Quest: Award +5 karma for accepting quest

--- src/games/stendhal/server/maps/quests/ZooFood.java
+++ src/games/stendhal/server/maps/quests/ZooFood.java
@@ -177,7 +177,7 @@ public class ZooFood extends AbstractQuest {
 		npc.add(ConversationStates.QUEST_OFFERED, ConversationPhrases.YES_MESSAGES,
 				null,
 				ConversationStates.ATTENDING, null,
-                new MultipleActions(new SetQuestAction(QUEST_SLOT, "start;"),
+                new MultipleActions(new SetQuestAndModifyKarmaAction(QUEST_SLOT, "start;", 5.0),
 				new StartRecordingRandomItemCollectionAction(QUEST_SLOT, 1, items, "Oh, thank you! Please help us by"
                 + " bringing [item] as soon as you can."))
 		);
