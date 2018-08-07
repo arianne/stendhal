2ab4ff30327d9b7a5f2b289f8c1be0def0892697
Jordan Irwin <antumdeluge@gmail.com>
2018-08-07 13:42:02 -0700

    Create inactive "Heretic" quest achievement...
    - Finish Kill Monks quest 50 times.
    - Achievement score: medium

--- src/games/stendhal/server/core/rp/achievement/factory/QuestAchievementFactory.java
+++ src/games/stendhal/server/core/rp/achievement/factory/QuestAchievementFactory.java
@@ -33,11 +33,14 @@ public class QuestAchievementFactory extends AbstractAchievementFactory {
 	public Collection<Achievement> createAchievements() {
 		List<Achievement> questAchievements = new LinkedList<Achievement>();
 
-		//elf princess quest achievement
+		// Elf Princess quest achievement
 		questAchievements.add(createAchievement("quest.special.elf_princess.0025", "Faiumoni's Casanova", "Finish elf princess quest 25 times",
 												Achievement.MEDIUM_BASE_SCORE, true, new QuestStateGreaterThanCondition("elf_princess", 2, 24)));
+		// Kill Monks quest achievement
+		questAchievements.add(createAchievement("quest.special.kill_monks.0050", "Heretic", "Finish Kill Monks quest 50 times",
+				Achievement.MEDIUM_BASE_SCORE, false, new QuestStateGreaterThanCondition("kill_monks", 2, 49)));
 
-		//Maze
+		// Maze
 		questAchievements.add(createAchievement("quest.special.maze", "Pathfinder", "Finish the maze",
 				Achievement.EASY_BASE_SCORE, true, new QuestStateGreaterThanCondition("maze", 2, 0)));
 		questAchievements.add(createAchievement("quest.deathmatch", "Deathmatch Hero", "Earn 100,000 points in deathmatch",
