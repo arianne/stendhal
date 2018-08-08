48b3c044d178d07ef7c73d0b787c22975e09f5da
Jordan Irwin <antumdeluge@gmail.com>
2018-08-07 18:43:09 -0700

    Add "Sergeant" achievement (disabled)...
    Description: Finish Kill Enemy Army quest 5 times
    Difficulty: medium

--- src/games/stendhal/server/core/rp/achievement/factory/MithrilbourghEnemyArmyAchievementFactory.java
+++ src/games/stendhal/server/core/rp/achievement/factory/MithrilbourghEnemyArmyAchievementFactory.java
@@ -16,6 +16,7 @@ import java.util.LinkedList;
 
 import games.stendhal.server.core.rp.achievement.Achievement;
 import games.stendhal.server.core.rp.achievement.Category;
+import games.stendhal.server.entity.npc.condition.QuestStateGreaterThanCondition;
 
 /**
  * Factory for MithrilbourghEnemyArmyAchievement
@@ -32,11 +33,10 @@ public class MithrilbourghEnemyArmyAchievementFactory extends AbstractAchievemen
 		LinkedList<Achievement> achievements = new LinkedList<Achievement>();
 
 		// Index where number of completions is stored
-		// final int IDX = 3;
+		final int IDX = 3;
 
-		/* Disabled until ready to implement
 		achievements.add(createAchievement("quest.special.kill_enemy_army.0005", "Sergeant", "Finish Kill Enemy Army quest 5 times",
-				Achievement.MEDIUM_BASE_SCORE, false, new QuestStateGreaterThanCondition("kill_enemy_army", IDX, 4)));*/
+				Achievement.MEDIUM_BASE_SCORE, false, new QuestStateGreaterThanCondition("kill_enemy_army", IDX, 4)));
 
 		return achievements;
 	}
