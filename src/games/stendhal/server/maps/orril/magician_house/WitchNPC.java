92cd4eb06003a02f10688eb9bfaffcc2b7e4ab9d
Jordan Irwin <antumdeluge@gmail.com>
2018-09-15 04:36:40 -0700

    Don't use CollisionAction.STOP for Jynath (she is flying & doesn't...
    ...want to stop).

--- src/games/stendhal/server/maps/orril/magician_house/WitchNPC.java
+++ src/games/stendhal/server/maps/orril/magician_house/WitchNPC.java
@@ -22,7 +22,6 @@ import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.pathfinder.FixedPath;
 import games.stendhal.server.core.pathfinder.Node;
-import games.stendhal.server.entity.CollisionAction;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.ShopList;
 import games.stendhal.server.entity.npc.SpeakerNPC;
@@ -115,7 +114,6 @@ public class WitchNPC implements ZoneConfigurator {
 
 		npc.setEntityClass("witchnpc");
 		npc.setPosition(24, 7);
-		npc.setCollisionAction(CollisionAction.STOP);
 		npc.initHP(100);
 		npc.setDescription("You see Jynath, the witch. She is riding on a broom.");
 		npc.setSounds(Arrays.asList("witch-cackle-1"));
