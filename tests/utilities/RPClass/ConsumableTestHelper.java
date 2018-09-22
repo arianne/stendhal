41a9aa3f039ad097a995e23996a4ae789fdc2791
Kimmo Rundelin <kiheru@users.sourceforge.net>
2018-09-22 22:58:40 +0300

    Create a StatusHealer in createImmunizer().

--- tests/utilities/RPClass/ConsumableTestHelper.java
+++ tests/utilities/RPClass/ConsumableTestHelper.java
@@ -16,12 +16,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 import games.stendhal.server.entity.item.ConsumableItem;
+import games.stendhal.server.entity.item.StatusHealer;
 
 public class ConsumableTestHelper {
 
 	public static ConsumableItem createEater(final String name) {
 		ItemTestHelper.generateRPClasses();
-		Map<String, String> attributes = new HashMap<String, String>();
+		Map<String, String> attributes = new HashMap<>();
 		attributes.put("amount", "1");
 		attributes.put("regen", "1");
 		attributes.put("frequency", "1");
@@ -30,13 +31,11 @@ public class ConsumableTestHelper {
 
 	public static ConsumableItem createImmunizer(final String name) {
 		ItemTestHelper.generateRPClasses();
-		Map<String, String> attributes = new HashMap<String, String>();
+		Map<String, String> attributes = new HashMap<>();
 		attributes.put("amount", "1");
 		attributes.put("regen", "0");
 		attributes.put("frequency", "1");
-		return new ConsumableItem(name, "class", "subclass", attributes);
+		attributes.put("immunization", "poisoned");
+		return new StatusHealer(name, "class", "subclass", attributes);
 	}
-
-
-
 }
