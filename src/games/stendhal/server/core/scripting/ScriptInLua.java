589ecb1c0a1b54db8f994c1873266b61218a0bf1
Jordan Irwin <antumdeluge@gmail.com>
2018-09-21 18:53:03 -0700

    Load luajava libraries & add "game" & "logger" Lua globals

--- src/games/stendhal/server/core/scripting/ScriptInLua.java
+++ src/games/stendhal/server/core/scripting/ScriptInLua.java
@@ -13,9 +13,14 @@ package games.stendhal.server.core.scripting;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.luaj.vm2.Globals;
 import org.luaj.vm2.LuaValue;
+import org.luaj.vm2.lib.PackageLib;
+import org.luaj.vm2.lib.jse.CoerceJavaToLua;
+import org.luaj.vm2.lib.jse.JseBaseLib;
 import org.luaj.vm2.lib.jse.JsePlatform;
+import org.luaj.vm2.lib.jse.LuajavaLib;
 
 import games.stendhal.server.entity.player.Player;
 
@@ -24,17 +29,37 @@ import games.stendhal.server.entity.player.Player;
  */
 public class ScriptInLua extends ScriptingSandbox {
 
+	private static final Logger logger = Logger.getLogger(ScriptInLua.class);
+
 	private Globals globals;
 
 	private final String luaScript;
+
+	private LuaValue game;
 
 	public ScriptInLua(String filename) {
 		super(filename);
 
 		globals = JsePlatform.standardGlobals();
 		luaScript = filename;
+
+		globals.load(new JseBaseLib());
+		globals.load(new PackageLib());
+		globals.load(new LuajavaLib());
+
+		game = CoerceJavaToLua.coerce(this);
+		globals.set("game", game);
+		globals.set("logger", CoerceJavaToLua.coerce(logger));
 	}
 
+	/**
+	 * Initial load of the script.
+	 *
+	 * @param player
+	 * 			The admin who loads script or <code>null</code> on server start.
+	 * @param args
+	 * 			The arguments the admin specified or <code>null</code> on server start.
+	 */
 	@Override
 	public boolean load(Player player, List<String> args) {
 		LuaValue chunk = globals.loadfile(luaScript);
