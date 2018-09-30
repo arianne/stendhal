56ac32f006305328aa665f0f32107ae37e54079e
Jordan Irwin <antumdeluge@gmail.com>
2018-09-30 14:59:06 -0700

    Create constructors in server.entity.creature.ItemGuardCreature to...
    ...set optional info string & description attributes to item.

--- src/games/stendhal/server/entity/creature/ItemGuardCreature.java
+++ src/games/stendhal/server/entity/creature/ItemGuardCreature.java
@@ -38,6 +38,12 @@ public class ItemGuardCreature extends Creature {
 	/** which Item to drop */
 	private final String itemType;
 
+	/** optional Item info string */
+	private final String itemInfostring;
+
+	/** optional Item description string */
+	private final String itemDescr;
+
 	/** which quest slot to check */
 	private final String questSlot;
 
@@ -74,7 +80,6 @@ public class ItemGuardCreature extends Creature {
 		this(copy, itemType, questSlot, questState, 0);
 	}
 
-
 	/**
 	 * Creates an ItemGuardCreature.
 	 *
@@ -90,9 +95,34 @@ public class ItemGuardCreature extends Creature {
 	 * 			  the index of the quest slot to look in
 	 */
 	public ItemGuardCreature(final Creature copy, final String itemType, final String questSlot, final String questState, final int questIndex) {
+		this(copy, itemType, null, null, questSlot, questState, questIndex);
+	}
+
+	/**
+	 * Creates an ItemGuardCreature.
+	 *
+	 * @param copy
+	 *            base creature
+	 * @param itemType
+	 *            the quest item to drop on death
+	 * @param itemInfostring
+	 *            optional info string to add to item
+	 * @param itemDescr
+	 *            optional description string to add to item
+	 * @param questSlot
+	 *            the quest slot for the active quest
+	 * @param questState
+	 * 			  the state of the quest to check on dead for
+	 * @param questIndex
+	 * 			  the index of the quest slot to look in
+	 */
+	public ItemGuardCreature(final Creature copy, final String itemType, final String itemInfostring, final String itemDescr,
+			final String questSlot, final String questState, final int questIndex) {
 		super(copy);
 
 		this.itemType = itemType;
+		this.itemInfostring = itemInfostring;
+		this.itemDescr = itemDescr;
 		this.questSlot = questSlot;
 		this.questState = questState;
 		this.questIndex = questIndex;
@@ -113,15 +143,22 @@ public class ItemGuardCreature extends Creature {
 
 	@Override
 	public Creature getNewInstance() {
-		return new ItemGuardCreature(this, itemType, questSlot, questState, questIndex);
+		return new ItemGuardCreature(this, itemType, itemInfostring, itemDescr, questSlot, questState, questIndex);
 	}
 
 	@Override
 	public void onDead(final Killer killer, final boolean remove) {
-
 		if (killer instanceof Player) {
 			final Player killerPlayer = (Player) killer;
-			if (!killerPlayer.isEquipped(itemType)) {
+			boolean playerEquipped;
+
+			if (itemInfostring != null) {
+				playerEquipped = killerPlayer.isEquippedWithInfostring(itemType, itemInfostring);
+			} else {
+				playerEquipped = killerPlayer.isEquipped(itemType);
+			}
+
+			if (!playerEquipped) {
 				if ((questSlot == null) || !killerPlayer.isQuestCompleted(questSlot)) {
 					if(questState != null) {
 						if (killerPlayer.isQuestInState(questSlot, questIndex, questState)) {
@@ -137,8 +174,16 @@ public class ItemGuardCreature extends Creature {
 	}
 
 	private void equipPlayerWithGuardedItem(final Player killerPlayer) {
-		final Item item = SingletonRepository.getEntityManager().getItem(
-				itemType);				item.setBoundTo(killerPlayer.getName());
+		final Item item = SingletonRepository.getEntityManager().getItem(itemType);
+
+		if (itemInfostring != null) {
+			item.put("infostring", itemInfostring);
+		}
+		if (itemDescr != null) {
+			item.put("description", itemDescr);
+		}
+
+		item.setBoundTo(killerPlayer.getName());
 		killerPlayer.equipOrPutOnGround(item);
 	}
 }
