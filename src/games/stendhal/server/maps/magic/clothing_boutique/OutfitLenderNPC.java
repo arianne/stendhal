511fbca186d957a4dc239e0904cf220b725166fb
Jordan Irwin <antumdeluge@gmail.com>
2018-10-01 14:33:28 -0700

    Re-create outfit previews for Saskia & Liliana...
    Notes:
    - "outfits.png" rename to "outfits1.png".
    - Alien body needs re-worked. Currently using placeholder image.

--- src/games/stendhal/server/maps/magic/clothing_boutique/OutfitLenderNPC.java
+++ src/games/stendhal/server/maps/magic/clothing_boutique/OutfitLenderNPC.java
@@ -199,7 +199,7 @@ public class OutfitLenderNPC implements ZoneConfigurator {
 					null,
 					ConversationStates.ATTENDING,
 					"Just tell me if you want to #hire a #gown, #hire a #green #dress, #hire #glasses, #hire #other #glasses, #hire a #hat, #hire an #alien suit, #hire a #horse outfit, #hire a #girl #horse outfit, #hire a #jumpsuit, #hire #dungarees, #hire a #bunny #suit or #hire an #orange outfit.",
-					new ExamineChatAction("outfits.png", "Outfits", "Price varies"));
+					new ExamineChatAction("outfits1.png", "Outfits", "Price varies"));
 				addJob(jobReply);
 				// addJob("I normally work in a clothes boutique, we use magic to put our clients into fantastic outfits. I'm here for Mine Town Revival Weeks, where we #offer our outfits at greatly reduced prices, but they last for less time!");
 				addHelp("Our hired outfits wear off after some time, but you can always come back for more!");
