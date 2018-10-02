2c94359900e8e7351be0dd8f2365aa613fcb7a9e
Kimmo Rundelin <kiheru@users.sourceforge.net>
2018-10-02 20:54:03 +0300

    Updated to current behaviour.

--- tests/games/stendhal/server/maps/quests/SadScientistTest.java
+++ tests/games/stendhal/server/maps/quests/SadScientistTest.java
@@ -196,7 +196,7 @@ public class SadScientistTest {
 		en.step(player, "hi");
 		assertEquals("Welcome citizen! Do you need #help?", getReply(npc));
 		en.step(player, "vera");
-		assertEquals("What? How do you know her? Well it is a sad story. She was picking arandula for Ilisa (they were friends) and she saw the catacombs entrance. 3 months later a young hero saw her, and she was a vampirette. What a sad story. I kept this for her husband. A letter. I think he is in Kalavan.", getReply(npc));
+		assertEquals("What? How do you know her? Well it is a sad story. She was picking arandula for Ilisa (they were friends) and she saw the catacombs entrance. 3 months later a young hero saw her, and she was a vampirette. I kept this for her husband. A letter. I think he is in Kalavan.", getReply(npc));
 		en.step(player, "bye");
 		assertEquals("Have a good day and enjoy your stay!", getReply(npc));
 
@@ -235,7 +235,7 @@ public class SadScientistTest {
 
 		// -----------------------------------------------
 
-		PlayerTestHelper.equipWithItem(player, "goblet");
+		PlayerTestHelper.equipWithItem(player, "goblet", QUEST_SLOT);
 		player.setSharedKill("Sergej Elos");
 
 		en.step(player, "hi");
