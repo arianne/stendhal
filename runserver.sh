12065b5ccb184b8ac0c373b551b6dfbdede7ab3d
Hendrik Brummermann <nhnb@users.sourceforge.net>
2018-10-07 19:24:50 +0200

    updated version to 1.28

--- runserver.sh
+++ runserver.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-STENDHAL_VERSION="1.27.5"
+STENDHAL_VERSION="1.28"
 
 LOCALCLASSPATH=.:data/script/:data/conf/:stendhal-server-$STENDHAL_VERSION.jar:marauroa.jar:mysql-connector.jar:log4j.jar:commons-lang.jar:h2.jar
 
