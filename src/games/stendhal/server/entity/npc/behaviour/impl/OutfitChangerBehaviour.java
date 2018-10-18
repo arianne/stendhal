c4dabf92d8583a482745dd30d08104a8cf07dcc4
Jordan Irwin <antumdeluge@gmail.com>
2018-10-17 23:54:29 -0700

    Code cleanup

--- src/games/stendhal/server/entity/npc/behaviour/impl/OutfitChangerBehaviour.java
+++ src/games/stendhal/server/entity/npc/behaviour/impl/OutfitChangerBehaviour.java
@@ -46,18 +46,24 @@ public class OutfitChangerBehaviour extends MerchantBehaviour {
 		// FIXME: Use new outfit system
 		// swimsuits for men
 		outfitTypes.put("trunks", Arrays.asList(
-				new Outfit(null, null, null, 95, null), new Outfit(null, null, null, 96,
-						null), new Outfit(null, null, null, 97, null), new Outfit(
-								null, null, null, 98, null)));
+				new Outfit(null, null, null, 95, null),
+				new Outfit(null, null, null, 96, null),
+				new Outfit(null, null, null, 97, null),
+				new Outfit(null, null, null, 98, null)));
 
 		// swimsuits for women
-		outfitTypes.put("swimsuit", Arrays.asList(new Outfit(null, null, null, 91,
-				null), new Outfit(null, null, null, 92, null), new Outfit(null, null, null,
-				93, null), new Outfit(null, null, null, 94, null)));
+		outfitTypes.put("swimsuit", Arrays.asList(
+				new Outfit(null, null, null, 91, null),
+				new Outfit(null, null, null, 92, null),
+				new Outfit(null, null, null, 93, null),
+				new Outfit(null, null, null, 94, null)));
 
-		outfitTypes.put("mask", Arrays.asList(new Outfit(null, 0, 80, null, null),
-				new Outfit(null, 0, 81, null, null), new Outfit(null, 0, 82, null, null),
-				new Outfit(null, 0, 83, null, null), new Outfit(null, 0, 84, null, null),
+		outfitTypes.put("mask", Arrays.asList(
+				new Outfit(null, 0, 80, null, null),
+				new Outfit(null, 0, 81, null, null),
+				new Outfit(null, 0, 82, null, null),
+				new Outfit(null, 0, 83, null, null),
+				new Outfit(null, 0, 84, null, null),
 				new Outfit(null, 0, 85, null, null)));
 
 		// wedding dress for brides
