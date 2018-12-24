fd86436f8d89f9e2b1730b2d54dfc78811edd497
Cosmin Humeniuc <cosmin@hume.ro>
2018-12-24 15:29:27 +0200

    Implemented some tests in the empty TestCase for AreaIsFullCondition.

--- tests/games/stendhal/server/entity/npc/condition/AreaIsFullConditionTest.java
+++ tests/games/stendhal/server/entity/npc/condition/AreaIsFullConditionTest.java
@@ -11,10 +11,64 @@
  ***************************************************************************/
 package games.stendhal.server.entity.npc.condition;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import games.stendhal.common.parser.ConversationParser;
+import games.stendhal.common.parser.Sentence;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.Entity;
+import games.stendhal.server.entity.player.Player;
+import games.stendhal.server.util.Area;
+import utilities.PlayerTestHelper;
+import utilities.SpeakerNPCTestHelper;
+
 /**
- * Tests for games.stendhal.server.entity.npc.AreaIsFullCondition
- * TODO:
+ * Tests for {@link AreaIsFullCondition}.
  */
 public class AreaIsFullConditionTest {
 
+	@Test
+	public void testAreaFullWhenMaxIsZero() {
+		final StendhalRPZone zone = new StendhalRPZone("zone");
+		Area area = new Area(zone, 0, 0, 1, 1);
+		Player player = PlayerTestHelper.createPlayer("player");
+
+		AreaIsFullCondition condition = new AreaIsFullCondition(area, 0);
+
+		assertTrue(condition.fire(player, someSentence(), someNpc()));
+	}
+
+	@Test
+	public void testAreaNotFullWhenMaxIsOneAndPlayerOutside() {
+		final StendhalRPZone zone = new StendhalRPZone("zone");
+		Area area = new Area(zone, 0, 0, 1, 1);
+		Player player = PlayerTestHelper.createPlayer("player");
+
+		AreaIsFullCondition condition = new AreaIsFullCondition(area, 1);
+
+		assertFalse(condition.fire(player, someSentence(), someNpc()));
+	}
+
+	@Test
+	public void testAreaFullWhenMaxIsOneAndPlayerInside() {
+		final StendhalRPZone zone = new StendhalRPZone("zone");
+		Area area = new Area(zone, 0, 0, 1, 1);
+		Player player = PlayerTestHelper.createPlayer("player");
+		zone.add(player);
+
+		AreaIsFullCondition condition = new AreaIsFullCondition(area, 1);
+
+		assertTrue(condition.fire(player, someSentence(), someNpc()));
+	}
+
+	private Sentence someSentence() {
+		return ConversationParser.parse("testAndConditionText");
+	}
+
+	private Entity someNpc() {
+		return SpeakerNPCTestHelper.createSpeakerNPC();
+	}
 }
