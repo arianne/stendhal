ca65917eadb111b98a2ed6cc2f6e34702fcf2731
Hendrik Brummermann <nhnb@users.sourceforge.net>
2019-09-08 12:28:43 +0200

    proof of concept for a boat moving along denrian_river_e

---
+++ src/games/stendhal/server/maps/deniran/river/BoatNPC.java
@@ -0,0 +1,45 @@
+package games.stendhal.server.maps.deniran.river;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import games.stendhal.common.Direction;
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.core.pathfinder.FixedPath;
+import games.stendhal.server.core.pathfinder.Node;
+import games.stendhal.server.entity.npc.PassiveNPC;
+
+public class BoatNPC implements ZoneConfigurator  {
+
+
+	@Override
+	public void configureZone(StendhalRPZone zone, Map<String, String> attributes) {
+		buildNPCs(zone);
+	}
+
+	private void buildNPCs(StendhalRPZone zone) {
+
+		final PassiveNPC npc = new PassiveNPC() {
+			@Override
+			protected void createPath() {
+				final List<Node> nodes = new LinkedList<Node>();
+				nodes.add(new Node(-3, 70));
+				nodes.add(new Node(-3, 72));
+				nodes.add(new Node(129, 72));
+				nodes.add(new Node(129, 70));
+				setPath(new FixedPath(nodes, true));
+			}
+		};
+
+		npc.setIgnoresCollision(true);
+		npc.setResistance(0);
+		npc.setEntityClass("row_boat");
+		npc.setDescription("You see a boat.");
+		npc.setPosition(125, 68);
+		npc.setDirection(Direction.LEFT);
+		npc.setName("Boat");
+		zone.add(npc);
+	}
+}
