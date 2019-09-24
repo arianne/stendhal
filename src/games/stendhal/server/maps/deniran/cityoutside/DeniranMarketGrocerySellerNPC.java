71db6e4f84f44dab568541086562acc12d92765a
omero <nylon0700@users.sourceforge.net>
2019-09-24 03:54:08 +0200

    Ambrogio in Deniran farmer's market (selling grocery items)
    + all grocery sold by ambrogio: items available verified
    + ambrogio npc logic verified

--- src/games/stendhal/server/maps/deniran/cityoutside/DeniranMarketGrocerySellerNPC.java
+++ src/games/stendhal/server/maps/deniran/cityoutside/DeniranMarketGrocerySellerNPC.java
@@ -27,7 +27,7 @@ import games.stendhal.server.entity.npc.behaviour.impl.MonologueBehaviour;
 import games.stendhal.server.entity.npc.behaviour.impl.SellerBehaviour;
 
 /**
- * Provides a grocery seller in Deniran Marketplace
+ * Provides Ambrogio, a grocery seller in Deniran Marketplace
  *
  * @author omero
  *
@@ -39,7 +39,7 @@ public class DeniranMarketGrocerySellerNPC implements ZoneConfigurator {
 		final String[] yells = {
 			"HEYOH! Grocery stuff here at the market... Come closer, have a look!",
 			"HOYEH! I have all the stuff to prepare a decent meal and more!",
-			"AHAWW! Is this a market or a cemetery?!... It seems so quiet around here..."
+			"HAYAH! Is this a market or a cemetery?!... It seems so quiet around here..."
 		};
 		new MonologueBehaviour(buildNPC(zone), yells, 1);
 	}
@@ -53,7 +53,9 @@ public class DeniranMarketGrocerySellerNPC implements ZoneConfigurator {
 				addGreeting(
 						"Hello visitor! " +
 						"If you came looking for grocery stuff, I #offer grocery stuff... " +
-						"Oh, I should really set up one of those blackboards where offers are listed!");
+						"Oh, I should really set up one of those blackboards where offers are listed!"
+				);
+				
 				addOffer(
 						"I sell grocery stuff... " +
 								"eggs, " +
@@ -62,9 +64,10 @@ public class DeniranMarketGrocerySellerNPC implements ZoneConfigurator {
 								"habanero peppers, " +
 								"olive oil, " +
 								"vinegar, " +
-								"sugar... " +
+								"sugar... " +						
 						"If you want to #buy some stuff, tell me... " +
-						"Oh, I should really set up one of those blackboards where offers are listed!");
+						"Oh, I should really set up one of those blackboards where offers are listed!"
+				);
 				//Offered items:
 				final Map<String, Integer> offerings = new HashMap<String, Integer>();
                 offerings.put("egg", 35);
@@ -85,8 +88,9 @@ public class DeniranMarketGrocerySellerNPC implements ZoneConfigurator {
 						"When you want to #buy something, tell me... " +
 						"Oh, I should really set up one of those blackboards where offers are listed");
 
-				addGoodbye("So long...");
-
+				addGoodbye(
+						"So long... " +
+						"Oh, I should really set up one of those blackboards where offers are listed");
 			}
 
 			@Override
@@ -102,14 +106,12 @@ public class DeniranMarketGrocerySellerNPC implements ZoneConfigurator {
 			}
 		};
 
-		// Finalize Deniran Market Seller NPC (Grocery)
-		//npc.setEntityClass("fatsellernpc");
+		// Finalize Ambrogio, Deniran Market Seller NPC (Grocery)
 		npc.setEntityClass("deniran_marketsellernpc1grocery");
 		npc.setPosition(26, 122);
 		npc.setCollisionAction(CollisionAction.REVERSE);
 		npc.setDescription("You see Ambrogio, a busy marketplace seller...");
 		zone.add(npc);
-
 		return npc;
 
 	}
