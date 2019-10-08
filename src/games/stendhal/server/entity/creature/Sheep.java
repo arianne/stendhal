b7f09ca92d6d636e93baba223ebdbcfb3264c3ed
Jordan Irwin <antumdeluge@gmail.com>
2019-10-07 19:23:40 -0700

    New sheep sounds (CC0) by cognito perceptu...
    Source: http://www.freesound.org/people/cognito%20perceptu/sounds/88423/

--- src/games/stendhal/server/entity/creature/Sheep.java
+++ src/games/stendhal/server/entity/creature/Sheep.java
@@ -47,8 +47,8 @@ public class Sheep extends DomesticAnimal {
 
 	/** the logger instance. */
 	private static final Logger logger = Logger.getLogger(Sheep.class);
-	private static final List<String> largeSheepSounds = Arrays.asList("sheep-01");
-	private static final List<String> smallSheepSounds = Arrays.asList("sheep-01");
+	private static final List<String> largeSheepSounds = Arrays.asList("sheep-01", "sheep-02", "sheep-03", "sheep-04");
+	private static final List<String> smallSheepSounds = Arrays.asList("sheep-01", "sheep-02", "sheep-03", "sheep-04");
 
 	/**
 	 * The amount of hunger that indicates hungry.
