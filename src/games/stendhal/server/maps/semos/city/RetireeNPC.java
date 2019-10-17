c042c172c3a0dfc2aac15c859f70d140c90da28a
Jordan Irwin <antumdeluge@gmail.com>
2019-10-16 19:20:07 -0700

    Created 2 new old man laugh sounds (CC0) for Diogenes

--- src/games/stendhal/server/maps/semos/city/RetireeNPC.java
+++ src/games/stendhal/server/maps/semos/city/RetireeNPC.java
@@ -12,6 +12,7 @@
  ***************************************************************************/
 package games.stendhal.server.maps.semos.city;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -107,6 +108,7 @@ public class RetireeNPC implements ZoneConfigurator {
 		npc.setCollisionAction(CollisionAction.STOP);
 		npc.setEntityClass("beggarnpc");
 		npc.setDescription("Diogenes is an elderly man, but sprightly for his age. He looks friendly and helpful.");
+		npc.setSounds(Arrays.asList("laugh-old-man-01", "laugh-old-man-02"));
 		zone.add(npc);
 	}
 }
