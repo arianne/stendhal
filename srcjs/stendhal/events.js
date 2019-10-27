088fe517839a8264d865fdb4d2d5e2a0220170ba
Hendrik Brummermann <nhnb@users.sourceforge.net>
2019-10-27 21:54:25 +0100

    moved logic for sound location from event to sound (#254)

--- srcjs/stendhal/events.js
+++ srcjs/stendhal/events.js
@@ -162,28 +162,9 @@ marauroa.rpeventFactory["sound_event"] = marauroa.util.fromProto(marauroa.rpeven
 		if (this.hasOwnProperty("volume")) {
 			volume *= parseInt(this["volume"], 10) / 100;
 		}
-		// Further adjustments if the sound has a radius
-		if (this.hasOwnProperty("radius")) {
-			if (!marauroa.me || !rpobject || !rpobject["_x"]) {
-				// Can't calculate the distance yet. Ignore the sound.
-				return;
-			}
+		var radius = parseInt(this["radius"], 10);
 
-			var radius = parseInt(this["radius"], 10);
-			var xdist = marauroa.me["_x"] - rpobject["_x"];
-			var ydist = marauroa.me["_y"] - rpobject["_y"];
-			var dist2 = xdist * xdist + ydist * ydist;
-			if (dist2 > radius * radius) {
-				// Outside the specified radius
-				return;
-			}
-			// The sound api does not guarantee anything about how the volume
-			// works, so it does not matter much how we scale it.
-			volume *= Math.min(radius * radius / (dist2 * 20), 1);
-		}
-		if (stendhal.ui.sound) {
-			stendhal.ui.sound.playEffect(this["sound"], volume);
-		}
+		stendhal.ui.sound.playLocalizedEffect(rpobject["_x"], rpobject["_y"], radius, this["layer"], this["sound"], volume);
 	}
 });
 
