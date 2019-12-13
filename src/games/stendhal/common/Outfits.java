3dd6714476b2bb7e9d974ae4c0c5f7141acbbc29
Jordan Irwin <antumdeluge@gmail.com>
2019-12-13 00:36:29 -0800

    New eyes outfit (CC0)

--- src/games/stendhal/common/Outfits.java
+++ src/games/stendhal/common/Outfits.java
@@ -40,7 +40,7 @@ public class Outfits {
 	public static final int BODY_OUTFITS = 3;
 
 	/** number of player selectable eyes */
-	public static final int EYES_OUTFITS = 25;
+	public static final int EYES_OUTFITS = 26;
 
 	/** number of player selectable mouths */
 	public static final int MOUTH_OUTFITS = 5;
