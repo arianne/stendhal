504fb6a54932bb4d52003d8bd09afef802e8ebbc
Jordan Irwin <antumdeluge@gmail.com>
2020-01-04 23:25:20 -0800

    PlayerNextToCondition: add option to check if player is next to a...
    ...third party entity

--- src/games/stendhal/server/entity/npc/condition/PlayerNextToCondition.java
+++ src/games/stendhal/server/entity/npc/condition/PlayerNextToCondition.java
@@ -12,18 +12,43 @@
 package games.stendhal.server.entity.npc.condition;
 
 import games.stendhal.common.parser.Sentence;
+import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.core.engine.StendhalRPRuleProcessor;
 import games.stendhal.server.entity.Entity;
 import games.stendhal.server.entity.npc.ChatCondition;
 import games.stendhal.server.entity.player.Player;
 
 /**
- * Checks if the player is next to NPC.
+ * Checks if the player is next to an entity. If an entity name is not supplied
+ * it will check if the player is next to the NPC checking the condition.
  */
 public class PlayerNextToCondition implements ChatCondition {
 
+	final String entityName;
+
+	public PlayerNextToCondition() {
+		entityName = null;
+	}
+
+	public PlayerNextToCondition(final String name) {
+		entityName = name;
+	}
+
 	@Override
 	public boolean fire(final Player player, final Sentence sentence, final Entity npc) {
-		return player.nextTo(npc);
+		if (entityName == null) {
+			return player.nextTo(npc);
+		}
+
+		Entity entity = SingletonRepository.getNPCList().get(entityName);
+		if (entity == null) {
+			entity = StendhalRPRuleProcessor.get().getPlayer(entityName);
+			if (entity == null) {
+				return false;
+			}
+		}
+
+		return player.nextTo(entity);
 	}
 
 	@Override
