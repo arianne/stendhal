3a4e5c3d86db2ff0676b92e74db8ff79323f7989
Jordan Irwin <antumdeluge@gmail.com>
2020-01-05 15:10:05 -0800

    D J Smith buys ugmash...
    Related issue: https://github.com/arianne/stendhal/issues/307

--- src/games/stendhal/server/maps/deniran/cityinterior/weaponsshop/WeaponDealerNPC.java
+++ src/games/stendhal/server/maps/deniran/cityinterior/weaponsshop/WeaponDealerNPC.java
@@ -11,6 +11,7 @@
  ***************************************************************************/
 package games.stendhal.server.maps.deniran.cityinterior.weaponsshop;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -19,7 +20,10 @@ import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.pathfinder.FixedPath;
 import games.stendhal.server.core.pathfinder.Node;
+import games.stendhal.server.entity.npc.ShopList;
 import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.behaviour.adder.BuyerAdder;
+import games.stendhal.server.entity.npc.behaviour.impl.BuyerBehaviour;
 
 public class WeaponDealerNPC implements ZoneConfigurator  {
 
@@ -35,7 +39,7 @@ public class WeaponDealerNPC implements ZoneConfigurator  {
 			public void createDialog() {
 				addGreeting("Hello, and welcome to the deniran weapon shop.");
 				addJob("I am the local weapons dealer.");
-				addOffer("I need nothing at this time, but come back later I'm sure I'll need something.");
+				//addOffer("I need nothing at this time, but come back later I'm sure I'll need something.");
 				addGoodbye();
 			}
 
@@ -47,6 +51,18 @@ public class WeaponDealerNPC implements ZoneConfigurator  {
 				setPath(new FixedPath(nodes, true));
 			}
 		};
+
+		final Map<String, Integer> pricesBuy = new HashMap<String, Integer>() {{
+			put("ugmash", 1200);
+		}};
+
+		new BuyerAdder().addBuyer(npc, new BuyerBehaviour(pricesBuy));
+
+		// add shop to the general shop list
+		final ShopList shops = ShopList.get();
+		for (final String key: pricesBuy.keySet()) {
+			shops.add("deniranequipbuy", key, pricesBuy.get(key));
+		}
 
 		npc.setPosition(11, 5);
 		npc.setEntityClass("wellroundedguynpc");
