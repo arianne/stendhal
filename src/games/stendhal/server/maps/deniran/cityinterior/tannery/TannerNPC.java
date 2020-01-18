f020b84dfd9dea67a4e40145bff523f8b783f7e9
Jordan Irwin <antumdeluge@gmail.com>
2020-01-17 17:07:07 -0800

    Tanner NPC will make money pouch for players who have looted at least...
    ...1,000,000 money.

--- src/games/stendhal/server/maps/deniran/cityinterior/tannery/TannerNPC.java
+++ src/games/stendhal/server/maps/deniran/cityinterior/tannery/TannerNPC.java
@@ -11,25 +11,72 @@
  ***************************************************************************/
 package games.stendhal.server.maps.deniran.cityinterior.tannery;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import games.stendhal.common.Direction;
 import games.stendhal.common.constants.SkinColor;
+import games.stendhal.common.grammar.Grammar;
 import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.rp.DaylightPhase;
 import games.stendhal.server.entity.RPEntity;
+import games.stendhal.server.entity.npc.ChatCondition;
 import games.stendhal.server.entity.npc.ConversationPhrases;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.action.DropItemAction;
+import games.stendhal.server.entity.npc.action.EnableFeatureAction;
+import games.stendhal.server.entity.npc.action.MultipleActions;
+import games.stendhal.server.entity.npc.action.SayTimeRemainingAction;
+import games.stendhal.server.entity.npc.action.SetQuestAction;
+import games.stendhal.server.entity.npc.action.SetQuestToTimeStampAction;
+import games.stendhal.server.entity.npc.condition.AndCondition;
 import games.stendhal.server.entity.npc.condition.DaylightCondition;
 import games.stendhal.server.entity.npc.condition.NotCondition;
+import games.stendhal.server.entity.npc.condition.PlayerHasItemWithHimCondition;
+import games.stendhal.server.entity.npc.condition.PlayerLootedNumberOfItemsCondition;
+import games.stendhal.server.entity.npc.condition.QuestActiveCondition;
+import games.stendhal.server.entity.npc.condition.QuestCompletedCondition;
+import games.stendhal.server.entity.npc.condition.QuestInStateCondition;
+import games.stendhal.server.entity.npc.condition.QuestNotActiveCondition;
+import games.stendhal.server.entity.npc.condition.QuestNotCompletedCondition;
+import games.stendhal.server.entity.npc.condition.QuestNotInStateCondition;
+import games.stendhal.server.entity.npc.condition.QuestNotStartedCondition;
+import games.stendhal.server.entity.npc.condition.TimePassedCondition;
 
 public class TannerNPC implements ZoneConfigurator {
 
+	private static TannerNPC instance;
+
+	private static SpeakerNPC tanner;
+
+	private final static String FEATURE_SLOT = "pouch";
+	private final static String QUEST_SLOT = "money_" + FEATURE_SLOT;
+
+	// players must have looted at least 1,000,000 money to get the money pouch
+	private static final int requiredMoneyLoot = 1000000;
+	private static final int serviceFee = 50000;
+	// XXX: enable when testing done & update sayWaitTime method
+	//private static final int TAN_TIME = MathHelper.MINUTES_IN_ONE_DAY;
+	// XXX: disable when testing done
+	private static final int TAN_TIME = 2; // 2 minute for testing
+	// required items to make pouch
+	//private static final String requiredItems = "pelt=1,money=" + Integer.toString(serviceFee);
+	private static final Map<String, Integer> requiredItems = new HashMap<String, Integer>() {{
+		put("pelt", 1);
+		put("money", serviceFee);
+	}};
+
+
 	@Override
-	public void configureZone(StendhalRPZone zone, Map<String, String> attributes) {
-		final SpeakerNPC tanner = new SpeakerNPC("Skinner Rawhide") {
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		prepareNPC(zone);
+		prepareDialogue();
+	}
+
+	private void prepareNPC(final StendhalRPZone zone) {
+		tanner = new SpeakerNPC("Skinner Rawhide") {
 
 			/**
 			 * Force NPC to face north after speaking with players.
@@ -43,20 +90,7 @@ public class TannerNPC implements ZoneConfigurator {
 			}
 		};
 
-		// dialogue
-		tanner.add(ConversationStates.IDLE,
-				ConversationPhrases.GREETING_MESSAGES,
-				new NotCondition(new DaylightCondition(DaylightPhase.NIGHT)),
-				ConversationStates.IDLE,
-				"Welcome to Deniran's tannery.",
-				null);
-
-		tanner.add(ConversationStates.IDLE,
-				ConversationPhrases.GREETING_MESSAGES,
-				new DaylightCondition(DaylightPhase.NIGHT),
-				ConversationStates.IDLE,
-				"It's late. I need to get to bed.",
-				null);
+		tanner.addGoodbye();
 
 		tanner.setPosition(10, 8);
 		tanner.setDirection(Direction.UP);
@@ -66,4 +100,212 @@ public class TannerNPC implements ZoneConfigurator {
 		zone.add(tanner);
 	}
 
+	private void prepareDialogue() {
+		// conditions to check if it is night or day time
+		final ChatCondition nightCondition = new DaylightCondition(DaylightPhase.NIGHT);
+		final ChatCondition dayCondition = new NotCondition(nightCondition);
+		// required items to begin tanning
+		final ChatCondition hasItemsCondition = new AndCondition(
+				new PlayerHasItemWithHimCondition("pelt"),
+				new PlayerHasItemWithHimCondition("money", serviceFee));
+		// player can start quest if they have looted 1,000,000 money & they have not already started or finished the quest
+		final ChatCondition canStartQuestCondition = new AndCondition(
+				new QuestNotStartedCondition(QUEST_SLOT),
+				new QuestNotCompletedCondition(QUEST_SLOT),
+				new PlayerLootedNumberOfItemsCondition(requiredMoneyLoot, "money"));
+		// condition to check if tanner is making the money pouch
+		final ChatCondition isTanningCondition = new AndCondition(
+				new QuestActiveCondition(QUEST_SLOT),
+				new QuestNotInStateCondition(QUEST_SLOT, "start"));
+
+
+		// cannot talk to tanner at night
+		tanner.add(ConversationStates.IDLE,
+				ConversationPhrases.GREETING_MESSAGES,
+				nightCondition,
+				ConversationStates.IDLE,
+				"It's late. I need to get to bed.",
+				null);
+
+		// player has not met requirement
+		tanner.add(ConversationStates.IDLE,
+				ConversationPhrases.GREETING_MESSAGES,
+				new AndCondition(
+						dayCondition,
+						new QuestNotActiveCondition(QUEST_SLOT),
+						new QuestNotCompletedCondition(QUEST_SLOT),
+						new NotCondition(new PlayerLootedNumberOfItemsCondition(requiredMoneyLoot, "money"))),
+				ConversationStates.IDLE,
+				"Welcome to Deniran's tannery.",
+				null);
+
+		// player has met requirement & can get money pouch
+		tanner.add(ConversationStates.IDLE,
+				ConversationPhrases.GREETING_MESSAGES,
+				new AndCondition(
+						dayCondition,
+						canStartQuestCondition),
+				ConversationStates.QUESTION_1,
+				"I see you are experienced at looting money. I can make a pouch for you to carry your money in."
+				+ " But I will need some items. Are you interested?",
+				null);
+
+		// player wants money pouch
+		tanner.add(ConversationStates.QUESTION_1,
+				ConversationPhrases.YES_MESSAGES,
+				null,
+				ConversationStates.IDLE,
+				"Okay. I will need a pelt and my fee is " + Integer.toString(serviceFee) + " money."
+				+ " Please come back when you have that.",
+				new SetQuestAction(QUEST_SLOT, "start"));
+
+		// player does not want money pouch
+		tanner.add(ConversationStates.QUESTION_1,
+				ConversationPhrases.NO_MESSAGES,
+				null,
+				ConversationStates.IDLE,
+				"Oh? I think it would be encouraged.",
+				null);
+
+		// player returns without items
+		tanner.add(ConversationStates.IDLE,
+				ConversationPhrases.GREETING_MESSAGES,
+				new AndCondition(
+						dayCondition,
+						new QuestInStateCondition(QUEST_SLOT, "start"),
+						new NotCondition(hasItemsCondition)),
+				ConversationStates.IDLE,
+				//"Bring me a pelt and " + Integer.toString(serviceFee) + " money and I will make a pouch to carry your money in.",
+				sayRequiredItems("Bring me [items] and I will make a pouch to carry your money in."),
+				null);
+
+		// player returns with items
+		tanner.add(ConversationStates.IDLE,
+				ConversationPhrases.GREETING_MESSAGES,
+				new AndCondition(
+						dayCondition,
+						new QuestInStateCondition(QUEST_SLOT, "start"),
+						hasItemsCondition),
+				ConversationStates.IDLE,
+				// XXX: update after testing
+				//"Okay, I will begin making your money pouch. Please come back in " + Grammar.quantityplnoun(TAN_TIME, "hour") + ".",
+				"Okay, I will begin making your money pouch. Please come back in " + Grammar.quantityplnoun(TAN_TIME, "minute") + ".",
+				new MultipleActions(
+						new DropItemAction("pelt"),
+						new DropItemAction("money", serviceFee),
+						new SetQuestToTimeStampAction(QUEST_SLOT)));
+
+		// player returns before money pouch is finished
+		tanner.add(ConversationStates.IDLE,
+				ConversationPhrases.GREETING_MESSAGES,
+				new AndCondition(
+						dayCondition,
+						isTanningCondition,
+						new NotCondition(new TimePassedCondition(QUEST_SLOT, TAN_TIME))),
+				ConversationStates.IDLE,
+				null,
+				new SayTimeRemainingAction(QUEST_SLOT, TAN_TIME, "I'm sorry, your money pouch is not ready yet. Please come back in "));
+
+		// money pouch is finished
+		tanner.add(ConversationStates.IDLE,
+				ConversationPhrases.GREETING_MESSAGES,
+				new AndCondition(
+						dayCondition,
+						isTanningCondition,
+						new TimePassedCondition(QUEST_SLOT, TAN_TIME)),
+				ConversationStates.IDLE,
+				"You came back just in time. Your money pouch is ready. Try it out. I know you will like it.",
+				new MultipleActions(
+						new EnableFeatureAction("pouch"),
+						new SetQuestAction(QUEST_SLOT, "done")));
+
+		// player speaks to tanner after completing quest
+		tanner.add(ConversationStates.IDLE,
+				ConversationPhrases.GREETING_MESSAGES,
+				new AndCondition(
+						dayCondition,
+						new QuestCompletedCondition(QUEST_SLOT)),
+				ConversationStates.IDLE,
+				"I knew you would enjoy the pouch.",
+				null);
+	}
+
+	public String sayRequiredItems(final String msg) {
+		final StringBuilder sb = new StringBuilder();
+
+		int idx = 0;
+		for (final String key: requiredItems.keySet()) {
+			if (idx == requiredItems.size() - 1) {
+				sb.append("and ");
+			}
+
+			sb.append(Integer.toString(requiredItems.get(key)) + " " + key);
+
+			if (idx < requiredItems.size() - 1) {
+				sb.append(", ");
+			}
+
+			idx++;
+		}
+
+		return msg.replace("[items]", sb.toString());
+	}
+
+	/*
+	private String requiredItemsToString() {
+		final StringBuilder sb = new StringBuilder();
+
+		int idx = 0;
+		for (final String key: requiredItems.keySet()) {
+			sb.append(key + "=" + requiredItems.get(key));
+
+			if (idx < requiredItems.size() - 1) {
+				sb.append(",");
+			}
+
+			idx++;
+		}
+
+		return sb.toString();
+	}
+	*/
+
+
+	// some helper methods for tests
+
+	public static TannerNPC getInstance() {
+		if (instance == null) {
+			instance = new TannerNPC();
+		}
+
+		return instance;
+	}
+
+	public SpeakerNPC getNPC() {
+		return tanner;
+	}
+
+	public String getQuestSlot() {
+		return QUEST_SLOT;
+	}
+
+	public String getFeatureSlot() {
+		return FEATURE_SLOT;
+	}
+
+	public Integer getRequiredMoneyLoot() {
+		return requiredMoneyLoot;
+	}
+
+	public Integer getServiceFee() {
+		return serviceFee;
+	}
+
+	public Map<String, Integer> getRequiredItems() {
+		return requiredItems;
+	}
+
+	public Integer getWaitTime() {
+		return TAN_TIME;
+	}
 }
---
+++ tests/games/stendhal/server/maps/deniran/cityinterior/tannery/TannerNPCTest.java
@@ -0,0 +1,307 @@
+/***************************************************************************
+ *                     Copyright © 2020 - Arianne                          *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.maps.deniran.cityinterior.tannery;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static utilities.SpeakerNPCTestHelper.getReply;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import games.stendhal.common.grammar.Grammar;
+import games.stendhal.server.core.rp.DaylightPhase;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.fsm.Engine;
+import utilities.PlayerTestHelper;
+import utilities.QuestHelper;
+import utilities.ZonePlayerAndNPCTestImpl;
+
+public class TannerNPCTest extends ZonePlayerAndNPCTestImpl {
+
+	private static final String ZONE_NAME = "testzone";
+	private static final String npcName = "Skinner Rawhide";
+
+	private static TannerNPC configurator;
+	private static SpeakerNPC tanner;
+	private static String QUEST_SLOT;
+	private static String FEATURE_SLOT;
+	private static int requiredMoneyLoot;
+	private static int serviceFee;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		QuestHelper.setUpBeforeClass();
+		setupZone(ZONE_NAME);
+	}
+
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		setZoneForPlayer(ZONE_NAME);
+		setNpcNames(npcName);
+
+		configurator = TannerNPC.getInstance();
+		addZoneConfigurator(configurator, ZONE_NAME);
+
+		super.setUp();
+
+		tanner = configurator.getNPC();
+		QUEST_SLOT = configurator.getQuestSlot();
+		FEATURE_SLOT = configurator.getFeatureSlot();
+		requiredMoneyLoot = configurator.getRequiredMoneyLoot();
+		serviceFee = configurator.getServiceFee();
+	}
+
+	private String getCurrentTimestamp() {
+		return Long.toString(System.currentTimeMillis());
+	}
+
+	private int getLootCount() {
+		return player.getNumberOfLootsForItem("money");
+	}
+
+	// for setting player's number of looted money
+	private void setLootCount(final int count) {
+		final int diff = count - player.getNumberOfLootsForItem("money");
+		player.incLootForItem("money", diff);
+
+		assertEquals(count, getLootCount());
+	}
+
+	// for resetting player money loot count to 0
+	private void resetLoots() {
+		setLootCount(0);
+	}
+
+	@Test
+	public void initTest() {
+		testEntities();
+		testNightTime();
+		testDayTime();
+
+		DaylightPhase.unsetTestingPhase();
+	}
+
+	private void testEntities() {
+		assertNotNull(tanner);
+		assertEquals(npcName, tanner.getName());
+		assertNotNull(player);
+		assertNotNull(player.getID());
+		assertNull(player.getQuest(QUEST_SLOT));
+		assertNull(player.getFeature(FEATURE_SLOT));
+	}
+
+	private void testNightTime() {
+		// player can not do quest at night
+
+		final Engine en = tanner.getEngine();
+
+		DaylightPhase.setTestingPhase(DaylightPhase.NIGHT);
+		assertEquals(DaylightPhase.NIGHT, DaylightPhase.current());
+
+		final String nightReply = "It's late. I need to get to bed.";
+
+		// XXX: is there an assert method for less/greater than comparison?
+		assertTrue(player.getNumberOfLootsForItem("money") < requiredMoneyLoot);
+
+		en.step(player, "hi");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertEquals(nightReply, getReply(tanner));
+
+		player.setQuest(QUEST_SLOT, "start");
+
+		en.step(player, "hi");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertEquals(nightReply, getReply(tanner));
+
+		player.setQuest(QUEST_SLOT, getCurrentTimestamp());
+
+		en.step(player, "hi");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertEquals(nightReply, getReply(tanner));
+
+		setLootCount(requiredMoneyLoot);
+		assertTrue(player.getNumberOfLootsForItem("money") >= requiredMoneyLoot);
+
+		player.setQuest(QUEST_SLOT, null);
+
+		en.step(player, "hi");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertEquals(nightReply, getReply(tanner));
+
+		player.setQuest(QUEST_SLOT, "start");
+
+		en.step(player, "hi");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertEquals(nightReply, getReply(tanner));
+
+		player.setQuest(QUEST_SLOT, getCurrentTimestamp());
+
+		en.step(player, "hi");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertEquals(nightReply, getReply(tanner));
+
+		player.setQuest(QUEST_SLOT, "done");
+
+		en.step(player, "hi");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertEquals(nightReply, getReply(tanner));
+	}
+
+	private void testDayTime() {
+		final Engine en = tanner.getEngine();
+
+		DaylightPhase.setTestingPhase(DaylightPhase.DAY);
+		assertEquals(DaylightPhase.DAY, DaylightPhase.current());
+
+		// reset player
+		player.setQuest(QUEST_SLOT, null);
+		resetLoots();
+
+		assertNull(player.getQuest(QUEST_SLOT));
+		assertTrue(player.getNumberOfLootsForItem("money") < serviceFee);
+		assertFalse(player.isEquipped("pelt"));
+		assertFalse(player.isEquipped("money", serviceFee));
+
+		en.step(player, "hi");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertEquals("Welcome to Deniran's tannery.", getReply(tanner));
+
+		setLootCount(requiredMoneyLoot - 1);
+
+		en.step(player, "hi");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertEquals("Welcome to Deniran's tannery.", getReply(tanner));
+
+		setLootCount(requiredMoneyLoot);
+		assertEquals(requiredMoneyLoot, getLootCount());
+
+		en.step(player, "hi");
+		assertEquals(ConversationStates.QUESTION_1, en.getCurrentState());
+		assertEquals(
+				"I see you are experienced at looting money. I can make a pouch for you to carry your money in."
+				+ " But I will need some items. Are you interested?",
+				getReply(tanner));
+
+		en.step(player, "bye");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+
+		en.step(player, "hi");
+		assertEquals(ConversationStates.QUESTION_1, en.getCurrentState());
+		assertEquals(
+				"I see you are experienced at looting money. I can make a pouch for you to carry your money in."
+				+ " But I will need some items. Are you interested?",
+				getReply(tanner));
+
+		en.step(player, "no");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertEquals("Oh? I think it would be encouraged.", getReply(tanner));
+
+		en.step(player, "hi");
+		assertEquals(ConversationStates.QUESTION_1, en.getCurrentState());
+
+		en.step(player, "yes");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertEquals(
+				"Okay. I will need a pelt and my fee is " + Integer.toString(configurator.getServiceFee()) + " money."
+				+ " Please come back when you have that.",
+				getReply(tanner));
+		assertEquals("start", player.getQuest(QUEST_SLOT));
+
+		final String noItemsReply = configurator.sayRequiredItems("Bring me [items] and I will make a pouch to carry your money in.");
+
+		// player has none of the required items
+		en.step(player, "hi");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertEquals(noItemsReply, getReply(tanner));
+
+		// player has pelt only
+		PlayerTestHelper.equipWithItem(player, "pelt");
+
+		assertTrue(player.isEquipped("pelt"));
+		assertFalse(player.isEquipped("money", serviceFee));
+
+		en.step(player, "hi");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertEquals(noItemsReply, getReply(tanner));
+
+		// player has money only
+		player.drop("pelt");
+		PlayerTestHelper.equipWithMoney(player, serviceFee);
+
+		assertFalse(player.isEquipped("pelt"));
+		assertTrue(player.isEquipped("money", serviceFee));
+
+		en.step(player, "hi");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertEquals(noItemsReply, getReply(tanner));
+
+		// player has pelt & money but not enough
+		PlayerTestHelper.equipWithItem(player, "pelt");
+		player.drop("money");
+
+		assertTrue(player.isEquipped("pelt"));
+		assertEquals(serviceFee - 1, player.getNumberOfEquipped("money"));
+
+		en.step(player, "hi");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertEquals(noItemsReply, getReply(tanner));
+
+		// player has pelt & enough money
+		PlayerTestHelper.equipWithItem(player, "money");
+
+		assertEquals(serviceFee, player.getNumberOfEquipped("money"));
+
+		en.step(player, "hi");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		// XXX: update after testing
+		/*
+		assertEquals(
+				"Okay, I will begin making your money pouch. Please come back in " + Grammar.quantityplnoun(configurator.getWaitTime(), "minute") + ".",
+				getReply(tanner));
+		*/
+		assertEquals(
+				"Okay, I will begin making your money pouch. Please come back in " + Grammar.quantityplnoun(configurator.getWaitTime(), "minute") + ".",
+				getReply(tanner));
+		assertFalse(player.getQuest(QUEST_SLOT).equals("start"));
+		assertFalse(player.isEquipped("pouch"));
+		assertFalse(player.isEquipped("money"));
+
+		// player returns before pouch is ready
+		en.step(player, "hi");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertTrue(getReply(tanner).startsWith("I'm sorry, your money pouch is not ready yet. Please come back in "));
+
+		// player returns after pouch is ready
+		player.setQuest(QUEST_SLOT, "0");
+		en.step(player, "hi");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertEquals(
+				"You came back just in time. Your money pouch is ready. Try it out. I know you will like it.",
+				getReply(tanner));
+		assertEquals("done", player.getQuest(QUEST_SLOT));
+		assertNotNull(player.getFeature(FEATURE_SLOT));
+
+		// player talks to tanner after receiving pouch
+		en.step(player, "hi");
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		assertEquals("I knew you would enjoy the pouch.", getReply(tanner));
+
+		PlayerTestHelper.resetNPC(tanner);
+	}
+}
