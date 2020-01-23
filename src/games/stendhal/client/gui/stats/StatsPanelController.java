2c11c135abf13c78c6bdb1093e0e2472fb55196e
Jordan Irwin <antumdeluge@gmail.com>
2020-01-23 13:46:05 -0800

    Client: add "pouch" to slots to listen for money changes...
    GitHub issue: https://github.com/arianne/stendhal/issues/325

--- src/games/stendhal/client/gui/stats/StatsPanelController.java
+++ src/games/stendhal/client/gui/stats/StatsPanelController.java
@@ -32,7 +32,7 @@ import marauroa.common.game.RPSlot;
  * be show.
  */
 public final class StatsPanelController {
-	private static final String[] MONEY_SLOTS = { "bag", "lhand", "rhand" };
+	private static final String[] MONEY_SLOTS = { "pouch", "bag", "lhand", "rhand" };
 	/**
 	 * A string used as a white space at the status labels. This is a
 	 * combination of carriage return and no-break space, so that a possible
