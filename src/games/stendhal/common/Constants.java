ad4a82022c1a231e0a3c5dd8010cdb188d55529a
Jordan Irwin <antumdeluge@gmail.com>
2020-01-23 10:37:11 -0800

    Add "pouch" to "carrying" slots...
    GitHub issue: https://github.com/arianne/stendhal/issues/325

--- src/games/stendhal/common/Constants.java
+++ src/games/stendhal/common/Constants.java
@@ -24,8 +24,10 @@ public final class Constants {
 	 * order (i.e. bag first).
 	 */
 	// TODO: let the slots decide that themselves
-	public static final String[] CARRYING_SLOTS = { "bag", "head", "rhand",
-			"lhand", "armor", "finger", "cloak", "legs", "feet", "keyring", "portfolio", "back", "belt" };
+	public static final String[] CARRYING_SLOTS = {
+			"bag", "pouch", "head", "rhand", "lhand", "armor", "finger", "cloak",
+			"legs", "feet", "keyring", "portfolio", "back", "belt"
+	};
 
 	/**
 	 * Modes that can be used for setting combat karma.
