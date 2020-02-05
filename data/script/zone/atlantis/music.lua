440469eea5ce09ae05d15394dd3e9c5eba0f2c67
Jordan Irwin <antumdeluge@gmail.com>
2020-02-04 19:37:21 -0800

    New music for Atlantis (CC BY 4.0) by Tad Miller...
    Source: https://opengameart.org/node/109342

---
+++ data/script/zone/atlantis/music.lua
@@ -0,0 +1,41 @@
+--[[
+ ***************************************************************************
+ *                       Copyright © 2020 - Arianne                        *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************
+]]
+
+
+-- city
+
+local zones = {
+	"deniran_atlantis_n2_w",
+	"deniran_atlantis_nw",
+	"deniran_atlantis_w",
+	"deniran_atlantis_sw",
+	"deniran_atlantis_n2",
+	"deniran_atlantis_n",
+	"deniran_atlantis",
+	"deniran_atlantis_s",
+	"deniran_atlantis_n_e2",
+	"deniran_atlantis_ne",
+	"deniran_atlantis_e",
+	"deniran_atlantis_se",
+}
+
+for _, z in pairs(zones) do
+	local zone = "-7_" .. z
+
+	if game:setZone(zone) then
+		setZoneMusic("settlement_of_the_frontier", 85)
+	else
+		logger:warn("Could not set zone " .. zone .. "to create BackGroundMusicSource")
+	end
+end
