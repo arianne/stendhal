37efc9e47dbca4cb1bd633debed107717362aaac
Jordan Irwin <antumdeluge@gmail.com>
2020-02-05 21:13:28 -0800

    Add license boilerplate to buit-in init.lua Lua script

--- src/games/stendhal/server/core/scripting/lua/init.lua
+++ src/games/stendhal/server/core/scripting/lua/init.lua
@@ -1,3 +1,16 @@
+--[[
+ ***************************************************************************
+ *                       Copyright © 2020 - Arianne                        *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************
+]]
 
 -- master script for defining global values visible to scripts & mods
 
