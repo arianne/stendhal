5c359dfb8e09722cb040699a0d414bdf11226423
Jordan Irwin <antumdeluge@gmail.com>
2020-02-10 08:42:31 -0800

    New quest: Unicorn Horns for Zelan (idea by bleutailfly)...
    GitHub issue: https://github.com/arianne/stendhal/issues/335

--- data/script/region/atlantis/city/exterior/ZelanNPC.lua
+++ data/script/region/atlantis/city/exterior/ZelanNPC.lua
@@ -36,6 +36,27 @@ if game:setZone(zoneName) then
 
 	-- add Zelan to the world
 	game:add(zelan)
+
+
+	-- quest
+	local quest = simpleQuest:create("unicorn_horn_for_Zelan", "Unicorn Horns for Zelan", "Zelan")
+	quest:setDescription("Zelan needs help gathering unicorn horns.")
+	quest:setRequestReply("Hello! I'm in need of some unicorn horns to make some daggers."
+		.. " It is really dangerous in the woods surrounding Atlantis. If you are a brave sort"
+		.. " I could really use some help gathering unicorn horns. Will you help me?")
+	quest:setAcceptReply("Great! Be careful out there lots of large monsters,"
+		.. " and those centaurs are really nasty")
+	quest:setRewardReply("Thank a bunch!")
+	quest:setRejectReply("Thats ok, I will find someone else to help me.")
+	quest:setItemToCollect("unicorn horn", 10)
+	quest:setRepeatable(false)
+	quest:setXPReward(50000)
+	quest:setKarmaReward(5.0)
+	quest:addItemReward("soup", 3)
+	quest:addItemReward("money", 20000)
+	quest:setRegion(Region.ATLANTIS)
+
+	quest:register()
 else
 	logger:error("Could not set zone: " .. zoneName)
 end
