c46165176b56b770e09c31ec3a1c3ae949adefc1
Jordan Irwin <antumdeluge@gmail.com>
2020-02-17 16:25:23 -0800

    Correct punctuation in daily item quest related achievement titles

--- src/games/stendhal/server/core/rp/achievement/factory/AdosItemQuestAchievementsFactory.java
+++ src/games/stendhal/server/core/rp/achievement/factory/AdosItemQuestAchievementsFactory.java
@@ -24,15 +24,15 @@ public class AdosItemQuestAchievementsFactory extends AbstractAchievementFactory
 	public Collection<Achievement> createAchievements() {
 		List<Achievement> achievements = new LinkedList<Achievement>();
 		//daily item quest achievements
-		achievements.add(createAchievement("quest.special.daily_item.0010", "Ados' Supporter", "Finish daily item quest 10 times",
+		achievements.add(createAchievement("quest.special.daily_item.0010", "Ados's Supporter", "Finish daily item quest 10 times",
 												Achievement.EASY_BASE_SCORE, true, new QuestStateGreaterThanCondition("daily_item", 2, 9)));
-		achievements.add(createAchievement("quest.special.daily_item.0050", "Ados' Provider", "Finish daily item quest 50 times",
+		achievements.add(createAchievement("quest.special.daily_item.0050", "Ados's Provider", "Finish daily item quest 50 times",
 												Achievement.EASY_BASE_SCORE, true, new QuestStateGreaterThanCondition("daily_item", 2, 49)));
-		achievements.add(createAchievement("quest.special.daily_item.0100", "Ados' Supplier", "Finish daily item quest 100 times",
+		achievements.add(createAchievement("quest.special.daily_item.0100", "Ados's Supplier", "Finish daily item quest 100 times",
 												Achievement.MEDIUM_BASE_SCORE, true, new QuestStateGreaterThanCondition("daily_item", 2, 99)));
-		achievements.add(createAchievement("quest.special.daily_item.0250", "Ados' Stockpiler", "Finish daily item quest 250 times",
+		achievements.add(createAchievement("quest.special.daily_item.0250", "Ados's Stockpiler", "Finish daily item quest 250 times",
 												Achievement.MEDIUM_BASE_SCORE, true, new QuestStateGreaterThanCondition("daily_item", 2, 249)));
-		achievements.add(createAchievement("quest.special.daily_item.0500", "Ados' Hoarder", "Finish daily item quest 500 times",
+		achievements.add(createAchievement("quest.special.daily_item.0500", "Ados's Hoarder", "Finish daily item quest 500 times",
 												Achievement.HARD_BASE_SCORE, true, new QuestStateGreaterThanCondition("daily_item", 2, 499)));
 		return achievements;
 	}
