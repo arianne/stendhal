61b34d67e51127a6148c612a55721f7ac5659307
Jordan Irwin <antumdeluge@gmail.com>
2020-02-19 13:56:26 -0800

    Use AchievementNotifier to update pending achievements from all...
    ...categories on login

--- src/games/stendhal/server/entity/player/UpdatePendingAchievementsOnLogin.java
+++ src/games/stendhal/server/entity/player/UpdatePendingAchievementsOnLogin.java
@@ -11,18 +11,13 @@
  ***************************************************************************/
 package games.stendhal.server.entity.player;
 
-import static games.stendhal.server.core.rp.achievement.factory.QuestAchievementFactory.ID_ELF_PRINCESS;
-
-import java.util.Map;
-
-import games.stendhal.common.MathHelper;
 import games.stendhal.server.core.engine.dbcommand.DeletePendingAchievementDetailsCommand;
 import games.stendhal.server.core.engine.dbcommand.ReadPendingAchievementDetailsCommand;
 import games.stendhal.server.core.events.LoginListener;
 import games.stendhal.server.core.events.TurnListener;
 import games.stendhal.server.core.events.TurnListenerDecorator;
 import games.stendhal.server.core.events.TurnNotifier;
-import games.stendhal.server.core.rp.achievement.factory.ItemAchievementFactory;
+import games.stendhal.server.core.rp.achievement.AchievementNotifier;
 import marauroa.server.db.command.DBCommand;
 import marauroa.server.db.command.DBCommandQueue;
 import marauroa.server.db.command.ResultHandle;
@@ -55,68 +50,18 @@ public class UpdatePendingAchievementsOnLogin implements LoginListener, TurnList
 		}
 		Player player = command.getPlayer();
 
-		updateElfPrincessAchievement(player, command.getDetails(ID_ELF_PRINCESS));
-		for (final String ID: ItemAchievementFactory.getIDs()) {
-			updateItemLoots(player, command.getDetails(ID));
-		}
-
-		// Could also check for reached achievements here. This is also checked on login but the order may vary due to the async access?
+		final AchievementNotifier notifier = AchievementNotifier.get();
+		notifier.onLevelChange(player);
+		notifier.onKill(player);
+		notifier.onFinishQuest(player);
+		notifier.onZoneEnter(player);
+		notifier.onAge(player);
+		notifier.onItemLoot(player);
+		notifier.onProduction(player);
+		notifier.onObtain(player);
 
 		// delete the entries. We don't need feedback
 		DBCommand deletecommand = new DeletePendingAchievementDetailsCommand(player);
 		DBCommandQueue.get().enqueue(deletecommand);
-
 	}
-
-	private static void updateElfPrincessAchievement(final Player player, final Map<String, Integer> details) {
-
-		// nothing to update
-		if (details == null) {
-			return;
-		}
-
-		final String QUEST_SLOT = "elf_princess";
-
-		// if player didn't start this quest yet, do nothing (shouldn't be details in this case but check anyway)
-		if(!player.hasQuest(QUEST_SLOT)) {
-			return;
-		}
-
-		// param (key) should be "" for this one, all we need to know is the count
-		int missingcount = details.get("");
-
-		if (missingcount > 0) {
-			final String[] parts = player.getQuest(QUEST_SLOT).split(";");
-
-			// is quest slot already time stamped?
-			if(parts.length>2) {
-				int newcount = MathHelper.parseInt(parts[2])+missingcount;
-				player.setQuest(QUEST_SLOT, 2, "" + newcount);
-				return;
-			} else if (parts.length==2) {
-				// the count was not stored, so we just store the count we had in the table
-				player.setQuest(QUEST_SLOT, 2, "" + missingcount);
-			} else {
-				// we didn't store a time before. so we just choose the value '1' for a long ago system time
-				player.setQuest(QUEST_SLOT, 1, "1");
-				// the count was also not stored, so we just store the count we had in the table
-				player.setQuest(QUEST_SLOT, 2, "" + missingcount);
-			}
-		}
-	}
-
-	private static void updateItemLoots(final Player player, final Map<String, Integer> details) {
-
-		// nothing to update
-		if (details == null) {
-			return;
-		}
-
-		// update player loots which have been stored as param (key) = itemname, count (value) = number of loots
-		for (Map.Entry<String, Integer> detail : details.entrySet())
-		{
-			player.incLootForItem(detail.getKey(), detail.getValue());
-		}
-	}
-
 }
