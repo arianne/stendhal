28648625d68f8f088c443640f96f246ab4d062c8
Jordan Irwin <antumdeluge@gmail.com>
2020-02-25 18:33:43 -0800

    Add grocer NPC to Deniran Grocery Store...
    Sells olive oil & vinegar.

---
+++ data/script/region/deniran/city/interior/grocery_store/GrocerNPC.lua
@@ -0,0 +1,67 @@
+--[[
+ ***************************************************************************
+ *                       Copyright © 2020 - Arianne                        *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************
+]]
+
+
+local grocer = nil
+
+local function addNPC()
+	grocer = npcHelper:createSpeakerNPC("Jimbo")
+	grocer:setOutfit("dress=5,mouth=1,eyes=0,mask=1")
+	grocer:setOutfitColor("skin", SkinColor.DARK)
+	grocer:setOutfitColor("dress", 0x8b4513) -- saddle brown
+	grocer:setOutfitColor("eyes", 0x228b22) -- forest green
+	grocer:setOutfitColor("mask", 0xffffff) -- white (doesn't work with glasses because they are fully black)
+	grocer:setPosition(25, 24)
+	grocer:setIdleDirection(Direction.UP)
+
+	grocer:addGreeting()
+	grocer:addGoodbye()
+	grocer:addJob("I run the Deniran grocery store.")
+	grocer:addOffer("Check the blackboard for the items I sell and their prices.")
+	grocer:addHelp(grocer:getReply("offer"))
+	grocer:addQuest("Sorry, there is nothing I need help with at this time.")
+
+	local sellPrices = {
+		{"olive oil", 135},
+		{"vinegar", 135},
+	}
+	npcHelper:addSeller(grocer, sellPrices, false)
+
+	-- add to shop list
+	for _, item in pairs(sellPrices) do
+		shops:add("denirangrocerysell", item[1], item[2])
+	end
+
+	game:add(grocer)
+end
+
+local function addSign()
+	if grocer ~= nil then
+		local sellSign = game:createShopSign("denirangrocerysell", grocer:getName() .. "'s Shop (selling)", "You can buy these things.")
+		sellSign:setEntityClass("blackboard")
+		sellSign:setPosition(23, 23)
+
+		game:add(sellSign)
+	end
+end
+
+
+local zone = "int_deniran_grocery_store"
+
+if game:setZone(zone) then
+	addNPC()
+	addSign()
+else
+	logger:error("Could not set zone: " .. zoneName)
+end
