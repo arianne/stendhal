6a811ca44749e46c66fb4b18e556344a40b97310
Jordan Irwin <antumdeluge@gmail.com>
2020-03-05 05:47:37 -0800

    Akutagawa will repair worn training swords for half the price of...
    ...buying a new one.

--- src/games/stendhal/server/maps/nalwor/forest/DojoSellerNPC.java
+++ src/games/stendhal/server/maps/nalwor/forest/DojoSellerNPC.java
@@ -11,22 +11,34 @@
  ***************************************************************************/
 package games.stendhal.server.maps.nalwor.forest;
 
+import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import games.stendhal.common.Direction;
+import games.stendhal.common.parser.Sentence;
 import games.stendhal.server.core.config.ZoneConfigurator;
 import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.Entity;
 import games.stendhal.server.entity.RPEntity;
+import games.stendhal.server.entity.item.BreakableItem;
+import games.stendhal.server.entity.item.Item;
 import games.stendhal.server.entity.mapstuff.sign.ShopSign;
 import games.stendhal.server.entity.npc.ChatAction;
 import games.stendhal.server.entity.npc.ChatCondition;
+import games.stendhal.server.entity.npc.ConversationPhrases;
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.EventRaiser;
 import games.stendhal.server.entity.npc.ShopList;
 import games.stendhal.server.entity.npc.SpeakerNPC;
 import games.stendhal.server.entity.npc.action.SayTextAction;
 import games.stendhal.server.entity.npc.behaviour.adder.SellerAdder;
 import games.stendhal.server.entity.npc.behaviour.impl.SellerBehaviour;
+import games.stendhal.server.entity.npc.condition.AndCondition;
+import games.stendhal.server.entity.npc.condition.NotCondition;
 import games.stendhal.server.entity.npc.condition.PlayerHasItemWithHimCondition;
+import games.stendhal.server.entity.player.Player;
 
 
 /**
@@ -39,12 +51,15 @@ public class DojoSellerNPC implements ZoneConfigurator {
 	private final String sellerName = "Akutagawa";
 	private SpeakerNPC seller;
 
+	private static final int swordPrice = 5600;
+
 	@Override
 	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
 		dojoZone = zone;
 
 		initNPC();
 		initShop();
+		initRepairShop();
 		initDialogue();
 	}
 
@@ -59,7 +74,7 @@ public class DojoSellerNPC implements ZoneConfigurator {
 
 	private void initShop() {
 		final Map<String, Integer> pricesSell = new LinkedHashMap<String, Integer>() {{
-			put("training sword", 5600);
+			put("training sword", swordPrice);
 			put("shuriken", 80);
 			put("fire shuriken", 105);
 		}};
@@ -106,14 +121,152 @@ public class DojoSellerNPC implements ZoneConfigurator {
 		dojoZone.add(shopSign);
 	}
 
+	/**
+	 * If players bring their worn training swords they can get them repaired for half the
+	 * price of buying a new one.
+	 */
+	private void initRepairShop() {
+		final List<String> repairPhrases = Arrays.asList("repair", "fix");
+
+		final ChatCondition needsRepairCondition = new ChatCondition() {
+			@Override
+			public boolean fire(final Player player, final Sentence sentence, final Entity npc) {
+				return getUsedSwordsCount(player) > 0;
+			}
+		};
+
+		final ChatCondition canAffordRepairsCondition = new ChatCondition() {
+			@Override
+			public boolean fire(final Player player, final Sentence sentence, final Entity npc) {
+				return player.isEquipped("money", getRepairPrice(getUsedSwordsCount(player)));
+			}
+		};
+
+		final ChatAction sayRepairPriceAction = new ChatAction() {
+			@Override
+			public void fire(final Player player, final Sentence sentence, final EventRaiser npc) {
+				final int usedSwords = getUsedSwordsCount(player);
+				final boolean multiple = usedSwords > 1;
+
+				final StringBuilder sb = new StringBuilder("You have " + Integer.toString(usedSwords) + " used training sword");
+				if (multiple) {
+					sb.append("s");
+				}
+				sb.append(". I can repair ");
+				if (multiple) {
+					sb.append("them all");
+				} else {
+					sb.append("it");
+				}
+				sb.append(" for " + Integer.toString(getRepairPrice(usedSwords)) + " money. Would you like me to do so?");
+
+				npc.say(sb.toString());
+			}
+		};
+
+		final ChatAction repairAction = new ChatAction() {
+			@Override
+			public void fire(final Player player, final Sentence sentence, final EventRaiser npc) {
+				final int swordsCount = getUsedSwordsCount(player);
+				player.drop("money", getRepairPrice(swordsCount));
+
+				for (final Item sword: player.getAllEquipped("training sword")) {
+					final BreakableItem breakable = (BreakableItem) sword;
+					if (breakable.isUsed()) {
+						breakable.repair();
+					}
+				}
+
+				if (swordsCount > 1) {
+					npc.say("Done! Your training swords are as good as new.");
+				} else {
+					npc.say("Done! Your training sword is as good as new.");
+				}
+			}
+		};
+
+
+		seller.add(ConversationStates.ATTENDING,
+				repairPhrases,
+				new NotCondition(new PlayerHasItemWithHimCondition("assassins id")),
+				ConversationStates.ATTENDING,
+				"Only members of the assassins guild can have their #'training swords' repaired.",
+				null);
+
+		seller.add(ConversationStates.ATTENDING,
+				repairPhrases,
+				new AndCondition(
+						new PlayerHasItemWithHimCondition("assassins id"),
+						new NotCondition(needsRepairCondition)),
+				ConversationStates.ATTENDING,
+				"You don't have any #'training swords' that need repaired.",
+				null);
+
+		seller.add(ConversationStates.ATTENDING,
+				repairPhrases,
+				new AndCondition(
+						new PlayerHasItemWithHimCondition("assassins id"),
+						needsRepairCondition),
+				ConversationStates.QUESTION_1,
+				null,
+				sayRepairPriceAction);
+
+		seller.add(ConversationStates.QUESTION_1,
+				ConversationPhrases.NO_MESSAGES,
+				null,
+				ConversationStates.ATTENDING,
+				"Okay. Let me know if you need anything else.",
+				null);
+
+		seller.add(ConversationStates.QUESTION_1,
+				ConversationPhrases.YES_MESSAGES,
+				new NotCondition(needsRepairCondition),
+				ConversationStates.ATTENDING,
+				"Did you drop your sword?",
+				null);
+
+		seller.add(ConversationStates.QUESTION_1,
+				ConversationPhrases.YES_MESSAGES,
+				new AndCondition(
+						needsRepairCondition,
+						new NotCondition(canAffordRepairsCondition)),
+				ConversationStates.ATTENDING,
+				"I'm sorry, you don't have enough money.",
+				null);
+
+		seller.add(ConversationStates.QUESTION_1,
+				ConversationPhrases.YES_MESSAGES,
+				new AndCondition(
+						needsRepairCondition,
+						canAffordRepairsCondition),
+				ConversationStates.ATTENDING,
+				null,
+				repairAction);
+	}
+
 	private void initDialogue() {
 		seller.addGreeting("If you're looking for training equipment, you have come to the right place.");
 		seller.addGoodbye();
-		seller.addOffer("See my blackboard for what I sell.");
-		seller.addJob("I run the assassins' dojo shop.");
-		seller.addQuest("I don't have any task for you to do. I only sell equipment.");
+		seller.addOffer("See my blackboard for what I sell. I can also #repair any used #'training swords' that you have.");
+		seller.addJob("I run the assassins' dojo shop where we sell equipment and do #repairs on #'training swords'.");
+		seller.addQuest("I don't have any task for you to do. I only #fix and sell equipment.");
 		seller.addHelp("If you want to train in the dojo, I recommend that you buy a #'training sword'.");
 		seller.addReply("training sword", "My training swords are light and easy to swing. And just because"
 				+ " they are made out of wood, doesn't mean that it won't hurt if you get whacked with one.");
+	}
+
+	private int getUsedSwordsCount(final Player player) {
+		int count = 0;
+		for (final Item sword: player.getAllEquipped("training sword")) {
+			if (((BreakableItem) sword).isUsed()) {
+				count++;
+			}
+		}
+
+		return count;
+	}
+
+	private int getRepairPrice(final int count) {
+		return count * (swordPrice / 2);
 	}
 }
