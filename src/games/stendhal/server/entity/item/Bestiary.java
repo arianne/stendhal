066e6327b7818f08307127b8a64da06f8e97950b
Jordan Irwin <antumdeluge@gmail.com>
2020-03-18 20:36:30 -0700

    Prevent non-owners from using Bestiary if infostring set

--- src/games/stendhal/server/entity/item/Bestiary.java
+++ src/games/stendhal/server/entity/item/Bestiary.java
@@ -13,6 +13,7 @@ package games.stendhal.server.entity.item;
 
 import java.util.Map;
 
+import games.stendhal.common.NotificationType;
 import games.stendhal.server.entity.RPEntity;
 import games.stendhal.server.entity.player.Player;
 import games.stendhal.server.events.BestiaryEvent;
@@ -36,6 +37,14 @@ public class Bestiary extends Item {
 	public boolean onUsed(final RPEntity user) {
 		if (user instanceof Player) {
 			final Player player = (Player) user;
+			final String infostring = getInfoString();
+
+			// only allow owner to use
+			if (infostring != null && !player.getName().equals(infostring)) {
+				player.sendPrivateText(NotificationType.RESPONSE, "You read: This bestiary is the property of " + infostring + ". Please return it to it's rightful owner.");
+				return false;
+			}
+
 			player.addEvent(new BestiaryEvent(player));
 			player.notifyWorldAboutChanges();
 
