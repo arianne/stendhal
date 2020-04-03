ac2dee2d9895bf6a85bd625cfe818551670c0fc2
Jordan Irwin <antumdeluge@gmail.com>
2020-04-02 22:25:55 -0700

    Add proper descriptions to non-interactive Atlantis citizens

--- data/script/region/atlantis/city/exterior/citizens.lua
+++ data/script/region/atlantis/city/exterior/citizens.lua
@@ -18,38 +18,62 @@
 local zoneName = "-7_deniran_atlantis"
 
 if game:setZone(zoneName) then
-	-- define paths & sprites
-	local detail1 = {
-		path = {
-			{39, 24}, {44, 24}, {44, 22}, {47, 22}, {47, 21}, {54, 21}, {54, 19},
-			{74, 19}, {74, 23}, {81, 23},
+	local genericDesc = "You see a citizen of Atlantis."
+
+	-- define silent NPCs
+	local details = {
+		{
+			path = {
+				{39, 24}, {44, 24}, {44, 22}, {47, 22}, {47, 21}, {54, 21}, {54, 19},
+				{74, 19}, {74, 23}, {81, 23},
+			},
+			class = "atlantisfemale01npc",
+			desc = genericDesc,
 		},
-		class = "atlantisfemale01npc",
-	}
-	local detail2 = {
-		path = {
-			{36, 105}, {40, 105}, {40, 108}, {81, 108}, {81, 107}, {85, 107},
-			{85, 106}, {89, 106}, {89, 101}, {92, 101}, {92, 99},
+		{
+			path = {
+				{36, 105}, {40, 105}, {40, 108}, {81, 108}, {81, 107}, {85, 107},
+				{85, 106}, {89, 106}, {89, 101}, {92, 101}, {92, 99},
+			},
+			class = "atlantismale02npc",
+			desc = genericDesc,
 		},
-		class = "atlantismale02npc",
 	}
 
-	local npcList = {}
+	for _, detail in pairs(details) do
+		local npc = npcHelper:createSilentNPC()
 
-	for _, details in pairs({detail1, detail2}) do
-		-- make entities walk the path backwards when reaching end
-		local nodeCount = #details.path
-		for idx = nodeCount - 1, 2, -1 do
-			details.path[#details.path + 1] = details.path[idx]
+		if detail.path ~= nil then
+			npcHelper:setPathAndPosition(npc, detail.path, true)
+			npc:retracePath() -- make entities walk the path backwards when reaching end
+		else
+			npc:setPosition(detail.pos.x, detail.pos.y)
+			npc:setDirection(detail.dir)
+		end
+		if detail.name ~= nil then
+			npc:setName(detail.name)
+		end
+		if detail.resistance ~= nil then
+			npc:setResistance(detail.resistance)
+		end
+		if detail.speed ~= nil then
+			npc:setBaseSpeed(detail.speed)
+		end
+		if detail.ignorecollision then
+			npc:setIgnoresCollision(true)
+		end
+		if detail.flags ~= nil then
+			for _, flag in ipairs(detail.flags) do
+				npc:put(flag, "")
+			end
+		end
+		if detail.sounds ~= nil then
+			npc:setSounds(detail.sounds)
 		end
 
-		local npc = npcHelper:createSilentNPC()
-		npcHelper:setPathAndPosition(npc, details.path, true)
-		npc:setEntityClass(details.class)
-		table.insert(npcList, npc)
-	end
+		npc:setEntityClass(detail.class)
+		npc:setDescription(detail.desc)
 
-	for _, npc in pairs(npcList) do
 		game:add(npc)
 	end
 else
