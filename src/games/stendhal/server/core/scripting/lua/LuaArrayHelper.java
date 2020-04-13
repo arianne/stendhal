6de790acb7caa3722bd3af69bd16f1a7e7856ae1
Jordan Irwin <antumdeluge@gmail.com>
2020-04-12 22:30:38 -0700

    Lua: add methods to "arrays" object to convert Lua table to Java...
    ...array or list.

--- src/games/stendhal/server/core/scripting/lua/LuaArrayHelper.java
+++ src/games/stendhal/server/core/scripting/lua/LuaArrayHelper.java
@@ -12,16 +12,22 @@
 package games.stendhal.server.core.scripting.lua;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.luaj.vm2.LuaTable;
+import org.luaj.vm2.LuaValue;
 import org.luaj.vm2.lib.jse.CoerceJavaToLua;
+
+import games.stendhal.server.core.scripting.ScriptInLua.LuaLogger;
 
 
 /**
  * Handles some conversion of Java arrays to Lua tables.
  */
 public class LuaArrayHelper {
+
+	private static LuaLogger logger = LuaLogger.get();
 
 	private static LuaArrayHelper instance;
 
@@ -69,5 +75,57 @@ public class LuaArrayHelper {
 		}
 
 		return table;
+	}
+
+	/**
+	 * Converts a Lua table to Java list.
+	 *
+	 * @param table
+	 * 		Table with contents to be transferred to new list.
+	 * @return
+	 * 		New <code>List<Object></code> instance.
+	 */
+	public List<Object> toList(final LuaTable table) {
+		final List<Object> objectList = new LinkedList<>();
+
+		for (final LuaValue key: table.keys()) {
+			final LuaValue lv = table.get(key);
+
+			if (lv.isnil()) {
+				objectList.add(null);
+			} else if (lv.isnumber()) {
+				if (lv.isint()) {
+					objectList.add(lv.toint());
+				} else if (lv.islong()) {
+					objectList.add(lv.tolong());
+				} else {
+					objectList.add(lv.todouble());  // all other number types to double
+				}
+			} else if (lv.isboolean()) {
+				objectList.add(lv.toboolean());
+			} else if (lv.istable()) {
+				objectList.add(toList(lv.checktable()));
+			} else if (lv.isuserdata()) {
+				objectList.add(lv.touserdata());
+			} else if (lv.isstring()) {
+				objectList.add(lv.tojstring());
+			} else {
+				logger.warn("Data type not added: " + lv.typename());
+			}
+		}
+
+		return objectList;
+	}
+
+	/**
+	 * Converts a Lua table to Java array.
+	 *
+	 * @param table
+	 * 		Table with contents to be transferred to new array.
+	 * @return
+	 * 		New <code>Object[]</code> instance.
+	 */
+	public Object[] toArray(final LuaTable table) {
+		return toList(table).toArray();
 	}
 }
