7044f5ca2e0f7a5fe4fd2694c724218eeeef98c1
Jordan Irwin <antumdeluge@gmail.com>
2020-04-18 22:56:22 -0700

    Fix missing sprite for bee in Atlantis

--- data/script/region/atlantis/city/exterior/citizens.lua
+++ data/script/region/atlantis/city/exterior/citizens.lua
@@ -52,7 +52,8 @@ if game:setZone(zoneName) then
 		{
 			pos = {x=61, y=79},
 			name = "bee",
-			class = "../monsters/insect/killer_bee",
+			--class = "../monsters/insect/killer_bee", -- this only works in Eclipse
+			class = "animal/bee",
 			dir = Direction.LEFT,
 			resistance = 0,
 			desc = "You see a bee gathering nectar from the flowers.",
