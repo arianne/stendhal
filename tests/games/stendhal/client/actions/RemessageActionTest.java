d15a28a6d2620336938b7368455fa9a488fccbeb
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2021-03-10 06:11:41 +0700

    Add test: Remessage

---
+++ tests/games/stendhal/client/actions/RemessageActionTest.java
@@ -0,0 +1,73 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.client.actions;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import games.stendhal.client.MockStendhalClient;
+import games.stendhal.client.StendhalClient;
+import marauroa.common.game.RPAction;
+
+public class RemessageActionTest {
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		SlashActionRepository.register();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		StendhalClient.resetClient();
+	}
+
+	/**
+	 * Tests for execute.
+	 */
+	@Test
+	public void testExecute() {
+		
+		new MockStendhalClient() {
+			@Override
+			public void send(final RPAction action) {
+				assertEquals("tell", action.get("type"));
+			}
+		};
+		
+		final RemessageAction action = new RemessageAction();
+		assertFalse(action.execute(null, "remainder"));
+	}
+
+	/**
+	 * Tests for getMaximumParameters.
+	 */
+	@Test
+	public void testGetMaximumParameters() {
+		final RemessageAction action = new RemessageAction();
+		assertThat(action.getMaximumParameters(), is(0));
+	}
+
+	/**
+	 * Tests for getMinimumParameters.
+	 */
+	@Test
+	public void testGetMinimumParameters() {
+		final RemessageAction action = new RemessageAction();
+		assertThat(action.getMinimumParameters(), is(0));
+	}
+}
