199298c87fcb12354b1382f3d681ad870a34f021
Rakha Djokosoetono <rakha.djokosoetono@student.manchester.ac.uk>
2021-03-10 06:10:21 +0700

    Add test: Jail

---
+++ tests/games/stendhal/client/actions/JailActionTest.java
@@ -0,0 +1,74 @@
+/* $Id$ */
+/***************************************************************************
+ *                   (C) Copyright 2003-2010 - Stendhal                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.client.actions;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import games.stendhal.client.MockStendhalClient;
+import games.stendhal.client.StendhalClient;
+import marauroa.common.game.RPAction;
+
+public class JailActionTest {
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		StendhalClient.resetClient();
+	}
+
+	/**
+	 * Tests for execute.
+	 */
+	@Test
+	public void testExecute() {
+		new MockStendhalClient() {
+			@Override
+			public void send(final RPAction action) {
+				assertEquals("jail", action.get("type"));
+			}
+		};
+
+		final JailAction action = new JailAction();
+		String[] params = {"target", "minutes"};
+		assertFalse(action.execute(params, ""));
+		assertTrue(action.execute(params, "remainder"));
+	}
+
+	/**
+	 * Tests for getMaximumParameters.
+	 */
+	@Test
+	public void testGetMaximumParameters() {
+		final JailAction action = new JailAction();
+		assertThat(action.getMaximumParameters(), is(2));
+	}
+
+	/**
+	 * Tests for getMinimumParameters.
+	 */
+	@Test
+	public void testGetMinimumParameters() {
+		final JailAction action = new JailAction();
+		assertThat(action.getMinimumParameters(), is(2));
+	}
+}
