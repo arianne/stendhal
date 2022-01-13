1fee359b67c4bd19b47332670798bacf6e12fc1b
Hendrik Brummermann <nhnb@users.sourceforge.net>
2022-01-13 22:31:34 +0100

    ported ItemContainerWindow to typescript

---
+++ srcjs/stendhal/ui/component/ItemContainerComponent.ts
@@ -0,0 +1,195 @@
+/***************************************************************************
+ *                (C) Copyright 2003-2022 - Faiumoni e. V.                 *
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Affero General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
+import { ui } from "../UI";
+import { Component } from "../toolkit/Component";
+
+import { ActionContextMenu } from "../dialog/ActionContextMenu";
+import { DropQuantitySelectorDialog } from "../dialog/DropQuantitySelectorDialog";
+
+declare var marauroa: any;
+declare var stendhal: any;
+
+/**
+ * a container for items like a bag or corpse
+ */
+export class ItemContainerComponent extends Component {
+	private timestampMouseDown = 0;
+
+	/**
+	 * slot name, slot size, object (a corpse or chest) or null for marauroa.me,
+	 * which changes on zone change.
+	 */
+	constructor(private slot: string, private size: number, private object: any, private suffix: string, private quickPickup: boolean, private defaultImage: string) {
+		super("empty-template");
+		for (let i = 0; i < size; i++) {
+			let e = document.getElementById(slot + suffix + i)!;
+			e.setAttribute("draggable", "true");
+			e.addEventListener("dragstart", (event: DragEvent) => {
+				this.onDragStart(event)
+			});
+			e.addEventListener("dragover", (event: DragEvent) => {
+				this.onDragOver(event)
+			});
+			e.addEventListener("drop", (event: DragEvent) => {
+				this.onDrop(event)
+			});
+			e.addEventListener("mousedown", (event: MouseEvent) => {
+				this.onMouseDown(event)
+			});
+			e.addEventListener("mouseup", (event: MouseEvent) => {
+				this.onMouseUp(event)
+			});
+			e.addEventListener("touchstart", (event: TouchEvent) => {
+				this.onMouseDown(event)
+			});
+			e.addEventListener("touchend", (event: TouchEvent) => {
+				this.onMouseUp(event)
+			});
+			e.addEventListener("contextmenu", (event: MouseEvent) => {
+				this.onContextMenu(event)
+			});
+		}
+	}
+
+	public update() {
+		this.render();
+	}
+
+	public render() {
+		let myobject = this.object || marauroa.me;
+		let cnt = 0;
+		if (myobject[this.slot]) {
+			for (let i = 0; i < myobject[this.slot].count(); i++) {
+				let o = myobject[this.slot].getByIndex(i);
+				let e = document.getElementById(this.slot + this.suffix + cnt)!;
+				e.style.backgroundImage = "url(/data/sprites/items/" + o["class"] + "/" + o["subclass"] + ".png " + ")";
+				e.textContent = o.formatQuantity();
+				(e as any).dataItem = o;
+				cnt++;
+			}
+		}
+
+		for (let i = cnt; i < this.size; i++) {
+			let e = document.getElementById(this.slot +this. suffix + i)!;
+			if (this.defaultImage) {
+				e.style.backgroundImage = "url(/data/gui/" + this.defaultImage + ")";
+			} else {
+				e.style.backgroundImage = "none";
+			}
+			e.textContent = "";
+			(e as any).dataItem = undefined;
+		}
+	}
+
+	private onDragStart(event: DragEvent) {
+		let myobject = this.object || marauroa.me;
+		if (!myobject[this.slot]) {
+			event.preventDefault();
+			return;
+		}
+
+		let slotNumber = (event.target as HTMLElement).id.slice(this.slot.length + this.suffix.length);
+		let item = myobject[this.slot].getByIndex(slotNumber);
+		if (item) {
+			let img = stendhal.data.sprites.getAreaOf(stendhal.data.sprites.get(item.sprite.filename), 32, 32);
+			event.dataTransfer!.setDragImage(img, 0, 0);
+			event.dataTransfer!.setData("Text", JSON.stringify({
+				path: item.getIdPath(),
+				zone: marauroa.currentZoneName
+			}));
+		} else {
+			event.preventDefault();
+		}
+	}
+
+	private onDragOver(event: DragEvent) {
+		event.preventDefault();
+		event.dataTransfer!.dropEffect = "move";
+		return false;
+	}
+
+	private onDrop(event: DragEvent) {
+		let myobject = this.object || marauroa.me;
+		let datastr = event.dataTransfer?.getData("Text") || event.dataTransfer?.getData("text/x-stendhal");
+		if (datastr) {
+			let data = JSON.parse(datastr);
+			let targetPath = "[" + myobject["id"] + "\t" + this.slot + "]";
+			let action = {
+				"type": "equip",
+				"source_path": data.path,
+				"target_path": targetPath,
+				"zone" : data.zone
+			};
+			// if ctrl is pressed, we ask for the quantity
+			if (event.ctrlKey) {
+				ui.createSingletonFloatingWindow("Quantity", 
+					new DropQuantitySelectorDialog(action),
+					event.pageX - 50, event.pageY - 25);
+			} else {
+				marauroa.clientFramework.sendAction(action);
+			}
+		}
+		event.stopPropagation();
+		event.preventDefault();
+	}
+
+	private onContextMenu(event: MouseEvent) {
+		event.preventDefault();
+	}
+
+	isRightClick(event: MouseEvent) {
+		if (+new Date() - this.timestampMouseDown > 300) {
+			return true;
+		}
+		if (event.which) {
+			return (event.which === 3);
+		} else {
+			return (event.button === 2);
+		}
+	}
+
+	onMouseDown(event: MouseEvent|TouchEvent) {
+		this.timestampMouseDown = +new Date();
+	}
+
+	onMouseUp(evt: MouseEvent|TouchEvent) {
+		evt.preventDefault();
+		let event = stendhal.ui.html.extractPosition(evt);
+		if ((event.target as any).dataItem) {
+			if (this.quickPickup) {
+				marauroa.clientFramework.sendAction({
+					type: "equip",
+					"source_path": (event.target as any).dataItem.getIdPath(),
+					"target_path": "[" + marauroa.me["id"] + "\tbag]",
+					"clicked": "", // useful for changing default target in equip action
+					"zone": marauroa.currentZoneName
+				});
+				return;
+			}
+
+			if (this.isRightClick(event)) {
+				ui.createSingletonFloatingWindow("Action",
+					new ActionContextMenu((event.target as any).dataItem),
+					event.pageX - 50, event.pageY - 5);
+			} else {
+				marauroa.clientFramework.sendAction({
+					type: "use",
+					"target_path": (event.target as any).dataItem.getIdPath(),
+					"zone": marauroa.currentZoneName
+				});
+			}
+		}
+		document.getElementById("gamewindow")!.focus();
+	}
+
+}
+
--- srcjs/stendhal/ui/itemcontainer.js
+++ srcjs/stendhal/ui/itemcontainer.js
@@ -11,158 +11,12 @@
 
 "use strict";
 
+var ItemContainerComponent = require("../../../build/ts/ui/component/ItemContainerComponent").ItemContainerComponent;
+
+
 var marauroa = window.marauroa = window.marauroa || {};
 var stendhal = window.stendhal = window.stendhal || {};
 stendhal.ui = stendhal.ui || {};
-
-
-/**
- * slot name, slot size, object (a corpse or chest) or null for marauroa.me,
- * which changes on zone change.
- *
- * @constructor
- */
-stendhal.ui.ItemContainerWindow = function(slot, size, object, suffix, quickPickup, defaultImage) {
-	this.update = function() {
-		render();
-	};
-
-	function render() {
-		var myobject = object || marauroa.me;
-		var cnt = 0;
-		if (myobject[slot]) {
-			for (var i = 0; i < myobject[slot].count(); i++) {
-				var o = myobject[slot].getByIndex(i);
-				var e = document.getElementById(slot + suffix + cnt);
-				e.style.backgroundImage = "url(/data/sprites/items/" + o["class"] + "/" + o["subclass"] + ".png " + ")";
-				e.textContent = o.formatQuantity();
-				e.dataItem = o;
-				cnt++;
-			}
-		}
-		for (var i = cnt; i < size; i++) {
-			var e = document.getElementById(slot + suffix + i);
-			if (defaultImage) {
-				e.style.backgroundImage = "url(/data/gui/" + defaultImage + ")";
-			} else {
-				e.style.backgroundImage = "none";
-			}
-			e.textContent = "";
-			e.dataItem = null;
-		}
-	}
-
-	function onDragStart(e) {
-		var myobject = object || marauroa.me;
-		if (!myobject[slot]) {
-			e.preventDefault();
-			return;
-		}
-
-		var slotNumber = e.target.id.slice(slot.length + suffix.length);
-		var item = myobject[slot].getByIndex(slotNumber);
-		if (item) {
-			var img = stendhal.data.sprites.getAreaOf(stendhal.data.sprites.get(item.sprite.filename), 32, 32);
-			window.event = e; // required by setDragImage polyfil
-			e.dataTransfer.setDragImage(img, 0, 0);
-			e.dataTransfer.setData("Text", JSON.stringify({
-				path: item.getIdPath(),
-				zone: marauroa.currentZoneName
-			}));
-		} else {
-			e.preventDefault();
-		}
-	}
-
-	function onDragOver(e) {
-		e.preventDefault();
-		e.dataTransfer.dropEffect = "move";
-		return false;
-	}
-
-	function onDrop(e) {
-		var myobject = object || marauroa.me;
-		var datastr = e.dataTransfer.getData("Text") || e.dataTransfer.getData("text/x-stendhal");
-		if (datastr) {
-			var data = JSON.parse(datastr);
-			var targetPath = "[" + myobject["id"] + "\t" + slot + "]";
-			var action = {
-				"type": "equip",
-				"source_path": data.path,
-				"target_path": targetPath,
-				"zone" : data.zone
-			};
-			// if ctrl is pressed, we ask for the quantity
-			if (e.ctrlKey) {
-				ui.createSingletonFloatingWindow("Quantity", new DropQuantitySelectorDialog(action), e.pageX - 50, e.pageY - 25);
-			} else {
-				marauroa.clientFramework.sendAction(action);
-			}
-		}
-		e.stopPropagation();
-		e.preventDefault();
-	}
-
-	function onContextMenu(e) {
-		e.preventDefault();
-	}
-
-	function isRightClick(e) {
-		if (+new Date() - stendhal.ui.timestampMouseDown > 300) {
-			return true;
-		}
-		if (e.which) {
-			return (e.which === 3);
-		} else {
-			return (e.button === 2);
-		}
-	}
-
-	function onMouseDown(e) {
-		stendhal.ui.timestampMouseDown = +new Date();
-	}
-
-	function onMouseUp(e) {
-		e.preventDefault();
-		let event = stendhal.ui.html.extractPosition(e);
-		if (event.target.dataItem) {
-			if (quickPickup) {
-				marauroa.clientFramework.sendAction({
-					type: "equip",
-					"source_path": event.target.dataItem.getIdPath(),
-					"target_path": "[" + marauroa.me["id"] + "\tbag]",
-					"clicked": "", // useful for changing default target in equip action
-					"zone": marauroa.currentZoneName
-				});
-				return;
-			}
-
-			if (isRightClick(e)) {
-				ui.createSingletonFloatingWindow("Action", new ActionContextMenu(event.target.dataItem), event.pageX - 50, event.pageY - 5);
-			} else {
-				marauroa.clientFramework.sendAction({
-					type: "use",
-					"target_path": event.target.dataItem.getIdPath(),
-					"zone": marauroa.currentZoneName
-				});
-			}
-		}
-		document.getElementById("gamewindow").focus();
-	}
-
-	for (var i = 0; i < size; i++) {
-		var e = document.getElementById(slot + suffix + i);
-		e.setAttribute("draggable", true);
-		e.addEventListener("dragstart", onDragStart);
-		e.addEventListener("dragover", onDragOver);
-		e.addEventListener("drop", onDrop);
-		e.addEventListener("mousedown", onMouseDown);
-		e.addEventListener("mouseup", onMouseUp);
-		e.addEventListener("touchstart", onMouseDown);
-		e.addEventListener("touchend", onMouseUp);
-		e.addEventListener("contextmenu", onContextMenu);
-	}
-};
 
 
 stendhal.ui.equip = {
@@ -178,7 +32,7 @@ stendhal.ui.equip = {
 		stendhal.ui.equip.inventory = [];
 		for (var i in this.slotNames) {
 			stendhal.ui.equip.inventory.push(
-				new stendhal.ui.ItemContainerWindow(
+				new ItemContainerComponent(
 					this.slotNames[i], this.slotSizes[i], null, "", false, this.slotImages[i]));
 		}
 
@@ -219,7 +73,7 @@ stendhal.ui.equip = {
 		html += "</div>";
 
 		var popup = new stendhal.ui.Popup(title, html, 160, 370);
-		var itemContainer = new stendhal.ui.ItemContainerWindow(slot, sizeX * sizeY, object, suffix, quickPickup, null);
+		var itemContainer = new ItemContainerComponent(slot, sizeX * sizeY, object, suffix, quickPickup, null);
 		stendhal.ui.equip.inventory.push(itemContainer);
 		itemContainer.update();
 		popup.onClose = function() {
