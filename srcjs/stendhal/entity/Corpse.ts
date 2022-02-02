40ae88ab3b2d7f011bf670594bbf7c4961bdce93
Hendrik Brummermann <nhnb@users.sourceforge.net>
2022-02-02 22:38:11 +0100

    ported corpse.js to Corpse.ts

---
+++ srcjs/stendhal/entity/Corpse.ts
@@ -0,0 +1,122 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2022 - Stendhal                    *
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Affero General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
+import { ItemInventoryComponent } from "../ui/component/ItemInventoryComponent";
+import { FloatingWindow } from "../ui/toolkit/FloatingWindow";
+import { Entity } from "./Entity";
+
+declare var marauroa: any;
+declare var stendhal: any;
+
+export class Corpse extends Entity {
+	
+	override minimapShow = false;
+	override zIndex = 5500;
+	autoOpenedAlready = false;
+
+	override set(key: string, value: any) {
+		super.set(key, value);
+
+		this.sprite = this.sprite || {};
+		if (stendhal.config.gamescreen.blood && (key === "image")) {
+			this.sprite.filename = "/data/sprites/corpse/" + value + ".png";
+		} else if (!stendhal.config.gamescreen.blood && (key === "harmless_image")) {
+			this.sprite.fFilename = "/data/sprites/corpse/" + value + ".png";
+		}
+	}
+
+	override createSlot(name: string) {
+		var slot = marauroa.util.fromProto(marauroa.rpslotFactory["_default"], {
+			add: function(object: any) {
+				marauroa.rpslotFactory["_default"].add.apply(this, arguments);
+				if (this._objects.length > 0) {
+					this._parent.autoOpenIfDesired();
+				}
+			},
+
+			del: function(key: any) {
+				marauroa.rpslotFactory["_default"].del.apply(this, arguments);
+				if (this._objects.length == 0) {
+					this._parent.closeCorpseInventory();
+				}
+			}
+		});
+		slot._name = name;
+		slot._objects = [];
+		slot._parent = this;
+		return slot;
+	}
+
+	override isVisibleToAction(_filter: boolean) {
+		return true;
+	}
+
+	closeCorpseInventory() {
+		if (this.inventory && this.inventory.isOpen()) {
+			this.inventory.close();
+			this.inventory = undefined;
+		}
+	}
+
+	openCorpseInventory() {
+		if (!this.inventory || !this.inventory.isOpen()) {
+			let content_row = 2;
+			const content_col = 2;
+			if (this["content"]) {
+				if (this["content"]._objects.length > 4) {
+					content_row = 3;
+				}
+			}
+
+			this.inventory = new FloatingWindow("Corpse",
+				new ItemInventoryComponent(this, "content", content_row, content_col, true, undefined),
+				160, 370);
+		}
+	}
+
+	autoOpenIfDesired() {
+		if (!this.autoOpenedAlready) {
+			this.autoOpenedAlready = true;
+			if (marauroa.me && (this["corpse_owner"] == marauroa.me["_name"])) {
+
+				// TODO: for unknown reason, /data/sprites/items/undefined/undefined.png is requested without this delay
+				var that = this;
+				window.setTimeout(function() {
+					that.openCorpseInventory();
+				}, 1);
+			}
+		}
+	}
+
+	override destroy() {
+		this.closeCorpseInventory();
+	}
+
+	override onclick(_x: number, _y: number) {
+		this.openCorpseInventory();
+	}
+
+	override getCursor(_x: number, _y: number) {
+		if (!this["content"] || this["content"]._objects.length === 0) {
+			return "url(/data/sprites/cursor/emptybag.png) 1 3, auto";
+		}
+
+		// owner
+		if (!this["corpse_owner"] || (this["corpse_owner"] == marauroa.me["_name"])) {
+			return "url(/data/sprites/cursor/bag.png) 1 3, auto";
+		}
+
+		if ((stendhal.data.group.lootmode === "shared") && (stendhal.data.group.members[this["corpse_owner"]])) {
+			return "url(/data/sprites/cursor/bag.png) 1 3, auto";
+		}
+		return "url(/data/sprites/cursor/lockedbag.png) 1 3, auto";
+	}
+}
--- srcjs/stendhal/entity/corpse.js
+++ srcjs/stendhal/entity/corpse.js
@@ -1,5 +1,5 @@
 /***************************************************************************
- *                   (C) Copyright 2003-2017 - Stendhal                    *
+ *                   (C) Copyright 2003-2022 - Stendhal                    *
  ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -8,114 +8,10 @@
  *   License, or (at your option) any later version.                       *
  *                                                                         *
  ***************************************************************************/
-
 "use strict";
 
 var marauroa = window.marauroa = window.marauroa || {};
-var stendhal = window.stendhal = window.stendhal || {};
 
-marauroa.rpobjectFactory["corpse"] = marauroa.util.fromProto(marauroa.rpobjectFactory["entity"], {
+var Corpse = require("../../../build/ts/entity/Corpse").Corpse;
 
-	minimapShow: false,
-	zIndex: 5500,
-	autoOpenedAlready: false,
-
-	set: function(key, value) {
-		marauroa.rpobjectFactory["corpse"].proto.set.apply(this, arguments);
-
-		this.sprite = this.sprite || {};
-		if (stendhal.config.gamescreen.blood && (key === "image")) {
-			this.sprite.filename = "/data/sprites/corpse/" + value + ".png";
-		} else if (!stendhal.config.gamescreen.blood && (key === "harmless_image")) {
-			this.sprite.fFilename = "/data/sprites/corpse/" + value + ".png";
-		}
-	},
-
-	createSlot: function(name) {
-		var slot = marauroa.util.fromProto(marauroa.rpslotFactory["_default"], {
-			add: function(object) {
-				marauroa.rpslotFactory["_default"].add.apply(this, arguments);
-				if (this._objects.length > 0) {
-					this._parent.autoOpenIfDesired();
-				}
-			},
-
-			del: function(key) {
-				marauroa.rpslotFactory["_default"].del.apply(this, arguments);
-				if (this._objects.length == 0) {
-					this._parent.closeCorpseInventory();
-				}
-			}
-		});
-		slot._name = name;
-		slot._objects = [];
-		slot._parent = this;
-		return slot;
-	},
-
-	isVisibleToAction: function(filter) {
-		return true;
-	},
-
-	closeCorpseInventory: function() {
-		if (this.inventory && this.inventory.isOpen()) {
-			this.inventory.close();
-			this.inventory = undefined;
-		}
-	},
-
-	openCorpseInventory: function() {
-		if (!this.inventory || !this.inventory.isOpen()) {
-			let content_row = 2;
-			const content_col = 2;
-			if (this["content"]) {
-				if (this["content"]._objects.length > 4) {
-					content_row = 3;
-				}
-			}
-
-			this.inventory = new FloatingWindow("Corpse",
-				new ItemInventoryComponent(this, "content", content_row, content_col, true, undefined),
-				160, 370);
-		}
-	},
-
-	autoOpenIfDesired: function() {
-		if (!this.autoOpenedAlready) {
-			this.autoOpenedAlready = true;
-			if (marauroa.me && (this["corpse_owner"] == marauroa.me["_name"])) {
-
-				// TODO: for unknown reason, /data/sprites/items/undefined/undefined.png is requested without this delay
-				var that = this;
-				window.setTimeout(function() {
-					that.openCorpseInventory();
-				}, 1);
-			}
-		}
-	},
-
-	destroy: function() {
-		this.closeCorpseInventory();
-	},
-
-	onclick: function(x, y) {
-		this.openCorpseInventory();
-	},
-
-	getCursor: function(x, y) {
-		if (!this["content"] || this["content"]._objects.length === 0) {
-			return "url(/data/sprites/cursor/emptybag.png) 1 3, auto";
-		}
-
-		// owner
-		if (!this["corpse_owner"] || (this["corpse_owner"] == marauroa.me["_name"])) {
-			return "url(/data/sprites/cursor/bag.png) 1 3, auto";
-		}
-
-		if ((stendhal.data.group.lootmode === "shared") && (stendhal.data.group.members[this["corpse_owner"]])) {
-			return "url(/data/sprites/cursor/bag.png) 1 3, auto";
-		}
-		return "url(/data/sprites/cursor/lockedbag.png) 1 3, auto";
-	}
-
-});
+marauroa.rpobjectFactory["corpse"] = Corpse;
