8cfa5d282f3c5de109a2d94f6f80423a320c7897
Hendrik Brummermann <nhnb@users.sourceforge.net>
2022-02-02 19:13:19 +0100

    created class MenuItem for the right click context menu actions

---
+++ srcjs/stendhal/action/MenuItem.ts
@@ -0,0 +1,16 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2022 - Stendhal                    *
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Affero General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
+export class MenuItem  {
+	title!: string;
+	type?: string;
+	action?: Function;
+}
