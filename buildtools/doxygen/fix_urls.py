58543800e15b4238f801615bd3ea19c12efc100b
Jordan Irwin <antumdeluge@gmail.com>
2022-02-02 12:04:19 -0800

    Doxygen: update fix_urls script to fix Android API links

--- buildtools/doxygen/fix_urls.py
+++ buildtools/doxygen/fix_urls.py
@@ -1,44 +1,62 @@
 #!/usr/bin/env python
 
-# Doxygen is trimming the .html suffix from external references in tag files.
-# So this script is a hack to fix URLs after HTML documentation is generated
-# until a fix is determined for tag files.
-#
-# This is only needed if HTML_FILE_EXTENSION is set to ".html".
+# Doxygen 1.9.3 has some issues with appending extensions to some tags files
+# URLs, & stripping them from others. This script scans the build directory
+# & updates documentation with proper URL paths.
 
 
-import os, codecs
+import os, sys, codecs
 
 dir_root = os.path.normpath(os.path.join(os.path.dirname(__file__), "../../"))
 os.chdir(dir_root)
 
-delim = "docs.oracle.com/en/java/javase/16/docs/api/"
+suffix = ".htm"
+delim = "https://developer.android.com/reference/"
 
+print("Fixing external links for URLs: {}".format(delim))
+
+updated_count = 0
 for ROOT, DIRS, FILES in os.walk("build/doxygen/html/"):
 	for file in FILES:
 		file = os.path.normpath(os.path.join(ROOT, file))
 
-		if os.path.isfile(file) and file.endswith(".html") or file.endswith(".js"):
+		if os.path.isfile(file) and file.endswith(suffix) or file.endswith(".js"):
 			lines = ()
 			new_lines = []
 			buffer = codecs.open(file, "r", "utf-8")
 			if buffer:
-				lines = tuple(buffer.readlines())
+				plaintext = buffer.read().replace("\r\n", "\n").replace("\r", "\n")
 				buffer.close()
+
+				if not delim in plaintext:
+					continue
+
+				lines = tuple(plaintext.split("\n"))
 
 			for li in lines:
 				if delim in li:
 					tmp = li.split(delim)
-					url = tmp[1].split("\"")[0]
-					if not url.lower().endswith(".html"):
-						li = "{}{}{}.html{}".format(tmp[0], delim, url, tmp[1].lstrip(url))
+					if len(tmp) > 1 and suffix in tmp[1]:
+						before = tmp[0]
+						tmp = tmp[1].split(suffix)
+						after = ""
+						if len(tmp) > 1:
+							after = tmp[1]
+						url = "{}{}".format(delim, tmp[0])
+
+						li = "{}{}{}".format(before, url, after)
 
 				new_lines.append(li)
 
 			if tuple(new_lines) != lines:
-				print("\nUpdating file:\n  {}".format(file))
+				sys.stdout.write("\nUpdating file:\n  {} ...".format(file))
 
 				buffer = codecs.open(file, "w", "utf-8")
 				if buffer:
 					buffer.write("\n".join(new_lines))
 					buffer.close()
+
+				print(" done")
+				updated_count = updated_count + 1
+
+print("\nJob done (updated {} files)".format(updated_count))
