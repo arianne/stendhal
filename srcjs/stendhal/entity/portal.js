3d708f75d7dfaa318b100f5c513f1649f2183d62
Hendrik Brummermann <nhnb@users.sourceforge.net>
2022-02-03 23:45:14 +0100

    ported portal.js to Portal.ts

--- srcjs/stendhal/entity/portal.js
+++ srcjs/stendhal/entity/portal.js
@@ -9,65 +9,12 @@
  *                                                                         *
  ***************************************************************************/
 
+
 "use strict";
 
 var marauroa = window.marauroa = window.marauroa || {};
 
-/**
- * Portal
- */
-marauroa.rpobjectFactory["portal"] = marauroa.util.fromProto(marauroa.rpobjectFactory["entity"], {
-	minimapShow: true,
-	minimapStyle: "rgb(0,0,0)",
-	zIndex: 5000,
+var Portal = require("../../../build/ts/entity/Portal").Portal;
 
-	buildActions: function(list) {
-		marauroa.rpobjectFactory["portal"].proto.buildActions.apply(this, arguments);
-
-		if (this["_rpclass"] == "house_portal") {
-			list.push({
-				title: "Use",
-				type: "use"
-			});
-			list.push({
-				title: "Kock",
-				type: "knock"
-			});
-
-		} else {
-
-			// remove default action "look" unless it is a house portal
-			list.splice(list.indexOf({title: "Look", type: "look"}), 1);
-
-			list.push({
-				title: "Use",
-				type: "use"
-			});
-		}
-	},
-
-	isVisibleToAction: function(filter) {
-		return true;
-	},
-
-
-	/**
-	 * Create the default action for this entity. If the entity specifies a
-	 * default action description, interpret it as an action command.
-	 */
-	getDefaultAction: function() {
-		return {
-			"type": "moveto",
-			"x": "" + this["x"],
-			"y": "" + this["y"],
-			"zone": marauroa.currentZoneName
-		};
-	},
-
-	getCursor: function(x, y) {
-		return "url(/data/sprites/cursor/portal.png) 1 3, auto";
-	}
-
-});
-
-marauroa.rpobjectFactory["house_portal"] = marauroa.rpobjectFactory["portal"];
+marauroa.rpobjectFactory["portal"] = Portal;
+marauroa.rpobjectFactory["house_portal"] = Portal;
