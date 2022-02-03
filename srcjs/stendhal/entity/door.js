5a75d1281f19be549b054880589e96c8ea4d76ea
Hendrik Brummermann <nhnb@users.sourceforge.net>
2022-02-03 23:58:31 +0100

    ported door.js to Door.ts

--- srcjs/stendhal/entity/door.js
+++ srcjs/stendhal/entity/door.js
@@ -9,63 +9,11 @@
  *                                                                         *
  ***************************************************************************/
 
+
 "use strict";
 
 var marauroa = window.marauroa = window.marauroa || {};
 
-/**
- * Portal
- */
-marauroa.rpobjectFactory["door"] = marauroa.util.fromProto(marauroa.rpobjectFactory["portal"], {
-	zIndex: 5000,
+var Door = require("../../../build/ts/entity/Door").Door;
 
-	draw: function(ctx) {
-		var imagePath = "/data/sprites/doors/" + this["class"] + ".png";
-		var image = stendhal.data.sprites.get(imagePath);
-		if (image.height) {
-			var x = (this["x"] - 1) * 32;
-			var y = (this["y"] - 1) * 32;
-			var height = image.height / 2;
-
-			var offsetY = height;
-			if (this["open"] === "") {
-				offsetY = 0;
-			}
-			ctx.drawImage(image, 0, offsetY, image.width, height, x, y, image.width, height);
-		}
-	},
-
-	buildActions: function(list) {
-		list.push({
-			title: "Look",
-			type: "look"
-		});
-		list.push({
-			title: "Use",
-			type: "use"
-		});
-	},
-
-	isVisibleToAction: function(filter) {
-		return true;
-	},
-
-
-	/**
-	 * Create the default action for this entity. If the entity specifies a
-	 * default action description, interpret it as an action command.
-	 */
-	getDefaultAction: function() {
-		return {
-			"type": "moveto",
-			"x": "" + this["x"],
-			"y": "" + this["y"],
-			"zone": marauroa.currentZoneName
-		};
-	},
-
-	getCursor: function(x, y) {
-		return "url(/data/sprites/cursor/portal.png) 1 3, auto";
-	}
-
-});
+marauroa.rpobjectFactory["door"] = Door;
