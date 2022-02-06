13b4ef974e9e7d1b96de382bd9c98bc5ad3161fc
Hendrik Brummermann <nhnb@users.sourceforge.net>
2022-02-06 13:16:41 +0100

    ported movement adjustment algorithm from the class client

--- srcjs/stendhal/entity/ActiveEntity.ts
+++ srcjs/stendhal/entity/ActiveEntity.ts
@@ -10,61 +10,213 @@
  ***************************************************************************/
 
 import { Entity } from "./Entity";
+import { Direction } from "../util/Direction";
+import { MathUtil } from "../util/MathUtil";
 
 declare var stendhal: any;
 
 export class ActiveEntity extends Entity {
 
-	override updatePosition(time: number) {
-		var serverX = parseFloat(this["x"]);
-		var serverY = parseFloat(this["y"]);
-		if (this["_x"] == undefined) {
-			this["_x"] = serverX;
+	direction: Direction;
+
+	/** The current speed of this entity horizontally (tiles?/sec). */
+	private dx: number;
+
+	/** The current speed of this entity vertically (tiles?/sec) . */
+	private dy: number;
+
+	/** If <code>true</code>, this entity is not blocked by FlyOverArea */
+	private flying = false;
+
+	/**
+	 * Create an active (moving) entity.
+	 */
+	constructor() {
+		super();
+		this.direction = Direction.DOWN;
+		this.dx = 0.0;
+		this.dy = 0.0;
+	}
+
+	override set(key: string, value: any) {
+		if (["dir", "speed", "x", "y"].indexOf(key) > -1) {
+			let numberValue = parseFloat(value);
+			if (isNaN(this["_" + key])) {
+				this["_" + key] = numberValue;
+			}
+			this.processPositioning(key, numberValue);
 		}
-		if (this["_y"] == undefined) {
-			this["_y"] = serverY;
+		super.set(key, value);
+	}
+
+	override unset(key: string) {
+		super.unset(key);
+		if (["dir", "speed"].indexOf(key) > -1) {
+			this.processPositioning(key, 0);
+		}
+	}
+
+	/**
+	 * Determine if this entity is not moving.
+	 *
+	 * @return <code>true</code> if not moving.
+	 */
+	public stopped() {
+		return (this.dx == 0.0) && (this.dy == 0.0);
+	}
+
+
+	/**
+	 * calculates the movement if the server an client are out of sync. for some
+	 * milliseconds. (server turns are not exactly 300 ms) Most times this will
+	 * slow down the client movement
+	 *
+	 * @param clientPos
+	 *            the position the client has calculated
+	 * @param serverPos
+	 *            the position the server has reported
+	 * @param delta
+	 *            the movement based on direction
+	 * @return the new delta to correct the movement error
+	 */
+	private calcDeltaMovement(clientPos: number, serverPos: number, delta: number) {
+		let moveErr = clientPos - serverPos;
+		let moveCorrection = (delta - moveErr) / delta;
+		return (delta + delta * moveCorrection) / 2;
+	}
+
+	/**
+	 * When entity moves, it will be called with the data.
+	 *
+	 * @param x new x coordinate
+	 * @param y new y coordinate
+	 * @param direction new direction
+	 * @param speed new speed
+	 */
+	private onMove(x: number, y: number, direction: Direction, speed: number) {
+
+		let oldx = x;
+		let oldy = y;
+
+		this.dx = direction.dx * speed;
+		this.dy = direction.dy * speed;
+
+		if ((Direction.LEFT == direction) || (Direction.RIGHT == direction)) {
+			this["_y"] = y;
+			if (MathUtil.compareDouble(this["_x"], x, 1.0)) {
+				// make the movement look more nicely: + this.dx * 0.1
+				this.dx = this.calcDeltaMovement(this["_x"] + this.dx * 0.1, x, direction.dx)
+						* speed || 0;
+			} else {
+				this["_x"] = x;
+			}
+			this.dy = 0;
+		} else if ((Direction.UP == direction) || (Direction.DOWN == direction)) {
+			this["_x"] = x;
+			if (MathUtil.compareDouble(this["_y"], y, 1.0)) {
+				// make the movement look more nicely: + this.dy * 0.1
+				this.dy = this.calcDeltaMovement(this["_y"] + this.dy * 0.1, y, direction.dy)
+						* speed || 0;
+			} else {
+				this["_y"] = y;
+			}
+			this.dx = 0;
+		} else {
+			// placing entities
+			this["_x"] = x;
+			this["_y"] = y;
 		}
 
-		if (this["speed"] > 0) {
-			var oldX = this["_x"];
-			var oldY = this["_y"];
-			var movement = this["speed"] * time / 300;
-			switch (this["dir"]) {
-			case "1":
-				this["_y"] = this["_y"] - movement;
-				this["_x"] = serverX;
-				break;
-			case "2":
-				this["_x"] = this["_x"] + movement;
-				this["_y"] = serverY;
-				break;
-			case "3":
-				this["_y"] = this["_y"] + movement;
-				this["_x"] = serverX;
-				break;
-			case "4":
-				this["_x"] = this["_x"] - movement;
-				this["_y"] = serverY;
-			}
+		// Call onPosition only if the entity actually moved. Also always call
+		// on partial coordinates - those are always predicted rather than real
+		// and thus should always be a result of prediction. However, the
+		// client collision detection does not always agree with that of the
+		// server, so relying on just the coordinate change checks can miss
+		// entities stopping when they collide with each other.
+		if (!MathUtil.compareDouble(this["_x"], oldx)
+			|| !MathUtil.compareDouble(this["_y"], oldy)
+				|| !MathUtil.compareDouble(oldx, Math.floor(oldx))
+				|| !MathUtil.compareDouble(oldy, Math.floor(oldy))) {
+			// onPosition(x, y);
+		}
+	}
 
-			// fix desynchronized position (can happen if game tab is in background)
-			if (Math.abs(this["_x"] - serverX) > 1.75) {
-				this["_x"] = serverX;
-			}
-			if (Math.abs(this["_y"] - serverY) > 1.75) {
-				this["_y"] = serverY;
-			}
+
+	/**
+	 * Update cycle.
+	 *
+	 * @param delta
+	 *            The time (in ms) since last call.
+	 */
+	override updatePosition(delta: number) {
+		// do not call super
+
+		if (!this.stopped()) {
+			let step = (delta / 300.0);
+
+			let oldX = this["_x"];
+			let oldY = this["_y"];
+
+			// update the location of the entity based on speeds
+			this["_x"] += (this.dx * step);
+			this["_y"] += (this.dy * step);
 
 			if (this.collidesMap() || this.collidesEntities()) {
 				this["_x"] = oldX;
 				this["_y"] = oldY;
+			} else {
+				// onPosition(x, y);
 			}
-		} else {
-			// Restore server coordinates when the entity is not moving
-			this["_x"] = serverX;
-			this["_y"] = serverY;
 		}
 	}
+
+	/**
+	 * Process attribute changes that may affect positioning. This is needed
+	 * because different entities may want to process coordinate changes more
+	 * gracefully.
+	 *
+	 * @param base
+	 *            The previous values.
+	 * @param diff
+	 *            The changes.
+	 */
+	processPositioning(key: string, value: number) {
+		// Real movement case
+		let oldx = this["x"];
+		let oldy = this["y"];
+
+		let newX = oldx;
+		let newY = oldy;
+
+		if (key == "x") {
+			newX = value;
+		}
+		if (key == "y") {
+			newY = value;
+		}
+
+		if (key == "dir") {
+			this.direction = Direction.VALUES[value];
+		}
+
+		let speed = this["speed"];
+		if (key == "speed") {
+			speed =  value;
+		}
+
+
+		this.onMove(newX, newY, this.direction, speed);
+
+		if ((this.direction == Direction.STOP) || (speed == 0)) {
+			this.dx = 0;
+			this.dy = 0;
+
+			// Store the new position before signaling it with onPosition().
+			this["_x"] = newX;
+			this["_y"] = newY;
+		}
+	}
+
 
 	/**
 	 * Check if the entity collides with the collision map.
