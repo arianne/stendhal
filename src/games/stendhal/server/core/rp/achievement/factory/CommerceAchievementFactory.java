39eac8acf42f96bbc3291519d6629b51ec2063ee
Jordan Irwin <antumdeluge@gmail.com>
2022-02-09 01:28:23 -0800

    Preliminary values for "Community Supporter" achievement

--- src/games/stendhal/server/core/rp/achievement/factory/CommerceAchievementFactory.java
+++ src/games/stendhal/server/core/rp/achievement/factory/CommerceAchievementFactory.java
@@ -53,9 +53,8 @@ public class CommerceAchievementFactory extends AbstractAchievementFactory {
 			Achievement.EASY_BASE_SCORE, true,
 			new BoughtNumberOfCondition(COUNT_HAPPY_HOUR, ITEMS_HAPPY_HOUR)));
 
-		// TODO: WIP
 		achievements.add(createAchievement(
-			"commerce.buy.all", "Community Supporter", "Spend X amount of money around the world",
+			"commerce.buy.all", "Community Supporter", "Spend money around the world",
 			Achievement.MEDIUM_BASE_SCORE, false,
 			new HasSpentAmountAtSellers()));
 
@@ -65,10 +64,53 @@ public class CommerceAchievementFactory extends AbstractAchievementFactory {
 
 	private class HasSpentAmountAtSellers implements ChatCondition {
 
-		// TODO: this is a WIP
+		// default value for sellers not included in this list is 1000
 		private final Map<String, Integer> TRADE_ALL_AMOUNTS = new HashMap<String, Integer>() {{
 			put("Margaret", 500);
-			put("Ilisa", 500);
+			put("Ilisa", 4000);
+			put("Adena", 500);
+			put("Coralia", 500);
+			put("Dale", 500);
+			put("Mayor Chalmers", 10000);
+			put("Mia", 2000);
+			put("Mrotho", 2500);
+			put("Karl", 50);
+			put("Philomena", 200);
+			put("Dr. Feelgood", 2000);
+			put("Haizen", 10000);
+			put("Edward", 22000); // 1 scuba gear
+			put("Mirielle", 20000);
+			put("D J Smith", 2000);
+			put("Wanda", 10000);
+			put("Sarzina", 5000);
+			put("Xhiphin Zohos", 8000);
+			put("Orchiwald", 6000);
+			put("Sam", 300);
+			put("Hazel", 10000);
+			put("Erodel Bmud", 15000);
+			put("Kendra Mattori", 8000);
+			put("Diehelm Brui", 400);
+			put("Lorithien", 10000);
+			put("Jynath", 16000);
+			put("Ouchit", 400);
+			put("Xin Blanca", 800);
+			put("Xoderos", 570); // 1 of each item
+			put("Barbarus", 400); // 1 pick
+			put("Jenny", 500);
+			put("Nishiya", 60); // 2 sheep
+			put("Wrviliza", 200);
+
+			// excluded
+			put("Gulimo", 0);
+			put("Hagnurk", 0);
+			put("Ognir", 0);
+			put("Mrs. Yeti", 0);
+			put("Wrvil", 0);
+			put("Zekiel", 0);
+			put("Mizuno", 0);
+			put("Rengard", 0);
+			put("Felina", 0);
+			put("Fidorea", 0);
 		}};
 
 
