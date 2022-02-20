974adca0877e7b1238d266da1d02c8ce9325f7ff
Jordan Irwin <antumdeluge@gmail.com>
2022-02-20 15:40:33 -0800

    Fix null pointer access

--- src/games/stendhal/server/actions/KnockAction.java
+++ src/games/stendhal/server/actions/KnockAction.java
@@ -104,8 +104,7 @@ public class KnockAction implements ActionListener {
 					houseplayer.sendPrivateText(message);
 				}
 			} else {
-				logger.debug("Invalid zone (" + zone.getName() + ") associated with "
-					+ mainZone.getName());
+				logger.debug("Invalid zone associated with " + mainZone.getName());
 			}
 		}
 
