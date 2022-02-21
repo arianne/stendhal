73b9a3cd3a3c1d2f9fe550754d55fb2bbe5865fc
Jordan Irwin <antumdeluge@gmail.com>
2022-02-20 22:19:09 -0800

    WebView: remove duplicate call to DPadArrows.onRefreshView

--- android/app/src/main/java/org/arianne/stendhal/client/PreferencesActivity.java
+++ android/app/src/main/java/org/arianne/stendhal/client/PreferencesActivity.java
@@ -21,7 +21,6 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.arianne.stendhal.client.input.DPadArrows;
 import org.arianne.stendhal.client.sound.MusicPlayer;
 
 
@@ -244,9 +243,7 @@ public class PreferencesActivity extends AppCompatActivity {
 
 		@Override
 		public void onSharedPreferenceChanged(final SharedPreferences sp, final String key) {
-			if (key.equals("show_dpad")) {
-				DPadArrows.get().onRefreshView();
-			}
+			// this is handled in MainActivity.onConfigurationChanged
 		}
 
 		@Override
