fdf0f90081032b093971d0915133a973f06d7cde
Jordan Irwin <antumdeluge@gmail.com>
2022-02-24 00:27:20 -0800

    Add more preliminary dialogue to tutorial island quest

--- src/games/stendhal/server/maps/quests/TutorialIsland.java
+++ src/games/stendhal/server/maps/quests/TutorialIsland.java
@@ -28,9 +28,15 @@ import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.engine.ZoneAttributes;
 import games.stendhal.server.core.events.TurnListener;
 import games.stendhal.server.core.events.TurnNotifier;
+import games.stendhal.server.entity.npc.action.MultipleActions;
+import games.stendhal.server.entity.npc.action.SetQuestAction;
+import games.stendhal.server.entity.npc.condition.OrCondition;
 import games.stendhal.server.entity.npc.condition.QuestInStateCondition;
+import games.stendhal.server.entity.npc.condition.QuestNotStartedCondition;
+import games.stendhal.server.entity.npc.condition.QuestStartedCondition;
 import games.stendhal.server.entity.npc.fsm.Engine;
 import games.stendhal.server.entity.npc.ChatAction;
+import games.stendhal.server.entity.npc.ChatCondition;
 import games.stendhal.server.entity.npc.ConversationPhrases;
 import games.stendhal.server.entity.npc.ConversationStates;
 import games.stendhal.server.entity.npc.EventRaiser;
@@ -78,6 +84,7 @@ import games.stendhal.server.entity.player.Player;
  * - make sure any items given to player during tutorial have infostring
  *   and are removed from player & ground when tutorial is finished
  * - handle player dieing in tutorial zone
+ * - make visible in travel log
  */
 public class TutorialIsland extends AbstractQuest {
 
@@ -89,6 +96,16 @@ public class TutorialIsland extends AbstractQuest {
 	private final String tutorTitle = "Tutor";
 	// NPCs created per player
 	private Map<String, SpeakerNPC> activeTutors;
+
+	// the quest states
+	private static final String ST_RULES = "rules";
+	private static final String ST_NPCS = "npcs";
+	private static final String ST_CHAT = "chat";
+	private static final String ST_ITEMS = "items";
+	private static final String ST_COMBAT = "combat";
+	private static final String ST_QUESTS = "quests";
+	private static final String ST_PETS = "pets";
+	private static final String ST_FINAL = "final";
 
 
 	@Override
@@ -122,11 +139,7 @@ public class TutorialIsland extends AbstractQuest {
 	}
 
 	public void startTutorialForPlayer(final Player player) {
-		if (!player.hasQuest(SLOT)) {
-			// TODO: set this at first interaction with NPC
-			// first time player has entered tutorial
-			player.setQuest(SLOT, "start");
-		}
+		// TODO: check if player is level 0 & never visited 0_semos_village_w
 
 		final String pname = player.getName();
 
@@ -236,92 +249,227 @@ public class TutorialIsland extends AbstractQuest {
 	}
 
 	private void onCompleted(final Player player) {
-		player.setQuest(SLOT, "done");
-		player.setQuest(SLOT, 1, Long.toString(System.currentTimeMillis()));
+		SingletonRepository.getTurnNotifier().notifyInTurns(10, new TurnListener() {
+			@Override
+			public void onTurnReached(final int currentTurn) {
+				final StendhalRPZone guardhouse = SingletonRepository.getRPWorld()
+					.getZone("int_semos_guard_house");
+				if (guardhouse == null) {
+					logger.error("could not get guardhouse zone to teleport player");
+					return;
+				}
 
-		final StendhalRPZone guardhouse = SingletonRepository.getRPWorld()
-			.getZone("int_semos_guard_house");
-		if (guardhouse == null) {
-			logger.error("could not get guardhouse zone to teleport player");
-			return;
-		}
+				guardhouse.placeObjectAtEntryPoint(player);
 
-		guardhouse.placeObjectAtEntryPoint(player);
+				if (player.getZone().getName().equals(player.getName() + "_" + SLOT)) {
+					logger.error("failed to teleport player out of tutorial zone");
+					return;
+				}
 
-		if (player.getZone().getName().equals(player.getName() + "_" + SLOT)) {
-			logger.error("failed to teleport player out of tutorial zone");
-			return;
-		}
-
-		dismantleIsland(player);
-
-		// TODO:
-		// - create GameEvent
+				dismantleIsland(player);
+			}
+		});
 	}
 
 	private void prepareSteps(final String pname) {
 		final SpeakerNPC tutor = activeTutors.get(pname);
 
-		// TODO:
-		// - add transition to set quest state to start, in case it was not
-		//   already set or, just use first transition to set state
-
+		prepareOfferQuest(tutor);
+		prepareRulesStep(tutor);
 		prepareNPCsStep(tutor);
+		prepareChatStep(tutor);
+		prepareItemsStep(tutor);
+		prepareCombatStep(tutor);
+		prepareQuestsStep(tutor);
+		preparePetsStep(tutor);
 		prepareFinalStep(tutor);
 	}
 
-	private void prepareNPCsStep(final SpeakerNPC tutor) {
+	/**
+	 * Creates a chat action that has NPC say a phrase after a set
+	 * number of turns has passed.
+	 *
+	 * @param delay
+	 *     Number of turns to wait.
+	 * @param msg
+	 *     The phrase that the NPC will say.
+	 * @return
+	 *     New ChatAction instance.
+	 */
+	private ChatAction delayMessage(final int delay, final String msg) {
+		return new ChatAction() {
+			@Override
+			public void fire(final Player player, final Sentence sentence, final EventRaiser raiser) {
+				SingletonRepository.getTurnNotifier().notifyInTurns(delay, new TurnListener() {
+					@Override
+					public void onTurnReached(final int currentTurn) {
+						raiser.say(msg);
+					}
+				});
+			}
+		};
+	}
+
+	private void prepareOfferQuest(final SpeakerNPC tutor) {
+		final ChatCondition questStarted = new QuestStartedCondition(SLOT);
+		final ChatCondition questNotStarted = new QuestNotStartedCondition(SLOT);
+
 		tutor.add(ConversationStates.IDLE,
 			ConversationPhrases.GREETING_MESSAGES,
-			new QuestInStateCondition(SLOT, 0, "start"),
-			ConversationStates.ATTENDING,
-			"You look like a fast learner. There are many helpful people in the #world."
-				+ " Don't be afraid to talk to them."
-				+ " If their reply contains any highlighted words, like #this,"
-				+ " that is an indication that they will respond to that word."
-				+ " When you are done talking, just say #bye."
-				+ " Now, why don't you ask me about some #rules.",
+			questNotStarted,
+			ConversationStates.QUEST_OFFERED,
+			"You look like a fast learner. Want to learn about this world?"
+				+ " If so, just say #yes.",
 			null);
 
-		tutor.add(ConversationStates.ATTENDING,
-			"rules",
-			null,
+		tutor.add(ConversationStates.QUEST_OFFERED,
+			ConversationPhrases.NO_MESSAGES,
+			questNotStarted,
+			ConversationStates.IDLE,
+			"Oh really? Well, good luck with that. If you change your mind, just say #hi.",
+			null);
+
+		tutor.add(ConversationStates.QUEST_OFFERED,
+			ConversationPhrases.YES_MESSAGES,
+			questNotStarted,
 			ConversationStates.ATTENDING,
-			"The rules of #Faiumoni are simple.... and I'll get to that later.",
+			"I thought so! Okay, the first thing you will need to learn is how to"
+				+ " communicate. So far you are doing great! There are many helpful"
+				+ " people in this #world who are always responsive to a friendly"
+				+ " #hello. You will need to talk with them if you want to get"
+				+ " anywhere.",
+			new MultipleActions(
+				new SetQuestAction(SLOT, ST_RULES),
+				delayMessage(10, "If their response contains a highlighted word, like"
+					+ " #this, that is an indication that they have some information"
+					+ " on the subject. For example, ask me about #rules.")));
+
+		tutor.add(ConversationStates.ATTENDING,
+			ConversationPhrases.GREETING_MESSAGES,
+			questStarted,
+			ConversationStates.ATTENDING,
+			"I'm already engaged in conversation with you. No need to greet me again.",
 			null);
 
 		tutor.add(ConversationStates.ATTENDING,
 			Arrays.asList("faiumoni", "world"),
-			new QuestInStateCondition(SLOT, 0, "start"),
-			ConversationStates.ATTENDING,
-			"Faiumoni is the name of our world, the world that you will soon enter,"
-				+ " if you are able to pass a few #tests.",
-			null);
-
-		tutor.add(ConversationStates.ATTENDING,
-			Arrays.asList("faiumoni", "world"),
-			new QuestInStateCondition(SLOT, 0, "start"),
+			questStarted,
 			ConversationStates.ATTENDING,
 			"Faiumoni is the name of our world, the world that you will soon enter.",
 			null);
+	}
+
+	private void prepareRulesStep(final SpeakerNPC tutor) {
+		final ChatCondition onRulesStep = new QuestInStateCondition(SLOT, 0, ST_RULES);
+
+		tutor.add(ConversationStates.IDLE,
+			ConversationPhrases.GREETING_MESSAGES,
+			onRulesStep,
+			ConversationStates.ATTENDING,
+			"If you are ready for the next step, just tell me #next. Or I can go over"
+				+ " the #rules again.",
+			null);
 
 		tutor.add(ConversationStates.ATTENDING,
-			Arrays.asList("test", "tests"),
-			new QuestInStateCondition(SLOT, 0, "start"),
+			Arrays.asList("rule", "rules"),
+			new QuestStartedCondition(SLOT),
 			ConversationStates.ATTENDING,
-			"Unfortunately, that is as far as this tutorial goes for now.",
-			null);
+			"The rules of #Faiumoni are simple. First, always be polite in conversation."
+				+ " Don't just walk away from someone without saying #bye. Secondly,"
+				+ " don't cheat. We don't like it when we have to remove others from"
+				+ " the community. It makes for a much smaller world with less people"
+				+ " to talk to. But don't doubt for a second that we would rather keep"
+				+ " it clean and fair.",
+			new MultipleActions(
+				delayMessage(10, "Never share your account password with another player,"
+					+ " not even with a #'game master'. Your account is sure to get banned."),
+				delayMessage(20, "For more information on our rules, you can read a more"
+					+ " complete guide here: #https://stendhalgame.org/player-guide/rules.html"),
+				delayMessage(30, "If you you are clear on the rules, tell me #next if you"
+					+ " feel you are ready to move on.")));
+
+		// TODO: change this to move on to NPCs step
+		tutor.add(ConversationStates.ATTENDING,
+			"next",
+			onRulesStep,
+			ConversationStates.ATTENDING,
+			"Unfortunately, that is as far as this tutorial goes for now. So until I can come"
+				+ " up with more stuff to teach, our session here will have to be #done.",
+			new SetQuestAction(SLOT, 0, ST_FINAL));
+	}
+
+	private void prepareNPCsStep(final SpeakerNPC tutor) {
+		final ChatCondition onNPCsStep = new QuestInStateCondition(SLOT, 0, ST_NPCS);
+	}
+
+	private void prepareChatStep(final SpeakerNPC tutor) {
+		final ChatCondition onChatStep = new QuestInStateCondition(SLOT, 0, ST_CHAT);
+	}
+
+	private void prepareItemsStep(final SpeakerNPC tutor) {
+		final ChatCondition onItemsStep = new QuestInStateCondition(SLOT, 0, ST_ITEMS);
+	}
+
+	private void prepareCombatStep(final SpeakerNPC tutor) {
+		final ChatCondition onCombtaStep = new QuestInStateCondition(SLOT, 0, ST_COMBAT);
+	}
+
+	private void prepareQuestsStep(final SpeakerNPC tutor) {
+		final ChatCondition onQuestsStep = new QuestInStateCondition(SLOT, 0, ST_QUESTS);
+	}
+
+	private void preparePetsStep(final SpeakerNPC tutor) {
+		final ChatCondition onPetsStep = new QuestInStateCondition(SLOT, 0, ST_PETS);
 	}
 
 	private void prepareFinalStep(final SpeakerNPC tutor) {
+		final ChatCondition onFinalStep = new QuestInStateCondition(SLOT, 0, ST_FINAL);
+		final ChatCondition questDone = new QuestInStateCondition(SLOT, 0, "done");
+
+		tutor.add(ConversationStates.IDLE,
+			ConversationPhrases.GREETING_MESSAGES,
+			onFinalStep,
+			ConversationStates.ATTENDING,
+			"I think we are all #done. There is nothing more I can teach you now.",
+			null);
+
+		tutor.add(ConversationStates.IDLE,
+			ConversationPhrases.GREETING_MESSAGES,
+			questDone,
+			ConversationStates.QUESTION_1,
+			"We are all done here. Just tell me if you want to #leave.",
+			null);
+
 		tutor.add(ConversationStates.ATTENDING,
 			ConversationPhrases.FINISH_MESSAGES,
-			new QuestInStateCondition(SLOT, 0, "start"), // TODO: this will be different after adding more steps
+			onFinalStep,
+			ConversationStates.QUESTION_1,
+			"Good work! You are now ready to enter the world of Faiumoni. Just let me"
+				+ " know when you want to #leave, and I will send you on your new"
+				+ " adventure.",
+			new ChatAction() {
+				@Override
+				public void fire(final Player player, final Sentence sentence, final EventRaiser raiser) {
+					player.setQuest(SLOT, "done");
+					player.setQuest(SLOT, 1, Long.toString(System.currentTimeMillis()));
+
+					// TODO:
+					// - create GameEvent
+				}
+			});
+
+		tutor.add(ConversationStates.QUESTION_1,
+			"leave",
+			new OrCondition(
+				onFinalStep,
+				questDone),
 			ConversationStates.IDLE,
-			"Good work! You are now ready to enter the world of Faiumoni.",
+			"Good luck in your future endeavors!",
 			new ChatAction(){
 				@Override
 				public void fire(final Player player, final Sentence sentence, final EventRaiser raiser) {
+					// TODO: play a sound & post area message about NPC casting a spell
+					//       to teleport player
 					onCompleted(player);
 				}
 			});
