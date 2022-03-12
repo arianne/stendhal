b82971e2d33a222a8cd28bfdf452bd86b84caa43
Jordan Irwin <antumdeluge@gmail.com>
2022-03-12 10:51:33 -0800

    Add some missing license boilerplates

--- src/games/stendhal/server/core/config/annotations/Dev.java
+++ src/games/stendhal/server/core/config/annotations/Dev.java
@@ -1,3 +1,14 @@
+/***************************************************************************
+ *                   (C) Copyright 2003-2022 - Arianne                     *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package games.stendhal.server.core.config.annotations;
 
 import java.lang.annotation.Documented;
--- src/games/stendhal/server/entity/GuidedEntity.java
+++ src/games/stendhal/server/entity/GuidedEntity.java
@@ -1,13 +1,15 @@
-/*
- * @(#) src/games/stendhal/server/entity/GuidedEntity.java
- *
- * $Id$
- */
-
+/***************************************************************************
+ *                    Copyright © 2003-2022 - Arianne                      *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package games.stendhal.server.entity;
-
-//
-//
 
 import static games.stendhal.common.constants.General.PATHSET;
 
