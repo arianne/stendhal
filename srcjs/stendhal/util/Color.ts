afc33358c3b1a42f533f5d4b5df4dacc253b67ef
Jordan Irwin <antumdeluge@gmail.com>
2022-03-23 09:27:13 -0700

    Define more colors

--- srcjs/stendhal/util/Color.ts
+++ srcjs/stendhal/util/Color.ts
@@ -11,11 +11,35 @@
 
 
 export enum Color {
+	AQUA = "#00ffff",
 	BLACK = "#000000",
+	BLUE = "#0000ff",
+	CYAN = AQUA,
 	DARKGRAY = "#a9a9a9",
 	GRAY = "#808080",
 	GREEN = "#008000",
+	LIGHTGRAY = "#c0c0c0",
+	MAGENTA = "#ff00ff",
 	ORANGE = "#ffa500",
 	PINK = "#ffc0cb",
-	RED = "#ff0000"
+	RED = "#ff0000",
+	WHITE = "#ffffff",
+	YELLOW = "#ffff00",
+
+	DOMESTICANIMAL = "rgb(255, 150, 0)",
+	GHOST = GRAY,
+	GROUP = "rgb(99, 61, 139)",
+	PLAYER = WHITE,
+	USER = BLUE,
+
+	BACKGROUND = "#cccccc",
+	BLOCKED = RED,
+	COLLISION = RED,
+	FLYOVER = "rgb(212, 158, 72)",
+	PROTECTION = "rgb(202, 230, 202)",
+	WALKBLOCK = "rgb(209, 144, 224)",
+
+	MOVING = "rgb(200, 255, 200)",
+	PORTAL = WHITE,
+	PORTALOUTLINE = BLACK
 }
