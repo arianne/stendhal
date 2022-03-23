95719f5258d389c443577ae7cab85615ac386e6e
Jordan Irwin <antumdeluge@gmail.com>
2022-03-23 02:37:41 -0700

    Suppress notification bubble for privmsg & significant_positive

--- srcjs/stendhal/util/Chat.ts
+++ srcjs/stendhal/util/Chat.ts
@@ -15,7 +15,7 @@ import { ChatLogComponent } from "../ui/component/ChatLogComponent";
 
 declare let marauroa: any;
 
-const headless_types = ["normal", "regular"];
+const headless_types = ["normal", "regular", "privmsg", "significant_positive"];
 
 
 /**
