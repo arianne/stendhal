9ea021f9a313254badeb57d5c3ceea9bb622bb19
Jordan Irwin <antumdeluge@gmail.com>
2022-03-23 07:51:37 -0700

    Move color definitions into their own file

--- srcjs/stendhal/util/Color.ts
+++ srcjs/stendhal/util/Color.ts
@@ -9,28 +9,13 @@
  *                                                                         *
  ***************************************************************************/
 
-import { Color } from "./Color";
 
-
-export const NotificationType = {
-	"client": Color.GRAY,
-	"detailed": Color.BLACK,
-	"emote": "rgb(99, 61, 139)",
-	"error": Color.RED,
-	"group": "rgb(00, 00, 160)",
-	"heal": Color.GREEN,
-	"information": Color.ORANGE,
-	"negative": Color.RED,
-	"normal": Color.BLACK,
-	"poison": Color.RED,
-	"positive": Color.GREEN,
-	"privmsg": Color.DARKGRAY,
-	"response": "#006400",
-	"scene_setting": "#572002",
-	"server": Color.DARKGRAY,
-	"significant_negative": Color.PINK,
-	"significant_positive": "rgb(65, 105, 225)",
-	"support": "#ff7200",
-	"tutorial": "rgb(172, 0, 172)",
-	"warning": "#a00000"
-} as {[index: string]: string;};
+export enum Color {
+	BLACK = "#000000",
+	DARKGRAY = "#a9a9a9",
+	GRAY = "#808080",
+	GREEN = "#008000",
+	ORANGE = "#ffa500",
+	PINK = "#ffc0cb",
+	RED = "#ff0000"
+}
