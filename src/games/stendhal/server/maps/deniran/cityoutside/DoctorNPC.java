79770ff95dc0efb96121dfa97b5b47e0463db66a
Hendrik Brummermann <nhnb@users.sourceforge.net>
2022-04-03 15:06:50 +0200

    code cleanup and further dialog

--- src/games/stendhal/server/maps/deniran/cityoutside/DoctorNPC.java
+++ src/games/stendhal/server/maps/deniran/cityoutside/DoctorNPC.java
@@ -32,7 +32,7 @@ public class DoctorNPC implements ZoneConfigurator {
 			@Override
 			protected void createDialog() {
 				addGreeting("Oh, hello there!");
-				addJob("I am inspector of fences or something.");
+				addJob("I am inspector of fences. Or something.");
 				addGoodbye("Be fantastic!");
 				addReply(Arrays.asList("advice", "doctor", "promise", "kind", "cowardly", "pear", "pears"),
 						"Never be cruel, never be cowardly. And never ever eat pears! Remember: Hate is always foolish and love is always wise.");
@@ -56,6 +56,9 @@ public class DoctorNPC implements ZoneConfigurator {
 						"Allons-y!");
 				addReply(Arrays.asList("11", "eleven", "Matt", "Smith"),
 						"Geronimo!");
+				addReply(Arrays.asList("13", "thirteen", "Jodie", "Whittaker"),
+						"Brilliant!");
+				addReply("bow", "Bow ties are cool.");
 			}
 
 		};
@@ -67,83 +70,4 @@ public class DoctorNPC implements ZoneConfigurator {
 		return npc;
 	}
 
-	
-/*
-12  The Zygon Inversion 
-
-So, let me ask you a question about this brave new world of yours.
-When you've killed all the bad guys and when it's all perfect and just and fair,
-when you have finally got it exactly the way you want it,
-what are you going to do with the people like you?
-The troublemakers
-How are you going to protect your glorious revolution
-from the next one?
- < We'll win
-Oh, will you? Well, meybe... maybe you will win!
-But nobody wins for long.
-The wheel just keeps turning.
----------
-
-< Do you know, what I see, Doctor?
-< A box.
-< A box with everything I need.
-< A 50% chance.
-<< For us, too.
-
-And we're off! Fingers on buzzers! Are you feeling lucky?
-Are you ready to play the game?
-Who's going to be quickest?
-Who's going to be luckiest?
-<< This is not a game.
-No, it's not a game, sweetheart, and I mean that most sincerely.
-< Why are you doing this?
-<< Yes, I'd quite like to know that, too. You set this up, why?
-Because it's not a game, Kate.
-This is a scale model of war...
-every war every fought right there in front of you...
-Because it'S always the same.
-When you fire that first shot, no matter how right you feel, you have no idea who's goign to die!
-You don't know whose children are going to scream and burn!
-How many hearts will be broken!
-How many lives shattered!
-How much blood will spill until everybody does what they were always going to have to do from the very beginning, sit down and talk!
-Listen to me. Listen, I just... I just want you to think.
-Do you know what thinking is? It's just a fancy word for changing your mind.
-< I will not change my mind.
-Then you will die stupid.
-Alternatively, you chould step away from that box.
-You can walk right out of that door and you could stand your revolution down.
-< No
-< I'm not stopping this, Doctor.
-< I started it. I will not stop it.
-< You think they'll let me go after what I've done
-You're all the same, you screaming ids, you know that?
-"Look at me, I'm unforgiveable."
-Well here's the unforeseeable. I forgive you...
-after all you've done...
-I forgive you.
-< You don't understand.
-< You will never understand.
-I don't understand?
-Are you kidding? Me? Of course I understand.
-You mean, you call this a war`This funny little thing?
-This is not a war!
-I fought in a bigger war than you will ever know.
-I did worse things than you could ever imagine.
-And when I close my eyes...
-I hear more screams than anyone could ever be able to count!
-And you know what you do with all that pain?
-Shall I tell you where you put it?
-You hold it tight...
-till it burns your hand...
-and you say this:
-No one else will ever have to live like this!
-No one else will have to feel this pain!
-Not on my watch!
-- Kate closes box
-Thank you.
-Thank you.
- */
-
-	
 }
