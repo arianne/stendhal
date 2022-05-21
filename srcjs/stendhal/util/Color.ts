5c49972107ceec49f7f7dd820b6b95d4fbcd2377
Jordan Irwin <antumdeluge@gmail.com>
2022-05-21 02:00:52 -0700

    Convert Color from enum to class...
    Enum values were not working with minimap.

--- srcjs/stendhal/util/Color.ts
+++ srcjs/stendhal/util/Color.ts
@@ -10,36 +10,36 @@
  ***************************************************************************/
 
 
-export enum Color {
-	AQUA = "#00ffff",
-	BLACK = "#000000",
-	BLUE = "#0000ff",
-	CYAN = AQUA,
-	DARKGRAY = "#a9a9a9",
-	GRAY = "#808080",
-	GREEN = "#008000",
-	LIGHTGRAY = "#c0c0c0",
-	MAGENTA = "#ff00ff",
-	ORANGE = "#ffa500",
-	PINK = "#ffc0cb",
-	RED = "#ff0000",
-	WHITE = "#ffffff",
-	YELLOW = "#ffff00",
+export class Color {
+	public static readonly AQUA = "#00ffff";
+	public static readonly BLACK = "#000000";
+	public static readonly BLUE = "rgb(0, 0, 255)";
+	public static readonly CYAN = Color.AQUA;
+	public static readonly DARKGRAY = "#a9a9a9";
+	public static readonly GRAY = "#808080";
+	public static readonly GREEN = "#008000";
+	public static readonly LIGHTGRAY = "#c0c0c0";
+	public static readonly MAGENTA = "#ff00ff";
+	public static readonly ORANGE = "#ffa500";
+	public static readonly PINK = "#ffc0cb";
+	public static readonly RED = "#ff0000";
+	public static readonly WHITE = "#ffffff";
+	public static readonly YELLOW = "#ffff00";
 
-	DOMESTICANIMAL = "rgb(255, 150, 0)",
-	GHOST = GRAY,
-	GROUP = "rgb(99, 61, 139)",
-	PLAYER = WHITE,
-	USER = BLUE,
+	public static readonly DOMESTICANIMAL = "rgb(255, 150, 0)";
+	public static readonly GHOST = Color.GRAY;
+	public static readonly GROUP = "rgb(99, 61, 139)";
+	public static readonly PLAYER = Color.WHITE;
+	public static readonly USER = Color.BLUE;
 
-	BACKGROUND = "#cccccc",
-	BLOCKED = RED,
-	COLLISION = RED,
-	FLYOVER = "rgb(212, 158, 72)",
-	PROTECTION = "rgb(202, 230, 202)",
-	WALKBLOCK = "rgb(209, 144, 224)",
+	public static readonly BACKGROUND = "#cccccc";
+	public static readonly BLOCKED = Color.RED;
+	public static readonly COLLISION = Color.RED;
+	public static readonly FLYOVER = "rgb(212, 158, 72)";
+	public static readonly PROTECTION = "rgb(202, 230, 202)";
+	public static readonly WALKBLOCK = "rgb(209, 144, 224)";
 
-	MOVING = "rgb(200, 255, 200)",
-	PORTAL = WHITE,
-	PORTALOUTLINE = BLACK
+	public static readonly MOVING = "rgb(200, 255, 200)";
+	public static readonly PORTAL = Color.WHITE;
+	public static readonly PORTALOUTLINE = Color.BLACK;
 }
