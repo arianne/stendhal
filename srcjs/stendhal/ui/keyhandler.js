7b14546eaa3ab50bdab4787086160a05a3d3f4a4
Jordan Irwin <antumdeluge@gmail.com>
2022-06-10 14:53:26 -0700

    Don't move chat input caret when Ctrl+arrow hotkey is used

--- srcjs/stendhal/ui/keyhandler.js
+++ srcjs/stendhal/ui/keyhandler.js
@@ -32,6 +32,7 @@ stendhal.ui.keyhandler = {
 
 	extractMoveOrFaceActionFromEvent: function(event) {
 		if (event.ctrlKey) {
+			event.preventDefault(); // don't move chat input caret
 			return "face";
 		} else if (event.shiftKey) {
 			return null;
