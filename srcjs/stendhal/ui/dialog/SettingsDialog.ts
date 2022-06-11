5d675063e8a0400561035577d5d8a73ec0156155
Jordan Irwin <antumdeluge@gmail.com>
2022-06-10 20:53:00 -0700

    Simplify making settings dialog check boxes

--- srcjs/stendhal/ui/dialog/SettingsDialog.ts
+++ srcjs/stendhal/ui/dialog/SettingsDialog.ts
@@ -35,114 +35,53 @@ export class SettingsDialog extends DialogContentComponent {
 
 		/* *** left panel *** */
 
-		const chk_light = this.createCheckBox("chk_light")!;
-		chk_light.checked = stendhal.config.getBoolean("gamescreen.lighting");
-		const tt_light = new CheckTooltip("Lighting effects are enabled",
-				"Lighting effects are disabled");
-		chk_light.parentElement!.title = tt_light.getValue(chk_light.checked);
-		chk_light.addEventListener("change", (e) => {
-			stendhal.config.set("gamescreen.lighting", chk_light.checked);
-			chk_light.parentElement!.title = tt_light.getValue(chk_light.checked);
-		});
-
-		// lighting effects not yet supported
+		const chk_light = this.createCheckBox("chk_light", "gamescreen.lighting",
+				"Lighting effects are enabled", "Lighting effects are disabled")!;
+		// FIXME: lighting effects not yet supported
 		chk_light.disabled = true;
 		chk_light.parentElement!.title = "Lighting effects not currently supported";
 
-		const chk_weather = this.createCheckBox("chk_weather")!;
-		chk_weather.checked = stendhal.config.getBoolean("gamescreen.weather");
-		const tt_weather = new CheckTooltip("Weather is enabled",
-				"Weather is disabled");
-		chk_weather.parentElement!.title = tt_weather.getValue(chk_weather.checked);
-		chk_weather.addEventListener("change", (e) => {
-			stendhal.config.set("gamescreen.weather", chk_weather.checked);
-			chk_weather.parentElement!.title = tt_weather.getValue(chk_weather.checked);
-		});
-
-		// weather effects not yet supported
+		const chk_weather = this.createCheckBox("chk_weather", "gamescreen.weather",
+				"Weather is enabled", "Weather is disabled")!;
+		// FIXME: weather effects not yet supported
 		chk_weather.disabled = true;
 		chk_weather.parentElement!.title = "Weather effects not currently supported";
 
-		const chk_blood = this.createCheckBox("chk_blood")!;
-		chk_blood.checked = stendhal.config.getBoolean("gamescreen.blood");
-		const tt_blood = new CheckTooltip("Gory images are enabled",
-				"Gory images are disabled");
-		chk_blood.parentElement!.title = tt_blood.getValue(chk_blood.checked);
-		chk_blood.addEventListener("change", (e) => {
-			stendhal.config.set("gamescreen.blood", chk_blood.checked);
-			chk_blood.parentElement!.title = tt_blood.getValue(chk_blood.checked);
-			this.refresh();
-		});
+		const sd = this;
+		this.createCheckBox("chk_blood", "gamescreen.blood",
+				"Gory images are enabled", "Gory images are disabled");
 
-		const chk_nonude = this.createCheckBox("chk_nonude")!;
-		chk_nonude.checked = stendhal.config.getBoolean("gamescreen.nonude");
-		const tt_nonude = new CheckTooltip("Naked entities have undergarments",
-				"Naked entities are not covered");
-		chk_nonude.parentElement!.title = tt_nonude.getValue(chk_nonude.checked);
-		chk_nonude.addEventListener("change", (e) => {
-			stendhal.config.set("gamescreen.nonude", chk_nonude.checked);
-			chk_nonude.parentElement!.title = tt_nonude.getValue(chk_nonude.checked);
-		});
+		this.createCheckBox("chk_nonude", "gamescreen.nonude",
+				"Naked entities have undergarments", "Naked entities are not covered");
 
-		const chk_shadows = this.createCheckBox("chk_shadows")!;
-		chk_shadows.checked = stendhal.config.getBoolean("gamescreen.shadows");
-		const tt_shadows = new CheckTooltip("Shadows are enabled",
-				"Shadows are disabled");
-		chk_shadows.parentElement!.title = tt_shadows.getValue(chk_shadows.checked);
-		chk_shadows.addEventListener("change", (e) => {
-			stendhal.config.set("gamescreen.shadows", chk_shadows.checked);
-			chk_shadows.parentElement!.title = tt_shadows.getValue(chk_shadows.checked);
-		});
+		this.createCheckBox("chk_shadows", "gamescreen.shadows",
+				"Shadows are enabled", "Shadows are disabled");
 
-		const chk_speechcr = this.createCheckBox("chk_speechcr");
-		chk_speechcr.checked = stendhal.config.getBoolean("gamescreen.speech.creature");
-		const tt_speechcr = new CheckTooltip("Creature speech bubbles are enabled",
-				"Creature speech bubbles are disabled");
-		chk_speechcr.parentElement!.title = tt_speechcr.getValue(chk_speechcr.checked);
-		chk_speechcr.addEventListener("change", (e) => {
-			stendhal.config.set("gamescreen.speech.creature", chk_speechcr.checked);
-			chk_speechcr.parentElement!.title = tt_speechcr.getValue(chk_speechcr.checked);
-		});
+		this.createCheckBox("chk_speechcr", "gamescreen.speech.creature",
+				"Creature speech bubbles are enabled", "Creature speech bubbles are disabled");
 
 
 		/* *** center panel *** */
 
-		const chk_dblclick = this.createCheckBox("chk_dblclick")!;
-		chk_dblclick.checked = stendhal.config.getBoolean("action.item.doubleclick");
-		const tt_dblclick = new CheckTooltip("Items are used/consumed with double click/touch",
+		this.createCheckBox("chk_dblclick", "action.item.doubleclick",
+				"Items are used/consumed with double click/touch",
 				"Items are used/consumed with single click/touch");
-		chk_dblclick.parentElement!.title = tt_dblclick.getValue(chk_dblclick.checked);
-		chk_dblclick.addEventListener("change", (e) => {
-			stendhal.config.set("action.item.doubleclick", chk_dblclick.checked);
-			chk_dblclick.parentElement!.title = tt_dblclick.getValue(chk_dblclick.checked);
-		});
 
 		// FIXME: open chest windows are not refreshed
-		const chk_chestqp = this.createCheckBox("chk_chestqp")!;
-		chk_chestqp.checked = stendhal.config.getBoolean("action.chest.quickpickup");
-		const tt_chestqp = new CheckTooltip("Click to tranfer items from chest",
+		this.createCheckBox("chk_chestqp", "action.chest.quickpickup",
+				"Click tranfers items from chests to player inventory",
 				"Click executes default action on items in chests");
-		chk_chestqp.parentElement!.title = tt_chestqp.getValue(chk_chestqp.checked);
-		chk_chestqp.addEventListener("change", (e) => {
-			stendhal.config.set("action.chest.quickpickup", chk_chestqp.checked);
-			chk_chestqp.parentElement!.title = tt_chestqp.getValue(chk_chestqp.checked);
-		});
 
-		const chk_movecont = this.createCheckBox("chk_movecont")!;
-		chk_movecont.checked = stendhal.config.getBoolean("input.movecont");
-		const tt_movecont = new CheckTooltip("Player will continue to walk after changing areas",
-				"Player will stop after changing areas");
-		chk_movecont.parentElement!.title = tt_movecont.getValue(chk_movecont.checked);
-		chk_movecont.addEventListener("change", (e) => {
-			stendhal.config.set("input.movecont", chk_movecont.checked);
-			chk_movecont.parentElement!.title = tt_movecont.getValue(chk_movecont.checked);
-
-			const action = {"type": "move.continuous"} as {[index: string]: string;};
-			if (chk_movecont.checked) {
-				action["move.continuous"] = "";
-			}
-			marauroa.clientFramework.sendAction(action);
-		});
+		const chk_movecont = this.createCheckBox("chk_movecont", "input.movecont",
+				"Player will continue to walk after changing areas",
+				"Player will stop after changing areas",
+				function() {
+					const action = {"type": "move.continuous"} as {[index: string]: string;};
+					if (chk_movecont.checked) {
+						action["move.continuous"] = "";
+					}
+					marauroa.clientFramework.sendAction(action);
+				})!;
 
 
 		/* *** right panel *** */
@@ -250,7 +189,7 @@ export class SettingsDialog extends DialogContentComponent {
 	 * @return
 	 *     HTMLInputElement.
 	 */
-	private createCheckBox(id: string, tooltip?: string): HTMLInputElement {
+	private createCheckBoxSkel(id: string, tooltip?: string): HTMLInputElement {
 		const checkbox = <HTMLInputElement> this.componentElement.querySelector(
 			"input[type=checkbox][id=" + id + "]")!;
 		if (tooltip) {
@@ -258,6 +197,40 @@ export class SettingsDialog extends DialogContentComponent {
 		}
 
 		return checkbox;
+	}
+
+	/**
+	 * Creates a fully function checkbox element.
+	 *
+	 * @param id
+	 *     Identifier of element to retrieve.
+	 * @param setid
+	 *     Identifier of associated configuration setting.
+	 * @param ttpos
+	 *     Tooltip to display when setting is enabled.
+	 * @param ttneg
+	 *     Tooltip to display when setting is disabled.
+	 * @return
+	 *     HTMLInputElement.
+	 */
+	private createCheckBox(id: string, setid: string, ttpos: string="",
+			ttneg: string="", actions?: Function): HTMLInputElement {
+
+		const chk = this.createCheckBoxSkel(id)!;
+		chk.checked = stendhal.config.getBoolean(setid);
+		const tt = new CheckTooltip(ttpos, ttneg);
+		chk.parentElement!.title = tt.getValue(chk.checked);
+		chk.addEventListener("change", (e) => {
+			stendhal.config.set(setid, chk.checked);
+			chk.parentElement!.title = tt.getValue(chk.checked);
+			this.refresh();
+
+			if (actions) {
+				actions();
+			}
+		});
+
+		return chk;
 	}
 
 	/**
