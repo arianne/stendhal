694340803e88c7c2c7099a60af46d3c06f863810
Jordan Irwin <antumdeluge@gmail.com>
2022-10-24 20:44:58 -0700

    Fix bug setting creature DEF to 10 during combat

--- src/games/stendhal/server/core/rp/StendhalRPAction.java
+++ src/games/stendhal/server/core/rp/StendhalRPAction.java
@@ -362,7 +362,12 @@ public class StendhalRPAction {
 			final boolean didDamage = damage > 0;
 
 			// give xp even if attack was blocked
-			if (defender.getsDefXpFrom(player)) {
+			/* FIXME: if we don't check if instance is Player then creature's
+			 *        DEF gets set to 10 due to change in def xp. This check
+			 *        is supposed to be done in CombatEntity but is not
+			 *        working for some reason.
+			 */
+			if ((defender instanceof Player) && defender.getsDefXpFrom(player)) {
 				defender.incDefXP();
 			}
 			if (player.getsAtkXpFrom(defender)) {
