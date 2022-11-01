174cc76f603d3d758fb69fabb161148f1c906fd6
Jordan Irwin <antumdeluge@gmail.com>
2022-10-31 17:03:28 -0700

    New NPC Elias Breland (WIP) for An Old Man's Wish quest
    - https://github.com/arianne/stendhal/issues/465
    - https://stendhalgame.org/wiki/Stendhal_Quest_Ideas/An_Old_Man%27s_Wish

---
+++ src/games/stendhal/server/maps/deniran/cityinterior/brelandhouse/OldManNPC.java
@@ -0,0 +1,38 @@
+/***************************************************************************
+ *                    Copyright © 2003-2022 - Arianne                      *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package games.stendhal.server.maps.deniran.cityinterior.brelandhouse;
+
+import java.util.Map;
+
+import games.stendhal.common.Direction;
+import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.StendhalRPZone;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+
+
+public class OldManNPC implements ZoneConfigurator  {
+
+	@Override
+	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
+		buildNPC(zone);
+	}
+
+	private void buildNPC(final StendhalRPZone zone) {
+		final SpeakerNPC elias = new SpeakerNPC("Elias Breland");
+
+		elias.setPosition(8, 15);
+		elias.setIdleDirection(Direction.DOWN);
+		elias.setOutfit("body=986,head=996,eyes=999");
+
+		zone.add(elias);
+	}
+}
