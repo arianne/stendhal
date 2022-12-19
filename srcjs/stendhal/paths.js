ad1c4363451cfc3e8201dc937a8f56ec378c8cf5
Jordan Irwin <antumdeluge@gmail.com>
2022-12-19 11:56:47 -0800

    Create standard paths file for webclient

---
+++ srcjs/stendhal/paths.js
@@ -0,0 +1,22 @@
+/***************************************************************************
+ *                    Copyright © 2003-2022 - Stendhal                     *
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Affero General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
+"use strict";
+
+var stendhal = window.stendhal = window.stendhal || {};
+
+
+stendhal.paths = { data: "/data" }
+stendhal.paths.font = stendhal.paths.data + "/font";
+stendhal.paths.gui = stendhal.paths.data + "/gui";
+stendhal.paths.music = stendhal.paths.data + "/music";
+stendhal.paths.sounds = stendhal.paths.data + "/sounds";
+stendhal.paths.sprites = stendhal.paths.data + "/sprites";
