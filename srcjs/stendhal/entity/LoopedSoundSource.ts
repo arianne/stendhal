0b2a9475d159db1f3db982676e93405a7fe0dfd2
Jordan Irwin <antumdeluge@gmail.com>
2022-12-24 06:55:19 -0800

    Webclient: Add code to start playing looped sound sources (WIP)
    https://github.com/arianne/stendhal/issues/254

--- srcjs/stendhal/entity/LoopedSoundSource.ts
+++ srcjs/stendhal/entity/LoopedSoundSource.ts
@@ -11,16 +11,51 @@
 
 import { InvisibleEntity } from "./InvisibleEntity";
 
-declare var marauroa: any;
+declare var stendhal: any;
 
 
 export class LoopedSoundSource extends InvisibleEntity {
 
-	// DEBUG:
-	constructor() {
-		super();
-		setTimeout(() => {
-			console.log("Looped sound source", "at", marauroa.currentZoneName, this["x"], this["y"], "is", this);
-		}, 1);
+	private audio?: HTMLAudioElement;
+	private loaded = false;
+
+
+	override set(key: string, value: object) {
+		super.set(key, value);
+
+		// FIXME:
+		// - better way to detect when entity is fully loaded?
+		// - does not get loaded at login if mauraroa.me isn't set
+		/* Disabled until looped sounds can cleaned up & volume scaled with player movement
+		if (!this.loaded) {
+			for (const prop of [this["x"], this["y"], this["sound"],
+					this["radius"], this["layer"], this["volume"]]) {
+				this.loaded = typeof(prop) !== "undefined";
+				if (!this.loaded) {
+					break;
+				}
+			}
+
+			if (this.loaded) {
+				this.onLoaded();
+			}
+		}
+		*/
+	}
+
+	/**
+	 * Plays sound once all required attributes are loaded.
+	 */
+	onLoaded() {
+		const snd = this["sound"];
+		if (snd && (!this.audio || snd !== this.audio.src)) {
+			const x = this["x"], y = this["y"];
+			if (typeof(x) !== "undefined" && typeof(y) !== "undefined") {
+				// start new audio
+				// FIXME: distinguish between music & looped sounds effects
+				this.audio = stendhal.ui.sound.playLocalizedMusic(x, y,
+						this["radius"], this["layer"], snd, this["volume"]);
+			}
+		}
 	}
 }
