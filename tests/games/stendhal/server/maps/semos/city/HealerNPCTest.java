16a3fac1c5b7d62691fef9b85cc1057a83b02290
Jordan Irwin <antumdeluge@gmail.com>
2023-01-23 10:33:06 -0800

    Clean up indentation

--- tests/games/stendhal/server/maps/semos/city/HealerNPCTest.java
+++ tests/games/stendhal/server/maps/semos/city/HealerNPCTest.java
@@ -1,6 +1,6 @@
 /* $Id$ */
 /***************************************************************************
- *                   (C) Copyright 2003-2010 - Stendhal                    *
+ *                   (C) Copyright 2003-2023 - Stendhal                    *
  ***************************************************************************
  ***************************************************************************
  *                                                                         *
@@ -38,7 +38,7 @@ import utilities.PlayerTestHelper;
 
 public class HealerNPCTest {
 
-    private SpeakerNPC npc;
+	private SpeakerNPC npc;
 	private Player player;
 	private Engine en;
 	private ShopList sl;
@@ -61,7 +61,7 @@ public class HealerNPCTest {
 		player = PlayerTestHelper.createPlayer("bob");
 		player.teleport(srpz, 10, 10, null, null);
 		sl = ShopList.get();
-        slh = (LinkedHashMap<String, Integer>) sl.get("healing");
+		slh = (LinkedHashMap<String, Integer>) sl.get("healing");
 	}
 
 	@Test
@@ -69,43 +69,43 @@ public class HealerNPCTest {
 		assertTrue(en.step(player, "hi"));
 		assertEquals("Hi, if I can #help, just say.", getReply(npc));
 		assertTrue(en.step(player, "job"));
-        assertEquals("My special powers help me to heal wounded people. I also sell potions and antidotes.", getReply(npc));
-        assertTrue(en.step(player, "help"));
-        assertEquals("I can #heal you here for free, or you can take one of my prepared medicines with you on your travels; just ask for an #offer.", getReply(npc));
+		assertEquals("My special powers help me to heal wounded people. I also sell potions and antidotes.", getReply(npc));
+		assertTrue(en.step(player, "help"));
+		assertEquals("I can #heal you here for free, or you can take one of my prepared medicines with you on your travels; just ask for an #offer.", getReply(npc));
 
-        final Collection<String> items = slh.keySet();
-        for(Map.Entry<String, Integer> it : slh.entrySet()) {
-        	final String key = it.getKey();
-        	final int price = it.getValue();
+		final Collection<String> items = slh.keySet();
+		for(Map.Entry<String, Integer> it : slh.entrySet()) {
+			final String key = it.getKey();
+			final int price = it.getValue();
 
-	        assertTrue(en.step(player, "offer"));
-	        assertEquals("I sell "+ Grammar.enumerateCollection(items)
+			assertTrue(en.step(player, "offer"));
+			assertEquals("I sell "+ Grammar.enumerateCollection(items)
 					+ ". "+"I can #heal you.", getReply(npc));
 
-	        player.setBaseHP(100);
-	        player.setHP(50);
-	        player.setAtkXP(100);
-	        player.setDefXP(100);
-	        PlayerTestHelper.equipWithMoney(player, price);
+			player.setBaseHP(100);
+			player.setHP(50);
+			player.setAtkXP(100);
+			player.setDefXP(100);
+			PlayerTestHelper.equipWithMoney(player, price);
 
-	        assertTrue(en.step(player, "heal"));
-	        assertEquals("There, you are healed. How else may I help you?", getReply(npc));
-	        assertEquals(player.getHP(),100);
+			assertTrue(en.step(player, "heal"));
+			assertEquals("There, you are healed. How else may I help you?", getReply(npc));
+			assertEquals(player.getHP(),100);
 
-	        //slh.get("antidote")
+			//slh.get("antidote")
 
-	        assertTrue(en.step(player, "buy "+key));
-	        final StringBuilder builder = new StringBuilder("");
+			assertTrue(en.step(player, "buy "+key));
+			final StringBuilder builder = new StringBuilder("");
 			builder.append(Grammar.quantityplnoun(1, key, "A"));
-		    builder.append(" will cost ");
-		    builder.append(price);
-		    builder.append(". Do you want to buy it?");
-	        assertEquals(builder.toString(), getReply(npc));
-	        assertTrue(en.step(player, "no"));
-	        assertEquals("Ok, how else may I help you?", getReply(npc));
-        }
+			builder.append(" will cost ");
+			builder.append(price);
+			builder.append(". Do you want to buy it?");
+			assertEquals(builder.toString(), getReply(npc));
+			assertTrue(en.step(player, "no"));
+			assertEquals("Ok, how else may I help you?", getReply(npc));
+		}
 
-        assertTrue(en.step(player, "!me hugs Carmen"));
+		assertTrue(en.step(player, "!me hugs Carmen"));
 		assertEquals("!me hugs bob", getReply(npc));
 		assertTrue(en.step(player, "bye"));
 		assertEquals("Bye.", getReply(npc));
