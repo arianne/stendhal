fb7efbd9632491bcc9aea295c2cf6578b5bb7c19
Jordan Irwin <antumdeluge@gmail.com>
2023-01-26 23:27:06 -0800

    Move obtain achievement tests

--- tests/games/stendhal/server/core/rp/achievement/factory/ObtainAchievementsFactoryTest.java
+++ tests/games/stendhal/server/core/rp/achievement/factory/ObtainAchievementsFactoryTest.java
@@ -1,5 +1,5 @@
 /***************************************************************************
- *                     Copyright © 2020 - Arianne                          *
+ *                    Copyright © 2020-2023 - Arianne                      *
  ***************************************************************************
  ***************************************************************************
  *                                                                         *
@@ -9,91 +9,24 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
-package games.stendhal.server.core.rp.achievement.obtain;
+package games.stendhal.server.core.rp.achievement.factory;
 
-import static games.stendhal.server.core.rp.achievement.factory.ObtainAchievementsFactory.ID_APPLES;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static utilities.PlayerTestHelper.createPlayer;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import games.stendhal.server.entity.player.Player;
-import games.stendhal.server.maps.MockStendlRPWorld;
-import marauroa.server.game.db.DatabaseFactory;
 import utilities.AchievementTestHelper;
-import utilities.PlayerTestHelper;
 
 
-public class BobbingForApplesAchievementTest {
+public class ObtainAchievementsFactoryTest extends AchievementTestHelper {
 
 	private Player player;
-
-	private final String item = "apple";
-	private final int reqCount = 1000;
-
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		new DatabaseFactory().initializeDatabase();
-		// initialize world
-		MockStendlRPWorld.get();
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		PlayerTestHelper.removeAllPlayers();
-	}
-
-	@Test
-	public void init() {
-		resetPlayer();
-		testAchievement();
-	}
-
-	private void testAchievement() {
-		player.incLootForItem(item, reqCount - 1);
-		assertEquals(reqCount - 1, player.getNumberOfLootsForItem(item));
-		assertFalse(achievementReached());
-		player.incLootForItem(item, 1);
-		assertEquals(reqCount, player.getNumberOfLootsForItem(item));
-		assertTrue(achievementReached());
-
-		resetPlayer();
-
-		player.incHarvestedForItem(item, reqCount - 1);
-		assertEquals(reqCount - 1, player.getQuantityOfHarvestedItems(item));
-		assertFalse(achievementReached());
-		player.incHarvestedForItem(item, 1);
-		assertEquals(reqCount, player.getQuantityOfHarvestedItems(item));
-		assertTrue(achievementReached());
-
-		resetPlayer();
-
-		final int halfCount = reqCount / 2;
-
-		player.incLootForItem(item, halfCount);
-		player.incHarvestedForItem(item, halfCount - 1);
-		assertFalse(achievementReached());
-		player.incHarvestedForItem(item, 1);
-		assertEquals(halfCount, player.getNumberOfLootsForItem(item));
-		assertEquals(halfCount, player.getQuantityOfHarvestedItems(item));
-		assertTrue(achievementReached());
-
-		resetPlayer();
-
-		player.incLootForItem(item, halfCount - 1);
-		player.incHarvestedForItem(item, halfCount);
-		assertFalse(achievementReached());
-		player.incLootForItem(item, 1);
-		assertEquals(halfCount, player.getNumberOfLootsForItem(item));
-		assertEquals(halfCount, player.getQuantityOfHarvestedItems(item));
-		assertTrue(achievementReached());
-	}
 
 
 	/**
@@ -102,23 +35,59 @@ public class BobbingForApplesAchievementTest {
 	private void resetPlayer() {
 		player = null;
 		assertNull(player);
-		player = PlayerTestHelper.createPlayer("player");
+		player = createPlayer("player");
 		assertNotNull(player);
 
-		assertEquals(0, player.getNumberOfLootsForItem(item));
-		assertEquals(0, player.getQuantityOfHarvestedItems(item));
+		assertEquals(0, player.getNumberOfLootsForItem("apple"));
+		assertEquals(0, player.getQuantityOfHarvestedItems("apple"));
 
-		AchievementTestHelper.init(player);
-		assertFalse(achievementReached());
+		init(player);
+		assertFalse(achievementReached(player, ObtainAchievementsFactory.ID_APPLES));
 	}
 
-	/**
-	 * Checks if the player has reached the achievement.
-	 *
-	 * @return
-	 * 		<code>true</player> if the player has the achievement.
-	 */
-	private boolean achievementReached() {
-		return AchievementTestHelper.achievementReached(player, ID_APPLES);
+	@Test
+	public void testBobbingForApples() {
+		final String item = "apple";
+		final int reqCount = 1000;
+
+		resetPlayer();
+
+		player.incLootForItem(item, reqCount - 1);
+		assertEquals(reqCount - 1, player.getNumberOfLootsForItem(item));
+		assertFalse(achievementReached(player, ObtainAchievementsFactory.ID_APPLES));
+		player.incLootForItem(item, 1);
+		assertEquals(reqCount, player.getNumberOfLootsForItem(item));
+		assertTrue(achievementReached(player, ObtainAchievementsFactory.ID_APPLES));
+
+		resetPlayer();
+
+		player.incHarvestedForItem(item, reqCount - 1);
+		assertEquals(reqCount - 1, player.getQuantityOfHarvestedItems(item));
+		assertFalse(achievementReached(player, ObtainAchievementsFactory.ID_APPLES));
+		player.incHarvestedForItem(item, 1);
+		assertEquals(reqCount, player.getQuantityOfHarvestedItems(item));
+		assertTrue(achievementReached(player, ObtainAchievementsFactory.ID_APPLES));
+
+		resetPlayer();
+
+		final int halfCount = reqCount / 2;
+
+		player.incLootForItem(item, halfCount);
+		player.incHarvestedForItem(item, halfCount - 1);
+		assertFalse(achievementReached(player, ObtainAchievementsFactory.ID_APPLES));
+		player.incHarvestedForItem(item, 1);
+		assertEquals(halfCount, player.getNumberOfLootsForItem(item));
+		assertEquals(halfCount, player.getQuantityOfHarvestedItems(item));
+		assertTrue(achievementReached(player, ObtainAchievementsFactory.ID_APPLES));
+
+		resetPlayer();
+
+		player.incLootForItem(item, halfCount - 1);
+		player.incHarvestedForItem(item, halfCount);
+		assertFalse(achievementReached(player, ObtainAchievementsFactory.ID_APPLES));
+		player.incLootForItem(item, 1);
+		assertEquals(halfCount, player.getNumberOfLootsForItem(item));
+		assertEquals(halfCount, player.getQuantityOfHarvestedItems(item));
+		assertTrue(achievementReached(player, ObtainAchievementsFactory.ID_APPLES));
 	}
 }
