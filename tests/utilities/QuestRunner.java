b20c4bc14f3dd0c06b70d2e0ee0b982769f2d83c
Jordan Irwin <antumdeluge@gmail.com>
2023-01-29 21:58:20 -0800

    Helper class for cycling quests in tests

---
+++ tests/utilities/QuestRunner.java
@@ -0,0 +1,58 @@
+/***************************************************************************
+ *                     Copyright © 2023 - Arianne                          *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package utilities;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static utilities.PlayerTestHelper.equipWithStackableItem;
+import static utilities.SpeakerNPCTestHelper.getSpeakerNPC;
+
+import games.stendhal.server.entity.npc.ConversationStates;
+import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.fsm.Engine;
+import games.stendhal.server.entity.player.Player;
+
+
+public class QuestRunner {
+
+	public static void doQuestRestockFlowerShop(final Player player) {
+		final String questSlot = "restock_flowershop";
+		if (player.getQuest(questSlot, 1) != null) {
+			// reset timestamp to do quest again
+			player.setQuest(questSlot, 1, "0");
+		}
+		final SpeakerNPC seremela = getSpeakerNPC("Seremela");
+		assertNotNull(seremela);
+		final Engine en = seremela.getEngine();
+		assertEquals(ConversationStates.IDLE, en.getCurrentState());
+		en.step(player, "hi");
+		en.step(player, "quest");
+		en.step(player, "yes");
+		assertEquals("start", player.getQuest(questSlot, 0));
+		en.step(player, "bye");
+		en.step(player, "hi");
+		for (final String s: player.getQuest(questSlot).split(";")) {
+			if (!s.contains("=")) {
+				continue;
+			}
+			final String[] item = s.split("=");
+			final String name = item[0];
+			final int quant = Integer.parseInt(item[1]);
+			equipWithStackableItem(player, name, quant);
+			assertEquals(quant, player.getNumberOfEquipped(name));
+			en.step(player, name);
+			assertEquals(0, player.getNumberOfEquipped(name));
+		}
+		assertEquals("done", player.getQuest(questSlot, 0));
+		en.step(player, "bye");
+	}
+}
