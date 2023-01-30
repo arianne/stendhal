9e2310cbb6b6fb5fc9158f871f9454089aa1b6de
Jordan Irwin <antumdeluge@gmail.com>
2023-01-29 21:43:44 -0800

    Support loading & unloading quests from QuestHelper

--- tests/utilities/QuestHelper.java
+++ tests/utilities/QuestHelper.java
@@ -1,6 +1,6 @@
 /* $Id$ */
 /***************************************************************************
- *                   (C) Copyright 2003-2010 - Stendhal                    *
+ *                   (C) Copyright 2003-2023 - Stendhal                    *
  ***************************************************************************
  ***************************************************************************
  *                                                                         *
@@ -12,12 +12,15 @@
  ***************************************************************************/
 package utilities;
 
+import java.util.List;
+
 import org.junit.BeforeClass;
 
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.rp.StendhalQuestSystem;
 import games.stendhal.server.maps.MockStendhalRPRuleProcessor;
 import games.stendhal.server.maps.MockStendlRPWorld;
+import games.stendhal.server.maps.quests.IQuest;
 import marauroa.common.Log4J;
 import marauroa.server.game.db.DatabaseFactory;
 import utilities.RPClass.ItemTestHelper;
@@ -29,7 +32,7 @@ import utilities.RPClass.ItemTestHelper;
  */
 public abstract class QuestHelper extends PlayerTestHelper  {
 
-	protected static final StendhalQuestSystem quests = StendhalQuestSystem.get();
+	protected static final StendhalQuestSystem quests = SingletonRepository.getStendhalQuestSystem();
 
 
 	@BeforeClass
@@ -48,4 +51,38 @@ public abstract class QuestHelper extends PlayerTestHelper  {
 		SingletonRepository.getNPCList().clear();
 	}
 
+	public static void loadQuests(final IQuest... qs) {
+		for (final IQuest q: qs) {
+			quests.loadQuest(q);
+		}
+	}
+
+	public static void unloadQuests(final IQuest... qs) {
+		for (final IQuest q: qs) {
+			quests.unloadQuest(q);
+		}
+	}
+
+	public static void unloadQuests(final String... slots) {
+		for (final String slot: slots) {
+			quests.unloadQuestSlot(slot);
+		}
+	}
+
+	/**
+	 * Unloads all loaded quests.
+	 */
+	public static void unloadQuests() {
+		for (final String slot: quests.getLoadedSlots()) {
+			quests.unloadQuestSlot(slot);
+		}
+	}
+
+	public static List<String> getLoadedSlots() {
+		return quests.getLoadedSlots();
+	}
+
+	public static boolean isLoaded(final IQuest q) {
+		return quests.isLoaded(q);
+	}
 }
