bdb519869445d4275b77adc1b81f7488edb51080
Jordan Irwin <antumdeluge@gmail.com>
2023-01-31 08:10:04 -0800

    Add support for loading all quests in a region in quest test helper

--- tests/utilities/QuestHelper.java
+++ tests/utilities/QuestHelper.java
@@ -12,7 +12,14 @@
  ***************************************************************************/
 package utilities;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import org.apache.log4j.Logger;
 
 import org.junit.BeforeClass;
 
@@ -20,6 +27,7 @@ import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.rp.StendhalQuestSystem;
 import games.stendhal.server.maps.MockStendhalRPRuleProcessor;
 import games.stendhal.server.maps.MockStendlRPWorld;
+import games.stendhal.server.maps.Region;
 import games.stendhal.server.maps.quests.IQuest;
 import marauroa.common.Log4J;
 import marauroa.server.game.db.DatabaseFactory;
@@ -31,6 +39,8 @@ import utilities.RPClass.ItemTestHelper;
  * @author hendrik
  */
 public abstract class QuestHelper extends PlayerTestHelper  {
+
+	private static final Logger logger = Logger.getLogger(QuestHelper.class);
 
 	protected static final StendhalQuestSystem quests = SingletonRepository.getStendhalQuestSystem();
 
@@ -54,10 +64,56 @@ public abstract class QuestHelper extends PlayerTestHelper  {
 	/**
 	 * Loads quest instances.
 	 */
-	public static void loadQuests(final IQuest... qs) {
+	public static void loadQuests(final List<IQuest> qs) {
 		for (final IQuest q: qs) {
 			quests.loadQuest(q);
 		}
+	}
+
+	/**
+	 * Loads quest instances.
+	 */
+	public static void loadQuests(final IQuest... qs) {
+		loadQuests(Arrays.asList(qs));
+	}
+
+	/**
+	 * Loads quests for a region.
+	 *
+	 * @param region
+	 *     Region identifier.
+	 * @return
+	 *     List of quests loaded for that region.
+	 */
+	public static List<IQuest> loadRegionalQuests(final String region) {
+		final List<IQuest> loaded = new ArrayList<>();
+		final String packagename = IQuest.class.getPackage().getName();
+		// this only works for non-jar packaged resources
+		final URL dirquests = ClassLoader.getSystemClassLoader().getResource(packagename.replace(".", "/"));
+		List<String> excludes = Arrays.asList("IQuest.class", "AbstractQuest.class", "package-info.class");
+		final String[] filenames = new File(dirquests.getFile()).list(new FilenameFilter() {
+			public boolean accept(final File dir, final String name) {
+				return name.endsWith(".class") && name.indexOf("$") < 0 && !excludes.contains(name);
+			}
+		});
+		for (String f: filenames) {
+			f = packagename + "." + f.substring(0, f.indexOf(".class"));
+			Object obj = null;
+			try {
+				obj = Class.forName(f).getConstructor().newInstance();
+			} catch (final Exception e) {
+				logger.error(e, e);
+			}
+			if (!(obj instanceof IQuest)) {
+				continue;
+			}
+			final IQuest q = (IQuest) obj;
+			if (region.equals(q.getRegion())) {
+				loaded.add(q);
+			}
+		}
+		loadQuests(loaded);
+		return loaded;
 	}
 
 	/**
@@ -104,11 +160,29 @@ public abstract class QuestHelper extends PlayerTestHelper  {
 	}
 
 	/**
-	 * Checks if quest instances are loaded.
+	 * Checks if quests are loaded using slot ID.
+	 *
+	 * @param qs
+	 *     Quest instances to be checked.
 	 */
 	public static boolean isLoaded(final IQuest... qs) {
 		for (final IQuest q: qs) {
-			if (!quests.isLoaded(q)) {
+			if (!quests.isLoadedSlot(q.getSlotName())) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	/**
+	 * Checks if quests are loaded using slot ID.
+	 *
+	 * @param slots
+	 *     Slot IDs to be checked.
+	 */
+	public static boolean isLoaded(final String... slots) {
+		for (final String slot: slots) {
+			if (!quests.isLoadedSlot(slot)) {
 				return false;
 			}
 		}
