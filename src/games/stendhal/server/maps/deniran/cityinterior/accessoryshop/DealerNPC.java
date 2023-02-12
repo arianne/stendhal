c261c38ec6e41e784a57ad31b1c15c5f07bbfbe9
Jordan Irwin <antumdeluge@gmail.com>
2023-02-12 02:42:49 -0800

    Create shop sign for Gwen

--- src/games/stendhal/server/maps/deniran/cityinterior/accessoryshop/DealerNPC.java
+++ src/games/stendhal/server/maps/deniran/cityinterior/accessoryshop/DealerNPC.java
@@ -11,16 +11,20 @@
 package games.stendhal.server.maps.deniran.cityinterior.accessoryshop;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 
 import games.stendhal.common.Direction;
 import games.stendhal.common.MathHelper;
 import games.stendhal.common.constants.SkinColor;
 import games.stendhal.server.core.config.ZoneConfigurator;
+import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.core.engine.StendhalRPZone;
 import games.stendhal.server.core.pathfinder.FixedPath;
 import games.stendhal.server.core.pathfinder.Node;
+import games.stendhal.server.entity.mapstuff.sign.OutfitShopSign;
 import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.shop.OutfitShopInventory;
 
 
 /**
@@ -31,6 +35,7 @@ public class DealerNPC implements ZoneConfigurator {
 	@Override
 	public void configureZone(final StendhalRPZone zone, final Map<String, String> attributes) {
 		zone.add(buildNPC());
+		zone.add(buildSign());
 	}
 
 	private SpeakerNPC buildNPC() {
@@ -59,5 +64,21 @@ public class DealerNPC implements ZoneConfigurator {
 		gwen.addSuspend(MathHelper.TURNS_IN_ONE_MINUTE / 2, Direction.DOWN, 4);
 
 		return gwen;
+	}
+
+	private OutfitShopSign buildSign() {
+		// TODO: move to XML
+		final OutfitShopInventory inventory = SingletonRepository.getOutfitShopsList().get("deniran_accessories");
+		final Map<String, String> addLayers = new HashMap<>();
+		if (inventory != null) {
+			for (final String name: inventory.keySet()) {
+				addLayers.put(name, "body=0,head=0,eyes=0,dress=11,hair=11");
+			}
+		}
+		final OutfitShopSign sign = new OutfitShopSign("deniran_accessories", "Deniran Accessories",
+				"Gwen sells the following accessories", addLayers, true);
+		sign.setPosition(12, 2);
+		sign.setEntityClass("book_purple");
+		return sign;
 	}
 }
