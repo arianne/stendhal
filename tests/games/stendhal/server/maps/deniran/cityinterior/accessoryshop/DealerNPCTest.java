a3475050de647f6be25d323368d5909119cc10f5
Jordan Irwin <antumdeluge@gmail.com>
2023-02-12 12:53:10 -0800

    Test outfits in Gwen's shop

--- tests/games/stendhal/server/maps/deniran/cityinterior/accessoryshop/DealerNPCTest.java
+++ tests/games/stendhal/server/maps/deniran/cityinterior/accessoryshop/DealerNPCTest.java
@@ -11,15 +11,24 @@
 package games.stendhal.server.maps.deniran.cityinterior.accessoryshop;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static utilities.PlayerTestHelper.createPlayerWithOutFit;
+import static utilities.PlayerTestHelper.equipWithStackableItem;
 import static utilities.ZoneAndPlayerTestImpl.setupZone;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import games.stendhal.server.actions.RemoveDetailAction;
 import games.stendhal.server.core.engine.SingletonRepository;
+import games.stendhal.server.entity.Outfit;
 import games.stendhal.server.entity.npc.SpeakerNPC;
+import games.stendhal.server.entity.npc.condition.PlayerIsWearingOutfitCondition;
 import games.stendhal.server.entity.npc.fsm.Engine;
+import games.stendhal.server.entity.npc.shop.OutfitShopInventory;
+import games.stendhal.server.entity.npc.shop.OutfitShopsList;
 import games.stendhal.server.entity.player.Player;
 import utilities.AchievementTestHelper;
 import utilities.PlayerTestHelper;
@@ -28,23 +37,39 @@ import utilities.SpeakerNPCTestHelper;
 
 public class DealerNPCTest extends SpeakerNPCTestHelper {
 
+	private static final OutfitShopsList oshops = SingletonRepository.getOutfitShopsList();
+
+
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		AchievementTestHelper.setUpBeforeClass();
 		setupZone("testzone", new DealerNPC());
 	}
 
+	private boolean isWearingOutfit(final Player player, final Outfit outfit) {
+		return new PlayerIsWearingOutfitCondition(outfit).fire(player, null, null);
+	}
+
+	private void removeDetail(final Player player) {
+		new RemoveDetailAction().onAction(player, null);
+	}
+
 	@Test
 	public void init() {
-		final Player player = PlayerTestHelper.createPlayer("player");
+		final Player player = createPlayerWithOutFit("player");
 		assertNotNull(player);
 		final SpeakerNPC gwen = getSpeakerNPC("Gwen");
 		assertNotNull(gwen);
 
 		// configure Gwen's shop
-		SingletonRepository.getOutfitShopsList().configureNPC(gwen, "deniran_accessories", "buy", true,
-				false);
+		oshops.configureNPC(gwen, "deniran_accessories", "buy", false, false);
 
+		testOffer(player, gwen);
+		testInsufficientFunds(player, gwen);
+		testInventory(player, gwen);
+	}
+
+	private void testOffer(final Player player, final SpeakerNPC gwen) {
 		final Engine en = gwen.getEngine();
 		en.step(player, "hi");
 		en.step(player, "offer");
@@ -53,6 +78,48 @@ public class DealerNPCTest extends SpeakerNPCTestHelper {
 					//~ + " umbrella, and white balloon.",
 			"Please see the catalog on the chair for a list of accessories that I offer.",
 			getReply(gwen));
+		en.step(player, "bye");
+	}
+
+	private void testInsufficientFunds(final Player player, final SpeakerNPC gwen) {
+		final Outfit outfit = new Outfit("detail=2");
+		final Engine en = gwen.getEngine();
+
+		assertEquals(0, player.getNumberOfEquipped("money"));
+		en.step(player, "hi");
+		en.step(player, "buy white balloon");
+		assertEquals(
+			"To buy a white balloon will cost 50000. Do you want to buy it?",
+			getReply(gwen));
+		en.step(player, "yes");
+		assertEquals(
+			"Sorry, you don't have enough money!",
+			getReply(gwen));
+		assertFalse(isWearingOutfit(player, outfit));
+		en.step(player, "bye");
+	}
+
+	private void testInventory(final Player player, final SpeakerNPC gwen) {
+		final OutfitShopInventory inventory = oshops.get("deniran_accessories");
+		final Engine en = gwen.getEngine();
+		en.step(player, "hi");
+
+		for (final String itemname: inventory.keySet()) {
+			final Outfit outfit = inventory.getOutfit(itemname);
+			final int price = inventory.getPrice(itemname);
+
+			assertEquals(0, player.getNumberOfEquipped("money"));
+			assertFalse(isWearingOutfit(player, outfit));
+			equipWithStackableItem(player, "money", price);
+			assertEquals(price, player.getNumberOfEquipped("money"));
+			en.step(player, "buy " + itemname);
+			en.step(player, "yes");
+			assertTrue(isWearingOutfit(player, outfit));
+			assertEquals(0, player.getNumberOfEquipped("money"));
+			removeDetail(player);
+			assertFalse(isWearingOutfit(player, outfit));
+		}
+
 		en.step(player, "bye");
 	}
 }
