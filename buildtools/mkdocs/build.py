ac0a5efa0698defe80a752246eb5c8b042901dd1
Jordan Irwin <antumdeluge@gmail.com>
2023-09-28 10:46:55 -0700

    Add some notes & detailed output to MkDocs script

--- buildtools/mkdocs/build.py
+++ buildtools/mkdocs/build.py
@@ -9,37 +9,50 @@ from mkdocs.commands    import build
 from mkdocs.config.base import load_config
 
 
+# directory where main script & MkDocs config are located
 dir_mkdocs = os.path.dirname(__file__)
+# Stendhal source root directory
 dir_root = os.path.normpath(os.path.join(dir_mkdocs, "../../"))
-dir_docs = os.path.normpath(os.path.join(dir_root, "doc/mkdocs"))
+# documentation templates root directory
+dir_templates = os.path.normpath(os.path.join(dir_root, "doc/mkdocs"))
+# build output directory
 dir_build = os.path.join(dir_root, "build")
+# documentation output directory
 dir_out = os.path.join(dir_build, "mkdocs")
+# temporary directory for staging documentation output
 dir_stage = "{}_stage".format(dir_out)
+# Java documentation directory
 dir_javadocs = os.path.join(dir_build, "javadocs")
 
+# ensure working from source root directory
 os.chdir(dir_root)
 
-print("\nPreparing stage ...")
+print("\nPreparing documentation staging directory ...")
 
-# clean up old output directory
+# clean up old files
 shutil.rmtree(dir_out, True)
-# in case stage dir failed to delete previously
+# in case staging dir failed to delete previously
 shutil.rmtree(dir_stage, True)
 
-# copy files to stage
-shutil.copytree(dir_docs, dir_stage)
+# copy template files to staging directory
+shutil.copytree(dir_templates, dir_stage)
 
+# create directory for image resources
 dir_img = os.path.join(dir_stage, "img")
 os.makedirs(dir_img)
 shutil.copy(os.path.join(dir_mkdocs, "favicon.ico"), dir_img)
 
+# copy Java documentation if available
 if os.path.isdir(dir_javadocs):
-  print("\nCopying javadocs ...")
+  print("\nCopying javadocs to staging directory ...")
   shutil.copytree(dir_javadocs, os.path.join(dir_stage, "java"))
+else:
+  print("\nJavadocs directory not found, skipping ...")
 
+# change to MkDocs directory for execution
 os.chdir(dir_mkdocs)
 
-print("\nGenerating documentation with mkdocs ...")
+print("\nConfiguring MkDocs ...")
 
 # workaround to trick mkdocs so that "docs_dir" can be set here instead of in config
 if not os.path.isdir("docs"):
@@ -75,13 +88,13 @@ if "custom_theme" in config:
     print("WARNING: '{}' theme broken ({}: {}), falling back to '{}'".format(theme.name,
         e.__class__.__name__, e, config.theme.name))
 
+# generate HTML documentation
+print("\nGenerating HTML documentation ...")
 build.build(config)
 
 # clean up
+print("\nCleaning up ...")
 shutil.rmtree("docs", True)
+shutil.rmtree(dir_stage, True)
 
 os.chdir(dir_root)
-
-# clean up
-print("\nCleaning up ...")
-shutil.rmtree(dir_stage, True)
