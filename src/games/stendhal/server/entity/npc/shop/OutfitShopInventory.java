9f8d44f3279e0e2f3109d30be630b36ca290fc8f
Jordan Irwin <antumdeluge@gmail.com>
2023-11-09 03:12:15 -0800

    Clean up indentation

--- src/games/stendhal/server/entity/npc/shop/OutfitShopInventory.java
+++ src/games/stendhal/server/entity/npc/shop/OutfitShopInventory.java
@@ -28,78 +28,78 @@ public class OutfitShopInventory extends ShopInventory<String, Pair<String, Inte
 		super(shopType, name);
 	}
 
-  /**
-   * Adds an outfit to shop.
-   *
-   * @param name
-   *     String identifier.
-   * @param outfit
-   *     Outfit to be sold.
-   * @param price
-   *     Amount of money required to buy outfit.
-   */
-  public void put(final String name, final String outfit, final int price) {
-    put(name, new Pair<String, Integer>(outfit, price));
-  }
+	/**
+	 * Adds an outfit to shop.
+	 *
+	 * @param name
+	 *     String identifier.
+	 * @param outfit
+	 *     Outfit to be sold.
+	 * @param price
+	 *     Amount of money required to buy outfit.
+	 */
+	public void put(final String name, final String outfit, final int price) {
+		put(name, new Pair<String, Integer>(outfit, price));
+	}
 
-  /**
-   * Adds an outfit to shop.
-   *
-   * @param name
-   *     String identifier.
-   * @param outfit
-   *     Outfit to be sold.
-   * @param price
-   *     Amount of money required to buy outfit.
-   */
-  public void put(final String name, final Outfit outfit, final int price) {
-    put(name, outfit.toString(), price);
-  }
+	/**
+	 * Adds an outfit to shop.
+	 *
+	 * @param name
+	 *     String identifier.
+	 * @param outfit
+	 *     Outfit to be sold.
+	 * @param price
+	 *     Amount of money required to buy outfit.
+	 */
+	public void put(final String name, final Outfit outfit, final int price) {
+		put(name, outfit.toString(), price);
+	}
 
-  /**
-   * Retrieves a string representation of an outfit sold by shop.
-   *
-   * @param name
-   *     String identifier.
-   * @return
-   *     Outfit string or null if name not found.
-   */
-  public String getOutfitString(final String name) {
-    if (containsKey(name)) {
-      return get(name).first();
-    }
-    return null;
-  }
+	/**
+	 * Retrieves a string representation of an outfit sold by shop.
+	 *
+	 * @param name
+	 *     String identifier.
+	 * @return
+	 *     Outfit string or null if name not found.
+	 */
+	public String getOutfitString(final String name) {
+		if (containsKey(name)) {
+			return get(name).first();
+		}
+		return null;
+	}
 
-  /**
-   * Retrieves an outfit sold by shop.
-   *
-   * @param name
-   *     String identifier.
-   * @return
-   *     Outfit or null if name not found.
-   */
-  public Outfit getOutfit(final String name) {
-    final String ost = getOutfitString(name);
-    if (ost != null) {
-      return new Outfit(ost);
-    }
-    return null;
-  }
+	/**
+	 * Retrieves an outfit sold by shop.
+	 *
+	 * @param name
+	 *     String identifier.
+	 * @return
+	 *     Outfit or null if name not found.
+	 */
+	public Outfit getOutfit(final String name) {
+		final String ost = getOutfitString(name);
+		if (ost != null) {
+			return new Outfit(ost);
+		}
+		return null;
+	}
 
-  /**
-   * Retrieves the price of an outfit sold by shop.
-   *
-   * @param name
-   *     String identifier.
-   * @return
-   *     Amount of money required to buy outfit or null if name not found.
-   */
-  @Override
-  public Integer getPrice(final String name) {
-    if (containsKey(name)) {
-      return get(name).second();
-    }
-    return null;
-  }
+	/**
+	 * Retrieves the price of an outfit sold by shop.
+	 *
+	 * @param name
+	 *     String identifier.
+	 * @return
+	 *     Amount of money required to buy outfit or null if name not found.
+	 */
+	@Override
+	public Integer getPrice(final String name) {
+		if (containsKey(name)) {
+			return get(name).second();
+		}
+		return null;
+	}
 }
