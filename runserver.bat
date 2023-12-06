52b6f4f68d8277636d557e0fdeee871ae1178f13
Jordan Irwin <antumdeluge@gmail.com>
2023-12-06 14:36:26 -0800

    Update runserver scripts to work with current Stendhal

--- runserver.bat
+++ runserver.bat
@@ -1,4 +1,43 @@
+@echo off
+
 set STENDHAL_VERSION=1.44.5
-set LOCALCLASSPATH=.;data\script;data\conf;stendhal-server-%STENDHAL_VERSION%.jar;marauroa.jar;mysql-connector.jar;log4j.jar;commons-lang.jar;h2.jar
-java -Xmx400m -cp "%LOCALCLASSPATH%" games.stendhal.server.StendhalServer -c server.ini -l
+set SERVER_JAR=stendhal-server-%STENDHAL_VERSION%.jar
+
+:: change to server directory
+for %%F in ("%0") do set server_root=%%~dpF
+cd "%server_root%"
+
+:: parse arguments
+set vm_args=
+set app_args=
+setlocal EnableDelayedExpansion
+for %%A in (%*) do (
+	set arg=%%A
+	if ["-D"] == ["!arg:~0,2!"] (
+		if [] == [!vm_args!] (
+			call set vm_args=!arg!
+		) else (
+			call set vm_args=!vm_args! !arg!
+		)
+	) else (
+		if [] == [!app_args!] (
+			set app_args=!arg!
+		) else (
+			set app_args=!app_args! !arg!
+		)
+	)
+)
+
+if exist "%SERVER_JAR%" (
+	echo Executing server from .jar ...
+
+	java -Xmx400m %vm_args% -jar "%SERVER_JAR%" %app_args%
+) else (
+	echo Executing server from source root ...
+
+	set LOCALCLASSPATH=.;.\build\build_server;.\build\build_server_maps;.\build\build_server_script;.\build\build_server_xmlconf;.\libs\*;
+	java -Xmx400m -cp "%LOCALCLASSPATH%" %vm_args% games.stendhal.server.StendhalServer %app_args%
+)
+
+:end
 @pause
--- runserver.sh
+++ runserver.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
+
 STENDHAL_VERSION="1.44.5"
+SERVER_JAR="stendhal-server-${STENDHAL_VERSION}.jar"
 
-LOCALCLASSPATH=.:data/script/:data/conf/:stendhal-server-$STENDHAL_VERSION.jar:marauroa.jar:mysql-connector.jar:log4j.jar:commons-lang.jar:h2.jar
+# change to server directory
+cd "$(dirname $0)"
 
-java -Xmx400m -cp "${LOCALCLASSPATH}" games.stendhal.server.StendhalServer -c server.ini -l
+# parse arguments
+vm_args=()
+app_args=()
+for arg in $@; do
+	if [[ "${arg}" =~ ^-D ]]; then
+		vm_args+=("${arg}")
+	else
+		app_args+=("${arg}")
+	fi
+done
 
+if [ -f "${SERVER_JAR}" ]; then
+	echo "Executing server from .jar ..."
+
+	java -Xmx400m ${vm_args[@]} -jar "${SERVER_JAR}" ${app_args[@]}
+else
+	echo "Executing server from source root ..."
+
+	LOCALCLASSPATH=".:./libs/*:./build/build_server:./build/build_server_maps:./build/build_server_script:./build/build_server_xmlconf"
+	java -Xmx400m -cp "${LOCALCLASSPATH}" ${vm_args[@]} games.stendhal.server.StendhalServer ${app_args[@]}
+fi
