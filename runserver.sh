c30495455b90a48c02b9bd27718bfc753da1d46b
Cosmin Humeniuc <cosmin@hume.ro>
2023-12-28 20:32:12 +0200

    fix: robustness and portability of runserver.sh
    - use a Bash shebang instead of the one for sh,
      as the script is Bash, not POSIX sh
    - fix shellcheck warnings and errors:
      - SC2046 (warning): Quote to prevent word splitting.
      - SC2164 (warning): Use 'cd ... || exit' in case cd fails.
      - SC2068 (error): Double quote array expansions to avoid
        re-splitting elements.

--- runserver.sh
+++ runserver.sh
@@ -1,15 +1,15 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 STENDHAL_VERSION="1.45.5"
 SERVER_JAR="stendhal-server-${STENDHAL_VERSION}.jar"
 
 # change to server directory
-cd "$(dirname $0)"
+cd "$(dirname "$0")" || exit
 
 # parse arguments
 vm_args=()
 app_args=()
-for arg in $@; do
+for arg in "$@"; do
 	if [[ "${arg}" =~ ^-D ]]; then
 		vm_args+=("${arg}")
 	else
@@ -23,4 +23,4 @@ else
 	LOCALCLASSPATH=".:./build/build_server:./build/build_server_maps:./build/build_server_script:./build/build_server_xmlconf:./libs/*"
 fi
 
-java -Xmx400m -cp "${LOCALCLASSPATH}" ${vm_args[@]} games.stendhal.server.StendhalServer ${app_args[@]}
+java -Xmx400m -cp "${LOCALCLASSPATH}" "${vm_args[@]}" games.stendhal.server.StendhalServer "${app_args[@]}"
