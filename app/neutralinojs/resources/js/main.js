b51f58c334dd719952d39c585622f80e41450599
Hendrik Brummermann <nhnb@users.sourceforge.net>
2024-01-09 19:50:28 +0100

    handle situations in which nativehelper is not working

--- app/neutralinojs/resources/js/main.js
+++ app/neutralinojs/resources/js/main.js
@@ -2,6 +2,8 @@ const SERVER_ORIGIN = "https://stendhalgame.org";
 // const SERVER_ORIGIN = "http://localhost";
 const SERVER_PATH = "/account/login.html";
 
+let authenticated = false;
+
 const byteToHex = [];
 for (let n = 0; n <= 0xff; ++n) {
 	const hexOctet = n.toString(16).padStart(2, "0");
@@ -19,6 +21,7 @@ function hex(arrayBuffer) {
 
 function onSteamAuthToken(event) {
 	console.log("onSteamAuthToken");
+	authenticated = true;
 	let ticketString = hex(event.detail);
 	document.querySelector("form").action = SERVER_ORIGIN + SERVER_PATH + "?" + Date.now();
 	document.querySelector("#steam_auth_ticket").value = ticketString;
@@ -30,9 +33,30 @@ function onNoAuthToken(_event) {
 	window.location = SERVER_ORIGIN + SERVER_PATH + "?" + Date.now();
 }
 
+async function timeoutExtensionConnection() {
+	if (authenticated) {
+		return;
+	}
+	let stats = await Neutralino.extensions.getStats();
+	let connected = stats.connected.includes("nativehelper");
+	if (!connected) {
+		console.log("timeoutExtensionConnection");
+		onNoAuthToken();
+	}
+	setTimeout(() => timeoutExtensionAuthentication(), 10000);
+}
+
+async function timeoutExtensionAuthentication() {
+	if (authenticated) {
+		return;
+	}
+	console.log("timeoutExtensionAuthentication");
+	onNoAuthToken();
+}
+
 Neutralino.init();
+setTimeout(() => timeoutExtensionConnection(), 2000);
+
 Neutralino.events.on("steamAuthToken", onSteamAuthToken);
 Neutralino.events.on("noAuthToken", onNoAuthToken);
 Neutralino.extensions.dispatch('nativehelper', 'request_authentication');
-console.log("init complete");
-
