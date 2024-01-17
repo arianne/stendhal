780cc1bac062d93c20a41d1e524f3dacee84325e
Jordan Irwin <antumdeluge@gmail.com>
2024-01-16 21:53:32 -0800

    Add "patronage" to known chat options for NPCs associated with Community Supporter achievement

--- src/games/stendhal/server/core/rp/achievement/factory/CommerceAchievementFactory.java
+++ src/games/stendhal/server/core/rp/achievement/factory/CommerceAchievementFactory.java
@@ -1,5 +1,5 @@
 /***************************************************************************
- *                    Copyright © 2003-2023 - Arianne                      *
+ *                    Copyright © 2003-2024 - Arianne                      *
  ***************************************************************************
  ***************************************************************************
  *                                                                         *
@@ -154,6 +154,8 @@ public class CommerceAchievementFactory extends AbstractAchievementFactory {
 							csSellers = csSellers + ", ";
 						}
 						csSellers = csSellers + name;
+
+						seller.addKnownChatOptions("patronage");
 					} else {
 						logger.warn("Cannot set up NPC " + name
 							+ " for \"Community Supporter\" achievement");
