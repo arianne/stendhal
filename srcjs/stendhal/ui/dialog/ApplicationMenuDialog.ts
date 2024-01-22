bfb35f48e3bcd31435fb0a667489c9f88e1da67b
Jordan Irwin <antumdeluge@gmail.com>
2024-01-21 18:58:52 -0800

    Fix potential TypeError referencing SingletonRepo

--- srcjs/stendhal/ui/dialog/ApplicationMenuDialog.ts
+++ srcjs/stendhal/ui/dialog/ApplicationMenuDialog.ts
@@ -1,5 +1,5 @@
 /***************************************************************************
- *                (C) Copyright 2003-2023 - Faiumoni e. V.                 *
+ *                (C) Copyright 2003-2024 - Faiumoni e. V.                 *
  ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -14,8 +14,6 @@ declare var stendhal: any;
 import { DialogContentComponent } from "../toolkit/DialogContentComponent";
 import { singletons } from "../../SingletonRepo";
 
-
-const slashActions = singletons.getSlashActionRepo();
 
 export class ApplicationMenuDialog extends DialogContentComponent {
 
@@ -127,7 +125,7 @@ export class ApplicationMenuDialog extends DialogContentComponent {
 
 		var cmd = (event.target as HTMLInputElement).id?.substring(11);
 		if (cmd) {
-			slashActions.execute("/" + cmd);
+			singletons.getSlashActionRepo().execute("/" + cmd);
 		}
 		this.componentElement.dispatchEvent(new Event("close"));
 		event.preventDefault();
