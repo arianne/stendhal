745333dce34f5d5ecef1b6727c32b753edf43ff5
Jordan Irwin <antumdeluge@gmail.com>
2024-01-22 23:34:37 -0800

    Make quick menu buttons components

--- srcjs/stendhal/ui/quickmenu/ButtonBase.ts
+++ srcjs/stendhal/ui/quickmenu/ButtonBase.ts
@@ -9,37 +9,31 @@
  *                                                                         *
  ***************************************************************************/
 
+import { Component } from "../toolkit/Component";
 import { Paths } from "../../data/Paths";
 
 
-export abstract class ButtonBase {
-
-	private element: HTMLImageElement;
-
+export abstract class ButtonBase extends Component {
 
 	protected constructor(id: string) {
-		this.element = document.getElementById("qm-" + id)! as HTMLImageElement;
-		this.element.src = Paths.gui + "/quickmenu/" + id + ".png";
-		this.element.style["cursor"] = "url(" + Paths.sprites + "/cursor/highlight.png) 1 3, auto";
-		this.element.draggable = false;
+		super("qm-" + id);
+		(this.componentElement as HTMLImageElement).src = Paths.gui + "/quickmenu/" + id + ".png";
+		this.componentElement.style["cursor"] = "url(" + Paths.sprites + "/cursor/highlight.png) 1 3, auto";
+		this.componentElement.draggable = false;
 		// listen for click events
-		this.element.addEventListener("click", (evt: Event) => {
+		this.componentElement.addEventListener("click", (evt: Event) => {
 			this.onClick(evt);
 		});
 		this.update();
 	}
 
-	public setVisible(visible: boolean) {
-		this.element.style["display"] = visible ? "block" : "none";
-	}
-
 	public setPos(x: number, y: number) {
-		this.element.style["left"] = x + "px";
-		this.element.style["top"] = y + "px";
+		this.componentElement.style["left"] = x + "px";
+		this.componentElement.style["top"] = y + "px";
 	}
 
 	protected setImageBasename(basename: string) {
-		this.element.src = Paths.gui + "/quickmenu/" + basename + ".png";
+		(this.componentElement as HTMLImageElement).src = Paths.gui + "/quickmenu/" + basename + ".png";
 	}
 
 	protected abstract onClick(evt: Event): void;
