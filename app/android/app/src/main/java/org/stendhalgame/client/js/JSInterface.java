1a3d31f432e8d3cb3d4c7c851d7084d8916be60e
Jordan Irwin <antumdeluge@gmail.com>
2024-01-25 11:33:11 -0800

    Restore accidentally deleted JSInterface

---
+++ app/android/app/src/main/java/org/stendhalgame/client/js/JSInterface.java
@@ -0,0 +1,34 @@
+/***************************************************************************
+ *                 Copyright © 2022-2024 - Faiumoni e. V.                  *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+package org.stendhalgame.client.js;
+
+
+public class JSInterface {
+
+	/** Singleton instance. */
+	private static JSInterface instance;
+
+
+	/**
+	 * Retrieves the singleton instance.
+	 */
+	public static JSInterface get() {
+		if (instance == null) {
+			instance = new JSInterface();
+		}
+		return instance;
+	}
+
+	private JSInterface() {
+		// singleton
+	}
+}
