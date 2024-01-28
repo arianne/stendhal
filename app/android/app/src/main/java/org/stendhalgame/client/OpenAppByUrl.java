0f39f2118314453ffcd6b58abb8b61b793593155
Hendrik Brummermann <nhnb@users.sourceforge.net>
2024-01-28 18:15:16 +0100

    proof of concept to redirect from browser back to the app

---
+++ app/android/app/src/main/java/org/stendhalgame/client/OpenAppByUrl.java
@@ -0,0 +1,20 @@
+package org.stendhalgame.client;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class OpenAppByUrl extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        // String action = intent.getAction();
+        Uri data = intent.getData();
+        DebugLog.debug("URL: " + data);
+
+     }
+
+}
