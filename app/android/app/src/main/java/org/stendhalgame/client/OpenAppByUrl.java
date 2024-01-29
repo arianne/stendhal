1e7bd342123a4247605c4e5ff6b449a52e25a854
Jordan Irwin <antumdeluge@gmail.com>
2024-01-29 15:55:06 -0800

    Clean up indentation

--- app/android/app/src/main/java/org/stendhalgame/client/OpenAppByUrl.java
+++ app/android/app/src/main/java/org/stendhalgame/client/OpenAppByUrl.java
@@ -7,14 +7,14 @@ import android.os.Bundle;
 
 public class OpenAppByUrl extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Intent intent = getIntent();
-        // String action = intent.getAction();
-        Uri data = intent.getData();
-        Logger.debug("URL: " + data);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Intent intent = getIntent();
+		// String action = intent.getAction();
+		Uri data = intent.getData();
+		Logger.debug("URL: " + data);
 
-     }
+	 }
 
 }
