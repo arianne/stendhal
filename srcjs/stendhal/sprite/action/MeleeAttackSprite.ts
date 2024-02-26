92d17e15b0944dca44c6d4b97dfa23437b467b3c
Jordan Irwin <antumdeluge@gmail.com>
2024-02-25 17:13:21 -0800

    Prepare melee attack sprite for multiple frames

--- srcjs/stendhal/sprite/action/MeleeAttackSprite.ts
+++ srcjs/stendhal/sprite/action/MeleeAttackSprite.ts
@@ -21,8 +21,12 @@ export class MeleeAttackSprite extends ActionSprite {
 
 	/** Direction entity is facing. */
 	private readonly dir: Direction;
-	/** Sprite image. */
-	private readonly image: HTMLImageElement;
+	/** Set of 3 frame images displayed in sequence. */
+	private readonly frames: HTMLImageElement[];
+	/** Pixel offsets for each frame on X axis. */
+	private readonly offsetX: number[];
+	/** Pixel offsets for each frame on Y axis. */
+	private readonly offsetY: number[];
 
 
 	/**
@@ -36,48 +40,93 @@ export class MeleeAttackSprite extends ActionSprite {
 	constructor(source: RPEntity, imagePath: string) {
 		super();
 		this.dir = Direction.VALUES[source["dir"]];
+		this.offsetX = [];
+		this.offsetY = [];
 		const rot = 90 * (this.dir.val - 1);
-		// TODO: rotate left & right 45 degrees & offset to center on entity
-		this.image = stendhal.data.sprites.getRotated(imagePath, rot);
+		imagePath = imagePath.replace(/\.png$/, "");
+		this.frames = [
+			//~ stendhal.data.sprites.getRotated(imagePath + "_rot45.png", rot-90),
+			undefined,
+			stendhal.data.sprites.getRotated(imagePath + ".png", rot),
+			//~ stendhal.data.sprites.getRotated(imagePath + "_rot45.png", rot)
+			undefined
+		];
+		if (this.frames[1].complete) {
+			this.onFrameReady(this.frames[1]);
+		} else {
+			this.frames[1].onload = () => {
+				this.frames[1].onload = null;
+				this.onFrameReady(this.frames[1]);
+			};
+		}
+	}
+
+	/**
+	 * Configures frame offsets when at least one frame image is ready.
+	 *
+	 * @param frame {HTMLImageElement}
+	 *   Image used to calculate offsets.
+	 */
+	private onFrameReady(frame: HTMLImageElement) {
+		// NOTE: all frame images should be same dimensions
+		const halfW = Math.floor(frame.width / 2);
+		const halfH = Math.floor(frame.height / 2);
+		// offset for facing direction
+		// FIXME: alignment needs improvement
+		switch (this.dir) {
+			case Direction.UP:
+				this.offsetX[0] = -frame.width;
+				this.offsetX[1] = -halfW;
+				this.offsetX[2] = 0;
+				this.offsetY[0] = -frame.height;
+				this.offsetY[1] = -frame.height;
+				this.offsetY[2] = -frame.height;
+				break;
+			case Direction.DOWN:
+				this.offsetX[0] = 0;
+				this.offsetX[1] = -halfW;
+				this.offsetX[2] = -frame.width;
+				this.offsetY[0] = 0;
+				this.offsetY[1] = 0;
+				this.offsetY[2] = 0;
+				break;
+			case Direction.LEFT:
+				this.offsetX[0] = -frame.width;
+				this.offsetX[1] = -frame.width;
+				this.offsetX[2] = -frame.width;
+				this.offsetY[0] = 0;
+				this.offsetY[1] = -halfH;
+				this.offsetY[2] = -frame.height;
+				break;
+			default: // RIGHT
+				this.offsetX[0] = 0;
+				this.offsetX[1] = 0;
+				this.offsetX[2] = 0;
+				this.offsetY[0] = -frame.height;
+				this.offsetY[1] = -halfH;
+				this.offsetY[2] = 0;
+		}
 	}
 
 	public override draw(ctx: CanvasRenderingContext2D, x: number, y: number, entityWidth: number,
 			entityHeight: number) {
-		if (!this.image || !this.image.height) {
+		const dtime = Date.now() - this.initTime;
+		const frameIndex = Math.floor(Math.min(dtime / 60, 2));
+		// TODO: add rotated images for each attack type
+		//~ const frame = this.frames[frameIndex];
+		const frame = this.frames[1];
+		if (!frame || !frame.height) {
 			return;
 		}
 
-		//~ const dtime = Date.now() - this.initTime;
-		//~ const frameIndex = Math.floor(Math.min(dtime / 60, 2));
-		const drawWidth = this.image.width;
-		const drawHeight = this.image.height;
-		var centerX = x + (entityWidth - drawWidth) / 2;
-		var centerY = y + (entityHeight - drawHeight) / 2;
+		const cx = x + Math.floor(entityWidth / 2);
+		const cy = y + Math.floor(entityHeight / 2);
+		// FIXME: offset should compensate for entity width/height
+		//~ const dx = cx + this.offsetX[frameIndex];
+		//~ const dy = cy + this.offsetY[frameIndex];
+		const dx = cx + this.offsetX[1];
+		const dy = cy + this.offsetY[1];
 
-		// offset sprite for facing direction
-		let sx, sy;
-		switch (this.dir) {
-			case Direction.UP:
-				sx = centerX + (stendhal.ui.gamewindow.targetTileWidth / 2);
-				sy = y - (stendhal.ui.gamewindow.targetTileHeight * 1.5);
-				break;
-			case Direction.DOWN:
-				sx = centerX;
-				sy = y + entityHeight - drawHeight + (stendhal.ui.gamewindow.targetTileHeight / 2);
-				break;
-			case Direction.LEFT:
-				sx = x - (stendhal.ui.gamewindow.targetTileWidth / 2);
-				sy = centerY - (stendhal.ui.gamewindow.targetTileHeight / 2);
-				break;
-			case Direction.RIGHT:
-				sx = x + entityWidth - drawWidth + (stendhal.ui.gamewindow.targetTileWidth / 2);
-				sy = centerY; // - ICON_OFFSET; // ICON_OFFSET = 8 in Java client
-				break;
-			default:
-				sx = centerX;
-				sy = centerY;
-		}
-
-		ctx.drawImage(this.image, 0, 0, drawWidth, drawHeight, sx, sy, drawWidth, drawHeight);
+		ctx.drawImage(frame, 0, 0, frame.width, frame.height, dx, dy, frame.width, frame.height);
 	}
 }
