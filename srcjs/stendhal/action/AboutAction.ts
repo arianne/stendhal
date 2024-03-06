c66e01678c6f528e0dd352ca9d43454e6c9d6af8
Jordan Irwin <antumdeluge@gmail.com>
2024-03-06 06:34:00 -0800

    Dynamically create slash action help info

--- srcjs/stendhal/action/AboutAction.ts
+++ srcjs/stendhal/action/AboutAction.ts
@@ -1,5 +1,6 @@
 /***************************************************************************
- *                       Copyright © 2023 - Stendhal                       *
+ *                 Copyright © 2023-2024 - Faiumoni e. V.                  *
+ ***************************************************************************
  ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -11,11 +12,14 @@
 
 declare var stendhal: any;
 
+import { ParamList } from "./ParamList";
 import { SlashAction } from "./SlashAction";
 
 import { ui } from "../ui/UI";
 
 import { AboutDialog } from "../ui/dialog/AboutDialog";
+
+import { Pair } from "../util/Pair";
 
 
 export class AboutAction extends SlashAction {
@@ -33,5 +37,9 @@ export class AboutAction extends SlashAction {
 		dialog.setId("about");
 		content.setFrame(dialog);
 		return true;
+	}
+
+	override getHelp(params?: ParamList|Pair<string, string>[]): string[] {
+		return ["", "Show dialog with information about project."];
 	}
 }
