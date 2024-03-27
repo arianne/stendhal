f790e89bf75223f1584c3a6a242a57c3e13ad84b
Hendrik Brummermann <nhnb@users.sourceforge.net>
2024-03-28 00:19:12 +0100

    implemented updater

--- app/neutralinojs/resources/js/main.js
+++ app/neutralinojs/resources/js/main.js
@@ -2,6 +2,9 @@ const SERVER_ORIGIN = "https://stendhalgame.org";
 // const SERVER_ORIGIN = "http://localhost";
 const SERVER_PATH = "/account/login.html";
 
+const UPDATE_URL = "https://arianne-project.org/stendhal/updates/manifest.json";
+
+
 let authenticated = false;
 
 const byteToHex = [];
@@ -28,8 +31,24 @@ function onSteamAuthToken(event) {
 	document.querySelector("form").submit();
 }
 
+async function update() {
+	try {
+		let manifest = await Neutralino.updater.checkForUpdates(UPDATE_URL);
+
+		if(manifest.version > NL_APPVERSION) {
+			console.log("Updating...");
+			await Neutralino.updater.install();
+		} else {
+			console.log('Stendhal is up to date.');
+		}
+	} catch (e) {
+		console.log("Update check failed", e);
+	}
+}
+
 function onNoAuthToken(_event) {
 	console.log("onNoAuthToken");
+	update();
 	window.location = SERVER_ORIGIN + SERVER_PATH + "?" + Date.now();
 }
 
@@ -60,4 +79,4 @@ setTimeout(() => timeoutExtensionConnection(), 2000);
 
 Neutralino.events.on("steamAuthToken", onSteamAuthToken);
 Neutralino.events.on("noAuthToken", onNoAuthToken);
-Neutralino.extensions.dispatch('nativehelper', 'request_authentication');
+Neutralino.extensions.dispatch("nativehelper", "request_authentication");
