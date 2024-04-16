412bcb28bc82d8e42b812693cdcf0fbbdb2d21e3
Jordan Irwin <antumdeluge@gmail.com>
2024-04-15 18:29:33 -0700

    Update Amazon Princess quest for checking completions
    https://github.com/arianne/stendhal/issues/230

--- src/games/stendhal/server/maps/quests/AmazonPrincess.java
+++ src/games/stendhal/server/maps/quests/AmazonPrincess.java
@@ -92,6 +92,9 @@ public class AmazonPrincess implements QuestManuscript {
 			.rewardWith(new PlaySoundAction("kiss-female-01"))
 			.rewardWith(new InflictStatusOnNPCAction("pina colada"));
 
+		// completions count is stored in 2nd index of quest slot
+		quest.setCompletionsIndexes(2);
+
 		return quest;
 	}
 
