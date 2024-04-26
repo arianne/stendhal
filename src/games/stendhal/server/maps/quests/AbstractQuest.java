53109c85f541d9bc79d751a1c244e52905ddd223
Jordan Irwin <antumdeluge@gmail.com>
2024-04-26 01:13:51 -0700

    Quest super class for tracking completions

--- src/games/stendhal/server/maps/quests/AbstractQuest.java
+++ src/games/stendhal/server/maps/quests/AbstractQuest.java
@@ -15,7 +15,6 @@ package games.stendhal.server.maps.quests;
 import java.util.ArrayList;
 import java.util.List;
 
-import games.stendhal.common.MathHelper;
 import games.stendhal.server.core.engine.SingletonRepository;
 import games.stendhal.server.entity.npc.NPCList;
 import games.stendhal.server.entity.player.Player;
@@ -161,23 +160,7 @@ public abstract class AbstractQuest implements IQuest {
 	 */
 	@Override
 	public int getCompletions(final Player player) {
-		final String questSlot = getSlotName();
-		final boolean completed = isCompleted(player);
-		if (player.hasQuest(questSlot)) {
-			final String[] state = player.getQuest(questSlot).split(";");
-			final Pair<Integer, Integer> completionsIndexes = questInfo.getCompletionsIndexes();
-			Integer stateIndex = null;
-			if (completed) {
-				stateIndex = completionsIndexes.second();
-			} else {
-				stateIndex = completionsIndexes.first();
-			}
-			if (stateIndex != null && state.length > stateIndex && !"".equals(state[stateIndex])) {
-				return MathHelper.parseIntDefault(state[stateIndex], completed ? 1 : 0);
-			}
-		}
-		// default is to return 1 if quest is in complete state and 0 otherwise
-		return completed ? 1 : 0;
+		return isCompleted(player) ? 1 : 0;
 	}
 
 	/**
