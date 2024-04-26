4bc4732d9a797ac90234554fd6e8cfd48658113e
Jordan Irwin <antumdeluge@gmail.com>
2024-04-25 21:08:22 -0700

    Add "menubutton" class to character select buttons
    https://github.com/arianne/stendhal/issues/417

--- src/js/stendhal/ui/dialog/ChooseCharacterDialog.ts
+++ src/js/stendhal/ui/dialog/ChooseCharacterDialog.ts
@@ -1,5 +1,5 @@
 /***************************************************************************
- *                (C) Copyright 2015-2023 - Faiumoni e. V.                 *
+ *                (C) Copyright 2015-2024 - Faiumoni e. V.                 *
  ***************************************************************************
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -24,6 +24,7 @@ export class ChooseCharacterDialog extends DialogContentComponent {
 			if (characters.hasOwnProperty(i)) {
 				let name = characters[i]["a"]["name"];
 				let button = document.createElement("button");
+				button.classList.add("menubutton");
 				button.innerText = name;
 				button.addEventListener("click", () => {
 					this.componentElement.dispatchEvent(new Event("close"));
