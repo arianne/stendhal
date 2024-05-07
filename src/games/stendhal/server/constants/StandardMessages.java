2c9bb438158e88098576ba9ccd7482466d641e9e
Jordan Irwin <antumdeluge@gmail.com>
2024-05-07 04:37:59 -0700

    Make standard messages more robust

--- src/games/stendhal/server/constants/StandardMessages.java
+++ src/games/stendhal/server/constants/StandardMessages.java
@@ -18,83 +18,190 @@ import games.stendhal.server.entity.player.Player;
 public abstract class StandardMessages {
 
 	/**
-	 * Sends a message to target notifying that the specified player was
-	 * not found online.
+	 * Processes a message.
 	 *
 	 * @param target
-	 *     Player being notified.
+	 *   Player receiving message or {@code null}.
+	 * @param _type
+	 *   Message type or {@code null} for default type.
+	 * @param msg
+	 *   Message text.
+	 * @return
+	 *   Message text.
+	 */
+	private static String process(final Player target, final NotificationType _type,
+			final String msg) {
+		if (target != null) {
+			if (_type != null) {
+				target.sendPrivateText(_type, msg);
+			} else {
+				target.sendPrivateText(msg);
+			}
+		}
+		return msg;
+	}
+
+	/**
+	 * Processes a message.
+	 *
+	 * @param target
+	 *   Player receiving message or {@code null}.
+	 * @param msg
+	 *   Message text.
+	 * @return
+	 *   Message text.
+	 */
+	private static String process(final Player target, final String msg) {
+		return process(target, null, msg);
+	}
+
+	/**
+	 * Processes an error type message.
+	 *
+	 * @param target
+	 *   Player receiving message or {@code null}.
+	 * @param msg
+	 *   Message text.
+	 * @return
+	 *   Message text
+	 */
+	private static String processError(final Player target, final String msg) {
+		return process(target, NotificationType.ERROR, msg);
+	}
+
+	/**
+	 * Specified player was not found online.
+	 *
+	 * @param target
+	 *   Player receiving message or {@code null}.
 	 * @param subject
-	 *     Name of the subject player.
+	 *   Name of the subject player.
+	 * @return
+	 *   Message text.
 	 */
-	public static void playerNotOnline(final Player target, final String subject) {
-		target.sendPrivateText("No player named \"" + subject + "\" is currently logged in.");
+	public static String playerNotOnline(final Player target, final String subject) {
+		return process(target, "No player named \"" + subject + "\" is currently logged in.");
 	}
 
 	/**
 	 * Script or command executed with too few parameters.
+	 *
+	 * @param target
+	 *   Player receiving message or {@code null}.
+	 * @return
+	 *   Message text.
 	 */
-	public static void missingParameter(final Player target) {
-		target.sendPrivateText(NotificationType.ERROR, "Missing parameter.");
+	public static String missingParameter(final Player target) {
+		return processError(target, "Missing parameter.");
 	}
 
 	/**
 	 * Script or command executed with too few parameters.
+	 *
+	 * @param target
+	 *   Player receiving message or {@code null}.
+	 * @param cmd
+	 *   Name of command that was to be executed.
+	 * @return
+	 *   Message text.
 	 */
-	public static void missingParameter(final Player target, final String command) {
-		target.sendPrivateText(NotificationType.ERROR, "Missing parameter for command \"" + command
-				+ "\".");
+	public static String missingParameter(final Player target, final String cmd) {
+		return processError(target, "Missing parameter for command \"" + cmd + "\".");
 	}
 
 	/**
 	 * Command parameter requires an additional value parameter.
 	 *
 	 * @param target
-	 *   Player being notified.
+	 *   Player receiving message or {@code null}.
 	 * @param param
-	 *   Parameter name.
+	 *   Name of parameter.
+	 * @return
+	 *   Message text.
 	 */
-	public static void missingParamValue(final Player target, final String param) {
-		target.sendPrivateText(NotificationType.ERROR, "Missing value for parameter \"" + param
-				+ "\".");
+	public static String missingParamValue(final Player target, final String param) {
+		return processError(target, "Missing value for parameter \"" + param + "\".");
 	}
 
 	/**
 	 * Script or command executed with too many parameters.
+	 *
+	 * @param target
+	 *   Player receiving message or {@code null}.
+	 * @return
+	 *   Message text.
 	 */
-	public static void excessParameter(final Player target) {
-		target.sendPrivateText(NotificationType.ERROR, "Too many parameters.");
+	public static String excessParameter(final Player target) {
+		return processError(target, "Too many parameters.");
 	}
 
 	/**
 	 * Script or command executed with too many parameters.
+	 *
+	 * @param target
+	 *   Player receiving message or {@code null}.
+	 * @param cmd
+	 *   Name of command that was to be executed.
+	 * @return
+	 *   Message text.
 	 */
-	public static void excessParameter(final Player target, final String command) {
-		target.sendPrivateText(NotificationType.ERROR, "Too many parameters for command \"" + command
-				+ "\".");
-	}
-
-	/**
-	 * Script or command executed with unacceptable parameter
-	 */
-	public static void unknownCommand(final Player target, final String cmd) {
-		target.sendPrivateText(NotificationType.ERROR, "Unknown command: " + cmd);
+	public static String excessParameter(final Player target, final String cmd) {
+		return processError(target, "Too many parameters for command \"" + cmd + "\".");
 	}
 
 	/**
 	 * Script or command executed with unacceptable parameter.
+	 *
+	 * @param target
+	 *   Player receiving message or {@code null}.
+	 * @param cmd
+	 *   Name of command that was to be executed.
+	 * @return
+	 *   Message text.
 	 */
-	public static void unknownParameter(final Player target, final String param) {
-		target.sendPrivateText(NotificationType.ERROR, "Unknown parameter: " + param);
+	public static String unknownCommand(final Player target, final String cmd) {
+		return processError(target, "Unknown command: " + cmd);
 	}
 
-	public static void paramMustBeNumber(final Player target, final String param) {
-		target.sendPrivateText(NotificationType.ERROR, param + " must be a number.");
+	/**
+	 * Script or command executed with unacceptable parameter.
+	 *
+	 * @param target
+	 *   Player receiving message or {@code null}.
+	 * @param param
+	 *   Name of parameter.
+	 * @return
+	 *   Message text.
+	 */
+	public static String unknownParameter(final Player target, final String param) {
+		return processError(target, "Unknown parameter: " + param);
 	}
 
-	public static void paramMustBeNumber(final Player target) {
-		paramMustBeNumber(target, "Parameter");
+	/**
+	 * Script or command executed with unacceptable parameter type.
+	 *
+	 * @param target
+	 *   Player receiving message or {@code null}.
+	 * @param param
+	 *   Name of parameter.
+	 * @return
+	 *   Message text.
+	 */
+	public static String paramMustBeNumber(final Player target, final String param) {
+		return processError(target, param + " must be a number.");
 	}
 
+	/**
+	 * Script or command executed with unacceptable parameter type.
+	 *
+	 * @param target
+	 *   Player receiving message or {@code null}.
+	 * @return
+	 *   Message text.
+	 */
+	public static String paramMustBeNumber(final Player target) {
+		return paramMustBeNumber(target, "Parameter");
+	}
 
 	/**
 	 * Messages admin and player when admin makes a change to player's quest state.
