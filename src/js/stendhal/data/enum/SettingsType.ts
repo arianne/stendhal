a47ccb6e2ff66a1fb4086c722f53b3a45654c404
Jordan Irwin <antumdeluge@gmail.com>
2024-05-10 05:28:06 -0700

    Component class to represent configuration setting element

---
+++ src/js/stendhal/data/enum/SettingsType.ts
@@ -0,0 +1,28 @@
+/***************************************************************************
+ *                    Copyright © 2024 - Faiumoni e. V.                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Affero General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
+import { Enum } from "../enum/Enum";
+
+
+/**
+ * Defines the different types of settings elements available.
+ */
+export class SettingsType extends Enum {
+	/** Type for text input. */
+	static readonly TEXT = new SettingsType("text");
+	/** Type for numeric input. */
+	static readonly NUMBER = new SettingsType("number");
+	/** Type for dual state checkbox. */
+	static readonly CHECK = new SettingsType("checkbox");
+	/** Type for multi-select enumeration. */
+	static readonly SELECT = new SettingsType("select");
+}
---
+++ src/js/stendhal/ui/toolkit/SettingsComponent.ts
@@ -0,0 +1,214 @@
+/***************************************************************************
+ *                    Copyright © 2024 - Faiumoni e. V.                    *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU Affero General Public License as        *
+ *   published by the Free Software Foundation; either version 3 of the    *
+ *   License, or (at your option) any later version.                       *
+ *                                                                         *
+ ***************************************************************************/
+
+import { ComponentBase } from "./ComponentBase";
+
+import { OptionsEnum } from "../../data/enum/OptionsEnum";
+import { SettingsType } from "../../data/enum/SettingsType";
+
+
+/**
+ * Unthemed component representing a configuration setting.
+ */
+export class SettingsComponent extends ComponentBase {
+
+	/** The `HTMLElement` associated with this component. */
+	override readonly componentElement!: HTMLElement;
+	/** Text description. */
+	readonly labelElement: HTMLLabelElement;
+	/** Setting type. */
+	private readonly _type: SettingsType;
+	/** Called when the settings state or value changes. */
+	public onchange?: Function;
+
+
+	/**
+	 * Creates a new settings component.
+	 *
+	 * @param id {string}
+	 *   DOM element ID.
+	 * @param label {string}
+	 *   Text label to display.
+	 * @param _type {data.enum.SettingsType.SettingsType}
+	 *   The type of this component. E.g. check box, text input, etc. (default: check box).
+	 * @param options {data.enum.OptionsEnum.OptionsEnum}
+	 *   Options for multi-select enumeration (default: empty object).
+	 * @param experimental {boolean}
+	 *   Marks this element to be hidden unless settings debugging is enabled.
+	 */
+	constructor(id: string, label: string, _type=SettingsType.CHECK, options: OptionsEnum={},
+			experimental=false) {
+		super();
+		this._type = _type;
+		// create label first
+		this.labelElement = document.createElement("label") as HTMLLabelElement;
+
+		if (SettingsType.SELECT.equals(_type)) {
+			this.componentElement = this.initSelect(id, label, options);
+		} else {
+			this.componentElement = this.initInput(id, label);
+		}
+
+		// listen for changes to component element
+		this.componentElement.addEventListener("change", (evt: Event) => {
+			if (this.onchange) {
+				this.onchange(evt);
+			}
+		});
+
+		if (experimental) {
+			this.componentElement.classList.add("experimental");
+		}
+	}
+
+	/**
+	 * Initializes main component element as input type.
+	 *
+	 * @param id {string}
+	 *   DOM element ID.
+	 * @param label {string}
+	 *   Text label to display.
+	 * @return {HTMLElement}
+	 *   Main component element.
+	 */
+	private initInput(id: string, label: string): HTMLElement {
+		let componentElement: HTMLElement;
+		switch(this._type) {
+			case SettingsType.CHECK:
+				this.labelElement.classList.add("checksetting");
+				const inputHTML = "<input type=\"" + this._type.value + "\" id=\"" + id + "\">";
+				this.labelElement.innerHTML = inputHTML + label;
+				componentElement = this.labelElement.querySelector("#" + id)!;
+				break;
+			default:
+				this.labelElement.htmlFor = id;
+				this.labelElement.innerText = label;
+				componentElement = document.createElement("input");
+				const inputElement = (componentElement as HTMLInputElement);
+				inputElement.type = this._type.value;
+				inputElement.id = id;
+		}
+		return componentElement;
+	}
+
+	/**
+	 * Initializes main component element as select type.
+	 *
+	 * @param id {string}
+	 *   DOM element ID.
+	 * @param label {string}
+	 *   Text label to display.
+	 * @param options {data.enum.OptionsEnum.OptionsEnum}
+	 *   Options for multi-select enumeration (default: empty object).
+	 * @return {HTMLElement}
+	 *   Main component element.
+	 */
+	private initSelect(id: string, label: string, options: OptionsEnum={}): HTMLElement {
+		this.labelElement.htmlFor = id;
+		this.labelElement.innerText = label;
+		const componentElement = document.createElement("select");
+		componentElement.id = id;
+
+		// populate options
+		for (const ol in options) {
+			this.addOption(ol, options[ol]);
+		}
+		this.setSelected(0);
+		return componentElement;
+	}
+
+	/**
+	 * Sets selected index.
+	 *
+	 * @param idx {number}
+	 *   Index to set as selected.
+	 */
+	setSelected(idx: number) {
+		if (!SettingsType.SELECT.equals(this._type)) {
+			throw new Error("Settings component of type \"" + this._type.value
+					+ "\" does not support index selection");
+		}
+		const selectElement = this.componentElement as HTMLSelectElement;
+		if (selectElement.options.length <= idx) {
+			console.warn("Invalid index of HTMLSelectElement");
+			return;
+		}
+		if (idx > -1) {
+			selectElement.selectedIndex = idx;
+		}
+	}
+
+	/**
+	 * Sets value of component element.
+	 *
+	 * @param value {string|number|boolean}
+	 *   New value to be used.
+	 */
+	setValue(value: string|number|boolean) {
+		switch(this._type) {
+			case SettingsType.SELECT:
+				const options = Array.from((this.componentElement as HTMLSelectElement).options)
+						.map(o => o.value);
+				const idx = typeof(value) === "number" ? value as number : options.indexOf(value as string);
+				this.setSelected(idx);
+				break;
+			case SettingsType.CHECK:
+				(this.componentElement as HTMLInputElement).checked = value as boolean;
+				break;
+			default:
+				// assume default to be text or numeric
+				(this.componentElement as HTMLInputElement).value = value as string;
+		}
+	}
+
+	/**
+	 * Adds a selectable option.
+	 *
+	 * @param label {string}
+	 *   Displayed text.
+	 * @param value {string}
+	 *   Value the option represents (default: empty string).
+	 */
+	addOption(label: string, value="") {
+		if (!SettingsType.SELECT.equals(this._type)) {
+			throw new Error("Settings component of type \"" + this._type.value
+					+ "\" does not support adding selection options");
+		}
+		if (!value) {
+			// use label as value default
+			value = label;
+		}
+		const selectElement = this.componentElement as HTMLSelectElement;
+		const opt = document.createElement("option");
+		opt.value = value;
+		opt.textContent = label;
+		selectElement.appendChild(opt);
+	}
+
+	/**
+	 * Adds as child to DOM element.
+	 *
+	 * @param parent {ui.toolkit.ComponentBase.ComponentBase|HTMLElement}
+	 *   Component or element to which to add.
+	 */
+	override addTo(parent: ComponentBase|HTMLElement) {
+		const isComponent = parent instanceof ComponentBase;
+		this.parentComponent = isComponent ? parent as ComponentBase : undefined;
+		const parentElement = isComponent ? this.parentComponent!.componentElement
+				: parent as HTMLElement;
+		parentElement.appendChild(this.labelElement);
+		if (!SettingsType.CHECK.equals(this._type)) {
+			// check box component element is already child of label
+			parentElement.appendChild(this.componentElement);
+		}
+	}
+}
