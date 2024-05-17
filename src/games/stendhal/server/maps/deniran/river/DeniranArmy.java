ae99d29757113dee988245ab0f87a1c2dea769b6
Jordan Irwin <antumdeluge@gmail.com>
2024-05-17 00:10:11 -0700

    Remove duplicate license boilerplate

--- src/games/stendhal/server/maps/deniran/river/DeniranArmy.java
+++ src/games/stendhal/server/maps/deniran/river/DeniranArmy.java
@@ -1,5 +1,5 @@
 /***************************************************************************
- *                   (C) Copyright 2003-2019 - Stendhal                    *
+ *                   (C) Copyright 2003-2024 - Stendhal                    *
  ***************************************************************************
  ***************************************************************************
  *                                                                         *
@@ -12,20 +12,6 @@
 package games.stendhal.server.maps.deniran.river;
 
 import java.util.LinkedList;
-
-/* $Id$ */
-/***************************************************************************
- *                   (C) Copyright 2003-2019 - Stendhal                    *
- ***************************************************************************
- ***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-
 import java.util.Map;
 
 import games.stendhal.common.Direction;
