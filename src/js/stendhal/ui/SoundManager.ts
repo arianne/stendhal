60173d582dbe979654731ab6cd2d31035243be13
Jordan Irwin <antumdeluge@gmail.com>
2024-05-31 19:43:53 -0700

    Move SoundLayer enum to data/sound

--- src/js/stendhal/ui/SoundManager.ts
+++ src/js/stendhal/ui/SoundManager.ts
@@ -16,10 +16,9 @@ import { ui } from "./UI";
 
 import { singletons } from "../SingletonRepo";
 
-import { SoundLayer } from "../data/enum/SoundLayer";
-
 import { SoundFactory } from "../data/sound/SoundFactory";
 import { SoundObject } from "../data/sound/SoundFactory";
+import { SoundLayer } from "../data/sound/SoundLayer";
 
 
 /**
