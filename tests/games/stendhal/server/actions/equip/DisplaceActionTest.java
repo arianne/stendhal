cdadb33ca044c80b4bdb77ef288b0cc191cfaf80
Jordan Irwin <antumdeluge@gmail.com>
2024-07-22 10:41:36 -0700

    Add license boilerplate

--- tests/games/stendhal/server/actions/equip/DisplaceActionTest.java
+++ tests/games/stendhal/server/actions/equip/DisplaceActionTest.java
@@ -1,4 +1,15 @@
 // $Id$
+/***************************************************************************
+ *                 Copyright © 2010-2024 - Faiumoni e. V.                  *
+ ***************************************************************************
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 package games.stendhal.server.actions.equip;
 
 import static org.junit.Assert.assertEquals;
