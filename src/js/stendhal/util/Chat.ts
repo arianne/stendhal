05d1fb34afbebfd7c5ab8e725c5bd19f375dc6b6
Hendrik Brummermann <nhnb@users.sourceforge.net>
2025-12-22 19:21:51 +0100

    properly format item names in description text of shop windows

--- src/js/stendhal/util/Chat.ts
+++ src/js/stendhal/util/Chat.ts
@@ -99,4 +99,121 @@ export class Chat {
 			Chat.log("client", message);
 		}
 	}
+
+	/**
+	 * Formats displayed text.
+	 *
+	 * @param message {string}
+	 *   Text to parse for special characters.
+	 * @return {string}
+	 *   Text formatted with keywords & item names highlighted.
+	 */
+	public static formatLogEntry(message: string): string {
+		if (!message) {
+			return "";
+		}
+		let res = "";
+		let delims = [" ", ",", ".", "!", "?", ":", ";"];
+		let length = message.length;
+		let inHighlight = false, inUnderline = false,
+			inHighlightQuote = false, inUnderlineQuote = false;
+		for (let i = 0; i < length; i++) {
+			let c = message[i];
+
+			if (c === "\\") {
+				let n = message[i + 1];
+				res += n;
+				i++;
+
+			// Highlight Start?
+			} else if (c === "#") {
+				if (inHighlight) {
+					res += c;
+					continue;
+				}
+				let n = message[i + 1];
+				if (n === "#") {
+					res += c;
+					i++;
+					continue;
+				}
+				if (n === "'") {
+					inHighlightQuote = true;
+					i++;
+				}
+				inHighlight = true;
+				res += "<span class=\"logh\">";
+
+			// Underline start?
+			} else if (c === "§") {
+				if (inUnderline) {
+					res += c;
+					continue;
+				}
+				let n = message[i + 1];
+				if (n === "§") {
+					res += c;
+					i++;
+					continue;
+				}
+				if (n === "'") {
+					inUnderlineQuote = true;
+					i++;
+				}
+				inUnderline = true;
+				res += "<span class=\"logi\">";
+
+			// End Highlight and Underline?
+			} else if (c === "'") {
+				if (inUnderlineQuote) {
+					inUnderline = false;
+					inUnderlineQuote = false;
+					res += "</span>";
+					continue;
+				}
+				if (inHighlightQuote) {
+					inHighlight = false;
+					inHighlightQuote = false;
+					res += "</span>";
+					continue;
+				}
+				res += c;
+
+			// HTML escape
+			} else if (c === "<") {
+				res += "&lt;";
+
+			// End of word
+			} else if (delims.indexOf(c) > -1) {
+				let n = message[i + 1];
+				if (c === " " || n === " " || n == undefined) {
+					if (inUnderline && !inUnderlineQuote && !inHighlightQuote) {
+						inUnderline = false;
+						res += "</span>" + c;
+						continue;
+					}
+					if (inHighlight && !inUnderlineQuote && !inHighlightQuote) {
+						inHighlight = false;
+						res += "</span>" + c;
+						continue;
+					}
+				}
+				res += c;
+
+			// Normal characters
+			} else {
+				res += c;
+			}
+		}
+
+		// Close opened formattings
+		if (inUnderline) {
+			res += "</span>";
+		}
+		if (inHighlight) {
+			res += "</span>";
+		}
+
+		return res;
+	}
 }
